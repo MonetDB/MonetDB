aa5ecf46762bd92e6db8b03d26888698e0e2beb4
Niels Nes <niels@cwi.nl>
2000-08-16 17:27:51 +0000

    moved some dummy defentions from  gdk_posix to winlibc.h

--- MonetDB/NT/winlibc.h
+++ MonetDB/NT/winlibc.h
@@ -22,6 +22,8 @@
  * For some functions, we provide emulators in glib, which are prefixed
  * with win_.
  */
+#include <winsock.h>
+#define sleep(s)
 #ifndef  __cplusplus
 /* first some dummies, later move to pwd.h */
 #define getpwuid(uid) NULL
@@ -50,8 +52,6 @@ struct passwd {
 #define F_ULOCK
 #define F_LOCK
 
-#define sleep(s)
-
 #define sysconf(flag) 0
 
 #define sigset_t int
@@ -68,10 +68,91 @@ struct passwd {
 
 #define link rename
 
+#define RUSAGE_SELF = 1
+
+/* Structure which says how much of each resource has been used.  */
+struct rusage
+  {
+    /* Total amount of user time used.  */
+    struct timeval ru_utime;
+    /* Total amount of system time used.  */
+    struct timeval ru_stime;
+    /* Maximum resident set size (in kilobytes).  */
+    long int ru_maxrss;
+    /* Amount of sharing of text segment memory
+       with other processes (kilobyte-seconds).  */
+    long int ru_ixrss;
+    /* Amount of data segment memory used (kilobyte-seconds).  */
+    long int ru_idrss;
+    /* Amount of stack memory used (kilobyte-seconds).  */
+    long int ru_isrss;
+    /* Number of soft page faults (i.e. those serviced by reclaiming
+       a page from the list of pages awaiting reallocation.  */
+    long int ru_minflt;
+    /* Number of hard page faults (i.e. those that required I/O).  */
+    long int ru_majflt;
+    /* Number of times a process was swapped out of physical memory.  */
+    long int ru_nswap;
+    /* Number of input operations via the file system.  Note: This
+       and `ru_oublock' do not include operations with the cache.  */
+    long int ru_inblock;
+    /* Number of output operations via the file system.  */
+    long int ru_oublock;
+    /* Number of IPC messages sent.  */
+    long int ru_msgsnd;
+    /* Number of IPC messages received.  */
+    long int ru_msgrcv;
+    /* Number of signals delivered.  */
+    long int ru_nsignals;
+    /* Number of voluntary context switches, i.e. because the process
+       gave up the process before it had to (usually to wait for some
+       resource to be available).  */
+    long int ru_nvcsw;
+    /* Number of involuntary context switches, i.e. a higher priority process
+       became runnable or the current process used up its time slice.  */
+    long int ru_nivcsw;
+  };
+
+#define getrusage(flag,usage) (int)memset((void*)usage, 0, sizeof(struct rusage))
+
+struct tms
+  {
+    int tms_utime;		/* User CPU time.  */
+    int tms_stime;		/* System CPU time.  */
+
+    int tms_cutime;		/* User CPU time of dead children.  */
+    int tms_cstime;		/* System CPU time of dead children.  */
+  };
+
+#define times(t) (int)memset(t,0,sizeof(struct tms))
+
+#define HZ 60
+#define asctime_r( _tm, _buf ) \
+        ( strcpy( (_buf), asctime( (_tm) ) ), \
+          (_buf) )
+
+#define ctime_r( _clock, _buf ) \
+        ( strcpy( (_buf), ctime( (_tm) ) ),  \
+          (_buf) )
+
+#define gmtime_r( _clock, _result ) \
+        ( *(_result) = *gmtime( (_clock) ), \
+          (_result) )
+
+#define localtime_r( _clock, _result ) \
+        ( *(_result) = *localtime( (_clock) ), \
+          (_result) )
+
+#define rand_r( _seed ) \
+        rand()
+
+
+
 #    define getcwd		_getcwd
 #    define getpid		_getpid
 #    define access		_access
 #	 define R_OK		1
+#	 define F_OK		0
 #    define open		_open
 #    define read		_read
 #    define write		_write
