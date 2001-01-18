109cd9063f622f2effbde2f8bfed7111ad614e9b
Niels Nes <niels@cwi.nl>
2001-01-18 11:56:21 +0000

    added checks for libz and libbz2

--- MonetDB/acconfig.h
+++ MonetDB/acconfig.h
@@ -6,6 +6,8 @@
 
 /* Define this if you have the readline library */
 #undef HAVE_LIBREADLINE
+#undef HAVE_LIBZ
+#undef HAVE_LIBBZ2
 
 /* Define this if you have ctime_r(time_t*,char *buf,size_t s) */
 #undef HAVE_CTIME_R3
