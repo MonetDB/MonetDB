7e26e5a71b57834296037600053e7b4d5f85f2de
Martin Kersten <mk@cwi.nl>
2001-11-25 19:35:33 +0000

    These files were not yet added to the V5, a new Mapi library for java is under development.

---
+++ MonetDB/src/modules/clients/Mapi.pm
@@ -0,0 +1,97 @@
+package Mapi;
+
+use strict;
+use IO::Socket;
+use IO::Handle;
+
+sub new {
+	
+    my($self,$server,$user) = @_;
+    my $self = {};
+    $self->{SERVER} = $server;
+    $self->{USER} = $user;
+    $self->{SOCKET} = new IO::Socket::INET( $server )
+	|| die "can't connect to $server : $!";
+	bless($self);
+    $self->{BUF} = "";
+    $self->{PROMPT} = "";
+    $self->cmd_intern("$user\n");
+    $self->result();
+    return $self;
+}
+
+sub mapiport_intern {
+  my $mapiport = 'localhost:50000';
+  $mapiport = $ENV{'MAPIPORT'} if defined($ENV{'MAPIPORT'});
+}
+
+sub hostname {
+  my ( $hostname ) = mapiport_intern() =~ /([^:]*)/;
+  $hostname = 'localhost' if ( $hostname eq '' );
+  return $hostname;
+}
+
+sub portnr {
+  my ( $portnr ) = mapiport_intern() =~ /:([^:]*)/;
+  $portnr = 50000 if ( $portnr eq '' );
+  return $portnr;
+}
+
+sub disconnect {
+
+    my ($self) = @_;
+    $self->cmd_intern("quit;\n");
+    $self->{SOCKET}->close;
+}
+
+sub cmd_intern {
+	
+    my($self,$cmd) = @_;
+    
+    #print $cmd;
+    #print "\n";
+    $self->{SOCKET}->send( $cmd ) 
+	|| die "can't send $cmd: $!";
+}
+
+sub result {
+	
+    my($self) = @_;
+    my($res,$buf,$index);
+    $buf = $self->{BUF};
+    while( ($index = index($self->{BUF},"\001")) < 0){
+	$self->{SOCKET}->recv($buf,4096);
+	$self->{BUF} = "$self->{BUF}$buf";
+    }
+    $res = substr($self->{BUF},0,$index);
+    $index += 1;
+    $self->{BUF} = substr($self->{BUF},$index,length($self->{BUF})); 
+    $self->getprompt();
+    return $res;
+}
+
+sub getprompt {
+
+    my($self) = @_;
+    my($prompt,$buf,$index);
+    $buf = $self->{BUF};
+    while( ($index = index($self->{BUF},"\001")) < 0){
+	$self->{SOCKET}->recv($buf,4096);
+	$self->{BUF} = "$self->{BUF}$buf";
+    }
+    $prompt = substr($self->{BUF},0,$index);
+    $index += 1;
+    $self->{BUF} = substr($self->{BUF},$index,length($self->{BUF})); 
+}
+
+sub cmd {
+    my($self,$cmd) = @_;
+    if (! ($cmd =~ /^.*\n$/)){
+	$cmd = "$cmd\n";
+    }
+    $self->cmd_intern($cmd);
+    return $self->result();
+}
+
+1;
+
