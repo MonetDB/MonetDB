01c3d1096d607b247335500a1f3e76a6a078cbce
Stefan Manegold <manegold@cwi.nl>
2001-12-14 17:41:26 +0000

    added multi-relations data mining example from Arno Knobbe

---
+++ sql/src/benchmarks/arno/load.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -x
+
+if [ $# -lt 1 ]; then
+	echo "usage load directory with data"
+	exit 1
+fi
+
+MapiClient <<EOF
+bulkload("atom",  "$1/ATOM.tbl",  ",", "\n", 4893);
+bulkload("bond",  "$1/BOND.tbl",  ",", "\n", 5243);
+bulkload("model", "$1/MODEL.tbl", ",", "\n",  188);
+commit;
+quit;
+EOF
+
