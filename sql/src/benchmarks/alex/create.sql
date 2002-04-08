6c3cca087e59f8697d1f77f188d4b40800450b8e
Niels Nes <niels@cwi.nl>
2002-04-08 15:47:27 +0000

    added create tables with 'strange type (UBYTE)'

---
+++ sql/src/benchmarks/alex/create.sql
@@ -0,0 +1,18 @@
+
+CREATE TABLE earth (
+   id  INTEGER NOT NULL,
+   x   INTEGER NOT NULL,
+   y   INTEGER NOT NULL,
+   val UBYTE   NOT NULL,
+   PRIMARY KEY (id,x,y)
+);
+     
+CREATE TABLE tomo (
+   x   INTEGER NOT NULL,
+   y   INTEGER NOT NULL,
+   z   INTEGER NOT NULL,
+   val UBYTE   NOT NULL,
+   PRIMARY KEY (x,y,z)
+);
+
+
