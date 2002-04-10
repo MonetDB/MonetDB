83542f6a85f2ddcefd344d238700b8cc48614efc
Niels Nes <niels@cwi.nl>
2002-04-10 14:52:02 +0000

    using COPY command to load data

---
+++ sql/src/benchmarks/alex/load.sql
@@ -0,0 +1,4 @@
+--COPY 9600000 RECORDS INTO earth from 'PWD/earth.tab' USING DELIMITERS ',','\n';
+--COPY 10092544 RECORDS INTO tomo from 'PWD/tomo.tab' USING DELIMITERS ',','\n';
+COPY 96 RECORDS INTO earth from 'PWD/earth.tab' USING DELIMITERS ',','\n';
+COPY 100 RECORDS INTO tomo from 'PWD/tomo.tab' USING DELIMITERS ',','\n';
--- sql/src/benchmarks/alex/load_data.sh
+++ sql/src/benchmarks/alex/load_data.sh
@@ -1,26 +1,31 @@
 #!/bin/sh
-
 PWD=`pwd`
 echo $PWD
+SQL=sql_client
+if [ $# -eq 1 ]; then
+	SQL=$1
+fi
 
 #Generate Data...
 
-gcc -o earth earth.c
-./earth
+#gcc -o earth earth.c
+#./earth
 
-gcc -o tomo  tomo.c
-./tomo
+#gcc -o tomo  tomo.c
+#./tomo
 
 #Bulk-Load into SQL server...
 
 sql_client < create.sql
+cat load.sql | sed -e s+PWD+$PWD+ | $SQL 
 
-MapiClient <<EOF
-bulkload("earth", "$PWD/earth.tab", ",", "\n", 9600000);
-bulkload("tomo",  "$PWD/tomo.tab",  ",", "\n", 10092544);
-commit;
-quit;
-EOF
+exit
+#MapiClient <<EOF
+#bulkload("earth", "$PWD/earth.tab", ",", "\n", 9600000);
+#bulkload("tomo",  "$PWD/tomo.tab",  ",", "\n", 10092544);
+#commit;
+#quit;
+#EOF
 
 #Cleanup
 
