b2e3c2c725d7d7e8ec5d2f1ead3bebb7fd371710
Niels Nes <niels@cwi.nl>
2002-06-26 19:24:51 +0000

    removed --enable-getopt lines new config.guess/config.sub fallback getopt.h moved to monet_getopt.h which is conditionaly included in monet_options.h, ie. users should include monet_options and 	server/clients of monet should use -I${top_srcdir}/src/common and for 	projects based upon monet should use -I$(MONET_PREFIX)/include/common
    moved more win-specific defines to monet_utils
    
    calib_common fix include time.h

--- MonetDB/src/modules/calibrator/calib_common.c
+++ MonetDB/src/modules/calibrator/calib_common.c
@@ -39,7 +39,18 @@ caliblng now(void)
 
 #else
 
-#include <sys/time.h>
+# include <config.h>
+
+#if TIME_WITH_SYS_TIME
+# include <sys/time.h>
+# include <time.h>
+#else
+# if HAVE_SYS_TIME_H
+#  include <sys/time.h>
+# else
+#  include <time.h>
+# endif
+#endif
 
 /*	#define	MINTIME  10000	*/
 
