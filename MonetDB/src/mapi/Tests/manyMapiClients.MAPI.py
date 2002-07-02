c6cefbd68defc0a443b1d8d55b2d37db2d804174
Stefan Manegold <manegold@cwi.nl>
2002-07-02 12:05:03 +0000

    fixed typ(e|o)

--- MonetDB/src/mapi/Tests/manyMapiClients.MAPI.py
+++ MonetDB/src/mapi/Tests/manyMapiClients.MAPI.py
@@ -20,7 +20,7 @@ while i < n:
     STDOUT.write("%d:\n" % i)
     STDERR.write("%d:\n" % i)
 
-    s = Mapi.server( "localhost", os.environ['MAPIPORT'], os.environ['USER'])
+    s = Mapi.server( "localhost", int(os.environ['MAPIPORT']), os.environ['USER'])
     print( s.cmd( "print(%d);\n" % i ) )
     s.disconnect()
 STDOUT.write("done: %d\n" % i)
