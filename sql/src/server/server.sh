1de210214d56a87a64ca5b50e0067529035a9c12
cvs2hg <unknown>
2002-07-09 12:46:03 +0000

    fixup commit for tag 'template-0'

--- sql/src/server/server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#$MONET_PREFIX/share/Monet/mapi.mil \
-
-Mserver --debug=34 --config ~/.monet.conf --dbname sql \
-	$SQL_PREFIX/share/sql/catalog.mil $SQL_PREFIX/share/sql/sqlserver.mil 
