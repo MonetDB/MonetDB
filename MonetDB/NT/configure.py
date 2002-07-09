4f9bdb5872a17678d895a468c483e9daf598435a
Sjoerd Mullender <sjoerd@acm.org>
2002-07-09 08:58:22 +0000

    Convert tabs to spaces.

--- MonetDB/NT/configure.py
+++ MonetDB/NT/configure.py
@@ -4,20 +4,20 @@ import re
 import sys
 import fileinput
 
-subs = [ 
-	('@prefix@',		'prefix'),
-	('@exec_prefix@',	'prefix'),
-	('@sysconfdir@',	'prefix/etc'),
-	('@localstatedir@',	'prefix/share'),
-	('@libdir@',		'prefix/lib'),
-	('@bindir@',		'prefix/bin'),
-	('@PACKAGE@',		'MonetDB')
+subs = [
+    ('@prefix@',            'prefix'),
+    ('@exec_prefix@',       'prefix'),
+    ('@sysconfdir@',        'prefix/etc'),
+    ('@localstatedir@',     'prefix/share'),
+    ('@libdir@',            'prefix/lib'),
+    ('@bindir@',            'prefix/bin'),
+    ('@PACKAGE@',           'MonetDB')
 ]
 
 def substitute(line):
-	for (p,v) in subs:
-		line = re.sub(p,v,line);
-	return line
+    for (p,v) in subs:
+        line = re.sub(p,v,line);
+    return line
 
 for line in fileinput.input():
-	sys.stdout.write(substitute(line))
+    sys.stdout.write(substitute(line))
