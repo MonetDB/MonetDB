99a582b9a86f8dd4c3d15210ee4251f74bb96781
Niels Nes <niels@cwi.nl>
2002-07-09 15:42:38 +0000

    same bootstrap as monet acconfig.h add all the defines from monet

--- sql/acconfig.h
+++ sql/acconfig.h
@@ -1,10 +1,24 @@
+/* Define this if you have the pthread library */
+#undef HAVE_LIBPTHREAD
+
+/* Define this if you have the readline library */
+#undef HAVE_LIBREADLINE
 #undef HAVE_LIBZ
 #undef HAVE_LIBBZ2
-#undef HAVE_LIBREADLINE
-#undef HAVE_LIBODBC
-#undef HAVE_LIBGETOPT
-#undef HAVE_LIBPTHREAD
+
+/* Define these if you have the respective hardware-counters library */
+#undef HAVE_LIBPPERF	/* Linux <= 2.2 */
+#undef HAVE_LIBPERFCTR	/* Linux >= 2.4 */
+#undef HAVE_LIBPERFMON	/* Solaris <= 7 */
+#undef HAVE_LIBCPC	/* Solaris >= 8 */
+
+/* Defines HOST */
+#undef HOST
 
 /* LONGLONG */
 #undef HAVE_LONGLONG
 #undef HAVE__INT64
+
+/* ODBC libs */
+#undef HAVE_LIBODBC
+
