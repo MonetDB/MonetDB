cd834c20f1163e7c34de789a16b7ef85a2baff37
Menzo Windhouwer <windhouw@users.sourceforge.net>
2002-07-15 14:31:53 +0000

    Java coding guidelines state that package names should be lower case. Suns JDK doesn't enforce this, but IBMs Jikes does. So the Mapi java package is now mapi.
    Thanks to Casper.

--- MonetDB/src/mapi/clients/MapiClient.java
+++ MonetDB/src/mapi/clients/MapiClient.java
@@ -1,6 +1,6 @@
 
 import java.io.*;
-import Mapi.*;
+import mapi.*;
 
 
 public class MapiClient 
---
+++ MonetDB/src/mapi/clients/mapi/MapiException.java
@@ -0,0 +1,25 @@
+
+package mapi;
+
+/*
+This exception is thrown by Mapi indicating an error in the Monet/java
+communication. 
+MapiException can be constructed with or without an exception message.
+*/
+
+public
+class MapiException extends Exception {
+    public MapiException() {
+        super();
+    }
+
+    public MapiException(String s) {
+        super(s);
+    }
+
+    public String toString() {
+        String message = getMessage();
+        return (message != null) ? (message) : "";
+    }
+}
+
