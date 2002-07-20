494113992b9b717fe751d6b35bb9852bb3f84221
Niels Nes <niels@cwi.nl>
2002-07-20 17:01:30 +0000

    bug fixes transaction support using a logging system. Next step is to implement a log manager (ie. to keep the (number of) logs small.

--- sql/src/server/server.sh
+++ sql/src/server/server.sh
@@ -2,5 +2,5 @@
 
 #$MONET_PREFIX/share/Monet/mapi.mil \
 
-Mserver --debug=34 --config ~/.monet.conf --dbname sql \
+Mserver --config ~/.monet.conf --dbname sql \
 	$SQL_PREFIX/share/sql/catalog.mil $SQL_PREFIX/share/sql/sqlserver.mil 
