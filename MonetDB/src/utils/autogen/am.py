906286a7df97b7d5c0f4ef2d816073f79435ce5a
Menzo Windhouwer <windhouw@users.sourceforge.net>
2002-07-24 08:00:26 +0000

    am.py: - instead of using $(shell $(JAR) ...) to expand the inner classes wildcard,   $(wildcard $(...-inner-classes)) is used. This prevents an error message,   which may stop the make sequence

--- MonetDB/src/utils/autogen/am.py
+++ MonetDB/src/utils/autogen/am.py
@@ -519,7 +519,7 @@ def am_jar(fd, var, jar, am):
     fd.write("\t$(JAVAC) -d . -classpath \"$(CLASSPATH)\" $(JAVACFLAGS) $^\n")
 
     fd.write("\n%s.jar: $(%s_class_files) $(%s_extra_files)\n" % (name,name,name))
-    fd.write("\t$(shell $(JAR) $(JARFLAGS) -cf $@ $(%s_class_files) $(%s_inner_class_files) $(%s_extra_files))\n" % (name,name,name) )
+    fd.write("\t$(JAR) $(JARFLAGS) -cf $@ $(%s_class_files) $(wildcard $(%s_inner_class_files)) $(%s_extra_files)\n" % (name,name,name) )
 
     fd.write("\ninstall-exec-local-%s_jar: %s.jar\n" % (name,name))
     fd.write("\t-mkdir -p $(DESTDIR)%s\n" % (jd))
