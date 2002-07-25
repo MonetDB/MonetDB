8461ecb77cfc8502933c62c856f0e6c8eb6f75aa
Niels Nes <niels@cwi.nl>
2002-07-25 18:08:53 +0000

    made logger alignment aware

--- sql/src/server/server.sh
+++ sql/src/server/server.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
 #$MONET_PREFIX/share/Monet/mapi.mil \
 
-Mserver --config ~/.monet.conf --dbname sql \
+Mserver --config ~/.monet.conf.IRIX64 --dbname sql \
 	$SQL_PREFIX/share/sql/catalog.mil $SQL_PREFIX/share/sql/sqlserver.mil 
