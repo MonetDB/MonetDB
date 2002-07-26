1263f37c63375da1084b5771938599137afd0590
Niels Nes <niels@cwi.nl>
2002-07-26 09:54:03 +0000

    cleanup

--- sql/src/server/server.sh
+++ sql/src/server/server.sh
@@ -2,5 +2,5 @@
 
 #$MONET_PREFIX/share/Monet/mapi.mil \
 
-Mserver --config ~/.monet.conf.IRIX64 --dbname sql \
-	$SQL_PREFIX/share/sql/catalog.mil $SQL_PREFIX/share/sql/sqlserver.mil 
+Mserver --config ~/.monet.conf.`uname -s` --dbname sql \
+	$SQL_PREFIX/share/sql/catalog.mil $SQL_PREFIX/share/sql/sqlserver.mil $*
