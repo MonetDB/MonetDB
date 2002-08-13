bc93621bff2ad82bfe25c5be225040f6b7814a0f
Arjen de Vries <arjen@acm.org>
2002-08-13 23:13:58 +0000

    request by Alex and Johan: lists temporary bat files from disk that do not exist in the BBP any more. use carefully with xargs rm -f on your own responsibility (but it worked for me :)

---
+++ MonetDB/scripts/adm/findtmpbats.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/perl
+#
+# (C) 2002, Arjen P. de Vries
+#
+
+#
+# Finds under a dbfarm directory all database directories, 
+# i.e., directories in which a BBP.dir file resides.
+# Next, finds all bats in that database directory that are stored on disk,
+# but do not exist in the BBP.
+#
+# Usage example (DISCLAIMER!):
+#   ./findtmpbats.pl ~/data/monetdb | xargs rm -f
+# 
+
+use File::Find;
+
+sub usage() {
+  print "Usage: $0 <dbfarm>\n";
+  exit -1;
+}
+
+my $dbfarm      = shift || usage;
+my @dbs         = ();
+my %bats        = ();
+
+sub readBBP ($) {
+  my $dbdir = shift;
+  open BBP, "<$dbdir/BBP.dir";
+  <BBP>;
+  while (<BBP>) {
+    my @fs = split /,\s+/;
+    $bats{$fs[3]}++;
+  }
+}
+
+sub checkBBP ($) {
+  my $dbdir = shift;
+  # print $dbdir, "\n";
+  %bats = ();
+  readBBP $dbdir;
+  find(\&findtmpbats, $dbdir);
+}
+
+sub findtmpbats {
+  return unless -f;
+  return unless $File::Find::name =~ m|bat/(\d+)/?(\d+)?|;
+  my $bat = $1;
+  $bat .= "/" . $2 if defined $2;
+  print $File::Find::name, "\n" if not defined $bats{$bat};
+}
+
+#
+# Main:
+#
+
+find 
+  sub { 
+    push @dbs, $1 if -f && $File::Find::name =~ m|(.*)/BBP.dir|; 
+  }, 
+  $dbfarm;
+map { checkBBP $_; } @dbs;
+
+
