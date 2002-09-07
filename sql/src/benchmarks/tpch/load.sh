ffc158b3bd49f41b87d1a0689becd9abf8c7ff93
Niels Nes <niels@cwi.nl>
2002-09-07 11:13:20 +0000

    adding new load code.

--- sql/src/benchmarks/tpch/load.sh
+++ sql/src/benchmarks/tpch/load.sh
@@ -1,32 +1,14 @@
 #!/bin/sh
 set -x
 
+PWD=`pwd`
+echo $PWD
+
 if [ $# -lt 1 ]; then
 	echo "usage load directory with data"
 	exit 1
 fi
 
-#Mdestroydb -db sql
-#Mcreatedb -db sql
-#Mserver -db sql ~/data/sql/Linux/share/sql/sqlserver.mil \
-#		~/data/monet/Linux/share/Monet/mapi.mil 
+sql_client < ~/data/sql/src/benchmarks/tpch/c.sql
+cat load.sql | sed -e s+PWD+$PWD+ | sql_client 
 
-#sql_client < ~/data/sql/src/benchmarks/tpch/c.sql
-
-MapiClient <<EOF
-bulkload("customer", "$1/customer.tbl", "|", "|\n", 150000);
-bulkload("nation", "$1/nation.tbl", "|", "|\n", 25);
-bulkload("orders", "$1/orders.tbl", "|", "|\n", 1500000);
-bulkload("partsupp", "$1/partsupp.tbl", "|", "|\n", 800000);
-bulkload("part", "$1/part.tbl", "|", "|\n", 200000);
-bulkload("region", "$1/region.tbl", "|", "|\n", 5);
-bulkload("supplier", "$1/supplier.tbl", "|", "|\n", 10000);
-commit;
-quit;
-EOF
-
-MapiClient -everything <<EOF
-bulkload("lineitem", "$1/lineitem.tbl", "|", "|\n", 7000000);
-commit;
-quit;
-EOF
