37a7b19e1561c66861cb74033ffd79ed0d02c9f6
Sjoerd Mullender <sjoerd@acm.org>
2002-12-09 15:05:37 +0000

    Added quotes to protect against spaces in file names.

--- MonetDB/src/tools/Tests/usage.bat
+++ MonetDB/src/tools/Tests/usage.bat
@@ -1,7 +1,7 @@
 @echo on
 @prompt # $t $g  
 
-	%MTIMEOUT% Mserver --config=%MONET_CONF% -?
+	%MTIMEOUT% Mserver "--config=%MONET_CONF%" -?
 	%MTIMEOUT% MapiClient -?
 	%MTIMEOUT% MapiClient.py -?
 	%MTIMEOUT% Mshutdown.py -?
