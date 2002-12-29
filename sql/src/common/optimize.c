c6c112da01bf582c7b7f79ac9cb01647582a4273
Niels Nes <niels@cwi.nl>
2002-12-29 21:53:57 +0000

    added empty optimizer

---
+++ sql/src/common/optimize.c
@@ -0,0 +1,7 @@
+
+#include "optimize.h"
+#include <stdio.h>
+
+stmt *optimize( context *c, stmt *s ){
+	return s;
+}
---
+++ sql/src/common/optimize.h
@@ -0,0 +1,9 @@
+#ifndef _OPTIMIZE_H
+#define _OPTIMIZE_H
+
+#include "catalog.h"
+#include "statement.h"
+
+extern stmt *optimize( context *c, stmt *s );
+	
+#endif /* _OPTIMIZE_H */
