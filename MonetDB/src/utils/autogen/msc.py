67b29a25066e08b1e4084b6aa04431f3be1d07d9
Niels Nes <niels@cwi.nl>
2003-04-15 15:51:26 +0000

    inc/dec modules use foreach client fix windoz install of mil files fix bug in module dependency handling in block of statements

--- MonetDB/src/utils/autogen/msc.py
+++ MonetDB/src/utils/autogen/msc.py
@@ -26,7 +26,6 @@ import os
 
 #automake_ext = ['c', 'cc', 'h', 'y', 'yy', 'l', 'll', 'glue.c']
 automake_ext = ['c', 'cc', 'h', 'tab.c', 'tab.cc', 'tab.h', 'yy.c', 'yy.cc', 'glue.c', 'proto.h', '']
-script_ext = ['mil']
 
 def split_filename(f):
     base = f
@@ -298,6 +297,14 @@ def msc_deps(fd, deps, objext, msc):
             if ext == "proto.h":
                 fd.write(getsrc)
                 fd.write("\t$(MEL) $(INCLUDES) -o %s -proto %s.m\n" % (t, b))
+	    if ext == "mil":
+		fd.write(getsrc)
+		print(deplist)
+		if b+".tmpmil" in deplist:
+			fd.write("\t$(MEL) $(INCLUDES) -mil %s.m > $@\n" % (b))
+			fd.write("\ttype %s.tmpmil >> $@\n" % (b))
+
+			
             if ext in ("obj", "glue.obj", "tab.obj", "yy.obj"):
                 target, name = msc_find_target(tar, msc)
                 if name[0] == '_':
