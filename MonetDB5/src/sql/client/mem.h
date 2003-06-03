cc5a1bdd03c44738e777311a57ab5cf7466f1a69
Martin Kersten <mk@cwi.nl>
2003-06-03 14:43:58 +0000

    *** empty log message ***

---
+++ MonetDB5/src/sql/client/mem.h
@@ -0,0 +1,20 @@
+#ifndef _MEM_H_ 
+#define _MEM_H_ 
+
+#include <config.h>
+
+#include <stdio.h>
+#include <assert.h>
+
+#ifdef HAVE_MALLOC_H 
+#include <malloc.h>
+#endif
+
+#define NEW( type ) (type*)malloc(sizeof(type) )
+#define NEW_ARRAY( type, size ) (type*)malloc((size)*sizeof(type))
+#define RENEW_ARRAY( type,ptr,size) (type*)realloc((void*)ptr,(size)*sizeof(type))
+#define NEWADT( size ) (adt*)malloc(size)
+#define _DELETE( ptr )	free(ptr)
+#define _strdup( ptr )	strdup((char*)ptr)
+
+#endif /*_MEM_H_*/
---
+++ MonetDB5/src/sql/server/server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#$MONET_PREFIX/share/Monet/mapi.mil \
+
+Mserver --config ~/.monet.conf.`uname -s` --dbname sql $SQL_PREFIX/share/MonetDB/sqlserver.mil $*
