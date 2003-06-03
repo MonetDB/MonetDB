2389d3dfc39f6bd78c184394b0ff22de22724909
Martin Kersten <mk@cwi.nl>
2003-06-03 14:42:22 +0000

    updated to current version of monet5

--- MonetDB5/rules.mk
+++ MonetDB5/rules.mk
@@ -1,5 +1,31 @@
+# The contents of this file are subject to the MonetDB Public
+# License Version 1.0 (the "License"); you may not use this file
+# except in compliance with the License. You may obtain a copy of
+# the License at 
+# http://monetdb.cwi.nl/Legal/MonetDBLicense-1.0.html
+# 
+# Software distributed under the License is distributed on an "AS
+# IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# rights and limitations under the License.
+# 
+# The Original Code is the Monet Database System.
+# 
+# The Initial Developer of the Original Code is CWI.
+# Portions created by CWI are Copyright (C) 1997-2002 CWI.  
+# All Rights Reserved.
+# 
+# Contributor(s):
+# 		Martin Kersten <Martin.Kersten@cwi.nl>
+# 		Peter Boncz <Peter.Boncz@cwi.nl>
+# 		Niels Nes <Niels.Nes@cwi.nl>
+# 		Stefan Manegold  <Stefan.Manegold@cwi.nl>
 
-MX=$(top_builddir)/src/utils/Mx/Mx
+#
+# ! this file should be kept identical in !
+# ! monet, sql, xml, acoi, template       !
+#
+
 CP=cp
 MV=mv
 MXFLAGS= -notouch
@@ -13,18 +39,13 @@ MXFLAGS= -notouch
 %.y: %.mx
 	$(MX) $(MXFLAGS) -x y $< 
 
-%.tab.c %.tab.h: %.y
-	$(YACC) $(YFLAGS) $*.y
-	if [ -f y.tab.c ]; then $(MV) y.tab.c $*.tab.c ; fi
-	if [ -f y.tab.h ]; then $(MV) y.tab.h $*.tab.h ; fi
-
 %.l: %.mx
 	$(MX) $(MXFLAGS) -x l $< 
 
 %.yy.c: %.l
-	$(LEX) $(LFLAGS) $*.l
+	$(LEX) $(LFLAGS) $<
 	if [ -f lex.yy.c ]; then $(MV) lex.yy.c $*.yy.c ; fi
-
+	if [ -f lex.$(PARSERNAME).c ]; then $(MV) lex.$(PARSERNAME).c $*.yy.c ; fi
 
 %.cc: %.mx
 	$(MX) $(MXFLAGS) -x C $<
@@ -32,16 +53,35 @@ MXFLAGS= -notouch
 %.yy: %.mx
 	$(MX) $(MXFLAGS) -x Y $< 
 
-%.tab.cc %.tab.h: %.yy
-	$(YACC) $(YFLAGS) $*.yy
+%.tab.cc: %.yy
+	$(LOCKFILE) waiting
+	$(YACC) $(YFLAGS) $<
 	if [ -f y.tab.c ]; then $(MV) y.tab.c $*.tab.cc ; fi
+	rm -f waiting
+
+%.tab.h: %.yy
+	$(LOCKFILE) waiting
+	$(YACC) $(YFLAGS) $<
 	if [ -f y.tab.h ]; then $(MV) y.tab.h $*.tab.h ; fi
+	rm -f waiting
+
+%.tab.c: %.y
+	$(LOCKFILE) waiting
+	$(YACC) $(YFLAGS) $<
+	if [ -f y.tab.c ]; then $(MV) y.tab.c $*.tab.c ; fi
+	rm -f waiting
+
+%.tab.h: %.y
+	$(LOCKFILE) waiting
+	$(YACC) $(YFLAGS) $<
+	if [ -f y.tab.h ]; then $(MV) y.tab.h $*.tab.h ; fi
+	rm -f waiting
 
 %.ll: %.mx
 	$(MX) $(MXFLAGS) -x L $<
 
 %.yy.cc: %.ll
-	$(LEX) $(LFLAGS) $*.ll
+	$(LEX) $(LFLAGS) $<
 	if [ -f lex.yy.c ]; then $(MV) lex.yy.c $*.yy.cc ; fi
 
 %.m: %.mx
@@ -52,15 +92,27 @@ MXFLAGS= -notouch
 
 %.mil: %.mx
 	$(MX) $(MXFLAGS) -x mil $<
-	$(MX) $(MXFLAGS) -x mal $<	# should be removed!
 
 %: %.mx
 	$(MX) $(MXFLAGS) -x sh $<
+	chmod a+x $@
+
+%.proto.h: %.m
+	$(MEL) $(INCLUDES) -proto $< > $@
+
+%.glue.c: %.m
+	$(MEL) $(INCLUDES) -glue $< > $@
 
 %.tex: %.mx
 	cat $< > /tmp/doc.mx
 	$(MX) -1 -H$(HIDE) -t /tmp/doc.mx 
 	$(MV) doc.tex $@
+	$(RM) /tmp/doc.mx
+
+%.html: %.mx
+	cat $< > /tmp/doc.mx
+	$(MX) -1 -H$(HIDE) -w /tmp/doc.mx 
+	$(MV) doc.html $@
 	$(RM) /tmp/doc.mx
 
 %.pdf: %.tex
@@ -73,9 +125,21 @@ MXFLAGS= -notouch
 	$(DVIPS) $< -o $@
 
 %.eps: %.fig
-	$(FIG2DEV) -Leps -e $< > $@
+	$(FIG2DEV) -L$(FIG2DEV_EPS) -e $< > $@
 
 %.eps: %.feps
 	$(CP) $< $@
 
+$(NO_INLINE_FILES:.mx=.lo): %.lo: %.c
+	$(LIBTOOL) --mode=compile $(COMPILE) $(NO_INLINE_CFLAGS) -c $<
+
+$(patsubst %.mx,%.lo,$(filter %.mx,$(NO_OPTIMIZE_FILES))): %.lo: %.c
+	$(LIBTOOL) --mode=compile $(COMPILE) -O0 -c $<
+
+$(patsubst %.c,%.o,$(filter %.c,$(NO_OPTIMIZE_FILES))): %.o: %.c
+	$(COMPILE) -O0 -c $<
+
 SUFFIXES-local: $(BUILT_SOURCES)
+
+html:
+	python $(top_srcdir)/doc/mkdoc.py $(top_srcdir) $(prefix)
