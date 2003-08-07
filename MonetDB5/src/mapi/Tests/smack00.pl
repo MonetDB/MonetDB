d4bc7f8918a7c242b22128ef4c0d569c9eafeb54
Martin Kersten <mk@cwi.nl>
2003-08-07 21:37:45 +0000

    Re-entered for cleanup

---
+++ MonetDB5/src/mapi/Tests/smack00.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+# This script implements the supersmack benchmark obtained from Mysql
+
+use Mapi;
+
+my ($monet, $line);
+$monet = new Mapi( Mapi::hostname() . ':' . Mapi::portnr(), $ENV{'USER'}, '' );
+
+print "Start the application\n";
+my($i)=0;
+for($i=0; $i<100; $i++){
+	$reply= $monet->cmd("print(1);");
+	#if( $monet->error() ){
+		#$monet->explain();
+	#} else { print $reply;}
+}
+
+$monet->disconnect();
+
+1;
+
