490e4e4f65ee9f33b5817f602b26f9d7876dc51a
Stefan Manegold <manegold@cwi.nl>
2003-08-22 17:43:25 +0000

    for debugging: added "--trace" switch to src/mapi//Tests/manyMapiClients.MAPI.py and src/mapi//clients/MapiClient.py.in

--- MonetDB/src/mapi/Tests/manyMapiClients.MAPI.py
+++ MonetDB/src/mapi/Tests/manyMapiClients.MAPI.py
@@ -29,21 +29,36 @@ if os.environ.has_key('MONET_PREFIX'):
 
 import Mapi
 
-STDOUT = sys.stdout
-STDERR = sys.stderr
 
-n = 195
+def main(argv) :
 
-STDOUT.write("\n# %d Mapi-Client connections\n\n" % n)
-STDERR.write("\n# %d Mapi-Client connections\n\n" % n)
+    STDOUT = sys.stdout
+    STDERR = sys.stderr
 
-i=0
-while i < n:
-    i = i + 1
-    STDOUT.write("%d:\n" % i)
-    STDERR.write("%d:\n" % i)
+    n = 195
 
-    s = Mapi.server( "localhost", int(os.environ['MAPIPORT']), 'Mtest.py')
-    print( s.cmd( "print(%d);\n" % i ) )
-    s.disconnect()
-STDOUT.write("done: %d\n" % i)
+    STDOUT.write("\n# %d Mapi-Client connections\n\n" % n)
+    STDERR.write("\n# %d Mapi-Client connections\n\n" % n)
+
+    i=0
+    while i < n:
+        i = i + 1
+        STDOUT.write("%d:\n" % i)
+        STDERR.write("%d:\n" % i)
+
+        s = Mapi.server( "localhost", int(os.environ['MAPIPORT']), 'Mtest.py')
+        print( s.cmd( "print(%d);\n" % i ) )
+        s.disconnect()
+    STDOUT.write("done: %d\n" % i)
+
+### main(argv) #
+
+
+if __name__ == "__main__":
+    if '--trace' in sys.argv:
+        sys.argv.remove('--trace')
+        import trace
+        t = trace.Trace(trace=1, count=0)
+        t.runfunc(main, sys.argv)
+    else:
+        main(sys.argv)
