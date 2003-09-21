4e2814072933428ce72e8cbbf670b885839cc5a1
Martin Kersten <mk@cwi.nl>
2003-09-21 08:58:04 +0000

    Addition of more files

---
+++ MonetDB/src/mapi/clients/Tests/sample3.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <monet_utils.h>
+#include <stream.h>
+#include <Mapi.h>
+
+#define die(X) {mapi_explain(X,stdout); exit(-1); }
+
+main(int argc, char **argv){
+	Mapi	dbh;
+	int    port,rows, i,j;
+	char   buf[40];
+
+	if( argc != 2){
+		printf("usage:%s <port>\n",argv[0]);
+			exit(-1);
+	}
+	port = atol( argv[1]);
+
+	snprintf(buf,40,"localhost:%d",port);
+	dbh= mapi_connect(buf,"guest",0,"sql");
+	if(mapi_error(dbh)) die(dbh);
+
+	/* mapi_trace(dbh,1);*/
+	if( mapi_query(dbh,"create table emp(name varchar,age int)")) die(dbh);
+
+	if( mapi_query(dbh,"insert into emp values(\"John\", 23)") ) die(dbh);
+
+	if( mapi_query(dbh,"insert into emp values(\"Mary\", 22)") ) die(dbh);
+
+	if( mapi_query(dbh,"select * from emp") ) die(dbh);
+
+	/* Retrieve all tuples in the client cache first */
+	rows= mapi_fetch_all(dbh);
+	printf("rows received %d with %d fields\n",rows, mapi_num_fields(dbh));
+
+	/* Interpret the cache as a two-dimensional array */
+	for(i=0;i<rows;i++){
+		char *v;
+		if( mapi_seek_row(dbh,i)) break;
+		for(j=0;j<mapi_num_fields(dbh);j++){
+			printf("%s=%s ", 
+				mapi_get_name(dbh,j),
+				mapi_fetch_field(dbh,j));
+		}
+		printf("\n");
+	}
+	if(mapi_error(dbh)) mapi_explain(dbh,stdout);
+	mapi_disconnect(dbh);
+}
---
+++ MonetDB/src/mapi/clients/Tests/sample4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <monet_utils.h>
+#include <stream.h>
+#include <Mapi.h>
+
+#define die(X) {mapi_explain(X,stdout); exit(-1); }
+
+main(int argc, char **argv){
+	Mapi	dbh;
+	int    port,rows, l;
+	char   buf[40];
+
+	if( argc != 2){
+		printf("usage:%s <port>\n",argv[0]);
+			exit(-1);
+	}
+	port = atol( argv[1]);
+
+	snprintf(buf,40,"localhost:%d",port);
+	dbh= mapi_connect(buf,"guest",0,"sql");
+	if(mapi_error(dbh)) die(dbh);
+
+	mapi_cache_limit(dbh,2);
+	mapi_trace_log(dbh,"/tmp/mapilog");
+	/* mapi_trace(dbh,1);*/
+	if( mapi_query(dbh,"create table emp(name varchar,age int)")) die(dbh);
+
+	if( mapi_query(dbh,"insert into emp values(\"John\", 23)") ) die(dbh);
+
+	if( mapi_query(dbh,"insert into emp values(\"Mary\", 22)") ) die(dbh);
+
+	if( mapi_query(dbh,"select * from emp") ) die(dbh);
+
+	l= mapi_fetch_all(dbh);
+	printf("rows received %d\n",l);
+	while( mapi_fetch_row(dbh)){
+		char *nme = mapi_fetch_field(dbh,0);
+		char *age = mapi_fetch_field(dbh,1);
+		printf("%s is %s\n", nme, age);
+	}
+	/* mapi_stat(dbh);
+	printf("mapi_ping %d\n",mapi_ping(dbh)); */
+	mapi_disconnect(dbh);
+}
