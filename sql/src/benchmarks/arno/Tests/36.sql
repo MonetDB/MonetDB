174bcf511da9b5b0a22d62e8b6787f90e407e891
cvs2hg <unknown>
2003-10-10 09:31:39 +0000

    fixup commit for tag 'JensTeubner'

--- sql/src/benchmarks/arno/Tests/36.sql
+++
@@ -1,1 +0,0 @@
-SELECT MODEL202.is_mutagen, count(distinct MODEL202.model_id ) FROM MODEL MODEL202, ATOM T1008290413400  WHERE MODEL202.model_id=T1008290413400.model_id AND MODEL202.lumo='-2' group by MODEL202.is_mutagen;
--- sql/src/client/simple_prompt.h
+++
@@ -1,6 +0,0 @@
-#ifndef SIMPLE_PROMPT
-#define SIMPLE_PROMPT
-
-extern char * simple_prompt(const char *prompt, int maxlen, int echo);
-
-#endif /* SIMPLE_PROMPT */
