1f5b7b4ef52f9f3a345580e38c282d2cc20cbcfb
Stefan Manegold <manegold@cwi.nl>
2004-01-13 06:18:50 +0000

    extended Sjoerd's "nmake prefix=..." feature on Windows: By specifying the install prefix already during the initial "nmake" and not only with "nmake install", all paths are set correctly, and hence the installed MonetDB/SQL (including Mtest.py) does work with the default settings.
    Caveat:
    If you specify a prefix, you need to do so with each "nmake" call,
    i.e., "nmake prefix=...", "nmake prefix=... check", "nmake prefix=... install".

--- sql/NT/configure.py
+++ sql/NT/configure.py
@@ -28,8 +28,9 @@ import fileinput
 import os
 import string
 
-prefix=os.path.abspath(sys.argv[1]);
-build=prefix
+build=os.path.abspath(sys.argv[1]);
+prefix=os.path.abspath(sys.argv[2]);
+
 source=os.path.abspath(os.path.join(build,os.pardir))
 
 # double back slashes
@@ -78,5 +79,5 @@ def substitute(line):
         line = string.replace(line, p, v);
     return line
 
-for line in fileinput.input(sys.argv[2:]):
+for line in fileinput.input(sys.argv[3:]):
     sys.stdout.write(substitute(line))
