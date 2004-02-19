699c7cd5a8ed6d29c535a2fef5fcb74697c389fe
Roberto Cornacchia <cornuz@users.sourceforge.net>
2004-02-19 09:52:22 +0000

    correction to some wrong comments

--- sql/src/test/decimal.sql
+++ sql/src/test/decimal.sql
@@ -1,5 +1,5 @@
 -- 'decimal' and 'numeric' data types should be equivalent.
--- 'dec' is a synonym of 'decimal'
+-- 'dec' is a synonym for 'decimal'
 --
 -- SEMANTICS: 
 --
@@ -16,7 +16,7 @@
 -- Example: 
 --  The allowed range of decimal numbers for dec(5,2) is (-999.99 , 999.99).
 --  1000 is not allowed, as the integer part is 4 digits long, which exceeds (5 - 2)=3 
---  999.6666666 is allowed and it should be rounded to 888.67
+--  999.6666666 is allowed and it should be rounded to 999.67
 --
 --
 -- Results from MySql are completely wrong with respect to this semantics.
@@ -32,21 +32,21 @@ insert into test values (12.12);
 insert into test values (-12.12);
 insert into test values (123.12);
 insert into test values (-123.12);
-      -- should be rounded to 123.12
+      -- next value should be rounded to 123.12
 insert into test values (123.123);
-      -- should be rounded to -123.12
+      -- next value should be rounded to -123.12
 insert into test values (-123.123);
-      -- should be rounded to 123.13
+      -- next value should be rounded to 123.13
 insert into test values (123.128);
-     -- should be rounded to -123.13
+     -- next value should be rounded to -123.13
 insert into test values (-123.128);
 
 
 -- these should fail
 insert into test values (1234);
-      -- a '.00' decimal part is assumed here
+      -- a '.00' decimal part is assumed for the next value 
 insert into test values (-1234);
-      -- a '.00' decimal part is assumed here
+      -- a '.00' decimal part is assumed for the next value 
 insert into test values (1234.1);
 insert into test values (-1234.1);
 
