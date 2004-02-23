b36c496fe822c61259ec2317f9dd06c62b6ee5f8
Niels Nes <niels@cwi.nl>
2004-02-23 10:26:28 +0000

    sourceforge keeps crashing my checkins So another atempt to checkin changing from Msql->MapiClient

--- sql/src/test/bugs/bug-sf-900198.sql
+++ sql/src/test/bugs/bug-sf-900198.sql
@@ -1,3 +1,4 @@
+set debug=4096;
 create table tst( a0 int unique, a1 int);
 insert into tst values(1,1);
 insert into tst values(2,2);
--- sql/src/test/bugs/bug-sf-900206.sql
+++ sql/src/test/bugs/bug-sf-900206.sql
@@ -8,8 +8,7 @@ insert into RKB select head+0, tail+0 from RKA;
 update RKB set tail=(tail*37) % 1024;
 update RKB set tail=(tail*7) % 1024;
 insert into tapestry select R0.head, R0.tail from RKB R0;
-drop table _tmp;
-Drop Table, table _tmp unknown
+--drop table _tmp;
 create table _tmp( a0 int(9) unique, a1 int(9));
 insert into _tmp select * from tapestry where a1>=0 and a1 <=1;
 delete from _tmp;
