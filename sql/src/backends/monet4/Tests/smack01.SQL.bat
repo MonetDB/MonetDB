02f0f82c5e42f2cf612ce046c6286892465cc746
cvs2hg <unknown>
2004-03-09 11:21:42 +0000

    fixup commit for branch 'MonetDB_4-3-16'

--- sql/src/backends/monet4/Tests/smack01.SQL.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-set PATH=%MONET_BUILD%\src\mapi\examples\C;%MONET_PREFIX%\lib\MonetDB\Tests;%PATH%
-
-call Mlog.bat -x smack01.exe %SQLPORT% sql
--- sql/src/test/string.sql
+++
@@ -1,5 +0,0 @@
-create table test ( str string, str2 string);
-insert into test values ('', 'test');
-insert into test values ('test', '');
-insert into test values ('','');
-select * from test;
