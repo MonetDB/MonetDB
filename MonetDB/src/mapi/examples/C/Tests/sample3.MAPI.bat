0c49d9de8955510581a8f2cc606dd344bbb34409
cvs2hg <unknown>
2004-03-12 15:17:49 +0000

    fixup commit for tag 'fabian'

--- MonetDB/src/mapi/examples/C/Tests/sample3.MAPI.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-set PATH=%MONET_BUILD%\src\mapi\examples\C;%MONET_PREFIX%\lib\MonetDB\Tests;%PATH%
-
-call Mlog.bat -x sample3.exe %HOST% %MAPIPORT% mil
--- MonetDB/src/mapi/examples/C/Tests/sample4.MAPI.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-set PATH=%MONET_BUILD%\src\mapi\examples\C;%MONET_PREFIX%\lib\MonetDB\Tests;%PATH%
-
-call Mlog.bat -x sample4.exe %HOST% %MAPIPORT% mil
