30b1036399a7bec22c39b1e77c9e0ff3c9df01bf
Stefan Manegold <manegold@cwi.nl>
2004-03-26 10:05:23 +0000

    enabled compilation with the Portland Group (PGI) compiler (pgcc/pgCC) on spin:
    - conf/conf.bash:
       now recognizes "COMP=PGI", sets the proper environment
       and adds "--with-gcc=pgcc --with-gxx=pgCC" to the configure alias
    
    - configure.ag, conf/monet.m4:
       recognize "--with-gcc=pgcc --with-gxx=pgCC" and set the proper flags;
       no optimization flags are set, yet ...
    
    - src/testing/Mfilter.py.in:
       ignore messages:
      "Symbol .(exit|_setjmp). causes overflow in R_X86_64_PC32 relocation"
    
    Be "warned": Though the usual compilation goes rather quick,
    pgcc takes 12 (twelve!) minutes to compile gdk_scanselect on spin
    --- even(?) without optimization...

--- MonetDB/conf/conf.bash
+++ MonetDB/conf/conf.bash
@@ -127,7 +127,7 @@ if [ ! "${WHAT_PREFIX}" ] ; then
 	WHAT_PREFIX="${WHAT_BUILD}"
 fi
 
-if [ "${COMP}" != "GNU"  -a  "${COMP}" != "ntv" ] ; then
+if [ "${COMP}" != "GNU"  -a  "${COMP}" != "ntv"  -a  "${os}${COMP}" != "LinuxPGI" ] ; then
 	echo ''
 	echo 'COMP not set to either "GNU" or "ntv" (native) to select the desired compiler.'
 	echo 'Using COMP="GNU" (default).'
@@ -207,6 +207,18 @@ if [ "${os}" = "Linux" ] ; then
 			conf_opts="${conf_opts} --with-gc=${softpath}"
 			;;
 		esac
+	fi
+	if [ "${COMP}" = "PGI" ] ; then
+		# Portland Group compiler on spin
+		cc='pgcc'
+		cxx='pgCC'
+		export PGI=/soft/32/pgi-5.1-3
+		if [ "${BITS}" = "64" ] ; then
+			binpath="${PGI}/linux86-64/5.1/bin:${binpath}"
+		  else
+		  	binpath="${PGI}/linux86/5.1/bin:${binpath}"
+		fi
+		export LM_LICENSE_FILE=${PGI}/license.dat
 	fi
 	if [ "${hw}" = "ia64" ] ; then
 		if [ "${host%.ins.cwi.nl}" = "titan" ] ; then
