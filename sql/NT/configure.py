94c144179074c75a9436a4f8872031513e1903c2
Stefan Manegold <manegold@cwi.nl>
2004-04-01 12:53:55 +0000

    Set version number to "2.0.16_rc06" to indicate that this version goes with MonetDB 4.3.16_rc06. Tagged this version as "MonetDB_4-3-16_rc06".

--- sql/NT/configure.py
+++ sql/NT/configure.py
@@ -60,7 +60,7 @@ subs = [
     ('@Qinfodir@',          "@Qprefix@@QDIRSEP@info"),
     ('@Qlibexecdir@',       "@Qprefix@@QDIRSEP@libexec"),
     ('@PACKAGE@',           "sql"),
-    ('@VERSION@',           "2.0"),
+    ('@VERSION@',           "2.0.16_rc06"),
     ('@DIRSEP@',            "\\"),
     ('@prefix@',            prefix),
     ('@MONET_BUILD@',       build),
