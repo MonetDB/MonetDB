dc3454ccb60d9fb717f4178b2b555fa33d8f67fb
Martin Kersten <mk@cwi.nl>
2004-04-03 19:01:20 +0000

    The scripts to built the VOC demo database

---
+++ sql/src/benchmarks/VOC/drop_voc_tables.sql
@@ -0,0 +1,9 @@
+DROP TABLE voyages ;
+DROP TABLE seafarers ;
+DROP TABLE soldiers ;
+DROP TABLE craftsmen ;
+DROP TABLE passengers ;
+DROP TABLE impotenten ;
+DROP TABLE total ;
+DROP TABLE invoices ;
+COMMIT;
