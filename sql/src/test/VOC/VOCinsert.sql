68bb41e4187fe1a359d47ba289c01f5391150a32
cvs2hg <unknown>
2004-04-18 08:46:41 +0000

    fixup commit for branch 'Core2MIL_Summer2004'

--- sql/src/test/VOC/VOCinsert.sql
+++
@@ -1,2 +0,0 @@
-insert into voyage values(4408,3,'BRESLAU','Jan Kornelis Roos',1150,'hired',1774,'A','Z',date '15-02-1783','Rammekens',date '27-05-1783',date '12-06-1783',date '06-08-1783','Batavia');
-commit work;
