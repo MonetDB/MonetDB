467282516ecdf0304a246ce0e406d8e3a028512c
Jan Rittinger <jan.rittinger@uni-tuebingen.de>
2004-04-26 14:30:33 +0000

    added abbreviations for the new helper functions in core.c

--- pathfinder/compiler/include/core_mnemonic.h
+++ pathfinder/compiler/include/core_mnemonic.h
@@ -63,6 +63,10 @@
 #undef true_      
 #undef false_     
 #undef _root
+#undef constr_elem
+#undef constr_attr
+#undef constr
+#undef constr_tag
 #undef locsteps   
 #undef step       
 #undef kindt      
@@ -72,39 +76,50 @@
 #undef ebv        
 #undef error      
 #undef error_loc  
+#undef fs_convert_op
+#undef fn_data
+#undef some
 
-#define nil()                PFcore_nil ()
-#define new_var(v)           PFcore_new_var (v)
-#define var(v)               PFcore_var (v)
-#define num(n)               PFcore_num (n)
-#define dec(d)               PFcore_dec (d)
-#define dbl(d)               PFcore_dbl (d)
-#define str(s)               PFcore_str (s)
-#define seqtype(t)           PFcore_seqtype (t)
-#define seqcast(e1,e2)       PFcore_seqcast ((e1), (e2))
-#define proof(e1,t,e2)       PFcore_proof ((e1), (t), (e2))
-#define typeswitch(e1,e2,e3) PFcore_typeswitch ((e1), (e2), (e3))
-#define case_(e1,e2)         PFcore_case ((e1), (e2))
-#define cases(e1,e2)         PFcore_cases ((e1),(e2))
-#define ifthenelse(e1,e2,e3) PFcore_ifthenelse ((e1), (e2), (e3))
-#define int_eq(e1,e2)        PFcore_int_eq ((e1), (e2))
-#define for_(e1,e2,e3,e4)    PFcore_for ((e1), (e2), (e3), (e4))
-#define let(e1,e2,e3)        PFcore_let ((e1), (e2), (e3))
-#define seq(e1,e2)           PFcore_seq ((e1), (e2))
-#define empty()              PFcore_empty ()
-#define true_()              PFcore_true ()
-#define false_()             PFcore_false ()
-#define _root()              PFcore_root ()
-#define locsteps(e1,e2)      PFcore_locsteps ((e1), (e2))
-#define step(a,e)            PFcore_step ((a), (e))
-#define kindt(k,e)           PFcore_kindt ((k), (e))
-#define namet(qn)            PFcore_namet (qn)
-#define function(qn)         PFcore_function (qn)
-#define arg(e1,e2)           PFcore_arg((e1), (e2))
-#define apply(fn,e)          PFcore_apply ((fn), (e))
-#define ebv(e)               PFcore_ebv (e)
-#define error(s,...)         PFcore_error ((s), __VA_ARGS__)
-#define error_loc(l,s,...)   PFcore_error_loc ((l), (s), __VA_ARGS__)
+#define nil()                 PFcore_nil ()
+#define new_var(v)            PFcore_new_var (v)
+#define var(v)                PFcore_var (v)
+#define num(n)                PFcore_num (n)
+#define dec(d)                PFcore_dec (d)
+#define dbl(d)                PFcore_dbl (d)
+#define str(s)                PFcore_str (s)
+#define seqtype(t)            PFcore_seqtype (t)
+#define seqcast(e1,e2)        PFcore_seqcast ((e1), (e2))
+#define proof(e1,t,e2)        PFcore_proof ((e1), (t), (e2))
+#define typeswitch(e1,e2,e3)  PFcore_typeswitch ((e1), (e2), (e3))
+#define case_(e1,e2)          PFcore_case ((e1), (e2))
+#define cases(e1,e2)          PFcore_cases ((e1),(e2))
+#define ifthenelse(e1,e2,e3)  PFcore_ifthenelse ((e1), (e2), (e3))
+#define int_eq(e1,e2)         PFcore_int_eq ((e1), (e2))
+#define for_(e1,e2,e3,e4)     PFcore_for ((e1), (e2), (e3), (e4))
+#define let(e1,e2,e3)         PFcore_let ((e1), (e2), (e3))
+#define seq(e1,e2)            PFcore_seq ((e1), (e2))
+#define empty()               PFcore_empty ()
+#define true_()               PFcore_true ()
+#define false_()              PFcore_false ()
+#define _root()               PFcore_root ()
+#define constr_elem(e1,e2)    PFcore_constr_elem(e1,e2)
+#define constr_attr(e1,e2)    PFcore_constr_attr(e1,e2)
+#define constr(k,e1)          PFcore_constr(k,e1)
+#define constr_tag(qn)        PFcore_tag(qn)
+#define locsteps(e1,e2)       PFcore_locsteps ((e1), (e2))
+#define step(a,e)             PFcore_step ((a), (e))
+#define kindt(k,e)            PFcore_kindt ((k), (e))
+#define namet(qn)             PFcore_namet (qn)
+#define function(qn)          PFcore_function (qn)
+#define arg(e1,e2)            PFcore_arg((e1), (e2))
+#define apply(fn,e)           PFcore_apply ((fn), (e))
+#define ebv(e)                PFcore_ebv (e)
+#define error(s,...)          PFcore_error ((s), __VA_ARGS__)
+#define error_loc(l,s,...)    PFcore_error_loc ((l), (s), __VA_ARGS__)
+                              
+#define fs_convert_op(e,t)    PFcore_fs_convert_op (e,t)
+#define fn_data(e1)           PFcore_fn_data (e1)
+#define some(v,e1,e2)         PFcore_some(v,e1,e2)
        
 #endif 
 
