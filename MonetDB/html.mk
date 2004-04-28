96048faaaff2edb0ff73a7e4eec5985fb788f848
Stefan Manegold <manegold@cwi.nl>
2004-04-28 07:32:48 +0000

    propagated "make html" changes of Tuesday April 27 2004 from the release branch to the main branch.
    changes are wrapped in tags
    "synching_MakeHtml_with_MonetWeb_main_before" & "synching_MakeHtml_with_MonetWeb_main_after"

--- MonetDB/html.mk
+++ MonetDB/html.mk
@@ -23,28 +23,32 @@
 
 # make rules to generate MonetDB'\''s documentation
 
-$(prefix)/doc/Mx/mxdoc.tex:	$(top_srcdir)/doc/mxdoc.tex
-	-@mkdir -p $(prefix)/doc/Mx
+$(prefix)/doc/www/Services/Mx/mxdoc.tex:	$(top_srcdir)/doc/mxdoc.tex
+	-@mkdir -p $(prefix)/doc/www/Services/Mx
 	cp $< $@
 
-$(prefix)/doc/Mx/mxdoc.aux:	$(prefix)/doc/Mx/mxdoc.tex
-	(cd $(prefix)/doc/Mx; latex mxdoc.tex; latex mxdoc.tex)
+$(prefix)/doc/www/Services/Mx/mxdoc.aux:	$(prefix)/doc/www/Services/Mx/mxdoc.tex
+	(cd $(prefix)/doc/www/Services/Mx; latex mxdoc.tex; latex mxdoc.tex)
 
-$(prefix)/doc/Mx/index.html:	$(prefix)/doc/Mx/mxdoc.aux
-	(cd $(prefix); latex2html -ascii_mode -no_images -address '' -style http://monetdb.cwi.nl/MonetDB.css -dir doc/Mx doc/Mx/mxdoc.tex)
+$(prefix)/doc/www/Services/Mx/index.html:	$(prefix)/doc/www/Services/Mx/mxdoc.aux
+	(cd $(prefix); latex2html -ascii_mode -no_images -address '' -style http://monetdb.cwi.nl/MonetDB.css -dir doc/www/Services/Mx doc/www/Services/Mx/mxdoc.tex)
 
-$(prefix)/doc/MapiJava/index.html:	$(top_srcdir)/src/mapi/clients/java/MapiClient.java	\
+$(prefix)/doc/www/APIs/MapiJava/index.html:	$(top_srcdir)/src/mapi/clients/java/MapiClient.java	\
 					$(top_srcdir)/src/mapi/clients/java/mapi/Mapi.java	\
 					$(top_srcdir)/src/mapi/clients/java/mapi/MapiException.java
-	-@mkdir -p $(prefix)/doc/MapiJava
-	lynx -source http://monetdb.cwi.nl/MonetDB.css > $(prefix)/doc/MapiJava/MonetDB.css
-	javadoc -d $(prefix)/doc/MapiJava -stylesheetfile $(prefix)/doc/MapiJava/MonetDB.css\
+	-@mkdir -p $(prefix)/doc/www/APIs/MapiJava
+	lynx -source http://monetdb.cwi.nl/MonetDB.css > $(prefix)/doc/www/APIs/MapiJava/MonetDB.css
+	javadoc -d $(prefix)/doc/www/APIs/MapiJava -stylesheetfile $(prefix)/doc/www/APIs/MapiJava/MonetDB.css\
 		$(top_srcdir)/src/mapi/clients/java/MapiClient.java       	\
 	        $(top_srcdir)/src/mapi/clients/java/mapi/Mapi.java        	\
 	        $(top_srcdir)/src/mapi/clients/java/mapi/MapiException.java
 
-html:	$(prefix)/doc/Mx/index.html	\
-	$(prefix)/doc/MapiJava/index.html	\
+html:	$(prefix)/doc/www/Services/Mx/index.html	\
+	$(prefix)/doc/www/APIs/MapiJava/index.html	\
 	$(top_srcdir)/doc/mkdoc.py
+	mv $(prefix)/doc/www/Services/Mx $(prefix)/doc/
+	mv $(prefix)/doc/www/APIs/MapiJava $(prefix)/doc/
 	python $(top_srcdir)/doc/mkdoc.py $(top_srcdir) $(prefix)
+	mv $(prefix)/doc/Mx $(prefix)/doc/www/Services/
+	mv $(prefix)/doc/MapiJava $(prefix)/doc/www/APIs/
 
