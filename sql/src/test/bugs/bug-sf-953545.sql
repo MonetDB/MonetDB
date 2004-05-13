be063c51a9645267b5d15785ed44d6e8f9abbe58
Fabian Groffen <fabian@cwi.nl>
2004-05-13 20:07:11 +0000

    Test script for Niels' newly introduced code + bugs

---
+++ sql/src/test/bugs/bug-sf-953545.sql
@@ -0,0 +1,13 @@
+SET auto_commit=true;
+
+-- the following statements should all fail
+ROLLBACK;
+
+COMMIT;
+
+SAVEPOINT failingsavepoint;
+
+-- this one is incorrect since the savepoint should not exist
+-- however, the server might answer with an error about auto_commit
+-- for that might be cheaper
+RELEASE SAVEPOINT failingsavepoint;
