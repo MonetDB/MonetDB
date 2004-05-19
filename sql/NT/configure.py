a29799f2fa33728f6dd712ddba751cf209fd5ea7
Stefan Manegold <manegold@cwi.nl>
2004-05-19 07:51:19 +0000

    latest sql HEAD now requires MonetDB-4.3.17, and is hence called 2.0.17

--- sql/NT/configure.py
+++ sql/NT/configure.py
@@ -60,7 +60,7 @@ subs = [
     ('@Qinfodir@',          "@Qprefix@@QDIRSEP@info"),
     ('@Qlibexecdir@',       "@Qprefix@@QDIRSEP@libexec"),
     ('@PACKAGE@',           "sql"),
-    ('@VERSION@',           "2.0.16"),
+    ('@VERSION@',           "2.0.17"),
     ('@DIRSEP@',            "\\"),
     ('@prefix@',            prefix),
     ('@MONET_BUILD@',       build),
