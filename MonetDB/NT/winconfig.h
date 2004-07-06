0b298391f2b955396b50203a14df052605e6f4af
Stefan Manegold <manegold@cwi.nl>
2004-07-06 14:05:57 +0000

    this is the "MonetDB 4.3.18" (pre-)release.

--- MonetDB/NT/winconfig.h
+++ MonetDB/NT/winconfig.h
@@ -551,7 +551,7 @@
 /* #undef TM_IN_SYS_TIME */
 
 /* Version number of package */
-#define VERSION "4.3.18_rc03"
+#define VERSION "4.3.18"
 
 /* Define on MS Windows (also under Cygwin) */
 #ifndef WIN32
