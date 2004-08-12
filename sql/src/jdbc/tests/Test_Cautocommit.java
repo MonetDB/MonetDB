1377097aa64a3e21e9d98f3948012cd2f5c18323
Fabian Groffen <fabian@cwi.nl>
2004-08-12 18:56:22 +0000

    great.  without debugging looks nicer

--- sql/src/jdbc/tests/Test_Cautocommit.java
+++ sql/src/jdbc/tests/Test_Cautocommit.java
@@ -3,7 +3,6 @@ import java.sql.*;
 public class Test_Cautocommit {
 	public static void main(String[] args) throws Exception {
 		Class.forName("nl.cwi.monetdb.jdbc.MonetDriver");
-		System.out.println(args[0]);
 		Connection con1 = DriverManager.getConnection(args[0]);
 		Connection con2 = DriverManager.getConnection(args[0]);
 		Statement stmt1 = con1.createStatement();
