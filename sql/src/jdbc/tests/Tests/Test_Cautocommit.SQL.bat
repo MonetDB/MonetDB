951dc712db283250af6421ab886ba6637eb3cfe6
Fabian Groffen <fabian@cwi.nl>
2004-08-14 11:14:57 +0000

    Another attempt to please windows.  Not that it will work, because Java is not installed on the windows machines.  It would be nice however if windows could tell us so.  (Bad command or file name.)

--- sql/src/jdbc/tests/Tests/Test_Cautocommit.SQL.bat
+++ sql/src/jdbc/tests/Tests/Test_Cautocommit.SQL.bat
@@ -1,5 +1,5 @@
 @echo off
 
-set URL="jdbc:monetdb://%HOST%:%SQLPORT%/database?user=monetdb&password=monetdb"
+set CLASSPATH=%CLASSPATH%;%SQL_BUILD%/src/jdbc/MonetJDBC.jar;%TSTBLDBASE%/src/jdbc/MonetJDBC.jar;%SQL_PREFIX%/lib/MonetDB/java/MonetJDBC.jar;%MONET_PREFIX%/lib/MonetDB/java/MonetJDBC.jar;%TSTTRGBASE%/lib/MonetDB/java/MonetJDBC.jar;%SQL_BUILD%/src/jdbc/tests;%TSTBLDBASE%/src/jdbc/tests;%SQL_PREFIX%/lib/sql/Tests;%MONET_PREFIX%/lib/sql/Tests;%TSTTRGBASE%/lib/sql/Tests
 
-call Mlog.bat -x "java -classpath %MONET_PREFIX%/lib/sql/Tests:%MONET_PREFIX%/lib/MonetDB/java/MonetJDBC.jar Test_Cautocommit %URL%"
+call Mlog.bat -x java "%TST% jdbc:monetdb://%HOST%:%SQLPORT%/database?user=monetdb&password=monetdb"
