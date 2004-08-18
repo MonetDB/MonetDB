a5db3c2cda4202803e9174648650506ae83f6913
Arjan Scherpenisse <arjan>
2004-08-18 21:01:42 +0000

    It now works. There was no rule for .pm.i in rules.mk

--- MonetDB/rules.mk
+++ MonetDB/rules.mk
@@ -132,15 +132,21 @@ HIDE=1
 %.glue.c: %.m $(MEL)
 	$(MEL) $(INCLUDES) -glue $< > $@
 
-# The following two rules both generate two files, the .py.c and the
-# .py file.  There may be a race condition here when using a parallel
-# make.  We try to alleviate the problem by sending the .py.c output
+# The following rules generate two files using swig, the .xx.c and the
+# .xx file.  There may be a race condition here when using a parallel
+# make.  We try to alleviate the problem by sending the .xx.c output
 # to a dummy file in the second rule.
 %.py.c: %.py.i
 	$(SWIG) -python $(SWIGFLAGS) -outdir . -o $@ $<
 
 %.py: %.py.i
 	$(SWIG) -python $(SWIGFLAGS) -outdir . -o dymmy.c $<
+
+%.pm.c: %.pm.i
+	$(SWIG) -perl5 -exportall $(SWIGFLAGS) -outdir . -o $@ $<
+
+%.pm: %.pm.i
+	$(SWIG) -perl5 -exportall $(SWIGFLAGS) -outdir . -o dymmy.c $<
 
 %.tex: %.mx
 	$(MX) -1 -H$(HIDE) -t $< 
