47a61880eed8254438af1a9f1a78f01f3391671f
Stefan Manegold <manegold@cwi.nl>
2004-09-07 19:52:45 +0000

    this is "MonetDB_4-3-18a"

--- MonetDB/NT/winconfig.h
+++ MonetDB/NT/winconfig.h
@@ -551,7 +551,7 @@
 /* #undef TM_IN_SYS_TIME */
 
 /* Version number of package */
-#define VERSION "4.3.18"
+#define VERSION "4.3.18a"
 
 /* Define on MS Windows (also under Cygwin) */
 #ifndef WIN32
