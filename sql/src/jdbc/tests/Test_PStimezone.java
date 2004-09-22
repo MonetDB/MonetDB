490412aa0f4111e31051d2a2e140f7600a63e6ba
Fabian Groffen <fabian@cwi.nl>
2004-09-22 12:51:22 +0000

    A test whether there is proper time zone handling in both JDBC and the server

---
+++ sql/src/jdbc/tests/Test_PStimezone.java
@@ -0,0 +1,87 @@
+import java.sql.*;
+import java.util.*;
+
+public class Test_PStimezone {
+	public static void main(String[] args) throws Exception {
+		Class.forName("nl.cwi.monetdb.jdbc.MonetDriver");
+		Connection con = DriverManager.getConnection(args[0]);
+		Statement stmt = con.createStatement();
+		PreparedStatement pstmt;
+		ResultSet rs = null;
+		//DatabaseMetaData dbmd = con.getMetaData();
+
+		con.setAutoCommit(false);
+		// >> false: auto commit was just switched off
+		System.out.println("0. false\t" + con.getAutoCommit());
+
+		try {
+			stmt.executeUpdate("CREATE TABLE table_Test_PStimezone (ts timestamp, tz timestamp with time zone)");
+		} catch (SQLException e) {
+			System.out.println(e);
+			System.out.println("Creation of test table failed! :(");
+			System.out.println("ABORTING TEST!!!");
+			System.exit(-1);
+		}
+
+		try {
+			pstmt = con.prepareStatement("INSERT INTO table_Test_PStimezone VALUES (?, ?)");
+			System.out.print("1. empty call...");
+			try {
+				// should fail (no arguments given)
+				pstmt.execute();
+				System.out.println(" PASSED :(");
+				System.out.println("ABORTING TEST!!!");
+				System.exit(-1);
+			} catch (SQLException e) {
+				System.out.println(" failed :)");
+			}
+
+			System.out.print("2. inserting a records...");
+			
+			Calendar c = Calendar.getInstance();
+			c.setTimeZone(TimeZone.getTimeZone("GMT+00:00"));
+			c.setTime(new java.util.Date(0L));
+			
+			pstmt.setTimestamp(1, new java.sql.Timestamp(c.getTime().getTime()));
+			pstmt.setTimestamp(2, new java.sql.Timestamp(c.getTime().getTime()));
+			pstmt.executeUpdate();
+			
+			c.setTimeZone(TimeZone.getTimeZone("PST"));
+			pstmt.setTimestamp(1, new java.sql.Timestamp(c.getTime().getTime()), c);
+			pstmt.setTimestamp(2, new java.sql.Timestamp(c.getTime().getTime()), c);
+			pstmt.executeUpdate();
+			
+			c.setTimeZone(TimeZone.getTimeZone("GMT+04:15"));
+			pstmt.setTimestamp(1, new java.sql.Timestamp(c.getTime().getTime()), c);
+			pstmt.setTimestamp(2, new java.sql.Timestamp(c.getTime().getTime()), c);
+			pstmt.executeUpdate();
+			
+			System.out.println(" passed :)");
+			System.out.print("3. closing PreparedStatement...");
+			pstmt.close();
+			System.out.println(" passed :)");
+
+			System.out.print("4. selecting records...");
+			pstmt = con.prepareStatement("SELECT * FROM table_Test_PStimezone");
+			rs = pstmt.executeQuery();
+			System.out.println(" passed :)");
+
+			while (rs.next()) {
+				System.out.println(rs.getString("ts") + "\t" + rs.getString("tz"));
+				System.out.println(rs.getTimestamp("ts") + "\t" + rs.getTimestamp("tz"));
+				c.setTimeZone(TimeZone.getTimeZone("PST"));
+				System.out.println(rs.getTimestamp("ts", c) + "\t" + rs.getTimestamp("tz", c));
+				c.setTimeZone(TimeZone.getTimeZone("GMT+00:00"));
+				System.out.println(rs.getTimestamp("ts", c) + "\t" + rs.getTimestamp("tz", c));
+				System.out.println();
+			}
+
+			con.rollback();
+		} catch (SQLException e) {
+			System.out.println("FAILED :( "+ e.getMessage());
+			System.out.println("ABORTING TEST!!!");
+		}
+
+		con.close();
+	}
+}
