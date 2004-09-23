b1cb54d49730f2c99ab86ac39f671b6fd479ecab
cvs2hg <unknown>
2004-09-23 14:31:32 +0000

    fixup commit for tag 'MonetDB_4-4-1_2a'

--- sql/src/benchmarks/arno/Tests/12.sql
+++
@@ -1,1 +0,0 @@
-SELECT MODEL110.is_mutagen, count(distinct MODEL110.model_id ) FROM MODEL MODEL110, BOND BOND111, BOND T1008290387970  WHERE MODEL110.model_id=BOND111.model_id AND MODEL110.model_id=T1008290387970.model_id group by MODEL110.is_mutagen;
