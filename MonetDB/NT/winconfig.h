e474a57d0778f2fe6ffc874ddeb63647cffbf45f
Sjoerd Mullender <sjoerd@acm.org>
2004-10-06 14:36:33 +0000

    Implemented the possibility to run the Mapi protocol over UNIX domain sockets (on systems that support this, obviously). There is a new, fundamental function defined in mapi.mil, called mapi_listen2 (*) which is like the old mapi_listen (which is still available, but now as PROC), except it has one extra argument which denotes the name of a UNIX domain socket file.  The listener started will listen to both the TCP/IP socket denoted by the port (if > 0) and the UNIX domain socket (if not "" or str(nil)).
    On the client side, if mapi_connect's host argument starts with a '/',
    it denotes the name of the socket file to be used and the port will
    then be ignored.  (This works, since UNIX domain sockets are not
    supported on Windows.)
    
    (*) What I wanted to do is have a PROC mapi_listen with the calling
    sequence of the old function, and a new builtin mapi_listen with one
    extra argument.  But this is not possible, since it is not possible to
    have builtins and procs with the same name, even though the arguments
    could disambiguate.

--- MonetDB/NT/winconfig.h
+++ MonetDB/NT/winconfig.h
@@ -431,6 +431,9 @@
 /* Define to 1 if you have the <sys/types.h> header file. */
 #define HAVE_SYS_TYPES_H 1
 
+/* Define to 1 if you have the <sys/un.h> header file. */
+/* #undef HAVE_SYS_UN_H */
+
 /* Define to 1 if you have the <sys/utime.h> header file. */
 #define HAVE_SYS_UTIME_H 1
 
