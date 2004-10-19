d2f36f3200eedd29dc05fc61f831682305dbb3c1
cvs2hg <unknown>
2004-10-19 18:29:01 +0000

    fixup commit for tag 'MonetDB_4-4-1_a'

--- sql/src/benchmarks/arno/Tests/49.sql
+++
@@ -1,1 +0,0 @@
-SELECT MODEL232.is_mutagen,MODEL232.is_mutagen, count(distinct MODEL232.model_id ) FROM MODEL MODEL232  WHERE MODEL232.logp='8' group by MODEL232.is_mutagen , MODEL232.is_mutagen;
--- sql/src/test/bugs/bug-sf-949255.sql
+++
@@ -1,6 +0,0 @@
-create table x (f float);
-insert into x values (3.14);
-select f/f from x;
-
-select cast(3.14/3.14 as integer);
-select cast(f/f as integer) from x;
