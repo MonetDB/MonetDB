645a60a9987ddd89d67711aae9fde7cc3fe1c0d1
Niels Nes <niels@cwi.nl>
2004-10-24 14:03:02 +0000

    sqls new version 2.5.0

--- sql/NT/configure.py
+++ sql/NT/configure.py
@@ -54,7 +54,7 @@ subs = [
     ('@Qinfodir@',          "@Qprefix@@QDIRSEP@info"),
     ('@Qlibexecdir@',       "@Qprefix@@QDIRSEP@libexec"),
     ('@PACKAGE@',           "sql"),
-    ('@VERSION@',           "2.4.1"),
+    ('@VERSION@',           "2.5.0"),
     ('@DIRSEP@',            "\\"),
     ('@prefix@',            prefix),
     ('@MONETDB_BUILD@',       build),
