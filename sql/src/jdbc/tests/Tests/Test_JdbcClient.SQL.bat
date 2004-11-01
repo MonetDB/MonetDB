b8faef7050ebad74361a1338191b8c1ec52e0f4d
Fabian Groffen <fabian@cwi.nl>
2004-11-01 14:18:20 +0000

    try to force UTF for systems that don't use it by default (gentor=POSIX)

--- sql/src/jdbc/tests/Tests/Test_JdbcClient.SQL.bat
+++ sql/src/jdbc/tests/Tests/Test_JdbcClient.SQL.bat
@@ -2,4 +2,6 @@
 
 set CLASSPATH=%CLASSPATH%;%SQL_BUILD%/src/jdbc/MonetDB_JDBC.jar;%TSTBLDBASE%/src/jdbc/MonetDB_JDBC.jar;%SQL_PREFIX%/lib/MonetDB/java/MonetDB_JDBC.jar;%MONETDB_PREFIX%/lib/MonetDB/java/MonetDB_JDBC.jar;%TSTTRGBASE%/lib/MonetDB/java/MonetDB_JDBC.jar;%SQL_BUILD%/src/jdbc/tests;%TSTBLDBASE%/src/jdbc/tests;%SQL_PREFIX%/lib/sql/Tests;%MONETDB_PREFIX%/lib/sql/Tests;%TSTTRGBASE%/lib/sql/Tests
 
+set LANG=en_US.UTF-8
+
 call Mlog.bat -x java "%TST%" "jdbc:monetdb://%HOST%:%SQLPORT%/database?user=monetdb&password=monetdb"
