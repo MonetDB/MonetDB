1a457f0405b55c058a8ecfbd78d11aad8233a144
Sjoerd Mullender <sjoerd@acm.org>
2004-11-12 10:25:30 +0000

    Put database and log files in Application Data folder instead of Program Files folder.

--- MonetDB/NT/Mserver.bat
+++ MonetDB/NT/Mserver.bat
@@ -1,12 +1,21 @@
-@rem figure out the folder name
-@set MONETDB=%~dp0
+@echo off
 
-@rem remove the final backslash from the path
-@set MONETDB=%MONETDB:~0,-1%
+rem figure out the folder name
+set MONETDB=%~dp0
 
-@rem extend the search path with our EXE and DLL folders
-@rem we depend on pthreadVCE.dll having been copied to the lib folder
-@set PATH=%MONETDB%\bin;%MONETDB%\lib;%PATH%
+rem remove the final backslash from the path
+set MONETDB=%MONETDB:~0,-1%
 
-@rem start the real server
-@"%MONETDB%\bin\Mserver.exe" --set "prefix=%MONETDB%" --set "exec_prefix=%MONETDB%" %*
+rem extend the search path with our EXE and DLL folders
+rem we depend on pthreadVCE.dll having been copied to the lib folder
+set PATH=%MONETDB%\bin;%MONETDB%\lib;%PATH%
+
+set MONETDBFARM=
+set SQLLOGDIR=
+if "%ALLUSERSPROFILE%" == "" goto skip
+set MONETDBFARM="--dbfarm=%ALLUSERSPROFILE%\Application Data\MonetDB"
+set SQLLOGDIR=--set "sql_logdir=%ALLUSERSPROFILE%\Application Data\MonetDB\var\MonetDB\log"
+:skip
+
+rem start the real server
+"%MONETDB%\bin\Mserver.exe" --set "prefix=%MONETDB%" --set "exec_prefix=%MONETDB%" %MONETDBFARM% %SQLLOGDIR% %*
