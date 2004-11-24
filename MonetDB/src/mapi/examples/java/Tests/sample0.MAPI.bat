3f782eecf0bde2077ee0559fae901baedcf2a669
Fabian Groffen <fabian@cwi.nl>
2004-11-24 10:50:00 +0000

    Make our Java tests SuSE compatible(tm).
    Because I had to change the code, I made it a real Java program
    immediately.
    Also removed redundancy; two programs with only one line differing,
    feels not completely correct, made one program with some extra command
    line options.

--- MonetDB/src/mapi/examples/java/Tests/sample0.MAPI.bat
+++ MonetDB/src/mapi/examples/java/Tests/sample0.MAPI.bat
@@ -7,11 +7,11 @@ goto s1
 set CLASSPATH=%MONETDB_BUILD%\src\mapi\clients\java\mapi.jar;%CLASSPATH%
 :s1
 
-if exist "%MONETDB_BUILD%\src\mapi\examples\java\%TST%.class" goto l2
+if exist "%MONETDB_BUILD%\src\mapi\examples\java\sample.class" goto l2
 set CLASSPATH=%MONETDB_PREFIX\lib\MonetDB\Tests;%CLASSPATH%
 goto s2
 :l2
 set CLASSPATH=%MONETDB_BUILD%\src\mapi\examples\java;%CLASSPATH%
 :s2
 
-call Mlog.bat -x java %TST% %MAPIPORT%
+call Mlog.bat -x java sample %HOST% %MAPIPORT%
