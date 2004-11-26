db590fd0a323feaa76a4973f8e80bce7dace9eed
Sjoerd Mullender <sjoerd@acm.org>
2004-11-26 12:45:23 +0000

    Added some comments and fixed the path to the SQL log directory.

--- MonetDB/NT/Mserver.bat
+++ MonetDB/NT/Mserver.bat
@@ -10,11 +10,24 @@ rem extend the search path with our EXE and DLL folders
 rem we depend on pthreadVCE.dll having been copied to the lib folder
 set PATH=%MONETDB%\bin;%MONETDB%\lib;%PATH%
 
+rem possibly move the database from a previous installation to our
+rem currently preferred location, and prepare the arguments to Mserver
+rem to tell it where that location is
+
 set MONETDBFARM=
 set SQLLOGDIR=
+rem use the Application Data folder for our database
 if "%ALLUSERSPROFILE%" == "" goto skip
-set MONETDBFARM="--dbfarm=%ALLUSERSPROFILE%\Application Data\MonetDB"
-set SQLLOGDIR=--set "sql_logdir=%ALLUSERSPROFILE%\Application Data\MonetDB\var\MonetDB\log"
+set MONETDBFARM="--dbfarm=%ALLUSERSPROFILE%\Application Data\MonetDB\dbfarm"
+set SQLLOGDIR=--set "sql_logdir=%ALLUSERSPROFILE%\Application Data\MonetDB\log"
+
+rem if the database exists by the old name, move it
+if not exist "%MONETDB%\var\MonetDB" goto skip
+rem if the new path already exists, don't try moving the old to the new
+if exist "%ALLUSERSPROFILE%\Application Data\MonetDB" goto skip
+move "%MONETDB%\var\MonetDB" "%ALLUSERSPROFILE%\Application Data\MonetDB"
+rmdir "%MONETDB%\var"
+
 :skip
 
 rem start the real server
