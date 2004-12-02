61e3a965953013afe96fa259df6916f690e00090
cvs2hg <unknown>
2004-12-02 08:05:22 +0000

    fixup commit for tag 'MonetDB_4-5-0_c'

--- sql/src/benchmarks/arno/Tests/56.sql
+++
@@ -1,1 +0,0 @@
-SELECT MODEL237.is_mutagen,MODEL237.logp, count(distinct MODEL237.model_id ) FROM MODEL MODEL237  WHERE MODEL237.logp='7' group by MODEL237.logp , MODEL237.is_mutagen;
--- sql/src/benchmarks/arno/Tests/71.sql
+++
@@ -1,1 +0,0 @@
-SELECT MODEL264.is_mutagen,MODEL264.logp, count(distinct MODEL264.model_id ) FROM MODEL MODEL264  WHERE MODEL264.logp='4' group by MODEL264.logp , MODEL264.is_mutagen;
