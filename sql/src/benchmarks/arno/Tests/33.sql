2daabf1278aec87b8e3e6dff11d48ce7caf4b244
cvs2hg <unknown>
2004-12-10 13:13:28 +0000

    fixup commit for tag 'fake_project_0'

--- sql/src/benchmarks/arno/Tests/33.sql
+++
@@ -1,1 +0,0 @@
-SELECT MODEL192.is_mutagen,MODEL192.is_mutagen, count(distinct MODEL192.model_id ) FROM MODEL MODEL192  WHERE MODEL192.lumo='-3' group by MODEL192.is_mutagen , MODEL192.is_mutagen;
--- sql/src/benchmarks/arno/Tests/85.sql
+++
@@ -1,1 +0,0 @@
-SELECT MODEL297.is_mutagen,MODEL297.lumo, count(distinct MODEL297.model_id ) FROM MODEL MODEL297  WHERE MODEL297.logp='1' group by MODEL297.lumo , MODEL297.is_mutagen;
