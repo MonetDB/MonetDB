16d00d9cb211324654f0ddd7d1a8cf77d2ff7aa1
cvs2hg <unknown>
2004-12-20 21:38:27 +0000

    fixup commit for tag 'fake_project_6'

--- sql/src/test/bugs/bug-sf-904025.sql
+++
@@ -1,9 +0,0 @@
-create table tmp(name string, id int);
-insert into tmp select name, id from tables where "istable" = true;
-select count(*) from tmp;
-delete from tmp;
-select count(*) from tmp;
-insert into tmp select name, id from tables where "istable" = true;
-select count(*) from tmp;
-delete from tmp;
-select count(*) from tmp;
