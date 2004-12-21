645200ca4f24ee11435c39b72823bf44af560b81
Niels Nes <niels@cwi.nl>
2004-12-21 23:15:11 +0000

    java tests also move to the pkgdatadir (example /usr/share/MonetDB/Tests/*)

--- MonetDB/src/mapi/examples/java/Tests/sample1.MAPI.bat
+++ MonetDB/src/mapi/examples/java/Tests/sample1.MAPI.bat
@@ -1,14 +1,14 @@
 @echo off
 
 if exist "%MONETDB_BUILD%\src\mapi\clients\java\mapi.jar" goto l1
-set CLASSPATH=%MONETDB_PREFIX\lib\MonetDB\java\mapi.jar;%CLASSPATH%
+set CLASSPATH=%MONETDB_PREFIX\share\MonetDB\lib\mapi.jar;%CLASSPATH%
 goto s1
 :l1
 set CLASSPATH=%MONETDB_BUILD%\src\mapi\clients\java\mapi.jar;%CLASSPATH%
 :s1
 
 if exist "%MONETDB_BUILD%\src\mapi\examples\java\sample.class" goto l2
-set CLASSPATH=%MONETDB_PREFIX\lib\MonetDB\Tests;%CLASSPATH%
+set CLASSPATH=%MONETDB_PREFIX\share\MonetDB\Tests;%CLASSPATH%
 goto s2
 :l2
 set CLASSPATH=%MONETDB_BUILD%\src\mapi\examples\java;%CLASSPATH%
