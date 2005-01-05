b3f8dfe6f9f4d4cfc317501fc561cfe2968ba844
Fabian Groffen <fabian@cwi.nl>
2005-01-05 19:17:57 +0000

    Approve output

--- sql/src/test/keys.sql
+++ sql/src/test/keys.sql
@@ -3,7 +3,7 @@ NULL, predicate integer NOT NULL, "object" integer NOT NULL, explicit
 boolean NOT NULL, CONSTRAINT unique_key UNIQUE(subject, predicate, "object"));
 CREATE INDEX allnewtriples_subject_idx ON allnewtriples (subject);
 CREATE INDEX allnewtriples_predicate_idx ON allnewtriples (predicate);
-CREATE INDEX allnewtriples_object_idx ON allnewtriples (object);
+CREATE INDEX allnewtriples_object_idx ON allnewtriples ("object");
 
 SELECT idxs.name, idxs."type", keys.name, keys."type" 
 FROM idxs LEFT JOIN keys on idxs.name = keys.name;
