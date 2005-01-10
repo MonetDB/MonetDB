77c52afb915c7c91f7b10b70073e511e9e259c5d
Steffen Goeldner <sgoeldner@users.sourceforge.net>
2005-01-10 09:47:23 +0000

    Revised the documentation (POD):
     - replaced paragraphs catered to the Mapi based DBD::monetdbPP
     - dropped paragraphs that are well documented in DBI
     - shifted emphasis towards specifics/discrepancies w.r.t. to DBI

--- MonetDB/src/mapi/clients/perl/DBD/monetdb.pm
+++ MonetDB/src/mapi/clients/perl/DBD/monetdb.pm
@@ -732,23 +732,17 @@ __END__
 
 =head1 NAME
 
-DBD::monetdb - DBD implementation on top of SWIG bindings
+DBD::monetdb - MonetDB Driver for DBI
 
 =head1 SYNOPSIS
 
-    use DBI;
+  use DBI();
 
-    $dsn = "dbi:monetdb:database=$database;host=$hostname";
+  my $dbh = DBI->connect('dbi:monetdb:');
 
-    $dbh = DBI->connect($dsn, $user, $password);
-
-    $drh = DBI->install_driver('monetdb');
-
-    $sth = $dbh->prepare('SELECT * FROM foo WHERE bla');
-    $sth->execute;
-    $numRows = $sth->rows;
-    $numFields = $sth->{'NUM_OF_FIELDS'};
-    $sth->finish;
+  my $sth = $dbh->prepare('SELECT * FROM env');
+  $sth->execute;
+  $sth->dump_results;
 
 =head1 EXAMPLE
 
@@ -758,9 +752,8 @@ DBD::monetdb - DBD implementation on top of SWIG bindings
   use DBI;
 
   # Connect to the database.
-  my $dbh = DBI->connect('dbi:monetdb:database=test;host=localhost',
-                         'joe', "joe's password",
-                         {'RaiseError' => 1});
+  my $dbh = DBI->connect('dbi:monetdb:host=localhost',
+    'joe', "joe's password", { RaiseError => 1 } );
 
   # Drop table 'foo'. This may fail, if 'foo' doesn't exist.
   # Thus we put an eval around it.
@@ -780,22 +773,23 @@ DBD::monetdb - DBD implementation on top of SWIG bindings
 
   # Now retrieve data from the table.
   my $sth = $dbh->prepare('SELECT id, name FROM foo');
-  $sth->execute();
-  while (my $ref = $sth->fetchrow_arrayref()) {
-    print "Found a row: id = $ref->[0], name = $ref->[1]\n";
+  $sth->execute;
+  while ( my $row = $sth->fetch ) {
+    print "Found a row: id = $row->[0], name = $row->[1]\n";
   }
 
   # Disconnect from the database.
-  $dbh->disconnect();
-
+  $dbh->disconnect;
 
 =head1 DESCRIPTION
 
-DBD::monetdb is a Pure Perl client interface for the MonetDB Database Server. It means this module enables you to connect to MonetDB server from any platform where Perl is running, but MonetDB has not been installed.
+DBD::monetdb is a Pure Perl client interface for the MonetDB Database Server.
+However, it requires the SWIG generated MapiLib - a wrapper module for
+libMapi.
 
 From perl you activate the interface with the statement
 
-    use DBI;
+  use DBI;
 
 After that you can connect to multiple MonetDB database servers
 and send multiple queries to any of them via a simple object oriented
@@ -803,43 +797,37 @@ interface. Two types of objects are available: database handles and
 statement handles. Perl returns a database handle to the connect
 method like so:
 
-  $dbh = DBI->connect("dbi:monetdb:database=$db;host=$host",
-		      $user, $password, {RaiseError => 1});
+  $dbh = DBI->connect("dbi:monetdb:host=$host",
+    $user, $password, { RaiseError => 1 } );
 
 Once you have connected to a database, you can can execute SQL
 statements with:
 
-  my $query = sprintf('INSERT INTO foo VALUES (%d, %s)',
-		      $number, $dbh->quote('name'));
-  $dbh->do($query);
+  my $sql = sprintf('INSERT INTO foo VALUES (%d, %s)',
+    $number, $dbh->quote('name'));
+  $dbh->do($sql);
 
-See L<DBI(3)> for details on the quote and do methods. An alternative
+See L<DBI> for details on the quote and do methods. An alternative
 approach is
 
-  $dbh->do('INSERT INTO foo VALUES (?, ?)', undef,
-	   $number, $name);
+  $dbh->do('INSERT INTO foo VALUES (?, ?)', undef, $number, $name);
 
 in which case the quote method is executed automatically. See also
-the bind_param method in L<DBI(3)>. See L<DATABASE HANDLES> below
-for more details on database handles.
+the bind_param method in L<DBI>.
 
 If you want to retrieve results, you need to create a so-called
 statement handle with:
 
   $sth = $dbh->prepare("SELECT id, name FROM $table");
-  $sth->execute();
+  $sth->execute;
 
 This statement handle can be used for multiple things. First of all
 you can retreive a row of data:
 
-  my $row = $sth->fetchow_arrayref();
+  my $row = $sth->fetch;
 
 If your table has columns ID and NAME, then $row will be array ref with
-index 0 and 1. See L<STATEMENT HANDLES> below for more details on
-statement handles.
-
-I's more formal approach:
-
+index 0 and 1.
 
 =head2 Class Methods
 
@@ -847,15 +835,13 @@ I's more formal approach:
 
 =item B<connect>
 
-    use DBI;
+  use DBI();
 
-    $dsn = "dbi:monetdb:$database";
-    $dsn = "dbi:monetdb:database=$database;host=$hostname";
-    $dsn = "dbi:monetdb:database=$database;host=$hostname;port=$port";
+  $dsn = 'dbi:monetdb:';
+  $dsn = "dbi:monetdb:host=$host";
+  $dsn = "dbi:monetdb:host=$host;port=$port";
 
-    $dbh = DBI->connect($dsn, $user, $password);
-
-A C<database> must always be specified.
+  $dbh = DBI->connect($dsn, $user, $password);
 
 =over
 
@@ -865,23 +851,18 @@ The default host to connect to is 'localhost', i.e. your workstation.
 
 =item port
 
-The port where MonetDB daemon listens to. default for SQL is 45123.
+The port where MonetDB daemon listens to. Default for SQL is 45123.
 
 =back
 
 =back
 
-=head2 MetaData Method
+=head2 MetaData Methods
 
-=over 4
-
-=item B<tables>
-
-    @names = $dbh->tables;
-
-Returns a list of table and view names, possibly including a schema prefix. This list should include all tables that can be used in a "SELECT" statement without further qualification.
-
-=back
+All MetaData methods are supported. However, column_info() currently doesn't
+provide much datatype related information.
+The foreign_key_info() method returns a SQL/CLI like result set,
+because it provides additional information about unique keys.
 
 =head2 Private MetaData Methods
 
@@ -899,9 +880,13 @@ B<WARNING>: This method is obsolete due to DBI's $dbh->tables().
 
     @tables = $dbh->func('_ListTables');
 
-Once connected to the desired database on the desired MonetDB server with the "DBI-"connect()> method, we may extract a list of the tables that have been created within that database.
+Once connected to the desired database on the desired MonetDB server with the
+DBI connect() method, we may extract a list of the tables that have been
+created within that database.
 
-"ListTables" returns an array containing the names of all the tables present within the selected database. If no tables have been created, an empty list is returned.
+"ListTables" returns an array containing the names of all the tables present
+within the selected database. If no tables have been created, an empty list
+is returned.
 
     @tables = $dbh->func('_ListTables');
     foreach $table (@tables) {
@@ -910,34 +895,31 @@ Once connected to the desired database on the desired MonetDB server with the "D
 
 =back
 
+=head1 HANDLE ATTRIBUTES
 
-=head1 DATABASE HANDLES
-=head1 STATEMENT HANDLES
+The following attributes are currently not supported:
 
-The statement handles of DBD::monetdb support a number
-of attributes. You access these by using, for example,
+  LongReadLen
+  LongTruncOk
 
-  my $numFields = $sth->{'NUM_OF_FIELDS'};
+=head2 Database Handle Attributes
 
-=over
+The following attributes are currently not supported:
 
-=item NUM_OF_FIELDS
+  RowCacheSize
 
-Number of fields returned by a I<SELECT> statement. You may use this for checking whether a statement returned a result.
-A zero value indicates a non-SELECT statement like I<INSERT>, I<DELETE> or I<UPDATE>.
+=head2 Statement Handle Attributes
 
-=back
+The following attributes are currently not supported:
 
-=head1 INSTALLATION
+  TYPE
+  PRECISION
+  SCALE
+  NULLABLE
+  CursorName
+  RowsInCache
 
-To install this module type the following:
-
-   perl Makefile.PL
-   make
-   make test
-   make install
-
-=head1 SUPPORT OPERATING SYSTEM
+=head1 OPERATING SYSTEM SUPPORT
 
 This module has not been tested on all these OSes.
 
@@ -963,59 +945,6 @@ TODO with perl5.005_03 build for i386-freebsd.
 
 =back
 
-=head1 DEPENDENCIES
-
-This module requires these other modules and libraries:
-
-  DBI
-  IO::Socket;
-  IO::Handle;
-  Mapi;
-
-B<Mapi> is a Pure Perl client interface routines to setup
-the communication with the MonetDB server.
-
-=head2 Database Handles
-
-Cannot be used
-
-=over 4
-
-=item * $dbh->{info}
-
-=back
-
-=head2 Statement Handles
-
-=over 4
-
-=item * The return value of I<execute('SELECT * from table')>
-
-<DBD::monetdb> surely returns <0E0>.
-
-=back
-
-Cannot be used.
-
-
-=back
-
-=head2 SQL Extensions
-
-=over 4
-
-=back
-
-=head1 TODO
-
-Encryption of the password independent of I<Math::BigInt>.
-
-Enables access to much metadata.
-
-Inclusion of info commands as presented in Mapi.mx
-
-=head1 SEE ALSO
-
 =head1 AUTHORS
 
 Martin Kersten <lt>Martin.Kersten@cwi.nl<gt>
@@ -1026,5 +955,16 @@ Copyright (C) 2003-2004 CWI Netherlands. All rights reserved.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=head2 MonetDB
+
+  Homepage    : http://monetdb.cwi.nl
+  SourceForge : http://sourceforge.net/projects/monetdb
+
+=head2 Perl modules
+
+L<DBI>, L<MapiLib>, L<DBD::monetdbPP>
 
 =cut
