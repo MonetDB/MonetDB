bd1e39fce42b05d370ecb1d13dedeabac257fc04
cvs2hg <unknown>
2005-01-11 23:25:23 +0000

    fixup commit for tag 'MonetDB_4-5-0_d'

--- pathfinder/tests/XPath/Tests/q15.xq
+++
@@ -1,1 +0,0 @@
-doc("auctions116KB.xml")/id(/site/closed_auctions/closed_auction[id(seller/@person)/name='Kaivalya Potorti']/itemref/@item)
--- sql/src/test/VOC/VOCinsert.sql
+++
@@ -1,2 +0,0 @@
-insert into voyage values(4408,3,'BRESLAU','Jan Kornelis Roos',1150,'hired',1774,'A','Z',date '1783-02-15','Rammekens',date '1783-05-27',date '1783-06-12',date '1783-08-06','Batavia');
-commit work;
