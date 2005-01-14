e77b0b091e510af2e2e75024eeb4536afe27d9ee
cvs2hg <unknown>
2005-01-14 17:18:30 +0000

    fixup commit for tag 'fake_project_8'

--- sql/src/benchmarks/as3ap/as3ap-schema.sql
+++
@@ -1,5 +0,0 @@
-/*database as3ap*/
-create table uniques(k integer  not null, i integer  not null, si integer , f float not null,d double not null, decim integer  not null, date date  not null, code char(10)  not null, name char(20)  not null, address string not null, fill char(6)  not null);
-create table hundred(k integer  not null, i integer  not null, si integer , f float  not null, d double  not null, decim integer  not null, date date  not null, code char(10)  not null, name char(20)  not null, address string not null, fill char(6)  not null);
-create table tenpct(k integer  not null, i integer  not null, si integer , f float  not null, d double  not null, decim integer  not null, date date  not null, code char(10)  not null, name char(20)  not null, address string not null, fill char(6)  not null);
-create table updates(k integer  not null, i integer  not null, si integer , f float  not null, d double  not null, decim integer  not null, date date  not null, code char(10)  not null, name char(20)  not null, address string not null, fill char(6)  not null);
--- sql/src/test/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-PWD=`pwd`
-
-rm -rf /ufs/niels/data/dbfarm/sql 
-Mcreatedb -db sql
-Mserver -db sql ../parser/catalog.mil ../parser/sql.mil <<EOF
-sql( "$PWD/as3ap.sql", "-", 1);
-quit;
-EOF
