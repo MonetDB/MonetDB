54e1e8c3c80211fbbd9237c6745e66b8ed002254
Fabian Groffen <fabian@cwi.nl>
2005-01-14 10:13:50 +0000

    Since the server can handle it now, approve/revive and activate testing of the dumping capabilities of JdbcClient, especially focussed on keys and indexes.

--- sql/src/jdbc/tests/JdbcClient_create_tables.sql
+++ sql/src/jdbc/tests/JdbcClient_create_tables.sql
@@ -19,3 +19,19 @@ CREATE TABLE "foreign" (
 	FOREIGN KEY (id) REFERENCES allnewtriples (id),
 	FOREIGN KEY (subject, predicate, object) REFERENCES allnewtriples (subject, predicate, object)
 );
+
+CREATE TABLE "triples" (
+	"id"        int NOT NULL,
+	"subject"   int NOT NULL,
+	"predicate" int NOT NULL,
+	"object"    int NOT NULL,
+	"explicit"  boolean     NOT NULL,
+	CONSTRAINT "triples_subject_predicate_object_unique" UNIQUE ("subject", "predicate", "object")
+);
+CREATE INDEX "triples_object_idx" ON "triples" ("object");
+CREATE INDEX "triples_predicate_idx" ON "triples" ("predicate");
+CREATE INDEX "triples_predicate_object_idx" ON "triples" ("predicate", "object");
+CREATE INDEX "triples_subject_idx" ON "triples" ("subject");
+CREATE INDEX "triples_subject_object_idx" ON "triples" ("subject", "object");
+CREATE INDEX "triples_subject_predicate_idx" ON "triples" ("subject", "predicate");
+
