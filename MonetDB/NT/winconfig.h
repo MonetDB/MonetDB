fddd748557033c10a8a477de95679a6a76cf30e2
Stefan Manegold <manegold@cwi.nl>
2005-01-20 23:44:57 +0000

    With the new release branch MonetDB_4-6 being spawned off, the main branch becomes MonetDB-4.7.0.
    Please see posting in the monetdb-announce mailing list
    (http://lists.sourceforge.net/lists/listinfo/monetdb-announce)
    for details.
    
    Please note:
    All bug fixes (and only these!) on MonetDB are bound to go to the
    new release branch MonetDB_4-6, only.
    On a regular basis, I will then take care of propagating these bug-fixes
    back to the main branch.
    
    All new features are bound to go to the main branche (and only there!).

--- MonetDB/NT/winconfig.h
+++ MonetDB/NT/winconfig.h
@@ -565,7 +565,7 @@
 /* #undef TM_IN_SYS_TIME */
 
 /* Version number of package */
-#define VERSION "4.6.0_rc0"
+#define VERSION "4.7.0"
 
 /* Define on MS Windows (also under Cygwin) */
 #ifndef WIN32
