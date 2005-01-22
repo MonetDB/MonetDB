7caf50e52f38cff1eb1b7a75bbbfc28c30ce674e
cvs2hg <unknown>
2005-01-22 00:57:34 +0000

    fixup commit for tag 'MonetDB_4-7-0_2'

--- sql/src/jdbc/tests/Tests/Test_JdbcClient.SQL.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-set CLASSPATH=%CLASSPATH%;%SQL_BUILD%/src/jdbc/MonetDB_JDBC.jar;%TSTBLDBASE%/src/jdbc/MonetDB_JDBC.jar;%SQL_PREFIX%/share/MonetDB/lib/MonetDB_JDBC.jar;%MONETDB_PREFIX%/share/MonetDB/lib/MonetDB_JDBC.jar;%TSTTRGBASE%/share/MonetDB/lib/MonetDB_JDBC.jar;%SQL_BUILD%/src/jdbc/tests;%TSTBLDBASE%/src/jdbc/tests;%SQL_PREFIX%/share/sql/Tests;%MONETDB_PREFIX%/share/sql/Tests;%TSTTRGBASE%/share/sql/Tests
-
-set LANG=en_US.UTF-8
-
-call Mlog.bat -x java "%TST%" "jdbc:monetdb://%HOST%:%SQLPORT%/database?user=monetdb&password=monetdb"
