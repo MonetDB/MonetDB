ae8f2fcf4309e11955c28b77ea690ca73a1f215b
Stefan Manegold <manegold@cwi.nl>
2005-01-25 06:58:32 +0000

    propagated changes of Monday Jan 24 2005 from the SQL_2-6 release branch to the development trunk

--- sql/src/jdbc/tests/Tests/Test_JdbcClient.SQL.bat
+++ sql/src/jdbc/tests/Tests/Test_JdbcClient.SQL.bat
@@ -1,7 +1,22 @@
 @echo off
 
-set CLASSPATH=%CLASSPATH%;%SQL_BUILD%/src/jdbc/MonetDB_JDBC.jar;%TSTBLDBASE%/src/jdbc/MonetDB_JDBC.jar;%SQL_PREFIX%/share/MonetDB/lib/MonetDB_JDBC.jar;%MONETDB_PREFIX%/share/MonetDB/lib/MonetDB_JDBC.jar;%TSTTRGBASE%/share/MonetDB/lib/MonetDB_JDBC.jar;%SQL_BUILD%/src/jdbc/tests;%TSTBLDBASE%/src/jdbc/tests;%SQL_PREFIX%/share/sql/Tests;%MONETDB_PREFIX%/share/sql/Tests;%TSTTRGBASE%/share/sql/Tests
+JAR=""
+if exist "%SQL_BUILD%\src\jdbc\MonetDB_JDBC.jar"			set JAR="%SQL_BUILD%\src\jdbc\MonetDB_JDBC.jar"
+if exist "%TSTBLDBASE%\src\jdbc\MonetDB_JDBC.jar"		set JAR="%TSTBLDBASE%\src\jdbc\MonetDB_JDBC.jar"
+if exist "%SQL_PREFIX%\share\MonetDB\lib\MonetDB_JDBC.jar"	set JAR="%SQL_PREFIX%\share\MonetDB\lib\MonetDB_JDBC.jar"
+if exist "%MONETDB_PREFIX%\share\MonetDB\lib\MonetDB_JDBC.jar"	set JAR="%MONETDB_PREFIX%\share\MonetDB\lib\MonetDB_JDBC.jar"
+if JAR == ""							set JAR="%TSTTRGBASE%\share\MonetDB\lib\MonetDB_JDBC.jar"
+fi
+
+echo user=monetdb>	.monetdb
+echo password=monetdb>>	.monetdb
 
 set LANG=en_US.UTF-8
 
-call Mlog.bat -x java "%TST%" "jdbc:monetdb://%HOST%:%SQLPORT%/database?user=monetdb&password=monetdb"
+call Mlog.bat -x java -jar "%JAR%" -h %HOST% -p %SQLPORT% -f "%RELSRCDIR%\\..\\JdbcClient_create_tables.sql"
+
+call Mlog.bat -x java -jar "%JAR%" -h %HOST% -p %SQLPORT% -f "%RELSRCDIR%\\..\\JdbcClient_inserts_selects.sql"
+
+call Mlog.bat -x java -jar "%JAR%" -h %HOST% -p %SQLPORT% -d
+
+del .monetdb
