aa5953882714a018e8da5b357696a7bfc88fe4c5
Stefan Manegold <manegold@cwi.nl>
2005-02-11 13:50:18 +0000

    ... Windoze requires double quotes iso single quotes ...

--- MonetDB/NT/MapiServer.bat
+++ MonetDB/NT/MapiServer.bat
@@ -5,4 +5,4 @@
 @set MONETDB=%MONETDB:~0,-1%
 
 @rem start the real server
-@"%MONETDB%\Mserver.bat" "%MONETDB%\lib\MonetDB\mapi.mil" %*
+@"%MONETDB%\Mserver.bat" --dbinit="module(mapi); mapi_start();" %*
