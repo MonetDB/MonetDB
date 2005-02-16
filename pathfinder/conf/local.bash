4140e51d73f90267e40b7dac47321b8b39533736
Stefan Manegold <manegold@cwi.nl>
2005-02-16 16:49:01 +0000

    the default libxml2-2.4.9 in /usr/lib/ on our RedHat 2.1AW / IA-64 machine (titan) seems to be broken (at least, xmlSwitchEncoding() messes-up xmlCtx->_private, resulting in a segfault in the shredder); hence, we make sure we use our "own" libxml2-2.6.10 in /usr/local/lib/ ...

--- pathfinder/conf/local.bash
+++ pathfinder/conf/local.bash
@@ -6,7 +6,7 @@ xml=""
 
 for d in /usr /usr/local ${softpath} ${acoi_soft}/${os}/${BITS} \
 		`ls -d /soft/${BITS}/libxml2* 2>/dev/null | tail -n1` ; do
-	if [ ! "${xml}"  -a  -x "${d}/bin/xml2-config" ] ; then
+	if [ ! "${xml}"  -a  -x "${d}/bin/xml2-config"  -a  "${host%.ins.cwi.nl}${d}" != "titan/usr" ] ; then
 		xml="${d}"
 		break
 	fi
