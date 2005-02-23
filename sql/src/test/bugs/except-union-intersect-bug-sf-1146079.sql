1edbb8f473db7d74c665d0f854001539dbf7e8ff
Fabian Groffen <fabian@cwi.nl>
2005-02-23 09:18:47 +0000

    Updated the test itself, added more specific (and correct) comments on what to expect.  Note that the stable output is not correct now.  Will patch this later by hand. (making available the test for those who try to generate the correct output)

--- sql/src/test/bugs/except-union-intersect-bug-sf-1146079.sql
+++ sql/src/test/bugs/except-union-intersect-bug-sf-1146079.sql
@@ -71,10 +71,11 @@ SELECT * FROM a INTERSECT SELECT * FROM b; -- should be first 50% of a
 ROLLBACK;
 
 -- now check the non-duplicate removing versions
+-- (they are very tricky, so be on your marks!)
 START TRANSACTION;
-SELECT class FROM a EXCEPT ALL SELECT 16; -- all but 16
+SELECT class FROM a EXCEPT ALL SELECT 16; -- all but one 16 (will have 16 in output!)
 SELECT class FROM a UNION ALL SELECT 16; -- all plus 16
-SELECT class FROM a INTERSECT ALL SELECT 16; -- a few 16s
+SELECT class FROM a INTERSECT ALL SELECT 16; -- just one 16 (!)
 ROLLBACK;
 
 -- do the same with a real table (because our tables have a key on both
@@ -82,10 +83,10 @@ ROLLBACK;
 -- would never be tested for tables)
 START TRANSACTION;
 SELECT * FROM a EXCEPT ALL SELECT * FROM b; -- last 50% of a
-SELECT class FROM a EXCEPT ALL SELECT class FROM b; -- dups
+SELECT class FROM a EXCEPT ALL SELECT class FROM b; -- a minus the elements from b that are in a (if count(x) in a > count(x) in b, x will appear in output)
 SELECT * FROM a UNION ALL SELECT * FROM b; -- a + b
 SELECT * FROM a INTERSECT ALL SELECT * FROM b; -- first 50% of a
-SELECT class FROM a INTERSECT ALL SELECT class FROM b; -- dups
+SELECT class FROM a INTERSECT ALL SELECT class FROM b; -- only those that are both in a and b (min(count(a, x), count(b, x)) !!!
 ROLLBACK;
 
 -- cleanup! (also should cascade into dropping the index)
