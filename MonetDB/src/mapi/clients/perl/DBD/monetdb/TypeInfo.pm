6c2ee645eddfe4f754f5ef4526ef40bdd8918b59
Steffen Goeldner <sgoeldner@users.sourceforge.net>
2005-02-23 15:04:39 +0000

    Added datatype synonyms. Favored SQL/CLI codes over ODBC codes.

--- MonetDB/src/mapi/clients/perl/DBD/monetdb/TypeInfo.pm
+++ MonetDB/src/mapi/clients/perl/DBD/monetdb/TypeInfo.pm
@@ -27,39 +27,55 @@ my %index =
 
 my @data =
 (
-        [ "boolean",                  SQL_BIT,                      1,      undef,undef,undef,            1,0,2,0,    1,0,    undef,           undef,undef,SQL_BIT,     undef,undef,undef, ],
-        [ "tinyint",                  SQL_TINYINT,                  2,      undef,undef,"precision",      1,0,2,0,    0,0,    undef,           0,    0,    SQL_TINYINT, undef,10,   undef, ],
-        [ "bigint",                   -5,                           19,     undef,undef,"precision",      1,0,2,0,    0,0,    undef,           0,    0,    -5,          undef,10,   undef, ],
-        [ "character",                SQL_CHAR,                     1000000,"'",  "'",  "length",         1,1,3,undef,0,0,    undef,           undef,undef,SQL_CHAR,    undef,undef,undef, ],
-        [ "char",                     SQL_CHAR,                     1000000,"'",  "'",  "length",         1,1,3,undef,0,0,    undef,           undef,undef,SQL_CHAR,    undef,undef,undef, ],
-        [ "numeric",                  SQL_NUMERIC,                  19,     undef,undef,"precision,scale",1,0,2,0,    0,0,    undef,           0,    19,   SQL_NUMERIC, undef,10,   undef, ],
-        [ "decimal",                  SQL_DECIMAL,                  19,     undef,undef,"precision,scale",1,0,2,0,    0,0,    undef,           0,    19,   SQL_DECIMAL, undef,10,   undef, ],
-        [ "integer",                  SQL_INTEGER,                  9,      undef,undef,"precision",      1,0,2,0,    0,0,    undef,           0,    0,    SQL_INTEGER, undef,10,   undef, ],
-        [ "mediumint",                SQL_INTEGER,                  9,      undef,undef,"precision",      1,0,2,0,    0,0,    undef,           0,    0,    SQL_INTEGER, undef,10,   undef, ],
-        [ "smallint",                 SQL_SMALLINT,                 4,      undef,undef,"precision",      1,0,2,0,    0,0,    undef,           0,    0,    SQL_SMALLINT,undef,10,   undef, ],
-        [ "float",                    SQL_FLOAT,                    24,     undef,undef,undef,            1,0,2,0,    0,0,    undef,           0,    0,    SQL_FLOAT,   undef,2,    undef, ],
-        [ "real",                     SQL_REAL,                     24,     undef,undef,undef,            1,0,2,0,    0,0,    undef,           0,    0,    SQL_REAL,    undef,2,    undef, ],
-        [ "double",                   SQL_DOUBLE,                   53,     undef,undef,undef,            1,0,2,0,    0,0,    undef,           0,    0,    SQL_DOUBLE,  undef,2,    undef, ],
-        [ "varchar",                  SQL_VARCHAR,                  1000000,"'",  "'",  "length",         1,1,3,undef,0,undef,undef,           undef,undef,SQL_VARCHAR, undef,undef,undef, ],
-        [ "date",                     SQL_TYPE_DATE,                10,     "'",  "'",  undef,            1,0,2,undef,0,undef,undef,           undef,undef,SQL_DATE,    1,    undef,undef, ],
-        [ "time",                     SQL_TYPE_TIME,                12,     "'",  "'",  undef,            1,0,2,undef,0,undef,undef,           undef,undef,SQL_DATE,    2,    undef,undef, ],
-        [ "timestamp",                SQL_TYPE_TIMESTAMP,           23,     "'",  "'",  "precision",      1,0,2,undef,0,undef,undef,           undef,undef,SQL_DATE,    3,    undef,undef, ],
-        [ "interval year",            SQL_INTERVAL_YEAR,            9,      "'",  "'",  "precision",      1,0,2,undef,0,undef,undef,           0,    0,    SQL_TIME,    1,    undef,9,     ],
-        [ "interval month",           SQL_INTERVAL_MONTH,           10,     "'",  "'",  "precision",      1,0,2,undef,0,undef,"month_interval",0,    0,    SQL_TIME,    2,    undef,10,    ],
-        [ "interval day",             SQL_INTERVAL_DAY,             5,      "'",  "'",  "precision",      1,0,2,undef,0,undef,undef,           0,    0,    SQL_TIME,    3,    undef,5,     ],
-        [ "interval hour",            SQL_INTERVAL_HOUR,            6,      "'",  "'",  "precision",      1,0,2,undef,0,undef,undef,           0,    0,    SQL_TIME,    4,    undef,6,     ],
-        [ "interval minute",          SQL_INTERVAL_MINUTE,          8,      "'",  "'",  "precision",      1,0,2,undef,0,undef,undef,           0,    0,    SQL_TIME,    5,    undef,8,     ],
-        [ "interval second",          SQL_INTERVAL_SECOND,          10,     "'",  "'",  "precision",      1,0,2,undef,0,undef,"sec_interval",  0,    0,    SQL_TIME,    6,    undef,10,    ],
-        [ "interval year to month",   SQL_INTERVAL_YEAR_TO_MONTH,   12,     "'",  "'",  "precision",      1,0,2,undef,0,undef,undef,           0,    0,    SQL_TIME,    7,    undef,9,     ],
-        [ "interval day to hour",     SQL_INTERVAL_DAY_TO_HOUR,     8,      "'",  "'",  "precision",      1,0,2,undef,0,undef,undef,           0,    0,    SQL_TIME,    8,    undef,5,     ],
-        [ "interval day to minute",   SQL_INTERVAL_DAY_TO_MINUTE,   11,     "'",  "'",  "precision",      1,0,2,undef,0,undef,undef,           0,    0,    SQL_TIME,    9,    undef,5,     ],
-        [ "interval day to second",   SQL_INTERVAL_DAY_TO_SECOND,   14,     "'",  "'",  "precision",      1,0,2,undef,0,undef,undef,           0,    0,    SQL_TIME,    10,   undef,5,     ],
-        [ "interval hour to minute",  SQL_INTERVAL_HOUR_TO_MINUTE,  9,      "'",  "'",  "precision",      1,0,2,undef,0,undef,undef,           0,    0,    SQL_TIME,    11,   undef,6,     ],
-        [ "interval hour to second",  SQL_INTERVAL_HOUR_TO_SECOND,  12,     "'",  "'",  "precision",      1,0,2,undef,0,undef,undef,           0,    0,    SQL_TIME,    12,   undef,6,     ],
-        [ "interval minute to second",SQL_INTERVAL_MINUTE_TO_SECOND,13,     "'",  "'",  "precision",      1,0,2,undef,0,undef,undef,           0,    0,    SQL_TIME,    13,   undef,10,    ],
+#   NAME                        TYPE                              SIZE PREFIX SUFFIX  PARAMS            N  C  S      U  F      A  NAME     MIN    MAX  TYPE            SUB  RADIX   IV_P
+  ['char'                     , SQL_CHAR                     , 1000000, "'"  , "'"  ,'length'         , 1, 1, 3, undef, 0,     0, undef, undef, undef, SQL_CHAR    , undef, undef, undef ]
+, ['character'                , SQL_CHAR                     , 1000000, "'"  , "'"  ,'length'         , 1, 1, 3, undef, 0,     0, undef, undef, undef, SQL_CHAR    , undef, undef, undef ]
+, ['decimal'                  , SQL_DECIMAL                  ,      19, undef, undef,'precision,scale', 1, 0, 2,     0, 0,     0, undef,     0,    19, SQL_DECIMAL , undef,    10, undef ]
+, ['dec'                      , SQL_DECIMAL                  ,      19, undef, undef,'precision,scale', 1, 0, 2,     0, 0,     0, undef,     0,    19, SQL_DECIMAL , undef,    10, undef ]
+, ['numeric'                  , SQL_DECIMAL                  ,      19, undef, undef,'precision,scale', 1, 0, 2,     0, 0,     0, undef,     0,    19, SQL_DECIMAL , undef,    10, undef ]
+, ['int'                      , SQL_INTEGER                  ,       9, undef, undef, undef           , 1, 0, 2,     0, 0,     0, undef,     0,     0, SQL_INTEGER , undef,    10, undef ]
+, ['integer'                  , SQL_INTEGER                  ,       9, undef, undef, undef           , 1, 0, 2,     0, 0,     0, undef,     0,     0, SQL_INTEGER , undef,    10, undef ]
+, ['mediumint'                , SQL_INTEGER                  ,       9, undef, undef, undef           , 1, 0, 2,     0, 0,     0, undef,     0,     0, SQL_INTEGER , undef,    10, undef ]
+, ['smallint'                 , SQL_SMALLINT                 ,       4, undef, undef, undef           , 1, 0, 2,     0, 0,     0, undef,     0,     0, SQL_SMALLINT, undef,    10, undef ]
+, ['tinyint'                  , SQL_SMALLINT                 ,       4, undef, undef, undef           , 1, 0, 2,     0, 0,     0, undef,     0,     0, SQL_SMALLINT, undef,    10, undef ]
+, ['float'                    , SQL_FLOAT                    ,      24, undef, undef,'precision,scale', 1, 0, 2,     0, 0,     0, undef,     0,     0, SQL_FLOAT   , undef,     2, undef ]
+, ['real'                     , SQL_REAL                     ,      24, undef, undef, undef           , 1, 0, 2,     0, 0,     0, undef,     0,     0, SQL_REAL    , undef,     2, undef ]
+, ['double'                   , SQL_DOUBLE                   ,      53, undef, undef, undef           , 1, 0, 2,     0, 0,     0, undef,     0,     0, SQL_DOUBLE  , undef,     2, undef ]
+, ['double precision'         , SQL_DOUBLE                   ,      53, undef, undef, undef           , 1, 0, 2,     0, 0,     0, undef,     0,     0, SQL_DOUBLE  , undef,     2, undef ]
+, ['varchar'                  , SQL_VARCHAR                  , 1000000, "'"  , "'"  ,'length'         , 1, 1, 3, undef, 0, undef, undef, undef, undef, SQL_VARCHAR , undef, undef, undef ]
+, ['character varying'        , SQL_VARCHAR                  , 1000000, "'"  , "'"  ,'length'         , 1, 1, 3, undef, 0, undef, undef, undef, undef, SQL_VARCHAR , undef, undef, undef ]
+, ['char varying'             , SQL_VARCHAR                  , 1000000, "'"  , "'"  ,'length'         , 1, 1, 3, undef, 0, undef, undef, undef, undef, SQL_VARCHAR , undef, undef, undef ]
+, ['boolean'                  , SQL_BOOLEAN                  ,       1, undef, undef, undef           , 1, 0, 2,     0, 1,     0, undef, undef, undef, SQL_BOOLEAN , undef, undef, undef ]
+, ['bool'                     , SQL_BOOLEAN                  ,       1, undef, undef, undef           , 1, 0, 2,     0, 1,     0, undef, undef, undef, SQL_BOOLEAN , undef, undef, undef ]
+, ['bigint'                   , 25                           ,      19, undef, undef, undef           , 1, 0, 2,     0, 0,     0, undef,     0,     0, 25          , undef,    10, undef ]
+, ['blob'                     , SQL_BLOB                     , 1000000, "'"  , "'"  ,'length'         , 1, 1, 3, undef, 0, undef, undef, undef, undef, SQL_BLOB    , undef, undef, undef ]
+, ['binary large object'      , SQL_BLOB                     , 1000000, "'"  , "'"  ,'length'         , 1, 1, 3, undef, 0, undef, undef, undef, undef, SQL_BLOB    , undef, undef, undef ]
+, ['clob'                     , SQL_CLOB                     , 1000000, "'"  , "'"  ,'length'         , 1, 1, 3, undef, 0, undef, undef, undef, undef, SQL_CLOB    , undef, undef, undef ]
+, ['character large object'   , SQL_CLOB                     , 1000000, "'"  , "'"  ,'length'         , 1, 1, 3, undef, 0, undef, undef, undef, undef, SQL_CLOB    , undef, undef, undef ]
+, ['char large object'        , SQL_CLOB                     , 1000000, "'"  , "'"  ,'length'         , 1, 1, 3, undef, 0, undef, undef, undef, undef, SQL_CLOB    , undef, undef, undef ]
+, ['string'                   , SQL_CLOB                     , 1000000, "'"  , "'"  ,'length'         , 1, 1, 3, undef, 0, undef, undef, undef, undef, SQL_CLOB    , undef, undef, undef ]
+, ['text'                     , SQL_CLOB                     , 1000000, "'"  , "'"  ,'length'         , 1, 1, 3, undef, 0, undef, undef, undef, undef, SQL_CLOB    , undef, undef, undef ]
+, ['tinytext'                 , SQL_CLOB                     , 1000000, "'"  , "'"  ,'length'         , 1, 1, 3, undef, 0, undef, undef, undef, undef, SQL_CLOB    , undef, undef, undef ]
+, ['date'                     , SQL_TYPE_DATE                ,      10, "'"  , "'"  , undef           , 1, 0, 2, undef, 0, undef, undef, undef, undef, SQL_DATE    ,     1, undef, undef ]
+, ['time'                     , SQL_TYPE_TIME                ,      12, "'"  , "'"  ,'precision'      , 1, 0, 2, undef, 0, undef, undef, undef, undef, SQL_DATE    ,     2, undef, undef ]
+, ['timestamp'                , SQL_TYPE_TIMESTAMP           ,      23, "'"  , "'"  ,'precision'      , 1, 0, 2, undef, 0, undef, undef, undef, undef, SQL_DATE    ,     3, undef, undef ]
+, ['interval year'            , SQL_INTERVAL_YEAR            ,       9, "'"  , "'"  ,'precision'      , 1, 0, 2, undef, 0, undef, undef,     0,     0, SQL_TIME    ,     1, undef,     9 ]
+, ['interval month'           , SQL_INTERVAL_MONTH           ,      10, "'"  , "'"  ,'precision'      , 1, 0, 2, undef, 0, undef, undef,     0,     0, SQL_TIME    ,     2, undef,    10 ]
+, ['month_interval'           , SQL_INTERVAL_MONTH           ,      10, "'"  , "'"  ,'precision'      , 1, 0, 2, undef, 0, undef, undef,     0,     0, SQL_TIME    ,     2, undef,    10 ]
+, ['interval day'             , SQL_INTERVAL_DAY             ,       5, "'"  , "'"  ,'precision'      , 1, 0, 2, undef, 0, undef, undef,     0,     0, SQL_TIME    ,     3, undef,     5 ]
+, ['interval hour'            , SQL_INTERVAL_HOUR            ,       6, "'"  , "'"  ,'precision'      , 1, 0, 2, undef, 0, undef, undef,     0,     0, SQL_TIME    ,     4, undef,     6 ]
+, ['interval minute'          , SQL_INTERVAL_MINUTE          ,       8, "'"  , "'"  ,'precision'      , 1, 0, 2, undef, 0, undef, undef,     0,     0, SQL_TIME    ,     5, undef,     8 ]
+, ['interval second'          , SQL_INTERVAL_SECOND          ,      10, "'"  , "'"  ,'precision'      , 1, 0, 2, undef, 0, undef, undef,     0,     0, SQL_TIME    ,     6, undef,    10 ]
+, ['sec_interval'             , SQL_INTERVAL_SECOND          ,      10, "'"  , "'"  ,'precision'      , 1, 0, 2, undef, 0, undef, undef,     0,     0, SQL_TIME    ,     6, undef,    10 ]
+, ['interval year to month'   , SQL_INTERVAL_YEAR_TO_MONTH   ,      12, "'"  , "'"  , undef           , 1, 0, 2, undef, 0, undef, undef,     0,     0, SQL_TIME    ,     7, undef,     9 ]
+, ['interval day to hour'     , SQL_INTERVAL_DAY_TO_HOUR     ,       8, "'"  , "'"  , undef           , 1, 0, 2, undef, 0, undef, undef,     0,     0, SQL_TIME    ,     8, undef,     5 ]
+, ['interval day to minute'   , SQL_INTERVAL_DAY_TO_MINUTE   ,      11, "'"  , "'"  , undef           , 1, 0, 2, undef, 0, undef, undef,     0,     0, SQL_TIME    ,     9, undef,     5 ]
+, ['interval day to second'   , SQL_INTERVAL_DAY_TO_SECOND   ,      14, "'"  , "'"  ,'precision'      , 1, 0, 2, undef, 0, undef, undef,     0,     0, SQL_TIME    ,    10, undef,     5 ]
+, ['interval hour to minute'  , SQL_INTERVAL_HOUR_TO_MINUTE  ,       9, "'"  , "'"  , undef           , 1, 0, 2, undef, 0, undef, undef,     0,     0, SQL_TIME    ,    11, undef,     6 ]
+, ['interval hour to second'  , SQL_INTERVAL_HOUR_TO_SECOND  ,      12, "'"  , "'"  ,'precision'      , 1, 0, 2, undef, 0, undef, undef,     0,     0, SQL_TIME    ,    12, undef,     6 ]
+, ['interval minute to second', SQL_INTERVAL_MINUTE_TO_SECOND,      13, "'"  , "'"  ,'precision'      , 1, 0, 2, undef, 0, undef, undef,     0,     0, SQL_TIME    ,    13, undef,    10 ]
 );
 
-$type_info_all = [ \%index, @data ];
 sub type_info_all { [ \%index, @data ] }
 
 1;
