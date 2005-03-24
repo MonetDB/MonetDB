6d698e30b0606094dd9096036c42a7d6d02997a8
Sjoerd Mullender <sjoerd@acm.org>
2005-03-24 15:01:09 +0000

    When installed "Just for you", put the database in the user's own Application Data folder.

--- MonetDB/NT/Mserver.bat
+++ MonetDB/NT/Mserver.bat
@@ -1,5 +1,7 @@
 @echo off
 
+setlocal
+
 rem figure out the folder name
 set MONETDB=%~dp0
 
@@ -14,21 +16,36 @@ rem possibly move the database from a previous installation to our
 rem currently preferred location, and prepare the arguments to Mserver
 rem to tell it where that location is
 
+set MONETDBDIR=
 set MONETDBFARM=
 set SQLLOGDIR=
 rem use the Application Data folder for our database
+
+rem if installed for just the current user, the file
+rem %APPDATA%\MonetDB\VERSION was created by the installer, so set
+rem MONETDBDIR accordingly.
+rem if ALLUSERSPROFILE and APPDATA variables don't exist, forget about
+rem this whole exercise and use the default (i.e. %MONETDB\var\MonetDB).
+
 if "%ALLUSERSPROFILE%" == "" goto skip
-set MONETDBFARM="--dbfarm=%ALLUSERSPROFILE%\Application Data\MonetDB\dbfarm"
-set SQLLOGDIR=--set "sql_logdir=%ALLUSERSPROFILE%\Application Data\MonetDB\log"
+if "%APPDATA%" == "" goto skip
+
+if exist "%APPDATA%\MonetDB\VERSION" set MONETDBDIR=%APPDATA%\MonetDB
+if "%MONETDBDIR%" == "" set MONETDBDIR=%ALLUSERSPROFILE%\Application Data\MonetDB
+
+set MONETDBFARM="--dbfarm=%MONETDBDIR%\dbfarm"
+set SQLLOGDIR=--set "sql_logdir=%MONETDBDIR%\log"
 
 rem if the database exists by the old name, move it
 if not exist "%MONETDB%\var\MonetDB" goto skip
 rem if the new path already exists, don't try moving the old to the new
-if exist "%ALLUSERSPROFILE%\Application Data\MonetDB" goto skip
-move "%MONETDB%\var\MonetDB" "%ALLUSERSPROFILE%\Application Data\MonetDB"
+if exist "%MONETDBDIR%" goto skip
+move "%MONETDB%\var\MonetDB" "%MONETDBDIR%"
 rmdir "%MONETDB%\var"
 
 :skip
 
 rem start the real server
 "%MONETDB%\bin\Mserver.exe" --set "prefix=%MONETDB%" --set "exec_prefix=%MONETDB%" %MONETDBFARM% %SQLLOGDIR% %*
+
+endlocal
