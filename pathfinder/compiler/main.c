42d9ce502fba69a99cea64edca8c760edb5cd272
Jan Flokstra <flokstra@cs.utwente.nl>
2005-03-24 08:46:57 +0000

    * Repair compilation errors on the (very strict) SuSe9.2 compilers
    compiler/main.c: hide the use of setjmp() in a hidden_setjmp() function because
    		 compiler complains about hidden variables.
    
    runtime/pathfinder.mx: (void) the xquery parameter because it is unused.

--- pathfinder/compiler/main.c
+++ pathfinder/compiler/main.c
@@ -492,6 +492,13 @@ static char *phases[] = {
  */
 static char *progname = 0;
 
+/* This function is created to hide the use of setjmp() for the compiler.
+ * If setjmp() is used directly it starts to complain about clobbered variables.
+ */
+static int hidden_setjmp(jmp_buf env) {
+	return setjmp(env);
+}
+
 /**
  * Entry point to the Pathfinder compiler,
  * parses the command line (switches), then invokes the compiler driver
@@ -686,7 +693,7 @@ main (int argc, char *argv[])
         }
     }
 
-    if (setjmp(PFexitPoint) != 0 ) {
+    if (hidden_setjmp(PFexitPoint) != 0 ) {
         fputs(PFerrbuf, stderr);
         goto failure;
     }
