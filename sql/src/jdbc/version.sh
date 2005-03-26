96dbf9fbd0aafdc892f351f427bb8c1872308ef2
Fabian Groffen <fabian@cwi.nl>
2005-03-26 08:10:11 +0000

    Make sure +, * and . are escaped in sed's input

--- sql/src/jdbc/version.sh
+++ sql/src/jdbc/version.sh
@@ -17,6 +17,10 @@ NEW_MAJOR=${CUR_MAJOR}
 NEW_MINOR=${CUR_MINOR}
 NEW_SUFFIX=${CUR_SUFFIX}
 
+ESC_MAJOR=`echo "$CUR_MAJOR" | sed -re 's/\+/\\\\+/g' | sed -re 's/\*/\\\\*/g' | sed -re 's/\./\\\\./g'`
+ESC_MINOR=`echo "$CUR_MINOR" | sed -re 's/\+/\\\\+/g' | sed -re 's/\*/\\\\*/g' | sed -re 's/\./\\\\./g'`
+ESC_SUFFIX=`echo "$CUR_SUFFIX" | sed -re 's/\+/\\\\+/g' | sed -re 's/\*/\\\\*/g' | sed -re 's/\./\\\\./g'`
+
 patch="cat"
 
 for param in $*;
@@ -57,27 +61,27 @@ diff="diff -Naur"
 
 file="release.txt"
 sed -r \
-	-e "s/version ${CUR_MAJOR}\.${CUR_MINOR} \(${CUR_SUFFIX}\)/version ${NEW_MAJOR}.${CUR_MINOR} \(${NEW_SUFFIX}\)/g" \
+	-e "s/version ${ESC_MAJOR}\.${ESC_MINOR} \(${ESC_SUFFIX}\)/version ${NEW_MAJOR}.${NEW_MINOR} \(${NEW_SUFFIX}\)/g" \
 	-e "s/Release date: 20[0-9][0-9]-[01][0-9]-[0-3][0-9]/Release date: `date +%F`/" \
 	${file} | ${diff} ${file} - | ${patch}
 
 file="../../NT/configure.py"
 sed -r \
-	-e "s/'@JDBC_MAJOR@',        '${CUR_MAJOR}'/'@JDBC_MAJOR@',        '${NEW_MAJOR}'/g" \
-	-e "s/'@JDBC_MINOR@',        '${CUR_MINOR}'/'@JDBC_MINOR@',        '${NEW_MINOR}'/g" \
-	-e "s/'@JDBC_VER_SUFFIX@',   '${CUR_SUFFIX}'/'@JDBC_VER_SUFFIX@',   '${NEW_SUFFIX}'/g" \
+	-e "s/'@JDBC_MAJOR@',        '${ESC_MAJOR}'/'@JDBC_MAJOR@',        '${NEW_MAJOR}'/g" \
+	-e "s/'@JDBC_MINOR@',        '${ESC_MINOR}'/'@JDBC_MINOR@',        '${NEW_MINOR}'/g" \
+	-e "s/'@JDBC_VER_SUFFIX@',   '${ESC_SUFFIX}'/'@JDBC_VER_SUFFIX@',   '${NEW_SUFFIX}'/g" \
 	${file} | ${diff} ${file} - | ${patch}
 
 file="../../configure.ag"
 sed -r \
-	-e "s/JDBC_MAJOR=${CUR_MAJOR}/JDBC_MAJOR=${NEW_MAJOR}/g" \
-	-e "s/JDBC_MINOR=${CUR_MINOR}/JDBC_MINOR=${NEW_MINOR}/g" \
-	-e "s/JDBC_VER_SUFFIX=\"${CUR_SUFFIX}\"/JDBC_VER_SUFFIX=\"${NEW_SUFFIX}\"/g" \
+	-e "s/JDBC_MAJOR=${ESC_MAJOR}/JDBC_MAJOR=${NEW_MAJOR}/g" \
+	-e "s/JDBC_MINOR=${ESC_MINOR}/JDBC_MINOR=${NEW_MINOR}/g" \
+	-e "s/JDBC_VER_SUFFIX=\"${ESC_SUFFIX}\"/JDBC_VER_SUFFIX=\"${NEW_SUFFIX}\"/g" \
 	${file} | ${diff} ${file} - | ${patch}
 
 file="build.properties"
 sed -r \
-	-e "s/JDBC_MAJOR=${CUR_MAJOR}/JDBC_MAJOR=${NEW_MAJOR}/g" \
-	-e "s/JDBC_MINOR=${CUR_MINOR}/JDBC_MINOR=${NEW_MINOR}/g" \
-	-e "s/JDBC_VER_SUFFIX=${CUR_SUFFIX}/JDBC_VER_SUFFIX=${NEW_SUFFIX}/g" \
+	-e "s/JDBC_MAJOR=${ESC_MAJOR}/JDBC_MAJOR=${NEW_MAJOR}/g" \
+	-e "s/JDBC_MINOR=${ESC_MINOR}/JDBC_MINOR=${NEW_MINOR}/g" \
+	-e "s/JDBC_VER_SUFFIX=${ESC_SUFFIX}/JDBC_VER_SUFFIX=${NEW_SUFFIX}/g" \
 	${file} | ${diff} ${file} - | ${patch}
