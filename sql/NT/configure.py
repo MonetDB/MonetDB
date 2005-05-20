666779d68ef205a5417feb9387eeeb72a4d1328f
Sjoerd Mullender <sjoerd@acm.org>
2005-05-20 15:25:34 +0000

    Added missing @...@ expansions; made configure.py more like MonetDB version; fixed Windows test script.

--- sql/NT/configure.py
+++ sql/NT/configure.py
@@ -20,65 +20,61 @@
 import sys
 import fileinput
 import os
-import string
 
 build=os.path.abspath(sys.argv[1]);
 prefix=os.path.abspath(sys.argv[2]);
 
 source=os.path.abspath(os.path.join(build,os.pardir))
 
-# double back slashes
-Qprefix = string.replace(prefix, '\\', '\\\\')
-Qbuild  = string.replace(build,  '\\', '\\\\')
-Qsource = string.replace(source, '\\', '\\\\')
-
 subs = [
-    ('@exec_prefix@',       "@prefix@"),
-    ('@sysconfdir@',        "@prefix@@DIRSEP@etc"),
-    ('@localstatedir@',     "@prefix@@DIRSEP@var"),
-    ('@libdir@',            "@prefix@@DIRSEP@lib"),
-    ('@bindir@',            "@prefix@@DIRSEP@bin"),
-    ('@mandir@',            "@prefix@@DIRSEP@man"),
-    ('@includedir@',        "@prefix@@DIRSEP@include"),
-    ('@datadir@',           "@prefix@@DIRSEP@share"),
-    ('@infodir@',           "@prefix@@DIRSEP@info"),
-    ('@libexecdir@',        "@prefix@@DIRSEP@libexec"),
-    ('@Qexec_prefix@',      "@Qprefix@"),
-    ('@Qsysconfdir@',       "@Qprefix@@QDIRSEP@etc"),
-    ('@Qlocalstatedir@',    "@Qprefix@@QDIRSEP@var"),
-    ('@Qlibdir@',           "@Qprefix@@QDIRSEP@lib"),
-    ('@Qbindir@',           "@Qprefix@@QDIRSEP@bin"),
-    ('@Qmandir@',           "@Qprefix@@QDIRSEP@man"),
-    ('@Qincludedir@',       "@Qprefix@@QDIRSEP@include"),
-    ('@Qdatadir@',          "@Qprefix@@QDIRSEP@share"),
-    ('@Qinfodir@',          "@Qprefix@@QDIRSEP@info"),
-    ('@Qlibexecdir@',       "@Qprefix@@QDIRSEP@libexec"),
-    ('@PACKAGE@',           "sql"),
-    ('@VERSION@',           "2.7.3"),
-    ('@DIRSEP@',            "\\"),
-    ('@prefix@',            prefix),
-    ('@MONETDB_BUILD@',     build),
-    ('@MONETDB_SOURCE@',    source),
-    ('@MONETDB_PREFIX@',    os.getenv('MONETDB_PREFIX')),
-    ('@QDIRSEP@',           "\\\\"),
-    ('@Qprefix@',           Qprefix),
-    ('@QMONETDB_BUILD@',    Qbuild),
-    ('@QMONETDB_SOURCE@',   Qsource),
-    ('@SQL_BACKEND@',       "monet4"),
-    ('@SQL_BUILD@',         os.getenv("SQL_BUILD")),
+    ('@exec_prefix@',            prefix),
+    ('@Xexec_prefix@',           prefix),
+    ('@sysconfdir@',             r'${prefix}\etc'),
+    ('@Xsysconfdir@',            r'${prefix}\etc'),
+    ('@localstatedir@',          r'${prefix}\var'),
+    ('@Xlocalstatedir@',         r'${prefix}\var'),
+    ('@libdir@',                 r'${prefix}\lib'),
+    ('@Xlibdir@',                r'${prefix}\lib'),
+    ('@bindir@',                 r'${prefix}\bin'),
+    ('@Xbindir@',                r'${prefix}\bin'),
+    ('@mandir@',                 r'${prefix}\man'),
+    ('@Xmandir@',                r'${prefix}\man'),
+    ('@includedir@',             r'${prefix}\include'),
+    ('@Xincludedir@',            r'${prefix}\include'),
+    ('@datadir@',                r'${prefix}\share'),
+    ('@Xdatadir@',               r'${prefix}\share'),
+    ('@infodir@',                r'${prefix}\info'),
+    ('@Xinfodir@',               r'${prefix}\info'),
+    ('@libexecdir@',             r'${prefix}\libexec'),
+    ('@Xlibexecdir@',            r'${prefix}\libexec'),
+    ('@PACKAGE@',                r'sql'),
+    ('@VERSION@',                r'2.7.3'),
+    ('@DIRSEP@',                 '\\'),
+    ('@prefix@',                 prefix),
+    ('@Xprefix@',                prefix),
+    ('@MONETDB_BUILD@',          build),
+    ('@XMONETDB_BUILD@',         build),
+    ('@MONETDB_SOURCE@',         source),
+    ('@XMONETDB_SOURCE@',        source),
+    ('@MONETDB_PREFIX@',         os.getenv('MONETDB_PREFIX')),
+    ('@SQL_BACKEND@',            r'monet4'),
+    ('@SQL_BUILD@',              os.getenv('SQL_BUILD')),
 # values needed for the JDBC driver;
 # keep in sync with src/jdbc/build.properties and configure.ag !
-    ('@JDBC_MAJOR@',        '1'),
-    ('@JDBC_MINOR@',        '1'),
-    ('@JDBC_VER_SUFFIX@',   'Rise-SR1'),
-    ('@JDBC_DEF_PORT@',     '45123'),
-    ('@JDBC_DEF_BLOCKMODE@','true'),
+    ('@JDBC_MAJOR@',             '1'),
+    ('@JDBC_MINOR@',             '1'),
+    ('@JDBC_VER_SUFFIX@',        'Rise-SR1'),
+    ('@JDBC_DEF_PORT@',          '45123'),
+    ('@JDBC_DEF_BLOCKMODE@',     'true'),
 ]
+
+for key, val in subs[:]:
+    subs.insert(0, ('@Q'+key[1:], val.replace('\\', r'\\')))
 
 
 def substitute(line):
     for (p,v) in subs:
-        line = string.replace(line, p, v);
+        line = line.replace(p, v);
     return line
 
 for line in fileinput.input(sys.argv[3:]):
