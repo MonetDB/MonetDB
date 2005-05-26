fdaa8f12e297b6138f58d6ca488cd3c169392aec
Sjoerd Mullender <sjoerd@acm.org>
2005-05-26 15:31:13 +0000

    Windows installer plus supporting files. The installer is currently very specific to my private setup.

--- pathfinder/NT/MPFclient.bat
+++ pathfinder/NT/MPFclient.bat
@@ -4,5 +4,14 @@
 @rem remove the final backslash from the path
 @set MONETDB=%MONETDB:~0,-1%
 
+@rem extend the search path with our EXE and DLL folders
+@rem we depend on pthreadVCE.dll having been copied to the lib folder
+@set PATH=%MONETDB%\bin;%MONETDB%\lib;%PATH%
+
 @rem start the real client
-@"%MONETDB%\MapiClient.bat --language=xml" %*
+set XMLFILE=%TEMP%\MonetDB-XQUery-%RANDOM%.xml
+@"%MONETDB%\bin\MapiClient.exe"  --set "prefix=%MONETDB%" --set "exec_prefix=%MONETDB%" -lxquery -sxml %* > "%XMLFILE%
+
+"%XMLFILE%
+
+del "%XMLFILE%"
