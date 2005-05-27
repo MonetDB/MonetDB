9be5bd5370d7c073c02dc0bc37d52fd59423113a
Stefan Manegold <manegold@cwi.nl>
2005-05-27 23:27:10 +0000

    propagated changes of Friday May 27 2005 from the XQuery_0-8 release branch to the development trunk
    Entreaty:
    =========
    If you by accident check in to the development trunk and then notice that
    these changes should actually have gone to the release branch (or vice
    verse), and then (try to) fix it by propagating your changes to the release
    branch (or development trunk),
    PLEASE use only cvs commands to do this!
    In case you don't know how, please feel free to ask me!
    
    PLEASE do NOT do not (try to) propagate changes "by hand".
    You WILL use slightly different whitespacing, which is no problem for the
    compiler, but diff & patch do not like this, making my automa[tgn]ical
    propagation from the release branch to the development trunk IMPOSSIBLE.
    I then have to check all changes ONE BY ONE, to solve all the conflicts |-(
    
    I just had to do this for runtime/serialize.mx; no only did it cost me quite
    some time, there is also no "guarantee", that I did fix all conflicts
    correctly...
    
    Thank you very much in advance for your cooperation!
    
    Stefan

--- pathfinder/NT/MPFclient.bat
+++ pathfinder/NT/MPFclient.bat
@@ -1,17 +1,23 @@
-@rem figure out the folder name
-@set MONETDB=%~dp0
+@echo off
 
-@rem remove the final backslash from the path
-@set MONETDB=%MONETDB:~0,-1%
+rem figure out the folder name
+set MONETDB=%~dp0
 
-@rem extend the search path with our EXE and DLL folders
-@rem we depend on pthreadVCE.dll having been copied to the lib folder
-@set PATH=%MONETDB%\bin;%MONETDB%\lib;%PATH%
+rem remove the final backslash from the path
+set MONETDB=%MONETDB:~0,-1%
 
-@rem start the real client
-set XMLFILE=%TEMP%\MonetDB-XQUery-%RANDOM%.xml
-@"%MONETDB%\bin\MapiClient.exe"  --set "prefix=%MONETDB%" --set "exec_prefix=%MONETDB%" -lxquery -sxml %* > "%XMLFILE%
+rem extend the search path with our EXE and DLL folders
+rem we depend on pthreadVCE.dll having been copied to the lib folder
+set PATH=%MONETDB%\bin;%MONETDB%\lib;%PATH%
 
-"%XMLFILE%
+rem start the real client
+set XMLFILE=%TEMP%\MonetDB-XQuery-%RANDOM%.xml
+"%MONETDB%\bin\MapiClient.exe"  --set "prefix=%MONETDB%" --set "exec_prefix=%MONETDB%" -lxquery -sxml %* > "%XMLFILE%"
+
+"%XMLFILE%"
+
+rem add a short sleep to make sure that there was time to open the
+rem file before we delete it
+sleep 10
 
 del "%XMLFILE%"
--- pathfinder/tests/BugsViaSourgeforce/Tests/ID.1209497.xq
+++ pathfinder/tests/BugsViaSourgeforce/Tests/ID.1209497.xq
@@ -1,6 +1,11 @@
 <a>
 &lt;
-&gt;
+&gt; >
 &amp;
-&quot;
+&quot; "
+&apos; '
+" &lt; &gt; > &amp; &quot; " &apos; ' "
+' &lt; &gt; > &amp; &quot; " &apos; ' '
+<tag attr1=' &lt; &gt; > &amp; &quot; "      &apos; &apos; ' 
+     attr2=" &lt; &gt; > &amp; &quot; &quot; &apos; '      "/>
 </a>
