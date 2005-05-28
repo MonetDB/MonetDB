99d66653d02752582ee6ce44a91582615343d6b1
cvs2hg <unknown>
2005-05-28 18:00:56 +0000

    fixup commit for tag 'MonetDB_4-9-0_1'

--- pathfinder/compiler/core/tests/fs/Tests/good-079.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-@prompt # $t $g
-
-@set NAME=%1
-
-pf -s9 < %NAME%.xq
-@echo %ERRORLEVEL%
--- sql/src/jdbc/tests/Tests/Test_Csavepoints.SQL.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-set CLASSPATH=%CLASSPATH%;%SQL_BUILD%/src/jdbc/MonetDB_JDBC.jar;%TSTBLDBASE%/src/jdbc/MonetDB_JDBC.jar;%SQL_PREFIX%/share/MonetDB/lib/MonetDB_JDBC.jar;%MONETDB_PREFIX%/share/MonetDB/lib/MonetDB_JDBC.jar;%TSTTRGBASE%/share/MonetDB/lib/MonetDB_JDBC.jar;%SQL_BUILD%/src/jdbc/tests;%TSTBLDBASE%/src/jdbc/tests;%SQL_PREFIX%/share/sql/Tests;%MONETDB_PREFIX%/share/sql/Tests;%TSTTRGBASE%/share/sql/Tests
-
-call Mlog.bat -x java "%TST%" "jdbc:monetdb://%HOST%:%SQLPORT%/database?user=monetdb&password=monetdb"
