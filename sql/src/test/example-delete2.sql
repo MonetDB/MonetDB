e9facaca5f0a67ba4d83450de43a524b02488b42
cvs2hg <unknown>
2005-05-28 21:58:34 +0000

    fixup commit for tag 'XQuery_0-9-0_'

--- sql/src/test/example-delete2.sql
+++
@@ -1,9 +0,0 @@
-select * from example;
-delete from example where val1 = 5;
-select * from example;
-delete from example where val1 = 9;
-select * from example;
-delete from example where val1 = 8;
-select * from example;
-
-commit;
