b1160997aa9cd56a0b84a9fc62d2ebbabef0e86b
Stefan Manegold <manegold@cwi.nl>
2005-05-29 19:19:09 +0000

    module "tpcd" has long gone ...

--- MonetDB/doc/mkdoc.py
+++ MonetDB/doc/mkdoc.py
@@ -166,7 +166,7 @@ for f in ['aggrX3', 'aggr', 'alarm', 'algebra', 'arith', 'ascii_io', 'bat',
     runMx(os.path.join(srcdir, 'src', 'modules', 'plain'), f,
           os.path.join(dstdir, 'doc', 'MonetDB', 'TechDocs', 'Core', 'Modules'))
 
-for f in ['bitset', 'bitvector', 'ddbench', 'mel', 'mprof', 'oo7', 'qt', 'tpcd',
+for f in ['bitset', 'bitvector', 'ddbench', 'mel', 'mprof', 'oo7', 'qt',
           'wisc']:
     runMx(os.path.join(srcdir, 'src', 'modules', 'contrib'), f,
           os.path.join(dstdir, 'doc', 'MonetDB', 'TechDocs', 'Core', 'Modules'))
