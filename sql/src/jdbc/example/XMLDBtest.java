1ad6256eca547abd61e1c9e3177d0643dc7b2a78
Fabian Groffen <fabian@cwi.nl>
2005-06-14 09:37:05 +0000

    Tell a bit about how to run and don't use absolute names when it's not necessary (it's in the imports)

--- sql/src/jdbc/example/XMLDBtest.java
+++ sql/src/jdbc/example/XMLDBtest.java
@@ -5,13 +5,16 @@ import org.xmldb.api.modules.*;
 /**
  * Quick example demonstrating the XML:DB driver.
  *
+ * To compile and run this example, MonetDB_XMLDB.jar, MonetDB_JDBC.jar
+ * and xmldb.jar (from the lib dir) should be in the classpath.
+ *
  * @author Fabian Groffen <Fabian.Groffen@cwi.nl>
  */
 public class XMLDBtest {
 	public static void main(String[] args) throws Exception {
 		Class.forName("nl.cwi.monetdb.xmldb.base.MonetDBDatabase");
 		try {
-			Collection col = org.xmldb.api.DatabaseManager.getCollection("xmldb:monetdb://localhost/demo", "monetdb", "monetdb"); 
+			Collection col = DatabaseManager.getCollection("xmldb:monetdb://localhost/demo", "monetdb", "monetdb"); 
 
 			XQueryService xqs = (XQueryService)col.getService("XQueryService", "1");
 			ResourceSet set = xqs.query("(<foo>1</foo>,<bar />)");
