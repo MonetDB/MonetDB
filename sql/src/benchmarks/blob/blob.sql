5eecf9858001606067337369dc461e57d18804ae
cvs2hg <unknown>
2005-06-24 08:18:33 +0000

    fixup commit for tag 'May2005_demo_root'

--- sql/src/benchmarks/blob/blob.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO adttest (id, dosname, filename, contenttype, blobsize, blobdata) VALUES (1, 'WXKZAYWFEPY', 'DECRYZWIEHLQNUAQPLFXHZLMHRDR', 'video/mpeg', 10, BLOB "(10: 41 41 80 41 41 80 41 41 80 41)");
-select * from adttest;
