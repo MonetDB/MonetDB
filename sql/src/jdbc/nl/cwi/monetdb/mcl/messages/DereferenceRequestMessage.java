9f7f28f031d5f5a2a75820a4454b4b2422614ee4
Fabian Groffen <fabian@cwi.nl>
2005-07-09 14:22:45 +0000

    Adding DereferenceRequestMessage implementation

--- sql/src/jdbc/nl/cwi/monetdb/mcl/messages/DereferenceRequestMessage.java
+++ sql/src/jdbc/nl/cwi/monetdb/mcl/messages/DereferenceRequestMessage.java
@@ -4,14 +4,14 @@ import java.util.*;
 import nl.cwi.monetdb.mcl.*;
 
 /**
- * An ExportRequestMessage is a server received message, sent by the
- * client in order to retrieve a (part of a) result set.
+ * An DereferenceRequestMessage is a server received message, sent by the
+ * client in order to retrieve a reference from a result set.
  *
  * @author Fabian Groffen <Fabian.Groffen>
  */
-public class ExportRequestMessage extends MCLMessage {
+public class DereferenceRequestMessage extends MCLMessage {
 	/** The character that identifies this message */
-	public static final char identifier = 'e';
+	public static final char identifier = 'r';
 
 	private final static MCLSentence startOfMessageSentence;
 	
@@ -25,40 +25,33 @@ public class ExportRequestMessage extends MCLMessage {
 
 	// these represent the internal values of this Message
 	private String id;
-	private int offset;
-	private int length;
 	
 	/**
-	 * Constructs an empty ExportRequestMessage.  The sentences need to
-	 * be added using the addSentence() method.  This constructor is
-	 * suitable when reconstructing messages from a stream.
+	 * Constructs an empty DereferenceRequestMessage.  The sentences
+	 * need to be added using the addSentence() method.  This
+	 * constructor is suitable when reconstructing messages from a
+	 * stream.
 	 */
-	public ExportRequestMessage() {
+	public DereferenceRequestMessage() {
 		// nothing has to be done here
-		sentences = new MCLSentence[3];
+		sentences = new MCLSentence[1];
 	}
 
 	/**
-	 * Constructs a filled ExportRequestMessage.  All required
-	 * information is supplied and stored in this ExportRequestMessage.
+	 * Constructs a filled DereferenceRequestMessage.  All required
+	 * information is supplied and stored in this
+	 * DreferenceRequestMessage.
 	 *
-	 * @param id the server side result id
-	 * @param offset the index position to start from, inclusive
-	 * starting from zero
-	 * @param length the maximum number of tuples to receive
+	 * @param id the server side reference id
 	 * @throws MCLException if the id string is null
 	 */
-	public ExportRequestMessage(String id, int offset, int length) throws MCLException {
+	public DereferenceRequestMessage(String id) throws MCLException {
 		if (id == null) throw
-			new MCLException("Result set id may not be null");
+			new MCLException("Reference id may not be null");
 		
-		sentences = new MCLSentence[3];
+		sentences = new MCLSentence[1];
 		this.id = id;
-		this.offset = offset;
-		this.length = length;
 		sentences[0] = new MCLSentence('$', "id", id);
-		sentences[1] = new MCLSentence('$', "offset", "" + offset);
-		sentences[2] = new MCLSentence('$', "length", "" + length);
 	}
 
 	/**
@@ -104,24 +97,6 @@ public class ExportRequestMessage extends MCLMessage {
 
 		if (prop.equals("id")) {
 			sentences[0] = in;
-		} else if (prop.equals("offset")) {
-			try {
-				offset = Integer.parseInt(value);
-				if (offset < 0) throw
-					new MCLException("Offset cannot be less than zero (" + offset + ")");
-				sentences[1] = in;
-			} catch (NumberFormatException e) {
-				throw new MCLException("Illegal value for header 'offset': " + value);
-			}
-		} else if (prop.equals("length")) {
-			try {
-				length = Integer.parseInt(value);
-				if (length < 0) throw
-					new MCLException("Length cannot be less than zero (" + length + ")");
-				sentences[2] = in;
-			} catch (NumberFormatException e) {
-				throw new MCLException("Illegal value for header 'length': " + value);
-			}
 		} else {
 			throw new MCLException("Illegal property '" + prop + "' for this Message");
 		}
@@ -132,29 +107,11 @@ public class ExportRequestMessage extends MCLMessage {
 	// values inside the message
 
 	/**
-	 * Retrieves the result set id contained in this Message object.
+	 * Retrieves the reference id contained in this Message object.
 	 *
 	 * @return the result set id
 	 */
 	public String getId() {
 		return(id);
-	}
-
-	/**
-	 * Retrieves the offset contained in this Message object.
-	 *
-	 * @return the offset
-	 */
-	public int getOffset() {
-		return(offset);
-	}
-
-	/**
-	 * Retrieves the length contained in this Message object.
-	 *
-	 * @return the length
-	 */
-	public int getLength() {
-		return(length);
 	}
 }
