3add2c4d921f216108e1729d315c2c394f3fa825
Stefan Manegold <manegold@cwi.nl>
2005-07-26 21:35:06 +0000

    this will become the bug-fix release MonetDB 4.8.2

--- MonetDB/NT/winconfig.h
+++ MonetDB/NT/winconfig.h
@@ -667,7 +667,7 @@
 /* #undef TM_IN_SYS_TIME */
 
 /* Version number of package */
-#define VERSION "4.8.0"
+#define VERSION "4.8.2"
 
 /* Define on MS Windows (also under Cygwin) */
 #ifndef WIN32
@@ -1314,7 +1314,7 @@
 /* #undef TM_IN_SYS_TIME */
 
 /* Version number of package */
-#define VERSION "4.8.0"
+#define VERSION "4.8.2"
 
 /* Define on MS Windows (also under Cygwin) */
 /* #undef WIN32 */
