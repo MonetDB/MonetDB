bdd8f3da791a52ea93ab912dd6f51f1e2ee69e67
cvs2hg <unknown>
2005-07-30 23:14:06 +0000

    fixup commit for tag 'XQuery_0-9-2'

--- sql/src/benchmarks/arno/load.int.sql
+++
@@ -1,4 +0,0 @@
-COPY INTO atom  from 'PWD/ATOM.int.tbl'  USING DELIMITERS ',','\n';
-COPY INTO bond  from 'PWD/BOND.int.tbl'  USING DELIMITERS ',','\n';
-COPY INTO model from 'PWD/MODEL.int.tbl' USING DELIMITERS ',','\n';
-commit;
