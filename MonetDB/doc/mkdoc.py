7a6d2710f464a8b537250d206de6c6d4d36971ba
Sjoerd Mullender <sjoerd@acm.org>
2005-08-10 09:16:58 +0000

    Generate pcre documentation.

--- MonetDB/doc/mkdoc.py
+++ MonetDB/doc/mkdoc.py
@@ -161,7 +161,7 @@ runMx(os.path.join(srcdir, 'src', 'modules', 'calibrator'), 'calib',
 
 for f in ['aggrX3', 'aggr', 'alarm', 'algebra', 'arith', 'ascii_io', 'bat',
           'blob', 'counters', 'decimal', 'enum', 'builtin',
-          'lock', 'mmath', 'monettime', 'pcl', 'radix', 'streams', 'str', 'sys',
+          'lock', 'mmath', 'monettime', 'pcl', 'pcre', 'radix', 'streams', 'str', 'sys',
           'tcpip', 'trans', 'unix', 'upgrade', 'url', 'xtables']:
     runMx(os.path.join(srcdir, 'src', 'modules', 'plain'), f,
           os.path.join(dstdir, 'doc', 'MonetDB', 'TechDocs', 'Core', 'Modules'))
