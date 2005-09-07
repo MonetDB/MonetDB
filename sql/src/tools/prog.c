4f46a46060b0727dc9b04a6edebdd874979d0838
Niels Nes <niels@cwi.nl>
2005-09-07 20:20:16 +0000

    allways build Mbeddedsql added options (dbname,dbfarm,set,time) to Mbeddedsql pass the options to embedded_sql

--- sql/src/tools/prog.c
+++ sql/src/tools/prog.c
@@ -16,19 +16,134 @@
  * All Rights Reserved.
  */
 
+#include <monet_options.h>
 #include "embeddedclient.h"
 
-int
-main(int argc, char **argv)
+/* stolen piece */
+#ifdef HAVE_FTIME
+#include <sys/timeb.h>
+#endif
+
+#if TIME_WITH_SYS_TIME
+# include <sys/time.h>
+# include <time.h>
+#else
+# if HAVE_SYS_TIME_H
+#  include <sys/time.h>
+# else
+#  include <time.h>
+# endif
+#endif
+
+static long
+gettime(void)
 {
+#ifdef HAVE_GETTIMEOFDAY
+	struct timeval tp;
+
+	gettimeofday(&tp, NULL);
+	return (long) tp.tv_sec * 1000000 + (long) tp.tv_usec;
+#else
+#ifdef HAVE_FTIME
+	struct timeb tb;
+
+	ftime(&tb);
+	return (long) tb.time * 1000000 + (long) tb.millitm * 1000;
+#endif
+#endif
+}
+
+void
+usage(char *prog)
+{
+	fprintf(stderr, "Usage: %s [ options ] [ script+ ]                   \n", prog);
+	fprintf(stderr, "Options are:                                        \n");
+	fprintf(stderr, " -c <config_file>    | --config=<config_file>       \n");
+	fprintf(stderr, "                       --dbname=<database_name>     \n");
+	fprintf(stderr, "                       --dbfarm=<database_directory>\n");
+	fprintf(stderr, " -s <option>=<value> | --set <option>=<value>       \n");
+	fprintf(stderr, " -?                  | --help                       \n");
+	exit(-1);
+}
+
+
+int
+main(int argc, char **av)
+{
+	char *prog = *av;
+	opt *set = NULL;
+	int setlen = 0, time = 0;
+	long t0;
 	Mapi mid;
 	MapiHdl hdl;
 	char *line;
+	char buf[BUFSIZ];
 
-	(void) argc;
-	mid = embedded_sql();
-	while (*++argv) {
-		hdl = mapi_query(mid, *argv);
+	static struct option long_options[] = {
+		{"config", 1, 0, 'c'},
+		{"dbname", 1, 0, 0},
+		{"dbfarm", 1, 0, 0},
+		{"time", 0, 0, 't'},
+		{"set", 1, 0, 's'},
+		{"help", 0, 0, '?'},
+		{0, 0, 0, 0}
+	};
+
+	if (!(setlen = mo_builtin_settings(&set)))
+		usage(prog);
+
+	for (;;) {
+		int option_index = 0;
+
+		int c = getopt_long(argc, av, "c:?s:t",
+				    long_options, &option_index);
+
+		if (c == -1)
+			break;
+
+		switch (c) {
+		case 0:
+			if (strcmp(long_options[option_index].name, "dbname") == 0) {
+				setlen = mo_add_option(&set, setlen, opt_cmdline, "gdk_dbname", optarg);
+				break;
+			}
+			if (strcmp(long_options[option_index].name, "dbfarm") == 0) {
+				setlen = mo_add_option(&set, setlen, opt_cmdline, "gdk_dbfarm", optarg);
+				break;
+			}
+			usage(prog);
+			break;
+		case 't':
+			time = 1;
+			break;
+		case 'c':
+			setlen = mo_add_option(&set, setlen, opt_cmdline, "config", optarg);
+			break;
+		case 's':{
+			/* should add option to a list */
+			char *tmp = strchr(optarg, '=');
+
+			if (tmp) {
+				*tmp = '\0';
+				setlen = mo_add_option(&set, setlen, opt_cmdline, optarg, tmp + 1);
+			} else {
+				fprintf(stderr, "!ERROR: wrong format %s\n", optarg);
+			}
+		}
+			break;
+		case '?':
+			usage(prog);
+		default:
+			fprintf(stderr, "!ERROR: getopt returned character code 0%o ??\n", c);
+			usage(prog);
+		}
+	}
+
+	mid = embedded_sql(set, setlen);
+	while ((line = fgets(buf, BUFSIZ, stdin)) != NULL) {
+		if (time)
+			t0 = gettime();
+		hdl = mapi_query(mid, line);
 		do {
 			if (mapi_result_error(hdl) != NULL)
 				mapi_explain_result(hdl, stderr);
@@ -36,6 +151,8 @@ main(int argc, char **argv)
 				printf("%s\n", line);
 		} while (mapi_next_result(hdl) == 1);
 		mapi_close_handle(hdl);
+		if (time)
+			printf("Timer: %ld (usec)\n", gettime()-t0);
 	}
 	return 0;
 }
