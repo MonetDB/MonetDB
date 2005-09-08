3ac6b3045108f978fd7b35ba3c92b120add7cfdb
Jens Teubner <jens.teubner@inf.ethz.ch>
2005-09-08 08:30:39 +0000

    -- Completed the implementation of fn:subsequence() by introducing     specific typing rules. Different variants of fn:subsequence()     in xquery_fo.c were thus no longer needed.
        This fixes bug #1280519.
    
     -- Made error messages on illegal function usage a bit more verbose.
        If a function is called with illegal types, we print out a list
        of possible candidates.

--- pathfinder/compiler/semantics/xquery_fo.c
+++ pathfinder/compiler/semantics/xquery_fo.c
@@ -1109,25 +1109,14 @@
                                 PFty_choice (PFty_xs_anyElement (),      \
                                              PFty_xs_anyAttribute ()))}, \
     .ret_ty = PFty_xs_boolean () }                                       \
-, /* fn:subsequence(node*, double) as node* */                           \
-  { .ns = PFns_fn, .loc = "subsequence",                                 \
-    .arity = 2, .par_ty = { PFty_star (PFty_node ()),                    \
-                            PFty_double () },                            \
-    .ret_ty = PFty_star (PFty_node ()) }                                 \
-, /* fn:subsequence(node*, double, double) as node* */                   \
-  { .ns = PFns_fn, .loc = "subsequence",                                 \
-    .arity = 3, .par_ty = { PFty_star (PFty_node ()),                    \
-                            PFty_double (),                              \
-                            PFty_double () },                            \
-    .ret_ty = PFty_star (PFty_node ()) }                                 \
-                                                                         \
-, { .loc = 0 }                                                           \
 , /* fn:subsequence(item*, double) as item* */                           \
+  /* Note that typecheck.brg implements a specific typing rule! */       \
   { .ns = PFns_fn, .loc = "subsequence",                                 \
     .arity = 2, .par_ty = { PFty_star (PFty_item ()),                    \
                             PFty_double () },                            \
     .ret_ty = PFty_star (PFty_item ()) }                                 \
 , /* fn:subsequence(item*, double, double) as item* */                   \
+  /* Note that typecheck.brg implements a specific typing rule! */       \
   { .ns = PFns_fn, .loc = "subsequence",                                 \
     .arity = 3, .par_ty = { PFty_star (PFty_item ()),                    \
                             PFty_double (),                              \
