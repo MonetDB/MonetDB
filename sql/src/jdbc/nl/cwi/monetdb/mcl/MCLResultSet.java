0ee8701563a6eef0d9e175c8596f1e303079c783
Fabian Groffen <fabian@cwi.nl>
2005-09-10 19:47:29 +0000

    Implemented methods to retrieve Header and Data Messages

--- sql/src/jdbc/nl/cwi/monetdb/mcl/MCLResultSet.java
+++ sql/src/jdbc/nl/cwi/monetdb/mcl/MCLResultSet.java
@@ -18,6 +18,7 @@
 
 package nl.cwi.monetdb.mcl;
 
+import nl.cwi.monetdb.mcl.messages.*;
 import java.util.*;
 import java.lang.reflect.Array; // for dealing with the anonymous columns
 
@@ -88,6 +89,180 @@ public class MCLResultSet {
 	}
 
 	/**
+	 * Retrieves the metadata contained in this MCLResultSet as
+	 * HeaderMessage.  If this MCLResultSet is not valid, an
+	 * MCLException is thrown.
+	 *
+	 * @param id the id to use in the HeaderMessage
+	 * @return a HeaderMessage populated with the metadata stored in
+	 *         this MCLResultSet
+	 * @throws MCLException if this MCLResultSet is not (yet) valid
+	 */
+	public HeaderMessage getHeaderMessage(String id) throws MCLException {
+		if (!isValid) throw
+			new MCLException("MCLResultSet should be valid");
+
+		String ctype = "";
+		for (int i = 0; i < columns.size(); i++)
+			ctype += ((Column)(columns.get(i))).ctype;
+
+		HeaderMessage ret = new HeaderMessage(
+				id,
+				columns.size(),
+				((Column)(columns.get(0))).data.length,
+				ctype
+		);
+		String tmpS[] = new String[columns.size()];
+		for (int i = 0; i < columns.size(); i++)
+			tmpS[i] = ((Column)(columns.get(0))).column;
+		ret.addColumn(tmpS);
+		for (int i = 0; i < columns.size(); i++)
+			tmpS[i] = ((Column)(columns.get(0))).schema;
+		ret.addSchema(tmpS);
+		for (int i = 0; i < columns.size(); i++)
+			tmpS[i] = ((Column)(columns.get(0))).table;
+		ret.addTable(tmpS);
+		for (int i = 0; i < columns.size(); i++)
+			tmpS[i] = ((Column)(columns.get(0))).type;
+		ret.addTypes(tmpS);
+		int tmpI[] = new int[columns.size()];
+		for (int i = 0; i < columns.size(); i++)
+			tmpI[i] = ((Column)(columns.get(0))).digits;
+		ret.addDigits(tmpI);
+		for (int i = 0; i < columns.size(); i++)
+			tmpI[i] = ((Column)(columns.get(0))).scale;
+		ret.addScale(tmpI);
+		for (int i = 0; i < columns.size(); i++)
+			tmpI[i] = ((Column)(columns.get(0))).width;
+		ret.addWidth(tmpI);
+		
+		return(ret);
+	}
+
+	/**
+	 * Retrieves a DataMessage for the given index position range.  If
+	 * the start or stop positions are out of the possible range an
+	 * MCLException is thrown.
+	 *
+	 * @param start the index position to start at, inclusive
+	 * @param stop the index position to stop at, exclusive
+	 * @throws MCLException if this MCLResultSet is not (yet) valid, or
+	 *         start and/or stop are out of range.
+	 */
+	public DataMessage getDataMessage(int start, int stop)
+		throws MCLException
+	{
+		int dataSize = ((Column)(columns.get(0))).data.length;
+		if (start >= dataSize || start < 0) throw
+			new MCLException("start index either too small or too large (0 <= " + start + " < " + dataSize + ")");
+		if (stop > dataSize || stop <= 0) throw
+			new MCLException("stop index either too small or too large (0 < " + stop + " <= " + dataSize + ")");
+
+		DataMessage ret = new DataMessage(stop - start);
+		String tmpS[] = new String[columns.size()];
+		for ( ; start < stop; start++) {
+			for (int i = 0; i < columns.size(); i++) {
+				Column col = (Column)(columns.get(i));
+				switch (col.ctype) {
+					case 'B':
+						tmpS[i] = ((Boolean)(col.data[i])).booleanValue() ? "1" : "0";
+					break;
+					case 'c':
+						tmpS[i] = "" + ((Character)(col.data[i])).charValue();
+					break;
+					case 'S':
+						tmpS[i] = col.data[i].toString().length() + ":" + col.data[i].toString();
+					break;				}
+					case 'o':
+						// not known (yet?)
+						tmpS[i] = "";
+					break;
+					case 'b':
+					case 's':
+					case 'i':
+						tmpS[i] = "" + ((Number)(col.data[i])).intValue();
+					break;
+					case 'l':
+						tmpS[i] = "" + ((Number)(col.data[i])).longValue();
+					break;
+					case 'f':
+					case 'd':
+						tmpS[i] = col.data[i].toString();
+					break;
+					case 'L':
+						// not (yet?) supported
+						tmpS[i] = "";
+					break;
+					case 'T':
+					{
+						long millis = ((java.util.Date)(col.data[i])).getTime();
+						Calendar cal = Calendar.getInstance();
+						cal.setTime((java.util.Date)(col.data[i]));
+						int zone = -(cal.get(Calendar.ZONE_OFFSET) + cal.get(Calendar.DST_OFFSET)) / (60 * 1000);
+						tmpS[i] = prefixZero(milis / 1000, 10) +
+							prefixZero(millis % 1000 * 10, 7) +
+							(zone < 0 ? "-" : "+") +
+							prefixZero(zone < 0 ? -zone : zone, 5);
+					}
+					break;
+					case 't':
+					{
+						long millis = ((java.sql.Time)(col.data[i])).        getTime();
+						Calendar cal = Calendar.getInstance();
+						cal.setTime((java.sql.Time)(col.data[i]));
+						int zone = -(cal.get(Calendar.ZONE_OFFSET) + cal.get(Calendar.DST_OFFSET)) / (60 * 1000);
+						tmpS[i] = prefixZero(milis / 1000, 5) +
+							(zone < 0 ? "-" : "+") +
+							prefixZero(zone < 0 ? -zone : zone, 5);
+					}
+					break;
+					case 'D':
+					{
+						long millis = ((java.sql.Date)(col.data[i])).getTime();
+						tmpS[i] = prefixZero(milis / 1000, 10);
+					}
+					break;
+					case 'u':
+					{
+						String clazz = col.data[i].getClass().getName();
+						clazz = clazz.substring(clazz.lastIndexOf(".") + 1);
+						tmpS[i] = clazz + ":" +
+							col.data[i].toString().length() + ":" +
+							col.data[i].toString();
+					}
+					break;
+			}
+			red.addSentence(new MCLSentence(MCLSentence.DATA, tmpS));
+		}
+
+		return(ret);
+	}
+
+	/**
+	 * Utility function that returns the given numerical object prefixed
+	 * with as much zeroes as necessary to return a String of the
+	 * desired length.
+	 *
+	 * @param num the numerical object
+	 * @param len the desired length
+	 * @return a String of length len representing the numerical object
+	 */
+	private String prefixZero(long num, int len) throws MCLException {
+		if (num < 0) throw
+			new MCLException("Negative values not supported");
+
+		StringBuffer ret = new StringBuffer(len);
+		ret.append("" + num);
+
+		if (ret.length() > len) throw
+			new MCLException("Given value does not fit in the desired length");
+
+		while (ret.length() < len) ret.insert(0, '0');
+
+		return(ret.toString());
+	}
+
+	/**
 	 * Small inner class that represents a column within an MCLResultSet
 	 * and its associated metadata.
 	 */
@@ -117,9 +292,9 @@ public class MCLResultSet {
 			this.data = data;
 			// set default values based on what we have
 			if (data instanceof Boolean[]) {
-				ctype = 'b';
+				ctype = 'B';
 				type = "boolean";
-				width = 5; // "false"
+				width = 1; // either 0 or 1
 			} else if (data instanceof Character[]) {
 				ctype = 'c';
 				type = "char";
@@ -132,6 +307,16 @@ public class MCLResultSet {
 					if (width < data[i].toString().length())
 						width = data[i].toString().length();
 				}
+			} else if (data instanceof Byte[]) {
+				ctype = 'b';
+				type = "tinyint";
+				byte tmp = 0;
+				for (int i = 0; i < data.length; i++) {
+					if (tmp < ((Byte)(data[i])).byteValue())
+						tmp = ((Byte)(data[i])).byteValue();
+				}
+				width = 0;
+				while ((tmp /= 10) > 0) width++;
 			} else if (data instanceof Short[]) {
 				ctype = 's';
 				type = "smallint";
@@ -170,6 +355,19 @@ public class MCLResultSet {
 				ctype = 'd';
 				type = "double";
 				width = 24; // SQL99 max
+			} else if (data instanceof java.sql.Timestamp[] ||
+					data instanceof java.util.Date[]) {
+				ctype = 'T';
+				type = "timestampz";
+				width = 22;
+			} else if (data instanceof java.sql.Time[]) {
+				ctype = 't';
+				type = "timez";
+				width = 11;
+			} else if (data instanceof java.sql.Date[]) {
+				ctype = 'D';
+				type = "date";
+				width = 10;
 			} else {
 				ctype = 'S';	// we map anything else to a String
 				type = "varchar";
