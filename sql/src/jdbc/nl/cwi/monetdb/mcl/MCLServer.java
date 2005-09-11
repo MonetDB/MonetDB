5e94d6ac8433053de051269109bbff1da293de5f
Fabian Groffen <fabian@cwi.nl>
2005-09-11 13:27:41 +0000

    A step forward in the implementation of MCLServer

--- sql/src/jdbc/nl/cwi/monetdb/mcl/MCLServer.java
+++ sql/src/jdbc/nl/cwi/monetdb/mcl/MCLServer.java
@@ -20,6 +20,7 @@ package nl.cwi.monetdb.mcl;
 
 import nl.cwi.monetdb.mcl.messages.*;
 import nl.cwi.monetdb.mcl.io.*;
+import java.util.*;
 
 /**
  * The MCLServer is the actual interface to a server process talking to
@@ -50,7 +51,7 @@ import nl.cwi.monetdb.mcl.io.*;
  * |                     O----------+
  * |                done |
  * |                     V
- * +-------------> endResponse()
+ * +-------------> endResponse(false)
  * </pre>
  * In the diagram above, <tt>X()</tt> can be replaced with one of the
  * methods <tt>successMessage()</tt>, <tt>affectedRowsMessage()</tt>,
@@ -60,6 +61,8 @@ public class MCLServer {
 	private MCLValidator val;
 	private MCLInputStream in;
 	private MCLOutputStream out;
+	private List responseQueue;
+	private Map resultSets;
 
 	/**
 	 * Constructs an MCLServer using the given MCLInputStream and
@@ -78,8 +81,11 @@ public class MCLServer {
 		this.in = in;
 		this.out = out;
 		val = new MCLValidator();
+		responseQueue = new ArrayList();
+		resultSets = new HashMap();
 
 		send(new ChallengeMessage());
+		sendPrompt();
 	}
 
 	/**
@@ -100,50 +106,210 @@ public class MCLServer {
 		return(ret);
 	}
 
-	public void send(MCLMessage m) throws MCLException {
+	/**
+	 * Checks if the message is valid given the previous message seen,
+	 * and sends it over the stream if so.  If the message is not valid,
+	 * an MCLException is thrown.
+	 *
+	 * @param m the MCLMessage to send
+	 * @throws MCLException if the message is not valid at this moment
+	 * @throws MCLIOException if writing to the stream failed
+	 */
+	private void send(MCLMessage m) throws MCLException {
 		if (!val.check(m)) {
 			throw new MCLException("Protocol violation: message is not allowed at this state");
 		}
 		m.writeToStream(out);
 	}
 
-	public void startResponse() {
+	/**
+	 * Sends a prompt over the stream.
+	 */
+	private void sendPrompt() {
 	}
 
-	public void endResponse() throws MCLException {
-		//out.writePrompt();
+	/**
+	 * Sends a 'more' prompt over the stream.
+	 */
+	private void sendMorePrompt() {
 	}
 
+	/**
+	 * Initialises a new response to a received message.  The response
+	 * can consist of multiple MCLMessages.  Only one response can be
+	 * created at a time.
+	 *
+	 * @throws MCLException if this method is called when there is
+	 *         already a response opened
+	 */
+	public void startResponse() throws MCLException {
+		if (responseQueue != null) throw
+			new MCLException("Response already started or not closed");
+		responseQueue = new ArrayList();
+	}
+
+	/**
+	 * Adds an MCLMessage to the currently open response.
+	 *
+	 * @throws MCLException if there is no open response
+	 */
+	public void addToResponse(MCLMessage m) throws MCLException {
+		if (responseQueue == null) throw
+			new MCLException("Response not yet started");
+		responseQueue.add(m);
+	}
+
+	/**
+	 * Ends the open response and sends it to the client.  After all
+	 * MCLMessages in the response are sent, a prompt is sent and the
+	 * response is closed.
+	 *
+	 * @param more whether or not the prompt should be a 'more' prompt
+	 * @throws MCLException if the reponse is not open, there are no
+	 *         MCLMessages in the response, or sending the message(s)
+	 *         failed.
+	 */
+	public void endResponse(boolean more) throws MCLException {
+		if (responseQueue == null) throw
+			new MCLException("Response not yet started");
+		if (responseQueue.size() == 0) throw
+			new MCLException("No messages in response");
+
+		for (Iterator it = responseQueue.iterator(); it.hasNext(); ) {
+			send((MCLMessage)(it.next()));
+		}
+		if (!more) {
+			sendPrompt();
+		} else {
+			sendMorePrompt();
+		}
+		responseQueue = null;
+	}
+
+	/**
+	 * Adds an ErrorMessage to the open response.  If there is no open
+	 * response, this method will create a new response and add a new
+	 * ErrorMessage to it.  Afterwards this method has been called, the
+	 * open response is immediately closed (and sent).
+	 *
+	 * @param message the optional error message (may be null)
+	 * @throws MCLException if sending the message failed
+	 */
 	public void errorMessage(String message) throws MCLException {
-		// should force endResponse somehow
+		// create a response if not yet there
+		if (responseQueue == null) startResponse();
+		
+		// add the ErrorMessage to the response
 		if (message == null) {
-			send(new ErrorMessage());
+			addToResponse(new ErrorMessage());
 		} else {
-			send(new ErrorMessage(message));
+			addToResponse(new ErrorMessage(message));
 		}
+
+		// end and send the response
+		endResponse(false);
 	}
 
+	/**
+	 * Adds an AffectedRowsMessage to the response.  If no response is
+	 * open yet, a new one is created, the message is added and closed.
+	 * Note that this method does *not* close the response if it was
+	 * already open.
+	 *
+	 * @param count the number of rows affected
+	 * @throws MCLException if sending of the message failed
+	 */
 	public void affectedRowsMessage(int count) throws MCLException {
-		send(new AffectedRowsMessage(count));
-	}
-
-	public void successMessage(String message) throws MCLException {
-		if (message == null) {
-			send(new SuccessMessage());
+		// create a response if not yet there
+		if (responseQueue == null) {
+			startResponse();
+			addToResponse(new AffectedRowsMessage(count));
+			endResponse(false);
 		} else {
-			send(new SuccessMessage(message));
+			addToResponse(new AffectedRowsMessage(count));
 		}
 	}
 
+	/**
+	 * Adds a SuccessMessage to the response.  If no response is
+	 * open yet, a new one is created, the message is added and closed.
+	 * Note that this method does *not* close the response if it was
+	 * already open.
+	 *
+	 * @param message the optional information, may be null
+	 * @throws MCLException if sending of the message failed
+	 */
+	public void successMessage(String message) throws MCLException {
+		boolean newResponse = false;
+		// create a response if not yet there
+		if (responseQueue == null) {
+			startResponse();
+			newResponse = true;
+		}
+		if (message == null) {
+			addToResponse(new SuccessMessage());
+		} else {
+			addToResponse(new SuccessMessage(message));
+		}
+		if (newResponse) endResponse(false);
+	}
+
+	/**
+	 * Adds a BatchMessage to the response.  If no response is
+	 * open yet, a new one is created, the message is added and closed.
+	 * Note that this method does *not* close the response if it was
+	 * already open.
+	 *
+	 * @param values the update values in the batch
+	 * @throws MCLException if sending of the message failed
+	 */
 	public void batchMessage(int[] values) throws MCLException {
-		send(new BatchResultMessage(values));
+		// create a response if not yet there
+		if (responseQueue == null) {
+			startResponse();
+			addToResponse(new BatchResultMessage(values));
+			endResponse(false);
+		} else {
+			addToResponse(new BatchResultMessage(values));
+		}
 	}
 
-	public void resultSet(MCLResultSet rs) {
-		// do something: store it and send the header
+	/**
+	 * Adds the given MCLResultSet to the response.  If no response is
+	 * open yet, a new one is created, the message is added and closed.
+	 * Note that this method does *not* close the response if it was
+	 * already open.
+	 *
+	 * @param rs the MCLResultSet to add to the response
+	 * @throws MCLException if sending of the message failed
+	 */
+	public void resultSet(MCLResultSet rs) throws MCLException {
+		if (rs == null) throw
+			new MCLException("MCLResultSet should not be null");
+
+		// create a response if not yet there
+		if (responseQueue == null) {
+			startResponse();
+			addToResponse(rs.getHeaderMessage());
+			endResponse(false);
+		} else {
+			addToResponse(rs.getHeaderMessage());
+		}
+
+		resultSets.put(rs.getId(), rs);
 	}
 
-	public void prepareResult(MCLPrepareResult pr) {
+	/**
+	 * Adds the given MCLPrepareResult to the response.  If no response
+	 * is open yet, a new one is created, the message is added and
+	 * closed.  Note that this method does *not* close the response if
+	 * it was already open.
+	 *
+	 * @param pr the MCLPrepareResult to add to the response
+	 * @throws MCLException if sending of the message failed
+	 */
+	public void prepareResult(MCLPrepareResult pr) throws MCLException {
 		// do something: send the header
+		throw new MCLException("TODO: implement it");
 	}
 }
