5e396809bdc3a5d9ddf694a3bd48b37af9bdddbb
Niels Nes <niels@cwi.nl>
2005-10-06 21:06:15 +0000

    because blob is a keyword, we added a special case to the value_exp
    fixed bug create_view_with_check_options.SF-1314951
    	(check with option, is removed from the view's query)
    
    bat_store.mx fixed bug
    	[ 1306171 ] SQL: update count makes no sense when deleting
    
    added check for a single primary key per table.

--- sql/src/benchmarks/blob/blob.sql
+++ sql/src/benchmarks/blob/blob.sql
@@ -1,2 +1,2 @@
-INSERT INTO adttest (id, dosname, filename, contenttype, blobsize, blobdata) VALUES (1, 'WXKZAYWFEPY', 'DECRYZWIEHLQNUAQPLFXHZLMHRDR', 'video/mpeg', 10, BLOB "(10: 41 41 80 41 41 80 41 41 80 41)");
+INSERT INTO adttest (id, dosname, filename, contenttype, blobsize, blobdata) VALUES (1, 'WXKZAYWFEPY', 'DECRYZWIEHLQNUAQPLFXHZLMHRDR', 'video/mpeg', 10, BLOB '41418041418041418041');
 select * from adttest;
