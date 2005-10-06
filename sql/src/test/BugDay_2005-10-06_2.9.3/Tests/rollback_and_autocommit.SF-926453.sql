11f985ab58ee263ab94898ac7b0c69e0a5ce694e
Stefan Manegold <manegold@cwi.nl>
2005-10-06 16:34:41 +0000

    BUG #926453 "Errorneous Rollback request":
    BugDay_2005-10-06: CLAIMED BY stmane
    
    BugDay_2005-10-06: TEST ADDED / SUCCESS
    sql/src/test/BugDay_2005-10-06_2.9.3/Tests/rollback_and_autocommit.SF-926453.*

---
+++ sql/src/test/BugDay_2005-10-06_2.9.3/Tests/rollback_and_autocommit.SF-926453.sql
@@ -0,0 +1,9 @@
+select count(*) from tapestry where attr2<0;
+select count(*) from tapestry where attr2<0;
+rollback;
+select count(*) from tapestry where attr2<0;
+select count(*) from tapestry where attr2<0;
+rollback;
+set auto_commit=true;
+select count(*) from tapestry where attr2<0;
+select count(*) from tapestry where attr2<0;
