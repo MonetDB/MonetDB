c0d0071b32046119975db10d23b546b67aa8e29c
Niels Nes <niels@cwi.nl>
2005-10-06 12:29:14 +0000

    added test output for test news.sql

--- sql/src/test/news.sql
+++ sql/src/test/news.sql
@@ -2,11 +2,11 @@ CREATE TABLE news (
   id mediumint,
   content text,
   user_id int,
-  date datetime,
+  "date" timestamp,
   PRIMARY KEY (id)
 );
 
-INSERT INTO news (id, content, user_id, date) VALUES
+INSERT INTO news (id, content, user_id, "date") VALUES
 (1, 'dwerg.net word volwassen, er is eindelijk content
 en het voicemail archief begint al aardig vol te lopen.
 De commentaar en rating code voor de voicemails is ook
@@ -19,5 +19,4 @@ je kan zien heb ik bij de voicemails al wat leuke
 knopjes gezet, en het aantal voicemails dat je in 1
 keer ziet verhoogd naar 15. Poepoe.\r\n<br /><br
 />\r\nHet is me allemaal wat...\r\n<br /><br
-/>\r\nMuziek: Air', 1, datetime '2002-02-22 00:21:00');
-
+/>\r\nMuziek: Air', 1, timestamp '2002-02-22 00:21:00');
