226bb53ae3fb8c240a75b59a8b6a8f499159ac29
Fabian Groffen <fabian@cwi.nl>
2005-10-06 09:38:23 +0000

    Adding first query from bug #920585

--- sql/src/test/BugDay_2005-10-06_2.9.3/Tests/UNION_JOIN_vs_plain_set_UNION-2.SF-920585.sql
+++ sql/src/test/BugDay_2005-10-06_2.9.3/Tests/UNION_JOIN_vs_plain_set_UNION-2.SF-920585.sql
@@ -1,1 +1,45 @@
 select * from (select * from tables union select * from tables) as a;
+
+SELECT 'null' AS TABLE_CAT,
+	schemas.name AS TABLE_SCHEM,
+	tables.name AS TABLE_NAME,
+	'SYSTEM TABLE' AS TABLE_TYPE,
+	'' AS REMARKS,
+	'null' AS TYPE_CAT,
+	'null' AS TYPE_SCHEM,
+	'null' AS TYPE_NAME,
+	'id' AS SELF_REFERENCING_COL_NAME,
+	'SYSTEM' AS REF_GENERATION
+FROM tables, schemas
+WHERE tables.schema_id = schemas.id
+	AND tables.system = 1
+	AND tables.istable = 0
+UNION ALL
+SELECT 'null' AS TABLE_CAT,
+	schemas.name AS TABLE_SCHEM,
+	tables.name AS TABLE_NAME,
+	'TABLE' AS TABLE_TYPE,
+	'' AS REMARKS,
+	'null' AS TYPE_CAT,
+	'null' AS TYPE_SCHEM,
+	'null' AS TYPE_NAME,
+	'id' AS SELF_REFERENCING_COL_NAME,
+	'SYSTEM' AS REF_GENERATION
+FROM tables, schemas
+WHERE tables.schema_id = schemas.id
+	AND tables.system = 0
+	AND tables.istable = 0
+UNION ALL
+SELECT 'null' AS TABLE_CAT,
+	schemas.name AS TABLE_SCHEM,
+	tables.name AS TABLE_NAME,
+	'VIEW' AS TABLE_TYPE,
+	tables.query AS REMARKS,
+	'null' AS TYPE_CAT,
+	'null' AS TYPE_SCHEM,
+	'null' AS TYPE_NAME,
+	'id' AS SELF_REFERENCING_COL_NAME,
+	'SYSTEM' AS REF_GENERATION
+FROM tables, schemas
+WHERE tables.schema_id = schemas.id
+	AND tables.istable = 0;
