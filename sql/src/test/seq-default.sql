fa0b2dae356e37d4e5e54216a04e6984904b7881
Fabian Groffen <fabian@cwi.nl>
2005-10-19 21:07:14 +0000

    Try to test some of the bleeding edge super alpha features...

--- sql/src/test/seq-default.sql
+++ sql/src/test/seq-default.sql
@@ -1,5 +1,24 @@
+-- this code currently triggers a multitude of bugs...
+
+
 create sequence test_seq as integer;
 create table test (ts timestamp default now, i integer default next value for test_seq);
+insert into test ;
+insert into test ;
+insert into test ;
+insert into test ;
+insert into test ;
+insert into test ;
+select * from test;
+
+drop sequence test_seq;
+drop table test;
+
+create table test (
+	d date default cast(now() as date),
+	id serial,
+	count int auto_increment
+);
 insert into test ;
 insert into test ;
 insert into test ;
