6a56c340a8f1fd85b9a9cefbeeb1978d403ade59
Niels Nes <niels@cwi.nl>
2005-11-09 22:41:40 +0000

    changed script and approved output

--- sql/src/test/BugDay_2005-11-09_2.9.3/Tests/schema_change.SF-1026845.sql
+++ sql/src/test/BugDay_2005-11-09_2.9.3/Tests/schema_change.SF-1026845.sql
@@ -1,3 +1,4 @@
 create schema voc;
-set schema voc;
 create table voctest (id int);
+select t.name, s.name from sys.tables as t, sys.schemas as s where t.schema_id = s.id and s.name = 'voc';
+set schema voc; -- not allowed
