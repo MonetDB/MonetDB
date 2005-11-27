6d487cfef3eac7ea6c3691636cf69b954132a735
Niels Nes <niels@cwi.nl>
2005-11-27 16:08:46 +0000

    added some order by's to have same output on 4 and 5 backends

--- sql/src/benchmarks/ATIS/Tests/select_distinct.sql
+++ sql/src/benchmarks/ATIS/Tests/select_distinct.sql
@@ -3,5 +3,5 @@ select distinct from_airport from flight order by from_airport;
 select distinct aircraft_code from flight order by aircraft_code;
 select distinct * from fare;
 select distinct flight_code from flight_fare order by flight_code;
-select distinct flight.flight_code,aircraft.aircraft_type from flight,aircraft where flight.aircraft_code=aircraft.aircraft_code;
-select distinct airline.airline_name,aircraft.aircraft_type from aircraft,airline,flight where flight.aircraft_code=aircraft.aircraft_code and flight.airline_code=airline.airline_code;
+select distinct flight.flight_code,aircraft.aircraft_type from flight,aircraft where flight.aircraft_code=aircraft.aircraft_code order by flight.flight_code;
+select distinct airline.airline_name,aircraft.aircraft_type from aircraft,airline,flight where flight.aircraft_code=aircraft.aircraft_code and flight.airline_code=airline.airline_code order by airline.airline_name, aircraft.aircraft_type;
