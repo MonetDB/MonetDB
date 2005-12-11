54123c4f06e171c8b0a7566f66a3f36ffb871ed3
Fabian Groffen <fabian@cwi.nl>
2005-12-11 20:45:57 +0000

    Restoring JdbcClient.SQL.bat file.  Not that it works, therefore I need m4d windows scripting sk1lzzzz.

--- sql/src/jdbc/tests/Tests/Test_JdbcClient.SQL.bat
+++ sql/src/jdbc/tests/Tests/Test_JdbcClient.SQL.bat
@@ -1,5 +1,24 @@
 @echo off
 
-URL="jdbc:monetdb://${HOST}:${SQLPORT}/database?user=monetdb&password=monetdb${JDBC_EXTRA_ARGS}"
+echo We don't know what the name of the JAR is here, and I don't know how
+echo to figure that out using windows...  Sorry, this test fails on Windows.
 
-call Mlog.bat -x "ant -e -buildfile ${TSTSRCDIR}/../build.xml -Dbuilddir=${TSTBLDDIR}/../ -Djdbc_url=\"${URL}\" -Djardir=${TSTBLDDIR}/../../ ${TST}"
+set JAR=""
+if exist "%SQL_BUILD%\src\jdbc\MonetDB_JDBC.jar"		set JAR=%SQL_BUILD%\src\jdbc\MonetDB_JDBC.jar
+if exist "%TSTBLDBASE%\src\jdbc\MonetDB_JDBC.jar"		set JAR=%TSTBLDBASE%\src\jdbc\MonetDB_JDBC.jar
+if exist "%SQL_PREFIX%\share\MonetDB\lib\MonetDB_JDBC.jar"	set JAR=%SQL_PREFIX%\share\MonetDB\lib\MonetDB_JDBC.jar
+if exist "%MONETDB_PREFIX%\share\MonetDB\lib\MonetDB_JDBC.jar"	set JAR=%MONETDB_PREFIX%\share\MonetDB\lib\MonetDB_JDBC.jar
+if JAR == ""							set JAR=%TSTTRGBASE%\share\MonetDB\lib\MonetDB_JDBC.jar
+
+echo user=monetdb>	.monetdb
+echo password=monetdb>>	.monetdb
+
+set LANG=en_US.UTF-8
+
+call Mlog.bat -x java -jar "%JAR%" -h %HOST% -p %SQLPORT% -f "%RELSRCDIR%\..\JdbcClient_create_tables.sql"
+
+call Mlog.bat -x java -jar "%JAR%" -h %HOST% -p %SQLPORT% -f "%RELSRCDIR%\..\JdbcClient_inserts_selects.sql"
+
+call Mlog.bat -x java -jar "%JAR%" -h %HOST% -p %SQLPORT% -d
+
+del .monetdb
