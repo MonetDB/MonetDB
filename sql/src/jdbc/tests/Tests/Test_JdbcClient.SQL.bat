fa862340881a133d6eee7a06ec5d849d64caf95f
Niels Nes <niels@cwi.nl>
2005-12-11 08:14:32 +0000

    fixing windows version

--- sql/src/jdbc/tests/Tests/Test_JdbcClient.SQL.bat
+++ sql/src/jdbc/tests/Tests/Test_JdbcClient.SQL.bat
@@ -2,4 +2,4 @@
 
 URL="jdbc:monetdb://${HOST}:${SQLPORT}/database?user=monetdb&password=monetdb${JDBC_EXTRA_ARGS}"
 
-call Mlog.bat -x "ant -e -buildfile ${TSTSRCDIR}/../build.xml -Dbuilddir=${PWD} -Djdbc_url=\"${URL}\" -Djardir=${TSTBLDDIR}/../../ ${TST}"
+call Mlog.bat -x "ant -e -buildfile ${TSTSRCDIR}/../build.xml -Dbuilddir=${TSTBLDDIR}/../ -Djdbc_url=\"${URL}\" -Djardir=${TSTBLDDIR}/../../ ${TST}"
