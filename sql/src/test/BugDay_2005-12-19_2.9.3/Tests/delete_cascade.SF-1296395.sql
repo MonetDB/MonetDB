83a002b247ac4b99036a70a05dbe0db904505c41
Fabian Groffen <fabian@cwi.nl>
2005-12-23 22:44:59 +0000

    Although a X-mas tree looks nice with some nice red balls in it, I tried to remove some of them to see more green.  Unfortunately some of the balls were from plastic and stubborn enough to go for the phenomenon called 'Kerstweb'.
    approved some, changed a test, and added some comments to clarify some
    behaviour.

--- sql/src/test/BugDay_2005-12-19_2.9.3/Tests/delete_cascade.SF-1296395.sql
+++ sql/src/test/BugDay_2005-12-19_2.9.3/Tests/delete_cascade.SF-1296395.sql
@@ -3,4 +3,7 @@ create table b1296395 (id int, foreign key (id) references a1296395(id) on delet
 insert into a1296395 values (1);
 insert into b1296395 values(1);
 delete from a1296395; 
-select * from b1296395;
+-- note, the "on delete cascade" constraint should have removed the
+-- single record from table b here.  Until the moment that we support
+-- this, this will return one record, which is wrong.
+select count(*) from b1296395;
