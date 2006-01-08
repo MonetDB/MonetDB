aeadadf3813441ff6dd7b4aa0c9c7a4a903df0ec
cvs2hg <unknown>
2006-01-08 22:22:22 +0000

    fixup commit for branch 'XQuery_0-9_soap'

--- sql/src/test/BugDay_2005-10-06_2.9.3/Tests/set_types.SF-932360.sql
+++
@@ -1,5 +0,0 @@
-set reply_size = 1;
-set reply_size = true;
-set current_timezone = 60;
-set reply_size = false;
-set current_timezone = 0;
--- sql/src/test/BugDay_2005-12-19_2.9.3/Tests/prepare_doesnot_like_LIKE.SF-1234205.sql
+++
@@ -1,4 +0,0 @@
-prepare select name from tables where name like ?;
-execute 1 ('%');
-prepare select name from tables where name like 's%';
-execute 2 ();
