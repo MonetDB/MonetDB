3a7bbd16a3878dd89975d558c09384d92bbab35a
Stefan Manegold <manegold@cwi.nl>
2006-01-20 15:32:13 +0000

    automake <= 1.6.3 (< 1.9.2 ?) seems to require that we always initialize BUILT_SOURCES, even if it's empty. this fixes bug #1410564 "MonetDB's bootstrap fails on Debian & Darwin (automake <1.7)".

--- buildtools/autogen/autogen/am.py
+++ buildtools/autogen/autogen/am.py
@@ -963,10 +963,9 @@ endif
         elif i != 'TARGETS':
             am_assignment(fd, i, v, am)
 
-    if len(am['BUILT_SOURCES']) >= 0:
-        fd.write("BUILT_SOURCES =%s\n" % am_list2string(am['BUILT_SOURCES'], " ", ""))
-        # the BUILT_SOURCES should be cleaned up by make (mostly)clean
-        fd.write("MOSTLYCLEANFILES =%s\n" % am_list2string(am['BUILT_SOURCES'], " ", ""))
+    fd.write("BUILT_SOURCES =%s\n" % am_list2string(am['BUILT_SOURCES'], " ", ""))
+    # the BUILT_SOURCES should be cleaned up by make (mostly)clean
+    fd.write("MOSTLYCLEANFILES =%s\n" % am_list2string(am['BUILT_SOURCES'], " ", ""))
 
     fd.write("EXTRA_DIST = Makefile.msc%s " % \
           am_list2string(am['EXTRA_DIST'], " ", ""))
