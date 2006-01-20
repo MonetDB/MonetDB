76f0ec195d51eafc71ddc65932d046664340ff8a
cvs2hg <unknown>
2006-01-20 20:44:05 +0000

    fixup commit for tag 'XQuery_0-9-3'

--- sql/src/test/identifiers.sql
+++
@@ -1,8 +0,0 @@
-SELECT 1 AS bla; -- gives bla
-SELECT 1 AS BLA; -- gives bla
-SELECT 1 AS "bla"; -- gives bla
-SELECT 1 AS "Bla"; -- gives Bla
-SELECT 1 AS "BLA"; -- gives BLA
-CREATE TABLE "B\"la\"" (id int); -- succeeds
-SELECT 1 AS "B\"la\""; -- should succeed, gives B"la"
-SELECT 1 AS "\"Bla\""; -- should fail
