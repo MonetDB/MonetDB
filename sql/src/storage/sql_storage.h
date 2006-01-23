78a3cf28444c00b9aa8b6806edb7d6dabbc231f0
Stefan Manegold <manegold@cwi.nl>
2006-01-23 04:07:31 +0000

    propagated changes of Friday Jan 20 2006 - Sunday Jan 22 2006 from the SQL_2-10 branch to the development trunk

--- sql/src/storage/sql_storage.h
+++ sql/src/storage/sql_storage.h
@@ -51,6 +51,7 @@ extern void
 
 extern void store_lock();
 extern void store_unlock();
+extern int store_schema_number();
 
 extern sql_trans *sql_trans_create(backend_stack stk, sql_trans *parent, char *name);
 extern sql_trans *sql_trans_destroy(sql_trans *tr);
