418dac6bc753747066dc161121664c4f45800a03
cvs2hg <unknown>
2006-01-24 20:25:43 +0000

    fixup commit for tag 'MonetDB_4-11-0'

--- sql/src/test/BugDay_2005-11-09_2.9.3/Tests/having_clause_quirks.SF-947601.sql
+++
@@ -1,2 +0,0 @@
-select name from tables having 1=1;
-select name from tables group by name having 1=1;
--- sql/src/test/BugTracker/Tests/cardinality_violation.SF-1240701.sql
+++
@@ -1,2 +0,0 @@
-select * from env where name = ( select 'prefix' from env );
-select * from columns where name = (select columns.name from ptables, columns where ptables.id = columns.table_id);
