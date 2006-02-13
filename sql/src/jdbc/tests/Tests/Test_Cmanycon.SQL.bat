bf425bd29e701a7274c43ed6200b67410b2f2caa
Fabian Groffen <fabian@cwi.nl>
2006-02-13 13:55:08 +0000

    Use database as supplied by mTest instead of some dummy value.  It helps Five to accept a specific connection for a database if it by coincidence matches.

--- sql/src/jdbc/tests/Tests/Test_Cmanycon.SQL.bat
+++ sql/src/jdbc/tests/Tests/Test_Cmanycon.SQL.bat
@@ -1,5 +1,5 @@
 @echo off
 
-URL="jdbc:monetdb://${HOST}:${MAPIPORT}/database?user=monetdb&password=monetdb${JDBC_EXTRA_ARGS}"
+URL="jdbc:monetdb://${HOST}:${MAPIPORT}/${TSTDB}?user=monetdb&password=monetdb${JDBC_EXTRA_ARGS}"
 
 call Mlog.bat -x "ant -emacs -buildfile ${TSTSRCDIR}/../build.xml -Dbuilddir=${TSTBLDDIR}/../ -Djdbc_url=\"${URL}\" -Djardir=${TSTBLDDIR}/../../ ${TST}"
