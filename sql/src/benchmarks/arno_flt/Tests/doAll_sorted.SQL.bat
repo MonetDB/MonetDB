e0566c37052fbfa8f18b8464c5e6421d1ff03c34
cvs2hg <unknown>
2006-02-16 23:37:32 +0000

    fixup commit for tag 'Jan2006_undone_Burkowski2StandOff'

--- sql/src/benchmarks/arno_flt/Tests/doAll_sorted.SQL.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-call Mlog.bat -x %SQL_CLIENT% < "%TSTSRCDIR%\check0.sql"
-
-call Mlog.bat -x %SQL_CLIENT% < "%TSTSRCBASE%\%TSTDIR%\queries_sorted.sql"
--- sql/src/test/BugDay_2005-10-06_2.9.3/Tests/timestamp_parsing.SF-927300.sql
+++
@@ -1,2 +0,0 @@
-select timestamp '2004-04-01 08:57';
-select timestamp '01-04-2004 08:57';
