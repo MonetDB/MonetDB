697c93d5ed5a6a9b1f7fc6527bfc6bd2e5e6be64
Stefan Manegold <manegold@cwi.nl>
2006-02-21 23:44:41 +0000

    aligned release number with SQL_2-10 release branch

--- sql/NT/configure.py
+++ sql/NT/configure.py
@@ -47,7 +47,7 @@ subs = [
     ('@libexecdir@',             r'${prefix}\libexec'),
     ('@Xlibexecdir@',            r'${prefix}\libexec'),
     ('@PACKAGE@',                r'sql'),
-    ('@VERSION@',                r'2.11.1'),
+    ('@VERSION@',                r'2.11.2'),
     ('@DIRSEP@',                 '\\'),
     ('@prefix@',                 prefix),
     ('@Xprefix@',                prefix),
