f60ed6b04b9559aff02046bd70c58889e6cbfe1a
cvs2hg <unknown>
2006-02-21 14:56:25 +0000

    fixup commit for tag 'XQuery_0-11-1'

--- sql/src/benchmarks/blob/blob.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO adttest (id, dosname, filename, contenttype, blobsize, blobdata) VALUES (1, 'WXKZAYWFEPY', 'DECRYZWIEHLQNUAQPLFXHZLMHRDR', 'video/mpeg', 10, BLOB '41418041418041418041');
-select * from adttest;
--- sql/src/test/bugs/Tests/view_limit.SF-1345861.sql
+++
@@ -1,1 +0,0 @@
-create view foo1345861 as select * from ttables union all select * from ptables;
