7f9fda0b67f00175b59debc9f14766f291a13a86
Stefan Manegold <manegold@cwi.nl>
2006-03-05 10:14:30 +0000

    - for the sake of comparison, added low-hgh cracker   that does not single out/cluster tail==low & tail==hgh;   i.e., now we have:
    command zcrack_@1 (b:bat[:oid,:@3], mid:@3) :bat[:oid,:@3]
    address CRKcrackZero_@1_@3
    comment "Break a BAT into two pieces with
             tail<=mid, tail>mid,
             respectively. (By @2.)";
    
    command crack_@1 (b:bat[:oid,:@3], mid:@3) :bat[:oid,:@3]
    address CRKcrackOne_@1_@3
    comment "Break a BAT into three pieces with
             tail<mid, tail==mid, tail>mid,
             respectively. (By @2.)";
    
    command crack_@1 (b:bat[:oid,:@3], low:@3, hgh:@3) :bat[:oid,:@3]
    address CRKcrackTwo_@1_@3
    comment "Break a BAT into five pieces with
             tail<low, tail==low, low<tail<hgh, tail==hgh, tail>hgh,
             respectively. (By @2.)";
    
    command zcrack_@1 (b:bat[:oid,:@3], low:@3, hgh:@3) :bat[:oid,:@3]
    address CRKcrackThree_@1_@3
    comment "Break a BAT into three pieces with
             tail<=low, low<tail<=hgh, tail>hgh,
             respectively. (By @2.)";
    
    - added some more tests

--- MonetDB5/src/modules/mal/Tests/testrun.sh
+++ MonetDB5/src/modules/mal/Tests/testrun.sh
@@ -1,12 +1,27 @@
-mserver5 fastcrack_One_MK.mal   </dev/null |tail -2
-#mserver5 fastcrack_One_SI.mal   </dev/null |tail -2
-mserver5 fastcrack_One_sm.mal   </dev/null |tail -2
-mserver5 fastcrack_One_SM.mal   </dev/null |tail -2
-mserver5 fastcrack_Two_MK.mal   </dev/null |tail -2
-#mserver5 fastcrack_Two_SI.mal   </dev/null |tail -2
-mserver5 fastcrack_Two_sm.mal   </dev/null |tail -2
-mserver5 fastcrack_Two_SM.mal   </dev/null |tail -2
-mserver5 fastcrack_Zero_MK.mal   </dev/null |tail -2
-#mserver5 fastcrack_Zero_SI.mal   </dev/null |tail -2
-mserver5 fastcrack_Zero_sm.mal   </dev/null |tail -2
-mserver5 fastcrack_Zero_SM.mal   </dev/null |tail -2
+mserver5 --set monet_prompt='' <fastcrack_scan.mal |tail -1
+mserver5 --set monet_prompt='' <fastcrack_copy.mal |tail -1
+mserver5 --set monet_prompt='' <fastcrack_sel_EQ_mid.mal |tail -1
+mserver5 --set monet_prompt='' <fastcrack_sel_LE_mid.mal |tail -1
+mserver5 --set monet_prompt='' <fastcrack_sel_low_hgh.mal |tail -1
+mserver5 --set monet_prompt='' <fastcrack_sel_ALL.mal |tail -1
+mserver5 --set monet_prompt='' <fastcrack_sort.mal |tail -1
+echo 'tail<=MID , tail>MID'
+mserver5 --set monet_prompt='' <fastcrack_Zero_MK.mal |tail -1
+#mserver5 --set monet_prompt='' <fastcrack_Zero_SI.mal |tail -1
+mserver5 --set monet_prompt='' <fastcrack_Zero_sm.mal |tail -1
+mserver5 --set monet_prompt='' <fastcrack_Zero_SM.mal |tail -1
+echo 'tail<MID , tail==MID , tail>MID'
+mserver5 --set monet_prompt='' <fastcrack_One_MK.mal |tail -1
+mserver5 --set monet_prompt='' <fastcrack_One_SI.mal |tail -1
+mserver5 --set monet_prompt='' <fastcrack_One_sm.mal |tail -1
+mserver5 --set monet_prompt='' <fastcrack_One_SM.mal |tail -1
+echo 'tail<=LOW , LOW<tail<=HGH , tail>HGH'
+#mserver5 --set monet_prompt='' <fastcrack_Three_MK.mal |tail -1
+#mserver5 --set monet_prompt='' <fastcrack_Three_SI.mal |tail -1
+mserver5 --set monet_prompt='' <fastcrack_Three_sm.mal |tail -1
+mserver5 --set monet_prompt='' <fastcrack_Three_SM.mal |tail -1
+echo 'tail<LOW , tail==LOW , LOW<tail<HGH , tail==HGH , tail>HGH'
+mserver5 --set monet_prompt='' <fastcrack_Two_MK.mal |tail -1
+#mserver5 --set monet_prompt='' <fastcrack_Two_SI.mal |tail -1
+mserver5 --set monet_prompt='' <fastcrack_Two_sm.mal |tail -1
+mserver5 --set monet_prompt='' <fastcrack_Two_SM.mal |tail -1
