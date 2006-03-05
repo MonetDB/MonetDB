433478d617bc460648d6e5a55ad4b734e3c994e5
Martin Kersten <mk@cwi.nl>
2006-03-05 10:29:21 +0000

    Added the test used in my analysis. It was done in parallel to Stefan's addition. Duplicate tests should be removed.

--- MonetDB5/src/modules/mal/Tests/testrun.sh
+++ MonetDB5/src/modules/mal/Tests/testrun.sh
@@ -1,3 +1,7 @@
+mserver5 --set monet_prompt='' <fastcrack_select_void_low_hgh.mal |tail -1 
+mserver5 --set monet_prompt='' <fastcrack_select_void_mid.mal   |tail -1 
+mserver5 --set monet_prompt='' <fastcrack_select_low_hgh.mal   |tail -1
+mserver5 --set monet_prompt='' <fastcrack_select_mid.mal   |tail -1
 mserver5 --set monet_prompt='' <fastcrack_scan.mal |tail -1
 mserver5 --set monet_prompt='' <fastcrack_copy.mal |tail -1
 mserver5 --set monet_prompt='' <fastcrack_sel_EQ_mid.mal |tail -1
@@ -5,6 +9,7 @@ mserver5 --set monet_prompt='' <fastcrack_sel_LE_mid.mal |tail -1
 mserver5 --set monet_prompt='' <fastcrack_sel_low_hgh.mal |tail -1
 mserver5 --set monet_prompt='' <fastcrack_sel_ALL.mal |tail -1
 mserver5 --set monet_prompt='' <fastcrack_sort.mal |tail -1
+
 echo 'tail<=MID , tail>MID'
 mserver5 --set monet_prompt='' <fastcrack_Zero_MK.mal |tail -1
 #mserver5 --set monet_prompt='' <fastcrack_Zero_SI.mal |tail -1
