951e3c9409541d94ff5efa9d04ee227bd4b686a9
Stefan Manegold <manegold@cwi.nl>
2006-03-10 11:07:46 +0000

    making case-insensitive file systems happy: renamed fastcrack_*_sm.mal to fastcrack_*_ms.mal

--- MonetDB5/src/modules/mal/Tests/validate.sh
+++ MonetDB5/src/modules/mal/Tests/validate.sh
@@ -3,10 +3,10 @@
 n=100
 echo "n:=$n"
 ./set_n.sh $n >/dev/null
-for x in 0 1 2 3 4 5 6 7 8 9 15 11 12 13 16 17 23 24 25 31 32 33 47 48 49 63 64 65 95 96 97 127 128 129 191 192 193 255 256 257 382 384 385 511 512 513 1023 1024 1025 ; do
+for x in 0 1 2 3 4 5 6 7 8 9 11 12 13 15 16 17 23 24 25 31 32 33 47 48 49 63 64 65 95 96 97 127 128 129 191 192 193 255 256 257 382 384 385 511 512 513 1023 1024 1025 ; do
 	echo "tuples:=$x"
 	./set_tuples.sh $x >/dev/null
-	for i in {Zero,One,Three,Two}_{sm,SM,MK} {One,Three}_SI ; do
+	for i in {Zero,One,Three,Two}_{ms,SM,MK} {One,Three}_SI ; do
 		echo $i:
 		mserver5 --set monet_prompt='' <fastcrack_$i.mal |egrep -v '^[# ]|^\[ .*\]$'
 	done
@@ -18,7 +18,7 @@ echo "n:=$n"
 for x in $[(512*1024)-1] $[(512*1024)] $[(512*1024)+1] $[(768*1024)-1] $[(768*1024)] $[(768*1024)+1] $[(1024*1024)-1] $[(1024*1024)] $[(1024*1024)+1] ; do
 	echo "tuples:=$x"
 	./set_tuples.sh $x >/dev/null
-	for i in {Zero,One,Three,Two}_{sm,SM,MK} {One,Three}_SI ; do
+	for i in {Zero,One,Three,Two}_{ms,SM,MK} {One,Three}_SI ; do
 		echo $i:
 		mserver5 --set monet_prompt='' <fastcrack_$i.mal |egrep -v '^[# ]|^\[ .*\]$'
 	done
