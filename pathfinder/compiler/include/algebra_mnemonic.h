02517a1132b86715839eae69e1478eb4f16bd3f9
Stefan Manegold <manegold@cwi.nl>
2006-03-18 13:06:31 +0000

    updates, fixed, added copyright statements and references to our License

--- pathfinder/compiler/include/algebra_mnemonic.h
+++ pathfinder/compiler/include/algebra_mnemonic.h
@@ -4,6 +4,9 @@
  * Mnemonic abbreviations for algebra constructors.
  * (Generic algebra constructors)
  *
+ * Copyright Notice:
+ * -----------------
+ *
  * The contents of this file are subject to the Pathfinder Public License
  * Version 1.1 (the "License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
