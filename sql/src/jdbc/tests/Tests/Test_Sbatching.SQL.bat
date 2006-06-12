e814ad134b4843c69c267f01057a1e9252257757
cvs2hg <unknown>
2006-06-12 12:16:13 +0000

    fixup commit for tag 'Jun2006-_'

--- sql/src/jdbc/tests/Tests/Test_Sbatching.SQL.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-URL="jdbc:monetdb://${HOST}:${MAPIPORT}/${TSTDB}?user=monetdb&password=monetdb${JDBC_EXTRA_ARGS}"
-
-call Mlog.bat -x "ant -emacs -buildfile ${TSTSRCDIR}/../build.xml -Dbuilddir=${TSTBLDDIR}/../ -Djdbc_url=\"${URL}\" -Djardir=${TSTBLDDIR}/../../ ${TST}"
--- sql/src/test/bugs/subselect_column-bug-sf-951206.sql
+++
@@ -1,2 +0,0 @@
-select x from (select count(*) as x from _tables where "istable" = true) as t;
-select x from (select name as x from _tables where "istable" = true) as t;
