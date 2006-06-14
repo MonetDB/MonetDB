ffc9840296f0bee919d993d7b402de23806fe5b0
Fabian Groffen <fabian@cwi.nl>
2006-06-14 10:43:10 +0000

    Changing test so it is independent of the users already around.  Make effects of the test clear.

--- sql/src/test/BugTracker/Tests/drop_schema_crash.SF-1504794-data.sql
+++ sql/src/test/BugTracker/Tests/drop_schema_crash.SF-1504794-data.sql
@@ -1,5 +1,10 @@
 CREATE USER "dbtapestry" WITH PASSWORD 'dbtapestry' NAME 'DB Tapestry' SCHEMA "sys";
 CREATE SCHEMA "dbtapestry" AUTHORIZATION "dbtapestry";
 ALTER USER "dbtapestry" SET SCHEMA "dbtapestry";
-DROP schema "dbtapestry";
-SELECT count(*) from "users";
+-- after this dbtapestry user gets "sys" schema; evil?
+DROP SCHEMA "dbtapestry";
+SELECT "users"."name", "schemas"."name"
+	FROM "users", "schemas"
+	WHERE "users"."default_schema" = "schemas"."id"
+--		AND "users"."name" LIKE 'dbtapestry';
+;
