2f07adf1b84cacc18500c47c2d06b189f86b3615
Fabian Groffen <fabian@cwi.nl>
2006-06-14 09:03:20 +0000

    Clean up table, such that other test don't get harmed by it.

--- sql/src/test/bugs/union-bug-sf-978045.sql
+++ sql/src/test/bugs/union-bug-sf-978045.sql
@@ -3,3 +3,5 @@ insert into t978045 values ('niels'),('fabian'),('martin');
 
 select name from t978045 union select name from t978045;
 select name from t978045 union all select name from t978045;
+
+drop table t978045;
