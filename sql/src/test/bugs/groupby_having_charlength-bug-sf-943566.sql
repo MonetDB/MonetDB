3c1920c9666a8215d39692edbd0c2d0801405b9c
Fabian Groffen <fabian@cwi.nl>
2006-06-16 19:27:27 +0000

    Reduce system catalog changes/differences/effects by limiting to a number of "stable" tables.

--- sql/src/test/bugs/groupby_having_charlength-bug-sf-943566.sql
+++ sql/src/test/bugs/groupby_having_charlength-bug-sf-943566.sql
@@ -1,2 +1,4 @@
 select s.name,char_length(s.name), count(*) from schemas s, tables t
+	where t.name IN ('args', 'columns', 'functions', 'idxs',
+		'keycolumns', 'keys', 'modules', 'sequences')
 	group by s.name having count(*) > char_length(s.name);
--- sql/src/test/bugs/innerjoin_multiple-bug-sf-943661.sql
+++ sql/src/test/bugs/innerjoin_multiple-bug-sf-943661.sql
@@ -6,6 +6,8 @@ select schemas.name, tables.name, columns.name from
 		inner join 
 	schemas on (tables.schema_id = schemas.id)
  where tables."system" = true 
+ 	and tables.name in ('args', 'columns', 'functions', 'idxs',
+			'keycolumns', 'keys', 'modules', 'sequences')
  order by schemas.name, tables.name, columns.name;
 
 
@@ -17,4 +19,6 @@ select s.name, t.name, c.name from
 		inner join 
 	schemas s on (t.schema_id = s.id)
  where t."system" = true
+ 	and t.name in ('args', 'columns', 'functions', 'idxs',
+			'keycolumns', 'keys', 'modules', 'sequences')
  order by s.name, t.name, c.name;
