902de05fce9d8dd34c39355bf713b096f2b774f0
Fabian Groffen <fabian@cwi.nl>
2006-06-17 19:49:31 +0000

    Remove or limit dependency on system catalog tables, since they differ between M4 and M5.

--- sql/src/test/bugs/subselect_ambigious_columns-bug-sf-949071.sql
+++ sql/src/test/bugs/subselect_ambigious_columns-bug-sf-949071.sql
@@ -1,2 +1,6 @@
-select name from tables, (select count(*) from tables where "istable" = true) as t2 where "istable" = true and "system" = true;
-select name from (select count(*) from tables where "istable" = true) as t2, tables where "istable" = true and "system" = true;
+select name from tables, (select count(*) from tables where "istable" = true) as t2 where "istable" = true and "system" = true
+and name IN ('args', 'columns', 'functions', 'idxs',
+		'keycolumns', 'keys', 'modules', 'sequences');
+select name from (select count(*) from tables where "istable" = true) as t2, tables where "istable" = true and "system" = true
+and name IN ('args', 'columns', 'functions', 'idxs',
+		'keycolumns', 'keys', 'modules', 'sequences');
