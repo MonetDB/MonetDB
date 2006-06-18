96ea6aa44558f1ba87bb0ea79d14ced8b18ca3cb
Stefan Manegold <manegold@cwi.nl>
2006-06-18 00:00:01 +0000

    propagated changes of Saturday Jun 17 2006 from the SQL_2-12 branch to the development trunk

--- sql/src/test/bugs/subselect_column-bug-sf-951206.sql
+++ sql/src/test/bugs/subselect_column-bug-sf-951206.sql
@@ -1,2 +1,8 @@
-select x from (select count(*) as x from _tables where "istable" = true) as t;
-select x from (select name as x from _tables where "istable" = true) as t;
+start transaction;
+
+create table "t951206" (name varchar(1024), isloco boolean);
+insert into "t951206" values ('niels', false), ('fabian', true),
+('martin', false);
+
+select x from (select count(*) as x from "t951206" where "isloco" = true) as t;
+select x from (select name as x from "t951206" where "isloco" = true) as t;
