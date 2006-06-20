e6f13fcb6199efb98fba264ad690f98902686ff1
Stefan Manegold <manegold@cwi.nl>
2006-06-20 15:54:57 +0000

    made test more maintainable

---
+++ pathfinder/tests/StandOff/parser/Tests/so-good.py
@@ -0,0 +1,13 @@
+import os
+import string
+
+TST = os.environ['TST']
+TSTSRCDIR = os.environ['TSTSRCDIR']
+
+CALL = "pf -b -s1 %s.xq" % (os.path.join(TSTSRCDIR,TST))
+
+if os.name == "nt":
+    os.system("%s & echo %ERRORLEVEL%" % CALL)
+else:
+    os.system("%s ; echo $?" % CALL)
+        
