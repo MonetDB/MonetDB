2ab79ed242c8b35deab6fa87be3316325b5a9b8e
cvs2hg <unknown>
2006-06-22 18:09:39 +0000

    fixup commit for tag 'XQuery_0-13-0'

--- sql/src/test/BugDay_2005-10-06_2.9.3/Tests/bad_sql_logdir.SF-927371.bat
+++
@@ -1,3 +0,0 @@
-call Mlog.bat "echo 'loaded(); quit();' \| Mserver %setMONETDB_MOD_PATH% --set monet_prompt='' --set sql_logdir=/xyz --dbinit='module(sql_server); loaded(); sql_server_start();'"
-call           echo 'loaded(); quit();'  | Mserver %setMONETDB_MOD_PATH% --set monet_prompt='' --set sql_logdir=/xyz --dbinit='module(sql_server); loaded(); sql_server_start();'
-               
--- sql/src/test/BugDay_2005-11-09_2.9.3/Tests/history_table_escape.SF-1072089.sql
+++
@@ -1,7 +0,0 @@
-START TRANSACTION;
-SET explain='profile';
-
-DELETE FROM profile;
-SELECT 1;
-SELECT 'test';
-SELECT query FROM profile;
