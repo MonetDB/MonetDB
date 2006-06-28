ba1e2d8c9f1d3661bf356f536e6f2c8ac2e55732
Niels Nes <niels@cwi.nl>
2006-06-28 20:29:39 +0000

    in m4 sql_server use _CTgroup with custom sized hashtables in server, multi attribute join/groupby/unique-keys distribute the values properly over the all the 32 bits of the integer hash. in m5 added mvc_append*, ie replace insert bat if current insert bat is empty in m5 added new daytime(lng) function sql_user fix oid vs sqlid problem
    fix m4/m5 for the new CTderive interface

--- sql/src/benchmarks/tpch/load-sf-1.sql
+++ sql/src/benchmarks/tpch/load-sf-1.sql
@@ -1,9 +1,10 @@
-COPY 150000 RECORDS INTO customer from 'DIR/customer.tbl' USING DELIMITERS '|', '|\n';
-COPY 25 RECORDS INTO nation from 'DIR/nation.tbl' USING DELIMITERS '|', '|\n';
-COPY 1500000 RECORDS INTO orders from 'DIR/orders.tbl' USING DELIMITERS '|', '|\n';
-COPY 800000 RECORDS INTO partsupp from 'DIR/partsupp.tbl' USING DELIMITERS '|', '|\n';
-COPY 200000 RECORDS INTO part from 'DIR/part.tbl' USING DELIMITERS '|', '|\n';
-COPY 5 RECORDS INTO region from 'DIR/region.tbl' USING DELIMITERS '|', '|\n';
-COPY 10000 RECORDS INTO supplier from 'DIR/supplier.tbl' USING DELIMITERS '|', '|\n';
-COPY 7000000 RECORDS INTO lineitem from 'DIR/lineitem.tbl' USING DELIMITERS '|', '|\n';
+START TRANSACTION;
+COPY 5 RECORDS INTO region from 'PWD/region.tbl' USING DELIMITERS '|', '|\n';
+COPY 25 RECORDS INTO nation from 'PWD/nation.tbl' USING DELIMITERS '|', '|\n';
+COPY 10000 RECORDS INTO supplier from 'PWD/supplier.tbl' USING DELIMITERS '|', '|\n';
+COPY 150000 RECORDS INTO customer from 'PWD/customer.tbl' USING DELIMITERS '|', '|\n';
+COPY 200000 RECORDS INTO part from 'PWD/part.tbl' USING DELIMITERS '|', '|\n';
+COPY 800000 RECORDS INTO partsupp from 'PWD/partsupp.tbl' USING DELIMITERS '|', '|\n';
+COPY 1500000 RECORDS INTO orders from 'PWD/orders.tbl' USING DELIMITERS '|', '|\n';
+COPY 7000000 RECORDS INTO lineitem from 'PWD/lineitem.tbl' USING DELIMITERS '|', '|\n';
 COMMIT;
