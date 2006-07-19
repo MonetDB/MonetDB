0af46fc2f9cd625deb17f2de0bba2cba7fc49775
Niels Nes <niels@cwi.nl>
2006-07-19 14:12:09 +0000

    while implemented

--- sql/src/test/psm.sql
+++ sql/src/test/psm.sql
@@ -9,3 +9,13 @@ end;
 
 select my_abs(-1);
 select my_abs(1);
+
+create function my_while(v int) returns int as 
+begin
+	while v > 0 do 
+		set v = v - 10;
+	end while;
+	return v;
+end;
+
+select my_while(105);
