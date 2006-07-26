609e7a388482abca89cefcfdf8fabc0c0f04f7fd
Martin Kersten <mk@cwi.nl>
2006-07-26 07:26:23 +0000

    Paragraphs and references

--- MonetDB5/doc/Assets/grabber.sh
+++ MonetDB5/doc/Assets/grabber.sh
@@ -8,7 +8,7 @@ cp ${SQLDIR}/src/jdbc/jdbcmanual.texi .
 
 echo "make Mapi manual"
 cp ${M4DIR}/src/mapi/clients/C/Mapi.mx .
-Mx -i -B -H1 Mapi.mx Mapi.mx
+Mx -i -B -H1 Mapi.mx 
 mv Mapi.bdy.texi mapimanual.texi
 rm Mapi.mx
 
