6c23b6b045baf5b5a443e2c0495139e08db24bb9
Martin Kersten <mk@cwi.nl>
2006-07-30 13:30:23 +0000

    Intermittent commit

--- MonetDB5/doc/Assets/grabber.sh
+++ MonetDB5/doc/Assets/grabber.sh
@@ -4,8 +4,6 @@ echo "Collect manuals from the source directories "
 M4DIR=/ufs/mk/opensource/MonetDB/
 SQLDIR=/ufs/mk/opensource/sql
 
-cp ${SQLDIR}/src/jdbc/jdbcmanual.texi .
-
 echo "make Mapi manual"
 cp ${M4DIR}/src/mapi/clients/C/Mapi.mx .
 Mx -i -B -H1 Mapi.mx 
