183469492513abe19bdc1cf96bd640cf3f7ecf30
Niels Nes <niels@cwi.nl>
2006-09-10 11:40:05 +0000

    ported set coersion rules to relational version
    allow for full qualified names in function calls (todo use the
    schema/module names)

--- sql/src/test/BugTracker/Tests/set_operation_coersions.SF-1543216.sql
+++ sql/src/test/BugTracker/Tests/set_operation_coersions.SF-1543216.sql
@@ -1,14 +1,14 @@
 start transaction;
-create table t1 (id int);
-create table s1 (id varchar(23));
-select * from t1 union select * from s1;
+create table t1543216 (id int);
+create table s1543216 (id varchar(23));
+select * from t1543216 union select * from s1543216;
 
 rollback;
 
 start transaction;
 
-create table f( id float);
-create table b( id boolean);
-select * from f union select * from b;
+create table f1543216( id float);
+create table b1543216( id boolean);
+select * from f1543216 union select * from b1543216;
 
 rollback;
