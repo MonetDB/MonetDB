33d4fffc95a637d691dfb86fde175a91a6a2b6b5
Stefan Manegold <manegold@cwi.nl>
2006-09-13 13:18:08 +0000

    "MapiClient -lsql --dump" is now called "msqldump"
    "$SQL_CLIENT --dump" is now called "$SQL_DUMP"

--- sql/src/test/BugDay_2005-10-06_2.8/Tests/MapiClient-dump.SF-905851.SQL.bat
+++ sql/src/test/BugDay_2005-10-06_2.8/Tests/MapiClient-dump.SF-905851.SQL.bat
@@ -2,4 +2,4 @@
 
 call Mlog.bat -x %SQL_CLIENT% "%TSTSRCBASE%\src/jdbc/tests/JdbcClient_create_tables.sql"
 call Mlog.bat -x %SQL_CLIENT% "%TSTSRCBASE%\src/jdbc/tests/JdbcClient_insert_selects.sql"
-call Mlog.bat -x %SQL_CLIENT% --dump
+call Mlog.bat -x %SQL_DUMP%
