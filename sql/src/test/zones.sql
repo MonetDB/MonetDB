c332860824924ad4c1b44394f7e3b2ee9a2f2cb8
Milena Ivanova <milena@cwi.nl>
2006-09-30 12:08:21 +0000

    Zones test reformulated to avoid failing due to precision differences on different platforms.

--- sql/src/test/zones.sql
+++ sql/src/test/zones.sql
@@ -1,3 +1,27 @@
+declare epsilon double;
+set epsilon=0.000001;
+
+create function Alpha(theta double, decl double) returns double
+    begin
+    if abs(decl)+theta > 89.9 then return cast(180.0 as double);
+    else 
+    return(degrees(abs(atan(sin(radians(theta)) /
+                             sqrt(abs( cos(radians(decl-theta))
+                                      * cos(radians(decl+theta))
+           )       )   )   )     ));
+	end if;
+    end;
+
+create table alphatest(expected double, computed double);
+insert into alphatest values(1.1547200925865859,Alpha(cast(1.0 as double),cast(30.0 as double)));
+insert into alphatest values(1.4142854109042293,Alpha(cast(1.0 as double),cast(45.0 as double)));
+insert into alphatest values(2.0003048809350625,Alpha(cast(1.0 as double),cast(60.0 as double)));
+
+select abs(expected - computed) as err
+from alphatest 
+where abs(expected - computed)>epsilon;
+
+---------Spatial Zone Index-------------
 create table ZoneIndex (
     objID       bigint         not null, -- object Identifier in table
     zone        int         not null, -- zone number (using 10 arcminu
@@ -20,21 +44,6 @@ insert into zoneindex
 values( 687726014001184895,	1,	193.75663392621499,	1.61956881843685,	-0.97092650035194339,	-0.23770345222609343,	0.028263045025703246,	0);
 insert into zoneindex
 values( 687726014001184896,	1,	193.75664598583401,	1.6195802985663199,	-0.97092644481955426,	-0.23770365524011272,	0.028263245311723893,	0);
-
-create function Alpha(theta double, decl double) returns double
-    begin
-    if abs(decl)+theta > 89.9 then return cast(180.0 as double);
-    else 
-    return(degrees(abs(atan(sin(radians(theta)) /
-                             sqrt(abs( cos(radians(decl-theta))
-                                      * cos(radians(decl+theta))
-           )       )   )   )     ));
-	end if;
-    end;
-
-select Alpha(cast(1.0 as double),cast(30.0 as double)) as alpha1;
-select Alpha(cast(1.0 as double),cast(45.0 as double)) as alpha2;
-select Alpha(cast(1.0 as double),cast(60.0 as double)) as alpha3;
 
 ------margins
 
@@ -76,13 +85,27 @@ begin
 	);
 end;
 
-select S.objID,S.distance 
-from GetNearbyObjects(cast(1.48 as double),cast(193.75 as double),cast(1.0 as double)) as S;
-
-select S.objID,S.distance 
+select count(S.objID) 
 from GetNearbyObjects(cast(1.48 as double),cast(193.75 as double),cast(0.1 as double)) as S;
+
+create table zonetest (
+    objID    bigint,
+    expected double);
+
+insert into zonetest values( 687726014001184891,	0.00669124492169760);
+insert into zonetest values( 687726014001184892,	0.00688278877005443);
+insert into zonetest values( 687726014001184894,	0.00700190450261338);
+insert into zonetest values( 687726014001184895,	0.13972627471136584);
+insert into zonetest values( 687726014001184896,	0.13973831452158963);
+
+select S.objID,S.distance, T.expected 
+from GetNearbyObjects(cast(1.48 as double),cast(193.75 as double),cast(1.0 as double)) as S, zonetest as T
+where S.objID=T.objID and abs(S.distance-T.expected)>epsilon;
+
 
 drop function GetNearbyObjects;
 drop function Alpha;
+drop table alphatest;
+drop table zonetest;
 drop table zoneindex;
 
