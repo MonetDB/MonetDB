13e1b0a3ecb867fd59ed79080e8dbb66978676d7
Niels Nes <niels@cwi.nl>
2006-12-04 14:21:02 +0000

    lets check for leaks directly from sql

--- sql/src/test/leaks/Tests/check0.sql
+++ sql/src/test/leaks/Tests/check0.sql
@@ -1,4 +1,7 @@
-create table x  ( i int, j int );
+
+select htype, ttype, count from bbp() as bbp 
+where kind = 'pers'
+order by htype, ttype, count;
 
 select 'transient', count(*) from bbp() as bbp where kind = 'tran';
 select 'persistent', count(*) from bbp() as bbp where kind = 'pers';
