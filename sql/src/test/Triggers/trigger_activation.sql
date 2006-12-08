1520d864f64168ddfce1a3aa5bfe137c3c9f4751
Romulo Gonçalves <goncalve@cwi.nl>
2006-12-08 14:22:56 +0000

    Approve out and err output for the trigger_activation test.

--- sql/src/test/Triggers/trigger_activation.sql
+++ sql/src/test/Triggers/trigger_activation.sql
@@ -2,7 +2,6 @@
 --the semantic should also be tested after the syntax test
 
 create table t1 (id int, name varchar(1024));
-create table t2 (id int);
 
 insert into t1 values(10, 'monetdb');
 insert into t1 values(20, 'monet');
@@ -10,11 +9,11 @@ insert into t1 values(20, 'monet');
 
 create trigger test1
 	after update on t1
-	delete from t1 where name = 'mo' and id = 10;
+	delete from t1 where name = 'monetdb' and id = 10;
 
 create trigger test2
 	before update on t1
-	select id into t2 from t1 where id = 10;
+	insert into t1 values(11,'amsterdam');
 
 create trigger test3
 	after update on t1
@@ -22,15 +21,13 @@ create trigger test3
 
 create trigger test4
 	after update on t1
-	insert into t1 values(20, 'update_from_test4');
+	insert into t1 values(30, 'update_from_test4');
 
 ALTER TABLE t1 add CONSTRAINT t1_constraint PRIMARY KEY(id);
 
-update t1 set name = 'mo' where id = 10;
+update t1 set name = 'mo' where id = 11;
 
 select * from t1;
-
-select * from t2;
 
 delete from t1 where id >1;
 
@@ -43,4 +40,3 @@ alter table t1 drop constraint t1_constraint;
 
 --Cleanup
 drop table t1;
-drop table t2;
