82b04b0cd8648998d92c3d1f7a5a80f0610015bc
Stefan Manegold <manegold@cwi.nl>
2006-12-15 20:06:26 +0000

    propagated changes of Wednesday Dec 13 2006 - Friday Dec 15 2006 from the SQL_2-14 branch to the development trunk

--- sql/src/sql/bbp.sql
+++ sql/src/sql/bbp.sql
@@ -1,3 +1,3 @@
 create function bbp () 
-	returns table (id int, name string, htype int, ttype int, count BIGINT, refcnt int, lrefcnt int, location string, heat int, dirty string, status string, kind string) 
+	returns table (id int, name string, htype string, ttype string, count BIGINT, refcnt int, lrefcnt int, location string, heat int, dirty string, status string, kind string) 
 	external name sql.bbp;
