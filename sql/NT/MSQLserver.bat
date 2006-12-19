e33e52f5abe2bf92939a1127b00cbf23fb08b5a8
Romulo Gonçalves <goncalve@cwi.nl>
2006-12-19 13:36:30 +0000

    Change the bat MSQLserver to support either M4 or M5 installation.

--- sql/NT/MSQLserver.bat
+++ sql/NT/MSQLserver.bat
@@ -1,5 +1,7 @@
 @echo off
 
+setlocal
+
 rem figure out the folder name
 set MONETDB=%~dp0
 
@@ -7,4 +9,11 @@ rem remove the final backslash from the path
 set MONETDB=%MONETDB:~0,-1%
 
 rem start the real server
-"%MONETDB%\Mserver.bat" --dbinit="module(sql_server); mil_start();" %*
+
+if exist "%MONETDB%\M5server.bat" goto m5server
+"%MONETDB%\Mserver.bat" --dbinit="module(sql_server); sql_server_start();" %*
+
+:m5server
+"%MONETDB%\M5server.bat" --dbinit="include sql; sql_start();" %*
+
+endlocal
