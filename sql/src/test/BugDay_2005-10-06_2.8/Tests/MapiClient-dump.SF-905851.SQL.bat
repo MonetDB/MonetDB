9d0facac8283fbbc02ea0414e4beb1f4ce6f0f72
Niels Nes <niels@cwi.nl>
2006-12-27 19:37:16 +0000

    use mjclient

--- sql/src/test/BugDay_2005-10-06_2.8/Tests/MapiClient-dump.SF-905851.SQL.bat
+++ sql/src/test/BugDay_2005-10-06_2.8/Tests/MapiClient-dump.SF-905851.SQL.bat
@@ -1,5 +1,5 @@
 @echo off
 
-call Mlog.bat -x %SQL_CLIENT% "%TSTSRCBASE%\src/jdbc/tests/JdbcClient_create_tables.sql"
-call Mlog.bat -x %SQL_CLIENT% "%TSTSRCBASE%\src/jdbc/tests/JdbcClient_insert_selects.sql"
+call Mlog.bat -x %SQL_CLIENT% "%TSTSRCDIR%\JdbcClient_create_tables.sql"
+call Mlog.bat -x %SQL_CLIENT% "%TSTSRCDIR%\JdbcClient_insert_selects.sql"
 call Mlog.bat -x %SQL_DUMP%
