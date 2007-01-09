e33bfd924d4bf84e78827dcbe53c10c8bf93de92
Stefan Manegold <manegold@cwi.nl>
2007-01-09 08:33:49 +0000

    back-ported fix from the development trunk of MonetDB4 to the MonetDB_4-14 release branch of MonetDB:
    made test no_monetconfig.SF-925140 also work with `make check`:
    
    we need to set monet_mod_path by hand in case it differs from the builtin default

--- MonetDB/tests/BugDay_2005-10-06_4.9.3/Tests/no_monetconfig.SF-925140.bat
+++ MonetDB/tests/BugDay_2005-10-06_4.9.3/Tests/no_monetconfig.SF-925140.bat
@@ -1,3 +1,6 @@
+@echo off
+
 echo "monet_daemon=no" > %TSTTRGBASE%\config
-call Mlog.bat "echo 'loaded(); quit();' \| Mserver --config=%TSTTRGBASE%\config"
-call           echo 'loaded(); quit();'  | Mserver --config=%TSTTRGBASE%\config
+
+call Mlog.bat "echo 'loaded(); quit();' \| Mserver %setMONETDB_MOD_PATH% --config=%TSTTRGBASE%\config"
+call           echo 'loaded(); quit();'  | Mserver %setMONETDB_MOD_PATH% --config=%TSTTRGBASE%\config
