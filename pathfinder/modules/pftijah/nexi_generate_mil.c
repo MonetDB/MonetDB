e11788d8a883eb0fbe138e2b724218821c62cc2e
Henning Rode <hrode@users.sourceforge.net>
2007-01-09 14:02:07 +0000

    collection root (previously virtual root) is now always in fragment 0, which is otherwise not used. this way, we can easily shortcut contained_by operations, like we did in the non-fragmented version

--- pathfinder/modules/pftijah/nexi_generate_mil.c
+++ pathfinder/modules/pftijah/nexi_generate_mil.c
@@ -1022,10 +1022,8 @@ int SRA_to_MIL(TijahParserContext* parserCtx, int query_num, struct_RMT *txt_ret
       MILPRINTF(MILOUT, "}\n");
       MILPRINTF(MILOUT, "R%d := collect;\n",com_num);
       MILPRINTF(MILOUT, "collect := nil;\n");
-      MILPRINTF(MILOUT, "R%d := R%d.tsort_rev();\n", com_num, com_num);
-    } else {
-      MILPRINTF(MILOUT, "R%d := R%d.tsort_rev();\n", com_num, com_num);
     }
+    MILPRINTF(MILOUT, "R%d := R%d.tsort_rev();\n", com_num, com_num);
     MILPRINTF(MILOUT, "if ( retNum >= 0 ) { R%d := R%d.slice(0, retNum - 1); }\n", com_num, com_num);
     MILPRINTF(MILOUT, "R%d.persists(true).rename(\"nexi_result\");\n", com_num);
     MILPRINTF(MILOUT, "}\n");
