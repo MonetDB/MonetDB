8ae97b2e7f6a1796fa1c502ec14282701a25b659
Stefan Manegold <manegold@cwi.nl>
2007-01-12 12:44:04 +0000

    replaced "redundant" identical test scripts by a single template one that's called by all tests

---
+++ sql/src/jdbc/tests/Tests/Test.SQL.bat
@@ -0,0 +1,5 @@
+@echo off
+
+set URL="jdbc:monetdb://%HOST%:%MAPIPORT%/%TSTDB%?user=monetdb&password=monetdb%JDBC_EXTRA_ARGS%"
+
+call Mlog.bat -x "java -classpath %CLIENTS_PREFIX%\share\MonetDB\lib\monetdb-1.5-jdbc.jar:%CLIENTS_PREFIX%\share\MonetDB\Tests %TST% %URL%"
