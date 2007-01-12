5f2b85970fa3156a6323fd1482a203db483eab8f
Stefan Manegold <manegold@cwi.nl>
2007-01-12 12:25:22 +0000

    - made JDBC tests work, again, on Unix.
    - made the *.bat Windows scripts at least look a bit more like Windows ---
      cannot test them, though ...

--- sql/src/jdbc/tests/Tests/Test_Ctransaction.SQL.bat
+++ sql/src/jdbc/tests/Tests/Test_Ctransaction.SQL.bat
@@ -1,5 +1,5 @@
 @echo off
 
-URL="jdbc:monetdb://${HOST}:${MAPIPORT}/${TSTDB}?user=monetdb&password=monetdb${JDBC_EXTRA_ARGS}"
+set URL="jdbc:monetdb://%HOST%:%MAPIPORT%/%TSTDB%?user=monetdb&password=monetdb%JDBC_EXTRA_ARGS%"
 
-call Mlog.bat -x "ant -emacs -buildfile ${TSTSRCDIR}/../build.xml -Dbuilddir=${TSTBLDDIR}/../ -Djdbc_url=\"${URL}\" -Djardir=${TSTBLDDIR}/../../ ${TST}"
+call Mlog.bat -x "java -classpath %CLIENTS_PREFIX%\share\MonetDB\lib\monetdb-1.5-jdbc.jar:%CLIENTS_PREFIX%\share\MonetDB\Tests %TST% %URL%"
--- sql/src/jdbc/tests/Tests/Test_JdbcClient.SQL.bat
+++ sql/src/jdbc/tests/Tests/Test_JdbcClient.SQL.bat
@@ -1,12 +1,6 @@
 @echo off
 
-echo We don't know what the name of the JAR is here, and I don't know how
-echo to figure that out using windows...  Sorry, this test fails on Windows.
-
-set JAR=""
-if exist "%SQL_PREFIX%\share\MonetDB\lib\jdbcclient-1.3.jar"	set JAR=%SQL_PREFIX%\share\MonetDB\lib\jdbcclient-1.3.jar
-if exist "%MONETDB_PREFIX%\share\MonetDB\lib\jdbcclient-1.3.jar"	set JAR=%MONETDB_PREFIX%\share\MonetDB\lib\jdbcclient-1.3.jar
-if JAR == ""							set JAR=%TSTTRGBASE%\share\MonetDB\lib\jdbcclient-1.3.jar
+set JAR="%CLIENTS_PREFIX%\share\MonetDB\lib\jdbcclient-1.5.jar"
 
 echo user=monetdb>	.monetdb
 echo password=monetdb>>	.monetdb
