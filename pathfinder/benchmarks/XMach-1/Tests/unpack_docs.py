470adfd87eac238ced09b01a73c379f0dc0921ad
Stefan Manegold <manegold@cwi.nl>
2007-01-17 15:38:12 +0000

    aligned with other unpack_docs.py script to get it working properly.

--- pathfinder/benchmarks/XMach-1/Tests/unpack_docs.py
+++ pathfinder/benchmarks/XMach-1/Tests/unpack_docs.py
@@ -1,11 +1,22 @@
-import zipfile
+import zipfile, os
 
 archive = 'unpack_docs.zip'
+
+def mkpardir(path):
+    i = path.find('/')
+    d = ''
+    while i >= 0:
+        d = os.path.join(d, path[:i])
+        if not os.path.exists(d):
+            os.mkdir(d)
+        path = path[i + 1:]
+        i = path.find('/')
 
 z = zipfile.ZipFile(archive)
 print 'Archive:  %s' % archive
 for name in z.namelist():
     print '  inflating: %s' % name
+    mkpardir(name)
     data = z.read(name)
     f = open(name, 'wb')
     f.write(data)
