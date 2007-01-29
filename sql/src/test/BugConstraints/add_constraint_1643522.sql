140b595e4ec3dc87518ce0efee7e9954ccd37f35
Romulo Gonçalves <goncalve@cwi.nl>
2007-01-29 12:01:19 +0000

    Add tests to cover the bugs related with constraints: add_constraint_1643522 delete_primarykey_1643845 drop_column_1643542 update_primarykey_1643375

---
+++ sql/src/test/BugConstraints/add_constraint_1643522.sql
@@ -0,0 +1,10 @@
+create table t1(id int, name varchar(1024));
+alter table t1 add constraint id_p primary key(id);
+
+alter table t1 add constraint id_p primary key(id);
+
+select * from keys;
+
+drop table t1;
+
+select * from keys;
