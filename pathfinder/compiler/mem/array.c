dbcee4e776ae0b5cb2c02cb41c3bf1e3b51f0734
Jan Rittinger <jan.rittinger@uni-tuebingen.de>
2007-02-01 11:00:08 +0000

    -- Added consistency check

--- pathfinder/compiler/mem/array.c
+++ pathfinder/compiler/mem/array.c
@@ -125,6 +125,7 @@ PFarray_at (PFarray_t *a, unsigned int i)
      */
     nbytes = ((i + 1) * a->esize + ACHUNK) & (~ACHUNK + 1);
 
+    assert(a->base);
     a->base = PFrealloc (nbytes, a->base);
 
     a->bound = nbytes / a->esize;
