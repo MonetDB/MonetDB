33f09b6a23fe893653f216b8acefa23dd96ecd39
Stefan Manegold <manegold@cwi.nl>
2007-02-09 23:19:42 +0000

    propagated changes of Wednesday Feb 07 2007 - Friday Feb 09 2007 from the SQL_2-16 branch to the development trunk

---
+++ sql/src/test/BugTracker/Tests/decimal_prepare.SF-1655818.sql
@@ -0,0 +1,15 @@
+-- origin: Anders Blaagaard
+
+START TRANSACTION;
+
+create table P1655818 (
+	time timestamp not null,
+	x numeric(5,2) not null,
+	y numeric(5,2) not null
+);
+
+PREPARE insert into P1655818 (time,x,y) values (?,?,?);
+
+exec 0(timestamp '2003-01-30 18:03:35.0', 71.91, 71.98);
+
+ROLLBACK;
