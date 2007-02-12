7e7c7b5a718f69d1c89363bd348e5576cc00cf73
Niels Nes <niels@cwi.nl>
2007-02-12 07:10:56 +0000

    correct output for test

--- sql/src/test/BugTracker/Tests/crash_on_delete.SF-1639108.sql
+++ sql/src/test/BugTracker/Tests/crash_on_delete.SF-1639108.sql
@@ -1,4 +1,3 @@
-set debug=32768;
 start transaction;
 create table number (value integer primary key);
 insert into number (value) select 1;
@@ -10,10 +9,6 @@ insert into number (value) select 8+value from number;
 insert into number (value) select 16+value from number;
 insert into number (value) select 32+value from number;
 insert into number (value) select 64+value from number;
-insert into number (value) select 128+value from number;
-insert into number (value) select 256+value from number;
-insert into number (value) select 512+value from number;
-insert into number (value) select 1024+value from number;
 delete from number where value=1;
 select 'the following query causes the server to crash printing "sql_optimize.mx:1011: set2pivot: Assertion `n'' failed."';
 delete from number where exists (select true from number as factor1,number as factor2 where number.value=factor1.value*factor2.value);
