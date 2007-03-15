20c013c7261e0000b812afd9d8e6567ddc653777
Fabian Groffen <fabian@cwi.nl>
2007-03-15 13:11:23 +0000

    Fix documentation

--- clients/src/java/src/nl/cwi/monetdb/mcl/io/MCLIOException.java
+++ clients/src/java/src/nl/cwi/monetdb/mcl/io/MCLIOException.java
@@ -21,12 +21,7 @@ package nl.cwi.monetdb.mcl.io;
 import nl.cwi.monetdb.mcl.*;
 
 /**
- * An MCLInputStream allows to conveniently read sentences of the
- * associated input stream.  MCL sentences are presented on the wire as
- * sequences of bytes prefixed with their length represented in an
- * 4-byte integer value.  It may be efficient to supply a
- * BufferedInputStream to the constructor of this class to increase
- * performance.
+ * An IOException in the MCL framework.
  *
  * @author Fabian Groffen <Fabian.Groffen>
  */
