ed98c07f1ad3a5747770bb3bce4f85ab72eabf77
Niels Nes <niels@cwi.nl>
2007-03-31 20:34:04 +0000

    We implemented RANK as ROW_NUMBER which isn't correct afcourse So now added ROW_NUMBER and a new implementation for RANK.

--- sql/src/test/rank.sql
+++ sql/src/test/rank.sql
@@ -1,3 +1,8 @@
+select ROW_NUMBER() over () as foo from tables;
+select ROW_NUMBER() over (PARTITION BY schema_id) as foo, schema_id from tables;
+select ROW_NUMBER() over (PARTITION BY schema_id ORDER BY schema_id) as foo, schema_id from tables;
+select ROW_NUMBER() over (ORDER BY schema_id) as foo, schema_id from tables;
+
 select RANK() over () as foo from tables;
 select RANK() over (PARTITION BY schema_id) as foo, schema_id from tables;
 select RANK() over (PARTITION BY schema_id ORDER BY schema_id) as foo, schema_id from tables;
