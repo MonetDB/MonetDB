de6a5114626cc8c340dcba05f66d45fea01fdf10
Stefan Manegold <manegold@cwi.nl>
2007-04-18 18:03:02 +0000

    make StandOff tests run, again: Mserver needs to be started with --dbinit="module(pathfinder);"

--- pathfinder/tests/StandOff/StandOff.py
+++ pathfinder/tests/StandOff/StandOff.py
@@ -6,7 +6,7 @@ TSTDB = os.environ['TSTDB']
 MSERVER = os.environ['MSERVER'].replace('--trace','')
 TSTSRCDIR = os.environ['TSTSRCDIR']
 
-CALL = "pf --enable-standoff %s.xq | %s --set standoff=enabled --dbname=%s" % (os.path.join(TSTSRCDIR,TST),MSERVER,TSTDB)
+CALL = 'pf --enable-standoff %s.xq | %s --set standoff=enabled --dbname=%s --dbinit="module(pathfinder);"' % (os.path.join(TSTSRCDIR,TST),MSERVER,TSTDB)
 
 if os.name == "nt":
     os.system("call Mlog.bat '%s'" % CALL.replace('|','\\|'))
