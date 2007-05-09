a5f4fbcaff3d65c44397851bfd886f726b9f8008
Romulo Gonçalves <goncalve@cwi.nl>
2007-05-09 22:16:54 +0000

    Approve out and err output...

--- sql/src/test/BugTracker/Tests/set_default_value.SF-1715553.sql
+++ sql/src/test/BugTracker/Tests/set_default_value.SF-1715553.sql
@@ -1,6 +1,20 @@
-create table t1(id int, name varchar(1024), age int );
-
-alter table t1 alter id set DEFAULT RANK() OVER (PARTITION BY id, age ORDER
-BY id DESC);
-
+create table t1(id int DEFAULT 50, name varchar(1024), age int );
+select name, "default", "null" from columns where name = 'id' and table_id = (select id from tables where name = 't1');
+alter table t1 alter id set DEFAULT 30;
+select name, "default", "null" from columns where name = 'id' and table_id = (select id from tables where name = 't1');
+alter table t1 alter id DROP DEFAULT;
+select name, "default", "null" from columns where name = 'id' and table_id = (select id from tables where name = 't1');
 drop table t1;
+
+create table t1(id int, name varchar(1024), age int );
+select name, "default", "null" from columns where name = 'id' and table_id = (select id from tables where name = 't1');
+alter table t1 alter id set NULL;
+select name, "default", "null" from columns where name = 'id' and table_id = (select id from tables where name = 't1');
+drop table t1;
+
+create table t1(id int, name varchar(1024), age int );
+select name, "default", "null" from columns where name = 'id' and table_id = (select id from tables where name = 't1');
+alter table t1 alter id set NOT NULL;
+select name, "default", "null" from columns where name = 'id' and table_id = (select id from tables where name = 't1');
+drop table t1;
+
