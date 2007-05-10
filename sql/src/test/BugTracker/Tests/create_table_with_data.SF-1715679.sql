b5fea0c0e7e9ca50bc26c6dd2865e5a3747576b2
Romulo Gonçalves <goncalve@cwi.nl>
2007-05-10 09:32:46 +0000

    Modify test to not use ids. Approve the new out.

--- sql/src/test/BugTracker/Tests/create_table_with_data.SF-1715679.sql
+++ sql/src/test/BugTracker/Tests/create_table_with_data.SF-1715679.sql
@@ -1,4 +1,5 @@
-create table t1 as select id from tables order by id asc with data;
-select * from t1;
+create table t1 as select name from tables order by name asc with data;
+--create table t1 as select id from tables with data;
+select name from t1;
 
 drop table t1;
