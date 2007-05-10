dd0e16a746b293919dda80224b10d4449de2df66
Romulo Gonçalves <goncalve@cwi.nl>
2007-05-10 12:21:01 +0000

    Remove table tables which should not be used in the test examples..

--- sql/src/test/BugTracker/Tests/create_table_with_data.SF-1715679.sql
+++ sql/src/test/BugTracker/Tests/create_table_with_data.SF-1715679.sql
@@ -1,5 +1,18 @@
-create table t1 as select name from tables order by name asc with data;
---create table t1 as select id from tables with data;
-select name from t1;
+create table t2 (id int);
+
+insert into t2 values(1);
+insert into t2 values(2);
+insert into t2 values(3);
+insert into t2 values(4);
+insert into t2 values(5);
+insert into t2 values(6);
+insert into t2 values(7);
+insert into t2 values(8);
+insert into t2 values(9);
+
+
+create table t1 as select * from t2 order by id asc with data;
+select * from t1;
 
 drop table t1;
+drop table t2;
--- sql/src/test/BugTracker/Tests/temporary_tables.SF-1715669.sql
+++ sql/src/test/BugTracker/Tests/temporary_tables.SF-1715669.sql
@@ -1,4 +1,4 @@
 create TEMPORARY table t1 (id int) ON COMMIT DROP;
-select name from tables;
+select name from tables where name = 't1';
 
 drop table t1;
