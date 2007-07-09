c95c18f5ad2069d761b37ad9fd1b118f36155b98
Manuel Mayr <mayrm@users.sourceforge.net>
2007-07-09 13:22:19 +0000

    Introducing a new CSE, that is able to recognize structural equivalency, while the former CSE recognized only patterns with the exactly the same names. The vision is to substititute the former CSE with this one. For now is available with the option 'E' as its own optimization phase via the optimization string. For Mil generation its better to invoke it with 'E[]'.

---
+++ pathfinder/compiler/include/opt_algebra_cse.h
@@ -0,0 +1,43 @@
+/**
+ * @file
+ *
+ * Declarations for common subexpression elimination
+ *
+ *
+ * Copyright Notice:
+ * -----------------
+ *
+ * The contents of this file are subject to the Pathfinder Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://monetdb.cwi.nl/Legal/PathfinderLicense-1.1.html
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is the Pathfinder system.
+ *
+ * The Original Code has initially been developed by the Database &
+ * Information Systems Group at the University of Konstanz, Germany and
+ * is now maintained by the Database Systems Group at the Technische
+ * Universitaet Muenchen, Germany.  Portions created by the University of
+ * Konstanz and the Technische Universitaet Muenchen are Copyright (C)
+ * 2000-2005 University of Konstanz and (C) 2005-2007 Technische
+ * Universitaet Muenchen, respectively.  All Rights Reserved.
+ *
+ * $Id$
+ */
+
+#ifndef OPT_ALGEBRA_CSE_H
+#define OPT_ALGEBRA_CSE_H
+
+#include "logical.h"
+
+/* Recursively search the algebra tree for common subexpressions */
+PFla_op_t * PFalgopt_cse (PFla_op_t *n);
+
+#endif /* ALGEBRA_CSE_SQL_H */
+
+/* vim:set shiftwidth=4 expandtab: */
