9ae0890844d5823243642c8038d7724fa1fd05d3
Stefan Manegold <manegold@cwi.nl>
2007-08-12 18:41:43 +0000

    propagated changes of Sunday Aug 12 2007 from the XQuery_0-18 branch to the development trunk

---
+++ pathfinder/tests/BugTracker/Tests/pf-O0_produces_wrong_MIL.SF-1771532.py
@@ -0,0 +1,14 @@
+import os
+import string
+
+TST = os.environ['TST']
+TSTDB = os.environ['TSTDB']
+MSERVER = os.environ['MSERVER'].replace('--trace','')
+
+CALL = "pf -O0 %s.xq | %s --dbname=%s --dbinit=\"module(pathfinder);\"" % (TST,MSERVER,TSTDB)
+
+if os.name == "nt":
+    os.system("call Mlog.bat '%s'" % CALL.replace('|','\\|'))
+else:
+    os.system("Mlog '%s'" % CALL.replace('|','\\|'))
+os.system(CALL)
