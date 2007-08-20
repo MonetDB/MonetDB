702036e152a28f234ac10eb38d0842235cf9b4df
Martin Kersten <mk@cwi.nl>
2007-08-20 19:03:29 +0000

    The next step is to manually construct the desired code.

--- sql/src/test/xml/publish.sql
+++ sql/src/test/xml/publish.sql
@@ -12,10 +12,11 @@ from victim V;
 
 
 -- SQL/XML code snippet
---	  _98 := xml.element("victim",_14);
---	  _99 := xml.document(_14,"result",_98);
+--    _97 := xml.xml(_14); # coercion to xml value
+--	  _98 := xml.element("victim",nil:bat,nil:bat,_97);
+--    _99 := xml.str(_98); # coercion to string for export
 --    _15 := sql.resultSet(1,1,_14);
---    sql.rsColumn(_15,"sys.","result","xml",1,0,_99);
+--    sql.rsColumn(_15,"sys.","result","str",1,0,_99);
 
 select xmlelement(name "victim", 
 		xmlattributes(V.name, V.dob ) )
@@ -29,15 +30,24 @@ from victim V;
 -- SQL/XML code snippet
 --	  _96 := xml.attribute("name",_9);
 --	  _97 := xml.attribute("dob",_11);
---	  _98 := xml.element("victim",_96,_97);
---	  _99 := xml.document(_9,"document",_98);
+--	  _98 := xml.attributes(_96,_97);
+--	  _99 := xml.element("victim",nil:bat,_98,nil:bat);
+--    _00 := xml.str(_99);
 --    _12 := sql.resultSet(1,1,_9);
---    sql.rsColumn(_12,"sys","document","xml",0,0,_99);
+--    sql.rsColumn(_12,"sys","result","str",0,0,_00);
 
 select xmlelement(name "victim", 
 		xmlelement(name "name", V.name ),
 		xmlelement(name "birthday", V.dob ) )
 from victim V;
+
+--   _94:= xml.xml(_8);
+--   _95:= xml.xml(_9);
+--   _96:= xml.element("name",nil:bat,nil:bat,_94);
+--   _97:= xml.element("birthday",nil:bat,nil:bat,_95);
+--   _98:= xml.element("victim",nil:bat,nil:bat, _96, _97);
+--   _99:= xml.str(_98);
+--   sql.rsColumn(_12,"sys","result","str",0,0,_99);
 
 select xmlelement(name "victim", 
 		xmlelement(name "name", V.name ),
@@ -45,15 +55,33 @@ select xmlelement(name "victim",
 		(select hair from victims where name=V.name)))
 from victim V;
 
+-- identical to previous one after evaluatoin of subquery
+-- it may produce multiple answers though
+
 select xmlconcat(
 		xmlelement(name "name", V.name ),
 		xmlelement(name "birthday", V.dob ) )
 from victim V;
 
+-- _94 := xml.xml(_8);
+-- _95 := xml.xml(_9);
+-- _96 := xml.element("name",nil:bat,nil:bat,_94);
+-- _97 := xml.element("birthday",nil:bat,nil:bat,_95);
+-- _98 := xml.concat(_96,97);
+-- _99 := xml.str(_98);
+-- sql.rsColumn(_12,"sys","result","str",0,0,_99);
+
 select xmlelement(name "victims",
 	xmlforest( V.name, V.dob as "birthday")
 	)
 from victim V;
+-- _94 := xml.xml(_8);
+-- _95 := xml.xml(_9);
+-- _96 := xml.trunk("birthday",_95);
+-- _97 := xml.forest(_95,96);
+-- _98 := xml.element("victims",nil:bat,nil:bat,_97);
+-- _99 := xml.str(_98);
+-- sql.rsColumn(_12,"sys","result","str",0,0,_99);
 
 -- group by hair color
 select xmlelement(name "jtr",
