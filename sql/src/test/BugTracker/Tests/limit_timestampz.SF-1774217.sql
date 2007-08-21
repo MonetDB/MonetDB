1bd2cc1a1542a852ee58695811f84690e1682e5f
Niels Nes <niels@cwi.nl>
2007-08-21 22:24:47 +0000

    replaced using local char arrays of size BUFSIZ, by a single function (sql_message). Reduces stack pressure, smaller code etc.
    approved output for m5/sql

--- sql/src/test/BugTracker/Tests/limit_timestampz.SF-1774217.sql
+++ sql/src/test/BugTracker/Tests/limit_timestampz.SF-1774217.sql
@@ -1,9 +1,9 @@
-create table t1 (
+create table timestamp1 (
 nr integer auto_increment,
 tr_time timestamp(3) with time zone
 )
 ;
-insert into t1 (tr_time)
+insert into timestamp1 (tr_time)
 select *
 from
 (
@@ -19,7 +19,7 @@ select timestamp with time zone '2005-04-12 07:15-07:00' as tr_time
 ) as foo
 ;
 -- this will crash server
-select nr from t1
+select nr from timestamp1
 order by tr_time
 limit 3
 ;
