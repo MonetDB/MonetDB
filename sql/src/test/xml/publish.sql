38ae2fafe11843759781fbffa8f416c3e1c40f64
Martin Kersten <mk@cwi.nl>
2007-08-22 21:18:59 +0000

    Update to reflect the new interface.

--- sql/src/test/xml/publish.sql
+++ sql/src/test/xml/publish.sql
@@ -2,6 +2,10 @@
 -- and an indication of the code to be derived from SQL
 -- assume simple string production and bottom up construction.
 
+-- Round 2. Attempt to simplify the code based on the assumption
+-- that we have to adjust the SQL parser. Simple masking a function
+-- call is not sufficient.
+
 select xmlelement(name "victim", V.name || ' ' || V.dob ) as result
 from victim V;
 
@@ -13,7 +17,7 @@ from victim V;
 
 -- SQL/XML code snippet
 --    _97 := xml.xml(_14); # coercion to xml value
---	  _98 := xml.element("victim",nil:bat,nil:bat,_97);
+--	  _98 := xml.element("victim",_97);
 --    _99 := xml.str(_98); # coercion to string for export
 --    _15 := sql.resultSet(1,1,_14);
 --    sql.rsColumn(_15,"sys.","result","str",1,0,_99);
@@ -30,8 +34,8 @@ from victim V;
 -- SQL/XML code snippet
 --	  _96 := xml.attribute("name",_9);
 --	  _97 := xml.attribute("dob",_11);
---	  _98 := xml.attributes(_96,_97);
---	  _99 := xml.element("victim",nil:bat,_98,nil:bat);
+--    _98 := xml.concat(_96,_97);
+--	  _99 := xml.element("victim",_98,nil:bat);
 --    _00 := xml.str(_99);
 --    _12 := sql.resultSet(1,1,_9);
 --    sql.rsColumn(_12,"sys","result","str",0,0,_00);
@@ -43,9 +47,10 @@ from victim V;
 
 --   _94:= xml.xml(_8);
 --   _95:= xml.xml(_9);
---   _96:= xml.element("name",nil:bat,nil:bat,_94);
---   _97:= xml.element("birthday",nil:bat,nil:bat,_95);
---   _98:= xml.element("victim",nil:bat,nil:bat, _96, _97);
+--   _96:= xml.element("name",_94);
+--   _97:= xml.element("birthday",_95);
+--   _77:= xml.concat(_96,_97);
+--   _98:= xml.element("victim",_77);
 --   _99:= xml.str(_98);
 --   sql.rsColumn(_12,"sys","result","str",0,0,_99);
 
@@ -65,8 +70,8 @@ from victim V;
 
 -- _94 := xml.xml(_8);
 -- _95 := xml.xml(_9);
--- _96 := xml.element("name",nil:bat,nil:bat,_94);
--- _97 := xml.element("birthday",nil:bat,nil:bat,_95);
+-- _96 := xml.element("name",_94);
+-- _97 := xml.element("birthday",_95);
 -- _98 := xml.concat(_96,97);
 -- _99 := xml.str(_98);
 -- sql.rsColumn(_12,"sys","result","str",0,0,_99);
@@ -77,9 +82,9 @@ select xmlelement(name "victims",
 from victim V;
 -- _94 := xml.xml(_8);
 -- _95 := xml.xml(_9);
--- _96 := xml.trunk("birthday",_95);
--- _97 := xml.forest(_95,96);
--- _98 := xml.element("victims",nil:bat,nil:bat,_97);
+-- _96 := xml.element("birthday",_95);
+-- _97 := xml.concat(_95,96);
+-- _98 := xml.element("victims",_97);
 -- _99 := xml.str(_98);
 -- sql.rsColumn(_12,"sys","result","str",0,0,_99);
 
