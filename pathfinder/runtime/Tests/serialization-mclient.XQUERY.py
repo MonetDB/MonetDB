fa390a27990634ccfdae08e491bd717a21174dd9
Stefan Manegold <manegold@cwi.nl>
2007-09-01 18:23:36 +0000

    Added serialization test using  `mclient -lx -o<mode>` with <mode> from {"xml", "xml-typed", "xml-noheader", "xml-noroot", "xml-root-name", "dm", "seq", "text"}.
    This test documents the current functionality of mclient's "-o" option
    before it might/will be replaced/superseded by the new "-f" option.

---
+++ pathfinder/runtime/Tests/serialization-mclient.XQUERY.py
@@ -0,0 +1,33 @@
+import os
+import sys
+
+XQUERY_CLIENT = os.environ['XQUERY_CLIENT']
+query = os.path.join(os.environ['TSTSRCDIR'],'serialization-query.xq')
+query_no_attr = os.path.join(os.environ['TSTSRCDIR'],'serialization-query_no_attr.xq')
+
+for mode in ['xml', 'xml-typed', 'xml-noheader', 'xml-noroot', 'xml-root-name', 'dm', 'seq', 'text']:
+    sys.stdout.write('\nmode="%s"\n' % mode)
+    sys.stderr.write('\nmode="%s"\n' % mode)
+    sys.stdout.flush()
+    sys.stderr.flush()
+    os.system('%s -o%s %s' % (XQUERY_CLIENT, mode, query))
+    sys.stdout.flush()
+    sys.stderr.flush()
+
+mode='text'
+sys.stdout.write('\nmode="%s" (w/o top-level attribute)\n' % mode)
+sys.stderr.write('\nmode="%s" (w/o top-level attribute)\n' % mode)
+sys.stdout.flush()
+sys.stderr.flush()
+os.system('%s -o%s %s' % (XQUERY_CLIENT, mode, query_no_attr))
+sys.stdout.flush()
+sys.stderr.flush()
+
+sys.stdout.write('\nmode="" (default)\n')
+sys.stderr.write('\nmode="" (default)\n')
+sys.stdout.flush()
+sys.stderr.flush()
+os.system('%s %s' % (XQUERY_CLIENT, query))
+sys.stdout.flush()
+sys.stderr.flush()
+
---
+++ pathfinder/runtime/Tests/serialization-query_no_attr.xq
@@ -0,0 +1,1 @@
+(1, 2.3, 3E1, "string", doc("nodekinds.xml")/descendant-or-self::node())
