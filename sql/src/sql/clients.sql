2821a428723f7211b0a1ee5d2429fee9ecab8ada
Niels Nes <niels@cwi.nl>
2007-09-10 07:57:00 +0000

    now it does work. The sql should match the mal implementation.

--- sql/src/sql/clients.sql
+++ sql/src/sql/clients.sql
@@ -1,4 +1,4 @@
 create function clients () 
-	returns table (id int, name string, login string, lastcmd string,
+	returns table ("user" string, login string, lastcommand string,
 		 actions int, seconds BIGINT) 
 	external name sql.clients;
