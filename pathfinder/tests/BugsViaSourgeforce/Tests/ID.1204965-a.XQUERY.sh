11d26e6836451d5fd25201389badcbdade947f0c
Sjoerd Mullender <sjoerd@acm.org>
2007-09-12 07:37:44 +0000

    Coalesced mclient's -o and -f options into a single -f (output format) option.
    The -o option was used to specify output formatting for XQuery,
    whereas -f was used to specify output formatting for the other
    languages.  Now the same option is used for both (similar) functions.
    
    This also frees up -o for a future, possible -o output file option.

--- pathfinder/tests/BugsViaSourgeforce/Tests/ID.1204965-a.XQUERY.sh
+++ pathfinder/tests/BugsViaSourgeforce/Tests/ID.1204965-a.XQUERY.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 sed 's|<TSTSRCDIR>|'$TSTSRCDIR'|' $TSTSRCDIR/ID.1204965-a.xq > $TSTTRGDIR/ID.1204965-a.xquery
-Mlog "$XQUERY_CLIENT -lxquery -oxml $TSTTRGDIR/ID.1204965-a.xquery"
-      $XQUERY_CLIENT -lxquery -oxml $TSTTRGDIR/ID.1204965-a.xquery
+Mlog "$XQUERY_CLIENT -lxquery -fxml $TSTTRGDIR/ID.1204965-a.xquery"
+      $XQUERY_CLIENT -lxquery -fxml $TSTTRGDIR/ID.1204965-a.xquery
