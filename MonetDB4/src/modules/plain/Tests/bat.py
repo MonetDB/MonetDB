da1b861acf85968294fab3675984726bea709c6b
Stefan Manegold <manegold@cwi.nl>
2007-09-12 08:38:51 +0000

    (more) Mtest.py cleaning: united extensions ".milM" & ".milS" into ".milS" (they have been treated equally for a long time)

--- MonetDB4/src/modules/plain/Tests/bat.py
+++ MonetDB4/src/modules/plain/Tests/bat.py
@@ -2,4 +2,4 @@ import os
 MSERVER =  os.environ['MSERVER']
 TSTDB = os.environ['TSTDB']
 TST = os.environ['TST']
-os.system("%s --dbname=%s --set gdk_mem_pagebits=16 < %s.milM" % (MSERVER, TSTDB, TST))
+os.system("%s --dbname=%s --set gdk_mem_pagebits=16 < %s.milS" % (MSERVER, TSTDB, TST))
