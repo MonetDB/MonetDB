99b0bf89f153f9c99d65621ecc01e91df222b10e
Peter Boncz <boncz@cwi.nl>
2007-09-18 08:11:04 +0000

    eliminate superfluous tests

--- pathfinder/tests/BugsViaSourgeforce/Tests/ID.1600704-a.XQUERY.sh
+++ pathfinder/tests/BugsViaSourgeforce/Tests/ID.1600704-a.XQUERY.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 sed 's|<TSTSRCDIR>|'$TSTSRCDIR'|' $TSTSRCDIR/ID.1600704-a.xq > $TSTTRGDIR/ID.1600704-a.xquery
-Mlog "$XQUERY_CLIENT -lxquery -fxml $TSTTRGDIR/ID.1600704-a.xquery"
-      $XQUERY_CLIENT -lxquery -fxml $TSTTRGDIR/ID.1600704-a.xquery
+Mlog "$XQUERY_CLIENT $TSTTRGDIR/ID.1600704-a.xquery"
+      $XQUERY_CLIENT $TSTTRGDIR/ID.1600704-a.xquery
