debd11c5d80648b2dde394d0e0e6bc46cbcd80ad
Peter Boncz <boncz@cwi.nl>
2007-09-18 08:11:50 +0000

    eliminate superfluous switches

--- pathfinder/tests/BugsViaSourgeforce/Tests/ID.1600704-b.XQUERY.sh
+++ pathfinder/tests/BugsViaSourgeforce/Tests/ID.1600704-b.XQUERY.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 sed 's|<TSTSRCDIR>|'$TSTSRCDIR'|' $TSTSRCDIR/ID.1600704-b.xq > $TSTTRGDIR/ID.1600704-b.xquery
-Mlog "$XQUERY_CLIENT -lxquery -fxml $TSTTRGDIR/ID.1600704-b.xquery"
-      $XQUERY_CLIENT -lxquery -fxml $TSTTRGDIR/ID.1600704-b.xquery
+Mlog "$XQUERY_CLIENT $TSTTRGDIR/ID.1600704-b.xquery"
+      $XQUERY_CLIENT $TSTTRGDIR/ID.1600704-b.xquery
