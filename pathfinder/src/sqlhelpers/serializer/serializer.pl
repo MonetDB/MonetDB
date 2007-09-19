ccdf8486fe366f10261375c09db4dff4344ab5ac
Manuel Mayr <mayrm@users.sourceforge.net>
2007-09-19 14:55:40 +0000

    The serializer doesn't rely on the correct position within the last query any more. Refactoring ...

--- pathfinder/src/sqlhelpers/serializer/serializer.pl
+++ pathfinder/src/sqlhelpers/serializer/serializer.pl
@@ -16,24 +16,120 @@
 # All Rights Reserved.
 
 use strict;
+use warnings;
+use Getopt::Long; # support options+arguments
+use Pod::Usage;
 
 # we require Perl's database interfaces
 use DBI;
 
-# change `XML' to your database name, or the entire string to
-# match something other than DB2
-my $DSN = 'DBI:DB2:XML';
+######### DBI wrappers ###########
 
-my $input_query = "";    # SQL code as received from PF
-my %schema;              # schema information we got from PF
+# connect to database
+sub db_connect {
+   my $dbh = shift;
+   my $dns = shift;
+
+   # connect to the database
+   # use available connections if available
+   $$dbh = DBI->connect_cached ($dns)
+       or die "Could not connect to database with DSN `$dns': " . DBI->errstr;
+}
+
+# prepare statement 
+sub stmt_prepare {
+   my $dbh = shift;
+   my $stmt = shift;
+   my $final_query = shift;
+
+   # prepare statement 
+   $$stmt = $dbh->prepare ($final_query)
+       or die "Could not prepare query :" . DBI->errstr;
+}
+
+# execute statement wrapper 
+sub stmt_execute {
+    my $stmt = shift; 
+    $stmt->execute () or 
+      die "Execute not successful: " . DBI->errstr ;
+}
+
+# bind the columns to the corresponding map 
+sub build_result_map { 
+
+    my $schema     = shift;
+    my $result_map = {};
+    my $querytype;
+  
+
+    $querytype = $schema->{'Type'};
+
+    # check if query type is known
+    die "Query type unknown"
+        unless (($querytype eq 'ATOMIC_AND_NODES')
+                || ($querytype eq 'ATOMIC_ONLY')
+                || ($querytype eq 'NODES_ONLY'));
+
+    # if query contains atomic values
+    # collect them
+    if (($querytype eq 'ATOMIC_AND_NODES')
+        || ($querytype eq 'ATOMIC_ONLY')) {
+        foreach my $t ('int', 'str', 'dbl', 'dec') {
+            if (defined $schema->{"item_$t"}) {
+                $result_map->{"item_$t"} = $schema->{"item_$t"};
+            }
+        }
+    }
+
+    # if query contain nodes we have even to bind
+    # the additional fields pre, size, etc
+    if (($querytype eq 'NODES_ONLY')
+        ||($querytype eq 'ATOMIC_AND_NODES')) {
+        $result_map->{"pre"}   = $schema->{"pre"}; 
+        $result_map->{"size"}  = $schema->{"size"};
+        $result_map->{"kind"}  = $schema->{"kind"};
+        $result_map->{"value"} = $schema->{"value"};
+        $result_map->{"name"}  = $schema->{"name"};
+        $result_map->{"tag"}   = $schema->{"tag"};
+    }
+
+    return $result_map;
+}
+
+# bind the columns
+sub bind_result_columns {
+     my $sth = shift;
+     my $RESULT_MAP = shift;
+
+     my $bound_fields = {};
+                 
+     while (my ($node_field, $db_col) = each %$RESULT_MAP) {
+          $db_col = lc $db_col;
+          if (defined $sth->{NAME_lc_hash}{$db_col}) {
+              $sth->bind_col(
+                  $sth->{NAME_lc_hash}{$db_col} + 1,
+                  \$bound_fields->{$node_field}
+              );
+          }
+          else {
+              die "DB column $db_col not returned from your SQL query!\n";
+          }
+     }
+     return $bound_fields;
+}
+
+############################### Utils #####################################
 
 # read query from stdin or a file
 sub read_query {
+    my $schema = shift;
+    my $final_query = shift;
 
     my $state = 'START';
 
     while (<>) {
         chomp;
+        # fetch the line
         my $line = $_;
 
         # start looking for schema information as soon as we
@@ -55,198 +151,200 @@ sub read_query {
         # in the Perl hash $schema (with key <name> and value <val>).
         #
         elsif ($state eq 'IN_SCHEMA_INFORMATION') {
-
             # these are the strings we look for
             if ($line =~ /\b(Type|
-                             result-item_int|
-                             result-item_str|
-                             result-item_dec|
-                             result-item_dbl):\s+
+                             Column\s+\((pre|size|kind|
+                                 value|name|tag|item_(int|dec|dbl|str))\)):\s+
                              \b(\w+)\b/x) {
-                $schema{$1} = $2;
-            }
 
+
+                if ($1 eq 'Type') {
+                    $schema->{$1} = $4;
+                }
+                else {
+                    $schema->{$2} = $4;
+                }
+            }
             elsif ($line eq '-- !! END SCHEMA INFORMATION ** DO NOT EDIT THESE LINES !!') {
                 $state = 'DONE_SCHEMA_INFORMATION';
             }
         }
-
         # simply collect all the other lines into the SQL query
         else {
-            $input_query .= "$line\n";
+            $$final_query .= "$line\n";
         }
     }
 
     die "No schema information found in query.\n"
         unless $state eq 'DONE_SCHEMA_INFORMATION';
 
-#    die "Schema information on document relation incomplete.\n"
-#        unless ($schema{'document-pre'}
-#                && $schema{'document-size'} && $schema{'document-kind'}
-#                && $schema{'document-value'} && $schema{'document-tag'})
-#                && $schema{'document-name'};
-
     # if Type is NODES_ONLY then only Type has to be defined
     # if Type is ATOM_ONLY or ATOMIC_AND_NODES one of the
     # result items has to be defined
+    # FIXME check pre, size ... 
     die "Schema information on result relation incomplete.\n"
-        unless (defined $schema{'Type'}
-                && (($schema{'Type'} eq 'NODES_ONLY')
-                    || ($schema{'Type'} eq 'ATOMIC_ONLY'
-                       || $schema{'Type'} eq 'ATOMIC_AND_NODES')
-                         && (defined $schema{'result-item_int'}
-                            || defined $schema{'result-item_str'}
-                            || defined $schema{'result-item_dec'}
-                            || defined $schema{'result-item_dbl'})))
+        unless (defined $schema->{'Type'}
+                && ($schema->{'Type'} eq 'ATOMIC_ONLY'
+                    || ((($schema->{'Type'} eq 'NODES_ONLY') ||
+                    ($schema->{'Type'} eq 'ATOMIC_AND_NODES'))
+                    && (defined $schema->{'pre'}  
+                        && defined $schema->{'size'}
+                        && defined $schema->{'kind'}
+                        && defined $schema->{'value'}
+                        && defined $schema->{'name'}))
+                 && ($schema->{'Type'} eq 'NODES_ONLY'
+                    || (($schema->{'Type'} eq 'ATOMIC_ONLY'
+                    || $schema->{'Type'} eq 'ATOMIC_AND_NODES')
+                    && (defined $schema->{'item_int'}
+                        || defined $schema->{'item_dec'}
+                        || defined $schema->{'item_dbl'} 
+                        || defined $schema->{'item_str'})))));
 }
+
+
+# print the xml-header
+sub print_xml_header {
+    # print XML header for result
+    print "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+    # print a root node to make a valid XQuery document
+    print "\n<XQuery>";
+}
+
+# print the xml-footer
+sub print_xml_footer {
+    print "\n</XQuery>";
+}
+
+sub print_xml {
+    my $stmt = shift;
+    my $row  = shift;
+    my $query_type = shift;
+
+    my @stack = ();       # stack to print all the closing tags
+    my $last = 'ELEM';    # assume the last thing we printed was an element
+
+    while ($stmt->fetch ()) {
+        # is it a node that we currently print?
+        if ((($query_type eq 'NODES_ONLY')
+            || ($query_type eq 'ATOMIC_AND_NODES')) && (defined $row->{'pre'})) {
+
+            # print trailing > to close an element
+            if (($last eq 'ATTR') && ($row->{'kind'} != 2)) {
+                print '>';
+                $last = 'ELEM';
+            }
+                
+
+            if ($row->{'kind'} == 1) {           # kind == elem
+                # newline between adjacent result elements
+                if ($#stack < 0 && $last eq 'ELEM') {
+                    print "\n";
+                }
+
+                # remove trailing spaces as they are returned by DB2
+                $row->{'tag'} =~ s/ +$//;
+
+                print '<'.$row->{'tag'} . '';    # print .tag
+                push @stack, [ $row->{'pre'},   # pre
+                               $row->{'size'},  # size
+                               $row->{'tag'}];   # tag
+                $last = 'ATTR';
+            }
+            elsif ($row->{'kind'} == 2) {         #kind == attr
+                print " ".$row->{'name'}."=\"".$row->{'value'}."\"";
+                $last = 'ATTR';
+            }
+            elsif ($row->{'kind'} == 3) {        # kind == text
+                # remove trailing spaces as they are returned by DB2
+                $row->{'value'} =~ s/ +$//;
+                print $row->{'value'};            # print doc.prop
+                $last = 'NODE';
+            }
+            elsif ($row->{'kind'} == 4) {           # kind == comm
+                # remove trailing spaces as they are returned by DB2
+                $row->{'value'} =~ s/ +$//;
+                print '<!--'.$row->{'value'} . '-->';  # print doc.prop
+                $last = 'NODE';
+            }
+            elsif ($row->{'kind'} == 5) {           # kind == pi
+                # remove trailing spaces as they are returned by DB2
+                $row->{'value'} =~ s/ +$//;
+                print '<?'.$row->{'value'}.'?>';  # print doc.prop
+                $last = 'NODE';
+            }
+
+            # print closing tags if necessary
+            while ($#stack >= 0
+                    && ($stack[-1][0] + $stack[-1][1]) <= $row->{'pre'}) {
+               if ($last eq 'ATTR') { print '>'; $last = 'ELEM'; next; }
+               print "</".$stack[-1][2].">";
+               pop @stack;
+               $last = 'ELEM'
+            }
+        }
+        else {
+
+            # space between adjacent atoms
+            if ($last eq 'ATOM') {
+                print ' ';
+            }
+
+            foreach my $t ('int', 'str', 'dbl', 'dec') {
+
+                if (defined $row->{"item_$t"} && defined $row->{"item_$t"}) {
+                    print $row->{"item_$t"};
+                    $last = 'ATOM';
+                }
+            }
+        }
+    }
+}
+
+#################################################################
+#                            Main                               # 
+#################################################################
+
+# Database handle
+my $dbh; 
+# Statement handle
+my $stmt;
+# query with atoms only, or are there nodes also?
+my $query_type;
+
+my $result_map;
+
+# change `XML' to your database name, or the entire string to
+# match something other than DB2
+my $DSN = 'DBI:DB2:XML';
+
+my $final_query;
+# schema information we got from PF
+my $schema = {};
 
 # read user input
-&read_query;
+read_query ($schema, \$final_query);
 
-my $query_type;   # query with atoms only, or are there nodes also?
-my %idx;          # in which of the return columns can we find all of
-                  # the values we are interested in?
+die "Query Type not defined"
+    unless (defined $schema->{'Type'});
 
-# set up the query we want to send to the database
-if (defined $schema{'Type'}
-    && $schema{'Type'} eq 'ATOMIC_AND_NODES') {
+$query_type = $schema->{'Type'};
 
-    # This query contains atomic values AND nodes
-    $query_type = 'ATOMIC_AND_NODES';
-
-    my $cur = 0;
-
-    foreach my $t ('int', 'str', 'dbl', 'dec') {
-
-        if (defined $schema{"result-item_$t"}) {
-            $idx{$t} = $cur;
-            $cur++;
-        }
-    }
-
-    $idx{'pre'} = $cur;
-}
-elsif (defined $schema{'Type'}
-       && $schema{'Type'} eq 'NODES_ONLY') {
-
-    # This query returns nodes, only
-    $query_type = 'NODES_ONLY';
-
-    $idx{'pre'} = 0; 
-}
-else {
-    # This query returns atomic values, only
-    $query_type = 'ATOMIC_ONLY';
-
-    my $cur = 0;
-
-    foreach my $t ('int', 'str', 'dbl', 'dec') {
-        if (defined $schema{"result-item_$t"}) {
-            $idx{$t} = $cur;
-            $cur++;
-        }
-    }
-}
-
-my $final_query = "";    # actual query sent to DBMS
-$final_query = "$input_query";
 #print $final_query;
 
-# connect to the database
-my $dbh = DBI->connect_cached ($DSN)
-    or die "Could not connect to database with DSN `$DSN': " . DBI->errstr;
 
-# prepare and execute the query
-my $sth = $dbh->prepare ($final_query)
-    or die "Could not prepare query: " . DBI->errstr;
-$sth->execute () or die "Could not execute query: " . DBI->errstr;
+# connect to the datase 
+db_connect (\$dbh, $DSN);
 
-# print XML header for result
-print "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-# print a root node to make a valid XQuery document
-print "\n<XQuery>";
+stmt_prepare ($dbh, \$stmt, $final_query);
+stmt_execute ($stmt);
 
-my @stack = ();       # stack to print all the closing tags
-my $last = 'ELEM';    # assume the last thing we printed was an element
+$result_map = build_result_map ($schema);
+my $row = bind_result_columns ($stmt, $result_map);
 
-while (my @data = $sth->fetchrow_array ()) {
+print_xml_header ();
+print_xml ($stmt, $row, $query_type);
+print_xml_footer ();
 
-    # is it a node that we currently print?
-    if ($query_type eq 'NODES_ONLY'
-        || ($query_type eq 'ATOMIC_AND_NODES' && defined $data[$idx{'pre'}])) {
-
-        # print trailing > to close an element
-        if (($last eq 'ATTR') && ($data[$idx{'pre'} + 2] != 2)) {
-            print '>';
-            $last = 'ELEM';
-        }
-            
-
-        if ($data[$idx{'pre'} + 2] == 1) {           # kind == elem
-
-            # newline between adjacent result elements
-            if ($#stack < 0 && $last eq 'ELEM') {
-                print "\n";
-            }
-
-            # remove trailing spaces as they are returned by DB2
-            $data[$idx{'pre'} + 5] =~ s/ +$//;
-
-            print '<'.$data[$idx{'pre'} + 5].'';    # print doc.tag
-            push @stack, [ $data[$idx{'pre'}],       # pre
-                           $data[$idx{'pre'} + 1],   # size
-                           $data[$idx{'pre'} + 5] ]; # tag
-            $last = 'ATTR';
-        }
-        elsif ($data[$idx{'pre'} + 2] == 2) {        #kind == attr
-            print " ".$data[$idx{'pre'} + 4]."=\"".$data[$idx{'pre'} + 3]."\"";
-            $last = 'ATTR';
-        }
-        elsif ($data[$idx{'pre'} + 2] == 3) {        # kind == text
-            # remove trailing spaces as they are returned by DB2
-            $data[$idx{'pre'} + 3] =~ s/ +$//;
-            print $data[$idx{'pre'} + 3];            # print doc.prop
-            $last = 'NODE';
-        }
-        elsif ($data[$idx{'pre'} + 2] == 4) {           # kind == comm
-            # remove trailing spaces as they are returned by DB2
-            $data[$idx{'pre'} + 3] =~ s/ +$//;
-            print '<!--'.$data[$idx{'pre'} + 3].'-->';  # print doc.prop
-            $last = 'NODE';
-        }
-        elsif ($data[$idx{'pre'} + 2] == 5) {           # kind == pi
-            # remove trailing spaces as they are returned by DB2
-            $data[$idx{'pre'} + 3] =~ s/ +$//;
-            print '<?'.$data[$idx{'pre'} + 3].'?>';  # print doc.prop
-            $last = 'NODE';
-        }
-
-        # print closing tags if necessary
-        while ($#stack >= 0
-                && ($stack[-1][0] + $stack[-1][1]) <= $data[$idx{'pre'}]) {
-           if ($last eq 'ATTR') { print '>'; $last = 'ELEM'; next; }
-           print "</".$stack[-1][2].">";
-           pop @stack;
-           $last = 'ELEM'
-        }
-    }
-    else {
-
-        # space between adjacent atoms
-        if ($last eq 'ATOM') {
-            print ' ';
-        }
-
-        foreach my $t ('int', 'str', 'dbl', 'dec') {
-
-            if (defined $idx{$t} && defined $data[$idx{$t}]) {
-                print $data[$idx{$t}];
-                $last = 'ATOM';
-            }
-        }
-    }
-}
-print "\n</XQuery>";
 print "\n";
 # close database connection
 $dbh->disconnect;
