36ae8f01f02681f12bc033aa17b0411372a6e9a2
Niels Nes <niels@cwi.nl>
2007-10-03 07:36:46 +0000

    test for the jdbc debug,trace  profile problem

---
+++ sql/src/test/BugTracker/Tests/jdbc_no_debug.SF-1739356.SQL.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat << EOF > .monetdb
+user=monetdb
+password=monetdb
+EOF
+
+LANG="en_US.UTF-8"
+export LANG
+
+Mlog   "$MTIMEOUT mjclient -h $HOST -p $MAPIPORT -d ${TSTDB} -f $RELSRCDIR/jdbc_no_debug.SF-1739356-data.sql"
+	$MTIMEOUT mjclient -h $HOST -p $MAPIPORT -d ${TSTDB} -f $RELSRCDIR/jdbc_no_debug.SF-1739356-data.sql 
+
+rm -f .monetdb
