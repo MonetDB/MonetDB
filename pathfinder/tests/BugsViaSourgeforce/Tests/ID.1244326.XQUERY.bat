a0bce8421a4f78976620b3531ad86f37522da7e4
Stefan Manegold <manegold@cwi.nl>
2007-10-05 11:36:40 +0000

    added Windows version --- still needs to be tested/checked!

---
+++ pathfinder/tests/BugsViaSourgeforce/Tests/ID.1244326.XQUERY.bat
@@ -0,0 +1,31 @@
+@echo off
+
+call Mlog.bat -x %XQUERY_CLIENT% ID.1244326.1.xq
+call Mlog.bat -x %XQUERY_CLIENT% ID.1244326.2.xq
+
+call Mlog.bat -x %XQUERY_CLIENT% ID.1244326.1.xq
+call Mlog.bat -x %XQUERY_CLIENT% ID.1244326.2.xq
+
+call Mlog.bat -x %XQUERY_CLIENT% ID.1244326.1.xq
+call Mlog.bat -x %XQUERY_CLIENT% ID.1244326.2.xq
+
+call Mlog.bat -x %XQUERY_CLIENT% ID.1244326.1.xq
+call Mlog.bat -x %XQUERY_CLIENT% ID.1244326.2.xq
+
+call Mlog.bat -x %XQUERY_CLIENT% ID.1244326.1.xq
+call Mlog.bat -x %XQUERY_CLIENT% ID.1244326.2.xq
+
+call Mlog.bat -x %XQUERY_CLIENT% ID.1244326.1.xq
+call Mlog.bat -x %XQUERY_CLIENT% ID.1244326.2.xq
+
+call Mlog.bat -x %XQUERY_CLIENT% ID.1244326.1.xq
+call Mlog.bat -x %XQUERY_CLIENT% ID.1244326.2.xq
+
+call Mlog.bat -x %XQUERY_CLIENT% ID.1244326.1.xq
+call Mlog.bat -x %XQUERY_CLIENT% ID.1244326.2.xq
+
+call Mlog.bat -x %XQUERY_CLIENT% ID.1244326.1.xq
+call Mlog.bat -x %XQUERY_CLIENT% ID.1244326.2.xq
+
+call Mlog.bat -x %XQUERY_CLIENT% ID.1244326.1.xq
+call Mlog.bat -x %XQUERY_CLIENT% ID.1244326.2.xq
