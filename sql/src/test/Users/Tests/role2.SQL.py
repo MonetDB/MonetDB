62cada300d7513dd6e425728689163dfb03553b0
Romulo Gonçalves <goncalve@cwi.nl>
2007-10-12 09:27:56 +0000

    On windows the stdout and stderr of a python subprocess is not automatically redirect to main process stdout and stderr. The proper way is to use Popen and then use a PIPE to read the stderr and stdout from the subprocess.
    NOTE: This script was tested on windows and Linux, and the test became green on both platforms.   ;)

--- sql/src/test/Users/Tests/role2.SQL.py
+++ sql/src/test/Users/Tests/role2.SQL.py
@@ -1,9 +1,16 @@
-import os, sys
+import os, sys, subprocess
 
+
+def client(cmd):
+	clt = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	sys.stdout.write(clt.stdout.read())
+	clt.stdout.close()
+	sys.stderr.write(clt.stderr.read())
+	clt.stderr.close()
+	
 
 def main():
 	clcmd = str(os.getenv('SQL_CLIENT')) + "-umy_user2 -Pp2 < %s" % ('%s/../role.sql' % os.getenv('RELSRCDIR'))
-	clt = os.popen(clcmd, 'w')
-	clt.close() 
+	client(clcmd)
 
 main()
