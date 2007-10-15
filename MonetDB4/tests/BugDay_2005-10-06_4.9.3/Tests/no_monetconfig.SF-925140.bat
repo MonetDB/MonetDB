fcc1f35097985d1fca4bbe1d2502930433550b68
Sjoerd Mullender <sjoerd@acm.org>
2007-10-15 15:24:09 +0000

    propagated changes of Friday Oct 12 2007 - Monday Oct 15 2007 from the MonetDB_4-20 branch to the development trunk

--- MonetDB4/tests/BugDay_2005-10-06_4.9.3/Tests/no_monetconfig.SF-925140.bat
+++ MonetDB4/tests/BugDay_2005-10-06_4.9.3/Tests/no_monetconfig.SF-925140.bat
@@ -2,5 +2,5 @@
 
 echo monet_daemon=no > "%TSTTRGBASE%\config"
 
-call Mlog.bat echo "loaded(); quit();" ^| Mserver %setMONETDB_MOD_PATH% "--config=%TSTTRGBASE%\config"
-call          echo  loaded(); quit();   | Mserver %setMONETDB_MOD_PATH% "--config=%TSTTRGBASE%\config"
+call Mlog.bat echo "loaded(); quit();" ^| Mserver %setGDK_DBFARM% %setMONETDB_MOD_PATH% "--config=%TSTTRGBASE%\config"
+call          echo  loaded(); quit();   | Mserver %setGDK_DBFARM% %setMONETDB_MOD_PATH% "--config=%TSTTRGBASE%\config"
