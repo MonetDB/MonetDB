2c634bc8beb0a8a7ed70b0a603ebeb391e99d403
Niels Nes <niels@cwi.nl>
2007-10-22 19:58:10 +0000

    added test for php (small issue remains, we need to call php sofar we do search for php and use it)

---
+++ sql/src/test/mapi/Tests/php_monetdb.SQL.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+path="`monetdb-clients-config --pkglibdir`/Tests"
+dir="`monetdb-clients-config --phpextensiondir`"
+
+Mlog -x php -n -d extension_dir=$dir -f $path/sqlsample.php $MAPIPORT 
