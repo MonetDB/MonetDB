4c6d9c89e3bdc9777327eaf2e1f3c5147661d832
Sjoerd Mullender <sjoerd@acm.org>
2007-10-24 13:59:12 +0000

    Fix quoting for Windows batch scripts.  Now the tests all work!

--- sql/src/jdbc/tests/Tests/Test_JdbcClient.SQL.bat
+++ sql/src/jdbc/tests/Tests/Test_JdbcClient.SQL.bat
@@ -1,16 +1,16 @@
 @echo off
 
-set JAR="%CLIENTS_PREFIX%\share\MonetDB\lib\jdbcclient.jar"
+set JAR=%CLIENTS_PREFIX%\share\MonetDB\lib\jdbcclient.jar
 
 echo user=monetdb>	.monetdb
 echo password=monetdb>>	.monetdb
 
 set LANG=en_US.UTF-8
 
-call Mlog.bat -x java -jar "%JAR%" -h %HOST% -p %MAPIPORT% -d %TSTDB% -f "%RELSRCDIR%\..\JdbcClient_create_tables.sql"
+call Mlog.bat -x java -jar "%JAR%" -h %HOST% -p %MAPIPORT% -d "%TSTDB%" -f "%RELSRCDIR%\..\JdbcClient_create_tables.sql"
 
-call Mlog.bat -x java -jar "%JAR%" -h %HOST% -p %MAPIPORT% -d %TSTDB% -f "%RELSRCDIR%\..\JdbcClient_inserts_selects.sql"
+call Mlog.bat -x java -jar "%JAR%" -h %HOST% -p %MAPIPORT% -d "%TSTDB%" -f "%RELSRCDIR%\..\JdbcClient_inserts_selects.sql"
 
-call Mlog.bat -x java -jar "%JAR%" -h %HOST% -p %MAPIPORT% -d %TSTDB% -D
+call Mlog.bat -x java -jar "%JAR%" -h %HOST% -p %MAPIPORT% -d "%TSTDB%" -D
 
 del .monetdb
