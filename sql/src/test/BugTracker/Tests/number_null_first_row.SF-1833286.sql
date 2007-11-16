51417dfa491358b6c442842f0bb4f8592307e4b6
Niels Nes <niels@cwi.nl>
2007-11-16 20:39:33 +0000

    extended test also to the case where null isn't allowed.

--- sql/src/test/BugTracker/Tests/number_null_first_row.SF-1833286.sql
+++ sql/src/test/BugTracker/Tests/number_null_first_row.SF-1833286.sql
@@ -10,3 +10,16 @@ INSERT into "decimal_null" values (2.3);
 INSERT into "decimal_null" values (NULL);
 
 drop table decimal_null;
+
+CREATE TABLE decimal_null ( promoid NUMERIC(8, 2) NOT NULL);
+
+-- this will fail
+INSERT into "decimal_null" values (NULL);
+
+-- this will work
+INSERT into "decimal_null" values (2.3);
+
+-- this will fail
+INSERT into "decimal_null" values (NULL);
+
+drop table decimal_null;
