27e6d7badf3b37860bc9cade71700274543c78b0
Niels Nes <niels@cwi.nl>
2007-11-19 08:18:25 +0000

    another attempt at fixing python_dbapi_embedded. We now add TSTBLDDIR/src/backend/monet{4,5}/{,.libs}.
    approved php_monetdb (perl test now cleans up table)

--- sql/src/test/mapi/Tests/python_dbapi_embedded.sh
+++ sql/src/test/mapi/Tests/python_dbapi_embedded.sh
@@ -3,11 +3,17 @@
 PATH="`monetdb-clients-config --pkglibdir`/Tests:$PATH"
 export PATH
 
-PYTHONPATH="`monetdb-config --pythonlibdir`:`monetdb-clients-config --pythonlibdir`:`monetdb-clients-config --pythonlibdir`/MonetDB:`monetdb-sql-config --pythonlibdir`:$PYTHONPATH"
+PYTHONPATH="`monetdb-config --pythonlibdir`:`monetdb-clients-config --pythonlibdir`:`monetdb-clients-config --pythonlibdir`/MonetDB:$PYTHONPATH"
 export PYTHONPATH
 
 if [ -n "$TST_FIVE" ] ; then
+	PYTHONPATH="$TSTBLDBASE/src/backends/monet5/:$PYTHONPATH"
+	PYTHONPATH="$TSTBLDBASE/src/backends/monet5/.libs:$PYTHONPATH"
+	export PYTHONPATH
 	Mlog -x "sqlsample.py $GDK_DBFARM $TSTDB 5"
 else
+	PYTHONPATH="$TSTBLDBASE/src/backends/monet4/:$PYTHONPATH"
+	PYTHONPATH="$TSTBLDBASE/src/backends/monet4/.libs:$PYTHONPATH"
+	export PYTHONPATH
 	Mlog -x "sqlsample.py $GDK_DBFARM $TSTDB 4"
 fi
