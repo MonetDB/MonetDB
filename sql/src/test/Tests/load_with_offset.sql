a7ef7248428c8f138181ba5ac744df48a641046a
Niels Nes <niels@cwi.nl>
2007-12-13 16:08:21 +0000

    added support for line skipping
    we can now for example do
    
    copy 5 OFFSET 5 RECORDS INTO my_test FROM stdin USING DELIMITERS '|','\n' ;
    
    Ie load 5 records starting at the 5th line

---
+++ sql/src/test/Tests/load_with_offset.sql
@@ -0,0 +1,35 @@
+CREATE TABLE my_test (
+  col1  INT,
+  col2  INT,
+  col3  INT,
+  col4  VARCHAR(1),
+  col5  VARCHAR(1)
+) ;
+
+COPY 4 OFFSET 5 RECORDS INTO my_test FROM stdin USING DELIMITERS '|','\n' ;
+123|1.01||aaa|bbb
+553|.02||aaa|bbb
+223|2.03||aaa|bbb
+123|.04||aaa|bbb
+823|3.05||aaa|bbb
+123|0.06||aaa|bbb
+590|0.07||aaa|bbb
+239|.08||aaa|bbb
+445|28.09||aaa|bbb
+
+select * from my_test;
+
+COPY 9 RECORDS INTO my_test FROM stdin USING DELIMITERS '|','\n' ;
+123|1.01||aaa|bbb
+553|.02||aaa|bbb
+223|2.03||aaa|bbb
+123|.04||aaa|bbb
+823|3.05||aaa|bbb
+123|0.06||aaa|bbb
+590|0.07||aaa|bbb
+239|.08||aaa|bbb
+445|28.09||aaa|bbb
+
+select * from my_test;
+
+DROP   TABLE my_test ;
