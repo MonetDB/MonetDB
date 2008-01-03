7dc7c69916fd7375437fc8d34ca664a430ac4d12
Niels Nes <niels@cwi.nl>
2008-01-03 09:41:18 +0000

    added support for copy select .. into 'filename';

--- sql/src/include/sql_catalog.h
+++ sql/src/include/sql_catalog.h
@@ -406,6 +406,10 @@ typedef struct res_table {
 	int query_type;
 	int nr_cols;
 	int cur_col;
+	char *filename;
+	char *tsep;
+	char *rsep;
+	char *ssep;
 	res_col *cols;
 	bat order;
 	struct res_table *next;
