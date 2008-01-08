f1fba3808d939375c2be9937667ce33a377db02a
Romulo Gonçalves <goncalve@cwi.nl>
2008-01-08 14:25:55 +0000

    Inspired by the sql/src/test/Tests/zones2.SQL.bat fix the bat script

--- sql/src/test/BugTracker/Tests/local_temp_table.SF-1865953.SQL.bat
+++ sql/src/test/BugTracker/Tests/local_temp_table.SF-1865953.SQL.bat
@@ -1,14 +1,2 @@
-@echo off
-
-echo user=monetdb>	.monetdb
-echo password=monetdb>>	.monetdb
-
-set LANG=en_US.UTF-8
-
-prompt # $t $g  
-echo on
-
-call mclient -lsql -h %HOST% -p %MAPIPORT% -d %TSTDB% "%RELSRCDIR%\local_temp_table_data.SF-1865953.sql"
-call mclient -lsql -h %HOST% -p %MAPIPORT% -d %TSTDB% "
-
-@del .monetdb
+@call mclient -lsql -h %HOST% -p %MAPIPORT% %RELSRCDIR%\local_temp_table_data.SF-1865953.sql
+@call mclient -lsql -h %HOST% -p %MAPIPORT%  
