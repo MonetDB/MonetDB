ccd7244e7f76d4961c0f1177e03413287200bd42
Niels Nes <niels@cwi.nl>
2008-02-06 10:05:32 +0000

    The scenario's should now allways set c->state[MAL_SCENARIO_READER], after a succesfull initialisation.

--- sql/src/test/Connections/Tests/connections.py
+++ sql/src/test/Connections/Tests/connections.py
@@ -8,7 +8,7 @@ def clean_ports(cmd,mapiport,xrpcport):
 
 def remote_server_start(x,s,dbinit):
     port = os.getenv('MAPIPORT')
-    srvcmd = '%s --dbname "%s_test1" --dbinit="%s"' % (os.getenv('MSERVER'),os.getenv('TSTDB'),dbinit)
+    srvcmd = '%s --dbfarm "%s" --dbname "%s_test1" --dbinit="%s"' % (os.getenv('MSERVER'),os.getenv('GDK_DBFARM'), os.getenv('TSTDB'),dbinit)
     srvcmd = srvcmd.replace(port,str(int(port)+1))
     srvcmd_ = clean_ports(srvcmd,str(int(port)+1),os.getenv('XRPCPORT'))
     sys.stdout.write('\nserver %d%d : "%s"\n' % (x,s, dbinit))
@@ -23,7 +23,7 @@ def remote_server_start(x,s,dbinit):
 
 def server_start(x,s,dbinit):
     port = int(os.getenv('MAPIPORT'))
-    srvcmd = '%s --dbname "%s" --dbinit="%s"' % (os.getenv('MSERVER'),os.getenv('TSTDB'),dbinit)
+    srvcmd = '%s --dbfarm "%s" --dbname "%s" --dbinit="%s"' % (os.getenv('MSERVER'),os.getenv('GDK_DBFARM'), os.getenv('TSTDB'),dbinit)
     srvcmd_ = clean_ports(srvcmd,str(port),os.getenv('XRPCPORT'))
     sys.stdout.write('\nserver %d%d : "%s"\n' % (x,s, dbinit))
     sys.stderr.write('\nserver %d%d : "%s"\n' % (x,s, dbinit))
