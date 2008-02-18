e409c76f85a4dc0809e5bb545b8950e1127e5f24
Sjoerd Mullender <sjoerd@acm.org>
2008-02-18 15:50:28 +0000

    propagated changes of Friday 15 Feb 2008 - Monday Feb 18 2008 from the SQL_2-22 branch to the development trunk

--- sql/src/test/bugs/Tests/seconds2timestamp.sql
+++ sql/src/test/bugs/Tests/seconds2timestamp.sql
@@ -1,2 +1,12 @@
 select epoch (1087488000);
 select epoch (timestamp '2004-06-17 16:00:00.000000');
+
+select abs(epoch(timestamp '2008-02-16 14:00:00'));
+CREATE TABLE "sys"."t10"("time1" int, "time2" timestamp(7));
+insert into t10 values(1202900916, timestamp '2008-02-13 11:08:06.000000');
+select * from t10;
+select epoch(1203170400);
+select epoch(time1) from t10;
+select epoch(time2) from t10;
+
+drop table t10;
