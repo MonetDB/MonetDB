303f271a4fd8b59935d7e7242369fb3a009a6145
Niels Nes <niels@cwi.nl>
2008-02-25 22:28:06 +0000

    approved output after 1) added new dense_rank test 2) fixes too row_number (start at 1 now) 3) fixes to algebra version 4) output now requires io.stdout()

--- sql/src/test/rank.sql
+++ sql/src/test/rank.sql
@@ -7,3 +7,13 @@ select RANK() over () as foo from tables;
 select RANK() over (PARTITION BY schema_id) as foo, schema_id from tables;
 select RANK() over (PARTITION BY schema_id ORDER BY schema_id) as foo, schema_id from tables;
 select RANK() over (ORDER BY schema_id) as foo, schema_id from tables;
+
+select RANK() over () as foo, name, "type" from columns;
+select RANK() over (PARTITION BY name) as foo, name, "type" from columns;
+select RANK() over (PARTITION BY name ORDER BY name, "type") as foo, name, "type" from columns;
+select RANK() over (ORDER BY name, "type") as foo, name, "type" from columns;
+
+select DENSE_RANK() over () as foo, name, "type" from columns;
+select DENSE_RANK() over (PARTITION BY name) as foo, name, "type" from columns;
+select DENSE_RANK() over (PARTITION BY name ORDER BY name, "type") as foo, name, "type" from columns;
+select DENSE_RANK() over (ORDER BY name, "type") as foo, name, "type" from columns;
