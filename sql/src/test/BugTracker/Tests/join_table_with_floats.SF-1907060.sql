276675790209841ed3ab5326b86a2e52b0e7668c
Romulo Gonçalves <goncalve@cwi.nl>
2008-03-07 16:13:38 +0000

    Remove the bugs tests because they should be added in the stable branch...

--- sql/src/test/BugTracker/Tests/join_table_with_floats.SF-1907060.sql
+++
@@ -1,7 +0,0 @@
-create table t2 (id float);
-create table t1 (age float);
-
-select age from t1, t2 where id < 0.0 limit 10;
-
-drop table t1;
-drop table t2;
--- sql/src/test/BugTracker/Tests/ms_round.SF-1907063.sql
+++
@@ -1,4 +0,0 @@
-create table t23(id float);
-select round(id,8) from t23; 
-
-drop table t23;
