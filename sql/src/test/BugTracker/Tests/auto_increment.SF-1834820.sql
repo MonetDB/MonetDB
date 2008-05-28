d217a31301b3d40859f18853762791e9505e545d
Romulo Gonçalves <goncalve@cwi.nl>
2008-05-28 15:43:51 +0000

    Extend the test to check if the sequence is dropped when a table is dropped

--- sql/src/test/BugTracker/Tests/auto_increment.SF-1834820.sql
+++ sql/src/test/BugTracker/Tests/auto_increment.SF-1834820.sql
@@ -18,4 +18,8 @@ select * from testusers;
 INSERT INTO testusers (name, fullname) VALUES ('fred', 'Fred Flintstone');
 select * from testusers;
 
+select * from sequences;
+
 drop table testusers;
+
+select * from sequences;
