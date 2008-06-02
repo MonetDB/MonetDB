15e82ebec3b24734f6b511507e6cfb68115409f3
Romulo Gonçalves <goncalve@cwi.nl>
2008-06-02 15:16:59 +0000

    If you create the user (voc) do not forget to drop it.
    This was the reason to break some following tests which were trying to create user voc...
    
    Thanks Stefan for dectecting the problem...

--- sql/src/test/BugTracker/Tests/authorization.SF-1430616.SQL.bat
+++ sql/src/test/BugTracker/Tests/authorization.SF-1430616.SQL.bat
@@ -15,4 +15,9 @@ call mjclient -h %HOST% -p %MAPIPORT% -d %TSTDB% -f "%RELSRCDIR%\authorization.S
 
 call mjclient -h %HOST% -p %MAPIPORT% -d %TSTDB% -q
 
+echo user=monetdb>	.monetdb
+echo password=monetdb>>	.monetdb
+
+call mjclient -h %HOST% -p %MAPIPORT% -d %TSTDB% -f "%RELSRCDIR%\authorization.SF-1430616-drop_user.sql"
+
 @del .monetdb
