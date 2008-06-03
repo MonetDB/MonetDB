53f997cfe13a2be4066df62a48e19cedfdcc3fd2
Niels Nes <niels@cwi.nl>
2008-06-03 12:25:00 +0000

    the skyserver test data

--- sql/src/test/Skyserver/Tests/Skyserver.SQL.py
+++ sql/src/test/Skyserver/Tests/Skyserver.SQL.py
@@ -2,19 +2,22 @@ import os, sys
 
 
 def main():
+    dir = os.getenv('RELSRCDIR')
     clcmd = str(os.getenv('SQL_CLIENT'))
     clcmd1 = str(os.getenv('SQL_CLIENT')) + " -uskyserver -Pskyserver"
     sys.stdout.write('Create User\n')
-    clt = os.popen(clcmd + "<%s" % ('%s/../create_user.sql' % os.getenv('RELSRCDIR')), 'w')
+    clt = os.popen(clcmd + "<%s" % ('%s/../create_user.sql' % dir), 'w')
     clt.close()
     sys.stdout.write('tables\n')
-    clt1 = os.popen(clcmd1 + "<%s" % ('%s/../../../sql/math.sql' % os.getenv('RELSRCDIR')), 'w')
+    clt1 = os.popen(clcmd1 + "<%s" % ('%s/../../../sql/math.sql' % dir), 'w')
     clt1.close()
-    clt1 = os.popen(clcmd1 + "<%s" % ('%s/../../../sql/cache.sql' % os.getenv('RELSRCDIR')), 'w')
+    clt1 = os.popen(clcmd1 + "<%s" % ('%s/../../../sql/cache.sql' % dir), 'w')
     clt1.close()
-    clt1 = os.popen(clcmd1 + "<%s" % ('%s/../../../sql/ms_functions.sql' % os.getenv('RELSRCDIR')), 'w')
+    clt1 = os.popen(clcmd1 + "<%s" % ('%s/../../../sql/ms_functions.sql' % dir), 'w')
     clt1.close()
-    clt1 = os.popen(clcmd1 + "<%s" % ('%s/../Skyserver_tables.sql' % os.getenv('RELSRCDIR')), 'w')
+    clt1 = os.popen(clcmd1 + "<%s" % ('%s/../Skyserver_tables.sql' % dir), 'w')
+    clt1.close()
+    clt1 = os.popen("cat %s/../Skyserver_import.sql | sed -e \"s|DATA|%s/../microsky|g\" | %s " % (dir, dir, clcmd1), 'w')
     clt1.close()
     clt1 = os.popen(clcmd1 + "<%s" % ('%s/../Skyserver_constraints.sql' % os.getenv('RELSRCDIR')), 'w')
     clt1.close()
