8c1ce7625099dac78ed19a760fed11bdb94fbdd0
Niels Nes <niels@cwi.nl>
2008-06-03 07:20:28 +0000

    changed count_crash (mrmeltdown was right), its better not to depend on the number of tables in sys._tables.

--- sql/src/test/BugTracker/Tests/count_crash.SF-1918780.sql
+++ sql/src/test/BugTracker/Tests/count_crash.SF-1918780.sql
@@ -1,1 +1,6 @@
-select count(1000) from _tables;
+create table count_crash (i int);
+insert into count_crash values(2);
+insert into count_crash values(7);
+insert into count_crash values(4);
+select count(1000) from count_crash;
+drop table count_crash;
