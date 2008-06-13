5f200cebbd2deda11379e6f1958209bf83b45219
Niels Nes <niels@cwi.nl>
2008-06-13 21:21:55 +0000

    add test for  bug in handling aggregates on single values

---
+++ sql/src/test/bugs/Tests/aggr_single_value.sql
@@ -0,0 +1,16 @@
+
+SELECT count(*);
+SELECT count(1);
+SELECT count(NULL);
+
+SELECT avg(1);
+SELECT avg(cast (NULL as int));
+
+SELECT sum(1);
+SELECT sum(cast (NULL as int));
+
+SELECT max(1);
+SELECT max(cast (NULL as int));
+
+SELECT min(1);
+SELECT min(cast (NULL as int));
