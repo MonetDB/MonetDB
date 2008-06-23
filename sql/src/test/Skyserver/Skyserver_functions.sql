16638ea3f86bce8469d7b56c7d492d4d42927fbf
Romulo Gonçalves <goncalve@cwi.nl>
2008-06-23 08:05:45 +0000

    Well, it seems there is more str functions used in the Skyserver test..

--- sql/src/test/Skyserver/Skyserver_functions.sql
+++ sql/src/test/Skyserver/Skyserver_functions.sql
@@ -774,10 +774,14 @@ BEGIN
 	SET t  = MS_STUFF(t,6-LENGTH(q),LENGTH(q), q);
 	-- seconds
 	SET d  = MS_ROUND( 60.0 * (d-nd),precision,truncat );
-	SET q  = LTRIM(STR(d,precision));
+	IF (precision < 1) 
+		THEN SET q  = LTRIM(cast( round(d, precision) as varchar(7)));
+	END IF;
+	IF (precision > 10) 
+		THEN SET q  = LTRIM(cast( round(d, precision) as varchar(16)));
+	END IF;
 	SET t = MS_STUFF(t,10+precision-LENGTH(q),LENGTH(q), q);
 --	SET d  = 60.0 * (d-nd);
---	SET q = LTRIM(STR(d,3));
 --	SET t = MS_STUFF(t,13-LENGTH(q),LENGTH(q), q);
 	--
 	RETURN(t);
