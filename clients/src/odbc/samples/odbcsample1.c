29d479331abd101ed4f828386e02883974443016
Sjoerd Mullender <sjoerd@acm.org>
2008-08-01 13:29:04 +0000

    Adapted test to test UTF-8 encoding going through.

--- clients/src/odbc/samples/odbcsample1.c
+++ clients/src/odbc/samples/odbcsample1.c
@@ -199,7 +199,9 @@ main(int argc, char **argv)
 	f5.second = 0;
 	for (i = 0; i < 2000; i++) {
 		f1 = i;
-		snprintf(f2, sizeof(f2), "value %d", i);
+		/* \xdf\xa2 is the UTF-8 encoding of U+2018 Left Single Quotation Mark;
+		   \xdf\xa3 is the UTF-8 encoding of U+2019 Right Single Quotation Mark */
+		snprintf(f2, sizeof(f2), "value \xdf\xa2%d\xdf\xa3", i);
 		f3 = i * 1.5;
 		f4.day++;
 		if ((f4.day == 29 && f4.month == 2) || (f4.day == 31 && (f4.month == 4 || f4.month == 6 || f4.month == 9 || f4.month == 11)) || f4.day == 32) {
