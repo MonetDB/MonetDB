1ab336645af2897ba01691deaa61fccf1cc43076
Niels Nes <niels@cwi.nl>
2008-08-29 11:28:14 +0000

    approve output

--- sql/src/test/BugTracker-2008/Tests/decimal_cast_in_view.SF-2075223.sql
+++ sql/src/test/BugTracker-2008/Tests/decimal_cast_in_view.SF-2075223.sql
@@ -4,6 +4,12 @@ SELECT CAST (a AS NUMERIC (5, 2)) AS b
 FROM A;
 INSERT INTO A VALUES (' 54.');
 
+-- first directly on A
+select * from A;
+
+-- first directly on A with cast
+select CAST (a as NUMERIC (5, 2)) from A;
+
 -- return -1546.00 (should be 54.00)
 select * from A_VIEW;
 
