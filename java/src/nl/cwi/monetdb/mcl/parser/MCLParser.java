f2e9060602969c29f32389eff2c276cb2cb55ed7
Fabian Groffen <fabian@cwi.nl>
2008-10-28 11:40:34 +0000

    Since this is an interface, it makes sense to document it properly.  Removed the remove() method since its never properly implemented by any sub-class, and I have no clue what was the intention of the method

--- java/src/nl/cwi/monetdb/mcl/parser/MCLParser.java
+++ java/src/nl/cwi/monetdb/mcl/parser/MCLParser.java
@@ -42,30 +42,75 @@ public abstract class MCLParser {
 	public final int intValues[];
 	private int colnr;
 
+	/**
+	 * Creates an MCLParser targetted at a given number of field values.
+	 * The lines parsed by an instance of this MCLParser should have
+	 * exactly capacity field values.
+	 *
+	 * @param capacity the number of field values to expect
+	 */
 	protected MCLParser(int capacity) {
 		values = new String[capacity];
 		intValues = new int[capacity];
 	}
 
+	/**
+	 * Parse the given string, and populate the internal field array
+	 * to allow for next() and hasNext() calls.
+	 *
+	 * @param source the String containing the line to parse
+	 * @throws MCLParseException if source cannot be (fully) parsed by
+	 * this parser
+	 * @see next()
+	 * @see nextInt()
+	 * @see hasNext()
+	 */
 	abstract public int parse(String source) throws MCLParseException;
 
+	/**
+	 * Repositions the internal field offset to the start, such that the
+	 * next call to next() will return the first field again.
+	 */
 	final public void reset() {
 		colnr = 0;
 	}
 
+	/**
+	 * Returns whether the next call to next() or nextInt() succeeds.
+	 *
+	 * @return true if the next call to next() or nextInt() is bound to
+	 * succeed
+	 * @see next()
+	 * @see nextInt()
+	 */
 	final public boolean hasNext() {
 		return(colnr < values.length);
 	}
 
+	/**
+	 * Returns the current field value, and advances the field counter
+	 * to the next value.  This method may fail with a RuntimeError if
+	 * the current field counter is out of bounds.  Call hasNext() to
+	 * determine if the call to next() will succeed.
+	 *
+	 * @return the current field value
+	 * @see nextInt()
+	 * @see hasNext()
+	 */
 	final public String next() {
 		return(values[colnr++]);
 	}
 
+	/**
+	 * Returns the current field value as integer, and advances the
+	 * field counter to the next value.  This method has the same
+	 * characteristics as the next() method, apart from returning the
+	 * field value as an integer.
+	 *
+	 * @return the current field value as integer
+	 * @see next()
+	 */
 	final public int nextInt() {
 		return(intValues[colnr++]);
-	}
-
-	final public void remove() {
-		throw new AssertionError("Not implemented");
 	}
 }
