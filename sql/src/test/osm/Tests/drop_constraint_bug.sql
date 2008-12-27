2c90bb9f8b435b42988a298e807382368550a6d9
Niels Nes <niels@cwi.nl>
2008-12-27 14:04:28 +0000

    add test for drop constraint bug

---
+++ sql/src/test/osm/Tests/drop_constraint_bug.sql
@@ -0,0 +1,18 @@
+
+CREATE TABLE "sys"."ways" (
+        "id" int NOT NULL,
+        "username" varchar(255),
+        "timestamp" timestamptz(7),
+        CONSTRAINT "ways_id_pkey" PRIMARY KEY ("id")
+);
+CREATE TABLE way_tags (way integer, k varchar(255), v varchar(1024));
+alter table ways drop constraint ways_id_pkey;
+SELECT count(*) FROM ways
+LEFT JOIN way_tags ON
+        ways.id = way_tags.way
+WHERE
+        k = 'highway' AND
+        v = 'secondary';
+
+drop table way_tags;
+drop table ways;
