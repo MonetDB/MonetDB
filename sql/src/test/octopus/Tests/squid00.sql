be3dc836e4e1416121480f5c420c6b87beccf77d
Martin Kersten <mk@cwi.nl>
2009-01-02 22:34:57 +0000

    Minor textual changes and the first experiment for the octopus.

---
+++ sql/src/test/octopus/Tests/squid00.sql
@@ -0,0 +1,3 @@
+set optimizer='inline,remap,evaluate,costModel,coercions,emptySet,mitosis,aliases,mergetable,octopus,commonTerms,accumulators,deadcode,reduce,garbageCollector,dataflow,history,multiplex';
+explain select Bid from squidA;
+select Bid from squidA;
