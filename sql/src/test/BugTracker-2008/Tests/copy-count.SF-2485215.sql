fc549cb7b9a2d59a7a3918a688c00533688a236c
Niels Nes <niels@cwi.nl>
2009-01-13 14:47:55 +0000

    introduced a avg-> sum/cnt optimize step. Currently disabled for decimal input columns (the current *100/100 etc stuff breaks it). This should also be moved to the optimizer, later.

--- sql/src/test/BugTracker-2008/Tests/copy-count.SF-2485215.sql
+++ sql/src/test/BugTracker-2008/Tests/copy-count.SF-2485215.sql
@@ -1,6 +1,6 @@
 start transaction;
 create table t(i int);
 insert into t values(1123);
-copy select * from t into stdout using delimiters ',';
-copy select count(*) from t into stdout using delimiters ',';
-abort;
+copy select * from t into stdout using delimiters ',', '\n';
+copy select count(*) from t into stdout using delimiters ',', '\n';
+rollback;
