5ef90b6060f06493ef04ee106bbe9dc6dfb66a37
Romulo Gonçalves <goncalve@cwi.nl>
2009-02-10 16:28:09 +0000

    Add the bug code to the database objects name created for this test..
    In this way we do not affect the other tests in case of segmenfault, i.e., in case the
    database objects are not dropped

--- sql/src/test/BugTracker-2009/Tests/subtraction_between_two_columns.SF-2582389.sql
+++ sql/src/test/BugTracker-2009/Tests/subtraction_between_two_columns.SF-2582389.sql
@@ -1,8 +1,8 @@
-create table t2(id int, inst float);
+create table t2_2582389(id int, inst float);
 
-insert into t2 values(1, 0.2);
-insert into t2 values(1, 0.4);
+insert into t2_2582389 values(1, 0.2);
+insert into t2_2582389 values(1, 0.4);
 
-select l.id, r.inst, l.inst, (r.inst - l.inst) as diff from t2 l,  t2 r where r.id = l.id order by inst limit 2;
+select l.id, r.inst, l.inst, (r.inst - l.inst) as diff from t2_2582389 l,  t2_2582389 r where r.id = l.id limit 2;
 
-drop table t2;
+drop table t2_2582389;
