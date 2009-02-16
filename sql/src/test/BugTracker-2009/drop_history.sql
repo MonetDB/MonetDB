a9955a65c8b6c20b6931bb8945c3963d57c7e448
Romulo Gonçalves <goncalve@cwi.nl>
2009-02-16 22:36:15 +0000

    add test for and stable output
    set_history_and_drop_table.SF-2607045

---
+++ sql/src/test/BugTracker-2009/drop_history.sql
@@ -0,0 +1,5 @@
+drop procedure keepCall;
+drop procedure keepQuery;
+drop view queryLog;
+drop table callHistory;
+drop table queryHistory;
---
+++ sql/src/test/BugTracker-2009/set_history_and_drop_table.SF-2607045.sql
@@ -0,0 +1,4 @@
+set history='true';
+select id from tables where id > 0;
+create table t1 (id int);
+drop table t1;
