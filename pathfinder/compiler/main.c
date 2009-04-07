32acb94b11c298ad9fec2bf08f1d160e05331300
Peter Boncz <boncz@cwi.nl>
2009-04-07 08:30:09 +0000

    issue 1981779 - slightly increase the stack limit

--- pathfinder/compiler/main.c
+++ pathfinder/compiler/main.c
@@ -632,9 +632,9 @@ main (int argc, char *argv[])
     _set_abort_behavior (0, _CALL_REPORTFAULT|_WRITE_ABORT_MSG);
 
 #ifndef NO_STACK_CHECK          /* define to not check for recursion depth */
-    /* the default Windows stack is 1 MiB, so give it 3/4 of that
+    /* the default Windows stack is 1 MiB, so give it 7/8 of that
      * above (below) the address of our argument */
-    PFmaxstack = (char *)  &argc - (3 << 18);
+    PFmaxstack = (char *)  &argc - (7 << 17);
 #endif
 #endif
 
