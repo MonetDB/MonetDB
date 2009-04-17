48c273ac911ac4ccf076fe48e63680e3240d2920
Sjoerd Mullender <sjoerd@acm.org>
2009-04-17 15:27:35 +0000

    Disallow files whose names end with a period.

--- CVSROOT/testcheck.sh
+++ CVSROOT/testcheck.sh
@@ -10,30 +10,32 @@ for file; do
     fi
 
     case "$file" in
-    *:*)
+    *:* | *.)
 	# you're never allowed to commit a file that has forbidden
 	# characters in the name (such as : -- doesn't work on Windows)
-	echo "Pre-commit check failed:"
-	echo "You are not allowed to commit files with a colon (:) in the name"
-	echo "since this doesn't work on Windows."
+	echo "\
+Pre-commit check failed for file \"$file\":
+You are not allowed to commit files with a colon (:) in the name or
+ones with a name that ends in a period.  This doesn't work on Windows."
 	exit 1
 	;;
     *.stable.out* | *.stable.err*)
 	# you're never allowed to commit a test that failed with a signal
 	if grep -q '^!Mtimeout: signal' "$file" || grep -q '^!ERROR: BATSIGcrash' "$file"; then
-	    echo "Pre-commit check failed:"
-	    echo "You are not allowed to commit this change to $file"
-	    echo "You are trying to approve a test which failed with a signal."
-	    echo "This is never acceptable."
+	    echo "\
+Pre-commit check failed for file \"$file\":
+You are not allowed to commit this change.  You are trying to approve
+a test which failed with a signal.  This is never acceptable."
 	    exit 1
 	fi
 	;;
     *.py | *.py.in)
 	# TABs and trailing white space are not allowed in Python source
 	if grep -q $'\t\\| $' "$file"; then
-	    echo "Pre-commit check failed:"
-	    echo "The file \"$file\" contains TABs and/or trailing white space."
-	    echo "Change TABs to spaces and remove any trailing white space, then try again."
+	    echo "\
+Pre-commit check failed for file \"$file\":
+This file contains TABs and/or trailing white space.  Change TABs to
+spaces and remove any trailing white space, then try again."
 	    exit 1
 	fi
 	;;
