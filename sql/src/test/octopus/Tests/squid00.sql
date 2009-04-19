1e02d225f2c8571d73da4005b856db12f4e31997
Martin Kersten <mk@cwi.nl>
2009-04-19 20:01:05 +0000

    Use the offline performance profiling.

--- sql/src/test/octopus/Tests/squid00.sql
+++ sql/src/test/octopus/Tests/squid00.sql
@@ -1,3 +1,3 @@
-set optimizer='inline,remap,evaluate,costModel,coercions,emptySet,mitosis,aliases,mergetable,octopus,commonTerms,accumulators,deadcode,reduce,garbageCollector,dataflow,history,multiplex';
+set optimizer='inline,remap,evaluate,costModel,coercions,emptySet,mitosis,aliases,mergetable,commonTerms,accumulators,octopus,deadcode,reduce,garbageCollector,dataflow,history,multiplex';
 explain select Bid from squidA;
-select Bid from squidA;
+trace select Bid from squidA;
