2798ae943e944f9e2090d6771430c8b3da717278
Gijs Molenaar <gijzelaerr@users.sourceforge.net>
2009-04-20 14:51:12 +0000

    added simple setup.py

--- clients/src/python/setup.py
+++ clients/src/python/setup.py
@@ -1,2 +1,11 @@
-#TODO: write this
+from distutils.core import setup
+
+setup(name='monetdb.sql',
+      version='1.0',
+      description='Native MonetDB driver',
+      author='Gijs Molenaar',
+      author_email='gijs.molenaar@cw.nl',
+      url='http://www.monetdb.nl',
+      packages=['monetdb', 'monetdb.sql'],
+     )
 
