d7204bc5a237684a03786c6aa218d88712331916
Martin Kersten <mk@cwi.nl>
2009-04-28 19:33:15 +0000

    first octopus test

--- sql/src/test/octopus/Tests/squid00.sql
+++ sql/src/test/octopus/Tests/squid00.sql
@@ -1,3 +1,2 @@
 set optimizer='inline,remap,evaluate,costModel,coercions,emptySet,mitosis,aliases,mergetable,commonTerms,accumulators,octopus,deadcode,reduce,garbageCollector,dataflow,history,multiplex';
-explain select Bid from squidA;
-trace select Bid from squidA;
+select Bid from squidA;
