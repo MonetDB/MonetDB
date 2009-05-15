865630684fd6ca417b6303319d8e918efce4112a
Sjoerd Mullender <sjoerd@acm.org>
2009-05-15 09:21:42 +0000

    The OpenSSL API has a problem on 64 bit Windows: SSL_set_fd expects an int for the socket, but the correct type is SOCKET which does not fit.  So: disable OpenSSL support for communication.

--- MonetDB/src/common/stream_socket.h
+++ MonetDB/src/common/stream_socket.h
@@ -36,6 +36,15 @@ stream_export stream *socket_wstream(SOCKET socket, const char *name);
 stream_export stream *socket_rastream(SOCKET socket, const char *name);
 stream_export stream *socket_wastream(SOCKET socket, const char *name);
 
+#if defined(HAVE_OPENSSL) && defined(_WIN64)
+/* OpenSSL has an interface bug in SSL_set_fd which affects 64 bit Windows */
+#include <openssl/opensslv.h>	/* check OpenSSL version number */
+#if OPENSSL_VERSION_NUMBER <= 0x10000002L
+/* the bug still exists in version 1.0.0-beta2 */
+#undef HAVE_OPENSSL
+#endif
+#endif
+
 #ifdef HAVE_OPENSSL
 #include <openssl/ssl.h>
 #include <openssl/err.h>
