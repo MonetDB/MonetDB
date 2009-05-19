4f242b0dd922f5f70e15440f96ff13d6a7973904
Sjoerd Mullender <sjoerd@acm.org>
2009-05-19 11:59:57 +0000

    Test dumping a slightly more complex scenaria with multiple users.

---
+++ sql/src/test/UserDump/Tests/create.SQL.py
@@ -0,0 +1,61 @@
+import os, sys
+try:
+    import subprocess
+except ImportError:
+    # use private copy for old Python versions
+    import MonetDBtesting.subprocess26 as subprocess
+
+def client(cmd, input = None):
+    clt = subprocess.Popen(cmd,
+                           shell = True,
+                           stdin = subprocess.PIPE,
+                           stdout = subprocess.PIPE,
+                           stderr = subprocess.PIPE,
+                           universal_newlines = True)
+    out, err = clt.communicate(input)
+    sys.stdout.write(out)
+    sys.stderr.write(err)
+
+def main():
+    sql_client = os.getenv('SQL_CLIENT')
+    client(sql_client, """\
+create user voc with password 'voc' name 'VOC Explorer' schema sys;
+create schema voc authorization voc;
+alter user voc set schema voc;
+""")
+    client('%s -uvoc -Pvoc' % sql_client, """\
+create table foo (
+        id int,
+        v int,
+        primary key (id)
+);
+create view bar as select * from foo;
+create function b(i int) returns int begin return select v from bar where id = i; end;
+create table a (
+        id int
+);
+create trigger a after insert on foo insert into a values (1);
+""")
+    client(sql_client, """\
+create user test with password 'test' name 'Test User' schema sys;
+create schema test authorization test;
+alter user test set schema test;
+""")
+    client('%s -utest -Ptest' % sql_client, '''\
+create table foo (
+        id int,
+        v int,
+        primary key (id)
+);
+create view bar as select * from foo;
+create function b(i int) returns int begin return select v from bar where id = i; end;
+create table a (
+        id int
+);
+create trigger a after insert on foo insert into a values (1);
+create trigger test.x after insert on foo insert into a values (1);
+create trigger "test"."z" after insert on "foo" insert into a values (1);
+''')
+    client(os.getenv('SQLDUMP'))
+
+main()
