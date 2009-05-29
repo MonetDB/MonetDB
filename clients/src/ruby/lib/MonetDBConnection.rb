8ddeedee21677fd87f509f18ad12b6541bbd8969
Gabriele Modena <gabriele_modena@users.sourceforge.net>
2009-05-29 11:43:09 +0000

    Fixed a challenge response bug (missing trailing : after the database name)

--- clients/src/ruby/lib/MonetDBConnection.rb
+++ clients/src/ruby/lib/MonetDBConnection.rb
@@ -215,7 +215,7 @@ class MonetDBConnection
       
     end    
     # Build the reply message with header
-    reply = @client_endianness + ":" + @user + ":{" + auth_type + "}" + hashsum + ":" + @lang + ":" + db_name 
+    reply = @client_endianness + ":" + @user + ":{" + auth_type + "}" + hashsum + ":" + @lang + ":" + db_name + ":"
   end
 
   #
@@ -249,7 +249,7 @@ class MonetDBConnection
       raise MonetDBConnectionError, "#{auth_type} not supported by the server. Please choose one from #{@supported_auth_types}"
     end    
     # Build the reply message with header
-    reply = @client_endianness + ":" + @user + ":{" + auth_type + "}" + hashsum + ":" + @lang + ":" + db_name 
+    reply = @client_endianness + ":" + @user + ":{" + auth_type + "}" + hashsum + ":" + @lang + ":" + db_name + ":"
   end
 
 
