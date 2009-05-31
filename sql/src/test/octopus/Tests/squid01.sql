cddfa121460520372aadd72cbae8fc8ffaf95b74
Martin Kersten <mk@cwi.nl>
2009-05-31 06:41:57 +0000

    The simple octopus distributed aggregate test

--- sql/src/test/octopus/Tests/squid01.sql
+++ sql/src/test/octopus/Tests/squid01.sql
@@ -1,4 +1,3 @@
 -- perform an aggregate using the octopus
 set optimizer='inline,remap,evaluate,costModel,coercions,emptySet,mitosis,aliases,mergetable,commonTerms,accumulators,octopus,deadcode,reduce,garbageCollector,dataflow,history,multiplex';
-explain select count(*) from squidA;
 select count(*) from squidA;
