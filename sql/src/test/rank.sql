2dd618d60c86062bf2552f24b4042dc5c502bf15
Martin Kersten <mk@cwi.nl>
2009-06-06 11:01:49 +0000

    Avoid tests that are dependent on the catalog size. Output to be approved.

--- sql/src/test/rank.sql
+++ sql/src/test/rank.sql
@@ -1,19 +1,65 @@
-select ROW_NUMBER() over () as foo from tables;
-select ROW_NUMBER() over (PARTITION BY schema_id) as foo, schema_id from tables;
-select ROW_NUMBER() over (PARTITION BY schema_id ORDER BY schema_id) as foo, schema_id from tables;
-select ROW_NUMBER() over (ORDER BY schema_id) as foo, schema_id from tables;
+create table ranktest ( id int, k string);
+COPY 40 RECORDS INTO ranktest FROM stdin USING DELIMITERS '|','\n';
+1061|varchar
+1061|int
+1061|varchar
+1061|varchar
+1061|int
+1061|int
+1061|varchar
+1061|varchar
+1061|boolean
+1061|boolean
+1061|int
+1061|varchar
+1061|bigint
+1061|bigint
+1061|bigint
+1061|boolean
+1061|int
+1061|smallint
+1061|int
+1061|char
+1062|int
+1062|char
+1062|char
+1062|int
+1062|varchar
+1062|int
+1062|varchar
+1062|smallint
+1062|boolean
+1062|smallint
+1062|int
+1062|varchar
+1062|boolean
+1061|int
+1061|int
+1061|varchar
+1061|int
+1061|int
+1061|smallint
+1061|smallint
 
-select RANK() over () as foo from tables;
-select RANK() over (PARTITION BY schema_id) as foo, schema_id from tables;
-select RANK() over (PARTITION BY schema_id ORDER BY schema_id) as foo, schema_id from tables;
-select RANK() over (ORDER BY schema_id) as foo, schema_id from tables;
+select count(*) from ranktest;
 
-select RANK() over () as foo, name, "type" from columns;
-select RANK() over (PARTITION BY name) as foo, name, "type" from columns;
-select RANK() over (PARTITION BY name ORDER BY name, "type") as foo, name, "type" from columns;
-select RANK() over (ORDER BY name, "type") as foo, name, "type" from columns;
+select ROW_NUMBER() over () as foo from ranktest;
+select ROW_NUMBER() over (PARTITION BY id) as foo, id from ranktest;
+select ROW_NUMBER() over (PARTITION BY id ORDER BY id) as foo, id from ranktest;
+select ROW_NUMBER() over (ORDER BY id) as foo, id from ranktest;
 
-select DENSE_RANK() over () as foo, name, "type" from columns;
-select DENSE_RANK() over (PARTITION BY name) as foo, name, "type" from columns;
-select DENSE_RANK() over (PARTITION BY name ORDER BY name, "type") as foo, name, "type" from columns;
-select DENSE_RANK() over (ORDER BY name, "type") as foo, name, "type" from columns;
+select RANK() over () as foo from ranktest;
+select RANK() over (PARTITION BY id) as foo, id from ranktest;
+select RANK() over (PARTITION BY id ORDER BY id) as foo, id from ranktest;
+select RANK() over (ORDER BY id) as foo, id from ranktest;
+
+select RANK() over () as foo, name, k from ranktest;
+select RANK() over (PARTITION BY name) as foo, name, k from ranktest;
+select RANK() over (PARTITION BY name ORDER BY name, k) as foo, name, k from ranktest;
+select RANK() over (ORDER BY name, k) as foo, name, k from ranktest;
+
+select DENSE_RANK() over () as foo, name, k from ranktest;
+select DENSE_RANK() over (PARTITION BY name) as foo, name, k from ranktest;
+select DENSE_RANK() over (PARTITION BY name ORDER BY name, k) as foo, name, k from ranktest;
+select DENSE_RANK() over (ORDER BY name, k) as foo, name, k from ranktest;
+drop table ranktest;
