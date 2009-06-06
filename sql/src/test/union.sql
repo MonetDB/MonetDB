81cfb84b301a1fd3593758fe0fce285f86fd8463
Martin Kersten <mk@cwi.nl>
2009-06-06 20:21:37 +0000

    Make the test independent from tables

--- sql/src/test/union.sql
+++ sql/src/test/union.sql
@@ -1,9 +1,12 @@
+create table uniontmp(i int, s string);
+insert into uniontmp values(1,'hello');
+insert into uniontmp values(2,'world');
+select * from 
+	( select * from uniontmp union all select * from uniontmp) as a;
 
 select * from 
-	( select * from tables union all select * from tables) as a;
+	( select * from uniontmp union select * from uniontmp) as a;
 
 select * from 
-	( select * from tables union select * from tables) as a;
-
-select * from 
-	( select * from tables union distinct select * from tables) as a;
+	( select * from uniontmp union distinct select * from uniontmp) as a;
+drop table uniontmp;
