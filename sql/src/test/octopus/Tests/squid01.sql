5a179eb1a9666f8cff3b2c30dbd5aea24c22b7b8
Martin Kersten <mk@cwi.nl>
2009-06-28 18:06:28 +0000

    remove optimizer

--- sql/src/test/octopus/Tests/squid01.sql
+++ sql/src/test/octopus/Tests/squid01.sql
@@ -1,3 +1,1 @@
--- perform an aggregate using the octopus
-set optimizer='inline,remap,evaluate,costModel,coercions,emptySet,mitosis,aliases,mergetable,commonTerms,accumulators,octopus,deadcode,reduce,garbageCollector,dataflow,history,multiplex';
 select count(*) from squidA;
