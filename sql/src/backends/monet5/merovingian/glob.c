fd2856ea06a24cf13407638736d48cfda84c077c
Fabian Groffen <fabian@cwi.nl>
2009-07-01 11:11:36 +0000

    make glob() a bit smarter, such that it tries harder to find a match when an intial match appears to be non-successfull.  For example, a pattern "*pegasus*" would fail to match on "mapi:monetdb://pegasus..." because of the first 'p' in "mapi".

--- sql/src/backends/monet5/merovingian/glob.c
+++ sql/src/backends/monet5/merovingian/glob.c
@@ -25,6 +25,7 @@
  * - * expands to an arbitrary string
  */
 
+#include <stdlib.h>  /* NULL */
 #include "glob.h"
 
 /**
@@ -33,12 +34,17 @@
 char
 glob(const char *expr, const char *haystack)
 {
+	const char *haymem = NULL;
+	const char *exprmem = NULL;
+
 	/* probably need to implement this using libpcre once we get
 	 * advanced users, doing even more advanced things */
 
 	while (*expr != '\0') {
 		switch (*expr) {
 			case '*':
+				/* store expression position for retry lateron */
+				exprmem = expr;
 				/* skip over haystack till the next char from expr */
 				expr++;
 				if (*expr == '\0')
@@ -46,13 +52,20 @@ glob(const char *expr, const char *haystack)
 					return(1);
 				while (*haystack != '\0' && *haystack != *expr)
 					haystack++;
+				/* store match position, for retry lateron */
+				haymem = haystack + 1;
 				if (*haystack == '\0')
-					/* couldn't find it, so no match */
+					/* couldn't find it, so no match  */
 					return(0);
 			break;
 			default:
-				if (*expr != *haystack)
-					return(0);
+				if (*expr != *haystack) {
+					if (haymem != NULL) {
+						return(glob(exprmem, haymem));
+					} else {
+						return(0);
+					}
+				}
 			break;
 		}
 		expr++;
