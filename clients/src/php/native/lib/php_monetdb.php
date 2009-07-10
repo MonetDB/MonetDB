b2299d6b7469aee852049914f34727f3426ad84f
Gabriele Modena <gabriele_modena@users.sourceforge.net>
2009-07-10 16:05:14 +0000

    Added the initial work for the PHP native interface. At the moment the code is not ready for building or release.

---
+++ clients/src/php/native/lib/php_monetdb.php
@@ -0,0 +1,55 @@
+<?php
+	/*
+	Implementation of the driver API.
+	
+	Follows the specifications of the previous Cimpl: http://monetdb.cwi.nl/SQL/Documentation/The-PHP-Library.html
+	
+	- function monetdb_connect() 
+	- function monetdb_disconnect() 
+    - function monetdb_connected() 
+	- function monetdb_query($query) 
+	- function monetdb_fetch_assoc($resource) 
+	- function monetdb_fetch_object($resource) 
+	- function monetdb_num_rows($resource) 
+	- function monetdb_affected_rows($resource) 
+	- function monetdb_last_error() 
+	- function monetdb_insert_id($seq) 
+	- function monetdb_quote_ident($str) 
+	- function monetdb_escape_string($str) 
+
+
+	*/
+
+	require 'php_mapi.inc';
+
+	
+	function monetdb_connect() {
+		mapi_connect();
+	}
+
+
+	function monetdb_disconnect() {
+		mapi_close();
+	}
+	
+	/* Return the operation performed */
+	function monetdb_query($query) {
+		$buf = mapi_write(format_query($query));
+		mapi_query(mapi_read());
+	}
+	
+	function format_query($query) {
+		return "s" . $query . ";";
+	}
+	
+	function monetdb_escape_string($str) {
+		
+	}
+
+	monetdb_connect();
+	
+	monetdb_query("select * FROM tables");
+	
+	monetdb_disconnect();
+		
+?>
