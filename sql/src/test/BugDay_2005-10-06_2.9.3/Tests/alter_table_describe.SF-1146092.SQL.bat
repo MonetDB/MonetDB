3c65483bd6325a9a928a2cb5cf38d6da86ceb241
Fabian Groffen <fabian@cwi.nl>
2009-07-20 17:13:06 +0000

    More correct dirs

--- sql/src/test/BugDay_2005-10-06_2.9.3/Tests/alter_table_describe.SF-1146092.SQL.bat
+++ sql/src/test/BugDay_2005-10-06_2.9.3/Tests/alter_table_describe.SF-1146092.SQL.bat
@@ -8,6 +8,6 @@ set LANG=en_US.UTF-8
 prompt # $t $g  
 echo on
 
-call java nl.cwi.monetdb.client.JdbcClient -h %HOST% -p %MAPIPORT% -d %TSTDB% -f "%TSTSRCBASE%\%TSTDIR%\alter_table_describe.SF-1146092-src.sql"
+call java nl.cwi.monetdb.client.JdbcClient -h %HOST% -p %MAPIPORT% -d %TSTDB% -f "%TSTSRCDIR%\alter_table_describe.SF-1146092-src.sql"
 
 @del .monetdb
--- sql/src/test/BugDay_2005-10-06_2.9.3/Tests/alter_table_describe.SF-1146092.SQL.sh
+++ sql/src/test/BugDay_2005-10-06_2.9.3/Tests/alter_table_describe.SF-1146092.SQL.sh
@@ -8,6 +8,6 @@ EOF
 LANG="en_US.UTF-8"
 export LANG
 
-Mlog -x "$MTIMEOUT java nl.cwi.monetdb.client.JdbcClient -h $HOST -p $MAPIPORT -d $TSTDB -f \"$TSTSRCBASE/$TSTDIR/alter_table_describe.SF-1146092-src.sql\""
+Mlog -x "$MTIMEOUT java nl.cwi.monetdb.client.JdbcClient -h $HOST -p $MAPIPORT -d $TSTDB -f \"$TSTSRCDIR/alter_table_describe.SF-1146092-src.sql\""
 
 rm -f .monetdb
