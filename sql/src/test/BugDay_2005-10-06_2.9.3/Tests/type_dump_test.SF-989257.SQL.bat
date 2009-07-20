a1939833efbd38349c3b0ff9b58071a61d24bf7c
Fabian Groffen <fabian@cwi.nl>
2009-07-20 09:47:49 +0000

    Do not require to be in the target directory

--- sql/src/test/BugDay_2005-10-06_2.9.3/Tests/type_dump_test.SF-989257.SQL.bat
+++ sql/src/test/BugDay_2005-10-06_2.9.3/Tests/type_dump_test.SF-989257.SQL.bat
@@ -8,6 +8,6 @@ set LANG=en_US.UTF-8
 prompt # $t $g  
 echo on
 
-call java nl.cwi.monetdb.client.JdbcClient -h %HOST% -p %MAPIPORT% -d %TSTDB% -f "%RELSRCDIR%\type_dump_test.SF-989257-src.sql"
+call java nl.cwi.monetdb.client.JdbcClient -h %HOST% -p %MAPIPORT% -d %TSTDB% -f "%TSTSRCBASE%\%TSTDIR%\type_dump_test.SF-989257-src.sql"
 
 @del .monetdb
