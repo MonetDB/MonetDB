078d5a01ca414bf20ad4c7921eaa79592489ec48
Fabian Groffen <fabian@cwi.nl>
2009-08-01 18:14:51 +0000

    Add loading test, manually fix create_table script for MonetDB

---
+++ sql/src/test/pg_regress/Tests/load.SQL.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+Mlog -x "$SQL_CLIENT < $RELSRCDIR/../monetdb/create_table.sql"
+
+Mlog "loading data with sed-foo"
+
+# don't load stud_emp, as it's the only data file with \N entries
+# (PostgreSQL NULL notation)
+sed -r \
+	-e s+@abs_srcdir@+$TSTSRCBASE/$TSTDIR+ig \
+	-e '/@abs_builddir@/d' \
+	-e '/DELETE FROM/d' \
+	-e "s/COPY (.*);/COPY INTO \1 USING DELIMITERS '\\\\t\', '\\\\n';/i" \
+	-e '/stud_emp/d' \
+	$RELSRCDIR/../input/copy.source \
+	| $SQL_CLIENT
+
+Mlog -x "$SQL_CLIENT -s 'select count(*) from aggtest;'"
