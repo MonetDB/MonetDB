9df4c52b15ec229c1167a68f4beb6c97449cf5eb
Martin Kersten <mk@cwi.nl>
2009-08-09 08:23:31 +0000

    Remove significant white space for string field.

--- sql/src/test/BugTracker/Tests/variable_stack_crash_load.SF-1711251.sql
+++ sql/src/test/BugTracker/Tests/variable_stack_crash_load.SF-1711251.sql
@@ -2,8 +2,8 @@ create table test1711251 (pre integer not null primary key, size integer not nul
 level smallint not null, kind smallint not null, prop char (32));
 
 copy 3 records into test1711251 from stdin DELIMITERS ',', '\n', '"';
-0, 2, 0, 6, "auctionG.xml"
-1, 1, 1, 1, "foo"
-2, 0, 2, 1, "bar"
+0, 2, 0, 6,"auctionG.xml"
+1, 1, 1, 1,"foo"
+2, 0, 2, 1,"bar"
 
 select * from test1711251;
