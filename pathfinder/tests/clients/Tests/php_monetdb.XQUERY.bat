246d0766a3ac9745ba00ecb2867fd2f726f608b4
Wouter Alink <vzzzbx@users.sourceforge.net>
2009-08-09 22:52:37 +0000

    adding test for xquery via php (still fails, stable output has been crafted manually)

---
+++ pathfinder/tests/clients/Tests/php_monetdb.XQUERY.bat
@@ -0,0 +1,10 @@
+@echo off
+
+call monetdb-clients-config --internal
+
+set testpath=%CLIENTS_PREFIX%\lib\MonetDB\Tests
+
+prompt # $t $g  
+echo on
+
+php -n -d "extension_dir=%phpextensiondir%" -f "%testpath%\xquerysample.php"
