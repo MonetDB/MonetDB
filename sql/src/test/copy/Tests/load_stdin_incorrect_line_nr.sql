3a56ebd5df33012ab7326e802f46b4a96f203588
Niels Nes <niels@cwi.nl>
2009-08-12 07:05:35 +0000

    add missing test. Test line numbers using copy into from stdin

---
+++ sql/src/test/copy/Tests/load_stdin_incorrect_line_nr.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "sys"."number" (
+        "isanumber" int
+);
+COPY 2 RECORDS INTO "number" FROM stdin USING DELIMITERS ';', '\n';
+1
+bla
+
+select * from "number";
+drop table "number";
