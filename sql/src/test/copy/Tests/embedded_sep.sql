b0a590c9954620c29cf72bc240e0b8b3f4c80572
Martin Kersten <mk@cwi.nl>
2009-08-14 07:44:10 +0000

    Cases that we had not covered concisely in the old version.

---
+++ sql/src/test/copy/Tests/embedded_sep.sql
@@ -0,0 +1,24 @@
+-- test the situation where separators are embedded in quoted fields
+create table t_str(s string, t string);
+copy 1 records into t_str from stdin delimiters ',','\n';
+hello,world
+
+copy 1 records into t_str from stdin delimiters ',','\n','"';
+hello,world
+
+copy 1 records into t_str from stdin delimiters ',','\n','"';
+"hello","world"
+
+copy 1 records into t_str from stdin delimiters ',','\n','"';
+"hello,world","all"
+
+copy 1 records into t_str from stdin delimiters ',','\n','"';
+"hello\nworld","all"
+
+copy 1 records into t_str from stdin delimiters ',','\n','"';
+"hello\
+world","all"
+
+select * from t_str;
+
+drop table t_str;
---
+++ sql/src/test/copy/Tests/escaped_char.sql
@@ -0,0 +1,20 @@
+-- test the situation where escapes are embedded in (quoted) fields
+create table t_str(s string, t string);
+copy 3 records into t_str from stdin delimiters ',','\n';
+\\hello,world
+hello\t,world
+hello\n,world
+
+copy 1 records into t_str from stdin delimiters ',','\n','"';
+"hello\"","world"
+
+copy 1 records into t_str from stdin delimiters ',','\n','"';
+hello\,world,"all"
+
+copy 1 records into t_str from stdin delimiters ',','\n','"';
+hello\,world,"all"\
+therest
+
+select * from t_str;
+
+drop table t_str;
