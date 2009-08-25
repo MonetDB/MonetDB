2b92fe03b6c1cad6d6b921688aa5acaf1fe10ff4
Fabian Groffen <fabian@cwi.nl>
2009-08-25 12:07:31 +0000

    kind of dramatic bugfix: make mero_port being recognised, thanks Roberto for pointing it out (backport pending)

--- sql/src/backends/monet5/merovingian/merovingian.c
+++ sql/src/backends/monet5/merovingian/merovingian.c
@@ -439,6 +439,7 @@ main(int argc, char *argv[])
 		{"sql_logdir",         NULL},
 		{"mero_msglog",        NULL},
 		{"mero_errlog",        NULL},
+		{"mero_port",          NULL},
 		{"mero_exittimeout",   NULL},
 		{"mero_pidfile",       NULL},
 		{"mero_doproxy",       NULL},
