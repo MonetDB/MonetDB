7ab1ed409241786bb048b16dc0131aed708fdc2e
Sjoerd Mullender <sjoerd@acm.org>
2009-09-10 09:43:02 +0000

    Added test for bug 2852928: PF: updates broken using algebra front-end?

---
+++ pathfinder/tests/BugTracker/Tests/update-with-timing.SF-2852928.XQUERY.py
@@ -0,0 +1,39 @@
+import os, sys
+try:
+    import subprocess
+except ImportError:
+    # user private copy for old Python versions
+    import MonetDBtesting.subprocess26 as subprocess
+
+def client(cmd, input = None):
+    clt = subprocess.Popen(cmd,
+                           stdin = subprocess.PIPE,
+                           stdout = subprocess.PIPE,
+                           stderr = subprocess.PIPE,
+                           universal_newlines = True)
+    out, err = clt.communicate(input)
+    sys.stdout.write(out)
+    sys.stderr.write(err)
+
+hellodoc = r'''
+<hello x="y" lang="en">
+  <foo>text of node foo<bar>text of node bar</bar></foo>
+  <!-- a comment node -->
+  <?pi action=beep?>
+  <world/>
+  <path>\asfda\asdfasf\asdf</path>
+</hello>
+'''
+
+def main():
+    xq_client = os.getenv('XQUERY_CLIENT').split()
+    # HACK ALERT: create updatable document by appending ,10 to collection name
+    client(xq_client + ['--input=hello-SF.2852928.xml', '--collection=hello-SF.2852928.xml,10'],
+           hellodoc)
+    sys.stdout.write('#~BeginVariableOutput~#\n')
+    client(xq_client + ['-t', '-s', 'do insert <a/> into doc("hello-SF.2852928.xml")/hello'])
+    sys.stdout.write('#~EndVariableOutput~#\n')
+    client(xq_client + ['-s', 'doc("hello-SF.2852928.xml")'])
+    client(xq_client + ['-s', 'pf:del-doc("hello-SF.2852928.xml")'])
+
+main()
