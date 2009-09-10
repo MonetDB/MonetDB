15e4583637f116ed83e2270334523436e6308a09
Fabian Groffen <fabian@cwi.nl>
2009-09-10 09:17:55 +0000

    move lock and release into monetdb.c again, since they are only simple wrappers

--- sql/src/backends/monet5/merovingian/monetdb.c
+++ sql/src/backends/monet5/merovingian/monetdb.c
@@ -487,8 +487,105 @@ command_destroy(int argc, char *argv[])
 	exit(state);
 }
 
-#include "monetdb_lock.c"
-#include "monetdb_release.c"
+static void
+command_lock(int argc, char *argv[])
+{
+	int i;
+	int state = 0;    /* return status */
+	int hadwork = 0;  /* did we do anything useful? */
+
+	if (argc == 1) {
+		/* print help message for this command */
+		command_help(argc + 1, &argv[-1]);
+		exit(1);
+	}
+
+	/* do for each listed database */
+	for (i = 1; i < argc; i++) {
+		hadwork = 1;
+		if (mero_running == 0) {
+			char *e;
+
+			if ((e = db_lock(argv[i])) != NULL) {
+				fprintf(stderr, "%s: %s\n", argv[0], e);
+				free(e);
+				state |= 1;
+				continue;
+			}
+		} else {
+			char *res;
+			char *out;
+
+			out = control_send(&res, mero_control, -1, argv[i], argv[0]);
+			if (out != NULL || strcmp(res, "OK") != 0) {
+				res = out == NULL ? res : out;
+				fprintf(stderr, "%s: %s\n", argv[0], res);
+				state |= 1;
+				free(res);
+				continue;
+			}
+			free(res);
+		}
+		printf("database '%s' is now under maintenance\n", argv[i]);
+	}
+
+	if (hadwork == 0) {
+		command_help(2, &argv[-1]);
+		state |= 1;
+	}
+	exit(state);
+}
+
+static void
+command_release(int argc, char *argv[])
+{
+	int i;
+	int state = 0;    /* return status */
+	int hadwork = 0;  /* did we do anything useful? */
+
+	if (argc == 1) {
+		/* print help message for this command */
+		command_help(argc + 1, &argv[-1]);
+		exit(1);
+	}
+
+	/* do for each listed database */
+	for (i = 1; i < argc; i++) {
+		hadwork = 1;
+		if (mero_running == 0) {
+			char *e;
+
+			if ((e = db_release(argv[i])) != NULL) {
+				fprintf(stderr, "%s: %s\n", argv[0], e);
+				free(e);
+				state |= 1;
+				continue;
+			}
+		} else {
+			char *res;
+			char *out;
+
+			out = control_send(&res, mero_control, -1, argv[i], argv[0]);
+			if (out != NULL || strcmp(res, "OK") != 0) {
+				res = out == NULL ? res : out;
+				fprintf(stderr, "%s: %s\n", argv[0], res);
+				state |= 1;
+				free(res);
+				continue;
+			}
+			free(res);
+		}
+		printf("database '%s' has been taken out of maintenance mode\n",
+				argv[i]);
+	}
+
+	if (hadwork == 0) {
+		command_help(2, &argv[-1]);
+		state |= 1;
+	}
+	exit(state);
+}
+
 
 
 int
