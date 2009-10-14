daade953c6293a031d781650b3228792773aed9e
Stefan Manegold <manegold@cwi.nl>
2009-10-14 12:30:58 +0000

    undo of accidental checkin

--- MonetDB4/src/modules/plain/Tests/bat.py
+++ MonetDB4/src/modules/plain/Tests/bat.py
@@ -2,5 +2,4 @@ import os
 MSERVER =  os.environ['MSERVER']
 TSTDB = os.environ['TSTDB']
 TST = os.environ['TST']
-TSTTRGDIR = os.environ['TSTTRGDIR']
-os.system("%s --dbname=%s --set gdk_mem_pagebits=16 < %s.milS" % (MSERVER, TSTDB, os.path.join(TSTTRGDIR,TST)))
+os.system("%s --dbname=%s --set gdk_mem_pagebits=16 < %s.milS" % (MSERVER, TSTDB, TST))
