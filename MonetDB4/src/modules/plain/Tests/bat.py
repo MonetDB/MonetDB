46f00e6c998bcbda16033aa5a21c48bc7bbc5e20
Stefan Manegold <manegold@cwi.nl>
2009-10-14 12:21:03 +0000

    make sure that mclient knows where to find the Mtest.py created .monetdb

--- MonetDB4/src/modules/plain/Tests/bat.py
+++ MonetDB4/src/modules/plain/Tests/bat.py
@@ -2,4 +2,5 @@ import os
 MSERVER =  os.environ['MSERVER']
 TSTDB = os.environ['TSTDB']
 TST = os.environ['TST']
-os.system("%s --dbname=%s --set gdk_mem_pagebits=16 < %s.milS" % (MSERVER, TSTDB, TST))
+TSTTRGDIR = os.environ['TSTTRGDIR']
+os.system("%s --dbname=%s --set gdk_mem_pagebits=16 < %s.milS" % (MSERVER, TSTDB, os.path.join(TSTTRGDIR,TST)))
