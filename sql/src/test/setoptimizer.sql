06270da5a2b0a8c4cb2a7fc388d13337dd6899f9
Martin Kersten <mk@cwi.nl>
2009-10-24 09:40:13 +0000

    More extensive optimizer test

--- sql/src/test/setoptimizer.sql
+++ sql/src/test/setoptimizer.sql
@@ -3,9 +3,20 @@
 select optimizer;
 set optimizer='off';
 select optimizer;
+set optimizer='off';
+select optimizer;
+
 set optimizer='on';
 select optimizer;
-set optimizer='off';
-set optimizer='default';
+set optimizer='on';
 select optimizer;
+
+set optimizer='mitosis_pipe';
+select optimizer;
+
+-- and some errors
 set optimizer='default,costModel';
+select optimizer;
+
+set optimizer='myfamous_pipe';
+select optimizer;
