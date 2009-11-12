72416296ea4835ccccb83416cb67907965a65092
Martin Kersten <mk@cwi.nl>
2009-11-12 08:32:57 +0000

    First record is passed correctly. Slavelog administration causes problems.

--- MonetDB5/src/modules/mal/Tests/replication.sh
+++ MonetDB5/src/modules/mal/Tests/replication.sh
@@ -1,6 +1,9 @@
 # this script requires merovingian to run
 
 # the minimal sequence to get a single record from master to slave
+#instantiate merovingian if it wasn't already
+merovingian
+mero=$!
 
 monetdb create dbmaster dbslave
 monetdb set master=true dbmaster
@@ -15,15 +18,22 @@ mclient -lsql -d dbslave -s 'create function slavelog ()
     returns table ("commit" timestamp, "valid" timestamp, "error" string)
     external name sql.dump_slave;'
 
-mclient -lsql -d dbmaster -s "insert into tmp values(2);"
+mclient -lsql -d dbmaster -s "insert into tmp values(123);"
 mclient -lsql -d dbmaster -s "select * from tmp;"
 
-# may be some delay in the propagation
-# because master need to flush the file again.
+#log records are flushed with a small delay
 sleep 10
+mclient -lsql -d dbslave -s "select * from tmp;"
+mclient -lsql -d dbslave -s "select * from slavelog();"
+
+# initiate the next propagation on the same log file
+mclient -lsql -d dbmaster -s "insert into tmp values(234);"
 mclient -lsql -d dbslave -s "select * from tmp;"
 mclient -lsql -d dbslave -s "select * from slavelog();"
 
 #remove all stuff
 monetdb stop dbmaster dbslave
 monetdb destroy -f dbmaster dbslave
+
+#remove local merovingian instance
+kill $mero
