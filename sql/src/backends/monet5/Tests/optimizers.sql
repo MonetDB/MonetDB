38faa24b295bddaf1aeb1f164639db0524e05d71
Martin Kersten <mk@cwi.nl>
2009-11-15 16:55:27 +0000

    Move the validation of the optimizer pipeline to the assignment itself, rather then checking them on the first call.

--- sql/src/backends/monet5/Tests/optimizers.sql
+++ sql/src/backends/monet5/Tests/optimizers.sql
@@ -7,46 +7,36 @@ select 'optimizer test:',optimizer;
 set optimizer='off';
 select 'optimizer test:',optimizer;
 
+set optimizer='on';
+select 'optimizer test:',optimizer;
+
 set optimizer='';
 select 'optimizer test:',optimizer;
 
 set optimizer='costModel,coercions,emptySet,accessmode,aliases,commonTerms,accumulators,joinPath,deadcode,reduce,garbageCollector,';
-select 'optimizer test:',optimizer;
 
 set optimizer='coercions,emptySet,accessmode,aliases,commonTerms,accumulators,joinPath,deadcode,reduce,garbageCollector,';
-select 'optimizer test:',optimizer;
 
 set optimizer='emptySet,accessmode,aliases,commonTerms,accumulators,joinPath,deadcode,reduce,garbageCollector,';
-select 'optimizer test:',optimizer;
 
 set optimizer='accessmode,aliases,commonTerms,accumulators,joinPath,deadcode,reduce,garbageCollector,';
-select 'optimizer test:',optimizer;
 
 set optimizer='aliases,commonTerms,accumulators,joinPath,deadcode,reduce,garbageCollector,';
-select 'optimizer test:',optimizer;
 
 set optimizer='commonTerms,accumulators,joinPath,deadcode,reduce,garbageCollector,';
-select 'optimizer test:',optimizer;
 
 set optimizer='accumulators,joinPath,deadcode,reduce,garbageCollector,';
-select 'optimizer test:',optimizer;
 
 set optimizer='joinPath,deadcode,reduce,garbageCollector,';
-select 'optimizer test:',optimizer;
 
 set optimizer='deadcode,reduce,garbageCollector,';
-select 'optimizer test:',optimizer;
 
 set optimizer='reduce,garbageCollector,';
-select 'optimizer test:',optimizer;
 
 set optimizer='garbageCollector,';
-select 'optimizer test:',optimizer;
 
 set optimizer='costModel,coercions,emptySet,accessmode,commonTerms,accumulators,joinPath,deadcode,reduce,garbageCollector,';
-select 'optimizer test:',optimizer;
 
 set optimizer='costModel,coercions,emptySet,accessmode,commonTerms,accumulators,joinPath,deadcode,garbageCollector,';
-select 'optimizer test:',optimizer;
 
 select 'done';
