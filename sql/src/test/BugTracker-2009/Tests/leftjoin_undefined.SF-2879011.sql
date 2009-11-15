365e15048e603e55bc575606a5179661ba08518e
Wouter Alink <vzzzbx@users.sourceforge.net>
2009-11-15 15:54:11 +0000

    adding test for bug 2879011. Fails as reported. Inserted hand-crafted expected output.

---
+++ sql/src/test/BugTracker-2009/Tests/leftjoin_undefined.SF-2879011.sql
@@ -0,0 +1,5 @@
+CREATE TABLE a (name VARCHAR(10));
+CREATE TABLE b (name VARCHAR(10));
+INSERT INTO a VALUES ('a'),('b');
+INSERT INTO b VALUES ('a'),('b');
+SELECT a.name as x, b.name as y FROM a,b WHERE a.name LIKE b.name;
