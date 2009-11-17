123deceb57dde9bcc40e732788fe46c39e4f1b12
Stefan Manegold <manegold@cwi.nl>
2009-11-17 00:27:50 +0000

    clean-up (drop created tables) such that subsequent tests (e.g., like_bug.SF-2878994) do not "suffer"

--- sql/src/test/BugTracker-2009/Tests/leftjoin_undefined.SF-2879011.sql
+++ sql/src/test/BugTracker-2009/Tests/leftjoin_undefined.SF-2879011.sql
@@ -3,3 +3,5 @@ CREATE TABLE b (name VARCHAR(10));
 INSERT INTO a VALUES ('a'),('b');
 INSERT INTO b VALUES ('a'),('b');
 SELECT a.name as x, b.name as y FROM a,b WHERE a.name LIKE b.name;
+DROP table b;
+DROP table a;
