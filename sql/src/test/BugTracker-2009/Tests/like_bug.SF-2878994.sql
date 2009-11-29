d95ab678ae183fa292a0fe959ec94eda522eb496
Niels Nes <niels@cwi.nl>
2009-11-29 16:03:06 +0000

    some more like_bug test script

--- sql/src/test/BugTracker-2009/Tests/like_bug.SF-2878994.sql
+++ sql/src/test/BugTracker-2009/Tests/like_bug.SF-2878994.sql
@@ -10,3 +10,13 @@ select a.x from a,b where a.x LIKE b.x;
 
 drop table b;
 drop table a;
+
+CREATE TABLE a (name VARCHAR(10));
+CREATE TABLE b (name VARCHAR(10));
+INSERT INTO a VALUES ('a'),('b');
+INSERT INTO b VALUES ('a'),('b');
+
+SELECT a.name as x, b.name as y FROM a,b WHERE a.name LIKE b.name;
+
+drop table b;
+drop table a;
