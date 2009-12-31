d5d346e7d16d8849ce6048f0a9f289556840e3ad
Martin Kersten <mk@cwi.nl>
2009-12-31 14:46:59 +0000

    New test added

--- sql/src/test/copy/Tests/int_parse.sql
+++ sql/src/test/copy/Tests/int_parse.sql
@@ -1,4 +1,25 @@
 create table t_int(i int);
+
+-- If a numeric is enclosed as string we have to be careful
+-- It seems that convention is to check for it, turning
+-- the example below into one null value added to the table.
+copy 1 records into t_int from stdin USING DELIMITERS ',','\n','\"' NULL AS '';
+""
+
+select * from t_int;
+
+-- A decimal value entered at a place where we expect an int
+-- should be recognized. It is the residu of dumping an int
+-- as decimal.
+copy 2 records into t_int from stdin USING DELIMITERS ',','\n','\"' NULL AS '';
+3.00
+-4.0000
+
+copy 1 records into t_int from stdin USING DELIMITERS ',','\n','\"' NULL AS '';
+5.1
+
+select * from t_int;
+
 copy 4 records into t_int from stdin delimiters ',','\n' NULL as '';
 1
 nil
