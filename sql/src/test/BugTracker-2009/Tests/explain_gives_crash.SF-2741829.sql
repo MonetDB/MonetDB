04feba3f42b2fb64a0beb6b71ea5f7ae2a11161d
Niels Nes <niels@cwi.nl>
2010-01-26 20:17:52 +0000

    solve dynamic output

--- sql/src/test/BugTracker-2009/Tests/explain_gives_crash.SF-2741829.sql
+++ sql/src/test/BugTracker-2009/Tests/explain_gives_crash.SF-2741829.sql
@@ -1,5 +1,10 @@
+\f csv
 create table blabla(id integer);
+select '#~BeginVariableOutput~#';
 explain alter table blabla add constraint dada unique (id);
 explain alter table blabla add constraint dada unique (id);
+select '#~EndVariableOutput~#';
 alter table blabla drop constraint dada;
+select '#~BeginVariableOutput~#';
 explain alter table blabla add constraint dada unique (id);
+select '#~EndVariableOutput~#';
