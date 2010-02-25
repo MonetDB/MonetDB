bdd02a786a44d4b36c4402e6f511129e448047f6
Niels Nes <niels@cwi.nl>
2010-02-25 13:11:40 +0000

    added test for SQL: got assertion for type-mismatch

---
+++ sql/src/test/BugTracker-2010/Tests/correlated_update_type_mismatch.SF-2947841.sql
@@ -0,0 +1,8 @@
+
+create table x (id1 varchar(1000), id2 int);
+create table dict (id int, x varchar(100));
+
+UPDATE dict SET id = (SELECT id2 FROM "x" WHERE id1=dict.id);
+
+drop table x;
+drop table dict;
