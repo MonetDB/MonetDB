000f413a7ccf9898cc1f635ef8fcb306dbe42931
Sjoerd Mullender <sjoerd@acm.org>
2010-04-16 14:45:22 +0000

    Test for bug 2926454.

---
+++ sql/src/test/BugTracker-2010/Tests/rank-over-crash.SF-2926454.sql
@@ -0,0 +1,20 @@
+-- preparation
+create table score_table (
+s_name varchar(20),
+subject varchar(10),
+score int
+);
+
+insert into score_table values('foo','english',70);
+insert into score_table values('foo','history',50);
+insert into score_table values('foo','math',60);
+insert into score_table values('bar','english',45);
+insert into score_table values('bar','history',75);
+
+-- this causes an assertion failure:
+select
+s_name,
+sum(score) as totalscore,
+rank() over (order by sum(score) desc) -- cannot use alias 'totalscore' here
+from score_table
+group by s_name;
