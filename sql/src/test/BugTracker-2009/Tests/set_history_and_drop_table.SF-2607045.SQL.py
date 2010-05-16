6497552f293b4cf8ec208f164f2833727331d40b
Niels Nes <niels@cwi.nl>
2010-05-16 11:47:07 +0200

    approved output after database gets loaded with extra set of functions/tables etc.

--- sql/src/test/BugTracker-2009/Tests/set_history_and_drop_table.SF-2607045.SQL.py
+++ sql/src/test/BugTracker-2009/Tests/set_history_and_drop_table.SF-2607045.SQL.py
@@ -4,10 +4,6 @@ from MonetDBtesting import process
 def main():
     dir = os.getenv('TSTSRCDIR')
     clcmd = str(os.getenv('SQL_CLIENT'))
-    sys.stdout.write('Load history\n')
-    clt1 = process.client('sql', args = [os.path.join(dir,'..', '..','..','sql','history.sql')], stdout = process.PIPE)
-    out, err = clt1.communicate()
-    sys.stdout.write(out)
     sys.stdout.write('Run test\n')
     clt1 = process.client('sql', args = [os.path.join(dir,'..','set_history_and_drop_table.SF-2607045.sql')], stdout = process.PIPE)
     out, err = clt1.communicate()
