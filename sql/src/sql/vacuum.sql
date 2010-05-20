7e9a45cd0f196a6bad43fdd8dae4a03687bf78fd
Unknown <mk@cwi.nl>
2010-05-20 17:03:37 +0200

    Primitives to vacuum a relational table has been added. One scheme is to simple shrink the table by removing the deleted entries. It retains order properties. The other scheme re-uses the delete space by reshuffling the data. It looses order properties. [ALpha code, to be tested]

---
+++ sql/src/sql/vacuum.sql
@@ -0,0 +1,29 @@
+-- The contents of this file are subject to the MonetDB Public License
+-- Version 1.1 (the "License"); you may not use this file except in
+-- compliance with the License. You may obtain a copy of the License at
+-- http://monetdb.cwi.nl/Legal/MonetDBLicense-1.1.html
+--
+-- Software distributed under the License is distributed on an "AS IS"
+-- basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+-- License for the specific language governing rights and limitations
+-- under the License.
+--
+-- The Original Code is the MonetDB Database System.
+--
+-- The Initial Developer of the Original Code is CWI.
+-- Copyright August 2008-2010 MonetDB B.V.
+-- All Rights Reserved.
+
+-- Vacuum a relational table should be done with care.
+-- For, the oid's are used in join-indices.
+
+-- Vacuum of tables may improve IO performance and disk footprint.
+-- The foreign key constraints should be dropped before
+-- and re-established after the cluster operation.
+
+create procedure shrink(sys string, tab string)
+	external name sql.shrink;
+
+create procedure reuse(sys string, tab string)
+	external name sql.reuse;
+
