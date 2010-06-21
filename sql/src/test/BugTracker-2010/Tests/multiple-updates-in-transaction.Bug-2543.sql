323974a3a42cd3278f9b3ef1cf301da1ae21b717
Niels Nes <niels@cwi.nl>
2010-06-21 20:03:55 +0200

    add test for Bugzilla Bug 2543   	multiple update statements in one transaction lose updates

---
+++ sql/src/test/BugTracker-2010/Tests/multiple-updates-in-transaction.Bug-2543.sql
@@ -0,0 +1,13 @@
+CREATE TABLE people (id TINYINT PRIMARY KEY, name VARCHAR(128) NOT NULL);
+INSERT INTO people (id,name) SELECT 0,'Phil Ivey';
+INSERT INTO people (id,name) SELECT 1,'Michael Jordan';
+INSERT INTO people (id,name) SELECT 2,'Lionel Messi';
+SELECT * FROM people;
+START TRANSACTION;
+UPDATE people SET id = -1 WHERE name='Phil Ivey';
+SELECT * FROM people;
+UPDATE people SET id = -2 WHERE name='Phil Ivey';
+SELECT * FROM people;
+COMMIT;
+SELECT * FROM people;
+DROP TABLE people;
