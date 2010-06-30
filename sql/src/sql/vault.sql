c29120980e29bc81b63e6a012fb6ad10cee34525
Unknown <mk@cwi.nl>
2010-06-30 21:27:45 +0200

    Initial checkin for Vault structure The first phase of the vault infrastructure to accomodate sci repositories. See description in vault.mx

---
+++ sql/src/sql/vault.sql
@@ -0,0 +1,34 @@
+-- The contents of this file are subject to the MonetDB Public License
+-- Version 1.1 (the "License"); you may not use this file except in
+-- compliance with the License. You may obtain a copy of the License at
+-- http://monetdb.cwi.nl/Legal/MonetDBLicense-1.1.html
+--
+-- Software distributed under the License is distributed on an "AS IS"
+-- basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+-- License for the specific language governing rights and limitations
+-- under the License.
+--
+-- The Original Code is the MonetDB Database System.
+--
+-- The Initial Developer of the Original Code is CWI.
+-- Copyright August 2008-2010 MonetDB B.V.
+-- All Rights Reserved.
+
+-- The data vault interface for SQL
+CREATE TABLE sys.vault (
+vid             int PRIMARY KEY,-- Internal key
+kind            string,         -- vault kind (CSV, MSEED, FITS,..)
+source          string,         -- remote file name for cURL to access
+refresh         boolean,        -- refresh each time of access
+cached          timestamp,      -- copy stored locally
+target          string          -- file name of source file in vault
+);
+
+create function getVaultDir(nme string)
+returns string
+external sql.getVaultDir;
+
+-- refresh the vault
+create procedure refreshVault(nme string)
+external sql.refreshVault;
+
