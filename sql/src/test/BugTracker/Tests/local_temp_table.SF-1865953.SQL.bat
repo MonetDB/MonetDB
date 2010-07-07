6980d3b4b33b802a4eee33bb9e66714787e40f3c
Stefan Manegold <Stefan.Manegold@cwi.nl>
2010-07-07 18:45:22 +0200

    use .monetdb file instead of -P option of mclient
    the -P of mclient to specify the password has been removed;
    hence, we need to create a local temporary .monetdb file instead

--- sql/src/test/BugTracker/Tests/local_temp_table.SF-1865953.SQL.bat
+++ sql/src/test/BugTracker/Tests/local_temp_table.SF-1865953.SQL.bat
@@ -1,2 +1,14 @@
-@call mclient -lsql -umonetdb -Pmonetdb -h %HOST% -p %MAPIPORT% %RELSRCDIR%\local_temp_table_data.SF-1865953.sql
-@call mclient -lsql -umonetdb -Pmonetdb -h %HOST% -p %MAPIPORT%  
+@echo off
+
+echo user=monetdb>	.monetdb
+echo password=monetdb>>	.monetdb
+
+set LANG=en_US.UTF-8
+
+prompt # $t $g  
+echo on
+
+call mclient -lsql -h %HOST% -p %MAPIPORT% %RELSRCDIR%\local_temp_table_data.SF-1865953.sql
+call mclient -lsql -h %HOST% -p %MAPIPORT%  
+
+@del .monetdb
