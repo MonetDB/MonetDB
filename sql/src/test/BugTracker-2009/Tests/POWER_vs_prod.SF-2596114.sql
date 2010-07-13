de0526c61bdc8926ab3b29e0cf61643c6d229ef3
Stefan Manegold <Stefan.Manegold@cwi.nl>
2010-07-13 10:16:33 +0200

    disable parallelism (mitosis & dataflow) to avoid ambiguous EXPLAIN results

--- sql/src/test/BugTracker-2009/Tests/POWER_vs_prod.SF-2596114.sql
+++ sql/src/test/BugTracker-2009/Tests/POWER_vs_prod.SF-2596114.sql
@@ -1,3 +1,5 @@
+-- disable parallelism (mitosis & dataflow) to avoid ambiguous results 
+set optimizer='inline,remap,evaluate,costModel,coercions,emptySet,aliases,mergetable,deadcode,commonTerms,joinPath,reorder,deadcode,reduce,history,multiplex,garbageCollector';
 create table SF_2596114 (x double);
 explain select power(x,2) from SF_2596114;
 explain select x*x from SF_2596114;
