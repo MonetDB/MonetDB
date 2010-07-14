cfe912fc89dea0f7cabf408d455fedc8c7c3e45f
Arjen de Rijke <arjen.de.rijke@cwi.nl>
2010-07-14 09:50:39 +0200

    Add test for bug #2611, creating second function with existing signature succeeds

---
+++ sql/src/test/BugTracker-2010/Tests/second_function_existing_signature.Bug-2611.sql
@@ -0,0 +1,24 @@
+create table table_a (
+    column_a int,
+    column_b int
+);
+
+create function next_a()
+returns integer
+begin
+    declare col_a integer;
+    set col_a = 1;
+
+    return col_a;
+end;
+
+create function next_a()
+returns integer
+begin
+    declare col_a integer;
+    set col_a = 1;
+
+    return col_a;
+end;
+
+drop function next_a;
