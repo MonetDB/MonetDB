0c50666f466c3caad22d1fa47e6b81f9722fe0d0
Stefan Manegold <Stefan.Manegold@cwi.nl>
2010-07-18 11:02:44 +0200

    disable parallelism (mitosis & dataflow) to avoid ambiguous explain output

--- sql/src/test/BugTracker/Tests/jdbc_no_debug.SF-1739356-data.sql
+++ sql/src/test/BugTracker/Tests/jdbc_no_debug.SF-1739356-data.sql
@@ -1,3 +1,5 @@
 debug select count(*) from tables;
 plan select count(*) from tables;
+-- disable parallelism (mitosis & dataflow) to avoid ambiguous results 
+set optimizer='inline,remap,evaluate,costModel,coercions,emptySet,aliases,mergetable,deadcode,commonTerms,joinPath,reorder,deadcode,reduce,history,multiplex,garbageCollector';
 explain select count(*) from tables;
