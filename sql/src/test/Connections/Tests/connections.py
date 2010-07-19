fcaca49b3c71337f2f1eb3d5a0db727e4c8bbf7e
Sjoerd Mullender <sjoerd@acm.org>
2010-07-19 10:58:40 +0200

    Serialize output.  This should fix bug 2621.

--- sql/src/test/Connections/Tests/connections.py
+++ sql/src/test/Connections/Tests/connections.py
@@ -11,7 +11,8 @@ def remote_server_start(x,s):
     port = os.getenv('MAPIPORT', '50000')
     srv = process.server('sql', mapiport = int(port) + 1,
                          dbname = '%s_test1' % os.getenv('TSTDB'),
-                         stdin = process.PIPE)
+                         stdin = process.PIPE,
+                         stdout = process.PIPE, stderr = process.PIPE)
     time.sleep(5)                      # give server time to start
     return srv
 
@@ -22,12 +23,13 @@ def server_start(x,s):
     sys.stderr.write('#mserver\n')
     sys.stdout.flush()
     sys.stderr.flush()
-    srv = process.server('sql', stdin = process.PIPE)
+    srv = process.server('sql', stdin = process.PIPE,
+                         stdout = process.PIPE, stderr = process.PIPE)
     time.sleep(5)                      # give server time to start
     return srv
 
 def server_stop(srv):
-    srv.communicate()
+    return srv.communicate()
 
 def client_load_file(clt, port, file):
     f = open(file, 'r')
@@ -37,7 +39,6 @@ def client_load_file(clt, port, file):
         clt.stdin.write(line)
     f.close()
 
-
 def client(x,s, c, file):
     sys.stdout.write('\nserver %d%d, client %d\n' % (x,s,c))
     sys.stderr.write('\nserver %d%d, client %d\n' % (x,s,c))
@@ -45,25 +46,54 @@ def client(x,s, c, file):
     sys.stderr.write('#client%d\n' % x)
     sys.stdout.flush()
     sys.stderr.flush()
-    clt = process.client('sql', stdin = process.PIPE)
+    clt = process.client('sql', stdin = process.PIPE,
+                         stdout = process.PIPE, stderr = process.PIPE)
     port = int(os.getenv('MAPIPORT', '50000'))
     client_load_file(clt, port, file)
-    clt.communicate()
-
+    return clt.communicate()
 
 def clients(x):
     s = 0
-    s += 1; srv = server_start(x,s)
-    s += 1; remote_srv = remote_server_start(x,s)
+    s += 1
+    srv = server_start(x,s)
+
+    s += 1
+    remote_srv = remote_server_start(x,s)
+
     c = 0
-    c += 1; client(x, s, c, os.path.join(os.getenv('RELSRCDIR'), '..', 'connections_syntax.sql'))
-    c += 1; client(x, s, c, os.path.join(os.getenv('RELSRCDIR'), '..', 'connections_semantic.sql'))
-    c += 1; client(x, s, c, os.path.join(os.getenv('RELSRCDIR'), '..', 'connections_default_values.sql'))
-    server_stop(remote_srv)
-    server_stop(srv)
+
+    c += 1
+    out, err = client(x, s, c,
+                      os.path.join(os.getenv('RELSRCDIR'), '..',
+                                   'connections_syntax.sql'))
+    sys.stdout.write(out)
+    sys.stderr.write(err)
+
+    c += 1
+    out, err = client(x, s, c,
+                      os.path.join(os.getenv('RELSRCDIR'), '..',
+                                   'connections_semantic.sql'))
+    sys.stdout.write(out)
+    sys.stderr.write(err)
+
+    c += 1
+    out, err = client(x, s, c,
+                      os.path.join(os.getenv('RELSRCDIR'), '..',
+                                   'connections_default_values.sql'))
+    sys.stdout.write(out)
+    sys.stderr.write(err)
+
+    out, err = server_stop(remote_srv)
+    sys.stdout.write(out)
+    sys.stderr.write(err)
+
+    out, err = server_stop(srv)
+    sys.stdout.write(out)
+    sys.stderr.write(err)
 
 def main():
     x = 0
-    x += 1; clients(x)
+    x += 1
+    clients(x)
 
 main()
