bfc02d62512bac7867addb16d569ca351c96f362
Stefan Manegold <Stefan.Manegold@cwi.nl>
2010-08-03 11:58:57 +0200

    merged tests python_test_monetdb_sql_dbapi20 & python_test_monetdb_sql_capabilities into test python_test_monetdb_sql to match with the (not that) new setup of the MonetDB python code

--- sql/src/test/mapi/Tests/python_test_monetdb_sql_dbapi20.SQL.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# must be aligned with the installation directory chosen in
-# clients/src/python/test/Makefile.ag
-testpath="`monetdb-clients-config --pkglibdir`/Tests"
-PYTHONPATH=$testpath:$PYTHONPATH
-export PYTHONPATH
-
-Mlog -x "python $testpath/dbapi20_monetdb.py"
