b93f8d99602f1107540c4d3101f0e6d052a84399
Stefan Manegold <Stefan.Manegold@cwi.nl>
2010-08-04 11:47:37 +0200

    merged tests python_test_monetdb_sql_dbapi20 & python_test_monetdb_sql_capabilities into test python_test_monetdb_sql to match with the (not that) new setup of the MonetDB python code

---
+++ sql/src/test/mapi/Tests/python_test_monetdb_sql.SQL.bat
@@ -0,0 +1,13 @@
+@echo off
+
+call monetdb-clients-config --internal
+
+rem must be aligned with the installation directory chosen in
+rem clients/src/python/test/Makefile.ag
+set testpath=%TSTSRCBASE%\..\python\test
+set PYTHONPATH=%testpath%;%PYTHONPATH%
+
+prompt # $t $g  
+echo on
+
+python %testpath%/runtests.py
