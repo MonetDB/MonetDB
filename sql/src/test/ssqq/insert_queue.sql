875787e235362eb02124a37781cfd737ec14b93d
Arjen de Rijke <arjen.de.rijke@cwi.nl>
2010-08-05 15:14:39 +0200

    Add tests for skyserver queue sql queries

---
+++ sql/src/test/ssqq/insert_queue.sql
@@ -0,0 +1,44 @@
+select * from quser;
+
+select * from query;
+
+select * from queue;
+
+declare id_result int;
+
+set id_result = -1;
+
+set id_result = insert_queue(1, 'monet', 'linux');
+
+select id_result;
+
+select * from queue;
+
+set id_result = -1;
+
+set id_result = insert_queue(10, 'monet', 'linux');
+
+select id_result;
+
+select * from queue;
+
+set id_result = -1;
+
+set id_result = insert_queue(2, 'monet', 'linux');
+
+select id_result;
+
+select * from queue;
+
+set id_result = -1;
+
+set id_result = insert_queue(3, 'monet', 'linux');
+
+select id_result;
+
+select * from quser;
+
+select * from query;
+
+select * from queue;
+
---
+++ sql/src/test/ssqq/insert_quser.sql
@@ -0,0 +1,43 @@
+select * from quser;
+
+select * from query;
+
+select * from queue;
+
+declare id_result int;
+
+set id_result = -1;
+
+set id_result = insert_quser(NULL, NULL, NULL, NULL);
+
+select id_result;
+
+select * from quser;
+
+set id_result = -1;
+
+set id_result = insert_quser('user1', 'email1@example.com', 1, 'secret');
+
+select id_result;
+
+select * from quser;
+
+set id_result = -1;
+
+set id_result = insert_quser('user2', 'email2@example.com', 2, 'notsecret');
+
+select id_result;
+
+select * from quser;
+
+set id_result = -1;
+
+set id_result = insert_quser('user1', 'email3@example.com', 3, 'nosecret');
+
+select id_result;
+
+select * from quser;
+
+select * from query;
+
+select * from queue;
