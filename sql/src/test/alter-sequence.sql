97ceaf6b367a02af50f3e696190009e9a8b38bda
Sjoerd Mullender <sjoerd@acm.org>
2010-09-09 15:49:27 +0200

    Don't call every test table "test".

--- sql/src/test/alter-sequence.sql
+++ sql/src/test/alter-sequence.sql
@@ -1,9 +1,9 @@
 START TRANSACTION;
 CREATE SEQUENCE "my_test_seq" AS INTEGER START WITH 2;
-CREATE TABLE test (t int default next value for "my_test_seq", v char);
-INSERT INTO test(v)  VALUES ('a');
-INSERT INTO test VALUES (10, 'b');
-ALTER SEQUENCE "my_test_seq" RESTART WITH (SELECT max(t) + 1 FROM test);
-INSERT INTO test(v)  VALUES ('c');
-SELECT * FROM test;
+CREATE TABLE altseqtest (t int default next value for "my_test_seq", v char);
+INSERT INTO altseqtest(v)  VALUES ('a');
+INSERT INTO altseqtest VALUES (10, 'b');
+ALTER SEQUENCE "my_test_seq" RESTART WITH (SELECT max(t) + 1 FROM altseqtest);
+INSERT INTO altseqtest(v)  VALUES ('c');
+SELECT * FROM altseqtest;
 ROLLBACK;
--- sql/src/test/string.sql
+++ sql/src/test/string.sql
@@ -1,50 +1,50 @@
-create table test ( str VARCHAR(20), str2 VARCHAR(20));
-insert into test values ('', 'test');
-insert into test values ('test', '');
-insert into test values ('','');
-insert into test values (' Test ','');
-select * from test;
+create table stringtest ( str VARCHAR(20), str2 VARCHAR(20));
+insert into stringtest values ('', 'test');
+insert into stringtest values ('test', '');
+insert into stringtest values ('','');
+insert into stringtest values (' Test ','');
+select * from stringtest;
 
-select length(str), str from test;
+select length(str), str from stringtest;
 
-select substring(str from 2 for 8), str2 from test;
-select substring(str, 2, 8), str2 from test;
-select substring(str from 2 for 1), str2 from test;
-select substring(str, 2, 1), str2 from test;
-select substring(str from 2), str2 from test;
-select substring(str, 2), str2 from test;
+select substring(str from 2 for 8), str2 from stringtest;
+select substring(str, 2, 8), str2 from stringtest;
+select substring(str from 2 for 1), str2 from stringtest;
+select substring(str, 2, 1), str2 from stringtest;
+select substring(str from 2), str2 from stringtest;
+select substring(str, 2), str2 from stringtest;
 
-select position(str in str2), str, str2 from test;
-select locate(str,str2), str, str2 from test;
+select position(str in str2), str, str2 from stringtest;
+select locate(str,str2), str, str2 from stringtest;
 
-select ascii(str), str from test;
-select code(ascii(str)), str from test;
+select ascii(str), str from stringtest;
+select code(ascii(str)), str from stringtest;
 
-select left(str,3), str from test;
-select right(str,3), str from test;
+select left(str,3), str from stringtest;
+select right(str,3), str from stringtest;
 
-select lower(str), str from test;
-select lcase(str), str from test;
-select upper(str), str from test;
-select ucase(str), str from test;
+select lower(str), str from stringtest;
+select lcase(str), str from stringtest;
+select upper(str), str from stringtest;
+select ucase(str), str from stringtest;
 
-select trim(str), str from test;
-select ltrim(str), str from test;
-select rtrim(str), str from test;
+select trim(str), str from stringtest;
+select ltrim(str), str from stringtest;
+select rtrim(str), str from stringtest;
 
-select insert(str,2,4,str2), str, str2 from test;
-select replace(str,'t',str), str, str2 from test;
+select insert(str,2,4,str2), str, str2 from stringtest;
+select replace(str,'t',str), str, str2 from stringtest;
 
-select repeat(str,4), str from test;
-select ascii(4), str from test;
+select repeat(str,4), str from stringtest;
+select ascii(4), str from stringtest;
 
-select str,str2,soundex(str),soundex(str2), editdistance2(soundex(str),soundex(str2)), editdistance(soundex(str),soundex(str2)), difference(str,str2) from test;
+select str,str2,soundex(str),soundex(str2), editdistance2(soundex(str),soundex(str2)), editdistance(soundex(str),soundex(str2)), difference(str,str2) from stringtest;
 
-insert into test values ('monetdbiscool', 'coolismonetdb');
-select levenshtein(str,str2, 1, 2, 3), str, str2 from test;
+insert into stringtest values ('monetdbiscool', 'coolismonetdb');
+select levenshtein(str,str2, 1, 2, 3), str, str2 from stringtest;
 
-insert into test values ('monetdb45is+ cool', '  123123  123123  ');
-select qgramnormalize(str) , qgramnormalize(str2), str, str2 from test;
+insert into stringtest values ('monetdb45is+ cool', '  123123  123123  ');
+select qgramnormalize(str) , qgramnormalize(str2), str, str2 from stringtest;
 
 
-drop table test;
+drop table stringtest;
