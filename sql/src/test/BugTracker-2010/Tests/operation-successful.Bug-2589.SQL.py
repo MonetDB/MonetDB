ea5b580c6d2e7e0a325ddfe35523613d54cc6f35
Sjoerd Mullender <sjoerd@acm.org>
2010-10-08 10:22:29 +0200

    Added test for bug 2589.

---
+++ sql/src/test/BugTracker-2010/Tests/operation-successful.Bug-2589.SQL.py
@@ -0,0 +1,18 @@
+import sys
+from MonetDBtesting import process
+
+c = process.client(lang = 'sql',
+                   args = ['-fsql',
+                           '-s', 'create table t2589 as select * from tables with data'],
+                   stdout = process.PIPE, stderr = process.PIPE)
+out, err = c.communicate()
+sys.stdout.write(out)
+sys.stderr.write(err)
+
+c = process.client(lang = 'sql',
+                   args = ['-fsql',
+                           '-s', 'drop table t2589'],
+                   stdout = process.PIPE, stderr = process.PIPE)
+out, err = c.communicate()
+sys.stdout.write(out)
+sys.stderr.write(err)
