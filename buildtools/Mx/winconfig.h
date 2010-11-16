4c34f8271e6757eaa9908e29779e2bd6a6408651
Sjoerd Mullender <sjoerd@acm.org>
2010-11-16 15:37:06 +0100

    utime is *only* used in Mx, and utime(file,NULL) works everywhere.

--- buildtools/Mx/winconfig.h
+++ buildtools/Mx/winconfig.h
@@ -103,11 +103,14 @@
 /* Define to 1 if you have the <sys/types.h> header file. */
 #define HAVE_SYS_TYPES_H 1
 
+/* Define to 1 if you have the <sys/utime.h> header file. */
+#define HAVE_SYS_UTIME_H 1
+
 /* Define to 1 if you have the <unistd.h> header file. */
 /* #undef HAVE_UNISTD_H */
 
 /* Define to 1 if you have the `utime' function. */
-/* #undef HAVE_UTIME */
+#define HAVE_UTIME 1
 
 /* Define to 1 if you have the <utime.h> header file. */
 /* #undef HAVE_UTIME_H */
