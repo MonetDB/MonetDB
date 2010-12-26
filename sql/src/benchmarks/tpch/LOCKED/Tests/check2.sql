35ca150a80db0acc928e4c743c3672f0282e2b7a
Stefan Manegold <Stefan.Manegold@cwi.nl>
2010-12-27 00:38:23 +0100

    extended sanity check, whether TPCH data is loaded loaded correctly

--- sql/src/benchmarks/tpch/LOCKED/Tests/check2.sql
+++ sql/src/benchmarks/tpch/LOCKED/Tests/check2.sql
@@ -6,3 +6,12 @@ select count(*) from part;
 select count(*) from region;
 select count(*) from supplier;
 select count(*) from lineitem;
+
+select * from region   limit 9;
+select * from nation   limit 9;
+select * from supplier limit 9;
+select * from customer limit 9;
+select * from part     limit 9;
+select * from partsupp limit 9;
+select * from orders   limit 9;
+select * from lineitem limit 9;
