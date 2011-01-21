9ad1b11f8f0fa30bcc52d92afd3bb537787e90ea
Jennie Zhang <y.zhang@cwi.nl>
2011-01-21 11:10:27 +0100

    corrected syntax error in query

--- sql/test/sciql/Tests/sci04.sql
+++ sql/test/sciql/Tests/sci04.sql
@@ -1,6 +1,6 @@
 INSERT INTO grid VALUES(1,1,25);
 
-DELETE matrix WHERE x = 2;
+DELETE FROM matrix WHERE x = 2;
 INSERT INTO matrix SELECT x-1, y, v FROM matrix WHERE x > 2;
 INSERT INTO matrix SELECT x, y, 0 FROM matrix WHERE x = 3;
 INSERT INTO matrix(v) SELECT v FROM stipes;
