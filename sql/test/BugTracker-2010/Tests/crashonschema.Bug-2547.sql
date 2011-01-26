ce7b1fd5f9e2099c19a60e2e62c9fd15f657eb25
Fabian Groffen <fabian@cwi.nl>
2011-01-26 14:33:38 +0100

    tests: don't use a transaction if errors are expected
    make sure we continue processing, since the file clearly seems to try a
    couple of faulty statements

--- sql/test/BugTracker-2010/Tests/crashonschema.Bug-2547.sql
+++ sql/test/BugTracker-2010/Tests/crashonschema.Bug-2547.sql
@@ -1,6 +1,5 @@
-start transaction;
-
 create schema intro1;
+-- hint: should use "authorization" instead
 create schema intro2 authorized monetdb;
 create schema authorized monetdb;
 
@@ -10,9 +9,12 @@ drop schema intro2;
 create schema intro1
  default character set whatever;
 
+drop schema intro1;
+
 -- following gives unexpected error
 create table t2010(i int);
 create schema intro3 
   grant insert on t2010 to monetdb;
 
-rollback;
+drop table t2010;
+drop schema intro3;
