6d8de198989c7cdf87296de7203fcb9e4d95cb00
Unknown <mk@cwi.nl>
2011-01-27 00:06:23 +0100

    Complete the ngram

--- sql/test/sciql/Tests/ngram.sql
+++ sql/test/sciql/Tests/ngram.sql
@@ -1,6 +1,8 @@
 -- The n-gram case was proposed by DLR
 -- it shows the use of expanded tiling
 
-select v1,v2,v3,v4, count( *) from
-	( select img.v as (v1,v2,v3,v4) from img group by img[x:x+4][y]) as x
+create array img( x integer dimension[5], y integer dimension[5], val float);
+
+select v1,v2,v3,v4, count( *) 
+from ( select img.val as (v1,v2,v3,v4) from img group by img[x:x+3][y]) as x
 group by v1,v2,v3,v4;
