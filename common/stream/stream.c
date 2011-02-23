86a558bb0de9c9306fc00d770950469087a79250
Sjoerd Mullender <sjoerd@acm.org>
2011-02-23 10:56:00 +0100

    Use standard blurb for alloca(). This should fix bug 2778.

--- common/stream/stream.c
+++ common/stream/stream.c
@@ -92,8 +92,19 @@
 #ifdef HAVE_LIBBZ2
 #include <bzlib.h>
 #endif
-#ifdef HAVE_MALLOC_H
-#include <malloc.h>		/* for alloca on Windows */
+
+#ifdef HAVE_ALLOCA_H
+# include <alloca.h>
+#elif defined __GNUC__
+# define alloca __builtin_alloca
+#elif defined _AIX
+# define alloca __alloca
+#elif defined _MSC_VER
+# include <malloc.h>
+# define alloca _alloca
+#else
+# include <stddef.h>
+void *alloca(size_t);
 #endif
 
 #ifndef SHUT_RD
