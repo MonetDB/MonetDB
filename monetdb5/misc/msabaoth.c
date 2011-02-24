a9e1c5432b7cc3f7b23326e469f0957a932bdc45
Fabian Groffen <fabian@cwi.nl>
2011-02-24 08:39:12 +0100

    msabaoth.c: add alloca blurb to fix compilation on Solaris
    msabaoth.c:180: error: implicit declaration of function ‘alloca’
    http://monetdb.cwi.nl/testing/projects/monetdb/CurrentNew/logs/39194:176ae2b45904/GNU-Solaris-sparc/make.html#l758

--- monetdb5/misc/msabaoth.c
+++ monetdb5/misc/msabaoth.c
@@ -42,6 +42,20 @@
 #include <string.h> /* for getting error messages */
 #include <assert.h>
 
+#ifdef HAVE_ALLOCA_H
+# include <alloca.h>
+#elif defined __GNUC__
+# define alloca __builtin_alloca
+#elif defined _AIX
+# define alloca __alloca
+#elif defined _MSC_VER
+# include <malloc.h>
+# define alloca _alloca
+#else
+# include <stddef.h>
+void *alloca(size_t);
+#endif
+
 #include "msabaoth.h"
 #include "mutils.h"
 
