421716b910c7cba3a14532970198bfa2647378aa
Stefan Manegold <Stefan.Manegold@cwi.nl>
2011-03-30 23:07:59 +0200

    add "order by" to get deterministic outout order

--- sql/test/BugDay_2005-12-19_2.9.3/Tests/select_from_env.SF-1240701.1242164.sql
+++ sql/test/BugDay_2005-12-19_2.9.3/Tests/select_from_env.SF-1240701.1242164.sql
@@ -1,2 +1,2 @@
 select * from env() as env where name = ( select 'prefix' from env() as env );
-select name from env() as env where 1 in ( select 1 from env() as env );
+select name from env() as env where 1 in ( select 1 from env() as env ) order by name;
