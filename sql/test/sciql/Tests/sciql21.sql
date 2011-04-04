4ac6ea253e6268b0af3111b8f947c6145fe878b2
Jennie Zhang <y.zhang@cwi.nl>
2011-04-04 11:30:49 +0200

    fix testing query
    external (function) name should not be given as a string

--- sql/test/sciql/Tests/sciql21.sql
+++ sql/test/sciql/Tests/sciql21.sql
@@ -2,4 +2,4 @@ CREATE FUNCTION markov(
   input ARRAY( x int DIMENSION, y int DIMENSION, f float),
   steps integer)
 RETURNS ARRAY( x int DIMENSION, y int DIMENSION, f float) 
-EXTERNAL NAME 'markov.loop';
+EXTERNAL NAME markov.loop;
