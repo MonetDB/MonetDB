9673dba678766ae782e55d8b692efe9a5d54af3d
Jennie Zhang <y.zhang@cwi.nl>
2011-04-05 15:41:56 +0200

    Add the query errors.sql to be tested, and its stable.{out,err}. Minor changes in errors.sql

--- sql/test/sciql/Tests/errors.sql
+++ sql/test/sciql/Tests/errors.sql
@@ -9,26 +9,25 @@ create array err3( x char dimension[1:128:1], v float);
 create array err4( s varchar(25) dimension['a':'z':1], v float);
 create array err5( s varchar(25) dimension[1:3:1], v float);
 
--- array dimensions may have undetermined step sizes
-
-create array array10(x integer dimension[1:128:*], v float);
-
 -- default value expressions should be of the proper type
-
-create array array15( x integer dimension[1:128:1],
+create array err6( x integer dimension[1:128:1],
 	v float default 'unknown');
-create array array16( x integer dimension[1:128:1],
+create array err7( x integer dimension[1:128:1],
 	v float default v > 0);
-create array array17( x integer dimension[1:128:1],
+create array err8( x integer dimension[1:128:1],
 	v float default true);
+
+-- array dimensions may have undetermined step sizes
+create array array10(x integer dimension[1:128:*], v float);
 
 drop array err1;
 drop array err2;
 drop array err3;
 drop array err4;
 drop array err5;
+drop array err6;
+drop array err7;
+drop array err8;
+
 drop array array10;
-drop array array15;
-drop array array16;
-drop array array17;
 
