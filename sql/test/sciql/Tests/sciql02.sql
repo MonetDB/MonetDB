7f2d7e91ff6ae91f61ee3fc359054810417d5553
Jennie Zhang <y.zhang@cwi.nl>
2011-04-06 17:29:05 +0200

    fix query: this is not a table, but an array

--- sql/test/sciql/Tests/sciql02.sql
+++ sql/test/sciql/Tests/sciql02.sql
@@ -6,7 +6,7 @@ CREATE ARRAY timeseries (
     tick TIMESTAMP DIMENSION,
     payload FLOAT DEFAULT 0.0 );
 
-CREATE TABLE experiment2(
+CREATE ARRAY experiment2(
     run date DIMENSION[ TIMESTAMP '2010-01-01':*: INTERVAL'1' day],
     payload ARRAY (
         x integer DIMENSION[4],
