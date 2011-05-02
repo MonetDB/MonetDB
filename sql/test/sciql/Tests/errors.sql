c5909502399b33c1afeb6d1619ebd5dc4ff1a589
Jennie Zhang <y.zhang@cwi.nl>
2011-05-02 17:10:08 +0200

    Correct test query: we allow arrays to have zero non-dimensional attributes. The text in the SciQL paper is correct => it doesn't state that an array must have at least one non-dimensional attrubite.

--- sql/test/sciql/Tests/errors.sql
+++ sql/test/sciql/Tests/errors.sql
@@ -1,9 +1,6 @@
 -- arrays must have at least one dimension
 create array err1( payload float);
 
--- arrays must have one non-dimensional attribute
-create array err2( x integer dimension[1:10:1]);
-
 -- array dimension types should comply with attribute type
 create array err3( x char dimension[1:128:1], v float);
 create array err4( s varchar(25) dimension['a':'z':1], v float);
@@ -19,9 +16,11 @@ create array err8( x integer dimension[1:128:1],
 
 -- array dimensions may have undetermined step sizes
 create array array10(x integer dimension[1:128:*], v float);
+-- arrays may have zero non-dimensional attribute
+create array array11( x integer dimension[1:10:1]);
+
 
 drop array err1;
-drop array err2;
 drop array err3;
 drop array err4;
 drop array err5;
@@ -30,4 +29,5 @@ drop array err7;
 drop array err8;
 
 drop array array10;
+drop array array11;
 
