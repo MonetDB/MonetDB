bcbfd77cff6daaf7277eb8b0fac0922e854aa114
Niels Nes <niels@cwi.nl>
2011-05-31 12:47:50 +0200

    added relational algebra parser using the sql procedure 'ra' you can now test relational plans.
    Beware this is for debuging only. No garantees what so ever
    are given on this function.

--- sql/server/rel_dump.c
+++ sql/server/rel_dump.c
@@ -22,7 +22,9 @@
 #define TABSTOP 2
 
 #include "rel_dump.h"
+#include "rel_exp.h"
 #include "rel_prop.h"
+#include "rel_select.h"
 
 static void
 print_indent(mvc *sql, stream *fout, int depth)
@@ -81,15 +83,17 @@ exp_print(mvc *sql, stream *fout, sql_exp *e, int depth, int comma, int alias)
 		return;
 	switch(e->type) {
 	case e_convert: {
-		list *l = e->r;
-		sql_subtype *f = l->h->data;
+		//list *l = e->r;
+		//sql_subtype *f = l->h->data;
 		char *to_type = sql_subtype_string(&e->tpe);
-		char *from_type = sql_subtype_string(f);
-		mnstr_printf(fout, "convert(");
+		//char *from_type = sql_subtype_string(f);
+		//mnstr_printf(fout, "convert(");
+		mnstr_printf(fout, "%s[", to_type);
 		exp_print(sql, fout, e->l, depth, 0, 0);
-		mnstr_printf(fout, " from %s to %s)", from_type, to_type);
+		//mnstr_printf(fout, " from %s to %s)", from_type, to_type);
+		mnstr_printf(fout, "]");
 		_DELETE(to_type);
-		_DELETE(from_type);
+		//_DELETE(from_type);
 	 	break;
 	}
 	case e_atom: {
@@ -101,11 +105,9 @@ exp_print(mvc *sql, stream *fout, sql_exp *e, int depth, int comma, int alias)
 					isStream(t)?"stream":isMergeTable(t)?"merge table":"table",
 					t->base.name);
 			} else {
+				char *t = sql_subtype_string(atom_type(a));
 				char *s = atom2string(sql->sa, a);
-				if (atom_type(a)->type->localtype == TYPE_str)
-					mnstr_printf(fout, "'%s'", s);
-				else
-					mnstr_printf(fout, "%s", s);
+				mnstr_printf(fout, "%s \"%s\"", t, s);
 			}
 		} else { /* variables */
 			if (e->r) { /* named parameters */
@@ -121,19 +123,26 @@ exp_print(mvc *sql, stream *fout, sql_exp *e, int depth, int comma, int alias)
 	} 	break;
 	case e_func: {
 		sql_subfunc *f = e->f;
-		mnstr_printf(fout, "%s", f->func->base.name );
+		mnstr_printf(fout, "%s.%s", 
+				f->func->s?f->func->s->base.name:"sys", 
+				f->func->base.name);
 		exps_print(sql, fout, e->l, depth, alias, 1);
 		if (e->r)
 			exps_print(sql, fout, e->r, depth, alias, 1);
 	} 	break;
 	case e_aggr: {
 		sql_subaggr *a = e->f;
-		mnstr_printf(fout, "%s", a->aggr->base.name );
+		mnstr_printf(fout, "%s.%s", 
+				a->aggr->s?a->aggr->s->base.name:"sys", 
+				a->aggr->base.name);
 		if (need_distinct(e))
 			mnstr_printf(fout, " unique ");
 		if (need_no_nil(e))
 			mnstr_printf(fout, " no nil ");
-		exps_print(sql, fout, e->l, depth, alias, 1);
+		if (e->l)
+			exps_print(sql, fout, e->l, depth, alias, 1);
+		else
+			mnstr_printf(fout, "()");
 	} 	break;
 	case e_column: 
 		if (e->l)
@@ -552,3 +561,562 @@ rel_print(mvc *sql, sql_rel *rel, int depth)
 
 	list_destroy(refs);
 }
+
+static void
+skipWS( char *r, int *pos)
+{
+	while(r[*pos] && (isspace(r[*pos]) || r[*pos] == '|')) 
+		(*pos)++;
+}
+
+static void
+skipIdent( char *r, int *pos)
+{
+	while(r[*pos] && (isalnum(r[*pos]) || r[*pos] == '_' || r[*pos] == '%'))
+		(*pos)++;
+}
+
+static int
+readInt( char *r, int *pos)
+{
+	int res = 0;
+
+	while (isdigit(r[*pos])) {
+		res *= 10;
+		res += r[*pos]-'0';
+		(*pos)++;
+	}
+	return res;
+}
+
+static char *
+readString( char *r, int *pos) 
+{
+	char *st = NULL;
+
+	if (r[*pos] == '"'){
+		(*pos)++;
+		st = r+*pos;
+		while (r[*pos] != '"')
+			(*pos)++;
+		r[*pos] = 0;
+		(*pos)++;
+	}
+	return st;
+}
+
+static sql_exp* exp_read(mvc *sql, sql_rel *lrel, sql_rel *rrel, char *r, int *pos, int grp) ;
+
+static list*
+read_exps(mvc *sql, sql_rel *lrel, sql_rel *rrel, char *r, int *pos, char bracket, int grp) 
+{
+	list *exps = new_exp_list(sql->sa);
+	sql_exp *e;
+	char ebracket = (bracket == '[')?']':')';
+
+	if (r[*pos] == bracket) {
+		skipWS( r, pos);
+
+		(*pos)++;
+		skipWS( r, pos);
+		e = exp_read(sql, lrel, rrel, r, pos, grp);
+		if (!e && r[*pos] != ebracket) {
+			return sql_error(sql, -1, "missing closing %c\n", ebracket);
+		} else if (!e) {
+			(*pos)++;
+			skipWS( r, pos);
+			return exps;
+		}
+		append(exps, e);
+		skipWS( r, pos);
+		while (r[*pos] == ',') {
+			(*pos)++;
+			skipWS( r, pos);
+			e = exp_read(sql, lrel, rrel, r, pos, grp);
+			if (!e)
+				return NULL;
+			append(exps, e);
+			skipWS( r, pos);
+		}
+		if (r[*pos] != ebracket) 
+			return sql_error(sql, -1, "missing closing %c\n", ebracket);
+		(*pos)++;
+		skipWS( r, pos);
+	}
+	return exps;
+}
+
+static sql_exp*
+exp_read(mvc *sql, sql_rel *lrel, sql_rel *rrel, char *r, int *pos, int grp) 
+{
+	int not = 1, f = 0, old, d=0, s=0, unique = 0, no_nils = 0;
+	char *tname, *cname, *e, *b = r + *pos, *st;
+	sql_exp *exp = NULL;
+	list *exps = NULL;
+	sql_subtype *tpe;
+
+	skipIdent(r, pos);
+	e = r+*pos;
+	skipWS(r, pos);
+	switch(r[*pos]) {
+	/* ident */
+	case '.': 
+		*e = 0;
+		(*pos)++;
+		tname = b;
+		cname = r + *pos;
+		skipIdent(r, pos);
+		e = r+*pos;
+		skipWS(r, pos);
+		old = *e;
+		*e = 0;
+		
+		if (lrel) { 
+			exp = rel_bind_column2(sql, lrel, tname, cname, 0);
+			if (!exp && rrel)
+				exp = rel_bind_column2(sql, rrel, tname, cname, 0);
+		} else {
+			exp = exp_column(sql->sa, tname, cname, NULL, CARD_ATOM, 1, (strchr(cname,'%') != NULL));
+		}
+		*e = old;
+		break;
+	/* atom */
+	case '(': 
+	case '[': 
+		old = *e;
+		*e = 0;
+		tname = b;
+		if (old != '[') {
+			(*pos)++;
+			d = readInt(r,pos);
+			if (r[*pos] != ')' && r[*pos] != ',')
+				return sql_error(sql, -1, "type: missing ')' or ','\n");
+			if (r[*pos] == ',') {
+				(*pos)++;
+				s = readInt(r,pos);
+			}
+			if (r[*pos] != ')') 
+				return sql_error(sql, -1, "type: missing ')'\n");
+			(*pos)++;
+		}
+		tpe = sql_bind_subtype(tname, d, s);
+		skipWS(r, pos);
+		*e = old;
+		if (r[*pos] == '[') { /* convert */
+			(*pos)++;
+			skipWS(r, pos);
+			exp = exp_read(sql, lrel, rrel, r, pos, 0);
+			if (r[*pos] != ']') 
+				return sql_error(sql, -1, "convert: missing ']'\n");
+			(*pos)++;
+			skipWS(r, pos);
+			exp = exp_convert(sql->sa, exp, exp_subtype(exp), tpe);
+		} else {
+			st = readString(r,pos);
+			exp = exp_atom(sql->sa, atom_general(sql->sa, tpe, st));
+		}
+		break;
+	case '\"': 
+		*e = 0;
+		tname = b;
+		tpe = sql_bind_subtype(tname, 0, 0);
+		st = readString(r,pos);
+		return exp_atom(sql->sa, atom_general(sql->sa, tpe, st));
+	default:
+		(void)sql;
+	}
+
+	/* func or aggr */
+	if (grp) {
+		skipWS(r, pos);
+		if (r[*pos] == 'u') {
+			unique = 1;
+			(*pos)+= strlen("unique");
+			skipWS(r, pos);
+		}
+		if (r[*pos] == 'n') {
+			no_nils = 1;
+			(*pos)+= strlen("no nil");
+			skipWS(r, pos);
+		}
+	}
+	if (r[*pos] == '(') {
+		sql_schema *s;
+		sql_subfunc *f = NULL;
+		sql_subaggr *a = NULL;
+		node *n;
+
+		exps = read_exps(sql, lrel, rrel, r, pos, '(', 0);
+		tname = b;
+		*e = 0;
+		s = mvc_bind_schema(sql, tname);
+		if (grp) {
+			if (exps && exps->h)
+				a = sql_bind_aggr(sql->sa, s, cname, exp_subtype(exps->h->data));
+			else
+				a = sql_bind_aggr(sql->sa, s, cname, NULL);
+			exp = exp_aggr( sql->sa, exps, a, unique, no_nils, CARD_ATOM, 1);
+		} else {
+			list *ops = list_new(sql->sa);
+			for( n = exps->h; n; n = n->next)
+				append(ops, exp_subtype(n->data));
+			f = sql_bind_func_(sql->sa, s, cname, ops);
+			exp = exp_op( sql->sa, exps, f);
+		}
+	}
+
+	if (!exp && b != e) { /* simple ident */
+		if (lrel) { 
+			old = *e;
+			*e = 0;
+			exp = rel_bind_column(sql, lrel, b, 0);
+			if (!exp && rrel)
+				exp = rel_bind_column(sql, rrel, b, 0);
+			*e = old;
+		}
+	}
+	/* [ ASC ] */
+	if (strncmp(r+*pos, "ASC",  strlen("ASC")) == 0) {
+		(*pos)+= strlen("NOT");
+		skipWS(r, pos);
+		set_direction(exp, ASCENDING);
+	}
+
+	/* [ NOT ] NULL */
+	if (strncmp(r+*pos, "NOT",  strlen("NOT")) == 0) {
+		(*pos)+= strlen("NOT");
+		skipWS(r, pos);
+		not = 1;
+	}
+	if (strncmp(r+*pos, "NULL",  strlen("NULL")) == 0) {
+		(*pos)+= strlen("NULL");
+		skipWS(r, pos);
+		if (not)
+			set_has_no_nil(exp);
+	}
+	/* as alias */
+	if (strncmp(r+*pos, "as", 2) == 0) {
+		int old;
+		(*pos)+=2;
+		skipWS(r, pos);
+
+		tname = r+*pos;
+		skipIdent(r, pos);
+		if (r[*pos] != '.') {
+			old = r[*pos];
+			r[*pos] = 0;
+			cname = tname;
+			exp_setname(sql->sa, exp, NULL, cname);
+			r[*pos] = old;
+			skipWS(r, pos);
+		} else {
+			r[*pos] = 0;
+			(*pos)++;
+			cname = r+*pos;
+			skipIdent(r, pos);
+			e = r+*pos;
+			skipWS(r, pos);
+			old = *e;
+			*e = 0;
+			exp_setname(sql->sa, exp, tname, cname);
+			*e = old;
+		}
+	}
+	switch(r[*pos]) {
+	case '=': 
+		f = cmp_equal;
+		(*pos)++;
+		break;
+	case '<': 
+		f = cmp_lt;
+		(*pos)++;
+		if (r[(*pos)] == '=') {
+			f = cmp_lte;
+			(*pos)++;
+		}
+		break;
+	case '>': 
+		f = cmp_gt;
+		(*pos)++;
+		if (r[(*pos)] == '=') {
+			f = cmp_gte;
+			(*pos)++;
+		}
+		break;
+	case ',': 
+	case ']': 
+	case ')': 
+		return exp;
+	default:
+		return NULL;
+	}
+	if (f) {
+		sql_exp *e;
+
+		skipWS(r,pos);
+	        e = exp_read(sql, lrel, rrel, r, pos, 0);
+		/* TODO case for compare 2 */
+		if (e)
+			return exp_compare(sql->sa, exp, e, f);
+	}
+	return exp;
+}
+
+sql_rel*
+rel_read(mvc *sql, char *r, int *pos)
+{
+	sql_rel *rel = NULL, *nrel, *lrel, *rrel;
+	list *exps, *gexps;
+	int distinct = 0, j=0;
+
+	skipWS(r,pos);
+	switch(r[*pos]) {
+	case 't': { 
+		sql_schema *s = NULL;
+		sql_table *t = NULL;
+		char *sname, *tname, *e;
+		*pos += strlen("table");
+		skipWS(r, pos);
+		if (r[*pos] != '(') 
+			return sql_error(sql, -1, "table: missing '('\n");
+		(*pos)++;
+		skipWS(r, pos);
+		sname = r+*pos;
+		skipIdent(r, pos);
+		e = r+*pos;
+		if (r[*pos] != '.') 
+			return sql_error(sql, -1, "table: missing '.' in table name\n");
+		*e = 0;
+		(*pos)++;
+		tname = r+*pos;
+		skipIdent(r, pos);
+		e = r+*pos;
+		skipWS(r, pos);
+		if (r[*pos] != ')') 
+			sql_error(sql, -1, "table: missing ')'\n");
+		*e = 0;
+		(*pos)++;
+		skipWS(r, pos);
+		s = mvc_bind_schema(sql, sname);
+		if (s)
+			t = mvc_bind_table(sql, s, tname);
+		if (!s || !t)
+			return sql_error(sql, -1, "table: missing '%s.%s'\n", sname, tname);
+		rel = rel_basetable(sql, t, tname);
+	
+		if (!r[*pos])
+			return rel;
+
+		/* scan aliases */
+		exps = read_exps(sql, rel, NULL, r, pos, '[', 0);
+		if (exps && list_length(exps))
+			rel = rel_project(sql->sa, rel, exps);
+		return rel;
+	} break;
+	case 'p':
+		*pos += strlen("project");
+		skipWS(r, pos);
+
+		if (r[*pos] != '(') 
+			return sql_error(sql, -1, "project: missing '('\n");
+		(*pos)++;
+		skipWS(r, pos);
+		nrel = rel_read(sql, r, pos);
+		skipWS(r, pos);
+		if (r[*pos] != ')') 
+			return sql_error(sql, -1, "project: missing ')'\n");
+		(*pos)++;
+		skipWS(r, pos);
+
+		exps = read_exps(sql, nrel, NULL, r, pos, '[', 0);
+		rel = rel_project(sql->sa, nrel, exps);
+		/* order by ? */
+		if (r[*pos] == '[') {
+			rel->r = rel->exps;
+			rel->exps = read_exps(sql, nrel, NULL, r, pos, '[', 0);
+		}
+		if (distinct)
+			set_distinct(rel);
+		distinct = 0;
+		return rel;
+	case 'g':
+		*pos += strlen("group by");
+		skipWS(r, pos);
+
+		if (r[*pos] != '(') 
+			return sql_error(sql, -1, "group by: missing '('\n");
+		(*pos)++;
+		skipWS(r, pos);
+		nrel = rel_read(sql, r, pos);
+		skipWS(r, pos);
+		if (r[*pos] != ')') 
+			return sql_error(sql, -1, "group by: missing ')'\n");
+		(*pos)++;
+		skipWS(r, pos);
+
+		gexps = read_exps(sql, nrel, NULL, r, pos, '[', 0);
+		skipWS(r, pos);
+		exps = read_exps(sql, nrel, NULL, r, pos, '[', 1);
+
+		rel = rel_groupby(sql->sa, nrel, gexps);
+		rel->exps = exps;
+		return rel;
+	case 's':
+	case 'a':
+		if (r[*pos+2] == 'l') {
+			*pos += strlen("select");
+			skipWS(r, pos);
+			if (r[*pos] != '(') 
+				return sql_error(sql, -1, "select: missing '('\n");
+			(*pos)++;
+			skipWS(r, pos);
+			nrel = rel_read(sql, r, pos);
+			skipWS(r, pos);
+			if (r[*pos] != ')') 
+				return sql_error(sql, -1, "select: missing ')'\n");
+			(*pos)++;
+			skipWS(r, pos);
+
+			exps = read_exps(sql, nrel, NULL, r, pos, '[', 0);
+			rel = rel_select_copy(sql->sa, nrel, exps);
+			return rel;
+			// semijoin or antijoin
+		} else if (r[*pos+2] == 'l' || r[*pos+1] == 'n') {
+			j = op_semi;
+
+			if (r[*pos+1] == 'n') 
+				j = op_semi;
+
+			*pos += strlen("semijoin");
+			skipWS(r, pos);
+			if (r[*pos] != '(') 
+				return sql_error(sql, -1, "semijoin: missing '('\n");
+			(*pos)++;
+			skipWS(r, pos);
+			lrel = rel_read(sql, r, pos);
+			skipWS(r, pos);
+
+			if (r[*pos] != ',') 
+				return sql_error(sql, -1, "semijoin: missing ','\n");
+			(*pos)++;
+			skipWS(r, pos);
+			rrel = rel_read(sql, r, pos);
+
+			skipWS(r, pos);
+			if (r[*pos] != ')') 
+				return sql_error(sql, -1, "semijoin: missing ')'\n");
+			(*pos)++;
+			skipWS(r, pos);
+
+			exps = read_exps(sql, lrel, rrel, r, pos, '[', 0);
+			rel = rel_crossproduct(sql->sa, lrel, rrel, j);
+			rel->exps = exps;
+			return rel;
+		}
+		break;
+	case 'l':
+		*pos += strlen("left outer join");
+		j = op_left;
+		/* fall through */
+	case 'r': 
+		if (!j) {
+			*pos += strlen("right outer join");
+			j = op_right;
+		}
+		/* fall through */
+	case 'f':
+		if (!j) {
+			*pos += strlen("full outer join");
+			j = op_full;
+		}
+		/* fall through */
+	case 'c':
+		if (!j) {
+			*pos += strlen("crossproduct");
+			j = op_join;
+		}
+		/* fall through */
+	case 'j':
+		if (!j) {
+			*pos += strlen("join");
+			j = op_join;
+		}
+		skipWS(r, pos);
+
+		if (r[*pos] != '(') 
+			return sql_error(sql, -1, "join: missing '('\n");
+		(*pos)++;
+		skipWS(r, pos);
+		lrel = rel_read(sql, r, pos);
+		skipWS(r, pos);
+
+		if (r[*pos] != ',') 
+			return sql_error(sql, -1, "join: missing ','\n");
+		(*pos)++;
+		skipWS(r, pos);
+		rrel = rel_read(sql, r, pos);
+
+		skipWS(r, pos);
+		if (r[*pos] != ')') 
+			return sql_error(sql, -1, "join: missing ')'\n");
+		(*pos)++;
+		skipWS(r, pos);
+
+		exps = read_exps(sql, lrel, rrel, r, pos, '[', 0);
+		rel = rel_crossproduct(sql->sa, lrel, rrel, j);
+		rel->exps = exps;
+		return rel;
+	case 'u':
+		if (!j) {
+			*pos += strlen("union");
+			j = op_union;
+		}
+	case 'i':
+		if (!j) {
+			*pos += strlen("intersect");
+			j = op_inter;
+		}
+	case 'e':
+		if (!j) {
+			*pos += strlen("except");
+			j = op_except;
+		}
+		skipWS(r, pos);
+
+		if (r[*pos] != '(') 
+			return sql_error(sql, -1, "setop: missing '('\n");
+		(*pos)++;
+		skipWS(r, pos);
+		lrel = rel_read(sql, r, pos);
+		skipWS(r, pos);
+
+		if (r[*pos] != ',') 
+			return sql_error(sql, -1, "setop: missing ','\n");
+		(*pos)++;
+		skipWS(r, pos);
+		rrel = rel_read(sql, r, pos);
+
+		skipWS(r, pos);
+		if (r[*pos] != ')') 
+			return sql_error(sql, -1, "setop: missing ')'\n");
+		(*pos)++;
+		skipWS(r, pos);
+
+		exps = read_exps(sql, lrel, rrel, r, pos, '[', 0);
+		rel = rel_setop(sql->sa, lrel, rrel, j);
+		rel->exps = exps;
+		return rel;
+	case 'd':
+		if (r[*pos+1] == 'i') {
+			*pos += strlen("distinct");
+			skipWS(r, pos);
+			distinct = 1;
+			break;
+		}
+		/* 'ddl' not supported */
+	default:
+		return NULL;
+	}
+	return NULL;
+}
+
