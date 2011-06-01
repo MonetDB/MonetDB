b2eb44df9bd2e8d60c85a803415d3887a7f80192
Niels Nes <niels@cwi.nl>
2011-06-01 23:19:43 +0200

    fixes for the Direct Relation Algebra execution

--- sql/server/rel_prop.c
+++ sql/server/rel_prop.c
@@ -68,3 +68,24 @@ propkind2string( prop *p)
 	}
 	return "UNKNOWN";
 }
+
+char *
+propvalue2string( prop *p)
+{
+	char buf [BUFSIZ];
+
+	if (p->value) {
+		switch(p->kind) {
+		case PROP_JOINIDX: {
+					   sql_idx *i = p->value;
+
+					   snprintf(buf, BUFSIZ, "%s.%s.%s", i->t->s->base.name, i->t->base.name, i->base.name);
+					   return _strdup(buf);
+				   }
+			break;
+		default:
+			break;
+		}
+	}
+	return "";
+}
