5e17affd93b3cf3374efa8c01df43bfd8976042e
Sjoerd Mullender <sjoerd@acm.org>
2011-07-17 10:43:49 +0200

    Added test for bug 2058.

---
+++ sql/test/BugTracker-2011/Tests/with-scope.Bug-2058.sql
@@ -0,0 +1,6 @@
+create table bug2058a (s varchar(10));
+create table bug2058b (s varchar(10));
+insert into bug2058a values ('bad');
+insert into bug2058b values ('good');
+-- this should result in the contents of table bug2058b, not bug2058a
+with bug2058a as (select * from bug2058b) select * from bug2058a;
