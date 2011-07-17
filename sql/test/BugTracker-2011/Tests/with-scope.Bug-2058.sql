f1e56364f4cd8cb5711f2ac0c216d1c5d80ebc0d
Sjoerd Mullender <sjoerd@acm.org>
2011-07-17 10:51:50 +0200

    Cleanup after test.

--- sql/test/BugTracker-2011/Tests/with-scope.Bug-2058.sql
+++ sql/test/BugTracker-2011/Tests/with-scope.Bug-2058.sql
@@ -4,3 +4,6 @@ insert into bug2058a values ('bad');
 insert into bug2058b values ('good');
 -- this should result in the contents of table bug2058b, not bug2058a
 with bug2058a as (select * from bug2058b) select * from bug2058a;
+-- clean up
+drop table bug2058a;
+drop table bug2058b;
