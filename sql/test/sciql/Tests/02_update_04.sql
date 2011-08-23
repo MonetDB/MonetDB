76c29a2010e2fd03ba3cc1a574b178bb7dedd421
Jennie Zhang <y.zhang@cwi.nl>
2011-08-23 18:26:11 +0200

    reorganising tests

--- sql/test/sciql/Tests/02_update_04.sql
+++ sql/test/sciql/Tests/02_update_04.sql
@@ -1,4 +1,4 @@
-CREATE ARRAY matrix (x INT DIMENSION[4], y INT DIMENSION[4], v FLOAT DEFAULT 0.0);
+CREATE ARRAY matrix (x INT DIMENSION[4], y INT DIMENSION[4], v FLOAT DEFAULT 3.3);
 
 -- FIXME: do these queries update all 'v'-s in a row/column?
 UPDATE matrix SET matrix[0:2].v = v * 1.19;
--- sql/test/sciql/Tests/06_slice_01.sql
+++ sql/test/sciql/Tests/06_slice_01.sql
@@ -1,11 +1,11 @@
-CREATE ARRAY matrix (x INT DIMENSION[4], y INT DIMENSION[4], v FLOAT DEFAULT 0.0);
+CREATE ARRAY matrix3 (x INT DIMENSION[4], y INT DIMENSION[4], v FLOAT DEFAULT 0.0);
 
 -- this update is necessary because 'v FLOAT DEFAULT <expr>' is not implemented yet.
-UPDATE matrix SET v = CASE WHEN x>y THEN x + y WHEN x<y THEN x - y ELSE 0 END;
+UPDATE matrix3 SET v = CASE WHEN x>y THEN x + y WHEN x<y THEN x - y ELSE 0 END;
 
-SELECT v FROM matrix[3][2];
-SELECT v FROM matrix[*][1:3]; 
-SELECT v FROM matrix[0:2:4][0:2:4];
+SELECT v FROM matrix3[3][2];
+SELECT v FROM matrix3[*][1:3]; 
+SELECT v FROM matrix3[0:2:4][0:2:4];
 
-DROP ARRAY matrix;
+DROP ARRAY matrix3;
 
