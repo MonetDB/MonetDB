cb557cb8511d0524640a944395721cad95e4067d
Stefan Manegold <Stefan.Manegold@cwi.nl>
2011-08-26 11:14:33 +0200

    updated test & stable output:
    with LIMIT queries, it appears that the slice() operation in no longer
    (or at least not always) pushed down below and into the mat.pack();
    thus, we need to check whether there is a matslice() or a mat.pack().
    
    This also makes this test work with single-threaded execution, again.

--- sql/test/BugTracker-2010/Tests/mat.slice_limit1.Bug-2645.sql
+++ sql/test/BugTracker-2010/Tests/mat.slice_limit1.Bug-2645.sql
@@ -58,19 +58,23 @@ create function GetTrace()
 
 TRACE select x,y from slice_test limit 1;
 -- When mitosis was activated (i.e., the MAL plan contains mat.*() statements,
--- then there sould also be at least one mat.slice() statement.
+-- then there sould also be at least one mat.slice() (or mat.pack()) statement.
+WITH TheTrace AS ( SELECT * FROM GetTrace() )
 SELECT count(*) FROM
-( SELECT count(*) AS mat       FROM GetTrace() WHERE stmt LIKE '% := mat.%'       ) as m,
-( SELECT count(*) AS mat_slice FROM GetTrace() WHERE stmt LIKE '% := mat.slice(%' ) as ms
-WHERE ( mat = 0 AND mat_slice = 0 ) OR ( mat > 0 AND mat_slice > 0 );
+( SELECT count(*) AS mat       FROM TheTrace WHERE stmt LIKE '% := mat.%'       ) as m,
+( SELECT count(*) AS mat_slice FROM TheTrace WHERE stmt LIKE '% := mat.slice(%' ) as ms,
+( SELECT count(*) AS mat_pack  FROM TheTrace WHERE stmt LIKE '% := mat.pack(%'  ) as mp
+WHERE ( mat = 0 AND mat_slice+mat_pack = 0 ) OR ( mat > 0 AND mat_slice+mat_pack > 0 );
 
 TRACE select cast(x as string)||'-bla-'||cast(y as string) from slice_test limit 1;
 -- When mitosis was activated (i.e., the MAL plan contains mat.*() statements,
--- then there sould also be at least one mat.slice() statement.
+-- then there sould also be at least one mat.slice() (or mat.pack()) statement.
+WITH TheTrace AS ( SELECT * FROM GetTrace() )
 SELECT count(*) FROM
-( SELECT count(*) AS mat       FROM GetTrace() WHERE stmt LIKE '% := mat.%'       ) as m,
-( SELECT count(*) AS mat_slice FROM GetTrace() WHERE stmt LIKE '% := mat.slice(%' ) as ms
-WHERE ( mat = 0 AND mat_slice = 0 ) OR ( mat > 0 AND mat_slice > 0 );
+( SELECT count(*) AS mat       FROM TheTrace WHERE stmt LIKE '% := mat.%'       ) as m,
+( SELECT count(*) AS mat_slice FROM TheTrace WHERE stmt LIKE '% := mat.slice(%' ) as ms,
+( SELECT count(*) AS mat_pack  FROM TheTrace WHERE stmt LIKE '% := mat.pack(%'  ) as mp
+WHERE ( mat = 0 AND mat_slice+mat_pack = 0 ) OR ( mat > 0 AND mat_slice+mat_pack > 0 );
 
 drop function GetTrace;
 
