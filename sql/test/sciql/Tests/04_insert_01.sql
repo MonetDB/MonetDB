21eaee135dbca186236acdf6142ff0d321879387
Jennie Zhang <y.zhang@cwi.nl>
2011-09-12 22:55:43 +0200

    Don't blindly set the 'nonil' property, since the second parameter of array.filler can be a NULL value
    approved tests 04_insert_{06,07}.
    INSERT INTO <ary> VALUES works (for the queries in the tests).
    INSERT INTO <ary> <sub-query> needs more investigation.
    
    added a new test for CREATE ARRAY with cells of various data types.
    the queries fail, so need more investigation.

--- sql/test/sciql/Tests/04_insert_01.sql
+++ sql/test/sciql/Tests/04_insert_01.sql
@@ -1,10 +1,10 @@
-CREATE ARRAY matrix (x INT DIMENSION[4], y INT DIMENSION[4], v FLOAT DEFAULT 0.0);
-SELECT * FROM matrix;
+CREATE ARRAY matrix_0401 (x INT DIMENSION[4], y INT DIMENSION[4], v FLOAT DEFAULT 0.0);
+SELECT * FROM matrix_0401;
 -- this update is necessary because 'v FLOAT DEFAULT (x + y) * 100' is not implemented yet.
-UPDATE matrix SET v = (x + y) * 100;
-SELECT * FROM matrix;
+UPDATE matrix_0401 SET v = (x + y) * 100;
+SELECT * FROM matrix_0401;
 -- This is wat we really want to test
-INSERT INTO matrix SELECT x-1, y, v FROM matrix WHERE x > 2;
-SELECT * FROM matrix;
-DROP ARRAY matrix;
+INSERT INTO matrix_0401 SELECT x-1, y, v FROM matrix_0401 WHERE x > 2;
+SELECT * FROM matrix_0401;
+DROP ARRAY matrix_0401;
 
