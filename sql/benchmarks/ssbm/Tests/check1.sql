b7d588e7758631942eda870079aeff7209963b97
Stefan Manegold <Stefan.Manegold@cwi.nl>
2011-09-27 18:32:37 +0200

    added SSBM functionality test @ SF 0.01

---
+++ sql/benchmarks/ssbm/Tests/check1.sql
@@ -0,0 +1,11 @@
+select count(*) from DWDATE;
+select count(*) from SUPPLIER;
+select count(*) from CUSTOMER;
+select count(*) from PART;
+select count(*) from LINEORDER;
+
+select * from DWDATE    limit 9;
+select * from SUPPLIER  limit 9;
+select * from CUSTOMER  limit 9;
+select * from PART      limit 9;
+select * from LINEORDER limit 9;
---
+++ sql/benchmarks/ssbm/Tests/load.SQL.py
@@ -0,0 +1,20 @@
+import os, sys
+try:
+    from MonetDBtesting import process
+except ImportError:
+    import process
+
+TSTSRCDIR = os.environ['TSTSRCDIR']
+DATADIR = os.path.join(TSTSRCDIR,"SF-0.01") + os.sep.replace('\\', r'\\')
+
+c = process.client('sql', stdin = process.PIPE, stdout = process.PIPE, stderr = process.PIPE)
+for ln in open(os.path.join(TSTSRCDIR,"load-sf-0.01.sql")):
+    c.stdin.write(ln.replace('PWD/', DATADIR))
+out, err = c.communicate()
+sys.stdout.write(out)
+sys.stderr.write(err)
+
+c = process.client('sql', stdin = open(os.path.join(TSTSRCDIR,"check1.sql")), stdout = process.PIPE, stderr = process.PIPE)
+out, err = c.communicate()
+sys.stdout.write(out)
+sys.stderr.write(err)
