7f4072b7904002fd3c2c662e2c24c67822eb8644
Sjoerd Mullender <sjoerd@acm.org>
2011-10-10 22:48:45 +0200

    Don't append partial queries inside mapi_query_part. When using the construct 	mclient -s 'COPY INTO t FROM STDIN ...' < file.csv mapi_query is called for the first part of the query (on the command line) which allocates just enough space to remember that query, and then mapi_query_part is called for each line of the input file, which assumed the allocated space was 512 bytes, resulting in out-of-bounds writes.

--- clients/mapilib/mapi.c
+++ clients/mapilib/mapi.c
@@ -4263,22 +4263,7 @@ mapi_query_part(MapiHdl hdl, const char *query, size_t size)
 	mid->active = hdl;
 	/* remember the query just for the error messages */
 	if (hdl->query == NULL) {
-		size_t sz = size;
-
-		sz = 512;
-		hdl->query = malloc(sz + 1);
-		assert(hdl->query);
-		hdl->query[0] = '\0';
-		strncpy(hdl->query, query, sz);
-		hdl->query[sz] = '\0';
-	} else {
-		size_t ln = strlen(hdl->query), sz = 512 - ln;
-		if (sz > 0) {
-			if (size < sz)
-				sz = size;
-			assert(hdl->query);
-			strncat(hdl->query, query, sz);
-		}
+		hdl->query = strdup(query);
 	}
 
 	if (mid->trace == MAPI_TRACE) {
