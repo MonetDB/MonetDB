f46c4ce25152870dd5600915420537fa2d072f70
Sjoerd Mullender <sjoerd@acm.org>
2011-10-20 17:26:30 +0200

    Changed test for bug 2908 so that it also works in isolation.

--- sql/test/BugTracker-2011/Tests/predicate.Bug-2908.sql
+++ sql/test/BugTracker-2011/Tests/predicate.Bug-2908.sql
@@ -6,5 +6,5 @@ and t."temporary" = 0 then cast('SYSTEM TABLE' as varchar(20)) when t."type" =
 t."temporary" = 1 then cast('LOCAL TEMPORARY' as varchar(20)) else
 cast('INTERNAL TABLE TYPE' as varchar(20)) end as table_type, cast('' as
 varchar(1)) as remarks from sys."schemas" s, sys."tables" t where s."id" =
-t."schema_id" and t."name" like '%' and (1 = 0 or (t."type" = 0 and t."system"
+t."schema_id" and t."name" like 'x%z' and (1 = 0 or (t."type" = 0 and t."system"
 = false and t."temporary" = 0)) order by table_type, table_schem, table_name;
