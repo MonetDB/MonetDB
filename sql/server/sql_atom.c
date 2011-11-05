db503a5829f5c547174809efcfa3bc0c7b270a56
Niels Nes <niels@cwi.nl>
2011-11-05 16:27:14 +0100

    fixed problem with timezone setting
    added truncing of decimals in atom_cast

--- sql/server/sql_atom.c
+++ sql/server/sql_atom.c
@@ -652,6 +652,52 @@ atom_cast(atom *a, sql_subtype *tp)
 			}
 			return 1;
 		}
+		/* truncating decimals */
+		if (at->type->eclass == EC_DEC && tp->type->eclass == EC_DEC &&
+		    at->type->localtype >= tp->type->localtype &&
+		    at->digits >= tp->digits && 
+			(at->digits - tp->digits) == (at->scale - tp->scale)) {
+			lng mul = 1, div = 0, rnd = 0, val = 0;
+
+			/* fix scale */
+
+			/* only round when going to a lower scale */
+			mul = scales[at->scale-tp->scale];
+			rnd = mul>>1;
+			div = 1;
+
+			if (a->data.vtype == TYPE_lng) {
+				val = a->data.val.lval;
+			} else if (a->data.vtype == TYPE_int) {
+				val = a->data.val.ival;
+			} else if (a->data.vtype == TYPE_sht) {
+				val = a->data.val.shval;
+			} else if (a->data.vtype == TYPE_bte) {
+				val = a->data.val.btval;
+			}
+
+			val += rnd;
+			if (div)
+				val /= mul;
+			else
+				val *= mul;
+
+			a->tpe = *tp;
+			a->data.vtype = tp->type->localtype;
+			if (a->data.vtype == TYPE_lng) {
+				a->data.val.lval = (int) val;
+			} else if (a->data.vtype == TYPE_int) {
+				assert( ((lng) GDK_int_min <= val && val <= (lng) GDK_int_max));
+				a->data.val.ival = (int) val;
+			} else if (a->data.vtype == TYPE_sht) {
+				assert( ((lng) GDK_sht_min <= val && val <= (lng) GDK_sht_max));
+				a->data.val.shval = (int) val;
+			} else if (a->data.vtype == TYPE_bte) {
+				assert( ((lng) GDK_bte_min <= val && val <= (lng) GDK_bte_max));
+				a->data.val.btval = (int) val;
+			}
+			return 1;
+		}
 		if (at->type->eclass == EC_NUM && tp->type->eclass == EC_DEC &&
 		    at->type->localtype <= tp->type->localtype &&
 		    (at->digits <= tp->digits || atom_num_digits(a) <= tp->digits) &&
