f1b2cf85e6b1eefa06f9d32b77dd215505502b61
Jennie Zhang <y.zhang@cwi.nl>
2011-11-25 14:16:27 +0100

    extended the test for array slicing and approved the stable output

--- sql/test/sciql/Tests/06_slice_01.sql
+++ sql/test/sciql/Tests/06_slice_01.sql
@@ -2,10 +2,21 @@ CREATE ARRAY matrix3 (x INT DIMENSION[4], y INT DIMENSION[4], v FLOAT DEFAULT 0.
 
 -- this update is necessary because 'v FLOAT DEFAULT <expr>' is not implemented yet.
 UPDATE matrix3 SET v = CASE WHEN x>y THEN x + y WHEN x<y THEN x - y ELSE 0 END;
+SELECT * FROM matrix3;
 
-SELECT v FROM matrix3[3][2];
-SELECT v FROM matrix3[*][1:3]; 
-SELECT v FROM matrix3[0:2:4][0:2:4];
+SELECT * FROM matrix3[3][2];
+SELECT * FROM matrix3[*][2];
+SELECT * FROM matrix3[3][*];
+SELECT * FROM matrix3[*][*];
+
+SELECT * FROM matrix3[2:4][1:3]; 
+SELECT * FROM matrix3[2:*][1:3]; 
+SELECT * FROM matrix3[2:4][*:3]; 
+
+SELECT * FROM matrix3[0:2:4][1:1:3];
+SELECT * FROM matrix3[0:2:4][*:1:3];
+SELECT * FROM matrix3[0:*:4][1:1:3];
+SELECT * FROM matrix3[0:2:4][1:1:*];
 
 DROP ARRAY matrix3;
 
