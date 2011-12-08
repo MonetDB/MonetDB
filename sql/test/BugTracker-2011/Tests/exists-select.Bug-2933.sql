1f48315c29acbcd57b69ece7797fb5955887c3b9
Sjoerd Mullender <sjoerd@acm.org>
2011-12-08 17:17:38 +0100

    Added test for bug 2933.

---
+++ sql/test/BugTracker-2011/Tests/exists-select.Bug-2933.sql
@@ -0,0 +1,29 @@
+CREATE TABLE _rank2933 (pre INTEGER, post INTEGER);
+INSERT INTO _rank2933 VALUES (22, 37);
+INSERT INTO _rank2933 VALUES (23, 24);
+INSERT INTO _rank2933 VALUES (33, 34);
+
+SELECT
+  count(*)
+FROM
+  _rank2933 AS _rank1,
+  _rank2933 AS _rank2
+WHERE
+  NOT _rank1.pre = _rank2.pre AND
+  EXISTS (SELECT pre FROM _rank2933 AS ancestor WHERE
+    ancestor.pre < _rank1.pre AND _rank1.pre < ancestor.post AND
+    ancestor.pre < _rank2.pre AND _rank2.pre < ancestor.post);
+
+SELECT
+  count(*)
+FROM
+  _rank2933 AS _rank1,
+  _rank2933 AS _rank2
+WHERE
+  NOT _rank1.pre = _rank2.pre AND
+  EXISTS (SELECT pre FROM _rank2933 AS ancestor WHERE
+    ancestor.pre < _rank1.pre AND _rank1.pre < ancestor.post 
+  INTERSECT SELECT pre FROM _rank2933 AS ancestor WHERE
+    ancestor.pre < _rank2.pre AND _rank2.pre < ancestor.post);
+
+DROP TABLE _rank2933;
