57c740b285d2e0fe0878dbb82e92089589debd81
Fabian Groffen <fabian@cwi.nl>
2011-12-11 10:38:06 +0100

    Mx: add back unreachable return for icc

--- buildtools/Mx/Code.c
+++ buildtools/Mx/Code.c
@@ -341,6 +341,7 @@ CodeSubBlk(char *sub, char **argv)
       outofmem:
 	Error("Limit of %d characters per block reached.", blk_size);
 	exit(1);
+	return NULL; /* icc demands this, clang-3.0 chokes on it */
 }
 
 void
