a31fa3e5e19819cd3e6af1aa768159cc9ec40b95
Jennie Zhang <y.zhang@cwi.nl>
2012-01-13 16:06:35 +0100

    clean up

--- sql/test/sciql/Tests/07_tiling_01.sql
+++ sql/test/sciql/Tests/07_tiling_01.sql
@@ -1,4 +1,5 @@
 create array a (x int dimension[4], y int dimension[4], v float default 37);
 select x, y,  avg(v) from a group by a[x-1:x+2][y-2:y+1];
 select x, y,  sum(v) from a group by a[x-1:x+2][y-2:y+1];
+drop array a;
 
