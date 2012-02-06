6fb9d1cf1083d724daa46374f070e13c8804e846
Stefan Manegold <Stefan.Manegold@cwi.nl>
2012-02-07 00:37:56 +0100

    UDF: added another example: fuse()
    to fuse two N-byte integer values into one 2N-byte integer value

--- sql/backends/monet5/UDF/80_udf.sql
+++ sql/backends/monet5/UDF/80_udf.sql
@@ -20,3 +20,16 @@ All Rights Reserved.
 -- Reverse a string
 create function reverse(src string)
 returns string external name udf.reverse;
+
+
+-- fuse two (1-byte) tinyint values into one (2-byte) smallint value
+create function fuse(one tinyint, two tinyint)
+returns smallint external name udf.fuse;
+
+-- fuse two (2-byte) smallint values into one (4-byte) integer value
+create function fuse(one smallint, two smallint)
+returns integer external name udf.fuse;
+
+-- fuse two (4-byte) integer values into one (8-byte) bigint value
+create function fuse(one integer, two integer)
+returns bigint external name udf.fuse;
