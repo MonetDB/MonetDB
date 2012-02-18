90e3781bae4955c7bb6120e4f8cd62f192e497a1
Stefan Manegold <Stefan.Manegold@cwi.nl>
2012-02-18 17:11:55 +0100

    opt_sql_append test: minor extension for debugging and demonstration

--- monetdb5/extras/mal_optimizer_template/Tests/opt_sql_append.sql
+++ monetdb5/extras/mal_optimizer_template/Tests/opt_sql_append.sql
@@ -1,5 +1,7 @@
 create table ttt (a int, b int, c int);
+select optimizer;
 explain copy into ttt from '/tmp/xyz';
 set optimizer = substring(optimizer,0,length(optimizer)-length('garbageCollector')) || 'sql_append,garbageCollector';
+select optimizer;
 explain copy into ttt from '/tmp/xyz';
 drop table ttt;
