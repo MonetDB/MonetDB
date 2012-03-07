16fb83d49dd4dea983ffa390b72e5e9f937e632b
Fabian Groffen <fabian@cwi.nl>
2012-03-07 13:34:28 +0100

    mvc_commit: send SQLSTATE 40000 for transaction aborts
    Because I don't know any specifics, send generic "transaction rollback"
    SQLSTATE on commit failures for bug #3052.

--- sql/server/sql_mvc.c
+++ sql/server/sql_mvc.c
@@ -206,7 +206,7 @@ mvc_commit(mvc *m, int chain, char *name)
 		fprintf(stderr, "#mvc_commit %s\n", (name) ? name : "");
 
 	if (m->session->status < 0) {
-		(void)sql_error(m, 010, "COMMIT: transaction is aborted, will ROLLBACK instead");
+		(void)sql_error(m, 010, "40000!COMMIT: transaction is aborted, will ROLLBACK instead");
 		mvc_rollback(m, chain, name);
 		return -1;
 	}
@@ -240,7 +240,7 @@ mvc_commit(mvc *m, int chain, char *name)
 	cur -> parent = tr;
 	tr = cur;
 	if (ok != SQL_OK) {
-		(void)sql_error(m, 010, "COMMIT: transaction is aborted, will ROLLBACK instead");
+		(void)sql_error(m, 010, "40000!COMMIT: transaction is aborted, will ROLLBACK instead");
 		mvc_rollback(m, chain, name);
 		return -1;
 	}
@@ -261,13 +261,13 @@ mvc_commit(mvc *m, int chain, char *name)
 	/* validation phase */
 	if (sql_trans_validate(tr)) {
 		if ((ok = sql_trans_commit(tr)) != SQL_OK) {
-			char *msg = sql_message("COMMIT: transation commit failed (perhaps your disk is full?) exiting (kernel error: %s)", GDKerrbuf);
+			char *msg = sql_message("40000!COMMIT: transation commit failed (perhaps your disk is full?) exiting (kernel error: %s)", GDKerrbuf);
 			GDKfatal("%s", msg);
 			_DELETE(msg);
 		}
 	} else {
 		store_unlock();
-		(void)sql_error(m, 010, "COMMIT: transaction is aborted because of concurency conflicts, will ROLLBACK instead");
+		(void)sql_error(m, 010, "40000!COMMIT: transaction is aborted because of concurency conflicts, will ROLLBACK instead");
 		mvc_rollback(m, chain, name);
 		return -1;
 	}
