8b9c06effa45b32d09f2f3b508d101044356de2d
Martin Kersten <mk@cwi.nl>
2012-04-21 00:47:06 +0200

    de-Mx mal_debugger.mx

--- monetdb5/mal/mal_debugger.c
+++ monetdb5/mal/mal_debugger.c
@@ -1,91 +1,25 @@
-@/
-The contents of this file are subject to the MonetDB Public License
-Version 1.1 (the "License"); you may not use this file except in
-compliance with the License. You may obtain a copy of the License at
-http://www.monetdb.org/Legal/MonetDBLicense
-
-Software distributed under the License is distributed on an "AS IS"
-basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-License for the specific language governing rights and limitations
-under the License.
-
-The Original Code is the MonetDB Database System.
-
-The Initial Developer of the Original Code is CWI.
-Portions created by CWI are Copyright (C) 1997-July 2008 CWI.
-Copyright August 2008-2012 MonetDB B.V.
-All Rights Reserved.
-@
-
-@c
 /*
- * @a M.L. Kersten
+ * The contents of this file are subject to the MonetDB Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.monetdb.org/Legal/MonetDBLicense
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * The Original Code is the MonetDB Database System.
+ * 
+ * The Initial Developer of the Original Code is CWI.
+ * Portions created by CWI are Copyright (C) 1997-July 2008 CWI.
+ * Copyright August 2008-2012 MonetDB B.V.
+ * All Rights Reserved.
+*/
+
+/*
+ * M.L. Kersten
  * For documentation see website.
- */
-@h
-#ifndef _MAL_DEBUGGER_H
-#define _MAL_DEBUGGER_H
-
-#include "mal_scenario.h"
-#include "mal_client.h"
-
-#define MAL_DEBUGGER		/* debugger is active */
-
-#define MAXBREAKS 32
-
-mal_export int MDBdelay;	/* do not immediately react */
-typedef struct {
-	MalBlkPtr brkBlock[MAXBREAKS];
-	int		brkPc[MAXBREAKS];
-	int		brkVar[MAXBREAKS];
-	str		brkMod[MAXBREAKS];
-	str		brkFcn[MAXBREAKS];
-	char	brkCmd[MAXBREAKS];
-	str		brkRequest[MAXBREAKS];
-	int		brkTop;
-} mdbStateRecord, *mdbState;
-
-typedef struct MDBSTATE{
-	MalBlkPtr mb;
-	MalStkPtr stk;
-	InstrPtr p;
-	int pc;
-} MdbState;
-
-mal_export void mdbInit(void);
-mal_export void mdbSetBreakpoint(Client cntxt, MalBlkPtr mb, int pc, char cmd);
-mal_export void mdbSetBreakRequest(Client cntxt, MalBlkPtr mb, str request, char cmd);
-mal_export void mdbClrBreakRequest(Client cntxt, str name);
-mal_export void mdbShowBreakpoints(Client cntxt);
-mal_export void mdbCommand(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr p, int pc);
-mal_export int mdbSetTrap(Client cntxt, str modnme, str fcnnme, int flag);
-mal_export str mdbGrab(Client cntxt, MalBlkPtr mb1, MalStkPtr stk1, InstrPtr pc1);
-mal_export str mdbTrapClient(Client cntxt, MalBlkPtr mb1, MalStkPtr stk1, InstrPtr pc1);
-mal_export str mdbTrap(Client cntxt, MalBlkPtr mb, MalStkPtr stk, int pc);
-mal_export int mdbSession(void);
-mal_export void mdbStep(Client cntxt, MalBlkPtr mb, MalStkPtr stk, int pc);
-mal_export void mdbDump(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci);
-mal_export void mdbHelp(stream *f);
-mal_export void printBATelm(stream *f, int i, BUN cnt, BUN first);
-mal_export void printStack(stream *f, MalBlkPtr mb, MalStkPtr s);
-mal_export void printBatDetails(stream *f, int bid);
-mal_export void printBatInfo(stream *f, VarPtr n, ValPtr v);
-mal_export void printBatProperties(stream *f, VarPtr n, ValPtr v, str props);
-mal_export void printTraceCall(stream *out, MalBlkPtr mb, MalStkPtr stk, int pc, int flags);
-mal_export char BBPTraceCall(Client cntxt, MalBlkPtr mb, MalStkPtr stk, int pc);
-
-mal_export str runMALDebugger(Client cntxt, Symbol s);
-mal_export void printBBPinfo(stream *out);
-
-mal_export void optimizerDebug(Client cntxt, MalBlkPtr mb, str name, int actions, lng usec);
-mal_export str debugOptimizers(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci);
-mal_export void debugLifespan(Client cntxt, MalBlkPtr mb, Lifespan span);
-#endif /* _MAL_DEBUGGER_h */
-@c
-/*
- * @+ Debugger framework
- * Before the interpreter is allowed to proceed we give control
- * to the user to alter the trace/debugger options.
  */
 #include "monetdb_config.h"
 #include "mal_readline.h"
@@ -1573,35 +1507,9 @@ printBatProperties(stream *f, VarPtr n, ValPtr v, str props)
 }
 
 /*
- * @-
  * The memory positions for the BATs is useful information to
  * asses for memory fragmentation.
  */
-@= heapinfo
-hp= @1;
-if(hp && hp->base){
-	mnstr_printf(out,"\t@2=" PTRFMT " size=" SZFMT "\n",PTRFMTCAST hp->base, hp->size);}
-@= hashinfo
-h= @1;
-if(h && h->mask){
-	    mnstr_printf(out,"\t@2=" PTRFMT " size=" SZFMT "\n",PTRFMTCAST h, sizeof(*h));
-	    mnstr_printf(out,"\t@2link=" PTRFMT " size=" SZFMT "\n",PTRFMTCAST h->link,
-	            (h->mask+h->lim+1)*sizeof(int));
-}
-@
- * @-
- * The memProfileVector routine produces a character string to represent
- * the usage of memory by BAT information. The characters are interpreted
- * as follows:
- * .=unused, X=completely used, [0-9]=small elements within the granule
- * More than 9 elements makes it full.
-@= setVector
-	start= (((long)@1)-min)/granule;
-	lim= (((long)@1)-min + @2)/granule;
-	mnstr_printf(out,"start %ld lim %ld\n",start,lim);
-
-@
-@c
 static str
 memProfileVector(stream *out, int cells)
 {
@@ -1633,8 +1541,12 @@ memProfileVector(stream *out, int cells)
 #endif
 
 			mnstr_printf(out, "\tdesc=" PTRFMT " size=" SZFMT "\n", PTRFMTCAST b, sizeof(*b));
-			@:heapinfo(&b->H->heap,tail)@
-			@:heapinfo(&b->T->heap,head)@
+			hp= &b->H->heap;
+			if(hp && hp->base){
+				mnstr_printf(out,"\ttail=" PTRFMT " size=" SZFMT "\n",PTRFMTCAST hp->base, hp->size);}
+			hp= &b->T->heap;
+			if(hp && hp->base){
+				mnstr_printf(out,"\thead=" PTRFMT " size=" SZFMT "\n",PTRFMTCAST hp->base, hp->size);}
 #ifdef HAVE_SBRK
 			if (min == 0) {
 				min = (long) b;
@@ -1642,14 +1554,32 @@ memProfileVector(stream *out, int cells)
 				granule = (max - min) / cells;
 				mnstr_printf(out, "granule %ldK\n", granule / 1024);
 			}
-			@:setVector(b, sizeof(*b))@
-			@:setVector(hp->base, hp->size)@
+			start= (((long)b)-min)/granule;
+			lim= (((long)b)-min + sizeof(*b))/granule;
+			mnstr_printf(out,"start %ld lim %ld\n",start,lim);
+			start= (((long)hp->base)-min)/granule;
+			lim= (((long)hp->base)-min + hp->size)/granule;
+			mnstr_printf(out,"start %ld lim %ld\n",start,lim);
 #endif
 
-			@:heapinfo(b->H->vheap,hheap)@
-			@:heapinfo(b->T->vheap,theap)@
-			@:hashinfo(b->H->hash,hhash)@
-			@:hashinfo(b->T->hash,thash)@
+			hp= b->H->vheap;
+			if(hp && hp->base){
+				mnstr_printf(out,"\thheap=" PTRFMT " size=" SZFMT "\n",PTRFMTCAST hp->base, hp->size);}
+			hp= b->T->vheap;
+			if(hp && hp->base){
+				mnstr_printf(out,"\ttheap=" PTRFMT " size=" SZFMT "\n",PTRFMTCAST hp->base, hp->size);}
+			h= b->H->hash;
+			if(h && h->mask){
+					mnstr_printf(out,"\thhash=" PTRFMT " size=" SZFMT "\n",PTRFMTCAST h, sizeof(*h));
+					mnstr_printf(out,"\thhashlink=" PTRFMT " size=" SZFMT "\n",PTRFMTCAST h->link,
+							(h->mask+h->lim+1)*sizeof(int));
+			}
+			h= b->T->hash;
+			if(h && h->mask){
+					mnstr_printf(out,"\tthash=" PTRFMT " size=" SZFMT "\n",PTRFMTCAST h, sizeof(*h));
+					mnstr_printf(out,"\tthashlink=" PTRFMT " size=" SZFMT "\n",PTRFMTCAST h->link,
+							(h->mask+h->lim+1)*sizeof(int));
+			}
 			BBPunfix(b->batCacheid);
 		}
 	return v;
@@ -1712,7 +1642,7 @@ mdbHelp(stream *f)
 }
 
 /*
- * @+ Optimizer debugging
+ * Optimizer debugging
  * The modular approach to optimize a MAL program brings with it the
  * need to check individual steps. Two options come to mind. If in
  * debug mode we could stop after each optimizer action for inspection.
