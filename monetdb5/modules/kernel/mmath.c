7b5780ad8cc594c221d46d3557eaaa2d5ff4a600
Martin Kersten <mk@cwi.nl>
2012-07-16 12:53:01 +0200

    De-Mx array and mmath modules

---
+++ monetdb5/modules/kernel/mmath.c
@@ -0,0 +1,286 @@
+/*
+ * The contents of this file are subject to the MonetDB Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.monetdb.org/Legal/MonetDBLicense
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * The Original Code is the MonetDB Database System.
+ * 
+ * The Initial Developer of the Original Code is CWI.
+ * Portions created by CWI are Copyright (C) 1997-July 2008 CWI.
+ * Copyright August 2008-2012 MonetDB B.V.
+ * All Rights Reserved.
+*/
+/*
+ * N.J. Nes, M. Kersten
+ * 07/01/1996
+ * The math module
+ * This module contains the math commands. The implementation is very simply,
+ * the c math library functions are called. See for documentation the
+ * ANSI-C/POSIX manuals of the equaly named functions.
+ *
+ * NOTE: the operand itself is being modified, rather than that we produce
+ * a new BAT. This to save the expensive copying.
+ */
+#include "monetdb_config.h"
+#include "mmath.h"
+
+#ifdef _MSC_VER
+# include <float.h>
+/* Windows spells these differently */
+# define isnan(x)	_isnan(x)
+# define finite(x)	_finite(x)
+/* NOTE: HAVE_FPCLASS assumed... */
+# define fpclass(x)	_fpclass(x)
+# define FP_NINF		_FPCLASS_NINF
+# define FP_PINF		_FPCLASS_PINF
+#else /* !_MSC_VER */
+# ifdef HAVE_IEEEFP_H
+#  include <ieeefp.h>
+# endif
+#endif
+
+#if defined(HAVE_FPCLASSIFY) || defined(fpclassify)
+/* C99 interface: fpclassify */
+# define MNisinf(x)   (fpclassify(x) == FP_INFINITE)
+# define MNisnan(x)   (fpclassify(x) == FP_NAN)
+# define MNfinite(x)  (!MNisinf(x) && !MNisnan(x))
+#else
+# define MNisnan(x)   isnan(x)
+# define MNfinite(x)  finite(x)
+# ifdef HAVE_ISINF
+#  define MNisinf(x)  isinf(x)
+# else
+static int
+MNisinf(double x)
+{
+#  ifdef HAVE_FPCLASS
+	int cl = fpclass(x);
+
+	return ((cl == FP_NINF) || (cl == FP_PINF));
+#  else
+	(void)x;
+	return 0;		/* XXX not correct if infinite */
+#  endif
+}
+# endif
+#endif /* HAVE_FPCLASSIFY */
+
+#define acos_unary(x, z)      *z = acos(*x)
+#define asin_unary(x, z)      *z = asin(*x)
+#define atan_unary(x, z)      *z = atan(*x)
+#define atan2_binary(x, y, z) *z = atan2(*x,*y)
+#define cos_unary(x, z)	      *z = cos(*x)
+#define sin_unary(x, z)	      *z = sin(*x)
+#define tan_unary(x, z)	      *z = tan(*x)
+#define cot_unary(x, z)	      *z = (1/tan(*x))
+
+#define cosh_unary(x, z)       *z = cosh(*x)
+#define sinh_unary(x, z)       *z = sinh(*x)
+#define tanh_unary(x, z)       *z = tanh(*x)
+
+#define radians_unary(x, z)       *z = *x * 3.14159265358979323846 /180.0
+#define degrees_unary(x, z)       *z = *x * 180.0/3.14159265358979323846
+
+#define exp_unary(x, z)	      *z = exp(*x)
+#define log_unary(x, z)	      *z = log(*x)
+#define log10_unary(x, z)     *z = log10(*x)
+
+#define pow_binary(x, y, z)   *z = pow(*x,*y)
+#define sqrt_unary(x, z)      *z = sqrt(*x)
+
+#define ceil_unary(x, z)      *z = ((-1.0<*x)&&(*x<0.0))?0.0:ceil(*x)
+#define fabs_unary(x, z)      *z = fabs(*x)
+#define floor_unary(x, z)     *z = floor(*x)
+
+static str
+math_unary_ISNAN(bit *res, dbl *a)
+{
+#ifdef DEBUG
+	printf("math_unary_ISNAN\n");
+#endif
+	if (*a == dbl_nil) {
+		*res = bit_nil;
+	} else {
+		*res = MNisnan(*a);
+	}
+	return MAL_SUCCEED;
+}
+
+static str
+math_unary_ISINF(int *res, dbl *a)
+{
+#ifdef DEBUG
+	printf("math_unary_ISINF\n");
+#endif
+	if (*a == dbl_nil) {
+		*res = int_nil;
+	} else {
+		if (MNisinf(*a)) {
+			*res = (*a < 0.0) ? -1 : 1;
+		} else {
+			*res = 0;
+		}
+	}
+	return MAL_SUCCEED;
+}
+
+static str
+math_unary_FINITE(bit *res, dbl *a)
+{
+#ifdef DEBUG
+	printf("math_unary_FINITE\n");
+#endif
+	if (*a == dbl_nil) {
+		*res = bit_nil;
+	} else {
+		*res = MNfinite(*a);
+	}
+	return MAL_SUCCEED;
+}
+
+#define unopbaseM5(X1,X2,X3)\
+str MATHunary##X1##X3(X3 *res , X3 *a ) {\
+#ifdef DEBUG\
+		printf( "MATHunary##X1##X3\n");\
+#endif\
+	dbl tmp1,tmp2;\
+	str msg= MAL_SUCCEED;\
+	if (*a == X3##_nil) {\
+		*res = X3##_nil;\
+	} else {\
+		tmp1= *a;\
+		X2##_unary( &tmp1, &tmp2 );\
+		*res = (X3) tmp2;\
+	}\
+   return msg;\
+}
+
+#define unopM5(X1,X2) \
+  unopbaseM5(X1,X2,dbl)\
+  unopbaseM5(X1,X2,flt)
+
+#define binopbaseM5(X1,X2,X3)\
+str MATHbinary##X1##X3(X3 *res, X3 *a, X3 *b ) {\
+#ifdef DEBUG\
+		printf( "MATHbinary##X1\n");\
+#endif\
+   if (*a == X3##_nil || *b == X3##_nil) {\
+		*res = X3##_nil;\
+   } else {\
+		dbl r1 ,a1 = *a, b1 = *b;\
+		X2_binary( &a1, &b1, &r1);\
+		*res= (X3) r1;\
+   }\
+   return MAL_SUCCEED;\
+}
+
+#define binopM5(X,Y) \
+  binopbaseM5(X,Y,dbl)\
+  binopbaseM5(X,Y,flt)
+
+#define roundM5(X1)\
+str MATHbinary_ROUND##X1(1X *res, X1 *x, int *y) {\
+  if(*x == X1_nil || *y == int_nil) {\
+	*res = X1_nil;\
+  } else {\
+	dbl factor = pow(10,*y), integral;\
+	dbl tmp = *y>0?modf(*x,&integral):*x;\
+\
+	tmp *= factor;\
+	if(tmp>=0)\
+	  tmp = floor(tmp+0.5);\
+	else\
+	  tmp = ceil(tmp-0.5);\
+	tmp /= factor;\
+\
+	if(*y>0)\
+	  tmp += integral;\
+\
+	*res = (X1) tmp;\
+  }\
+  return MAL_SUCCEED;\
+}
+
+
+unopM5(_ACOS,acos)
+unopM5(_ASIN,asin)
+unopM5(_ATAN,atan)
+binopM5(_ATAN2,atan2)
+unopM5(_COS,cos)
+unopM5(_SIN,sin)
+unopM5(_TAN,tan)
+unopM5(_COT,cot)
+unopM5(_RADIANS,radians)
+unopM5(_DEGREES,degrees)
+
+unopM5(_COSH,cosh)
+unopM5(_SINH,sinh)
+unopM5(_TANH,tanh)
+
+unopM5(_EXP,exp)
+unopM5(_LOG,log)
+unopM5(_LOG10,log10)
+
+binopM5(_POW,pow)
+unopM5(_SQRT,sqrt)
+
+unopM5(_CEIL,ceil)
+unopbaseM5(_FABS,fabs,dbl)
+unopM5(_FLOOR,floor)
+
+roundM5(dbl)
+roundM5(flt)
+
+str
+MATHunary_ISNAN(bit *res, dbl *a)
+{
+	return math_unary_ISNAN(res, a);
+}
+
+str
+MATHunary_ISINF(int *res, dbl *a)
+{
+	return math_unary_ISINF(res, a);
+}
+
+str
+MATHunary_FINITE(bit *res, dbl *a)
+{
+	return math_unary_FINITE(res, a);
+}
+
+str
+MATHrandint(int *res)
+{
+	*res = rand();
+	return MAL_SUCCEED;
+}
+
+str
+MATHsrandint(int *seed)
+{
+	srand(*seed);
+	return MAL_SUCCEED;
+}
+
+str
+MATHsqlrandint(int *res, int *seed)
+{
+	srand(*seed);
+	*res = rand();
+	return MAL_SUCCEED;
+}
+
+str
+MATHpi(dbl *pi)
+{
+	*pi = 3.14159265358979323846;
+	return MAL_SUCCEED;
+}
+
---
+++ monetdb5/modules/kernel/mmath.h
@@ -0,0 +1,94 @@
+/*
+ * The contents of this file are subject to the MonetDB Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.monetdb.org/Legal/MonetDBLicense
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * The Original Code is the MonetDB Database System.
+ * 
+ * The Initial Developer of the Original Code is CWI.
+ * Portions created by CWI are Copyright (C) 1997-July 2008 CWI.
+ * Copyright August 2008-2012 MonetDB B.V.
+ * All Rights Reserved.
+*/
+/*
+ * @-
+ * The constants defined in math.h are defined in const.mx
+ */
+#ifndef __MMATH_H__
+#define __MMATH_H__
+#include "mal.h"
+#include "mal_exception.h"
+#include <math.h>
+
+extern double sqrt(double x);
+extern double cbrt(double x);
+extern double sin(double x);
+extern double cos(double x);
+extern double fabs(double x);
+
+#ifdef WIN32
+#if !defined(LIBMAL) && !defined(LIBATOMS) && !defined(LIBKERNEL) && !defined(LIBMAL) && !defined(LIBOPTIMIZER) && !defined(LIBSCHEDULER) && !defined(LIBMONETDB5)
+#define mmath_export extern __declspec(dllimport)
+#else
+#define mmath_export extern __declspec(dllexport)
+#endif
+#else
+#define mmath_export extern
+#endif
+
+#define unopbaseM5_export(X1,X2,X3)\
+mmath_export str MATHunary##X1##X2(X2 *res , X2 *a );
+
+#define unopM5_export(X1)\\
+  unopbaseM5_export(X1,dbl)\\
+  unopbaseM5_export(X1,flt)
+
+#define binopbaseM5_export(X1,X2,X3)\
+mmath_export str MATHbinary##X1##X2(X2 *res, X2 *a, X3 *b );
+
+#define binopM5_export(X1)\
+  binopbaseM5_export(X1,dbl,dbl)\
+  binopbaseM5_export(X1,flt,flt)
+
+unopM5_export(_ACOS)
+unopM5_export(_ASIN)
+unopM5_export(_ATAN)
+binopM5_export(_ATAN2)
+unopM5_export(_COS)
+unopM5_export(_SIN)
+unopM5_export(_TAN)
+unopM5_export(_COT)
+
+unopM5_export(_COSH)
+unopM5_export(_SINH)
+unopM5_export(_TANH)
+unopM5_export(_RADIANS)
+unopM5_export(_DEGREES)
+
+unopM5_export(_EXP)
+unopM5_export(_LOG)
+unopM5_export(_LOG10)
+
+binopM5_export(_POW)
+unopM5_export(_SQRT)
+
+unopM5_export(_CEIL)
+unopbaseM5_export(_FABS,dbl,dbl)
+unopM5_export(_FLOOR)
+binopbaseM5_export(_ROUND,dbl,int)
+binopbaseM5_export(_ROUND,flt,int)
+
+mmath_export str MATHunary_ISNAN(bit *res, dbl *a);
+mmath_export str MATHunary_ISINF(int *res, dbl *a);
+mmath_export str MATHunary_FINITE(bit *res, dbl *a);
+mmath_export str MATHrandint(int *res);
+mmath_export str MATHsrandint(int *seed);
+mmath_export str MATHsqlrandint(int *res, int *seed);
+mmath_export str MATHpi(dbl *pi);
+#endif /* __MMATH_H__ */
