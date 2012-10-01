c065dcb18909346d951959af3693a2ecee28e3d4
Fabian Groffen <fabian@cwi.nl>
2012-10-01 15:09:32 +0200

    Fix indentation: use tabs

--- monetdb5/extras/rdf/rdfalgebra.c
+++ monetdb5/extras/rdf/rdfalgebra.c
@@ -17,8 +17,7 @@
  * All Rights Reserved.
  */
 
-/* This contains algebra functions used for RDF store only
-*/
+/* This contains algebra functions used for RDF store only */
 
 #include "monetdb_config.h"
 #include "rdf.h"
@@ -28,27 +27,26 @@
 str
 RDFleftfetchjoin_sorted(bat *result, bat *lid, bat *rid)
 {
-        BAT *left, *right, *bn = NULL;
+	BAT *left, *right, *bn = NULL;
 
-        if ((left = BATdescriptor(*lid)) == NULL) {
-                throw(MAL, "rdf.leftfetchjoin_sorted", RUNTIME_OBJECT_MISSING);
-        }
-        if ((right = BATdescriptor(*rid)) == NULL) {
-                BBPreleaseref(left->batCacheid);
-                throw(MAL, "rdf.leftfetchjoin_sorted", RUNTIME_OBJECT_MISSING);
-        }
-        bn = BATleftfetchjoin(left, right, BUN_NONE);
-        BBPreleaseref(left->batCacheid);
-        BBPreleaseref(right->batCacheid);
-        if (bn == NULL)
-                throw(MAL, "rdf.leftfetchjoin_sorted", GDK_EXCEPTION);
+	if ((left = BATdescriptor(*lid)) == NULL) {
+		throw(MAL, "rdf.leftfetchjoin_sorted", RUNTIME_OBJECT_MISSING);
+	}
+	if ((right = BATdescriptor(*rid)) == NULL) {
+		BBPreleaseref(left->batCacheid);
+		throw(MAL, "rdf.leftfetchjoin_sorted", RUNTIME_OBJECT_MISSING);
+	}
+	bn = BATleftfetchjoin(left, right, BUN_NONE);
+	BBPreleaseref(left->batCacheid);
+	BBPreleaseref(right->batCacheid);
+	if (bn == NULL)
+		throw(MAL, "rdf.leftfetchjoin_sorted", GDK_EXCEPTION);
 
-        bn->tsorted = TRUE;
+	bn->tsorted = TRUE;
 
-        if (!(bn->batDirty&2))
-                bn = BATsetaccess(bn, BAT_READ);
-        *result = bn->batCacheid;
-        BBPkeepref(*result);
-        return MAL_SUCCEED;
+	if (!(bn->batDirty & 2))
+		bn = BATsetaccess(bn, BAT_READ);
+	*result = bn->batCacheid;
+	BBPkeepref(*result);
+	return MAL_SUCCEED;
 }
-
