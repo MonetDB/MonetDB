059062a0363ac4aae9663ea7164777bb6b7f5fd3
Fabian Groffen <fabian@monetdb.org>
2012-12-10 14:57:44 +0100

    Mtest/python: export PYTHON{2,3}PATH for tests to use
    Since Mtest can only run with one Python interpreter, the PYTHONPATH it
    builds and exports cannot always be right if we're running a specific
    python version.  Hence, export PYTHON2PATH and PYTHON3PATH such that we
    can override the PYTHONPATH manually, discarding incompatible python
    code for the python interpreter being used.

--- sql/test/mapi/Tests/python2_test_monetdb_sql.SQL.bat
+++ sql/test/mapi/Tests/python2_test_monetdb_sql.SQL.bat
@@ -3,7 +3,9 @@
 rem must be aligned with the installation directory chosen in
 rem clients/python/test/Makefile.ag
 set testpath=%TSTSRCBASE%\..\clients\python2\test
-set PYTHONPATH=%testpath%;%PYTHONPATH%
+rem ignore PYTHONPATH from Mtest, it is hardcoded to the dirs for the
+rem Python that runs Mtest (currently always Python 2)
+set PYTHONPATH=%testpath%;%PYTHON2PATH%
 
 prompt # $t $g  
 echo on
--- sql/test/mapi/Tests/python3_test_monetdb_sql.SQL.sh
+++ sql/test/mapi/Tests/python3_test_monetdb_sql.SQL.sh
@@ -3,7 +3,9 @@
 # must be aligned with the installation directory chosen in
 # clients/python/test/Makefile.ag
 testpath="$TSTSRCBASE/../clients/python3/test"
-PYTHONPATH=$testpath:$PYTHONPATH
+# ignore PYTHONPATH from Mtest, it is hardcoded to the dirs for the
+# Python that runs Mtest (currently always Python 2)
+PYTHONPATH=$testpath:${PYTHON3PATH}
 export PYTHONPATH
 
 Mlog -x "${PYTHON3} $testpath/runtests.py"
