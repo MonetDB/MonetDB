34adefb1b44960d08c402521549d98ae0c3d1db6
Fabian Groffen <fabian@monetdb.org>
2012-12-10 15:23:49 +0100

    python_dbapi: make Python2 and Python3 tests

---
+++ sql/test/mapi/Tests/python3_dbapi.SQL.bat
@@ -0,0 +1,13 @@
+@echo off
+
+prompt # $t $g  
+echo on
+
+rem must be aligned with the installation directory chosen in
+rem clients/examples/python
+set testpath=%TSTSRCBASE%\..\clients\examples\python
+rem ignore PYTHONPATH from Mtest, it is hardcoded to the dirs for the
+rem Python that runs Mtest (currently always Python 2)
+set PYTHONPATH=%PYTHON3PATH%
+
+%PYTHON3% "%testpath%/sqlsample.py" %MAPIPORT% %TSTDB%
