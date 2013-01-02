03f1d5fd1bfbbe1f7c8eb1ba8f5cd6d0f72b12de
Stefan Manegold <Stefan.Manegold@cwi.nl>
2013-01-02 20:58:19 +0100

    print correct unit

--- clients/mapiclient/tomograph.c
+++ clients/mapiclient/tomograph.c
@@ -1041,7 +1041,7 @@ static void fprintf_tm ( FILE *f, lng time, int TME )
 	}
 	if (TME & TME_MS && (tail || time >= US_MS)) {
 		fmt = tail ? "%0*d%s" : "%*d%s";
-		fprintf(f, fmt, digits, (int) (time / US_MS), (TME & TME_US) ? "." : "s");
+		fprintf(f, fmt, digits, (int) (time / US_MS), (TME & TME_US) ? "." : tail ? "s" : "ms");
 		if (time / US_MS > 99)
 			digits = 1;
 		else if (time / US_MS > 9)
