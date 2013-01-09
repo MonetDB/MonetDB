303b4443fa962785bf8361eee7cdd5df4c3a588d
Minh-Duc Pham <duc@cwi.nl>
2013-01-09 18:06:51 +0100

    Move the function TKNZRrdf2str from tokenizer to RDF module
    This function is only used for RDF thus it is moved to RDF module.

--- monetdb5/extras/rdf/rdf.h
+++ monetdb5/extras/rdf/rdf.h
@@ -50,6 +50,10 @@ RDFleftfetchjoin_sortedestimate(int *result, int *lid, int *rid, lng *estimate);
 rdf_export str 
 RDFleftfetchjoin_sorted(int *result, int* lid, int *rid);
 
+rdf_export str 
+TKNZRrdf2str (bat *res, bat *bid, bat *map);
+
+#define RDF_MIN_LITERAL (((oid) 1) << ((sizeof(oid)==8)?62:30))
 
 #define TRIPLE_STORE 1
 #define MLA_STORE    2
