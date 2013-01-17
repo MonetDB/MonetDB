f2aa769a6ce9274b9797318c5483d05aff5e0e5b
Stefan Manegold <Stefan.Manegold@cwi.nl>
2013-01-17 22:28:21 +0100

    use SQL type REAL instead of FLOAT; like SQL type DOUBLE, also FLOAT maps to C tyep double, while SQL type REAL maps to C type float.

--- sql/test/BugTracker-2013/Tests/select-around-zero.Bug-3220.sql
+++ sql/test/BugTracker-2013/Tests/select-around-zero.Bug-3220.sql
@@ -1,5 +1,5 @@
 start transaction;
-create table t3220 (y double, z float);
+create table t3220 (y double, z real);
 insert into t3220 values (0,0);
 insert into t3220 values (0.1,0.1);
 insert into t3220 values (-0.1,-0.1);
