4fb15e4646600c52951e3cd54f3a6dae3bdfe2b0
Stefan Manegold <Stefan.Manegold@cwi.nl>
2013-01-17 15:28:07 +0100

    enabled "top-level" Mtest.py
    So far, while Mtest.py could be called in any subdirectory of the MonetDB
    source tree (and could then run all tests in the entire sub-tree), it was
    not possible to call Mtest.py in the top-level MonetDB source directory to
    run all tests.
    Instead, to run all tests, Mtest.py had to be called at least 4 times, once
    in each of these directories: "clients", "monetdb5", "sql", "geom".
    
    Now, it is possible to call Mtest.py once in the top-level MonetDB source
    directory to run all tests in one go.
    
    The behaviour of calling Mtest.py in any subdirectory, including the four
    mentioned above, did not changed, other than that now obsolete command line
    options "-p / --package <package>" and "-5 / --monetdb5" have been removed.

--- sql/test/mapi/Tests/python2_test_monetdb_sql.SQL.bat
+++ sql/test/mapi/Tests/python2_test_monetdb_sql.SQL.bat
@@ -2,7 +2,7 @@
 
 rem must be aligned with the installation directory chosen in
 rem clients/python/test/Makefile.ag
-set testpath=%TSTSRCBASE%\..\clients\python2\test
+set testpath=%TSTSRCBASE%\clients\python2\test
 rem ignore PYTHONPATH from Mtest, it is hardcoded to the dirs for the
 rem Python that runs Mtest (currently always Python 2)
 set PYTHONPATH=%testpath%;%PYTHON2PATH%
