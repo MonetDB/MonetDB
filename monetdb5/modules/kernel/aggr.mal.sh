030722c8fa6f5572afce363a59842f93b1661ce8
Sjoerd Mullender <sjoerd@acm.org>
2013-01-29 21:41:27 +0100

    Fixed comments for various aggr.* functions.

--- monetdb5/modules/kernel/aggr.mal.sh
+++ monetdb5/modules/kernel/aggr.mal.sh
@@ -148,46 +148,48 @@ comment "Grouped tail average on ${tp}";
 
 command subavg(b:bat[:oid,:${tp}],g:bat[:oid,:oid],e:bat[:oid,:any_1],skip_nils:bit,abort_on_error:bit) :bat[:oid,:dbl]
 address AGGRsubavg_dbl
-comment "Grouped avg aggregate";
+comment "Grouped average aggregate";
 
 command subavg(b:bat[:oid,:${tp}],g:bat[:oid,:oid],e:bat[:oid,:any_1],s:bat[:oid,:oid],skip_nils:bit,abort_on_error:bit) :bat[:oid,:dbl]
 address AGGRsubavgcand_dbl
-comment "Grouped avg aggregate with candidates list";
+comment "Grouped average aggregate with candidates list";
 
 EOF
-    for func in stdev variance; do
+    for func in stdev:'standard deviation' variance:variance; do
+	comm=${func#*:}
+	func=${func%:*}
 	cat <<EOF
 command ${func}(b:bat[:oid,:${tp}], e:bat[:oid,:any_1]) :bat[:oid,:dbl]
 address AGGR${func}2_dbl
-comment "Grouped tail average on ${tp}";
+comment "Grouped tail ${comm} (sample/non-biased) on ${tp}";
 
 command ${func}(b:bat[:oid,:${tp}], g:bat[:oid,:oid], e:bat[:oid,:any_1]):bat[:oid,:dbl]
 address AGGR${func}3_dbl
-comment "Grouped tail average on ${tp}";
+comment "Grouped tail ${comm} (sample/non-biased) on ${tp}";
 
 command sub${func}(b:bat[:oid,:${tp}],g:bat[:oid,:oid],e:bat[:oid,:any_1],skip_nils:bit,abort_on_error:bit) :bat[:oid,:dbl]
 address AGGRsub${func}_dbl
-comment "Grouped ${func} aggregate";
+comment "Grouped ${comm} (sample/non-biased) aggregate";
 
 command sub${func}(b:bat[:oid,:${tp}],g:bat[:oid,:oid],e:bat[:oid,:any_1],s:bat[:oid,:oid],skip_nils:bit,abort_on_error:bit) :bat[:oid,:dbl]
 address AGGRsub${func}cand_dbl
-comment "Grouped ${func} aggregate with candidates list";
+comment "Grouped ${comm} (sample/non-biased) aggregate with candidates list";
 
 command ${func}p(b:bat[:oid,:${tp}], e:bat[:oid,:any_1]) :bat[:oid,:dbl]
 address AGGR${func}p2_dbl
-comment "Grouped tail average on ${tp}";
+comment "Grouped tail ${comm} (population/biased) on ${tp}";
 
 command ${func}p(b:bat[:oid,:${tp}], g:bat[:oid,:oid], e:bat[:oid,:any_1]):bat[:oid,:dbl]
 address AGGR${func}p3_dbl
-comment "Grouped tail average on ${tp}";
+comment "Grouped tail ${comm} (population/biased) on ${tp}";
 
 command sub${func}p(b:bat[:oid,:${tp}],g:bat[:oid,:oid],e:bat[:oid,:any_1],skip_nils:bit,abort_on_error:bit) :bat[:oid,:dbl]
 address AGGRsub${func}p_dbl
-comment "Grouped ${func}p aggregate";
+comment "Grouped ${comm} (population/biased) aggregate";
 
 command sub${func}p(b:bat[:oid,:${tp}],g:bat[:oid,:oid],e:bat[:oid,:any_1],s:bat[:oid,:oid],skip_nils:bit,abort_on_error:bit) :bat[:oid,:dbl]
 address AGGRsub${func}pcand_dbl
-comment "Grouped ${func}p aggregate with candidates list";
+comment "Grouped ${comm} (population/biased) aggregate with candidates list";
 
 EOF
     done
