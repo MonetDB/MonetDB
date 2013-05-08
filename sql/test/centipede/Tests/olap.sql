4c2fe5072bfdfd4e9486147b03d30dcb1839d324
Martin Kersten <mk@cwi.nl>
2013-05-08 19:53:46 +0200

    Complete the test script for centipede.

--- sql/test/centipede/Tests/olap.sql
+++ sql/test/centipede/Tests/olap.sql
@@ -21,6 +21,16 @@ explain select i,avg(i) from Rc group by j;
 explain select b,avg(b) from Sc group by a;
 explain select a,avg(a) from Sc group by b;
 
+explain select i,min(j) from Rc group by i;
+explain select j,min(i) from Rc group by j;
+explain select a,min(b) from Sc group by a;
+explain select b,min(a) from Sc group by b;
+
+explain select i,max(j) from Rc group by i;
+explain select j,max(i) from Rc group by j;
+explain select a,max(b) from Sc group by a;
+explain select b,max(a) from Sc group by b;
+
 select i,count(*) from Rc group by i;
 select j,count(*) from Rc group by j;
 select a,count(*) from Sc group by a;
