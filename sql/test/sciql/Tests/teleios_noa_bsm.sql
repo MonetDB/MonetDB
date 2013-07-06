5ebc9730382bfac900f0f62c1d6735cedcfdeb9a
Stefan Manegold <Stefan.Manegold@cwi.nl>
2013-07-06 08:20:30 +0200

    teleios_noa_bsm: finished first complete & (hopefully) correct implementation also cleaned-up code

--- sql/test/sciql/Tests/teleios_noa_bsm.sql
+++ sql/test/sciql/Tests/teleios_noa_bsm.sql
@@ -10,15 +10,10 @@ DECLARE ndviThreshold DOUBLE;
 SET ndviThreshold = 0; -- what is the correct value ?
 
 
--- implementation --
+-- loading data (images) --
 
-DECLARE d1 INT, d2 INT, majority INT;
-SET d1 = window_size / 2;
-SET d2 = d1 + 1;
-SET majority = (window_size * window_size) / 2;
-
--- Assuming these example TIF images are stored in /tmp
--- The orthorectified images need the GDAL functions attach2() and import2()
+---- Assuming these example TIF images are stored in /tmp
+---- The orthorectified images need the GDAL functions attach2() and import2()
 CALL rs.attach2('/tmp/img1_b3.tif');
 CALL rs.attach2('/tmp/img1_b4.tif');
 CALL rs.attach2('/tmp/img1_b7.tif');
@@ -34,17 +29,31 @@ CALL rs.import2(3);
 CALL rs.import2(4);
 CALL rs.import2(5);
 CALL rs.import2(6);
--- Now the TIF images have been imported as the following:
---   the b3, b4 and b7 of the 1st image as rs.image1, rs.image2 and rs.image3
---   the b3, b4 and b7 of the 2nd image as rs.image4, rs.image5 and rs.image6
+---- Now the TIF images have been imported as the following:
+---- b3, b4 and b7 of the 1st image as rs.image1, rs.image2 and rs.image3
+---- b3, b4 and b7 of the 2nd image as rs.image4, rs.image5 and rs.image6
+
+
+-- global variables and array --
+
+DECLARE d1 INT, d2 INT, majority INT;
+SET d1 = window_size / 2;
+SET d2 = d1 + 1;
+SET majority = (window_size * window_size) / 2;
 
 DECLARE size_x INT, size_y INT;
 SET size_x = (SELECT MAX(x) + 1 FROM rs.image1);
 SET size_y = (SELECT MAX(y) + 1 FROM rs.image1);
 
--- BSM classification (landsatFirePredicate()) using one image
-CREATE ARRAY fire_1 (x INT DIMENSION[size_x], y INT DIMENSION[size_y], f INT);
-INSERT INTO fire_1 (
+CREATE ARRAY fire (x INT DIMENSION[size_x], y INT DIMENSION[size_y], f INT);
+
+
+-- BSM classification (landsatFirePredicate()) --
+
+--- two versions; please choose one:
+
+---- version 1: using one image
+INSERT INTO fire (
   SELECT b3.x, b3.y, 1
   FROM rs.image1 AS b3, rs.image2 AS b4, rs.image3 AS b7
   WHERE b3.x = b4.x AND b3.y = b4.y AND b3.x = b7.x AND b3.y = b7.y -- join the images
@@ -55,9 +64,8 @@ INSERT INTO fire_1 (
     AND (CAST(b4.intensity-b7.intensity AS DOUBLE)/(b4.intensity + b7.intensity) + 1.0) * 127.5 <= 126.0 -- indexNBR, 255.0/2.0=127.5
 );
 
--- BSM classification (landsatFirePredicate()) using two images
-CREATE ARRAY fire_2 (x INT DIMENSION[size_x], y INT DIMENSION[size_y], f INT);
-INSERT INTO fire_2 (
+---- version 2: using two images
+INSERT INTO fire (
   SELECT img1_b3.x, img1_b3.y, 1
   FROM rs.image1 AS img1_b3, rs.image2 AS img1_b4, rs.image3 AS img1_b7,
        rs.image4 AS img2_b3, rs.image5 AS img2_b4
@@ -75,292 +83,255 @@ INSERT INTO fire_2 (
              CAST(img2_b4.intensity-img2_b3.intensity AS DOUBLE)/(img2_b4.intensity + img2_b3.intensity) ) > ndviThreshold
 );
 
--- BSM majority filter
----- should be:
-INSERT INTO fire_1 (
-  SELECT [x], [y], 1
-  FROM fire_1
-  GROUP BY fire_1[x-d1:x+d2][y-d1:y+d2]
-  HAVING f IS NULL AND SUM(f) > majority
-);
 
-INSERT INTO fire_2 (
-  SELECT [x], [y], 1
-  FROM fire_2
-  GROUP BY fire_2[x-d1:x+d2][y-d1:y+d2]
-  HAVING f IS NULL AND SUM(f) > majority
-);
+-- BSM majority filter --
 
----- however, the conjunctive HAVING clause yields wrong results;
----- hence:
+---- SHOULD be:
+--INSERT INTO fire (
+--  SELECT [x], [y], 1
+--  FROM fire
+--  GROUP BY fire[x-d1:x+d2][y-d1:y+d2]
+--  HAVING f IS NULL AND SUM(f) > majority
+--);
+---- HOWEVER, conjunctive HAVING clauses appear to YIELD WRONG RESULTS !??
 
-INSERT INTO fire_1 (
+---- hence, we need to avoid conjunctive HAVING clauses (for now?)
+INSERT INTO fire (
   SELECT [x], [y], 1
   FROM [
     SELECT [x], [y], f
-    FROM fire_1
-    GROUP BY fire_1[x-d1:x+d2][y-d1:y+d2]
+    FROM fire
+    GROUP BY fire[x-d1:x+d2][y-d1:y+d2]
     HAVING SUM(f) > majority
   ] AS tmp
   WHERE f IS NULL
 );
 
-INSERT INTO fire_2 (
-  SELECT [x], [y], 1
-  FROM [
-    SELECT [x], [y], f
-    FROM fire_2
-    GROUP BY fire_2[x-d1:x+d2][y-d1:y+d2]
-    HAVING SUM(f) > majority
-  ] AS tmp
-  WHERE f IS NULL
-);
 
--- BSM clump&eliminate filter
+-- BSM clump&eliminate filter --
+
 ---- initialize with distinct group ID per pixel
-UPDATE fire_1 SET f = x * size_y + y WHERE f IS NOT NULL;
-UPDATE fire_2 SET f = x * size_y + y WHERE f IS NOT NULL;
+UPDATE fire SET f = x * size_y + y WHERE f IS NOT NULL;
 
----- Clump adjacent pixels using 4-connected, i.e., each pixel has 8 neighbors,
-----   namely N, NE, E, SE, S, SW, W, NW.
-CREATE FUNCTION clump_4connected_1()
+--- two versions; please choose one:
+--- CAVEAT: these take more than 30 minutes to execute !
+
+---- version 1:
+---- Clump adjacent pixels using 4-connected,
+---- i.e., each pixel has 4 neighbors: N, E, S, W
+---- HOWEVER, this DOES NOT WORK (yet?), as the SciQL implementation does
+---- not support structural grouping (tiling) with non-rectangular windows
+---- (tiles) (yet?) !??
+--CREATE FUNCTION clump_4connected()
+--RETURNS INT
+--BEGIN
+--  DECLARE iterations INT;
+--  SET iterations = 0;
+--  DECLARE moreupdates INT;
+--  SET moreupdates = 1;
+--  WHILE moreupdates > 0 DO
+--    SET iterations = iterations + 1;
+--    INSERT INTO fire (
+--      SELECT [x], [y], MAX(f)
+--      FROM fire AS a
+--      -- the SciQL implementation does not support this GROUP BY (yet?) !??
+--      GROUP BY a[x][y], a[x+1][y], a[x][y+1], a[x-1][y], a[x][y-1]
+--      HAVING f IS NOT NULL
+--    );
+--    SELECT SUM(res) INTO moreupdates
+--    FROM (
+--      SELECT MAX(f) - MIN(f) AS res
+--      FROM fire AS a
+--      -- the SciQL implementation does not support this GROUP BY (yet?) !??
+--      GROUP BY a[x][y], a[x+1][y], a[x][y+1], a[x-1][y], a[x][y-1]
+--      HAVING f IS NOT NULL
+--    ) AS updates;
+--  END WHILE;
+--  RETURN iterations;
+--END;
+--SELECT clump_4connected();
+
+---- version 2:
+---- Clump adjacent pixels using 8-connected,
+---- i.e., each pixel has 8 neighbors: N, NE, E, SE, S, SW, W, NW
+CREATE FUNCTION clump_8connected()
 RETURNS INT
 BEGIN
   DECLARE iterations INT;
   SET iterations = 0;
   DECLARE moreupdates INT;
   SET moreupdates = 1;
-
   WHILE moreupdates > 0 DO
     SET iterations = iterations + 1;
-    INSERT INTO fire_1 (
+    INSERT INTO fire (
       SELECT [x], [y], MAX(f)
-      FROM fire_1 AS a
-      -- the SciQL implementation does not seem to support this GROUP BY (yet?) !?
-      GROUP BY a[x][y], a[x+1][y], a[x][y+1], a[x-1][y], a[x][y-1]
+      FROM fire
+      GROUP BY fire[x-1:x+2][y-1:y+2]
       HAVING f IS NOT NULL
     );
-    SELECT SUM(res) into moreupdates
+    SELECT SUM(res) INTO moreupdates
     FROM (
       SELECT MAX(f) - MIN(f) AS res
-      FROM fire_1 AS a
-      -- the SciQL implementation does not seem to support this GROUP BY (yet?) !?
-      GROUP BY a[x][y], a[x+1][y], a[x][y+1], a[x-1][y], a[x][y-1]
+      FROM fire
+      GROUP BY fire[x-1:x+2][y-1:y+2]
       HAVING f IS NOT NULL
     ) AS updates;
   END WHILE;
-
   RETURN iterations;
 END;
-
-CREATE FUNCTION clump_4connected_2()
-RETURNS INT
-BEGIN
-  DECLARE iterations INT;
-  SET iterations = 0;
-  DECLARE moreupdates INT;
-  SET moreupdates = 1;
-
-  WHILE moreupdates > 0 DO
-    SET iterations = iterations + 1;
-    INSERT INTO fire_2 (
-      SELECT [x], [y], MAX(f)
-      FROM fire_2 AS a
-      -- the SciQL implementation does not seem to support this GROUP BY (yet?) !?
-      GROUP BY a[x][y], a[x+1][y], a[x][y+1], a[x-1][y], a[x][y-1]
-      HAVING f IS NOT NULL
-    );
-    SELECT SUM(res) into moreupdates
-    FROM (
-      SELECT MAX(f) - MIN(f) AS res
-      FROM fire_2 AS a
-      -- the SciQL implementation does not seem to support this GROUP BY (yet?) !?
-      GROUP BY a[x][y], a[x+1][y], a[x][y+1], a[x-1][y], a[x][y-1]
-      HAVING f IS NOT NULL
-    ) AS updates;
-  END WHILE;
-
-  RETURN iterations;
-END;
-
-select clump_4connected_1();
-select clump_4connected_2();
-
----- Clump adjacent pixels using 8-connected, i.e., each pixel has 8 neighbors,
-----   namely N, NE, E, SE, S, SW, W, NW.
-CREATE FUNCTION clump_8connected_1()
-RETURNS INT
-BEGIN
-  DECLARE iterations INT;
-  SET iterations = 0;
-  DECLARE moreupdates INT;
-  SET moreupdates = 1;
-
-  WHILE moreupdates > 0 DO
-    SET iterations = iterations + 1;
-    INSERT INTO fire_1 (
-      SELECT [x], [y], MAX(f)
-      FROM fire_1
-      GROUP BY fire_1[x-1:x+2][y-1:y+2]
-      HAVING f IS NOT NULL
-    );
-    SELECT SUM(res) into moreupdates
-    FROM (
-      SELECT MAX(f) - MIN(f) AS res
-      FROM fire_1
-      GROUP BY fire_1[x-1:x+2][y-1:y+2]
-      HAVING f IS NOT NULL
-    ) AS updates;
-  END WHILE;
-
-  RETURN iterations;
-END;
-
-CREATE FUNCTION clump_8connected_2()
-RETURNS INT
-BEGIN
-  DECLARE iterations INT;
-  SET iterations = 0;
-  DECLARE moreupdates INT;
-  SET moreupdates = 1;
-
-  WHILE moreupdates > 0 DO
-    SET iterations = iterations + 1;
-    INSERT INTO fire_2 (
-      SELECT [x], [y], MAX(f)
-      FROM fire_2
-      GROUP BY fire_2[x-1:x+2][y-1:y+2]
-      HAVING f IS NOT NULL
-    );
-    SELECT SUM(res) into moreupdates
-    FROM (
-      SELECT MAX(f) - MIN(f) AS res
-      FROM fire_2
-      GROUP BY fire_2[x-1:x+2][y-1:y+2]
-      HAVING f IS NOT NULL
-    ) AS updates;
-  END WHILE;
-
-  RETURN iterations;
-END;
-
-select clump_8connected_1();
-select clump_8connected_2();
+SELECT clump_8connected();
 
 ---- Eliminate any groups that have few members (<10 pixels)
-UPDATE fire_1 SET f = NULL WHERE f IN (
+UPDATE fire SET f = NULL WHERE f IN (
   SELECT f
-  FROM fire_1
+  FROM fire
   WHERE f IS NOT NULL
   GROUP BY f
   HAVING COUNT(f) < 10
 );
 
-UPDATE fire_2 SET f = NULL WHERE f IN (
-  SELECT f
-  FROM fire_2
-  WHERE f IS NOT NULL
-  GROUP BY f
-  HAVING COUNT(f) < 10
-);
 
--- BSM connect nearby fires filter
+-- BSM connect nearby fires filter --
+
+--- CAVEAT: this takes more than 15 minutes to execute !
+
 ---- Union fires which are less that 3 pixels apart (using 8-CONNECTED)
 ---- Add fire bridge between them
-
----- This impl. is most probably slower,
-----   but it only(?) uses SciQL features implemented
-CREATE PROCEDURE connect_nearby_fires1()
+---- These two tables SHOULD be DECLARED inside the function,
+---- but that does not seem to work (correctly) yet !??
+---- Hence, we create them globally, for now ...
+CREATE TABLE bridges (x INT, y INT, i INT, a INT);
+CREATE TABLE trans (i INT UNIQUE, a INT, x INT);
+CREATE FUNCTION connect_neighbors()
+RETURNS TABLE (i1 INT, i2 INT)
 BEGIN
-  DECLARE merge_more INT, update_more INT;
+  --DECLARE TABLE bridges (x INT, y INT, i INT, a INT);
+  --DECLARE TABLE trans (i INT UNIQUE, a INT, x INT);
+  DECLARE iter_0 INT, iter_1 INT;
+  SET iter_0 = 0;
+  SET iter_1 = 0;
+  DECLARE merge_more INT, recurse INT;
   SET merge_more = 1;
-  SET update_more = 1;
-
-  -- Denote pairs of nearby fire groups that 
-  --   should be merged from gid_from into gid_into
-  CREATE TABLE nearby_groups (gid_from INT, gid_into INT);
-
-  -- This process needs to be repeated, because it can happen that one pixel is
-  --   the _only_ connecting point for >2 fire groups
   WHILE merge_more > 0 DO
-	-- Find pairs of nearby fire groups by checking all 8 neighbours of each
-	--   non-fire pixel to see if the neighbours denoting more than one fire
-	--   groups.
-    INSERT INTO nearby_groups (
-        SELECT DISTINCT MIN(gid), MAX(gid) FROM fire_eliminated AS fe
-        GROUP BY fe[x-2:x+3][y-2:y+3] -- 5x5 window => max distance within the window = 3
-        HAVING gid IS NULL -- this is a non-fire pixel
-           AND MIN(gid) IS NOT NULL -- this group has at least one fire pixel
-           AND MIN(gid) <> MAX(gid) -- this group has >1 fire groups
+    SET iter_0 = iter_0 + 1;
+
+    -- find neighboring fire clumps
+    DELETE FROM bridges;
+    -- SHOULD be:
+    --INSERT INTO bridges (
+    --  -- 3x3 window is to small and 5x5 is to large; hence,
+    --  -- we need to union the four possible 4x4 windows ...
+    --  SELECT x, y, MIN(f) AS i, MAX(f) AS a
+    --  FROM fire
+    --  GROUP BY fire[x-2:x+2][y-2:y+2]
+    --  HAVING f IS NULL
+    --     AND MIN(f) IS NOT NULL
+    --     AND MIN(f) <> MAX(f)
+    --  UNION ALL
+    --  SELECT x, y, MIN(f) AS i, MAX(f) AS a
+    --  FROM fire
+    --  GROUP BY fire[x-1:x+3][y-2:y+2]
+    --  HAVING f IS NULL
+    --     AND MIN(f) IS NOT NULL
+    --     AND MIN(f) <> MAX(f)
+    --  UNION ALL
+    --  SELECT x, y, MIN(f) AS i, MAX(f) AS a
+    --  FROM fire
+    --  GROUP BY fire[x-2:x+2][y-1:y+3]
+    --  HAVING f IS NULL
+    --     AND MIN(f) IS NOT NULL
+    --     AND MIN(f) <> MAX(f)
+    --  UNION ALL
+    --  SELECT x, y, MIN(f) AS i, MAX(f) AS a
+    --  FROM fire
+    --  GROUP BY fire[x-1:x+3][y-1:y+3]
+    --  HAVING f IS NULL
+    --     AND MIN(f) IS NOT NULL
+    --     AND MIN(f) <> MAX(f)
+    --);
+    ---- HOWEVER, conjunctive HAVING clauses appear to YIELD WRONG RESULTS !??
+    ---- hence, we need to avoid conjunctive HAVING clauses (for now?)
+    INSERT INTO bridges (
+      SELECT *
+      FROM (
+        -- 3x3 window is to small and 5x5 is to large; hence,
+        -- we need to union the four possible 4x4 windows ...
+        SELECT x, y, MIN(f) AS i, MAX(f) AS a
+        FROM fire
+        GROUP BY fire[x-2:x+2][y-2:y+2]
+        HAVING f IS NULL
+        UNION ALL
+        SELECT x, y, MIN(f) AS i, MAX(f) AS a
+        FROM fire
+        GROUP BY fire[x-1:x+3][y-2:y+2]
+        HAVING f IS NULL
+        UNION ALL
+        SELECT x, y, MIN(f) AS i, MAX(f) AS a
+        FROM fire
+        GROUP BY fire[x-2:x+2][y-1:y+3]
+        HAVING f IS NULL
+        UNION ALL
+        SELECT x, y, MIN(f) AS i, MAX(f) AS a
+        FROM fire
+        GROUP BY fire[x-1:x+3][y-1:y+3]
+        HAVING f IS NULL
+      ) AS t
+      WHERE i IS NOT NULL
+        AND i <> a
     );
 
-    SELECT COUNT(*) INTO merge_more FROM nearby_groups;
+    SELECT COUNT(*) INTO merge_more FROM bridges;
+    IF merge_more > 0 THEN
 
-	-- Merge fire groups by overwriting gid of the group with a smaller ID with
-	--   the gid of the other group
-	-- This process needs to be repeated to deal with recursive merging, e.g.,
-	--   (gid_from, gid_into) = {(2, 4), (4, 5)}
-    WHILE update_more > 0 DO
-      INSERT INTO fire_eliminated (
-        SELECT [x], [y], gid_into FROM fire_eliminated, nearby_groups
-        WHERE gid = gid_from);
+      -- create "bridges"
+      -- these are "big" / "wide" bridges, i.e., all pixels that are at
+      -- most 2 pixels away from both neigbors
+      INSERT INTO fire (
+        SELECT [x], [y], i
+        FROM bridges
+      );
 
-      SELECT COUNT(gid) INTO update_more FROM fire_eliminated, nearby_groups
-      WHERE gid = gid_from;
-    END WHILE;
+      -- create transition map
+      DELETE FROM trans;
+      INSERT INTO trans (i,a) (
+        SELECT i, MAX(a)
+        FROM bridges
+        GROUP BY i
+      );
+      DELETE FROM bridges;
 
-    Dare testedELETE FROM nearby_groups WHERE gid_from IS NOT NULL; 
+      -- calculate transitive closure
+      SET recurse = 1;
+      WHILE recurse > 0 DO
+        SET iter_1 = iter_1 + 1;
+        UPDATE trans SET x = (SELECT step.a FROM trans AS step WHERE trans.a = step.i);
+        UPDATE trans SET a = x WHERE x IS NOT NULL;
+        SELECT COUNT(x) INTO recurse FROM trans;
+      END WHILE;
+
+      -- connect neigboring clumps
+      INSERT INTO fire (
+        SELECT [fire.x], [fire.y], trans.a
+        FROM fire JOIN trans
+          ON fire.f = trans.i
+      );
+      DELETE FROM trans;
+
+    END IF;
+
   END WHILE;
-
-  -- TODO: add bridge!
+  RETURN SELECT iter_0, iter_1;
 END;
-
----- This impl. is most probably faster,
-----   but it uses unimplemented SciQL features
-CREATE PROCEDURE connect_nearby_fires2()
-BEGIN
-  -- Denote neighbours in the outer circle of a 5x5 window aroudn a non-fire pixel
-  -- Only gid-s in the outer circle are sufficient, since small fire groups (in
-  --   this case, contains only on fire pixel) have been eliminated by the
-  --   clump&eliminate filter
-  CREATE TABLE nearby_groups (
-	n1 INT, n2 INT,  n3 INT,  n4 INT,  n5 INT,  n6 INT,  n7 INT,  n8 INT, 
-	n9 INT, n10 INT, n11 INT, n12 INT, n13 INT, n14 INT, n15 INT, n16 INT, 
-	gid_into INT);
-
-  -- This way, we find all GIDs that need to be merged in one go
-  INSERT INTO nearby_groups (
-      SELECT fe[x-2][y-2].gid, fe[x-1][y-2].gid, fe[x][y-2].gid, fe[x+1][y-2].gid, fe[x+2][y-2].gid,
-	         fe[x-2][y-1].gid,                                                     fe[x+2][y-1].gid,
-	         fe[x-2][y].gid,                                                       fe[x+2][y].gid,
-	         fe[x-2][y+1].gid,                                                     fe[x+2][y+1].gid,
-			 fe[x-2][y+2].gid, fe[x-1][y+2].gid, fe[x][y+2].gid, fe[x+1][y+2].gid, fe[x+2][y+2].gid,
-			 MAX(gid)
-      FROM fire_eliminated AS fe
-      GROUP BY fe[x-1:x+2][y-1:y+2]
-      HAVING gid IS NULL -- this is a non-fire pixel
-         AND MIN(gid) IS NOT NULL -- this group has at least one fire pixel
-         AND MIN(gid) <> MAX(gid) -- this group has >1 fire groups
-  );
-
-  -- We overwrite all GIDs that should be merged in one go
-  INSERT INTO fire_eliminated (
-    SELECT [x], [y], gid_into FROM fire_eliminated, nearby_groups
-    WHERE gid IS NOT NULL AND gid < gid_into
-	  AND (gid = n1  OR gid = n2  OR gid = n3  OR gid = n4  OR
-		   gid = n5  OR gid = n6  OR gid = n7  OR gid = n8  OR
-		   gid = n9  OR gid = n10 OR gid = n11 OR gid = n12 OR
-		   gid = n13 OR gid = n14 OR gid = n15 OR gid = n16)
-  );
-
-  -- TODO: add bridge!
-END;
+SELECT * FROM connect_neighbors();
 
 
--- ALTERNATIVE: create single array with 3 cell values (one per band) --
+
+-- (future) ALTERNATIVE:
+-- create single array with 3 cell values (one per band)
+-- to avoid (expensive) joins (in particular in initial classification)
 
 -- option 1:
-CREATE ARRAY image123 (x INT DIMENSION[size_x], y INT DIMENSION[size_y], b3 INT DEFAULT 0, b4 INT DEFAULT 0, b7 INT DEFAULT 0);
+CREATE ARRAY image123 (x INT DIMENSION[size_x], y INT DIMENSION[size_y], b3 INT, b4 INT, b7 INT);
 INSERT INTO image123 (
   SELECT b3.x, b3.y, b3.intensity, b4.intensity, b7.intensity
   FROM rs.image1 AS b3, rs.image2 AS b4, rs.image3 AS b7
@@ -368,8 +339,14 @@ INSERT INTO image123 (
 );
 
 -- option 2:
-CREATE ARRAY image347 (x INT DIMENSION[size_x], y INT DIMENSION[size_y], b3 INT DEFAULT 0, b4 INT DEFAULT 0, b7 INT DEFAULT 0);
+CREATE ARRAY image347 (x INT DIMENSION[size_x], y INT DIMENSION[size_y], b3 INT, b4 INT, b7 INT);
 UPDATE image347 SET b3 = ( SELECT intensity FROM rs.image1 as i WHERE image347.x = i.x and image347.y = i.y );
 UPDATE image347 SET b4 = ( SELECT intensity FROM rs.image2 as i WHERE image347.x = i.x and image347.y = i.y );
 UPDATE image347 SET b7 = ( SELECT intensity FROM rs.image3 as i WHERE image347.x = i.x and image347.y = i.y );
+
+-- option 3:
+CREATE ARRAY imageB347 (x INT DIMENSION[size_x], y INT DIMENSION[size_y], b3 INT, b4 INT, b7 INT);
+INSERT INTO imageB347 (x,y,b3) SELECT [x], [y], intensity FROM rs.image1;
+INSERT INTO imageB347 (x,y,b4) SELECT [x], [y], intensity FROM rs.image2;
+INSERT INTO imageB347 (x,y,b7) SELECT [x], [y], intensity FROM rs.image3;
 
