c17880919b33cc6e4b93faa59dfc03f697c80aea
Sjoerd Mullender <sjoerd@acm.org>
2013-07-31 21:27:17 +0200

    Don't ignore php.ini file. This makes that socket_read() can be found again.

--- sql/test/BugTracker-2013/Tests/php-size-limit-bug.SQL.sh
+++ sql/test/BugTracker-2013/Tests/php-size-limit-bug.SQL.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-Mlog -x "php -n -d include_path=$PHP_INCPATH -f $TSTSRCDIR/php-size-limit-bug.php $MAPIPORT $TSTDB"
+Mlog -x "php -d include_path=$PHP_INCPATH -f $TSTSRCDIR/php-size-limit-bug.php $MAPIPORT $TSTDB"
