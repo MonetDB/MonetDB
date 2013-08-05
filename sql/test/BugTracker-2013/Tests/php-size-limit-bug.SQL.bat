7554d066b049492159db965d8946697ea060d456
Stefan Manegold <Stefan.Manegold@cwi.nl>
2013-08-05 10:42:14 +0200

    Backing out changeset 5c511d5ec062:
    Not using "-n" with php on Windows results in a segfault,
    and thus does not help to avoid
    Fatal error: Call to undefined function socket_create() in ...\install\share\php\monetdb\php_mapi.inc on line 636

--- sql/test/BugTracker-2013/Tests/php-size-limit-bug.SQL.bat
+++ sql/test/BugTracker-2013/Tests/php-size-limit-bug.SQL.bat
@@ -3,4 +3,4 @@
 prompt # $t $g  
 echo on
 
-php -d "include_path=%PHP_INCPATH%" -f "%TSTSRCDIR%\php-size-limit-bug.php" %MAPIPORT% %TSTDB%
+php -n -d "include_path=%PHP_INCPATH%" -f "%TSTSRCDIR%\php-size-limit-bug.php" %MAPIPORT% %TSTDB%
