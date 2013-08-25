ceecc6620e6e3634060fb89bd68e7f3d6fc8ce8a
Niels Nes <niels@cwi.nl>
2013-08-25 17:30:16 +0200

    more missing tests ..

---
+++ sql/benchmarks/tpch/fileleak/Tests/delete_all.SQL.py
@@ -0,0 +1,28 @@
+
+
+import monetdb.sql
+import os, sys, time
+
+port = int(os.environ['MAPIPORT'])
+db = os.environ['TSTDB']
+
+dbh = monetdb.sql.Connection(port=port,database=db,autocommit=True)
+
+cursor = dbh.cursor();
+
+cursor.execute('select p.*, "location", "count" from storage(), (select value from env() where name = \'gdk_dbpath\') as p where "table"=\'lineitem\';');
+res = (cursor.fetchall())
+for (dbpath, fn, count) in res:
+    fn =  os.path.join(dbpath, 'bat', fn + '.tail');
+    print(fn,os.path.getsize(fn), count)
+
+cursor.execute('delete from lineitem;');
+
+cursor.execute('select "location", "count" from storage() where "table" = \'lineitem\';');
+print(cursor.fetchall())
+
+cursor.execute('select p.*, "location", "count" from storage(), (select value from env() where name = \'gdk_dbpath\') as p where "table"=\'lineitem\';');
+res = (cursor.fetchall())
+for (dbpath, fn, count) in res:
+    fn =  os.path.join(dbpath, 'bat', fn + '.tail');
+    print(fn,os.path.getsize(fn), count)
