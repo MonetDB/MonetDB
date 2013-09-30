20e21caf0d0119280540eceb75dcc72e4c5e4d53
Stefan Manegold <Stefan.Manegold@cwi.nl>
2013-09-30 10:39:00 +0200

    constraint_checking.Bug_3335: removed redundant test; see also sql/test/BugTracker-2013/Tests/check-constraint.Bug-3335.*

--- sql/test/BugTracker-2013/Tests/constraint_checking.Bug_3335.sql
+++
@@ -1,7 +0,0 @@
-create table ary(x integer check(x >0 and x <2));
-insert into ary values(1);
-insert into ary values(0);
-insert into ary values(2);
-insert into ary values(-1);
-insert into ary values(3);
-drop table ary;
