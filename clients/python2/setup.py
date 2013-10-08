91a4a57259429af8906c790e9262de326cce11c5
Hannes Muehleisen <hannes@cwi.nl>
2013-10-08 16:11:04 +0200

    Pre-release version number update.

--- clients/python2/setup.py
+++ clients/python2/setup.py
@@ -24,14 +24,14 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='python-monetdb',
-    version='11.15.16',
+    version='11.15.17',
     description='Native MonetDB client Python API',
     long_description=read('README.rst'),
     author='MonetDB BV',
     author_email='info@monetdb.org',
     url='http://www.monetdb.org/',
     packages=['monetdb', 'monetdb.sql'],
-    download_url='<will be filled in before a release>/python2-monetdb-11.15.16.tar.gz',
+    download_url='http://dev.monetdb.org/downloads/sources/Feb2013-SP5/python2-monetdb-11.15.17.tar.gz',
     classifiers=[
         "Topic :: Database",
         "Topic :: Database :: Database Engines/Servers",
