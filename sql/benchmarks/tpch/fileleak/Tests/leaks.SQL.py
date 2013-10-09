e99362bb23313b6cc03a18cf4f34d3895bd518bf
Sjoerd Mullender <sjoerd@acm.org>
2013-10-09 10:06:57 +0200

    Take changeset f8067152042d to its logical conclusion. No need for nogeom tests, also make leaks test independent of file names.

--- sql/benchmarks/tpch/fileleak/Tests/leaks.SQL.py
+++ sql/benchmarks/tpch/fileleak/Tests/leaks.SQL.py
@@ -10,10 +10,8 @@ dbh = monetdb.sql.Connection(port=port,database=db,autocommit=True)
 
 cursor = dbh.cursor();
 
-cursor.execute('select p.*, "location", "count" from storage(), (select value from env() where name = \'gdk_dbpath\') as p where "table"=\'lineitem\';');
+cursor.execute('select p.*, "location", "count", "column" from storage(), (select value from env() where name = \'gdk_dbpath\') as p where "table"=\'lineitem\' order by "column"');
 res = (cursor.fetchall())
-for (dbpath, fn, count) in res:
-    f = fn
+for (dbpath, fn, count, column) in res:
     fn =  os.path.join(dbpath, 'bat', fn + '.tail');
-    print(f, int(os.path.getsize(fn)), count)
-
+    print(column, int(os.path.getsize(fn)), count)
