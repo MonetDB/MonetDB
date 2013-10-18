a4d6302a53f9c0f7bb8545d91f99e41786610471
Sjoerd Mullender <sjoerd@acm.org>
2013-10-18 09:21:09 +0200

    Fix test. The previous version was used to produce the desired output, this is what we want to test.

--- sql/test/BugTracker-2013/Tests/case-orderby.Bug-3388.sql
+++ sql/test/BugTracker-2013/Tests/case-orderby.Bug-3388.sql
@@ -11,6 +11,6 @@ insert into table3388 values (7,null);
 insert into table3388 values (8,null);
 insert into table3388 values (9,null);
 
-select sum(b) from table3388 group by k order by sum(b);
+select sum(b) from table3388 group by k order by case when sum(b) is null then 1 else 0 end,sum(b);
 
 rollback;
