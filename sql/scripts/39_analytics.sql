c96e5906dc7abac51eeb15b9e7cf51bf27127db4
Hannes Muehleisen <hannes@cwi.nl>
2013-11-09 12:36:32 +0100

    moved the comments to the correct place, no SQL qantiles for now

--- sql/scripts/39_analytics.sql
+++ sql/scripts/39_analytics.sql
@@ -108,20 +108,20 @@ create aggregate median(val TIME) returns TIME
 create aggregate median(val TIMESTAMP) returns TIMESTAMP
 	external name "aggr"."median";
 	
-create aggregate quantile(val TINYINT, q DOUBLE) returns TINYINT
-	external name "aggr"."quantile";
-create aggregate quantile(val SMALLINT, q DOUBLE) returns SMALLINT
-	external name "aggr"."quantile";
-create aggregate quantile(val INTEGER, q DOUBLE) returns INTEGER
-	external name "aggr"."quantile";
-create aggregate quantile(val WRD, q DOUBLE) returns WRD
-	external name "aggr"."quantile";
-create aggregate quantile(val BIGINT, q DOUBLE) returns BIGINT
-	external name "aggr"."quantile";
-create aggregate quantile(val REAL, q DOUBLE) returns REAL
-	external name "aggr"."quantile";
-create aggregate quantile(val DOUBLE, q DOUBLE) returns DOUBLE
-	external name "aggr"."quantile";
+-- create aggregate quantile(val TINYINT, q DOUBLE) returns TINYINT
+-- 	external name "aggr"."quantile";
+-- create aggregate quantile(val SMALLINT, q DOUBLE) returns SMALLINT
+-- 	external name "aggr"."quantile";
+-- create aggregate quantile(val INTEGER, q DOUBLE) returns INTEGER
+-- 	external name "aggr"."quantile";
+-- create aggregate quantile(val WRD, q DOUBLE) returns WRD
+-- 	external name "aggr"."quantile";
+-- create aggregate quantile(val BIGINT, q DOUBLE) returns BIGINT
+-- 	external name "aggr"."quantile";
+-- create aggregate quantile(val REAL, q DOUBLE) returns REAL
+-- 	external name "aggr"."quantile";
+-- create aggregate quantile(val DOUBLE, q DOUBLE) returns DOUBLE
+-- 	external name "aggr"."quantile";
 
 create aggregate quantile(val DATE, q DOUBLE) returns DATE
 	external name "aggr"."quantile";
