aa279536c927cf5f1244c2eaf5cccdfcd3214216
Sjoerd Mullender <sjoerd@acm.org>
2013-11-26 11:02:57 +0100

    Make test independent on the id a schema happens to have.

--- sql/test/BugTracker/Tests/set_sql_debug_64__breaking_the_DB.SF-1906287_drop.sql
+++ sql/test/BugTracker/Tests/set_sql_debug_64__breaking_the_DB.SF-1906287_drop.sql
@@ -1,4 +1,4 @@
-select * from users where name like '%skyserver%';
+select u.name, u.fullname, s.name as default_schema from sys.users u, sys.schemas s where u.default_schema = s.id and u.name like '%skyserver%';
 
 alter user "skyserver" set schema "sys";
 drop schema sky;
