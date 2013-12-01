20287848b32b6cbdeffd63f9948114a5bb7b384c
Niels Nes <niels@cwi.nl>
2013-12-01 12:08:24 +0100

    fixed bug 3399, if no apply is found/needed we can fallback to an empty projection. fixed bug in pqueue.c: handle voids (again)

--- sql/test/BugTracker-2013/Tests/syntaxerror.Bug-3399.sql
+++ sql/test/BugTracker-2013/Tests/syntaxerror.Bug-3399.sql
@@ -4,5 +4,11 @@ create table bla (id int);
 
 select (select id) from bla;
 
+insert into bla values(10);
+insert into bla values(1);
+select (select id) from bla;
+
+drop table bla;
+
 select *, (select *) from tmp;
 
