f43fd9f1f588d0338a5ad1ab350baef7e3be4bb5
Sjoerd Mullender <sjoerd@acm.org>
2014-01-21 13:42:08 +0100

    Calculate current time to compare with as late as possible. This way there should be a better chance of getting the desired answer from the query.

--- sql/test/BugTracker-2012/Tests/currenttime.Bug-2781.py
+++ sql/test/BugTracker-2012/Tests/currenttime.Bug-2781.py
@@ -4,12 +4,6 @@ try:
 except ImportError:
     import process
 
-
-currenttime = time.strftime('%H:%M:%S', time.localtime(time.time()))
-
-#SQL command for checking the localtime
-sqlcommand = "select (localtime() - time '%s' < time '00:00:20') and (time '%s' - localtime() < time '00:00:20');" %(currenttime, currenttime)
-
 def server_start():
     sys.stderr.write('#mserver\n')
     sys.stderr.flush()
@@ -17,17 +11,20 @@ def server_start():
                          stdout = process.PIPE, stderr = process.PIPE)
     return srv
 
-def client(lang, sqlCommand, user = 'monetdb', passwd = 'monetdb'):
+def client(lang, user = 'monetdb', passwd = 'monetdb'):
     sys.stderr.write('#client\n')
     sys.stderr.flush()
     clt = process.client(lang, user = user, passwd = passwd,
                          stdin = process.PIPE,
                          stdout = process.PIPE, stderr = process.PIPE)
-    return clt.communicate(sqlCommand)
+    currenttime = time.strftime('%H:%M:%S', time.localtime(time.time()))
+    #SQL command for checking the localtime
+    sqlcommand = "select (localtime() - time '%s' < time '00:00:20') and (time '%s' - localtime() < time '00:00:20');" % (currenttime, currenttime)
+    return clt.communicate(sqlcommand)
 
 def main():
     srv = server_start()
-    out, err = client('sql',sqlcommand)
+    out, err = client('sql')
 
     sys.stdout.write(out)
     sys.stderr.write(err)
