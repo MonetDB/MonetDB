88603c1e462b8e1cca4d2db2b9599ac2ab161a91
Niels Nes <niels@cwi.nl>
2014-01-23 21:35:44 +0100

    enable debugging of the storage layer and gdk logging This is passed using --set sql_debug=
    Known debug levels at this point
    
    512		join index
    256		enable rel_planner
    128		gdk_logger	don't log bat changes
    64		singleuser
    32		readonly
    2		debug - store
    1 		debug - logger

--- sql/storage/bat/bat_logger.c
+++ sql/storage/bat/bat_logger.c
@@ -242,11 +242,11 @@ bl_postversion( void *lg)
 }
 
 static int 
-bl_create(char *logdir, int cat_version)
+bl_create(int debug, char *logdir, int cat_version)
 {
 	if (bat_logger)
 		return LOG_ERR;
-	bat_logger = logger_create(0, "sql", logdir, cat_version, bl_preversion, bl_postversion);
+	bat_logger = logger_create(debug, "sql", logdir, cat_version, bl_preversion, bl_postversion);
 	if (bat_logger)
 		return LOG_OK;
 	return LOG_ERR;
