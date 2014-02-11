56c5c505874053b6950062ea868ca1cb8e9ba39c
Stefan Manegold <Stefan.Manegold@cwi.nl>
2014-02-12 00:17:26 +0100

    sql_cast.c: use C include files for type-expansion

---
+++ sql/backends/monet5/sql_cast_impl_up_to_int.h
@@ -0,0 +1,304 @@
+/*
+ * The contents of this file are subject to the MonetDB Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.monetdb.org/Legal/MonetDBLicense
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is the MonetDB Database System.
+ *
+ * The Initial Developer of the Original Code is CWI.
+ * Portions created by CWI are Copyright (C) 1997-July 2008 CWI.
+ * Copyright August 2008-2014 MonetDB B.V.
+ * All Rights Reserved.
+ */
+
+/* ! ENSURE THAT THESE LOCAL MACROS ARE UNDEFINED AT THE END OF THIS FILE ! */
+
+/* stringify token */
+#define _STRNG_(s) #s
+#define STRNG(t) _STRNG_(t)
+
+/* concatenate two or four tokens */
+#define CONCAT_2(a,b)     a##b
+#define CONCAT_4(a,b,c,d) a##b##c##d
+
+#define NIL(t)       CONCAT_2(t,_nil)
+#define TPE(t)       CONCAT_2(TYPE_,t)
+#define FUN(a,b,c,d) CONCAT_4(a,b,c,d)
+
+
+str
+FUN(,TP1,_2_,TP2) (TP2 *res, TP1 *v)
+{
+	/* shortcut nil */
+	if (*v == NIL(TP1)) {
+		*res = NIL(TP2);
+		return (MAL_SUCCEED);
+	}
+
+	/* since the TP2 type is bigger than or equal to the TP1 type, it will
+	   always fit */
+	*res = (TP2) *v;
+	return (MAL_SUCCEED);
+}
+
+str
+FUN(bat,TP1,_2_,TP2) (int *res, int *bid)
+{
+	BAT *b, *bn;
+	TP1 *p, *q;
+	TP2 *o;
+
+	if ((b = BATdescriptor(*bid)) == NULL) {
+		throw(SQL, "batcalc."STRNG(FUN(,TP1,_2_,TP2)), "Cannot access descriptor");
+	}
+	bn = BATnew(TYPE_void, TPE(TP2), BATcount(b));
+	if (bn == NULL) {
+		BBPreleaseref(b->batCacheid);
+		throw(SQL, "sql."STRNG(FUN(,TP1,_2_,TP2)), MAL_MALLOC_FAIL);
+	}
+	bn->hsorted = b->hsorted;
+	bn->hrevsorted = b->hrevsorted;
+	BATseqbase(bn, b->hseqbase);
+	o = (TP2 *) Tloc(bn, BUNfirst(bn));
+	p = (TP1 *) Tloc(b, BUNfirst(b));
+	q = (TP1 *) Tloc(b, BUNlast(b));
+	bn->T->nonil = 1;
+	if (b->T->nonil) {
+		for (; p < q; p++, o++)
+			*o = (TP2) *p;
+	} else {
+		for (; p < q; p++, o++)
+			if (*p == NIL(TP1)) {
+				*o = NIL(TP2);
+				bn->T->nonil = FALSE;
+			} else
+				*o = (TP2) *p;
+	}
+	BATsetcount(bn, BATcount(b));
+	bn->hrevsorted = bn->batCount <= 1;
+	bn->tsorted = 0;
+	bn->trevsorted = 0;
+	BATkey(BATmirror(bn), FALSE);
+
+	if (!(bn->batDirty & 2))
+		bn = BATsetaccess(bn, BAT_READ);
+
+	if (b->htype != bn->htype) {
+		BAT *r = VIEWcreate(b, bn);
+
+		BBPkeepref(*res = r->batCacheid);
+		BBPreleaseref(bn->batCacheid);
+		BBPreleaseref(b->batCacheid);
+		return MAL_SUCCEED;
+	}
+	BBPkeepref(*res = bn->batCacheid);
+	BBPreleaseref(b->batCacheid);
+	return MAL_SUCCEED;
+}
+
+str
+FUN(,TP1,_dec2_,TP2) (TP2 *res, int *s1, TP1 *v)
+{
+	int scale = *s1;
+	TP2 r, h = (*v < 0) ? -5 : 5;
+
+	/* shortcut nil */
+	if (*v == NIL(TP1)) {
+		*res = NIL(TP2);
+		return (MAL_SUCCEED);
+	}
+
+	/* since the TP2 type is bigger than or equal to the TP1 type, it will
+	   always fit */
+	r = (TP2) *v;
+	if (scale)
+		r = (TP2) ((r + h * scales[scale - 1]) / scales[scale]);
+	*res = r;
+	return (MAL_SUCCEED);
+}
+
+str
+FUN(,TP1,_dec2dec_,TP2) (TP2 *res, int *S1, TP1 *v, int *d2, int *S2)
+{
+	int p = *d2, inlen = 1;
+	TP1 cpyval = *v;
+	int s1 = *S1, s2 = *S2;
+	TP2 r, h = (*v < 0) ? -5 : 5;
+
+	/* shortcut nil */
+	if (*v == NIL(TP1)) {
+		*res = NIL(TP2);
+		return (MAL_SUCCEED);
+	}
+
+	/* count the number of digits in the input */
+	while (cpyval /= 10)
+		inlen++;
+
+	/* rounding is allowed */
+	inlen += (s2 - s1);
+	if (p && inlen > p) {
+		throw(SQL, "convert", "22003!too many digits (%d > %d)", inlen, p);
+	}
+
+	/* since the TP2 type is bigger than or equal to the TP1 type, it will
+	   always fit */
+	r = (TP2) *v;
+	if (s2 > s1)
+		r *= (TP2) scales[s2 - s1];
+	else if (s2 != s1)
+		r = (TP2) ((r + h * scales[s1 - s2 - 1]) / scales[s1 - s2]);
+	*res = r;
+	return (MAL_SUCCEED);
+}
+
+str
+FUN(,TP1,_num2dec_,TP2) (TP2 *res, TP1 *v, int *d2, int *s2)
+{
+	int zero = 0;
+	return FUN(,TP1,_dec2dec_,TP2)(res, &zero, v, d2, s2);
+}
+
+str
+FUN(bat,TP1,_dec2_,TP2) (int *res, int *s1, int *bid)
+{
+	BAT *b, *bn;
+	TP1 *p, *q;
+	char *msg = NULL;
+	int scale = *s1;
+	TP2 *o;
+
+	if ((b = BATdescriptor(*bid)) == NULL) {
+		throw(SQL, "batcalc."STRNG(FUN(,TP1,_dec2_,TP2)), "Cannot access descriptor");
+	}
+	bn = BATnew(TYPE_void, TPE(TP2), BATcount(b));
+	if (bn == NULL) {
+		BBPreleaseref(b->batCacheid);
+		throw(SQL, "sql."STRNG(FUN(,TP1,_dec2_,TP2)), MAL_MALLOC_FAIL);
+	}
+	bn->hsorted = b->hsorted;
+	bn->hrevsorted = b->hrevsorted;
+	BATseqbase(bn, b->hseqbase);
+	o = (TP2 *) Tloc(bn, BUNfirst(bn));
+	p = (TP1 *) Tloc(b, BUNfirst(b));
+	q = (TP1 *) Tloc(b, BUNlast(b));
+	bn->T->nonil = 1;
+	if (b->T->nonil) {
+		if (scale)
+			for (; p < q; p++, o++)
+				*o = (TP2) ((*p + (*p < 0 ? -5 : 5) * scales[scale - 1]) / scales[scale]);
+		else
+			for (; p < q; p++, o++)
+				*o = (TP2) (*p);
+	} else {
+		for (; p < q; p++, o++) {
+			if (*p == NIL(TP1)) {
+				*o = NIL(TP2);
+				bn->T->nonil = FALSE;
+			} else if (scale) {
+				*o = (TP2) ((*p + (*p < 0 ? -5 : 5) * scales[scale - 1]) / scales[scale]);
+			} else {
+				*o = (TP2) (*p);
+			}
+		}
+	}
+	BATsetcount(bn, BATcount(b));
+	bn->hrevsorted = bn->batCount <= 1;
+	bn->tsorted = 0;
+	bn->trevsorted = 0;
+	BATkey(BATmirror(bn), FALSE);
+
+	if (!(bn->batDirty & 2))
+		bn = BATsetaccess(bn, BAT_READ);
+
+	if (b->htype != bn->htype) {
+		BAT *r = VIEWcreate(b, bn);
+
+		BBPkeepref(*res = r->batCacheid);
+		BBPreleaseref(bn->batCacheid);
+		BBPreleaseref(b->batCacheid);
+		return msg;
+	}
+	BBPkeepref(*res = bn->batCacheid);
+	BBPreleaseref(b->batCacheid);
+	return msg;
+}
+
+str
+FUN(bat,TP1,_dec2dec_,TP2) (int *res, int *S1, int *bid, int *d2, int *S2)
+{
+	BAT *b, *dst;
+	BATiter bi;
+	BUN p, q;
+	char *msg = NULL;
+
+	if ((b = BATdescriptor(*bid)) == NULL) {
+		throw(SQL, "batcalc."STRNG(FUN(,TP1,_dec2dec_,TP2)), "Cannot access descriptor");
+	}
+	bi = bat_iterator(b);
+	dst = BATnew(b->htype, TPE(TP2), BATcount(b));
+	if (dst == NULL) {
+		BBPreleaseref(b->batCacheid);
+		throw(SQL, "sql."STRNG(FUN(,TP1,_dec2dec_,TP2)), MAL_MALLOC_FAIL);
+	}
+	BATseqbase(dst, b->hseqbase);
+	BATloop(b, p, q) {
+		TP1 *v = (TP1 *) BUNtail(bi, p);
+		TP2 r;
+		msg = FUN(,TP1,_dec2dec_,TP2)(&r, S1, v, d2, S2);
+		if (msg)
+			break;
+		BUNins(dst, BUNhead(bi, p), &r, FALSE);
+	}
+	BBPkeepref(*res = dst->batCacheid);
+	BBPunfix(b->batCacheid);
+	return msg;
+}
+
+str
+FUN(bat,TP1,_num2dec_,TP2) (int *res, int *bid, int *d2, int *s2)
+{
+	BAT *b, *dst;
+	BATiter bi;
+	BUN p, q;
+	char *msg = NULL;
+
+	if ((b = BATdescriptor(*bid)) == NULL) {
+		throw(SQL, "batcalc."STRNG(FUN(,TP1,_num2dec_,TP2)), "Cannot access descriptor");
+	}
+	bi = bat_iterator(b);
+	dst = BATnew(b->htype, TPE(TP2), BATcount(b));
+	if (dst == NULL) {
+		BBPreleaseref(b->batCacheid);
+		throw(SQL, "sql."STRNG(FUN(,TP1,_num2dec_,TP2)), MAL_MALLOC_FAIL);
+	}
+	BATseqbase(dst, b->hseqbase);
+	BATloop(b, p, q) {
+		TP1 *v = (TP1 *) BUNtail(bi, p);
+		TP2 r;
+		msg = FUN(,TP1,_num2dec_,TP2)(&r, v, d2, s2);
+		if (msg)
+			break;
+		BUNins(dst, BUNhead(bi, p), &r, FALSE);
+	}
+	BBPkeepref(*res = dst->batCacheid);
+	BBPunfix(b->batCacheid);
+	return msg;
+}
+
+
+/* undo local defines */
+#undef FUN
+#undef NIL
+#undef TPE
+#undef CONCAT_2
+#undef CONCAT_4
+#undef STRNG
+#undef _STRNG_
+
