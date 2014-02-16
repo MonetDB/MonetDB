e3e8f6e78ba3561e3626c033cfca562243d8a5aa
Fabian Groffen <fabian@monetdb.org>
2014-02-16 22:28:56 +0100

    AUTHinitTables: allow passing a password for monetdb user
    This allows to securely setup a new database.

--- monetdb5/mal/mal_authorize.c
+++ monetdb5/mal/mal_authorize.c
@@ -112,12 +112,13 @@ AUTHcommit(void)
  * Localize the authorization tables in the database.  The authorization
  * tables are a set of aligned BATs that store username, password (hashed)
  * and scenario permissions.
- * If the BATs do not exist, they are created, and the monetdb/monetdb
- * administrator account is added.  Initialising the authorization tables
- * can only be done after the GDK kernel has been initialized.
+ * If the BATs do not exist, they are created, and the monetdb
+ * administrator account is added with the given password (or 'monetdb'
+ * if NULL).  Initialising the authorization tables can only be done
+ * after the GDK kernel has been initialized.
  */
 str
-AUTHinitTables(void) {
+AUTHinitTables(str *passwd) {
 	bat bid;
 	BAT *b;
 	int isNew = 1;
@@ -168,12 +169,13 @@ AUTHinitTables(void) {
 		/* insert the monetdb/monetdb administrator account on a
 		 * complete fresh and new auth tables system */
 		str user = "monetdb";
-		str pw; /* will become the right hash for "monetdb" */
-		int len = (int) strlen(user);
+		str pw = "monetdb";
 		oid uid;
 		Client c = &mal_clients[0];
 
-		pw = mcrypt_BackendSum(user /* because user == pass */, len);
+		if (passwd != NULL && *passwd != NULL)
+			pw = *passwd;
+		pw = mcrypt_BackendSum(pw, strlen(pw));
 		msg = AUTHaddUser(&uid, &c, &user, &pw);
 		free(pw);
 		if (msg)
