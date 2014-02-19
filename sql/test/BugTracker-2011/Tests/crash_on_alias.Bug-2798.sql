190acbf8bc1fd2b5f0079e79bdc7b0b6de597aa0
Stefan Manegold <Stefan.Manegold@cwi.nl>
2014-02-19 18:21:36 +0100

    crash_on_alias.Bug-2798: extended test with EXPLAIN to analyze/debug crash with int128

--- sql/test/BugTracker-2011/Tests/crash_on_alias.Bug-2798.sql
+++ sql/test/BugTracker-2011/Tests/crash_on_alias.Bug-2798.sql
@@ -5,9 +5,22 @@ CREATE TABLE dbg (a INT, b INT);
 INSERT INTO dbg (a,b) VALUES (10,10);
 
 -- no alias around the SUM(b), works
+set optimizer = 'sequential_pipe';
+explain SELECT a as d, SUM(b), (2 * (SUM(b) / (SELECT 2))) as f FROM dbg GROUP BY d;
+set optimizer = 'default_pipe';
 SELECT a as d, SUM(b), (2 * (SUM(b) / (SELECT 2))) as f FROM dbg GROUP BY d;
 
+ROLLBACK;
+
+START TRANSACTION;
+
+CREATE TABLE dbg (a INT, b INT);
+INSERT INTO dbg (a,b) VALUES (10,10);
+
 -- alias e, crashes :/
+set optimizer = 'sequential_pipe';
+explain SELECT a as d, SUM(b) as e, (2 * (SUM(b) / (SELECT 2))) as f FROM dbg GROUP BY d;
+set optimizer = 'default_pipe';
 SELECT a as d, SUM(b) as e, (2 * (SUM(b) / (SELECT 2))) as f FROM dbg GROUP
 BY d;
 
