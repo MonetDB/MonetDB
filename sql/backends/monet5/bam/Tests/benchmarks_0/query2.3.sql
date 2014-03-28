1af52b71bbd5f852f2875dabc9e875fd0425f27f
Robin Cijvat <c.p.cijvat@cwi.nl>
2014-03-28 08:07:58 +0100

    Some renaming; module is not *just* a BAM loader

--- sql/backends/monet5/bam/Tests/benchmarks_0/query2.3.sql
+++ sql/backends/monet5/bam/Tests/benchmarks_0/query2.3.sql
@@ -1,8 +1,8 @@
 SELECT qname, flag, rname, pos, mapq, cigar, rnext, pnext, tlen, seq, qual
-FROM bam.alignments_4
+FROM bam.alignments_i
 WHERE qname IN (
     SELECT qname
-    FROM bam.alignments_4
+    FROM bam.alignments_i
     GROUP BY qname
     HAVING SUM(bam_flag(flag, 'firs_segm')) = 0
         OR SUM(bam_flag(flag, 'last_segm')) = 0
--- sql/backends/monet5/bamloader/Tests/load.sql
+++
@@ -1,2 +0,0 @@
-CALL bam_loader_repos('PWD/bam', 0, 32);
-CALL bam_loader_repos('PWD/bam/queryname', 1, 32);
