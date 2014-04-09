eaa265aa59d60595628f3d109baa5e0fd718cb93
Robin Cijvat <c.p.cijvat@cwi.nl>
2014-04-09 21:52:49 +0200

    Fixed tests.

--- sql/backends/monet5/bam/Tests/test.SQL.py
+++ sql/backends/monet5/bam/Tests/test.SQL.py
@@ -30,6 +30,9 @@ sys.stderr.write(err)
 
 
 # now retrieve the file ids that have been inserted
+# (should be 1,2,..,nr_files, but still we extract it from the
+# db to be flexible to logical changes in the bam loader as
+# much as possible)
 c = new_client()
 c.stdin.write("SELECT file_id, dbschema FROM bam.files;")
 out, err = c.communicate()
@@ -38,7 +41,8 @@ files_to_test = []
 for match in p.finditer(out):
     files_to_test.append((int(match.group(1)), int(match.group(2))))
 
-#now we will execute all benchmark queries on all BAM files in the bam.files table
+# Now we will execute all benchmark queries on all BAM files in the bam.files table
+# and output all data contained in the aux table.
 c = new_client()
 set_var(c, 'rname_1_3', 'chr22', numeric=False)
 set_var(c, 'pos_1_3_1', 1000000)
@@ -57,13 +61,25 @@ for f in files_to_test:
     for uc in range(1, 6):
         for ln in open(os.path.join(SRCDIR, 'benchmarks_%s/query1.%d.sql'% (f[1], uc))):
             c.stdin.write(ln.replace('alignments_i', 'alignments_%d'% f[0]))
+
+    #benchmark 2
     for uc in range(1, 13):
         for ln in open(os.path.join(SRCDIR, 'benchmarks_%s/query2.%d.sql'% (f[1], uc))):
             c.stdin.write(ln.replace('alignments_i', 'alignments_%d'% f[0])
                             .replace('alignments_j', 'alignments_%d'% f[0]))
 
-
+    #write all aux data
+    c.stdin.write("SELECT * FROM bam.alignments_extra_%d;"% f[0]);
 
 out, err = c.communicate()
+
+# The output will contain explicit file_ids in the table names, making the table names
+# variable. We don't want a test to fail on this, so we have to remove te file
+# ids from the table names.
+def replace(matchobj):
+    return 'alignments_%si'% (matchobj.group(1) if matchobj.group(1) else '')
+p = re.compile('alignments_(extra_)?\d+')
+out = (re.subn(p, replace, out))[0]
+
 sys.stdout.write(out)
 sys.stderr.write(err)
