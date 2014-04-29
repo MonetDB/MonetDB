d024940f7b63f75ca4a000692b06a7352c1f1138
Martin Kersten <mk@cwi.nl>
2014-04-29 14:50:10 +0200

    Defensive code for getPC

--- monetdb5/scheduler/run_octopus.c
+++ monetdb5/scheduler/run_octopus.c
@@ -408,7 +408,7 @@ OCTOPUSdiscoverRegister(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci)
 
 	start= getPC(mb,pci);
 	if( start< 0)
-		throw(MAL,"run.octopus","illegal instruction");
+		throw(MAL,"run.octopus","Illegal instruction");
 	for (j = start + 1; j<mb->stop ; j++){
 		p= getInstrPtr(mb,j);
 		if ( p->barrier == EXITsymbol )
@@ -505,6 +505,8 @@ OCTOPUSbidding(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci)
     *res = 1;       /* execute the block */
 
     start = getPC(mb,pci);
+	if( start < 0)
+		throw(MAL,"octopus.bidding","Illegal instruction pointer");
     for (j = start + 1; j< mb->stop ; j++){
     	p = getInstrPtr(mb,j);
     	if ( p->barrier == EXITsymbol )
@@ -673,6 +675,8 @@ OCTOPUSrun(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci)
 
 	*res = 1;	/* execute the block */
 	start = getPC(mb,pci);
+	if( start < 0)
+		throw(MAL,"octopus.run","Illegal instruction pointer");
 	for (j = start + 1; j< mb->stop ; j++){
 		p = getInstrPtr(mb,j);
 		if ( p->barrier == EXITsymbol )
