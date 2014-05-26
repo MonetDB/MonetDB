1d70f5b7787d9f3314dab940fcc22b6636dc2df0
Sjoerd Mullender <sjoerd@acm.org>
2014-05-26 16:56:20 +0200

    Coverity fixes.

--- common/stream/stream.c
+++ common/stream/stream.c
@@ -752,8 +752,12 @@ open_stream(const char *filename, const char *flags)
 		if (file_read(s, buf, 1, UTF8BOMLENGTH) == UTF8BOMLENGTH &&
 		    strncmp(buf, UTF8BOM, UTF8BOMLENGTH) == 0)
 			s->isutf8 = 1;
-		else
-			file_fsetpos(s, pos);
+		else if (file_fsetpos(s, pos) < 0) {
+			/* unlikely: we couldn't seek the file back */
+			fclose(fp);
+			destroy(s);
+			return NULL;
+		}
 	}
 	return s;
 }
@@ -1038,6 +1042,11 @@ open_bzstream(const char *filename, const char *flags)
 		free(bzp);
 		return NULL;
 	}
+	s->read = stream_bzread;
+	s->write = stream_bzwrite;
+	s->close = stream_bzclose;
+	s->flush = NULL;
+	s->stream_data.p = (void *) bzp;
 	if (strchr(flags, 'r') != NULL) {
 		bzp->b = BZ2_bzReadOpen(&err, bzp->f, 0, 0, NULL, 0);
 		s->access = ST_READ;
@@ -1054,11 +1063,6 @@ open_bzstream(const char *filename, const char *flags)
 		destroy(s);
 		return NULL;
 	}
-	s->read = stream_bzread;
-	s->write = stream_bzwrite;
-	s->close = stream_bzclose;
-	s->flush = NULL;
-	s->stream_data.p = (void *) bzp;
 	return s;
 }
 
@@ -1666,8 +1670,8 @@ socket_update_timeout(stream *s)
 	tv.tv_sec = s->timeout;
 	tv.tv_usec = 0;
 	/* cast to char * for Windows, no harm on "normal" systems */
-	setsockopt(fd, SOL_SOCKET, SO_RCVTIMEO, (char *) &tv, (socklen_t) sizeof(tv));
-	setsockopt(fd, SOL_SOCKET, SO_SNDTIMEO, (char *) &tv, (socklen_t) sizeof(tv));
+	(void) setsockopt(fd, SOL_SOCKET, SO_RCVTIMEO, (char *) &tv, (socklen_t) sizeof(tv));
+	(void) setsockopt(fd, SOL_SOCKET, SO_SNDTIMEO, (char *) &tv, (socklen_t) sizeof(tv));
 }
 
 static stream *
@@ -1690,27 +1694,28 @@ socket_open(SOCKET sock, const char *name)
 #if defined(SO_DOMAIN)
 	{
 		socklen_t len = (socklen_t) sizeof(domain);
-		getsockopt(sock, SOL_SOCKET, SO_DOMAIN, (void *) &domain, &len);
+		if (getsockopt(sock, SOL_SOCKET, SO_DOMAIN, (void *) &domain, &len) < 0)
+			domain = AF_INET; /* give it a value if call fails */
 	}
 #endif
 #if defined(SO_KEEPALIVE) && !defined(WIN32)
 	if (domain != PF_UNIX) {	/* not on UNIX sockets */
 		int opt = 1;
-		setsockopt(sock, SOL_SOCKET, SO_KEEPALIVE, (void *) &opt, sizeof(opt));
+		(void) setsockopt(sock, SOL_SOCKET, SO_KEEPALIVE, (void *) &opt, sizeof(opt));
 	}
 #endif
 #if defined(IPTOS_THROUGHPUT) && !defined(WIN32)
 	if (domain != PF_UNIX) {	/* not on UNIX sockets */
 		int tos = IPTOS_THROUGHPUT;
 
-		setsockopt(sock, IPPROTO_IP, IP_TOS, (void *) &tos, sizeof(tos));
+		(void) setsockopt(sock, IPPROTO_IP, IP_TOS, (void *) &tos, sizeof(tos));
 	}
 #endif
 #ifdef TCP_NODELAY
 	if (domain != PF_UNIX) {	/* not on UNIX sockets */
 		int nodelay = 1;
 
-		setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, (void *) &nodelay, sizeof(nodelay));
+		(void) setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, (void *) &nodelay, sizeof(nodelay));
 	}
 #endif
 #ifdef HAVE_FCNTL
@@ -2073,8 +2078,6 @@ file_wastream(FILE *fp, const char *name)
 		return NULL;
 	s->access = ST_WRITE;
 	s->type = ST_ASCII;
-	if (fp == NULL)
-		s->errnr = MNSTR_OPEN_ERROR;
 	s->stream_data.p = (void *) fp;
 	return s;
 }
