6e76c034f2fdd99974200517e83bf9fa1f174e8a
Stefan Manegold <Stefan.Manegold@cwi.nl>
2014-05-27 23:39:11 +0200

    extended php_dec38 test

--- sql/test/mapi/Tests/php_dec38.php
+++ sql/test/mapi/Tests/php_dec38.php
@@ -6,10 +6,10 @@ $db = monetdb_connect("sql", "localhost", $argv[1], "monetdb", "monetdb", $argv[
 $res = monetdb_query('START TRANSACTION;') or die(monetdb_last_error());
 while ( $row = monetdb_fetch_assoc($res) ) { print_r($row); }
 
-$res = monetdb_query('CREATE TABLE php_dec38 (d0 DECIMAL(38,0), d19 DECIMAL(38,19));') or die(monetdb_last_error());
+$res = monetdb_query('CREATE TABLE php_dec38 (d38_0 DECIMAL(38,0), d38_19 DECIMAL(38,19), d38_38 DECIMAL(38,38));') or die(monetdb_last_error());
 while ( $row = monetdb_fetch_assoc($res) ) { print_r($row); }
 
-$res = monetdb_query('INSERT INTO php_dec38 VALUES (12345678901234567899876543210987654321, 1234567890123456789.9876543210987654321);') or die(monetdb_last_error());
+$res = monetdb_query('INSERT INTO php_dec38 VALUES (12345678901234567899876543210987654321, 1234567890123456789.9876543210987654321, .12345678901234567899876543210987654321);') or die(monetdb_last_error());
 while ( $row = monetdb_fetch_assoc($res) ) { print_r($row); }
 
 $res = monetdb_query('SELECT * FROM php_dec38;') or die(monetdb_last_error());
