4fcc01edb13af052219d019b4b806c55f49a5bb8
Stefan Manegold <Stefan.Manegold@cwi.nl>
2014-05-28 07:51:22 +0200

    added MAL & SQL tests for 128-bit integers (hge / HUGEINT);
    there is still some bug / hidden down-cast to lng (?) !

--- sql/test/mapi/Tests/php_int128.php
+++ sql/test/mapi/Tests/php_int128.php
@@ -9,7 +9,7 @@ while ( $row = monetdb_fetch_assoc($res) ) { print_r($row); }
 $res = monetdb_query('CREATE TABLE php_int128 (i HUGEINT);') or die(monetdb_last_error());
 while ( $row = monetdb_fetch_assoc($res) ) { print_r($row); }
 
-$res = monetdb_query('INSERT INTO php_int128 VALUES (123456789012345678909876543210987654321);') or die(monetdb_last_error());
+$res = monetdb_query('INSERT INTO php_int128 VALUES (123456789098765432101234567890987654321);') or die(monetdb_last_error());
 while ( $row = monetdb_fetch_assoc($res) ) { print_r($row); }
 
 $res = monetdb_query('SELECT * FROM php_int128;') or die(monetdb_last_error());
