21c53f7dbe45351c92ba4cf17d9e54cc1b85dda1
Robin Cijvat <robin.cijvat@monetdbsolutions.com>
2014-07-15 10:24:48 +0200

    Fix system absolute paths in test outputs

--- sql/backends/monet5/bam/Tests/check_files.sql
+++ sql/backends/monet5/bam/Tests/check_files.sql
@@ -1,3 +1,3 @@
-SELECT file_location, dbschema, format_version, sorting_order, comments
+SELECT dbschema, format_version, sorting_order, comments
 FROM bam.files
 ORDER BY file_location;
--- sql/backends/monet5/bam/Tests/check_files_exact.sql
+++ sql/backends/monet5/bam/Tests/check_files_exact.sql
@@ -1,2 +1,2 @@
-SELECT *
+SELECT file_id, dbschema, format_version, sorting_order, comments
 FROM bam.files;
