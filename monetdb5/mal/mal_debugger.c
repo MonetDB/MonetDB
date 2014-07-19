d6eafec97dc4300faaf93e1a4a33da975633b418
Martin Kersten <mk@cwi.nl>
2014-07-19 23:31:39 +0200

    Protect against negative variable ids

--- monetdb5/mal/mal_debugger.c
+++ monetdb5/mal/mal_debugger.c
@@ -352,9 +352,14 @@ mdbLocateMalBlk(Client cntxt, MalBlkPtr mb, str b, stream *out)
 	/* start with function in context */
 	if (*b == '[') {
 		idx = atoi(b + 1);
+		if( idx < 0)
+			return NULL;
 		return getMalBlkHistory(mb, idx);
 	} else if (isdigit((int) *b)) {
-		return getMalBlkHistory(mb, atoi(b));
+		idx = atoi(b);
+		if( idx < 0)
+			return NULL;
+		return getMalBlkHistory(mb, idx);
 	} else if (*b != 0) {
 		char *fcnname = strchr(b, '.');
 		Symbol fsym;
@@ -364,6 +369,8 @@ mdbLocateMalBlk(Client cntxt, MalBlkPtr mb, str b, stream *out)
 		if ((h = strchr(fcnname + 1, '['))) {
 			*h = 0;
 			idx = atoi(h + 1);
+			if( idx < 0)
+				return NULL;
 		}
 		fsym = findSymbolInModule(findModule(cntxt->nspace, putName(b, strlen(b))), fcnname + 1);
 		*fcnname = '.';
@@ -830,7 +837,7 @@ retryRead:
 					printBATelm(out, i, size, first);
 				} else {
 					i = atoi(b);
-					if (i || *b == '0')
+					if (i>-0 || *b == '0')
 						printStackElm(out, mb, stk->stk + i, i, size, first);
 					else
 						mnstr_printf(out, "%s Symbol not found\n", "#mdb ");
