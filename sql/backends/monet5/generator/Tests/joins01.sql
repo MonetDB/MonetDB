dd58544eb71c2f58ce49a852f4a25e46b84fa027
Martin Kersten <mk@cwi.nl>
2014-07-20 10:10:44 +0200

    Temporal join tests

--- sql/backends/monet5/generator/Tests/joins01.sql
+++ sql/backends/monet5/generator/Tests/joins01.sql
@@ -1,4 +1,32 @@
-select * from generate_series(0,3,1) as A, generate_series(0,6,2) as B;
+-- To be done tests.
+-- Using an 'int' rather then a 'tinyint' calls for casting the generated values first
+-- The two join cases illustrate how a join could be optimized by 'looking' up the correct value.
 
-select * from generate_series(0,3,1) as A, generate_series(0,6,2) as B
-where A.value < B.value;
+create table tmp(i timestamp);
+insert into tmp values
+(timestamp '2008-03-01 00:00'),
+(timestamp '2008-03-01 10:00'),
+(timestamp '2008-03-01 20:00');
+select * from tmp;
+
+select * from generate_series(timestamp '2008-03-01 00:00',timestamp '2008-03-04 12:00',cast( '10' as interval hour)) X, tmp Y where X.value = Y.i;
+select * from generate_series(timestamp '2008-03-01 00:00',timestamp '2008-03-04 12:00',cast( '10' as interval hour)) X, tmp Y where Y.i = X.value;
+
+select * from generate_series(timestamp '2008-03-01 00:00',timestamp '2008-03-04 12:00',cast( '10' as interval hour)) X, tmp Y where X.value = Y.i and value < timestamp '2008-03-01 20:00';
+select * from generate_series(timestamp '2008-03-01 00:00',timestamp '2008-03-04 12:00',cast( '10' as interval hour)) X, tmp Y where Y.i = X.value  and value < timestamp '2008-03-01 20:00';
+
+select * from generate_series(timestamp '2008-03-01 00:00',timestamp '2008-03-04 12:00',cast( '10' as interval hour)) as  X, tmp Y where X.value = Y.i and value < timestamp '2008-03-01 20:00' and value > timestamp '200-03-01 00:00';
+
+-- negative range
+select * from generate_series(timestamp '2008-03-04 18:00',timestamp '2008-03-01 00:00',cast( '-10' as interval hour)) X;
+select * from generate_series(timestamp '2008-03-04 18:00',timestamp '2008-03-01 00:00',cast( '-10' as interval hour)) X, tmp Y where X.value = Y.i;
+select * from generate_series(timestamp '2008-03-04 18:00',timestamp '2008-03-01 00:00',cast( '-10' as interval hour)) X, tmp Y where Y.i = X.value;
+
+select * from generate_series(timestamp '2008-03-04 18:00',timestamp '2008-03-01 00:00',cast( '-10' as interval hour)) X, tmp Y where X.value = Y.i
+and value > timestamp '2008-03-01 09:00';
+
+select * from generate_series(timestamp '2008-03-04 18:00',timestamp '2008-03-01 00:00',cast( '-10' as interval hour)) X, tmp Y where X.value = Y.i
+and value > timestamp '2008-03-01 09:00'
+and value < timestamp '2008-03-01 20:00';
+
+drop table tmp;
