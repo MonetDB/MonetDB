ae72b6902b8c3d8f75cbc31704f6dd93029cf41f
Martin Kersten <mk@cwi.nl>
2014-07-21 16:32:42 +0200

    Point selections for other types.

--- sql/backends/monet5/generator/Tests/generator00.sql
+++ sql/backends/monet5/generator/Tests/generator00.sql
@@ -2,6 +2,8 @@ select * from generate_series(0,10,2);
 
 select * from generate_series(0,10,2) where value <5;
 
+select * from generate_series(0,10,2) where value =4;
+
 select * from generate_series(0,10,2) as v where value <7 and value >3;
 
 select * from generate_series(0,10,2) as v where value <7 and value >3 and value <=6 and value >=4;
@@ -9,6 +11,8 @@ select * from generate_series(0,10,2) as v where value <7 and value >3 and value
 select * from generate_series(10,0,-2);
 
 select * from generate_series(10,0,-2) where value <5;
+
+select * from generate_series(10,0,-2) where value = 6;
 
 select * from generate_series(10,0,-2) as v where value <7 and value >3;
 
