e332b424eaf8b377adf660d74138422ae27ab65b
Sjoerd Mullender <sjoerd@acm.org>
2014-07-22 17:47:19 +0200

    Use spaces instead of tabs.

--- sql/backends/monet5/bam/85_bam.sql
+++ sql/backends/monet5/bam/85_bam.sql
@@ -34,69 +34,69 @@ EXTERNAL NAME bam.bam_export;
 
 
 
-CREATE TABLE bam.files ( 
-	file_id						BIGINT	  		NOT NULL, 
-	file_location				STRING	  		NOT NULL, 
-	dbschema					SMALLINT		NOT NULL, 
-	format_version				VARCHAR(7), 
-	sorting_order				VARCHAR(10), 
-	comments					STRING, 
-	CONSTRAINT files_pkey_file_id PRIMARY KEY (file_id) 
+CREATE TABLE bam.files (
+    file_id                     BIGINT          NOT NULL,
+    file_location               STRING          NOT NULL,
+    dbschema                    SMALLINT        NOT NULL,
+    format_version              VARCHAR(7),
+    sorting_order               VARCHAR(10),
+    comments                    STRING,
+    CONSTRAINT files_pkey_file_id PRIMARY KEY (file_id)
 );
 
-CREATE TABLE bam.sq ( 
-	sn							STRING	  		NOT NULL, 
-	file_id 					BIGINT	  		NOT NULL, 
-	ln							INT, 
-	"as"						INT, 
-	m5							STRING, 
-	sp							STRING, 
-	ur							STRING, 
-	CONSTRAINT sq_pkey_sn_file_id PRIMARY KEY (sn, file_id), 
-	CONSTRAINT sq_fkey_file_id FOREIGN KEY (file_id) REFERENCES bam.files (file_id) 
+CREATE TABLE bam.sq (
+    sn                          STRING          NOT NULL,
+    file_id                     BIGINT          NOT NULL,
+    ln                          INT,
+    "as"                        INT,
+    m5                          STRING,
+    sp                          STRING,
+    ur                          STRING,
+    CONSTRAINT sq_pkey_sn_file_id PRIMARY KEY (sn, file_id),
+    CONSTRAINT sq_fkey_file_id FOREIGN KEY (file_id) REFERENCES bam.files (file_id)
 );
 
-CREATE TABLE bam.rg ( 
-	id							STRING	  		NOT NULL, 
-	file_id						BIGINT	  		NOT NULL, 
-	cn							STRING, 
-	ds							STRING, 
-	dt							TIMESTAMP, 
-	fo							STRING, 
-	ks							STRING, 
-	lb							STRING, 
-	pg							STRING, 
-	pi							INT, 
-	pl							STRING, 
-	pu							STRING, 
-	sm							STRING, 
-	CONSTRAINT rg_pkey_id_file_id PRIMARY KEY (id, file_id), 
-	CONSTRAINT rg_fkey_file_id FOREIGN KEY (file_id) REFERENCES bam.files (file_id) 
+CREATE TABLE bam.rg (
+    id                          STRING          NOT NULL,
+    file_id                     BIGINT          NOT NULL,
+    cn                          STRING,
+    ds                          STRING,
+    dt                          TIMESTAMP,
+    fo                          STRING,
+    ks                          STRING,
+    lb                          STRING,
+    pg                          STRING,
+    pi                          INT,
+    pl                          STRING,
+    pu                          STRING,
+    sm                          STRING,
+    CONSTRAINT rg_pkey_id_file_id PRIMARY KEY (id, file_id),
+    CONSTRAINT rg_fkey_file_id FOREIGN KEY (file_id) REFERENCES bam.files (file_id)
 );
 
-CREATE TABLE bam.pg ( 
-	id							STRING	  		NOT NULL, 
-	file_id						BIGINT	  		NOT NULL, 
-	pn							STRING, 
-	cl							STRING, 
-	pp							STRING, 
-	vn							STRING, 
-	CONSTRAINT pg_pkey_id_file_id PRIMARY KEY (id, file_id), 
-	CONSTRAINT pg_fkey_file_id FOREIGN KEY (file_id) REFERENCES bam.files (file_id) 
+CREATE TABLE bam.pg (
+    id                          STRING          NOT NULL,
+    file_id                     BIGINT          NOT NULL,
+    pn                          STRING,
+    cl                          STRING,
+    pp                          STRING,
+    vn                          STRING,
+    CONSTRAINT pg_pkey_id_file_id PRIMARY KEY (id, file_id),
+    CONSTRAINT pg_fkey_file_id FOREIGN KEY (file_id) REFERENCES bam.files (file_id)
 );
 
-CREATE TABLE bam.export ( 
-	qname						STRING	  		NOT NULL, 
-	flag						SMALLINT		NOT NULL, 
-	rname						STRING	  		NOT NULL, 
-	pos						    INT		 		NOT NULL, 
-	mapq						SMALLINT		NOT NULL, 
-	cigar						STRING	  		NOT NULL, 
-	rnext						STRING	  		NOT NULL, 
-	pnext						INT		 		NOT NULL, 
-	tlen						INT		 		NOT NULL, 
-	seq						   	STRING	  		NOT NULL, 
-	qual						STRING	  		NOT NULL  
+CREATE TABLE bam.export (
+    qname                       STRING          NOT NULL,
+    flag                        SMALLINT        NOT NULL,
+    rname                       STRING          NOT NULL,
+    pos                         INT             NOT NULL,
+    mapq                        SMALLINT        NOT NULL,
+    cigar                       STRING          NOT NULL,
+    rnext                       STRING          NOT NULL,
+    pnext                       INT             NOT NULL,
+    tlen                        INT             NOT NULL,
+    seq                         STRING          NOT NULL,
+    qual                        STRING          NOT NULL
 );
 
 update sys._tables
