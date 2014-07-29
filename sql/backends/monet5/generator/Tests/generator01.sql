eb93e8abc8aa2e475b966430a89394ff806ba736
Martin Kersten <mk@cwi.nl>
2014-07-29 14:19:22 +0200

    More cornercases tested

--- sql/backends/monet5/generator/Tests/generator01.sql
+++ sql/backends/monet5/generator/Tests/generator01.sql
@@ -8,5 +8,11 @@ select * from generate_series(0,10,null) ;
 select * from generate_series(null,10,1) ;
 select * from generate_series(10,null,1) ;
 
+select * from generate_series(null,10) ;
+select * from generate_series(10,null) ;
+
+select * from generate_series(null,-1) ;
+select * from generate_series(-1,null) ;
+
 select * from generate_series(0,10,-2) as v
 where value <7 and value >3;
