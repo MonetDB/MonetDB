33cfaaa455c1166c68b15da5e40dc37c8785021a
Hannes Muehleisen <hannes@cwi.nl>
2014-08-20 17:13:29 +0200

    Test output wrangling

--- clients/R/Tests/install.sh
+++ clients/R/Tests/install.sh
@@ -2,18 +2,13 @@
 # install dependencies first
 # R --vanilla --quiet -e "install.packages(c('MonetDB.R','dplyr','sqlsurvey'),repos=c('http://r-forge.r-project.org','http://cran.rstudio.com/'),type='source')"
 
-echo "#~BeginVariableOutput~#"
->&2 echo "#~BeginVariableOutput~#"
-
 # install local version
-R CMD INSTALL $TSTSRCDIR/../MonetDB.R
+R CMD INSTALL $TSTSRCDIR/../MonetDB.R >> /dev/null 2>&1
 
 # try loading the required packages
-R --vanilla --quiet -e "library(MonetDB.R); library(dplyr); library(sqlsurvey)"
+R --vanilla --quiet -e "library(MonetDB.R); library(dplyr); library(sqlsurvey)" >> /dev/null 2>&1
 RRET=$?
 
-echo "#~EndVariableOutput~#"
->&2 echo "#~EndVariableOutput~#"
 
 if [ $RRET -eq 0 ];
 then
