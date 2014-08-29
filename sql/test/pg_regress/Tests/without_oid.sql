97e9d0e2dee205a0bf7186a23a36e32110844407
Martin van Dinther <martin.van.dinther@monetdbsolutions.com>
2014-08-29 17:34:11 +0200

    Made without_oid.sql a usable test. Updated table.* Added substitution rules to postgresql2sql99.sh

--- sql/test/pg_regress/Tests/without_oid.sql
+++ sql/test/pg_regress/Tests/without_oid.sql
@@ -8,49 +8,54 @@
 -- space, depending on the size of the tuple header + null bitmap.
 -- As of 8.0 we need a 9-bit null bitmap to force the difference to appear.
 --
-CREATE TABLE wi (i INT,
+CREATE TABLE wi (oid oid GENERATED ALWAYS AS IDENTITY, i INT,
                  n1 int, n2 int, n3 int, n4 int,
-                 n5 int, n6 int, n7 int, n8 int) WITH OIDS;
+                 n5 int, n6 int, n7 int, n8 int) /* WITH OIDS */;
 CREATE TABLE wo (i INT,
                  n1 int, n2 int, n3 int, n4 int,
-                 n5 int, n6 int, n7 int, n8 int) WITHOUT OIDS;
+                 n5 int, n6 int, n7 int, n8 int) /* WITHOUT OIDS */;
 
-INSERT INTO wi VALUES (1);  -- 1
-INSERT INTO wo SELECT i FROM wi;  -- 1
-INSERT INTO wo SELECT i+1 FROM wi;  -- 1+1=2
-INSERT INTO wi SELECT i+1 FROM wo;  -- 1+2=3
-INSERT INTO wi SELECT i+3 FROM wi;  -- 3+3=6
-INSERT INTO wo SELECT i+2 FROM wi;  -- 2+6=8
-INSERT INTO wo SELECT i+8 FROM wo;  -- 8+8=16
-INSERT INTO wi SELECT i+6 FROM wo;  -- 6+16=22
-INSERT INTO wi SELECT i+22 FROM wi;  -- 22+22=44
-INSERT INTO wo SELECT i+16 FROM wi;  -- 16+44=60
-INSERT INTO wo SELECT i+60 FROM wo;  -- 60+60=120
-INSERT INTO wi SELECT i+44 FROM wo;  -- 44+120=164
-INSERT INTO wi SELECT i+164 FROM wi;  -- 164+164=328
-INSERT INTO wo SELECT i+120 FROM wi;  -- 120+328=448
-INSERT INTO wo SELECT i+448 FROM wo;  -- 448+448=896
-INSERT INTO wi SELECT i+328 FROM wo;  -- 328+896=1224
-INSERT INTO wi SELECT i+1224 FROM wi;  -- 1224+1224=2448
-INSERT INTO wo SELECT i+896 FROM wi;  -- 896+2448=3344
-INSERT INTO wo SELECT i+3344 FROM wo;  -- 3344+3344=6688
-INSERT INTO wi SELECT i+2448 FROM wo;  -- 2448+6688=9136
-INSERT INTO wo SELECT i+6688 FROM wi WHERE i<=2448;  -- 6688+2448=9136
+INSERT INTO wi (i) VALUES (1);  -- 1
+INSERT INTO wo (i) SELECT i FROM wi;  -- 1
+INSERT INTO wo (i) SELECT i+1 FROM wi;  -- 1+1=2
+INSERT INTO wi (i) SELECT i+1 FROM wo;  -- 1+2=3
+INSERT INTO wi (i) SELECT i+3 FROM wi;  -- 3+3=6
+INSERT INTO wo (i) SELECT i+2 FROM wi;  -- 2+6=8
+INSERT INTO wo (i) SELECT i+8 FROM wo;  -- 8+8=16
+INSERT INTO wi (i) SELECT i+6 FROM wo;  -- 6+16=22
+INSERT INTO wi (i) SELECT i+22 FROM wi;  -- 22+22=44
+INSERT INTO wo (i) SELECT i+16 FROM wi;  -- 16+44=60
+INSERT INTO wo (i) SELECT i+60 FROM wo;  -- 60+60=120
+INSERT INTO wi (i) SELECT i+44 FROM wo;  -- 44+120=164
+INSERT INTO wi (i) SELECT i+164 FROM wi;  -- 164+164=328
+INSERT INTO wo (i) SELECT i+120 FROM wi;  -- 120+328=448
+INSERT INTO wo (i) SELECT i+448 FROM wo;  -- 448+448=896
+INSERT INTO wi (i) SELECT i+328 FROM wo;  -- 328+896=1224
+INSERT INTO wi (i) SELECT i+1224 FROM wi;  -- 1224+1224=2448
+INSERT INTO wo (i) SELECT i+896 FROM wi;  -- 896+2448=3344
+INSERT INTO wo (i) SELECT i+3344 FROM wo;  -- 3344+3344=6688
+INSERT INTO wi (i) SELECT i+2448 FROM wo;  -- 2448+6688=9136
+INSERT INTO wo (i) SELECT i+6688 FROM wi WHERE i<=2448;  -- 6688+2448=9136
 
 SELECT count(oid) FROM wi;
--- should fail
-SELECT count(oid) FROM wo;
+SELECT count(oid) FROM wo;-- should fail
+SELECT count(i) FROM wo;
 
--- Replaced PostgreSQL "VACUUM ANALYZE my_table;" with MonetDB "call vacuum('sys', 'my_table');"
-call vacuum('sys', 'wi');
-call vacuum('sys', 'wo');
+-- Replaced PostgreSQL "VACUUM ANALYZE my_table;" with MonetDB "call vacuum('sys', 'my_table');" and "call analyze('sys', 'my_table');"
+call vacuum('sys', 'wi'); call analyze('sys', 'wi');
+call vacuum('sys', 'wo'); call analyze('sys', 'wo');
 
+/*
 SELECT min(relpages) < max(relpages), min(reltuples) - max(reltuples)
   FROM pg_class
  WHERE relname IN ('wi', 'wo');
+*/
+SELECT * FROM sys.storage
+ WHERE "table" IN ('wi', 'wo');
 
 DROP TABLE wi;
 DROP TABLE wo;
+
 
 --
 -- WITH / WITHOUT OIDS in CREATE TABLE AS
@@ -60,20 +65,18 @@ CREATE TABLE create_table_test (
     b int
 );
 
-COPY create_table_test FROM stdin;
-5	10
-10	15
-\.
+INSERT INTO create_table_test VALUES (5, 10);
+INSERT INTO create_table_test VALUES (10, 15);
 
-CREATE TABLE create_table_test2 WITH OIDS AS
-    SELECT a + b AS c1, a - b AS c2 FROM create_table_test;
+CREATE TABLE create_table_test2 /* WITH OIDS */ AS
+    SELECT row_number() over () AS oid, a + b AS c1, a - b AS c2 FROM create_table_test WITH DATA;
 
-CREATE TABLE create_table_test3 WITHOUT OIDS AS
-    SELECT a + b AS c1, a - b AS c2 FROM create_table_test;
+CREATE TABLE create_table_test3 /* WITHOUT OIDS */ AS
+    SELECT a + b AS c1, a - b AS c2 FROM create_table_test WITH DATA;
 
 SELECT count(oid) FROM create_table_test2;
--- should fail
-SELECT count(oid) FROM create_table_test3;
+SELECT count(oid) FROM create_table_test3; -- should fail
+SELECT count(*) FROM create_table_test3;
 
 DROP TABLE create_table_test;
 DROP TABLE create_table_test2;
--- sql/test/pg_regress/postgresql2sql99.sh
+++ sql/test/pg_regress/postgresql2sql99.sh
@@ -101,7 +101,7 @@ sed -r \
 	-e 's/^COMMENT.*;$//Ig' \
 	-e 's/\) (INHERITS.*);/\); -- \1/Ig' \
 	-e 's/SELECT * INTO TABLE * FROM *;/\CREATE TABLE \2 AS SELECT \1 FROM \3 WITH DATA;/Ig' \
-	-e 's/VACUUM ANALYZE *;/call vacuum('sys', '\1');/Ig' \
+	-e 's/VACUUM ANALYZE *;/call vacuum('sys', '\1'); call analyze('sys', '\1');/Ig' \
 	-e 's/VACUUM FULL *;/call vacuum('sys', '\1');/Ig' \
 	-e 's/VACUUM *;/call vacuum('sys', '\1');/Ig' \
 	-e 's/alter table * alter column * set storage external;/\/* alter table \1 alter column \2 set storage external; *\//Ig' \
@@ -109,6 +109,8 @@ sed -r \
 	-e 's/SET geqo TO *;/\/* SET geqo TO \1; *\//Ig' \
 	-e 's/RESET datestyle;/\/* RESET datestyle; *\//Ig' \
 	-e 's/RESET geqo;/\/* RESET geqo; *\//Ig' \
+	-e 's/ WITH OIDS;/\/* WITH OIDS; *\//Ig' \
+	-e 's/ WITHOUT OIDS;/\/* WITHOUT OIDS; *\//Ig' \
 	-e 's/\s+([^\s]+)::float[248]\b/ cast(\1 as double)/Ig' \
 	-e 's/\s+([^\s]+)::int2\b/ cast(\1 as smallint)/Ig' \
 	-e 's/\s+([^\s]+)::int4\b/ cast(\1 as integer)/Ig' \
