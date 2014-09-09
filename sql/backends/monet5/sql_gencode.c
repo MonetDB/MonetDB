2f033bf925e4e561e9b716658675f91a8bc1ca9b
Niels Nes <niels@cwi.nl>
2014-09-09 08:24:31 +0200

    split update bat within the catalog interface to remove dependency on BATselect.

--- sql/backends/monet5/sql_gencode.c
+++ sql/backends/monet5/sql_gencode.c
@@ -897,7 +897,7 @@ _dumpstmt(backend *sql, MalBlkPtr mb, stmt *s)
 			q = newStmt2(mb, sqlRef, bindRef);
 			if (q == NULL)
 				return -1;
-			if (s->flag == RD_UPD) {
+			if (s->flag == RD_UPD_ID) {
 				q = pushReturn(mb, q, newTmpVariable(mb, newBatType(ht, tt)));
 			} else
 				setVarType(mb, getArg(q, 0), newBatType(ht, tt));
@@ -910,7 +910,7 @@ _dumpstmt(backend *sql, MalBlkPtr mb, stmt *s)
 				return -1;
 			s->nr = getDestVar(q);
 
-			if (s->flag == RD_UPD) {
+			if (s->flag == RD_UPD_ID) {
 				/* rename second result */
 				renameVariable(mb, getArg(q, 1), "r1_%d", s->nr);
 			}
@@ -924,7 +924,7 @@ _dumpstmt(backend *sql, MalBlkPtr mb, stmt *s)
 			q = newStmt2(mb, sqlRef, bindidxRef);
 			if (q == NULL)
 				return -1;
-			if (s->flag == RD_UPD) {
+			if (s->flag == RD_UPD_ID) {
 				q = pushReturn(mb, q, newTmpVariable(mb, newBatType(ht, tt)));
 			} else
 				setVarType(mb, getArg(q, 0), newBatType(ht, tt));
@@ -937,7 +937,7 @@ _dumpstmt(backend *sql, MalBlkPtr mb, stmt *s)
 				return -1;
 			s->nr = getDestVar(q);
 
-			if (s->flag == RD_UPD) {
+			if (s->flag == RD_UPD_ID) {
 				/* rename second result */
 				renameVariable(mb, getArg(q, 1), "r1_%d", s->nr);
 			}
