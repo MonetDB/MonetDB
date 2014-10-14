a8c1b5c503a24fa04322ae7653b938ff3f4f9efd
Martin Kersten <mk@cwi.nl>
2014-10-14 16:24:25 +0200

    Addition of the first merge table tests. 2 tests causes the server to crash

---
+++ sql/test/mergetables/Tests/crash0.sql
@@ -0,0 +1,25 @@
+START TRANSACTION;
+CREATE TABLE part1 ( x double, y double, z double);
+COPY 4 RECORDS INTO part1 FROM stdin USING DELIMITERS ' ','\n';
+0.0 0.0 0.0
+1.0 0.0 0.0 
+0.0 1.0 0.0 
+1.0 1.0 0.0 
+
+CREATE TABLE part2 ( x double, y double, z double);
+COPY 4 RECORDS INTO part2 FROM stdin USING DELIMITERS ' ','\n';
+2.0 0.0 0.0
+3.0 0.0 0.0 
+2.0 1.0 0.0 
+3.0 1.0 0.0 
+
+CREATE MERGE TABLE complete ( x double, y double, z double);
+
+ALTER TABLE complete ADD TABLE part1;
+ALTER TABLE complete ADD TABLE part2;
+ALTER TABLE complete SET READ ONLY;
+
+DROP TABLE complete;
+DROP TABLE part1;
+DROP TABLE part2;
+ABORT;
