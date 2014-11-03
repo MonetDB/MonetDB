74f4cd9618aae88681dc5add8ee5445c12ee2ea2
Sjoerd Mullender <sjoerd@acm.org>
2014-11-03 17:26:43 +0100

    Resource leak.

--- tools/merovingian/daemon/proxy.c
+++ tools/merovingian/daemon/proxy.c
@@ -151,6 +151,7 @@ startProxy(int psock, stream *cfdin, stream *cfout, char *url, char *client)
 		strncpy(server.sun_path, conn, sizeof(server.sun_path) - 1);
 		free(conn);
 		if (connect(ssock, (SOCKPTR) &server, sizeof(struct sockaddr_un)) == -1) {
+			closesocket(ssock);
 			return(newErr("cannot connect: %s", strerror(errno)));
 		}
 
@@ -180,17 +181,17 @@ startProxy(int psock, stream *cfdin, stream *cfout, char *url, char *client)
 		Mfprintf(stdout, "target connection is on local UNIX domain socket, "
 				"passing on filedescriptor instead of proxying\n");
 		if (sendmsg(ssock, &msg, 0) < 0) {
-			close(ssock);
+			closesocket(ssock);
 			return(newErr("could not send initial byte: %s", strerror(errno)));
 		}
 		/* block until the server acknowledges that it has psock
 		 * connected with itself */
 		if (recv(ssock, buf, 1, 0) == -1) {
-			close(ssock);
+			closesocket(ssock);
 			return(newErr("could not receive initial byte: %s", strerror(errno)));
 		}
-		close(ssock);
-		close(psock);
+		closesocket(ssock);
+		closesocket(psock);
 		close_stream(cfdin);
 		close_stream(cfout);
 		return(NO_ERR);
