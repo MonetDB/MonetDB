1c51cf213ca3446e54e8160d0601aaf785e563b6
Foteini Alvanaki <foteini.alvanaki@monetdbsolutions.com>
2015-01-21 15:23:22 +0100

    new geometries loaded in geneeral table New outputs of tests approved

--- geom/sql/Tests/functions/Tests/ST_IsSimple.sql
+++ geom/sql/Tests/functions/Tests/ST_IsSimple.sql
@@ -17,4 +17,4 @@ insert into geo values (st_mpointfromtext('multipoint(10 10, 20 20, 10 5)', 4326
 select st_isvalid(g) from geo;
 drop table geo;
 
-SELECT geom AS "GEOMETRY" FROM geometries WHERE id<10 AND ST_IsSimple(geom);
+SELECT geom AS "GEOMETRY" FROM geometries WHERE id<11 AND ST_IsSimple(geom);
--- geom/sql/Tests/functions/Tests/loadTestGeometries.sql
+++ geom/sql/Tests/functions/Tests/loadTestGeometries.sql
@@ -1,25 +1,28 @@
 CREATE TABLE geometries(id serial, geom geometry);
 
---simple geometries
---1 closed
+--1 closed, simple => ring
 INSERT INTO geometries(geom) VALUES(ST_WKTToSQL('POINT(10 20)'));
---2 
+--2 simple
 INSERT INTO geometries(geom) VALUES(ST_WKTToSQL('LINESTRING(10 20, 30 40, 50 60)'));
---3 closed
+--3 closed, simple, ring
+INSERT INTO geometries(geom) VALUES(ST_WKTToSQL('LINESTRING(10 20, 30 40, 50 60, 80 60, 60 40, 10 20)'));
+--4
+INSERT INTO geometries(geom) VALUES(ST_WKTToSQL('LINESTRING(10 10, 50 50, 30 80, 25 5)'));
+--5 closed, simple
 INSERT INTO geometries(geom) VALUES(ST_WKTToSQL('POLYGON((10 10, 10 20, 20 20, 20 10, 10 10))'));
---4 closed
+--6 closed, simple
 INSERT INTO geometries(geom) VALUES(ST_WKTToSQL('MULTIPOINT(10 20, 30 40)'));
---5
+--7 simple
 INSERT INTO geometries(geom) VALUES(ST_WKTToSQL('MULTILINESTRING((30 40, 40 50), (50 60, 60 70))'));
---6
-INSERT INTO geometries(geom) VALUES(ST_WKTToSQL('MULTILINESTRING((30 40, 40 50, 30 40), (50 60, 60 70))'));
---7 closed
-INSERT INTO geometries(geom) VALUES(ST_WKTToSQL('MULTILINESTRING((30 40, 40 50, 30 40), (50 60, 40 50, 20 30, 50 60))'));
 --8
+INSERT INTO geometries(geom) VALUES(ST_WKTToSQL('MULTILINESTRING((30 40, 40 50, 30 40), (50 60, 60 70))'));
+--9 closed
+INSERT INTO geometries(geom) VALUES(ST_WKTToSQL('MULTILINESTRING((30 40, 40 50, 30 40), (50 60, 40 50, 20 30, 50 60))'));
+--10 simple
 INSERT INTO geometries(geom) VALUES(ST_WKTToSQL('MULTIPOLYGON(((10 10, 10 20, 20 20, 20 10, 10 10),(30 30, 30 40, 40 40, 40 30, 30 30)))'));
---9
+--11 simple
 INSERT INTO geometries(geom) VALUES(ST_WKTToSQL('MULTIPOLYGON EMPTY'));
---10
+--12
 INSERT INTO geometries(geom) VALUES(ST_WKTToSQL('GEOMETRYCOLLECTION(POINT(10 20),LINESTRING(10 20, 30 40),POLYGON((10 10, 10 20, 20 20, 20 10, 10 10)))'));
---11 closed
+--13 closed
 INSERT INTO geometries(geom) VALUES(ST_WKTToSQL('GEOMETRYCOLLECTION(POINT(10 20),LINESTRING(10 20, 30 40, 10 20),POLYGON((10 10, 10 20, 20 20, 20 10, 10 10)))'));
