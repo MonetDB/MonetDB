796538e20dcafe1096d97426e3c9444ec10ee05d
Foteini Alvanaki <foteini.alvanaki@monetdbsolutions.com>
2015-01-21 12:59:03 +0100

    mtests: more geometries loaded at the beginning, ST_Boundary + ST_Contains new results approved

--- geom/sql/Tests/functions/Tests/ST_Boundary.sql
+++ geom/sql/Tests/functions/Tests/ST_Boundary.sql
@@ -6,6 +6,6 @@ SELECT ST_Boundary(ST_GeomFromText('POLYGON((1 1 1,0 0 1, -1 1 1, 1 1 1))'));
 
 SELECT ST_Boundary(ST_GeomFromText('MULTILINESTRING((1 1 1,0 0 0.5, -1 1 1),(1 1 0.5,0 0 0.5, -1 1 0.5, 1 1 0.5) )'));
 
-SELECT geom AS "GEOMETRY", ST_Boundary(geom) AS "BOUNDARY" FROM geometries WHERE id<7;
+SELECT geom AS "GEOMETRY", ST_Boundary(geom) AS "BOUNDARY" FROM geometries WHERE id<9;
 
 
