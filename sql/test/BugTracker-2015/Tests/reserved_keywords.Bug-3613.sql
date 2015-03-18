01cc9894e1b7fd14ee3cadf5e95f0139d2bd9d99
Sjoerd Mullender <sjoerd@acm.org>
2015-03-18 11:09:02 +0100

    Make test independent of contents of system tables.

--- sql/test/BugTracker-2015/Tests/reserved_keywords.Bug-3613.sql
+++ sql/test/BugTracker-2015/Tests/reserved_keywords.Bug-3613.sql
@@ -1,44 +1,24 @@
-SELECT tbl.action FROM sys.keys tbl;
-SELECT tbl.action FROM tmp.keys tbl;
+create table keywords (
+	"action" integer,
+	"default" integer,
+	"schema" integer,
+	"start" integer,
+	"statement" integer,
+	"user" integer
+);
+insert into keywords values (1, 2, 3, 4, 5, 6);
 
-SELECT tbl.default FROM sys._columns tbl;
-SELECT tbl.default FROM sys.columns tbl;
-SELECT tbl.default FROM tmp._columns tbl;
+SELECT tbl.action FROM sys.keywords tbl;
+SELECT tbl.default FROM sys.keywords tbl;
+SELECT tbl.schema FROM sys.keywords tbl;
+SELECT tbl.start FROM sys.keywords tbl;
+SELECT tbl.statement FROM sys.keywords tbl;
+SELECT tbl.user FROM sys.keywords tbl;
 
-SELECT tbl.schema FROM sys.statistics tbl;
-SELECT tbl.schema FROM sys.storage tbl;
-SELECT tbl.schema FROM sys.storagemodel tbl;
-SELECT tbl.schema FROM sys.storagemodelinput tbl;
-SELECT tbl.schema FROM sys.tablestoragemodel tbl;
+SELECT action FROM sys.keywords;
+SELECT default FROM sys.keywords;
+SELECT schema FROM sys.keywords;
+SELECT start FROM sys.keywords;
+SELECT statement FROM sys.keywords;
 
-SELECT tbl.start FROM sys.querylog_calls tbl;
-SELECT tbl.start FROM sys.querylog_history tbl;
-SELECT tbl.start FROM sys.sequences tbl;
-
-SELECT tbl.statement FROM sys.triggers tbl;
-SELECT tbl.statement FROM tmp.triggers tbl;
-
-SELECT tbl.user FROM sys.connections tbl;
-SELECT tbl.user FROM sys.queue tbl;
-SELECT tbl.user FROM sys.sessions tbl;
-SELECT tbl.user FROM sys.tracelog tbl;
-
-SELECT action FROM sys.keys;
-SELECT action FROM tmp.keys;
-
-SELECT default FROM sys._columns;
-SELECT default FROM sys.columns;
-SELECT default FROM tmp._columns;
-
-SELECT schema FROM sys.statistics;
-SELECT schema FROM sys.storage;
-SELECT schema FROM sys.storagemodel;
-SELECT schema FROM sys.storagemodelinput;
-SELECT schema FROM sys.tablestoragemodel;
-
-SELECT start FROM sys.querylog_calls;
-SELECT start FROM sys.querylog_history;
-SELECT start FROM sys.sequences;
-
-SELECT statement FROM sys.triggers;
-SELECT statement FROM tmp.triggers;
+drop table keywords;
