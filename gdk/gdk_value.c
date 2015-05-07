2ec8ede9d89d1103f59872426774fb483ac099d1
Sjoerd Mullender <sjoerd@acm.org>
2015-05-07 10:47:45 +0200

    Fixes to make sure actual results of calls match expected results.

--- gdk/gdk_value.c
+++ gdk/gdk_value.c
@@ -175,7 +175,7 @@ VALinit(ValPtr d, int tpe, const void *s)
 		GDKerror("VALinit:unsupported init\n");
 		d->vtype = TYPE_int;
 	} else if (tpe >= TYPE_str && ATOMstorage(tpe) == TYPE_str) {
-		d->vtype = TYPE_str;
+		d->vtype = tpe;
 		d->val.sval = GDKstrdup(s);
 		d->len = strLen(s);
 	} else {
