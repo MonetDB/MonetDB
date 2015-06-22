469a0fe00c3608b81fe17633c8afbc1437be36ee
Martin Kersten <mk@cwi.nl>
2015-06-22 23:12:52 +0200

    Add missing optimizer pipe-line requirements For user defined pipelines a minimal set is required to ensure proper SQL functionality.

--- sql/test/BugTracker-2009/Tests/table-leftovers.SF-2779462.sql
+++ sql/test/BugTracker-2009/Tests/table-leftovers.SF-2779462.sql
@@ -1,6 +1,6 @@
 start transaction;
 
-set optimizer='optimizer.inline();optimizer.remap();optimizer.evaluate();optimizer.costModel();optimizer.coercions();optimizer.aliases();optimizer.mergetable();optimizer.deadcode();optimizer.constants();optimizer.commonTerms();optimizer.reorder();optimizer.joinPath();optimizer.deadcode();optimizer.recycler();optimizer.reduce();optimizer.dataflow();optimizer.querylog();optimizer.multiplex();optimizer.garbageCollector();';
+set optimizer='optimizer.inline();optimizer.remap();optimizer.evaluate();optimizer.costModel();optimizer.coercions();optimizer.aliases();optimizer.mergetable();optimizer.deadcode();optimizer.constants();optimizer.commonTerms();optimizer.reorder();optimizer.joinPath();optimizer.deadcode();optimizer.recycler();optimizer.reduce();optimizer.dataflow();optimizer.querylog();optimizer.multiplex();optimizer.generator();optimizer.garbageCollector();';
 
 CREATE TABLE y (x int);
 INSERT INTO y VALUES (10);
