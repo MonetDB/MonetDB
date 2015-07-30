dc2a8ea06371e77b1e961c09f7b4a683766139f0
Sjoerd Mullender <sjoerd@acm.org>
2015-07-30 13:53:41 +0200

    Move simple_prompt function to mutils library.

---
+++ common/utils/mprompt.h
@@ -0,0 +1,20 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0.  If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright 2008-2015 MonetDB B.V.
+ */
+
+#ifdef NATIVE_WIN32
+#if !defined(LIBMUTILS) && !defined(LIBGDK) && !defined(LIBMEROUTIL)
+#define mutils_export extern __declspec(dllimport)
+#else
+#define mutils_export extern __declspec(dllexport)
+#endif
+#else
+#define mutils_export extern
+#endif
+
+mutils_export char *prompt_getlogin(void);
+mutils_export char *simple_prompt(const char *prompt, int maxlen, int echo, const char *def);
