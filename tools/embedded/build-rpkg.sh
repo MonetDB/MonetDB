e0def44577f6c3b246faa68be4ce0db1de878a05
Hannes Muehleisen <hannes@cwi.nl>
2015-07-31 11:36:45 +0200

    Package dieting

--- tools/embedded/build-rpkg.sh
+++ tools/embedded/build-rpkg.sh
@@ -4,22 +4,17 @@ set -x
 STAGEDIR=/tmp/monetdb-embedded-stage
 RPKG=MonetDB_1.0.0.tar.gz
 
-rm -rf $TARBALL
 rm -rf $STAGEDIR
 
 hg archive $STAGEDIR/sourcetree
-
 cd $STAGEDIR/sourcetree
-
 ./bootstrap
-
-#tar --exclude-from=tools/embedded/tar-excludes -zcvf $TARBALL .
 
 cd ..
 mv sourcetree/tools/reverserapi .
 rsync -av --exclude-from sourcetree/tools/embedded/pkg-excludes sourcetree/ reverserapi/src
-
 R CMD build reverserapi
+
 scp $RPKG lyon.ins.cwi.nl:/cwi/www/homepages/hannes/WWW/R
 
 # install.packages("MonetDB", repos="http://homepages.cwi.nl/~hannes/R/", type="source")
