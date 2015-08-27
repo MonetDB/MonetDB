97b657b743eb85cce3263bc04203c0acac17db9f
Martin Kersten <mk@cwi.nl>
2015-08-27 22:34:47 +0200

    Fixing errors in Best Effort loading - properly handle the case of less/more fields in a line The line is added to the reject list - properly construct the rejects message - continue parsing lines in best-effort mode

--- sql/test/copy/Tests/incorrect_columns.sql
+++ sql/test/copy/Tests/incorrect_columns.sql
@@ -1,12 +1,14 @@
 -- test the situation where the number of column values are incorrect
 create table t_columns(i int, t string);
 
-copy 5 records into t_columns from stdin delimiters ',','\n' best effort;
+copy 7 records into t_columns from stdin delimiters ',','\n' best effort;
 1,hello
 2
 no tag
-3,too much, data
+3,too much,xyz
 4,world
+5,wereld
+6,maan
 
 select * from t_columns;
 
