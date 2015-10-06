2f930c248d56fafc7d34b533858ae547ef4aa171
Martin Kersten <mk@cwi.nl>
2015-10-06 10:21:43 +0200

    test safe/restore

--- sql/test/mosaic/Tests/compressionRLE.sql
+++ sql/test/mosaic/Tests/compressionRLE.sql
@@ -104,17 +104,3 @@ explain select f from tmp3RLE;
 explain select t from tmp3RLE;
 select * from tmp3RLE;
 
-select * from storage where "table" = 'tmp3rle';
-select count(*) from tmp3RLE;
-
-alter table tmp3RLE alter column i set storage NULL;
-alter table tmp3RLE alter column b set storage NULL;
-alter table tmp3RLE alter column f set storage NULL;
-alter table tmp3RLE alter column t set storage NULL;
-explain select * from tmp3RLE;
-select * from tmp3RLE;
-
-alter table tmp3RLE set read write;
---select * from tmp3RLE;
-
-drop table tmp3RLE;
