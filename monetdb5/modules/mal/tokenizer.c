6d2c21fcec390674452157db2780c7d7d77a871c
Sjoerd Mullender <sjoerd@acm.org>
2015-10-06 15:25:03 +0200

    Get rid of unnecessary call to BATkey.

--- monetdb5/modules/mal/tokenizer.c
+++ monetdb5/modules/mal/tokenizer.c
@@ -261,7 +261,6 @@ TKNZRappend(oid *pos, str *s)
 				GDKfree(url);
 				throw(MAL, "tokenizer.append", MAL_MALLOC_FAIL);
 			}
-			BATkey(bVal, FALSE);
 			BATseqbase(bVal, 0);
 			
 			tokenBAT[i].val = bVal;
@@ -288,7 +287,6 @@ TKNZRappend(oid *pos, str *s)
 				GDKfree(url);
 				throw(MAL, "tokenizer.append", MAL_MALLOC_FAIL);
 			}
-			BATkey(bIdx, FALSE);
 			BATseqbase(bIdx, 0);
 			
 			tokenBAT[i].idx = bIdx;
