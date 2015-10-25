6b78f10c3a509095cdf4080b002b40619d85ccd1
Niels Nes <niels@cwi.nl>
2015-10-25 09:59:41 +0100

    revert script changes

--- sql/test/BugTracker-2015/Tests/schema_view2.Bug-3766.sql
+++ sql/test/BugTracker-2015/Tests/schema_view2.Bug-3766.sql
@@ -1,9 +1,9 @@
 create table t1 (i int);
 create schema tst;
-create view tst.v1 (i) as select * from sys.t1;
+create view tst.v1 (i) as select * from t1;
 select * from tst.v1;
 select * from _tables where name like '%v1%';
-create view tst.v1 (i) as select * from sys.t1;
+create view tst.v1 (i) as select * from t1;
 drop view tst.v1;
 
 drop schema tst;
