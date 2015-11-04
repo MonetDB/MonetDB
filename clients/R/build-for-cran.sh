6312353d6dc9da9463539d2283189f57553f00ed
Hannes Muehleisen <hannes@cwi.nl>
2015-11-04 14:45:25 +0100

    R connector: mclient function

--- clients/R/build-for-cran.sh
+++ clients/R/build-for-cran.sh
@@ -2,8 +2,8 @@
 
 # set this to a path where the latest svn version or R was installed. 
 # CRAN policies state that R CMD check must use the latest version
-#R=~/r-trunk-install/R.framework/Resources/bin/R
-R=R
+R=~/r-trunk-install/bin/R
+#R=R
 # can't have these in a R package, it wont build
 find MonetDB.R -name "Makefile*" -exec rm {} \;
 find MonetDB.R -name ".DS_Store" -exec rm {} \;
@@ -26,7 +26,7 @@ $R CMD build MonetDB.R
 $R CMD check -o /tmp/rcheck --as-cran MonetDB.R_*.tar.gz
 
 #scp MonetDB.R_*.tar.gz cwi:WWW/R/
-scp MonetDB.R_*.tar.gz release@dev.monetdb.org:/var/www/html/Assets/R/
+#scp MonetDB.R_*.tar.gz release@dev.monetdb.org:/var/www/html/Assets/R/
 
 #hgid=`hg id -i | sed s/+//`
 #newname=`basename MonetDB.R_*.tar.gz .tar.gz`-$hgid.tar.gz
