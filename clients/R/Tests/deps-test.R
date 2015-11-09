cd03b82804a4d652c6b4c327f4785c1738c28a4e
Sjoerd Mullender <sjoerd@acm.org>
2015-11-09 17:18:46 +0100

    On Windows, embedded R doesn't work, and we don't want to have to build dependencies from source.

--- clients/R/Tests/deps-test.R
+++ clients/R/Tests/deps-test.R
@@ -3,8 +3,8 @@ dd <- capture.output(suppressMessages(suppressWarnings({
 	(function(lp) {
 	np <- lp[!(lp %in% installed.packages()[,"Package"])]
 	repos <- c('http://download.r-forge.r-project.org','http://cran.rstudio.com/')
-	if(length(np)) install.packages(np,repos=repos, type="source", quiet=T)
-	update.packages(repos=repos, ask=F, oldPkgs=lp, type="source", quiet=T)
+	if(length(np)) install.packages(np,repos=repos, quiet=T)
+	update.packages(repos=repos, ask=F, oldPkgs=lp, quiet=T)
 	x <- lapply(lp,function(x){library(x,character.only=TRUE,quietly=T)}) 
 	})(c('Rcpp', 'dplyr','survey','sqlsurvey','Lahman','nycflights13'))
 })))
