1b8cf4bd041aa6ea9ae0eb3c7304d9a65b7afe28
Hannes Muehleisen <hannes@cwi.nl>
2015-12-16 13:52:52 +0100

    version bump

--- tools/embedded/build-on-windows.bat
+++ tools/embedded/build-on-windows.bat
@@ -1,4 +1,4 @@
 "c:\Program Files (x86)\GnuWin32\bin\wget.exe" http://dev.monetdb.org/Assets/R/MonetDBLite_0.1.0.tar.gz
-"c:\Program Files\R\R-3.2.2\bin\R" CMD INSTALL --build MonetDBLite_0.1.0.tar.gz 
+"c:\Program Files\R\R-3.2.3\bin\R" CMD INSTALL --build MonetDBLite_0.1.0.tar.gz 
 rm MonetDBLite_0.1.0.tar.gz 
 pause
