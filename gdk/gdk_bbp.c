df7effea31afe04fdbed1def0ab151f807b0a5ed
Niels Nes <niels@cwi.nl>
2015-12-27 18:31:36 +0100

    revert change (was old code)

--- gdk/gdk_bbp.c
+++ gdk/gdk_bbp.c
@@ -1230,7 +1230,6 @@ heap_entry(FILE *fp, COLrec *col)
 			   ((unsigned short) col->dense << 9) |
 			   ((unsigned short) col->nonil << 10) |
 			   ((unsigned short) col->nil << 11),
-			   ((unsigned short) col->heap.compressed << 12),
 		       col->nokey[0],
 		       col->nokey[1],
 		       col->nosorted,
