24bf229ffbd522c441188547b9572b350fe0cddf
Sjoerd Mullender <sjoerd@acm.org>
2016-01-04 14:00:36 +0100

    Updated copyright year.

--- clients/ruby/lib/test/test_capabilities.rb
+++ clients/ruby/lib/test/test_capabilities.rb
@@ -2,7 +2,7 @@
 # License, v. 2.0.  If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-# Copyright 2008-2015 MonetDB B.V.
+# Copyright 1997 - July 2008 CWI, August 2008 - 2016 MonetDB B.V.
 
 # unit test suite for monetdb.
 # connects to the 'ruby_test' database and runs test on the server capabilities and SQL language.
