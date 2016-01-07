a3664f19f549d59d09ed0685844daace3eb91f2b
Martin van Dinther <martin.van.dinther@monetdbsolutions.com>
2016-01-07 18:21:00 +0100

    Added ON COMMIT PRESERVE ROWS when creating a temporary table else no dat is remembered. Occasionaly the construction 1.0/rand() would cause a division by zero error. We do not want that to happen so removed the "1.0/" part.

--- sql/test/pg_regress/Tests/random.sql
+++ sql/test/pg_regress/Tests/random.sql
@@ -7,7 +7,7 @@
 SELECT count(*) FROM onek;
 
 -- pick three random rows, they shouldn't match
--- MonetDB does not allow LIMIT in subqueries
+-- !! MonetDB does not allow ORDER BY or LIMIT in subqueries (and views), so disabled this test
 -- (SELECT unique1 AS random
 --   FROM onek ORDER BY random() LIMIT 1)
 -- INTERSECT
@@ -19,31 +19,34 @@ SELECT count(*) FROM onek;
 
 -- count roughly 1/10 of the tuples
 -- this one causes an undetected error
-CREATE TEMPORARY TABLE random_tbl( random double);
+CREATE TEMPORARY TABLE random_tbl( random double)
+ ON COMMIT PRESERVE ROWS;
 
 INSERT INTO random_tbl SELECT count(*) AS random 
   FROM onek WHERE rand() < 1.0/10;
 
 DROP TABLE random_tbl;
 
-CREATE TEMPORARY TABLE random_tbl( random bigint);
+CREATE TEMPORARY TABLE random_tbl( random bigint)
+ ON COMMIT PRESERVE ROWS;
+
 INSERT INTO random_tbl SELECT count(*) AS random 
-  FROM onek WHERE 1.0/rand() < 1.0/10;
+  FROM onek WHERE rand() < 1.0/10;
 
 -- select again, the count should be different
 INSERT INTO RANDOM_TBL (random)
   SELECT count(*)
-  FROM onek WHERE 1.0/rand() < 1.0/10;
+  FROM onek WHERE rand() < 1.0/10;
 
 -- select again, the count should be different
 INSERT INTO RANDOM_TBL (random)
   SELECT count(*)
-  FROM onek WHERE 1.0/rand() < 1.0/10;
+  FROM onek WHERE rand() < 1.0/10;
 
 -- select again, the count should be different
 INSERT INTO RANDOM_TBL (random)
   SELECT count(*)
-  FROM onek WHERE 1.0/rand() < 1.0/10;
+  FROM onek WHERE rand() < 1.0/10;
 
 -- now test that they are different counts
 SELECT random, count(random) FROM RANDOM_TBL
