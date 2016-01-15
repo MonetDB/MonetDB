1ad096dc4249dfc709d10ad52ac400f21c5de274
Hannes Muehleisen <hannes@cwi.nl>
2016-01-15 17:14:36 +0100

    Fixed build script to deal with missing sql parser header

--- tools/embedded/build-rpkg.sh
+++ tools/embedded/build-rpkg.sh
@@ -8,9 +8,12 @@ rm -rf $STAGEDIR
 
 hg archive $STAGEDIR/sourcetree
 
-make ../../sql/server/sql_parser.tab.c ../../sql/server/sql_parser.tab.h
-cp ../../sql/server/sql_parser.tab.* $STAGEDIR/sourcetree/sql/server/
-
+# generate mal/sql scripts and sql_parser.tab.c/h to remove our dependency on bison.
+rm -f ../../sql/server/sql_parser.tab.*
+cd ../../sql/server/
+make sql_parser.tab.c 
+make sql_parser.tab.h
+cp sql_parser.tab.c sql_parser.tab.h $STAGEDIR/sourcetree/sql/server/
 cd $STAGEDIR/sourcetree
 
 # patch build system to only include things we need
@@ -18,7 +21,8 @@ echo "SUBDIRS = embedded" > tools/Makefile.ag
 echo "SUBDIRS = mapilib" > clients/Makefile.ag
 echo "SUBDIRS = mal modules optimizer scheduler tools" > monetdb5/Makefile.ag
 echo "SUBDIRS = buildtools common clients gdk monetdb5 sql tools\nEXTRA_DIST = bootstrap configure configure.ac configure.ag libversions rpm.mk.in\nheaders_config = {\nDIR = includedir/monetdb\nHEADERS = h\nSOURCES = monetdb_config.h\n}\n" > Makefile.ag
-sed -i -e "/^SUBDIRS = .*$/d" sql/backends/monet5/Makefile.ag
+sed -i -e '/^SUBDIRS = .*$/d' sql/backends/monet5/Makefile.ag
+sed -i -e 's/sql_parser\.y/sql_parser.tab.h sql_parser.tab.c/' sql/server/Makefile.ag
 
 ./bootstrap
 # buildtools/conf/lt~obsolete.m4 file name violates CRAN policies, rename and replace references
@@ -29,7 +33,6 @@ cd ..
 mv sourcetree/tools/embedded/rpackage .
 rsync -av --exclude-from sourcetree/tools/embedded/pkg-excludes sourcetree/ rpackage/src
  
-# generate mal/sql scripts and sql_parser.tab.c/h to remove our dependency on bison.
 export R_INCLUDE_DIR=`R CMD config --cppflags | sed s/^-I//`
 
 # # inline mal/sql scripts, we need R with the stringr package for that
