acf880832b77081dd2382fba97b6e62e06907fe9
Sjoerd Mullender <sjoerd@acm.org>
2016-01-18 15:00:06 +0100

    Fix copyright messages.

--- sql/backends/monet5/vaults/bam/85_bam.sql
+++ sql/backends/monet5/vaults/bam/85_bam.sql
@@ -1,3 +1,9 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0.  If a copy of the MPL was not distributed with this
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/.
+--
+-- Copyright 1997 - July 2008 CWI, August 2008 - 2016 MonetDB B.V.
+
 CREATE SCHEMA bam;
 
 CREATE PROCEDURE bam.bam_loader_repos(bam_repos STRING, dbschema SMALLINT)
