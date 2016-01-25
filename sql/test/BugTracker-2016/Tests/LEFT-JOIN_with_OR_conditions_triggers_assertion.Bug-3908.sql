94093a96c5ca638110029483e8f0ecb151d9baec
Jennie Zhang <y.zhang@cwi.nl>
2016-01-25 12:38:55 +0100

    Added test for Bug 3908

---
+++ sql/test/BugTracker-2016/Tests/LEFT-JOIN_with_OR_conditions_triggers_assertion.Bug-3908.sql
@@ -0,0 +1,11 @@
+CREATE TABLE foo (id INTEGER, bar1 INTEGER, bar2 INTEGER);
+CREATE TABLE bar (barid INTEGER, value CHAR(10));
+INSERT INTO bar VALUES (1, 'aaa');
+INSERT INTO bar VALUES (2, 'bbb');
+INSERT INTO bar VALUES (3, 'ccc');
+INSERT INTO foo VALUES (100, 1, 2);
+INSERT INTO foo VALUES (101, 2, 3);
+SELECT B.value, F.id FROM bar B LEFT JOIN foo F ON (F.bar1 = B.barid OR F.bar2 = B.barid);
+
+DROP TABLE bar;
+DROP TABLE foo;
