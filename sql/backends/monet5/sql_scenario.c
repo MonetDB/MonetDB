744c7cc905c212355336fb018ac6d1f345511ccd
Niels Nes <niels@cwi.nl>
2016-02-03 10:45:29 +0100

    use same optimizer pipe for inlined queries.

--- sql/backends/monet5/sql_scenario.c
+++ sql/backends/monet5/sql_scenario.c
@@ -1263,17 +1263,20 @@ recompilequery:
 
 		chkTypes(c->fdout, c->nspace, c->curprg->def, TRUE);	/* resolve types */
 		if (opt) {
+			str msg = optimizeQuery(c);
+			/*
 			MalBlkPtr mb = c->curprg->def;
 
 			trimMalBlk(mb);
 			chkProgram(c->fdout, c->nspace, mb);
 			addOptimizers(c, mb, "default_pipe"); // TODO change to active pipe! 
 			msg = optimizeMALBlock(c, mb);
+			*/
 			if (msg != MAL_SUCCEED) {
 				sqlcleanup(m, err);
 				goto finalize;
 			}
-			c->curprg->def = mb;
+			//c->curprg->def = mb;
 		}
 		//printFunction(c->fdout, c->curprg->def, 0, LIST_MAL_ALL);
 		/* we know more in this case than chkProgram(c->fdout, c->nspace, c->curprg->def); */
