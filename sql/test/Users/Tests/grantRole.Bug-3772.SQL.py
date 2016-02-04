10b3df6f603e4447676d25690813d8be89d14edf
Sjoerd Mullender <sjoerd@acm.org>
2016-02-04 17:08:21 +0100

    Inline code from util.py since it cannot be found during testing on Windows.

--- sql/test/Users/Tests/grantRole.Bug-3772.SQL.py
+++ sql/test/Users/Tests/grantRole.Bug-3772.SQL.py
@@ -2,7 +2,16 @@
 # Let any user grant any role (not possible).
 ###
 
-from util import sql_test_client
+import os, sys
+try:
+    from MonetDBtesting import process
+except ImportError:
+    import process
+
+def sql_test_client(user, passwd, input):
+    process.client(lang = "sql", user = user, passwd = passwd, communicate = True,
+                   stdin = process.PIPE, stdout = process.PIPE, stderr = process.PIPE,
+                   input = input, port = int(os.getenv("MAPIPORT")))
 
 sql_test_client('monetdb', 'monetdb', input = """\
 CREATE SCHEMA s1;
