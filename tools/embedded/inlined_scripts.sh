5bae89c6f4c12af84a1e5aec063796e6102127ff
Hannes Muehleisen <hannes@cwi.nl>
2016-02-23 13:31:18 +0100

    update for inlined scripts

--- tools/embedded/inlined_scripts.sh
+++ tools/embedded/inlined_scripts.sh
@@ -2,14 +2,15 @@
 
 PREFIX=/tmp/monetdb-embedded-dummytarget
 rm -r $PREFIX
+mkdir -p $PREFIX
 
 cd ../../
-./configure --prefix=$PREFIX --enable-embedded \
+./configure --prefix=$PREFIX \
 --disable-fits --disable-geom --disable-rintegration --disable-gsl --disable-netcdf \
 --disable-jdbc --disable-merocontrol --disable-odbc --disable-console --disable-microhttpd \
 --without-perl --without-python2 --without-python3 --without-rubygem --without-unixodbc \
 --without-samtools --without-sphinxclient --without-geos --without-samtools --without-readline \
---enable-optimize --enable-silent-rules --disable-assert --enable-strict --disable-int128
+--enable-debug --enable-silent-rules --disable-assert --disable-strict --disable-int128
 
 make -j clean install
 
