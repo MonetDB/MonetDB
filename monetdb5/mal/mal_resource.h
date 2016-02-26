44e45b3211f894f09fd919c248966640650e8b07
Martin Kersten <mk@cwi.nl>
2016-02-26 07:13:35 +0100

    Refine size calculations

--- monetdb5/mal/mal_resource.h
+++ monetdb5/mal/mal_resource.h
@@ -15,8 +15,9 @@
 #define DELAYUNIT 2 /* ms delay in parallel processing decisions */
 #define MAX_DELAYS 1000 /* never wait forever */
 
-#define heapinfo(X) ((X) && (X)->base ? (X)->free: 0)
-#define hashinfo(X) ( (X)? heapinfo((X)->heap):0)
+//#define heapinfo(X,Id)	(((X) && (X)->base && ((X)->parentid == 0 || (X)->parentid == Id)) ? (X)->free : 0)
+#define heapinfo(X,Id)	(((X) && (X)->base ) ? (X)->free : 0)
+#define hashinfo(X,Id) ( (X)? heapinfo((X)->heap, Id):0)
 
 #define USE_MAL_ADMISSION
 #ifdef USE_MAL_ADMISSION
