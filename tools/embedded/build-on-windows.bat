8c88ed13c672fa3bd1d548d8cfad12d39c60f880
Hannes Muehleisen <hannes@cwi.nl>
2016-03-08 10:16:26 +0100

    multi-arch builds on windows

--- tools/embedded/build-on-windows.bat
+++ tools/embedded/build-on-windows.bat
@@ -1,4 +1,4 @@
 "c:\Program Files (x86)\GnuWin32\bin\wget.exe" http://dev.monetdb.org/Assets/R/MonetDBLite_0.2.0.tar.gz
-"c:\Program Files\R\R-3.2.3\bin\R" CMD INSTALL --build MonetDBLite_0.2.0.tar.gz 
+"c:\Program Files\R\R-3.2.3\bin\R" CMD INSTALL --build --force-biarch MonetDBLite_0.2.0.tar.gz 
 rm MonetDBLite_0.2.0.tar.gz 
 pause
