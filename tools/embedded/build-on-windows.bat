e371bb48ededa473db9b93eefcb6f34bcf709bae
Hannes Muehleisen <hannes@cwi.nl>
2016-03-21 17:25:12 +0100

    versions bump for r packages

--- tools/embedded/build-on-windows.bat
+++ tools/embedded/build-on-windows.bat
@@ -1,5 +1,5 @@
-rm MonetDBLite_0.2.1.zip
-"c:\Program Files (x86)\GnuWin32\bin\wget.exe" http://dev.monetdb.org/Assets/R/MonetDBLite_0.2.1.tar.gz
-"c:\Program Files\R\R-3.2.3\bin\R" CMD INSTALL --build --merge-multiarch MonetDBLite_0.2.1.tar.gz 
-rm MonetDBLite_0.2.1.tar.gz 
+rm MonetDBLite_0.2.2.zip
+"c:\Program Files (x86)\GnuWin32\bin\wget.exe" http://dev.monetdb.org/Assets/R/MonetDBLite_0.2.2.tar.gz
+"c:\Program Files\R\R-3.2.4revised\bin\R" CMD INSTALL --build --merge-multiarch MonetDBLite_0.2.2.tar.gz 
+rm MonetDBLite_0.2.2.tar.gz 
 pause
