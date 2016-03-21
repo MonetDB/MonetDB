8e382653dddfbf7034719c3fe3854eaf69270373
Sjoerd Mullender <sjoerd@acm.org>
2016-03-21 10:56:32 +0100

    Backing out changeset d4ae7aed539c: it belongs on branch iot.

--- sql/backends/monet5/iot/Tests/iot99.sql
+++
@@ -1,9 +0,0 @@
--- initialize the stream testing environment
-set schema iot;
-set optimizer='iot_pipe';
-
-call iot.receptor();
-drop procedure clk1;
-drop procedure clk3;
-drop procedure collector;
-drop table stream_tmp;
