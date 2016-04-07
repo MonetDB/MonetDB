d02f42e734f6bda460f834890cda3f1b89be725a
Martin van Dinther <martin.van.dinther@monetdbsolutions.com>
2016-04-07 18:21:49 +0200

    Extending tests and approved new outputs.

--- sql/test/BugTracker-2016/Tests/column_alias_in_where_clause.Bug-3947.sql
+++ sql/test/BugTracker-2016/Tests/column_alias_in_where_clause.Bug-3947.sql
@@ -8,15 +8,31 @@ SELECT a AS "A", b AS "B", c AS "C" FROM t_alias ORDER BY 1;
 -- column aliases should be able to be used in WHERE clause, see Bug 3947
 SELECT a AS "A", b AS "B", c AS "C" FROM t_alias WHERE "A" < "B";
 -- SELECT: identifier 'A' unknown
+SELECT * FROM (SELECT a AS "A", b AS "B", c AS "C" FROM t_alias) T1 WHERE "A" < "B";
+
 SELECT a AS "A", b * b AS "B", c AS "C" FROM t_alias WHERE "b" * b >99;
 SELECT a AS "A", b * b AS "B", c AS "C" FROM t_alias WHERE "B" >99;
 -- SELECT: identifier 'B' unknown
+SELECT * FROM (SELECT a AS "A", b * b AS "B", c AS "C" FROM t_alias) T1 WHERE "B" >99;
+
 SELECT a AS "A", b AS "B", c AS "C" FROM t_alias WHERE "c" LIKE '%en';
 SELECT a AS "A", b AS "B", c AS "C" FROM t_alias WHERE "C" LIKE '%en';
 -- SELECT: identifier 'C' unknown
+SELECT * FROM (SELECT a AS "A", b AS "B", c AS "C" FROM t_alias) T1 WHERE "C" LIKE '%en';
+
 SELECT cast(null as char(1)) AS "CAT", a AS "A", c AS "C" FROM t_alias WHERE "c" = 'null';
-SELECT cast(null as char(1)) AS "CAT", a AS "A", c AS "C" FROM t_alias WHERE "CAT" = 'null';
+
+SELECT cast(null as char(1)) AS "CAT", a AS "A", c AS "C" FROM t_alias WHERE "CAT" = NULL;
 -- SELECT: identifier 'CAT' unknown
+SELECT * FROM (SELECT cast(null as char(1)) AS "CAT", a AS "A", c AS "C" FROM t_alias) T1 WHERE "CAT" = NULL;
+
+SELECT cast(null as char(1)) AS "CAT", a AS "A", c AS "C" FROM t_alias WHERE "CAT" IS NULL;
+-- SELECT: identifier 'CAT' unknown
+SELECT * FROM (SELECT cast(null as char(1)) AS "CAT", a AS "A", c AS "C" FROM t_alias) T1 WHERE "CAT" IS NULL;
+
+SELECT cast(null as char(1)) AS "CAT", a AS "A", c AS "C" FROM t_alias WHERE "CAT" IS NULL or "CAT" = NULL;
+-- SELECT: identifier 'CAT' unknown
+SELECT * FROM (SELECT cast(null as char(1)) AS "CAT", a AS "A", c AS "C" FROM t_alias) T1 WHERE "CAT" IS NULL or "CAT" = NULL;
 
 -- column aliases can be used in ORDER BY and GROUP BY clauses
 SELECT a AS "A", b AS "B", c AS "C" FROM t_alias ORDER BY "C", "A", "B";
