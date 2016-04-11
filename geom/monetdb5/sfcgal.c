70c592f4a5b47a7f0b0f69b5555ad9bf897f7be2
Kostis Kyzirakos <kostis@cwi.nl>
2016-04-12 00:30:29 +0200

    Added a minimal extension of the geom module that utilizes the SFCGAL library

---
+++ geom/monetdb5/sfcgal.c
@@ -0,0 +1,22 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0.  If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright 1997 - July 2008 CWI, August 2008 - 2016 MonetDB B.V.
+ */
+
+/*
+ * @a Kostis Kyzirakos
+ * @* SFCGAL-based functionality for the geom module
+ */
+
+#include "sfcgal.h"
+
+char *
+msfcgal_version(char **ret)
+{
+	*ret = GDKstrdup(sfcgal_version());
+
+	return MAL_SUCCEED;
+}
---
+++ geom/monetdb5/sfcgal.h
@@ -0,0 +1,41 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0.  If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright 1997 - July 2008 CWI, August 2008 - 2016 MonetDB B.V.
+ */
+
+/*
+ * @a Kostis Kyzirakos
+ */
+
+#include <monetdb_config.h>
+#include "libgeom.h"
+#include "geom.h"
+
+#include <mal.h>
+#include <mal_atom.h>
+#include <mal_exception.h>
+#include <mal_client.h>
+#include <stream.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+
+#include <SFCGAL/capi/sfcgal_c.h>
+
+#ifdef WIN32
+#ifndef LIBGEOM
+#define geom_export extern __declspec(dllimport)
+#else
+#define geom_export extern __declspec(dllexport)
+#endif
+#else
+#define geom_export extern
+#endif
+
+geom_export char * msfcgal_version(char **ret);
