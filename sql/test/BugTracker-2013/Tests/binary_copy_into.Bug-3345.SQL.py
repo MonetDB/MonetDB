5ab458b3f2d7ce6e9fafa7a51842673e7f04e738
Sjoerd Mullender <sjoerd@acm.org>
2016-05-04 15:08:54 +0200

    Fix backslashes in COPY INTO for Windows.

--- sql/test/BugTracker-2013/Tests/binary_copy_into.Bug-3345.SQL.py
+++ sql/test/BugTracker-2013/Tests/binary_copy_into.Bug-3345.SQL.py
@@ -19,7 +19,9 @@ shutil.copyfile(os.path.join(src,'n_regionkey.sorted'), os.path.join(dst,'n_regi
 
 clt.stdin.write('start transaction;');
 clt.stdin.write('create table bug (n_nationkey INTEGER,n_regionkey INTEGER);\n')
-clt.stdin.write('copy binary into bug from \'%s/n_nationkey.sorted\', \'%s/n_regionkey.sorted\';\n'% (dst,dst))
+clt.stdin.write('copy binary into bug from \'%s\', \'%s\';\n' %
+                (os.path.join(dst, 'n_nationkey.sorted').replace('\\', '\\\\'),
+                 os.path.join(dst, 'n_regionkey.sorted').replace('\\', '\\\\')))
 
 out, err = clt.communicate()
 sys.stdout.write(out.replace(os.environ['TSTTRGBASE'],'${TSTTRGBASE}').replace('\\','/'))
