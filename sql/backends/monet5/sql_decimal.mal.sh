7955f6015ad6edeafadc5954775d4910a818bbf9
Sjoerd Mullender <sjoerd@acm.org>
2016-05-09 10:28:36 +0200

    Indentation.

--- sql/backends/monet5/sql_decimal.mal.sh
+++ sql/backends/monet5/sql_decimal.mal.sh
@@ -15,8 +15,8 @@ integer="bte sht int wrd lng"	# all integer types
 numeric="$integer flt dbl"	# all numeric types
 
 for tp1 in $integer ; do
-	for tp2 in flt dbl ; do
-	    cat <<EOF
+    for tp2 in flt dbl ; do
+	cat <<EOF
 command calc.${tp1}( v:${tp2}, digits:int, scale:int ) :${tp1}
 address ${tp2}_num2dec_${tp1}
 comment "cast number to decimal(${tp1}) and check for overflow";
@@ -26,12 +26,12 @@ address bat${tp2}_num2dec_${tp1}
 comment "cast number to decimal(${tp1}) and check for overflow";
 
 EOF
-done
+    done
 done
 
 for tp1 in $numeric ; do
-	for tp2 in $integer ; do
-	    cat <<EOF
+    for tp2 in $integer ; do
+	cat <<EOF
 command calc.${tp1}( v:${tp2}, digits:int, scale:int ) :${tp1}
 address ${tp2}_num2dec_${tp1}
 comment "cast number to decimal(${tp1}) and check for overflow";
@@ -55,5 +55,5 @@ address bat${tp2}_dec2dec_${tp1}
 comment "cast decimal(${tp2}) to decimal(${tp1}) and check for overflow";
 
 EOF
-done
+    done
 done
