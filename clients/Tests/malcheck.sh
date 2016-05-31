2654201ed93ae7172ed9d6efdda372d09a1867ca
Sjoerd Mullender <sjoerd@acm.org>
2016-05-31 16:18:56 +0200

    Use 'hg files' so as to not fail when files were removed during development.

--- clients/Tests/malcheck.sh
+++ clients/Tests/malcheck.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd $TSTSRCBASE
-hg locate -I monetdb5 -I geom -I sql -X '**/Tests/**' '*.[ch]' '*.mal' | python -c 'import MonetDBtesting.malcheck'
+hg files -I 'monetdb5/**.[ch]' -I 'geom/**.[ch]' -I 'sql/**.[ch]' -X '**/Tests/**' | python -c 'import MonetDBtesting.malcheck'
