5d66c13323ce8075f2b2708317bb40fa316eeeab
Sjoerd Mullender <sjoerd@acm.org>
2016-06-01 11:51:11 +0200

    Plug memory leak.

--- monetdb5/extras/rapi/rapi.c
+++ monetdb5/extras/rapi/rapi.c
@@ -441,6 +441,8 @@ str RAPIeval(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci, bit groupe
   wrapup:
 	MT_lock_unset(&rapiLock);
 	free(rcall);
+	for (i = 0; i < pci->argc; i++)
+		GDKfree(args[i]);
 	GDKfree(args);
 
 	return msg;
