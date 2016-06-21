afc860263f0a0ca9e95abfc1a462c48a8244e007
Sjoerd Mullender <sjoerd@acm.org>
2016-06-21 08:06:14 +0200

    Use correct type. (grafted from 42207239a7ed6a086f802a7b375abeb2e49de4d5)

--- sql/storage/bat/bat_storage.c
+++ sql/storage/bat/bat_storage.c
@@ -2087,7 +2087,7 @@ tr_update_dbat_swap(sql_dbat *tdb, sql_dbat *fdb)
 	 * Swap both tdb->dbid/fdb->dbid */
 	int ok = LOG_OK;
 	BAT *db = NULL;
-	size_t cnt;
+	BUN cnt;
 
 	if (!fdb)
 		return ok;
