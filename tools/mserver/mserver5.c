01f9a50b9560354df4c6d4b51caeac52198ca8d1
Sjoerd Mullender <sjoerd@acm.org>
2016-07-21 14:40:36 +0200

    Use two digits for exponents on Windows (if it fits, obviously).

--- tools/mserver/mserver5.c
+++ tools/mserver/mserver5.c
@@ -286,6 +286,7 @@ main(int argc, char **av)
 	_CrtSetReportMode(_CRT_ERROR, 0);
 	_CrtSetReportMode(_CRT_ASSERT, 0);
 	_set_invalid_parameter_handler(mserver_invalid_parameter_handler);
+	_set_output_format(_TWO_DIGIT_EXPONENT);
 #endif
 	if (setlocale(LC_CTYPE, "") == NULL) {
 		GDKfatal("cannot set locale\n");
