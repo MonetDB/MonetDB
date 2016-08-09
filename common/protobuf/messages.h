65a40cb5ae5b395e4ba982b5ebd8867b998c0fe7
Hannes Muehleisen <hannes@cwi.nl>
2016-08-09 10:51:49 +0200

    Backed out changeset d5d332081c12

--- common/protobuf/messages.h
+++
@@ -1,9 +0,0 @@
-#include "mhapi.pb-c.h"
-#include "stream.h"
-
-Mhapi__Message* message_read(stream *s, compression_method comp);
-Mhapi__Message* message_read_length(stream *s, compression_method comp, lng length);
-
-ssize_t message_write(stream *s, compression_method comp, Mhapi__Message *msg);
-void message_send_error(stream *s, protocol_version proto, const char *format, ...);
-void message_send_warning(stream *s, protocol_version proto, const char *format, ...);
