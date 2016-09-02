474330307464853e43948cd06aa89ffd8d94ecb8
Martin Kersten <mk@cwi.nl>
2016-09-02 21:48:47 +0200

    Protect against failing client thread creation

--- monetdb5/mal/mal_session.c
+++ monetdb5/mal/mal_session.c
@@ -44,11 +44,14 @@ malBootstrap(void)
 	initLibraries();
 	if ( (msg = defaultScenario(c)) ) {
 		GDKfree(msg);
-		GDKerror("Failed to initialise default scenario");
+		GDKerror("malBootstrap:Failed to initialise default scenario");
 		return 0;
 	}
 	MSinitClientPrg(c, "user", "main");
-	(void) MCinitClientThread(c);
+	if( MCinitClientThread(c) < 0){
+		GDKerror("malBootstrap:Failed to create client thread");
+		return 0;
+	}
 	s = malInclude(c, bootfile, 0);
 	if (s != NULL) {
 		mnstr_printf(GDKout, "!%s\n", s);
