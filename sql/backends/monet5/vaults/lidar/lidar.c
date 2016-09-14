1f4a76774ac8bfb958d32139bc124d81f872607e
Sjoerd Mullender <sjoerd@acm.org>
2016-09-14 09:42:04 +0200

    %lc printf format is for *wide* characters.

--- sql/backends/monet5/vaults/lidar/lidar.c
+++ sql/backends/monet5/vaults/lidar/lidar.c
@@ -1018,8 +1018,8 @@ str LIDARloadTable(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci)
 				"number of returns : %d\n"
 				"scan direction    : %d\n"
 				"flight line edge  : %d\n"
-				"scan flags        : %lc\n"
-				"classification    : %lc\n"
+				"scan flags        : %d\n"
+				"classification    : %d\n"
 				"time              : %f\n"
 				"scan angle rank   : %d\n"
 				"point source id   : %d\n",
