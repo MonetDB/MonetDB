2643aef05a2e3704a818fe0d8ac7709a4536c7f3
Martin Kersten <mk@cwi.nl>
2016-09-29 22:20:22 +0200

    Revert optimizer defense lines The optimizerIsApplied encounters NULL instruction pointers or doesn't find optimizer that has been called. More search is needed

--- monetdb5/optimizer/opt_mergetable.c
+++ monetdb5/optimizer/opt_mergetable.c
@@ -1569,8 +1569,8 @@ OPTmergetableImplementation(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr 
 	char buf[256];
 	lng usec = GDKusec();
 
-	if( optimizerIsApplied(mb, "mergetable") || !optimizerIsApplied(mb,"mitosis"))
-		return 0;
+	//if( optimizerIsApplied(mb, "mergetable") || !optimizerIsApplied(mb,"mitosis"))
+		//return 0;
 	old = mb->stmt;
 	oldtop= mb->stop;
 #ifdef DEBUG_OPT_MERGETABLE
