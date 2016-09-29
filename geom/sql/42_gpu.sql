232f9fc42844c84e280c6f14ff2d81762b47f349
Romulo Goncalves <goncalve@cwi.nl>
2016-09-29 15:32:20 +0200

    Improve names. Function to Setup GPU and reset the settings

--- geom/sql/42_gpu.sql
+++ geom/sql/42_gpu.sql
@@ -4,4 +4,6 @@
 --
 -- Copyright 1997 - July 2008 CWI, August 2008 - 2016 MonetDB B.V.
 
-CREATE FUNCTION G_Contains(a Geometry, x double, y double, z double, srid int) RETURNS BOOLEAN external name geom."GContains";
+CREATE FUNCTION G_Setup(flag integer) RETURNS BOOLEAN external name geom."gsetup";
+CREATE FUNCTION G_Reset(flag integer) RETURNS BOOLEAN external name geom."greset";
+CREATE FUNCTION G_Contains(a Geometry, x double, y double, z double, srid int) RETURNS BOOLEAN external name geom."gcontains";
