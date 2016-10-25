11c27101a877449b273bcdb2776d877e61b40577
Hannes Muehleisen <hannes@cwi.nl>
2016-10-25 15:21:40 +0200

    r tests using mfilter variable output now

--- clients/R/Tests/deps-install.R
+++ clients/R/Tests/deps-install.R
@@ -1,8 +1,15 @@
 # autoinstall DBI and digest, we need those to install MonetDB.R
-dd <- capture.output(suppressMessages(suppressWarnings({
-	repos <- "http://cran.rstudio.com/"
-	lp <- c("devtools", "digest")
-	install.packages(lp, repos=repos, quiet=T)
-	update.packages(repos=repos, ask=F, oldPkgs=lp)
-	devtools::install_github("rstats-db/DBI", quiet=T)
-})))
+
+cat("#~BeginProfilingOutput~#\n", file=stderr())
+cat("#~BeginProfilingOutput~#\n", file=stdout())
+
+repos <- "http://cran.rstudio.com/"
+packages_required <- c("devtools", "digest")
+install.packages(packages_required, repos=repos, quiet=T)
+update.packages(repos=repos, ask=F, oldPkgs=packages_required)
+devtools::install_github("rstats-db/DBI", quiet=T)
+
+cat("#~EndProfilingOutput~#\n", file=stderr())
+cat("#~EndProfilingOutput~#\n\n", file=stdout())
+
+stopifnot(all(c(packages_required, "DBI") %in% installed.packages()[,"Package"]))
