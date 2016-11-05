7c69b0931b2025290ab1783bdf2f2f725e1ba851
Martin Kersten <mk@cwi.nl>
2016-11-05 09:43:26 +0100

    Only keep total statistics when optimizers have been applied

--- monetdb5/optimizer/opt_support.c
+++ monetdb5/optimizer/opt_support.c
@@ -154,9 +154,11 @@ optimizeMALBlock(Client cntxt, MalBlkPtr mb)
 			}
 		}
 	} while (qot && cnt++ < mb->stop);
-	mb->optimize= GDKusec() - clk;
-	snprintf(buf,256,"%-20s actions=%2d time=" LLFMT " usec","total",1,mb->optimize);
-	newComment(mb,buf);
+	if( qot){
+		mb->optimize= GDKusec() - clk;
+		snprintf(buf,256,"%-20s actions=%2d time=" LLFMT " usec","total",1,mb->optimize);
+		newComment(mb,buf);
+	}
 	if (cnt >= mb->stop)
 		throw(MAL, "optimizer.MALoptimizer", OPTIMIZER_CYCLE);
 	return 0;
