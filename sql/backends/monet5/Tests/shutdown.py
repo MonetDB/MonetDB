7376f6a910403e10e7e70259b3be56c54351c858
Mark Raasveldt <m.raasveldt@cwi.nl>
2016-11-30 10:11:04 +0100

    Properly call shutdown program in the test.

--- sql/backends/monet5/Tests/shutdown.py
+++ sql/backends/monet5/Tests/shutdown.py
@@ -2,14 +2,14 @@
 import os
 import shutil
 
-bindir = os.getenv('TSTTRGBASE')
 databasedir = os.getenv('TSTTRGDIR')
 if databasedir == None:
     exit()
 databasedir = os.path.join(databasedir, 'SHUTDOWN_DATABASE')
-if bindir == None:
-    os.system('shutdowntest "%s"' % databasedir)
+
+if os.name == 'nt':
+    os.system('shutdowntest.exe "%s"' % databasedir)
 else:
-    os.system(os.path.join(bindir, 'bin', 'shutdowntest') + ' "%s"' % databasedir)
+    os.system('Mlog -x shutdowntest "%s"' % databasedir)
 
 shutil.rmtree(databasedir)
