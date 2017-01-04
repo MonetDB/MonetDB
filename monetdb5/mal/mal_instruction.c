a041f32da72b9fb37556311aa73026d783a94ca7
Niels Nes <niels@cwi.nl>
2017-01-04 13:36:11 +0100

    fix concurrency problem, do not keep array of Symbols, simply use malloc (which is thread safe), were a  'a = b++' isn't atomic.

--- monetdb5/mal/mal_instruction.c
+++ monetdb5/mal/mal_instruction.c
@@ -16,10 +16,6 @@
 #include "mal_utils.h"
 #include "mal_exception.h"
 
-#define MAXSYMBOLS 12000 /* enough for the startup and some queries */
-static SymRecord symbolpool[MAXSYMBOLS];
-static int symboltop;
-
 Symbol
 newSymbol(str nme, int kind)
 {
@@ -29,13 +25,9 @@ newSymbol(str nme, int kind)
 		GDKerror("newSymbol:unexpected name (=null)\n");
 		return NULL;
 	}
-	if( symboltop < MAXSYMBOLS){
-		cur = symbolpool + symboltop++;
-	} else {
-		cur = (Symbol) GDKzalloc(sizeof(SymRecord));
-		if (cur == NULL)
-			return NULL;
-	}
+	cur = (Symbol) GDKzalloc(sizeof(SymRecord));
+	if (cur == NULL)
+		return NULL;
 	cur->name = putName(nme);
 	cur->kind = kind;
 	cur->peer = NULL;
@@ -56,8 +48,7 @@ freeSymbol(Symbol s)
 		freeMalBlk(s->def);
 		s->def = NULL;
 	}
-	if( !( s >= symbolpool && s < symbolpool + MAXSYMBOLS))
-		GDKfree(s);
+	GDKfree(s);
 }
 
 void
