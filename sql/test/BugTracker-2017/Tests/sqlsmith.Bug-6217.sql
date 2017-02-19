b5af9399e46fd0dc7fb18f56ac55c3f550666659
Niels Nes <niels@cwi.nl>
2017-02-19 14:05:12 +0100

    add tests for bugs 6216 and 6217

---
+++ sql/test/BugTracker-2017/Tests/sqlsmith.Bug-6217.sql
@@ -0,0 +1,10 @@
+select
+  ref_2.name as c0,
+  ref_2.name as c1,
+  ref_2.condition as c2,
+  90 as c3,
+  ref_2.condition as c4,
+  ref_2.id as c5
+from
+  sys.triggers as ref_2
+where 24 is NULL;
