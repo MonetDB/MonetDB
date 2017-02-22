739eab40ddbd7512ad0fa4fa0e244805382e54f7
Sjoerd Mullender <sjoerd@acm.org>
2017-02-22 13:57:41 +0100

    Add checks: I've seen allocation fail in hash_add.  It's not academic.

--- sql/storage/sql_catalog.c
+++ sql/storage/sql_catalog.c
@@ -26,12 +26,19 @@ _list_find_name(list *l, const char *name)
 		MT_lock_set(&l->ht_lock);
 		if ((!l->ht || l->ht->size*16 < list_length(l)) && list_length(l) > HASH_MIN_SIZE && l->sa) {
 			l->ht = hash_new(l->sa, list_length(l), (fkeyvalue)&base_key);
+			if (l->ht == NULL) {
+				MT_lock_unset(&l->ht_lock);
+				return NULL;
+			}
 
 			for (n = l->h; n; n = n->next ) {
 				sql_base *b = n->data;
 				int key = base_key(b);
 
-				hash_add(l->ht, key, b);
+				if (hash_add(l->ht, key, b) == NULL) {
+					MT_lock_unset(&l->ht_lock);
+					return NULL;
+				}
 			}
 		}
 		if (l->ht) {
