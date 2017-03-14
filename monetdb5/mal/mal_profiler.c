dcde9e2f6f7e52dbd3bba93d8c304928a183472d
Panagiotis Koutsourakis <kutsurak@monetdbsolutions.com>
2017-03-14 17:24:25 +0100

    Add ctime back to the mal profiler output
    This time however we don't use the gettimeofday for every MAL instruction. We
    call it at server startup and compute the offset using GDKusec. This should be
    accurate to a few nanoseconds.

--- monetdb5/mal/mal_profiler.c
+++ monetdb5/mal/mal_profiler.c
@@ -22,6 +22,8 @@
 #include "mal_debugger.h"
 #include "mal_resource.h"
 
+#include <sys/time.h>
+
 static void cachedProfilerEvent(MalBlkPtr mb, MalStkPtr stk, InstrPtr pci);
 
 stream *eventstream = 0;
@@ -35,6 +37,8 @@ static int highwatermark = 5;	// conservative initialization
 
 static int TRACE_init = 0;
 int malProfileMode = 0;     /* global flag to indicate profiling mode */
+
+static struct timeval startup_time;
 
 static volatile ATOMIC_TYPE hbdelay = 0;
 
@@ -107,7 +111,12 @@ renderProfilerEvent(MalBlkPtr mb, MalStkPtr stk, InstrPtr pci, int start, str us
 	str stmt, c;
 	str stmtq;
 	lng usec= GDKusec();
+	lng sec = (lng)startup_time.tv_sec + usec/1000000;
+	long microseconds = (long)startup_time.tv_usec + (usec % 1000000);
 
+	assert (microseconds / 1000000 >= 0 && microseconds / 1000000 < 2);
+	sec += (microseconds / 1000000);
+	microseconds %= 1000000;
 
 	// ignore generation of events for instructions that are called too often
 	if(highwatermark && highwatermark + (start == 0) < pci->calls)
@@ -120,6 +129,7 @@ renderProfilerEvent(MalBlkPtr mb, MalStkPtr stk, InstrPtr pci, int start, str us
 	if( usrname)
 		logadd("\"user\":\"%s\",%s",usrname, prettify);
 	logadd("\"clk\":"LLFMT",%s",usec,prettify);
+	logadd("\"ctime\":"LLFMT".%06ld,%s", sec, microseconds, prettify);
 	logadd("\"thread\":%d,%s", THRgettid(),prettify);
 
 	logadd("\"function\":\"%s.%s\",%s", getModuleId(getInstrPtr(mb, 0)), getFunctionId(getInstrPtr(mb, 0)), prettify);
@@ -1022,6 +1032,7 @@ void setHeartbeat(int delay)
 
 void initProfiler(void)
 {
+	gettimeofday(&startup_time, NULL);
 	if( mal_trace)
 		openProfilerStream(mal_clients[0].fdout,0);
 }
