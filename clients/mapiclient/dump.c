ef2c8fe2e82e341cced323a4c80dfb7dfd19e7f2
Sjoerd Mullender <sjoerd@acm.org>
2017-04-25 10:55:57 +0200

    Approve output.

--- clients/mapiclient/dump.c
+++ clients/mapiclient/dump.c
@@ -1219,10 +1219,10 @@ dump_table_data(Mapi mid, char *schema, char *tname, stream *toConsole,
 	string = malloc(sizeof(unsigned char) * cnt);
 	for (i = 0; i < cnt; i++) {
 		string[i] = (strcmp(mapi_get_type(hdl, i), "char") == 0 ||
-					 strcmp(mapi_get_type(hdl, i), "varchar") == 0 ||
-					 strcmp(mapi_get_type(hdl, i), "clob") == 0 ||
-					 strcmp(mapi_get_type(hdl, i), "timestamp") == 0 ||
-					 strcmp(mapi_get_type(hdl, i), "timestamptz") == 0);
+			     strcmp(mapi_get_type(hdl, i), "varchar") == 0 ||
+			     strcmp(mapi_get_type(hdl, i), "clob") == 0 ||
+			     strcmp(mapi_get_type(hdl, i), "timestamp") == 0 ||
+			     strcmp(mapi_get_type(hdl, i), "timestamptz") == 0);
 	}
 	while (mapi_fetch_row(hdl)) {
 		char *s;
