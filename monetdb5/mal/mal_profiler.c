12a9a539e3f02209b38b64aa7942272db51d194d
Sjoerd Mullender <sjoerd@acm.org>
2017-05-08 14:21:23 +0200

    Cleanup *all* columns of trace table. (grafted from f9aa04614d2a49eebabbd3547f6acb789c6c07c7)

--- monetdb5/mal/mal_profiler.c
+++ monetdb5/mal/mal_profiler.c
@@ -715,6 +715,8 @@ _cleanupProfiler(void)
 	CLEANUPprofile(TRACE_id_event);
 	CLEANUPprofile(TRACE_id_time);
 	CLEANUPprofile(TRACE_id_pc);
+	CLEANUPprofile(TRACE_id_thread);
+	CLEANUPprofile(TRACE_id_ticks);
 	CLEANUPprofile(TRACE_id_rssMB);
 	CLEANUPprofile(TRACE_id_tmpspace);
 	CLEANUPprofile(TRACE_id_inblock);
@@ -722,7 +724,6 @@ _cleanupProfiler(void)
 	CLEANUPprofile(TRACE_id_minflt);
 	CLEANUPprofile(TRACE_id_majflt);
 	CLEANUPprofile(TRACE_id_nvcsw);
-	CLEANUPprofile(TRACE_id_thread);
 	CLEANUPprofile(TRACE_id_stmt);
 	TRACE_init = 0;
 }
