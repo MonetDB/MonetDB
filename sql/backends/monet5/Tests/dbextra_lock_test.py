77673eda75473f0bbb44ff6a7c3d0093422cc06a
Panagiotis Koutsourakis <kutsurak@monetdbsolutions.com>
2017-05-11 13:52:45 +0200

    Make the dbextra locking test portable

--- sql/backends/monet5/Tests/dbextra_lock_test.py
+++ sql/backends/monet5/Tests/dbextra_lock_test.py
@@ -4,14 +4,17 @@ except ImportError:
     import process
 
 import sys
+import os
+
+p = os.path.join(os.getenv('GDK_DBFARM'), 'transient_dir')
 
 s1 = process.server(stdin = process.PIPE, stdout = process.PIPE,
-                    stderr = process.PIPE, dbextra='/tmp/dbextra/')
+                    stderr = process.PIPE, dbextra=p)
 
 
 
 s2 = process.server(stdin = process.PIPE, stdout = process.PIPE,
-                    stderr = process.PIPE, dbextra='/tmp/dbextra/')
+                    stderr = process.PIPE, dbextra=p)
 
 out, err = s1.communicate()
 sys.stdout.write(out)
