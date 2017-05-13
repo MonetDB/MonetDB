5ada11bd9107e86b8e36934c0036b37f861e34b1
Martin Kersten <mk@cwi.nl>
2017-05-13 23:17:44 +0200

    Extended test with similar issue

--- sql/test/BugTracker-2017/Tests/exist-bigint-missing.Bug-6315.sql
+++ sql/test/BugTracker-2017/Tests/exist-bigint-missing.Bug-6315.sql
@@ -5,3 +5,17 @@ select
         end
 from sys.tables;
 
+
+insert into sys.privileges values (
+        1,
+        31,
+        87,
+        case when EXISTS (
+              select (select io from sys.querylog_calls) as c0
+              from
+                sys.rejects as ref_0
+              where ref_0.fldid is NULL) 
+			then 56 else 56 end ,
+        case when (select dependency_type_name from sys.dependency_types)
+               is not NULL then 7 else 7 end
+          );
