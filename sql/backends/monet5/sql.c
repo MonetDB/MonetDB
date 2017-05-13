45a0fa495940af1757afc10db6aace8918046927
Unknown <martin.kersten@cwi.nl>
2017-05-13 17:56:06 +0200

    A little explanation

--- sql/backends/monet5/sql.c
+++ sql/backends/monet5/sql.c
@@ -2784,7 +2784,7 @@ zero_or_one(ptr ret, const bat *bid)
 		p = BUNtail(bi, 0);
 	} else {
 		p = NULL;
-		throw(SQL, "zero_or_one", "21000!cardinality violation");
+		throw(SQL, "zero_or_one", "21000!cardinality violation, scalar value expected");
 	}
 	_s = ATOMsize(ATOMtype(b->ttype));
 	if (ATOMextern(b->ttype)) {
