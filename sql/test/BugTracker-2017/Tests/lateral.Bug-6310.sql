f8ac98054a8224187ac1f4d7f02d3d2d6e22bb7f
Niels Nes <niels@cwi.nl>
2017-05-31 16:50:19 +0200

    adapted test to include part/partsupp keys

--- sql/test/BugTracker-2017/Tests/lateral.Bug-6310.sql
+++ sql/test/BugTracker-2017/Tests/lateral.Bug-6310.sql
@@ -10,6 +10,14 @@ CREATE TABLE PART  ( P_PARTKEY     INTEGER NOT NULL,
                           P_COMMENT     VARCHAR(23) NOT NULL,
                           PRIMARY KEY   (P_PARTKEY) );
 
+CREATE TABLE PARTSUPP ( PS_PARTKEY     INTEGER NOT NULL,
+                             PS_SUPPKEY     INTEGER NOT NULL,
+                             PS_AVAILQTY    INTEGER NOT NULL,
+                             PS_SUPPLYCOST  DECIMAL(15,2)  NOT NULL,
+                             PS_COMMENT     VARCHAR(199) NOT NULL,
+                             PRIMARY KEY    (PS_PARTKEY,PS_SUPPKEY),
+                             FOREIGN KEY (PS_PARTKEY) references PART );
+
 select
   ref_0.message as c0
 from
