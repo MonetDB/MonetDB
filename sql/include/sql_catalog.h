eafc60f3120d55fb63feaceb5784ef146f79938a
Martin van Dinther <martin.van.dinther@monetdbsolutions.com>
2017-06-01 15:25:23 +0200

    Correcting content in sys.function_languages to match the code by adding entries PYTHON2, PYTHON2_MAP, PYTHON3, PYTHON3_MAP and removing unused entries C and J. Also added and corrected comments in sql_catalog.h

--- sql/include/sql_catalog.h
+++ sql/include/sql_catalog.h
@@ -295,13 +295,13 @@ typedef struct sql_arg {
 #define FUNC_LANG_MAL 1 /* create sql external mod.func */
 #define FUNC_LANG_SQL 2 /* create ... sql function/procedure */
 #define FUNC_LANG_R   3 /* create .. language R */
-#define FUNC_LANG_C   4
-#define FUNC_LANG_J   5
+#define FUNC_LANG_C   4 /* create .. language C, Not used/implemented */
+#define FUNC_LANG_J   5 /* create .. language JavaScript, Not used/implemented */
 // this should probably be done in a better way
 #define FUNC_LANG_PY  6 /* create .. language PYTHON */
 #define FUNC_LANG_MAP_PY  7 /* create .. language PYTHON_MAP */
-#define FUNC_LANG_PY2  8 /* create .. language PYTHON */
-#define FUNC_LANG_MAP_PY2  9 /* create .. language PYTHON_MAP */
+#define FUNC_LANG_PY2  8 /* create .. language PYTHON2 */
+#define FUNC_LANG_MAP_PY2  9 /* create .. language PYTHON2_MAP */
 #define FUNC_LANG_PY3  10 /* create .. language PYTHON3 */
 #define FUNC_LANG_MAP_PY3  11 /* create .. language PYTHON3_MAP */
 
