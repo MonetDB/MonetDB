595990e9bf9ab966bc9556d621f8b3368bc3032c
Mark Raasveldt <m.raasveldt@cwi.nl>
2017-06-06 13:17:23 +0200

    Add actual C UDF files for JIT CUDF.

---
+++ sql/backends/monet5/UDF/cudf/cudf.c
@@ -0,0 +1,31 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0.  If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright 1997 - July 2008 CWI, August 2008 - 2017 MonetDB B.V.
+ */
+
+#include "cudf.h"
+
+static str
+CUDFeval(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci, bit grouped);
+
+str CUDFevalStd(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci) {
+	return CUDFeval(cntxt, mb, stk, pci, 0);
+}
+
+str CUDFevalAggr(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci) {
+	return CUDFeval(cntxt, mb, stk, pci, 1);
+}
+
+str CUDFprelude(void *ret) {
+	(void) ret;
+	return MAL_SUCCEED;
+}
+
+
+static str
+CUDFeval(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci, bit grouped) {
+	
+}
