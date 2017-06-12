f3fb956d7ba25647b6b502da92008a4ef6a21b22
Mark Raasveldt <m.raasveldt@cwi.nl>
2017-06-12 12:59:09 +0200

    Add test for automatically freeing stray mallocs.

---
+++ sql/backends/monet5/UDF/capi/Tests/capi05.sql
@@ -0,0 +1,20 @@
+# memory leak test
+# randomly allocate some buffers
+
+
+START TRANSACTION;
+
+CREATE FUNCTION capi05(inp INTEGER) RETURNS INTEGER LANGUAGE C {
+	char* mydata = malloc(1000);
+	result->initialize(result, inp.count);
+	for(size_t i = 0; i < inp.count; i++) {
+		result->data[i] = inp.data[i] * 2;
+	}
+};
+
+CREATE TABLE integers(i INTEGER);
+INSERT INTO integers VALUES (1), (2), (3), (4), (5);
+
+SELECT capi05(i) FROM integers;
+
+ROLLBACK;
