2337be286a74f15d84b8cb8ee2694053ef931591
Sjoerd Mullender <sjoerd@acm.org>
2017-06-13 11:47:43 +0200

    Fix for joining a BAT with candidate list with a dense bat.

--- gdk/gdk_join.c
+++ gdk/gdk_join.c
@@ -832,8 +832,7 @@ mergejoin_void(BAT *r1, BAT *r2, BAT *l, BAT *r, BAT *sl, BAT *sr,
 			lcand += i;
 			cnt -= i;
 			o = l->hseqbase + BATcount(l);
-			i = binsearch_oid(NULL, 0, lcand, 0, cnt - 1, o, 1, 0);
-			cnt -= i;
+			cnt = binsearch_oid(NULL, 0, lcand, 0, cnt - 1, o, 1, 0);
 
 			if (BATextend(r1, cnt) != GDK_SUCCEED)
 				goto bailout;
