def517572ea0c2069afd21e437761162f3b0e2c7
Sjoerd Mullender <sjoerd@acm.org>
2017-08-11 13:56:15 +0200

    Don't produce an error message for an empty line if transaction already failed.

--- sql/backends/monet5/sql_scenario.c
+++ sql/backends/monet5/sql_scenario.c
@@ -344,7 +344,7 @@ handle_error(mvc *m, int pstatus, str msg)
 	} else if( GDKerrbuf && GDKerrbuf[0]){
 		new = GDKstrdup(GDKerrbuf);
 		GDKerrbuf[0] = 0;
-	}else if( *m->errstr){
+	} else if( *m->errstr){
 		new = GDKstrdup(m->errstr);
 		m->errstr[0] = 0;
 	}
@@ -1058,7 +1058,8 @@ SQLparser(Client c)
 				msg = createException(PARSE, "SQLparser", SQLSTATE(42000) "%s", m->errstr);
 			*m->errstr = 0;
 		}
-		msg = handle_error(m, pstatus, msg);
+		if (m->sym)
+			msg = handle_error(m, pstatus, msg);
 		sqlcleanup(m, err);
 		goto finalize;
 	}
