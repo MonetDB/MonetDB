b22bc42ad4554dd57df04d91c38f2997447a2eff
Stefan Manegold <Stefan.Manegold@cwi.nl>
2017-10-23 20:22:07 +0200

    fixed and revised (MAL-)implementation of (sub)covar() & (sub)corr():
    - always produce result of type dbl (nothing else makes sense)
    - use correct types internally to avoid overflows (as much as possible)
    - use avg() instead of sum()/count() to avoid overflows (as much as possible)
      and to properly respect / handle NIL/NULL values
    - correctly handle "mutual" NIL/NULL values, i.e., only consider
      values from either column in case both column values are not NIL/NULL
    - correctly return NIL/NULL as corr() in case at least one stddev() is 0

--- sql/test/BugTracker-2016/Tests/subcorr-missing.Bug-3978.sql
+++ sql/test/BugTracker-2016/Tests/subcorr-missing.Bug-3978.sql
@@ -3,5 +3,5 @@ insert into x values (1,1,1);
 insert into x values (2,2,2);
 insert into x values (2,2,1);
 insert into x values (2,2,2);
-select corr(i,j) from x group by id;
+select id,corr(i,j) from x group by id order by id;
 drop table x;
