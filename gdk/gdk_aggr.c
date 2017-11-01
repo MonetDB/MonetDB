1fefee0dd24f3793dfc4a893e1835d3ef05ec767
Sjoerd Mullender <sjoerd@acm.org>
2017-11-01 11:42:51 +0100

    Add attribution.

--- gdk/gdk_aggr.c
+++ gdk/gdk_aggr.c
@@ -186,6 +186,7 @@ exchange(double *x, double *y)
 	*y = t;
 }
 
+/* this function was adapted from https://bugs.python.org/file10357/msum4.py */
 static BUN
 dofsum(const void *restrict values, oid seqb, BUN start, BUN end,
        void *restrict results, BUN ngrp, int tp1, int tp2,
