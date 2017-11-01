1d76d8b8f75fa79a21a4e3963e0e5c18494be270
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2017-11-01 15:39:15 +0100

    Look for returning values of functions

--- sql/backends/monet5/sql_gencode.c
+++ sql/backends/monet5/sql_gencode.c
@@ -194,7 +194,7 @@ _create_relational_function(mvc *m, const char *mod, const char *name, sql_rel *
 			int varid = 0;
 			char buf[64];
 
-			if (e->type == e_atom) 
+			if (e->type == e_atom)
 				snprintf(buf,64,"A%d",e->flag);
 			else
 				snprintf(buf,64,"A%s",e->name);
@@ -220,11 +220,12 @@ _create_relational_function(mvc *m, const char *mod, const char *name, sql_rel *
 		curBlk->inlineProp = 1;
 	/* optimize the code */
 	SQLaddQueryToCache(c);
-	if( curBlk->inlineProp == 0)
-		SQLoptimizeQuery(c, c->curprg->def);
-	else{
+	if (curBlk->inlineProp == 0 && !c->curprg->def->errors) {
+		c->curprg->def->errors = SQLoptimizeQuery(c, c->curprg->def);
+	} else if(curBlk->inlineProp != 0) {
 		chkProgram(c->usermodule, c->curprg->def);
-		SQLoptimizeFunction(c,c->curprg->def);
+		if(!c->curprg->def->errors)
+			c->curprg->def->errors = SQLoptimizeFunction(c,c->curprg->def);
 	}
 	if (backup)
 		c->curprg = backup;
@@ -491,7 +492,8 @@ _create_relational_remote(mvc *m, const char *mod, const char *name, sql_rel *re
 
 	SQLaddQueryToCache(c);
 	//chkProgram(c->usermodule, c->curprg->def);
-	SQLoptimizeFunction(c, c->curprg->def);
+	if(!c->curprg->def->errors)
+		c->curprg->def->errors = SQLoptimizeFunction(c, c->curprg->def);
 	if (backup)
 		c->curprg = backup;
 	GDKfree(lname);		/* make sure stub is called */
@@ -700,8 +702,8 @@ backend_dumpproc(backend *be, Client c, cq *cq, sql_rel *r)
 	if (cq){
 		SQLaddQueryToCache(c);
 		// optimize this code the 'old' way
-		if ( m->emode == m_prepare || !qc_isaquerytemplate(getFunctionId(getInstrPtr(c->curprg->def,0))) )
-			SQLoptimizeFunction(c,c->curprg->def);
+		if ( (m->emode == m_prepare || !qc_isaquerytemplate(getFunctionId(getInstrPtr(c->curprg->def,0)))) && !c->curprg->def->errors )
+			c->curprg->def->errors = SQLoptimizeFunction(c,c->curprg->def);
 	}
 
 	// restore the context for the wrapper code
@@ -1023,11 +1025,12 @@ backend_create_sql_func(backend *be, sql_func *f, list *restypes, list *ops)
 		curBlk->unsafeProp = 1;
 	/* optimize the code */
 	SQLaddQueryToCache(c);
-	if( curBlk->inlineProp == 0)
-		SQLoptimizeFunction(c, c->curprg->def);
-	else{
+	if( curBlk->inlineProp == 0 && !c->curprg->def->errors) {
+		c->curprg->def->errors = SQLoptimizeFunction(c, c->curprg->def);
+	} else if(curBlk->inlineProp != 0){
 		chkProgram(c->usermodule, c->curprg->def);
-		SQLoptimizeFunction(c,c->curprg->def);
+		if(!c->curprg->def->errors)
+			c->curprg->def->errors = SQLoptimizeFunction(c,c->curprg->def);
 	}
 	if (backup)
 		c->curprg = backup;
