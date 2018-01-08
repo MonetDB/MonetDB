2670186f0f86182b897110bedf72b177d2056337
Sjoerd Mullender <sjoerd@acm.org>
2018-01-08 10:15:49 +0100

    Added test for bug 6508.

---
+++ sql/test/BugTracker-2018/Tests/pyUDF-crash.Bug-6508.sql
@@ -0,0 +1,17 @@
+CREATE FUNCTION test_timestamp_with_nulls(actual_takeoff_datetime timestamp)
+RETURNS STRING LANGUAGE PYTHON {
+        return "it works";
+};
+
+
+CREATE TABLE example (
+    "column1" timestamp
+);
+
+insert into example ("column1") values ('2017-01-01 00:00:01');
+insert into example ("column1") values (NULL);
+
+select test_timestamp_with_nulls("column1") from example;
+
+drop table example;
+drop function test_timestamp_with_nulls(timestamp);
