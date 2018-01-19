6fa94e5aba457e7135e1b3c4294a50f1321c1b04
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2018-01-19 16:59:56 +0100

    Added tests for bugs 6518 and 6519 (enough bugs for some time)

---
+++ sql/test/BugTracker-2018/Tests/sqlitelogictest-algebra-join-not-in.Bug-6519.sql
@@ -0,0 +1,4 @@
+CREATE TABLE tab2(col0 INTEGER, col1 INTEGER, col2 INTEGER);
+INSERT INTO tab2 VALUES (64,77,40), (75,67,58), (46,51,23);
+SELECT * FROM tab2 WHERE col2 NOT IN ( - + col1, col2 * + col1, + - col0 * + + 36, + + col2, 84 * + + col0 ) AND NOT NULL IS NULL;
+DROP TABLE tab2;
