4ed1f371527cf4961d32ca899c0b3fd571022fba
Niels Nes <niels@cwi.nl>
2018-02-17 09:27:41 +0100

    fixes for bug 6526, handle combination of aggregation in a case statement (in the having part).

--- sql/server/rel_select.c
+++ sql/server/rel_select.c
@@ -3835,7 +3835,7 @@ rel_case(mvc *sql, sql_rel **rel, int token, symbol *opt_cond, dlist *when_searc
 					return NULL;
 				cond = rel_binop_(sql, l, r, NULL, "=", card_value);
 			} else {
-				cond = rel_logical_value_exp(sql, rel, when->h->data.sym, sql_sel);
+				cond = rel_logical_value_exp(sql, rel, when->h->data.sym, f);
 			}
 			result = rel_value_exp(sql, rel, when->h->next->data.sym, f, ek);
 		}
@@ -3874,9 +3874,9 @@ rel_case(mvc *sql, sql_rel **rel, int token, symbol *opt_cond, dlist *when_searc
 					return NULL;
 				cond = rel_binop_(sql, l, r, NULL, "=", card_value);
 			} else {
-				cond = rel_logical_value_exp(sql, rel, when->h->data.sym, sql_sel);
+				cond = rel_logical_value_exp(sql, rel, when->h->data.sym, f);
 			}
-			result = rel_value_exp(sql, rel, when->h->next->data.sym, sql_sel, ek);
+			result = rel_value_exp(sql, rel, when->h->next->data.sym, f, ek);
 		}
 		if (!cond || !result) 
 			return NULL;
