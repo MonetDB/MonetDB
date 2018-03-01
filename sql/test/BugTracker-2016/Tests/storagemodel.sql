59ac33abe90d01df9ef5e57d930c0d9e00a39173
Martin van Dinther <martin.van.dinther@monetdbsolutions.com>
2018-03-01 13:20:09 +0100

    Update test to make output stable This reliefs us to create Windows, FreeBSD and Darwin specific outputs

--- sql/test/BugTracker-2016/Tests/storagemodel.sql
+++ sql/test/BugTracker-2016/Tests/storagemodel.sql
@@ -2,7 +2,7 @@
 create table bug3923(i integer);
 
 -- skip columns: location, count, columnsize, sorted.
-select "schema", "table", "column", "type", "mode", case when typewidth <= 64 then typewidth else 99 end as typewidth, hashes, phash, imprints from storage('sys','_tables');
+select "schema", "table", "column", "type", "mode", case when typewidth < 14 then typewidth else 99 end as typewidth, hashes, phash, imprints from storage('sys','_tables');
 
 call storagemodelinit();
 update storagemodelinput set "count" =10000 where "table" ='bug3923';
@@ -18,7 +18,7 @@ set schema bug3923schema;
 create table bug3923(i integer);
 
 -- skip columns: location, count, columnsize, sorted.
-select "schema", "table", "column", "type", "mode", case when typewidth <= 64 then typewidth else 99 end as typewidth, hashes, phash, imprints from sys.storage('sys','_tables');
+select "schema", "table", "column", "type", "mode", case when typewidth < 14 then typewidth else 99 end as typewidth, hashes, phash, imprints from sys.storage('sys','_tables');
 
 call sys.storagemodelinit();
 update sys.storagemodelinput set "count" =10000 where "table" ='bug3923';
