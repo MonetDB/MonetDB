a6acb90a9fca9a94c5341297687a04742fab8581
Joeri van Ruth <joeri.van.ruth@monetdbsolutions.com>
2018-03-06 14:06:10 +0100

    Inline view commented_function_signatures into dump.c

--- sql/scripts/97_comments.sql
+++ sql/scripts/97_comments.sql
@@ -8,23 +8,3 @@
 -- in sys.describe_all_objects and sys.commented_function_signatures defined below.
 CREATE TABLE sys.systemfunctions (function_id INTEGER NOT NULL);
 GRANT SELECT ON sys.systemfunctions TO PUBLIC;
-
-
-CREATE VIEW sys.commented_function_signatures AS
-SELECT f.id AS fid,
-       s.name AS schema,
-       f.name AS fname,
-       sys.function_type_keyword(f.type) AS category,
-       sf.function_id IS NOT NULL AS system,
-       CASE RANK() OVER (PARTITION BY f.id ORDER BY p.number ASC) WHEN 1 THEN f.name ELSE NULL END AS name,
-       CASE RANK() OVER (PARTITION BY f.id ORDER BY p.number DESC) WHEN 1 THEN c.remark ELSE NULL END AS remark,
-       p.type, p.type_digits, p.type_scale,
-       ROW_NUMBER() OVER (ORDER BY f.id, p.number) AS line
-  FROM sys.functions f
-  JOIN sys.comments c ON f.id = c.id
-  JOIN sys.schemas s ON f.schema_id = s.id
-  LEFT OUTER JOIN sys.systemfunctions sf ON f.id = sf.function_id
-  LEFT OUTER JOIN sys.args p ON f.id = p.func_id AND p.inout = 1
- ORDER BY line;
-GRANT SELECT ON sys.commented_function_signatures TO PUBLIC;
-
