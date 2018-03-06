eedb70d7ec243583c0d1e600fd6e5890c746ed8d
Joeri van Ruth <joeri.van.ruth@monetdbsolutions.com>
2018-03-06 14:10:19 +0100

    Move creation of sys.systemfunctions back to 99_system.sql

--- sql/scripts/97_comments.sql
+++ sql/scripts/97_comments.sql
@@ -3,8 +3,3 @@
 -- file, You can obtain one at http://mozilla.org/MPL/2.0/.
 --
 -- Copyright 1997 - July 2008 CWI, August 2008 - 2018 MonetDB B.V.
-
--- This table used to be in 99_system.sql but we need the systemfunctions table
--- in sys.describe_all_objects and sys.commented_function_signatures defined below.
-CREATE TABLE sys.systemfunctions (function_id INTEGER NOT NULL);
-GRANT SELECT ON sys.systemfunctions TO PUBLIC;
