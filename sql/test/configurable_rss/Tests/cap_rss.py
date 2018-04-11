462d4d68b007b147ed0d994273e7b30b43115837
Aris Koning <aris.koning@monetdbsolutions.com>
2018-04-11 12:25:38 +0200

    The cap_rss tests use a single python script and embedded sql template.

--- sql/test/configurable_rss/Tests/cap_rss.py
+++ sql/test/configurable_rss/Tests/cap_rss.py
@@ -4,6 +4,57 @@ try:
 except ImportError:
     import process
 
+class RSSTestConfig:
+
+    # CAUTION: switch implementation without default.
+    test_2_gdk_mem_maxsize = {
+        "cap_rss_64" : 20000000,
+        "cap_rss_32" : 10000000
+    }
+
+    sql_template = \
+"""
+create function getrss()
+returns bigint external name status.rss_cursize;
+
+create table test(a int, b int, c double);
+
+insert into test values (1, 0, 1);
+
+create procedure loop_insert(maximum_size bigint)
+begin
+    declare size bigint;
+    set size = (select count(*) from test);
+
+    while size < maximum_size do
+        insert into test (select a+1, b+2, rand()*c from test);
+
+        set size = (select count(*) from test);
+    end while;
+end;
+
+call loop_insert(1000000);
+
+-- it seems that it requires an analytical query to keep memory in ram.
+select getrss() < {0} as resident_set_size_is_less_then_{0}_kB, quantile(c/a, 0.8) * 0  from test;
+
+drop table test cascade;
+drop function getrss;
+"""
+
+    def __init__(self, test):
+        self.rss_max_in_Bytes = RSSTestConfig.test_2_gdk_mem_maxsize[test]
+
+    def prepare_server_options(self):
+        return ["--set", "gdk_mem_maxsize={}".format(self.rss_max_in_Bytes)]
+
+    def prepare_sql_script(self):
+            return RSSTestConfig.sql_template.format(self.rss_max_in_Bytes / 1000)
+
+def build_test_config():
+        test = sys.argv[1]
+        return RSSTestConfig(test)
+
 def server_start(args):
     sys.stderr.write('#mserver: "%s"\n' % ' '.join(args))
     sys.stderr.flush()
@@ -11,19 +62,22 @@ def server_start(args):
                          stdout = process.PIPE, stderr = process.PIPE)
     return srv
 
-def client(lang, file):
-    sys.stderr.write('#client: "%s"\n' % file)
-    sys.stderr.flush()
-    clt = process.client(lang.lower(), stdin = open(file),
+def client(lang, query):
+    clt = process.client(lang.lower(), stdin = process.PIPE,
                          stdout = process.PIPE, stderr = process.PIPE)
-    return clt.communicate()
+    return clt.communicate(input = query)
 
 def main():
-    srv = server_start(["--set", "gdk_mem_maxsize=20000000"])
 
-    out, err = client('SQL',
-                      os.path.join(os.getenv('RELSRCDIR'),
-                                   'push_and_check_rss.sql'))
+    test_config = build_test_config()
+
+    options = test_config.prepare_server_options()
+
+    srv = server_start(options)
+
+    script = test_config.prepare_sql_script()
+
+    out, err = client('SQL', script)
     sys.stdout.write(out)
     sys.stderr.write(err)
     out, err = srv.communicate()
