807c10cb434ad2dcc801ecb67d47bbe65045fd80
Martin van Dinther <martin.van.dinther@monetdbsolutions.com>
2018-04-19 12:42:51 +0200

    Adding a CAST (  AS INTEGER) to the SELECT query such that the output becomes a bigint (instead of hugeint). This prevents the need to create an additional .int128 output file or limiting the testing of these tests to platforms which HAVE_HGE.

--- sql/test/BugTracker-2018/Tests/sqlitelogictest-not-between-wrong.Bug-6569.sql
+++ sql/test/BugTracker-2018/Tests/sqlitelogictest-not-between-wrong.Bug-6569.sql
@@ -1,4 +1,4 @@
 CREATE TABLE tab1(col0 INTEGER, col1 INTEGER, col2 INTEGER);
 INSERT INTO tab1 VALUES(51,14,96), (85,5,59), (91,47,68);
-SELECT ALL + MIN ( ALL + ( + col0 ) ) + - 9 * - ( + COUNT ( * ) ) AS col0 FROM tab1 AS cor0 WHERE NOT ( - col0 + col0 ) BETWEEN ( - 39 ) AND ( - 92 );
+SELECT ALL + MIN ( ALL + ( + col0 ) ) + - 9 * - ( + CAST ( COUNT ( * ) AS INTEGER) ) AS col0 FROM tab1 AS cor0 WHERE NOT ( - col0 + col0 ) BETWEEN ( - 39 ) AND ( - 92 );
 DROP TABLE tab1;
