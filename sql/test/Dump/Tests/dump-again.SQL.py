28ec1f1511879b6fa0b4c90da86808b1d4eda3b4
Sjoerd Mullender <sjoerd@acm.org>
2018-05-14 22:41:31 +0200

    Added test for dumping using INSERT queries. The output currently required for the INTERVAL types is debatable, but I thought I'd make them as precise as possible.

---
+++ sql/test/Dump/Tests/dump-again.SQL.py
@@ -0,0 +1,16 @@
+import os, sys
+try:
+    from MonetDBtesting import process
+except ImportError:
+    import process
+
+p = process.client('sqldump', args = ['--inserts'], stdout = process.PIPE, stderr = process.PIPE)
+dump, err = p.communicate()
+
+print(os.path.join(os.environ['TSTTRGDIR'], 'dumpoutput2.sql'))
+f = open(os.path.join(os.environ['TSTTRGDIR'], 'dumpoutput2.sql'), 'w')
+f.write(dump)
+f.close()
+
+sys.stdout.write(dump)
+sys.stderr.write(err)
