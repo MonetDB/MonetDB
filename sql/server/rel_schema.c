3c9a0fdb998e6c3e0a17372a6f4a77ddf3a6d84f
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2018-05-18 10:47:33 +0200

    Steps to fix bug 6604. When creating a table inside a sequential block (UDF) check for the rel_insert case when the table is created with data.
    However even in the scenario without data, the table doesn't get created. Another bug :(

--- sql/server/rel_schema.c
+++ sql/server/rel_schema.c
@@ -920,7 +920,7 @@ rel_create_table(mvc *sql, sql_schema *ss, int temp, const char *sname, const ch
 		if (temp != SQL_PERSIST && tt == tt_table) {
 			s = mvc_bind_schema(sql, "tmp");
 			if (temp == SQL_LOCAL_TEMP && sname && strcmp(sname, s->base.name) != 0)
-				return sql_error(sql, 02, SQLSTATE(3F000) "CREATE TABLE: local tempory tables should be stored in the '%s' schema", s->base.name);
+				return sql_error(sql, 02, SQLSTATE(3F000) "CREATE TABLE: local temporary tables should be stored in the '%s' schema", s->base.name);
 		} else if (s == NULL) {
 			s = ss;
 		}
