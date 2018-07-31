6cd16a81cbc7cee0c21ad6ef91fa4a463850ad85
Sjoerd Mullender <sjoerd@acm.org>
2018-07-31 13:20:30 +0200

    Old GCC does have __attribute__ but not __has_attribute().

--- gdk/gdk.h
+++ gdk/gdk.h
@@ -339,9 +339,23 @@
  * attributes completely; if it is known, use it to find out whether
  * specific attributes that we use are known */
 #ifndef __has_attribute
+#ifndef __GNUC__
 #define __has_attribute(attr)	0
 #ifndef __attribute__
 #define __attribute__(attr)	/* empty */
+#endif
+#else
+/* older GCC does have attributes, but not __has_attribute and not all
+ * attributes that we use are known */
+#define __has_attribute__alloc_size__ 1
+#define __has_attribute__cold__ 1
+#define __has_attribute__format__ 1
+#define __has_attribute__malloc__ 1
+#define __has_attribute__noreturn__ 1
+#define __has_attribute__returns_nonnull__ 0
+#define __has_attribute__visibility__ 1
+#define __has_attribute__warn_unused_result__ 1
+#define __has_attribute(attr)	__has_attribute##attr
 #endif
 #endif
 #if !__has_attribute(__warn_unused_result__)
