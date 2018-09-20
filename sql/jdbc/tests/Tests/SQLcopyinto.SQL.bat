94ffadaa9e47d70704b253b7b886e1f871fc8d7e
Martin van Dinther <martin.van.dinther@monetdbsolutions.com>
2018-09-20 18:35:18 +0200

    Correct the new SQLcopyinto.SQL.[bat|sh] files to reference the Test.SQL.[bat|sh] as done with all other tests This eases future maintenance.

--- sql/jdbc/tests/Tests/SQLcopyinto.SQL.bat
+++ sql/jdbc/tests/Tests/SQLcopyinto.SQL.bat
@@ -1,8 +1,1 @@
-@echo off
-
-set URL=jdbc:monetdb://%HOST%:%MAPIPORT%/%TSTDB%?user=monetdb^&password=monetdb%JDBC_EXTRA_ARGS%
-
-prompt # $t $g  
-echo on
-
-java %TST% "%URL%"
+@call "%TSTSRCDIR%\Test.SQL.bat" %*
--- sql/jdbc/tests/Tests/SQLcopyinto.SQL.sh
+++ sql/jdbc/tests/Tests/SQLcopyinto.SQL.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-URL="jdbc:monetdb://${HOST}:${MAPIPORT}/${TSTDB}?user=monetdb&password=monetdb${JDBC_EXTRA_ARGS}"
-
-Mlog -x "java ${TST} \"${URL}\""
+$TSTSRCDIR/Test.SQL.sh $*
