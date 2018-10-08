dbb09454efa57fc11f67571c4f2493c29cfccf96
Aris Koning <aris.koning@monetdbsolutions.com>
2018-10-08 11:17:26 +0200

    Use custom c code to test configurable rss.

---
+++ sql/backends/monet5/config_rss/test_rss.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#include<Psapi.h>
+#include<Windows.h>
+
+#include<mal.h>
+#include<mal_exception.h>
+
+#define MB(x) ((SIZE_T) (x) << 20)
+#define GB(x) ((SIZE_T) (x) << 30)
+
+#define ALLOC_CHUNK_SIZE (MB(10))
+#define MIN_WORKING_SET MB(100)
+#define MAX_WORKING_SET MB(200)
+#define MAX_VIRTUAL_MEMORY MB(400)
+
+#define TEST_FILE "test_file"
+
+mal_export str TESTrestricted_rss(lng* GDK_mem_maxsize /*in bytes*/)
+{
+	// We are going to try to allocate twice the maximum configured rss bound.
+	size_t rss_bound = (size_t) *GDK_mem_maxsize, to_be_allocated = 2 * rss_bound, allocated = 0;
+
+	// A test file is going to be (re)created in the current directory.
+	FILE* const pfile = fopen(TEST_FILE, "w");
+
+	char* committedMemory = NULL;
+
+	while(allocated < to_be_allocated)
+	{
+		if(!(committedMemory = (char*) malloc(ALLOC_CHUNK_SIZE)))
+		{
+			break;
+		}
+
+		// Write something to newly committed memory.
+		for (int i = 0; i < ALLOC_CHUNK_SIZE - 1; i++)
+		{
+			committedMemory[i] = rand()%26+'a';
+		}
+			committedMemory[ALLOC_CHUNK_SIZE] = '\0';
+
+		// Perform some I/O to make sure the OS is actually doing something with these dirty pages.
+
+		fputs(committedMemory, pfile);
+
+		// Query process memory counters from OS.
+		PROCESS_MEMORY_COUNTERS_EX psmemCounters;
+		GetProcessMemoryInfo(GetCurrentProcess(), &psmemCounters, sizeof(PROCESS_MEMORY_COUNTERS_EX));
+		
+		printf("Working Set:\t%lld MB\t", psmemCounters.WorkingSetSize >> 20);
+		printf("Total Memory:\t%lld MB\n", psmemCounters.PrivateUsage >> 20);
+
+		if (psmemCounters.WorkingSetSize > rss_bound) {
+			fclose(pfile);
+			throw(MAL, "test_config_rss.run_test_config_rss", "physical memory allocation violates configured working set size limit" );
+		}
+	}
+
+	fclose(pfile);
+
+	return MAL_SUCCEED;
+}
--- sql/test/configurable_rss/Tests/rss.py
+++ sql/test/configurable_rss/Tests/rss.py
@@ -5,63 +5,25 @@ except ImportError:
     import process
 
 class RSSTestConfig:
-    CAPPED = True
-
-    # CAUTION: switch implementation without default.
-    test_2_gdk_mem_maxsize = {
-        "cap_rss_64" : (25000000,CAPPED),
-        "cap_rss_32" : (30000000,CAPPED),
-        "no_cap_rss_64" : (25000000,not CAPPED),
-        "no_cap_rss_32" : (30000000,not CAPPED)
-    }
 
     sql_template = \
 """
-create function getrss()
-returns bigint external name status.rss_cursize;
+create function test_rss(bound big int)
+returns bigint external name test_config_rss.run_test_config_rss;
 
-create table test(a int, b int, c double);
+select test_rss({gdk_mem_maxsize});
 
-insert into test values (1, 0, 1);
-
-create procedure loop_insert(maximum_size bigint)
-begin
-    declare size bigint;
-    set size = (select count(*) from test);
-
-    while size < maximum_size do
-        insert into test (select a+1, b+2, rand()*c from test);
-
-        set size = (select count(*) from test);
-    end while;
-end;
-
-call loop_insert(1000000);
-
--- it seems that it requires an analytical query to keep memory in ram.
-select quantile(c/a, 0.7) * 0, getrss() as resident_set_size_in_kB, quantile(c/a, 0.8) * 0  from test;
-
-drop table test cascade;
-drop function getrss;
+drop function test_rss;
 """
 
     def __init__(self, test):
-        self.rss_max_in_Bytes, self.is_capped = RSSTestConfig.test_2_gdk_mem_maxsize[test]
+        self.rss_max_in_Bytes = 30000000
 
     def prepare_server_options(self):
         return ["--set", "gdk_mem_maxsize={}".format(self.rss_max_in_Bytes)] if self.is_capped else []
 
-    def get_template_parameters(self):
-
-        compare_sign, compare_string = ("<", "less") if self.is_capped else (">", "bigger")
-
-        return (self.rss_max_in_Bytes / 1000, compare_sign, compare_string)
-
-
     def prepare_sql_script(self):
-            cap_in_kB, compare_sign, compare_string = self.get_template_parameters()
-
-            return RSSTestConfig.sql_template.format(cap_in_kB=cap_in_kB, compare_sign=compare_sign, compare_string=compare_string)
+        return RSSTestConfig.sql_template.format(gdk_mem_maxsize=self.rss_max_in_Bytes)
 
 def build_test_config():
         test = sys.argv[1]
@@ -82,11 +44,8 @@ def client(lang, query):
 def main():
 
     test_config = build_test_config()
-
     options = test_config.prepare_server_options()
-
     srv = server_start(options)
-
     script = test_config.prepare_sql_script()
 
     out, err = client('SQL', script)
