3c5759c42c6d8d4047e75f5b31beb5782f809540
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2018-12-14 17:57:14 +0100

    Testing aggregations with distinct values.

--- sql/test/miscellaneous/Tests/groupby_expressions.sql
+++ sql/test/miscellaneous/Tests/groupby_expressions.sql
@@ -8,25 +8,30 @@ select cast(("aa"+1) + ("bb"+1) as bigint) from "groupings" group by "aa"+1, "bb
 select cast(("aa"+1) + ("bb"+1) as bigint) from "groupings" group by ("aa"+1) + ("bb"+1);
 
 select cast(sum("aa"+1) as bigint) from "groupings" group by "aa"+1;
+select cast(sum(distinct "aa"+1) as bigint) from "groupings" group by "aa"+1;
 select cast(sum(("aa"+1) + ("bb"+2)) as bigint) from "groupings" group by "aa"+1, "bb"+2;
 select cast(sum(("aa"+1) + ("bb"+2)) as bigint) from "groupings" group by ("aa"+1) + ("bb"+2);
 
 select cast("aa"+1 as bigint) from "groupings" group by "aa"+1 having "aa"+1 > 2;
 select cast("aa"+1 as bigint) from "groupings" group by "aa"+1 order by "aa"+1;
 
-create function sumints(a int, b int) returns int begin return a + b; end;
+create function sumints("a" int, "b" int) returns int begin return "a" + "b"; end;
 select count(*) from "groupings" group by sumints("aa","bb");
 select sumints("aa","bb") from "groupings" group by sumints("aa","bb");
 select cast(sumints("aa","bb")*sum("bb") as bigint) from "groupings" group by sumints("aa","bb");
 select cast(sum("bb") as bigint) from "groupings" group by sumints("aa","bb")*sumints("aa",19);
 
-select count(*) from "groupings" group by "aa" > 1;
+select count("aa") from "groupings" group by "aa" > 1;
+select count(distinct "aa") from "groupings" group by "aa" > 1;
+select distinct count(distinct "aa") from "groupings" group by "aa" > 1;
 select "aa" > 1 from "groupings" group by "aa" > 1;
 select count(*) from "groupings" group by case when "aa" > 1 then "aa" else "aa" + 10 end;
 select case when "aa" > 1 then "aa" else "aa" * 4 end from "groupings" group by case when "aa" > 1 then "aa" else "aa" * 4 end;
 
 select cast(sum("aa"+"bb") as bigint) from "groupings" group by "aa"+"bb";
+select cast(sum(distinct "aa"+"bb") as bigint) from "groupings" group by "aa"+"bb";
 select cast(sum("aa"+3452) as bigint) from "groupings" group by "aa"+"bb";
+select cast(sum(distinct "aa"+3452) as bigint) from "groupings" group by "aa"+"bb";
 
 select count(*) from "groupings" having count("aa"-54) > 2;
 select count(*) from "groupings" order by count("bb"+1);
