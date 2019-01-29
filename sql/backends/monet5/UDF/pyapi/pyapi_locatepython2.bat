4131c3cb428304bad55c0955778058b9e6a28fb5
Sjoerd Mullender <sjoerd@acm.org>
2019-01-29 13:08:39 +0100

    Deal with spaces in registry value.

--- sql/backends/monet5/UDF/pyapi/pyapi_locatepython2.bat
+++ sql/backends/monet5/UDF/pyapi/pyapi_locatepython2.bat
@@ -10,7 +10,7 @@ setlocal ENABLEEXTENSIONS
 set KEY_NAME="HKEY_LOCAL_MACHINE\SOFTWARE\Python\PythonCore\2.7\InstallPath"
 set VALUE_NAME=""
  
-FOR /F "usebackq skip=2 tokens=1-3" %%A IN (`REG QUERY %KEY_NAME% /v %VALUE_NAME% 2^>nul`) DO (
+FOR /F "usebackq skip=2 tokens=1,2,*" %%A IN (`REG QUERY %KEY_NAME% /v %VALUE_NAME% 2^>nul`) DO (
     set ValueName=%%A
     set ValueType=%%B
     set ValueValue=%%C
