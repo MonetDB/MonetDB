47d2fe335048ad7eb0724371fe389455447602dd
Martin Kersten <mk@cwi.nl>
2019-03-09 09:13:52 +0100

    Add the experimental oltp pipe

--- monetdb5/optimizer/opt_pipes.c
+++ monetdb5/optimizer/opt_pipes.c
@@ -81,7 +81,6 @@ static struct PIPELINES {
 	 "optimizer.projectionpath();"
 	 "optimizer.deadcode();"
 	 "optimizer.reorder();"
-//	 "optimizer.reduce();" deprecated
 	 "optimizer.matpack();"
 	 "optimizer.dataflow();"
 	 "optimizer.querylog();"
@@ -92,7 +91,43 @@ static struct PIPELINES {
 	 "optimizer.postfix();"
 	 "optimizer.deadcode();"
 //	 "optimizer.jit();" awaiting the new batcalc api
-//	 "optimizer.oltp();"awaiting the autocommit front-end changes
+	 "optimizer.wlc();"
+	 "optimizer.garbageCollector();",
+	 "stable", NULL, NULL, 1},
+/*
+ * Optimistic concurreny control in general leads to more transaction failures 
+ * in an OLTP setting. The partial solution provided is to give out 
+ * advisory locks and delay updates until they are released or timeout.
+ */
+	{"oltp_pipe",
+	 "optimizer.inline();"
+	 "optimizer.remap();"
+	 "optimizer.costModel();"
+	 "optimizer.coercions();"
+	 "optimizer.evaluate();"
+	 "optimizer.emptybind();"
+	 "optimizer.pushselect();"
+	 "optimizer.aliases();"
+	 "optimizer.mitosis();"
+	 "optimizer.mergetable();"
+	 "optimizer.deadcode();"
+	 "optimizer.aliases();"
+	 "optimizer.constants();"
+	 "optimizer.commonTerms();"
+	 "optimizer.projectionpath();"
+	 "optimizer.deadcode();"
+	 "optimizer.reorder();"
+	 "optimizer.matpack();"
+	 "optimizer.dataflow();"
+	 "optimizer.querylog();"
+	 "optimizer.multiplex();"
+	 "optimizer.generator();"
+	 "optimizer.profiler();"
+	 "optimizer.candidates();"
+	 "optimizer.postfix();"
+	 "optimizer.deadcode();"
+//	 "optimizer.jit();" awaiting the new batcalc api
+	 "optimizer.oltp();"
 	 "optimizer.wlc();"
 	 "optimizer.garbageCollector();",
 	 "stable", NULL, NULL, 1},
@@ -117,7 +152,6 @@ static struct PIPELINES {
 	 "optimizer.projectionpath();"
 	 "optimizer.deadcode();"
 	 "optimizer.reorder();"
-//	 "optimizer.reduce();" deprecated
 	 "optimizer.matpack();"
 	 "optimizer.dataflow();"
 	 "optimizer.querylog();"
@@ -129,7 +163,6 @@ static struct PIPELINES {
 	 "optimizer.postfix();"
 	 "optimizer.deadcode();"
 //	 "optimizer.jit();" awaiting the new batcalc api
-//	 "optimizer.oltp();"awaiting the autocommit front-end changes
 	 "optimizer.wlc();"
 	 "optimizer.garbageCollector();",
 	 "stable", NULL, NULL, 1},
@@ -160,7 +193,6 @@ static struct PIPELINES {
 	 "optimizer.projectionpath();"
 	 "optimizer.deadcode();"
 	 "optimizer.reorder();"
-//	 "optimizer.reduce();" deprecated
 	 "optimizer.matpack();"
 	 "optimizer.dataflow();"
 	 "optimizer.querylog();"
@@ -171,7 +203,6 @@ static struct PIPELINES {
 	 "optimizer.postfix();"
 	 "optimizer.deadcode();"
 //	 "optimizer.jit();" awaiting the new batcalc api
-//	 "optimizer.oltp();"awaiting the autocommit front-end changes
 	 "optimizer.wlc();"
 	 "optimizer.garbageCollector();",
 	 "stable", NULL, NULL, 1},
@@ -202,7 +233,6 @@ static struct PIPELINES {
 	 "optimizer.projectionpath();"
 	 "optimizer.deadcode();"
 	 "optimizer.reorder();"
-//	 "optimizer.reduce();" deprecated
 	 "optimizer.matpack();"
 	 "optimizer.querylog();"
 	 "optimizer.multiplex();"
@@ -212,7 +242,6 @@ static struct PIPELINES {
 	 "optimizer.postfix();"
 	 "optimizer.deadcode();"
 //	 "optimizer.jit();" awaiting the new batcalc api
-//	 "optimizer.oltp();"awaiting the autocommit front-end changes
 	 "optimizer.wlc();"
 	 "optimizer.garbageCollector();",
 	 "stable", NULL, NULL, 1},
