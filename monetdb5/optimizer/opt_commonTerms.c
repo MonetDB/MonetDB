8f4d3300d108cee0e0ce5e106653dc9f043aa099
Martin Kersten <mk@cwi.nl>
2019-03-22 11:28:06 +0100

    Be more defensive in keeping instructions around for re-use

--- monetdb5/optimizer/opt_commonTerms.c
+++ monetdb5/optimizer/opt_commonTerms.c
@@ -170,9 +170,11 @@ OPTcommonTermsImplementation(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr
 				i, getArg(p,p->argc-1), HASHinstruction(p), hash[HASHinstruction(p)]);
 		fprintInstruction(stderr, mb, 0, p, LIST_MAL_ALL);
 #endif
-		list[i] = hash[HASHinstruction(p)];
-		hash[HASHinstruction(p)] = i;
-		pushInstruction(mb,p);
+		if ( !mayhaveSideEffects(cntxt, mb, p, TRUE) &&  !isUnsafeFunction(p) && !isUpdateInstruction(p) &&){
+			list[i] = hash[HASHinstruction(p)];
+			hash[HASHinstruction(p)] = i;
+			pushInstruction(mb,p);
+		}
 	}
 	for(; i<slimit; i++)
 		if( old[i])
