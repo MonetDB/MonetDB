d8f62cdfcd6546b3b7c2c2c94c041396a56a89cb
Martin Kersten <mk@cwi.nl>
2019-05-14 04:45:53 +0200

    Deprecate sql.start() and sql.start2().

--- sql/backends/monet5/sql_scenario.c
+++ sql/backends/monet5/sql_scenario.c
@@ -86,47 +86,19 @@ monet5_freecode(int clientid, backend_code code, backend_stack stk, int nr, char
 str
 SQLsession(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci)
 {
-	str msg = MAL_SUCCEED;
-	str logmsg;
-	int cnt=0;
-
 	(void) mb;
 	(void) stk;
 	(void) pci;
-	if (SQLinitialized == 0)// && (msg = SQLprelude(NULL)) != MAL_SUCCEED)
-		return msg;
-	msg = setScenario(cntxt, "sql");
-	// Wait for any recovery process to be finished
-	do {
-		MT_sleep_ms(1000);
-		logmsg = GDKgetenv("recovery");
-		if( logmsg== NULL && ++cnt  == 5)
-			throw(SQL,"SQLinit", "#WARNING server not ready, recovery in progress\n");
-	} while (logmsg == NULL);
-	return msg;
+	throw(SQL,"SQLinit", "#ERROR Deprecated functionality\n");
 }
 
 str
 SQLsession2(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci)
 {
-	str msg = MAL_SUCCEED;
-	str logmsg;
-	int cnt=0;
-
 	(void) mb;
 	(void) stk;
-	(void) pci;
-	if (SQLinitialized == 0)// && (msg = SQLprelude(NULL)) != MAL_SUCCEED)
-		return msg;
-	msg = setScenario(cntxt, "msql");
-	// Wait for any recovery process to be finished
-	do {
-		MT_sleep_ms(1000);
-		logmsg = GDKgetenv("recovery");
-		if( logmsg== NULL && ++cnt  == 5)
-			throw(SQL,"SQLinit","#WARNING server not ready, recovery in progress\n");
-	} while (logmsg == NULL);
-	return msg;
+	(svoid) pci;
+	throw(SQL,"SQLinit", "#ERROR Deprecated functionality\n");
 }
 
 static str SQLinit(Client c);
