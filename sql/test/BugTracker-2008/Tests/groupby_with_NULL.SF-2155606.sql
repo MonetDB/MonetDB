be9ab03178c808f62cecd44e0b1e33cfb7a6d772
Martin van Dinther <martin.van.dinther@monetdbsolutions.com>
2019-05-29 18:55:54 +0200

    Eliminated *.int128 output files (by adding cast(... as bigint))

--- sql/test/BugTracker-2008/Tests/groupby_with_NULL.SF-2155606.sql
+++ sql/test/BugTracker-2008/Tests/groupby_with_NULL.SF-2155606.sql
@@ -3,6 +3,6 @@ create table testola (color varchar(128), count integer);
 insert into testola values ('blue', 12);
 insert into testola values ('red', 2);
 
-select color, null, sum(count) from testola group by color;
+select color, null, cast( sum(count) as bigint) from testola group by color;
 
 drop table testola;
--- sql/test/BugTracker-2009/Tests/case_evaluates_all_branches.SF-2893484.sql
+++ sql/test/BugTracker-2009/Tests/case_evaluates_all_branches.SF-2893484.sql
@@ -12,6 +12,6 @@ insert into stats_2009_week_31 values (0,1),
 
 SELECT CASE SUM(payout_units_cpc) WHEN 0 THEN 0 ELSE 1 END AS avg_cost_cpc FROM stats_2009_week_31; 
 
-SELECT CASE SUM(payout_units_cpc) WHEN 0 THEN 0 ELSE cast(SUM(payout_units_cpc * payout_cpc) as numeric(12,4)) / SUM(payout_units_cpc) END AS avg_cost_cpc FROM stats_2009_week_31;
+SELECT cast( CASE SUM(payout_units_cpc) WHEN 0 THEN 0 ELSE cast(SUM(payout_units_cpc * payout_cpc) as numeric(12,4)) / SUM(payout_units_cpc) END as decimal(18,4)) AS avg_cost_cpc FROM stats_2009_week_31;
 
 drop table stats_2009_week_31;
