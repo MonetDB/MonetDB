fbd0f2569bf0100952ce6eade82870143ddcb7c8
Martin van Dinther <martin.van.dinther@monetdbsolutions.com>
2019-06-05 17:27:04 +0200

    Eliminated *.int128 output files (by adding cast(... as bigint)) Also extended some tests to provide more coverage.

--- sql/test/BugTracker-2011/Tests/crash_on_alias.Bug-2798.sql
+++ sql/test/BugTracker-2011/Tests/crash_on_alias.Bug-2798.sql
@@ -5,11 +5,11 @@ CREATE TABLE dbg (a INT, b INT);
 INSERT INTO dbg (a,b) VALUES (10,10);
 
 -- no alias around the SUM(b), works
-plan SELECT a as d, SUM(b), (2 * (SUM(b) / (SELECT 2))) as f FROM dbg GROUP BY d;
+plan SELECT a as d, cast(SUM(b) as bigint), cast((2 * (SUM(b) / (SELECT 2))) as bigint) as f FROM dbg GROUP BY d;
 set optimizer = 'sequential_pipe';
---explain SELECT a as d, SUM(b), (2 * (SUM(b) / (SELECT 2))) as f FROM dbg GROUP BY d;
+--explain SELECT a as d, cast(SUM(b) as bigint), cast((2 * (SUM(b) / (SELECT 2))) as bigint) as f FROM dbg GROUP BY d;
 set optimizer = 'default_pipe';
-SELECT a as d, SUM(b), (2 * (SUM(b) / (SELECT 2))) as f FROM dbg GROUP BY d;
+SELECT a as d, cast(SUM(b) as bigint), cast((2 * (SUM(b) / (SELECT 2))) as bigint) as f FROM dbg GROUP BY d;
 
 ROLLBACK;
 
@@ -19,11 +19,11 @@ CREATE TABLE dbg (a INT, b INT);
 INSERT INTO dbg (a,b) VALUES (10,10);
 
 -- alias e, crashes :/
-plan SELECT a as d, SUM(b) as e, (2 * (SUM(b) / (SELECT 2))) as f FROM dbg GROUP BY d;
+plan SELECT a as d, cast(SUM(b) as bigint) as e, cast((2 * (SUM(b) / (SELECT 2))) as bigint) as f FROM dbg GROUP BY d;
 set optimizer = 'sequential_pipe';
---explain SELECT a as d, SUM(b) as e, (2 * (SUM(b) / (SELECT 2))) as f FROM dbg GROUP BY d;
+--explain SELECT a as d, cast(SUM(b) as bigint) as e, cast((2 * (SUM(b) / (SELECT 2))) as bigint) as f FROM dbg GROUP BY d;
 set optimizer = 'default_pipe';
-SELECT a as d, SUM(b) as e, (2 * (SUM(b) / (SELECT 2))) as f FROM dbg GROUP
+SELECT a as d, cast(SUM(b) as bigint) as e, cast((2 * (SUM(b) / (SELECT 2))) as bigint) as f FROM dbg GROUP
 BY d;
 
 ROLLBACK;
