25ea9c40f9103c1e1d0dfd586608253fd0e652f2
Martin van Dinther <martin.van.dinther@monetdbsolutions.com>
2019-06-06 13:17:00 +0200

    Eliminated *.int128 output files (by adding cast(... as bigint)) Also extended test: incorrect_result_type.Bug-3687

--- sql/test/BugTracker-2015/Tests/nil_cast.Bug-3787.sql
+++ sql/test/BugTracker-2015/Tests/nil_cast.Bug-3787.sql
@@ -1,4 +1,6 @@
 START TRANSACTION;
 create table foo (a smallint);
-select count(y),sum(z) from (select '',false,a=200 from foo union all select '',null,a=200 from foo) as t(x,y,z) group by x;
+insert into foo values (1), (200);
+select '',false,a=200 from foo union all select '',null,a=200 from foo;
+select count(y),cast(sum(z)as bigint) from (select '',false,a=200 from foo union all select '',null,a=200 from foo) as t(x,y,z) group by x;
 ROLLBACK;
