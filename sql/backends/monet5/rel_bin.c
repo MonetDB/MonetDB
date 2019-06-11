793cbd3c580e0fb3f5e858f1cd0165f093f60183
Niels Nes <niels@cwi.nl>
2019-06-11 14:31:26 +0200

    fixes for  bug 6657

--- sql/backends/monet5/rel_bin.c
+++ sql/backends/monet5/rel_bin.c
@@ -2976,6 +2976,11 @@ rel2bin_select(backend *be, sql_rel *rel, list *refs)
 		if (s->nrcols == 0){
 			if (!predicate && sub)
 				predicate = stmt_const(be, bin_first_column(be, sub), stmt_bool(be, 1));
+			if (e->type != e_cmp) {
+				sql_subtype *bt = sql_bind_localtype("bit");
+
+				s = stmt_convert(be, s, exp_subtype(e), bt);
+			}
 			sel = stmt_uselect(be, predicate, s, cmp_equal, sel, 0);
 		} else if (e->type != e_cmp) {
 			sel = stmt_uselect(be, s, stmt_bool(be, 1), cmp_equal, NULL, 0);
