9bf03219588e7e2a7183ead3aae30aa7a26dfc5f
Aris Koning <aris.koning@monetdbsolutions.com>
2019-06-25 13:34:47 +0200

    Add sql prefix test.

---
+++ sql/test/mosaic/Tests/prefix.sql
@@ -0,0 +1,18 @@
+set optimizer='mosaic_pipe';
+
+create table tmpPrefix( i integer);
+insert into tmpPrefix values (1), (1), (1), (1), (1), (1), (2), (2);
+
+select * from tmpPrefix;
+
+alter table tmpPrefix alter column i set storage 'prefix';
+
+select * from tmpPrefix;
+select count(*) from tmpPrefix;
+
+alter table tmpPrefix alter column i set storage NULL;
+
+select * from tmpPrefix;
+select sum(i) from tmpPrefix;
+
+drop table tmpPrefix;
