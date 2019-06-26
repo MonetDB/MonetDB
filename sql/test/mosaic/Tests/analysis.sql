0b6140fe08bbfd9fe0ce82bfcee6e2b3628e8e04
Aris Koning <aris.koning@monetdbsolutions.com>
2019-06-26 20:32:33 +0200

    Add test for mosaic.analysis.

---
+++ sql/test/mosaic/Tests/analysis.sql
@@ -0,0 +1,27 @@
+create table tmp4 (i int);
+
+insert into tmp4 select * from generate_series(0, 10000000);
+
+-- tmp4 is currently uncompressed
+
+select technique, factor from mosaic.analysis('sys', 'tmp4', 'i') order by factor desc, technique;
+
+select technique, factor from mosaic.analysis('sys', 'tmp4', 'i', 'dictionary, runlength') order by factor desc, technique;
+
+-- should be materialized as the graph of a cutoff function.
+
+insert into tmp4 select 10000000 from tmp4;
+
+select technique, factor from mosaic.analysis('sys', 'tmp4', 'i') order by factor desc, technique;
+
+select technique, factor from mosaic.analysis('sys', 'tmp4', 'i', 'linear, runlength') order by factor desc, technique;
+
+-- Make sure that mosaic.analysis also works correctly on a column with compression
+
+set optimizer='mosaic_pipe';
+
+alter table tmp4 alter column i set storage 'dictionary';
+
+select technique, factor from mosaic.analysis('sys', 'tmp4', 'i', 'linear, runlength') order by factor desc, technique;
+
+drop table tmp4;
