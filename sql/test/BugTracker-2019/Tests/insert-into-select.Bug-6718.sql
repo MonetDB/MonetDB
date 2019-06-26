eaa32bc92d30bcef3fdf081da68d454544cbe3e0
Martin van Dinther <martin.van.dinther@monetdbsolutions.com>
2019-06-26 13:41:29 +0200

    Entended test with showing table descriptions and using CREATE TABLE AS SELECT ... alternative.

--- sql/test/BugTracker-2019/Tests/insert-into-select.Bug-6718.sql
+++ sql/test/BugTracker-2019/Tests/insert-into-select.Bug-6718.sql
@@ -4,6 +4,8 @@ order_id INTEGER NOT NULL,
 qnt INTEGER NOT NULL
 );
 
+\d test2
+
 COPY 5 RECORDS INTO test2 FROM stdin USING DELIMITERS '|','\n';
 1|32|57
 2|15|105
@@ -14,6 +16,7 @@ COPY 5 RECORDS INTO test2 FROM stdin USING DELIMITERS '|','\n';
 
 SELECT * FROM test2;
 
+
 CREATE TABLE test1 (
 id INTEGER NOT NULL,
 qnt INTEGER,
@@ -23,6 +26,8 @@ CONSTRAINT pk_order_number FOREIGN KEY ("order_number") REFERENCES
 test2("order_number")
 );
 
+\d test1
+
 SELECT test2.order_id, 
 test2.qnt, test2.order_number from test2;
 
@@ -31,6 +36,23 @@ test2.qnt, test2.order_number from test2;
 
 SELECT * FROM test1;
 
+
+CREATE TABLE test3 (id, qnt, order_number) AS SELECT test2.order_id, 
+test2.qnt, test2.order_number from test2;
+
+\d test3
+
+SELECT * FROM test3;
+
+ALTER TABLE test3 ALTER id SET NOT NULL;
+ALTER TABLE test3 ALTER order_number SET NOT NULL;
+ALTER TABLE test3 ADD CONSTRAINT pk_test3_id PRIMARY KEY ("id", "qnt");
+ALTER TABLE test3 ADD CONSTRAINT fk_order_number FOREIGN KEY ("order_number") REFERENCES 
+test2("order_number");
+
+\d test3
+
+DROP TABLE test3;
 DROP TABLE test1;
 DROP TABLE test2;
 
