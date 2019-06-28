b616c600740d084b3ce2d135c787514f3ba0d2be
Aris Koning <aris.koning@monetdbsolutions.com>
2019-06-28 16:39:46 +0200

    Update comments.

--- sql/storage/bat/bat_storage.h
+++ sql/storage/bat/bat_storage.h
@@ -17,8 +17,8 @@ typedef struct sql_delta {
 	int bid;
 	oid ibase;		/* ibase: first id of inserts */
 	int ibid;		/* bat with inserts */
-	int uibid;		/* bat with updates */
-	int uvbid;		/* bat with updates */
+	int uibid;		/* bat with positions of updates */
+	int uvbid;		/* bat with values of updates */
 	size_t cnt;		/* number of tuples (excluding the deletes) */
 	size_t ucnt;		/* number of updates */
 	BAT *cached;		/* cached copy, used for schema bats only */
