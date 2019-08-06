4e5c16c073be6b957e58be73cd5f6b26fe521c7d
Sjoerd Mullender <sjoerd@acm.org>
2019-08-06 11:18:12 +0200

    When we have a connection to a server, make sure that server isn't us. The kernel allocates a port to connect from.  If that ports happens to be the port we're trying to connect to, we end up with a connection to ourselves.  We now figure this out and refuse to continue since continuing would lead to deadlock.

--- clients/mapilib/mapi.c
+++ clients/mapilib/mapi.c
@@ -2477,6 +2477,24 @@ mapi_reconnect(Mapi mid)
 			return mapi_setError(mid, errbuf, "mapi_reconnect", MERROR);
 		}
 #endif
+		/* compare our own address with that of our peer and
+		 * if they are the same, we were connected to our own
+		 * socket, so then we can't use this connection */
+		union {
+			struct sockaddr s;
+			struct sockaddr_in i;
+		} myaddr, praddr;
+		socklen_t myaddrlen, praddrlen;
+		myaddrlen = (socklen_t) sizeof(myaddr);
+		praddrlen = (socklen_t) sizeof(praddr);
+		if (getsockname(s, &myaddr.s, &myaddrlen) == 0 &&
+		    getpeername(s, &praddr.s, &praddrlen) == 0 &&
+		    myaddr.i.sin_addr.s_addr == praddr.i.sin_addr.s_addr &&
+		    myaddr.i.sin_port == praddr.i.sin_port) {
+			closesocket(s);
+			return mapi_setError(mid, "connected to self",
+					     "mapi_reconnect", MERROR);
+		}
 	}
 
 	mid->to = socket_wstream(s, "Mapi client write");
