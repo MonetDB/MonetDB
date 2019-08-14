df9394c23d0d66811ac55271a9441a2a90ac42c2
Aris Koning <aris.koning@monetdbsolutions.com>
2019-08-14 09:49:09 +0200

    Typo.

--- monetdb5/modules/mosaic/mosaic_frame.c
+++ monetdb5/modules/mosaic/mosaic_frame.c
@@ -167,7 +167,7 @@ do {\
 				current_bits++;\
 			}\
 			if ( (current_bits >= (int) ((sizeof(TPE) * CHAR_BIT) / 2))\
-				/*TODO: this extra condition should be removed once bitvector is extended to int63's*/\
+				/*TODO: this extra condition should be removed once bitvector is extended to int64's*/\
 				|| (current_bits > (int) sizeof(unsigned int) * CHAR_BIT) ) {\
 				/*If we can from here on not compress better then the half of the original data type, we give up. */\
 				break;\
