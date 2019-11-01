e870259862427851781c23eb18d517f54ce344ef
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2019-11-01 15:29:09 +0100

    Added rel_distinct_aggregate_on_unique_values optimizer where transforms aggr(distinct unique_val) -> aggr(unique_val)

--- sql/test/Tests/keys.sql
+++ sql/test/Tests/keys.sql
@@ -1,3 +1,4 @@
+START TRANSACTION;
 CREATE TABLE allnewtriples (id integer NOT NULL, subject integer NOT
 NULL, predicate integer NOT NULL, "object" integer NOT NULL, explicit
 boolean NOT NULL, CONSTRAINT unique_key UNIQUE(subject, predicate, "object"));
@@ -9,3 +10,18 @@ SELECT idxs.name, idxs."type", keys.name, keys."type"
 FROM idxs LEFT JOIN keys on idxs.name = keys.name;
 SELECT idxs.name, idxs."type", keys.name, keys."type" 
 FROM idxs JOIN keys on idxs.name = keys.name;
+
+/* test elimination of distinct restriction on aggregates */
+create table dummyme (a int primary key, b int);
+insert into dummyme values (1,1), (2,1), (3,1);
+
+/* eliminated */
+plan select count(distinct a) from dummyme;
+plan select count(distinct a) from dummyme group by b;
+
+/* not eliminated */
+plan select count(distinct b) from dummyme;
+plan select count(distinct a + 1) from dummyme;
+plan select count(distinct a + b) from dummyme;
+plan select count(distinct abs(a)) from dummyme;
+ROLLBACK;
