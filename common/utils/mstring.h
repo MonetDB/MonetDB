7fa3b2594e434aca3b32264a06ac1f4f1225a250
Sjoerd Mullender <sjoerd@acm.org>
2019-11-05 15:17:13 +0100

    Turn functions str{cpy,concat}_len into static inline functions. This makes it a log easier to compile (and link) on Windows.

--- common/utils/mstring.h
+++ common/utils/mstring.h
@@ -6,5 +6,43 @@
  * Copyright 1997 - July 2008 CWI, August 2008 - 2019 MonetDB B.V.
  */
 
-size_t strcpy_len(char *restrict dst, const char *restrict src, size_t n);
-size_t strconcat_len(char *restrict dst, size_t n, const char *restrict src, ...);
+
+/* copy at most (n-1) bytes from src to dst and add a terminating NULL
+ * byte; return length of src (i.e. can be more than what is copied) */
+static inline size_t
+strcpy_len(char *restrict dst, const char *restrict src, size_t n)
+{
+	size_t i = 0;
+	if (dst) {
+		while (i < n) {
+			if ((dst[i] = src[i]) == 0)
+				return i;
+			i++;
+		}
+		if (i > 0)
+			dst[i - 1] = 0;
+	}
+	return i + strlen(src + i);
+}
+
+/* copy the NULL terminated list of src strings with a maximum of n
+ * bytes to dst; return the combined length of the src strings */
+static inline size_t
+strconcat_len(char *restrict dst, size_t n, const char *restrict src, ...)
+{
+	va_list ap;
+	size_t i = 0;
+
+	va_start(ap, src);
+	while (src) {
+		size_t l;
+		if (dst && i < n)
+			l = strcpy_len(dst + i, src, n - i);
+		else
+			l = strlen(src);
+		i += l;
+		src = va_arg(ap, const char *);
+	}
+	va_end(ap);
+	return i;
+}
