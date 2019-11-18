620b40b56ba845fb2b8fd2e719aff64173b1f334
Niels Nes <niels@cwi.nl>
2019-11-18 10:18:08 +0100

    moved subquery code into its own file

--- sql/backends/monet5/sql.h
+++ sql/backends/monet5/sql.h
@@ -112,22 +112,6 @@ sql5_export str mvc_bat_get_value(Client cntxt, MalBlkPtr mb, MalStkPtr stk, Ins
 sql5_export str mvc_getVersion(lng *r, const int *clientid);
 sql5_export str mvc_restart_seq(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci);
 sql5_export str mvc_bat_restart_seq(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci);
-sql5_export str zero_or_one_error(ptr ret, const bat *bid, const bit *err );
-sql5_export str zero_or_one_error_bat(ptr ret, const bat *bid, const bat *err );
-sql5_export str zero_or_one(ptr ret, const bat *bid);
-sql5_export str SQLsubzero_or_one(bat *ret, const bat *b, const bat *gp, const bat *gpe, bit *no_nil);
-sql5_export str SQLall(ptr ret, const bat *bid);
-sql5_export str SQLall_grp(bat *ret, const bat *l, const bat *gp, const bat *gpe, bit *no_nil);
-sql5_export str SQLnil(bit *ret, const bat *bid);
-sql5_export str SQLnil_grp(bat *ret, const bat *l, const bat *gp, const bat *gpe, bit *no_nil);
-sql5_export str SQLany_cmp(bit *ret, const bit *cmp, const bit *nl, const bit *nr);
-sql5_export str SQLall_cmp(bit *ret, const bit *cmp, const bit *nl, const bit *nr);
-sql5_export str SQLanyequal(bit *ret, const bat *l, const bat *r);
-sql5_export str SQLanyequal_grp(bat *ret, const bat *l, const bat *r, const bat *gp, const bat *gpe, bit *no_nil);
-sql5_export str SQLanyequal_grp2(bat *ret, const bat *l, const bat *r, const bat *rid, const bat *gp, const bat *gpe, bit *no_nil);
-sql5_export str SQLallnotequal(bit *ret, const bat *l, const bat *r);
-sql5_export str SQLallnotequal_grp(bat *ret, const bat *l, const bat *r, const bat *gp, const bat *gpe, bit *no_nil);
-sql5_export str SQLallnotequal_grp2(bat *ret, const bat *l, const bat *r, const bat *rid, const bat *gp, const bat *gpe, bit *no_nil);
 sql5_export str not_unique(bit *ret, const bat *bid);
 sql5_export str SQLshrink(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci);
 sql5_export str SQLreuse(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci);
@@ -324,13 +308,5 @@ sql5_export str STRstrings(str *res, const str *src);
 sql5_export str BATSTRstrings(bat *res, const bat *src);
 
 sql5_export str SQLflush_log(void *ret);
-
-sql5_export str SQLexist(bit *res, bat *id);
-sql5_export str SQLexist_val(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci);
-sql5_export str SQLsubexist(bat *ret, const bat *b, const bat *gp, const bat *gpe, bit *no_nil);
-
-sql5_export str SQLnot_exist(bit *res, bat *id);
-sql5_export str SQLnot_exist_val(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci);
-sql5_export str SQLsubnot_exist(bat *ret, const bat *b, const bat *gp, const bat *gpe, bit *no_nil);
 
 #endif /* _SQL_H */
