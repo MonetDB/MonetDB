7e2bc74630cbe2eeef7e24463d9eb6e712e4beeb
Thodoris Zois <thodoris.zois@monetdbsolutions.com>
2019-11-24 03:03:51 +0100

    Added GDKtracer to geom.c

--- geom/monetdb5/geom.c
+++ geom/monetdb5/geom.c
@@ -904,7 +904,8 @@ segmentizeLineString(GEOSGeometry **outGeometry, const GEOSGeometry *geosGeometr
 
 		//compute the distance of the current point to the last added one
 		while ((dist = sqrt(pow(xl - xCoords_org[i], 2) + pow(yl - yCoords_org[i], 2) + pow(zl - zCoords_org[i], 2))) > sz) {
-//fprintf(stderr, "OLD : (%f, %f, %f) vs (%f, %f, %f) = %f\n", xl, yl, zl, xCoords_org[i], yCoords_org[i], zCoords_org[i], dist);
+			DEBUG(GEOM, "Old : (%f, %f, %f) vs (%f, %f, %f) = %f\n", xl, yl, zl, xCoords_org[i], yCoords_org[i], zCoords_org[i], dist);
+
 			additionalPoints++;
 			//compute the point
 			xl = xl + (xCoords_org[i] - xl) * sz / dist;
@@ -917,7 +918,9 @@ segmentizeLineString(GEOSGeometry **outGeometry, const GEOSGeometry *geosGeometr
 		zl = zCoords_org[i];
 
 	}
-//fprintf(stderr, "Adding %u\n", additionalPoints);
+
+	DEBUG(GEOM, "Adding %u\n", additionalPoints);
+
 	//create the coordinates sequence for the translated geometry
 	if ((gcs_new = GEOSCoordSeq_create(pointsNum + additionalPoints, coordinatesNum)) == NULL) {
 		*outGeometry = NULL;
@@ -950,7 +953,8 @@ segmentizeLineString(GEOSGeometry **outGeometry, const GEOSGeometry *geosGeometr
 		//compute the distance of the current point to the last added one
 		double dist;
 		while ((dist = sqrt(pow(xl - xCoords_org[i], 2) + pow(yl - yCoords_org[i], 2) + pow(zl - zCoords_org[i], 2))) > sz) {
-//fprintf(stderr, "OLD : (%f, %f, %f) vs (%f, %f, %f) = %f\n", xl, yl, zl, xCoords_org[i], yCoords_org[i], zCoords_org[i], dist);
+			DEBUG(GEOM, "Old: (%f, %f, %f) vs (%f, %f, %f) = %f\n", xl, yl, zl, xCoords_org[i], yCoords_org[i], zCoords_org[i], dist);
+			
 			assert(j < additionalPoints);
 
 			//compute intermediate point
@@ -2388,7 +2392,7 @@ wkbAsBinary(char **toStr, wkb **geomWKB)
 		*s++ = hexit[val];
 		val = (*geomWKB)->data[i] & 0xf;
 		*s++ = hexit[val];
-//fprintf(stderr, "%d First: %c - Second: %c ==> Original %c (%d)\n", i, *(s-2), *(s-1), (*geomWKB)->data[i], (int)((*geomWKB)->data[i]));
+		DEBUG(GEOM, "%d: First: %c - Second: %c ==> Original %c (%d)\n", i, *(s-2), *(s-1), (*geomWKB)->data[i], (int)((*geomWKB)->data[i]));
 	}
 	*s = '\0';
 	return MAL_SUCCEED;
