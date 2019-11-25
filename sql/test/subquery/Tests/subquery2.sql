d83873a8d74ad6e5b5b7ad2450d9aa62633ae5e2
Niels Nes <niels@cwi.nl>
2019-11-25 17:29:41 +0100

    enabled queries

--- sql/test/subquery/Tests/subquery2.sql
+++ sql/test/subquery/Tests/subquery2.sql
@@ -106,15 +106,13 @@ GROUP BY col1;
 -- TODO incorrect empty result
 SELECT NOT col2 <> ANY (SELECT 20 FROM tbl_ProductSales GROUP BY ColID HAVING NOT MAX(col1) <> col1 * AVG(col1 + ColID) * ColID) FROM another_T GROUP BY col1, col2, col5, col8;
 
-/* BROKEN
 SELECT
 	NOT -SUM(col2) NOT IN (SELECT ColID FROM tbl_ProductSales GROUP BY ColID HAVING SUM(ColID - col8) <> col5),
 	NOT col5 = ALL (SELECT 1 FROM tbl_ProductSales HAVING MAX(col8) > 2 AND MIN(col8) IS NOT NULL),
---	NOT col2 <> ANY (SELECT 20 FROM tbl_ProductSales GROUP BY ColID HAVING NOT MAX(col1) <> col1 * AVG(col1 + ColID) * ColID),
+	NOT col2 <> ANY (SELECT 20 FROM tbl_ProductSales GROUP BY ColID HAVING NOT MAX(col1) <> col1 * AVG(col1 + ColID) * ColID),
 	NOT EXISTS (SELECT ColID - 12 FROM tbl_ProductSales GROUP BY ColID HAVING MAX(col2) IS NULL OR NOT col8 <> 2 / col1)
 FROM another_T
 GROUP BY col1, col2, col5, col8;
-*/
 	-- False True True True
 	-- False True True True
 	-- False True True True
@@ -184,7 +182,6 @@ HAVING NOT col1 = ANY (SELECT 0 FROM tbl_ProductSales GROUP BY ColID HAVING NOT 
 	-- 55
 	-- 5555
 
--- TODO incorrect empty result
 SELECT
 	CAST(SUM(col3) * col1 AS BIGINT)
 FROM another_T
@@ -204,7 +201,6 @@ GROUP BY t1.col2;
 	-- 1
 	-- 1
 
--- TODO incorrect empty result
 SELECT
     (SELECT MIN(ColID) FROM tbl_ProductSales INNER JOIN another_T t2 ON t1.col7 <> SOME(SELECT MAX(t1.col1 + t3.col4) FROM another_T t3))
 FROM another_T t1;
@@ -227,14 +223,9 @@ FROM another_T t1;
 	-- NULL
 	-- NULL
 
-/* BROKEN
 SELECT
 	CASE WHEN 1 IN (SELECT (SELECT MAX(col7))) THEN 2 ELSE NULL END
 FROM another_T t1;
-*/
-	-- NULL
-	-- NULL
-	-- NULL
 	-- NULL
 
 SELECT
