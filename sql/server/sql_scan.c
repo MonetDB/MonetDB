2a7262b3646dc2cf00701d31ecf916c5bf5cec64
Panagiotis Koutsourakis <kutsurak@monetdbsolutions.com>
2019-12-02 10:45:47 +0100

    Revert "Add new quotation mode for easier json debugging"
    This has been implemented in the default branch and will be merged from there.
    
    This reverts commit 824d68fa3b8f9b54b0beb516969369d02ee82a24.

--- sql/server/sql_scan.c
+++ sql/server/sql_scan.c
@@ -1172,11 +1172,6 @@ tokenize(mvc * c, int cur)
 			    lc->rs->buf[lc->rs->pos + lc->yycur] == '\'') {
 				return scanner_string(c, scanner_getc(lc), true);
 			}
-			if ((cur == 'J' || cur == 'j') &&
-			    lc->rs->buf[lc->rs->pos + lc->yycur] == '\'') {
-				return scanner_string(c, scanner_getc(lc), false);
-			}
-
 			if ((cur == 'U' || cur == 'u') &&
 			    lc->rs->buf[lc->rs->pos + lc->yycur] == '&' &&
 			    (lc->rs->buf[lc->rs->pos + lc->yycur + 1] == '\'' ||
@@ -1269,7 +1264,7 @@ sql_get_next_token(YYSTYPE *yylval, void *parm)
 	else if (token == STRING) {
 		char quote = *yylval->sval;
 		char *str = sa_alloc( c->sa, (lc->yycur-lc->yysval-2)*2 + 1 );
-		assert(quote == '"' || quote == '\'' || quote == 'E' || quote == 'e' || quote == 'U' || quote == 'u' || quote == 'X' || quote == 'x' || quote == 'J' || quote == 'j');
+		assert(quote == '"' || quote == '\'' || quote == 'E' || quote == 'e' || quote == 'U' || quote == 'u' || quote == 'X' || quote == 'x');
 
 		lc->rs->buf[lc->rs->pos + lc->yycur - 1] = 0;
 		if (quote == '"') {
@@ -1300,14 +1295,6 @@ sql_get_next_token(YYSTYPE *yylval, void *parm)
 			*dst = 0;
 			quote = '\'';
 			token = XSTRING;
-		} else if (quote == 'J' || quote == 'j') {
-			assert(yylval->sval[1] == '\'');
-			char *dst = str;
-			for (char *src = yylval->sval + 2; *src; dst++)
-				if ((*dst = *src++) == '\'' && *src == '\'')
-					src++;
-			quote = '\'';
-			*dst = 0;
 		} else {
 #if 0
 			char *dst = str;
