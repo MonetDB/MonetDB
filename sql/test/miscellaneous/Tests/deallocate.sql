df8d25121ab284a9c955bb00e10e8b1abc3fbd7e
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2019-12-13 17:11:12 +0100

    Updated ChangeLog and tests approval

--- sql/test/miscellaneous/Tests/deallocate.sql
+++ sql/test/miscellaneous/Tests/deallocate.sql
@@ -1,6 +1,11 @@
+deallocate all; --deallocate all the prepared statements from the current directory run
+
 prepare select "system" or ? from _tables WHERE false;
+select "statement" from prepared_statements; --only 1
 EXEC **(false);
+select "statement" from prepared_statements; --only 1
 deallocate **;
+select "statement" from prepared_statements; --empty
 exec **(false); --error, the last prepared statement, no longer exists;
 DEALLOCATE **; --error, last prepared statement already closed
 
@@ -9,5 +14,9 @@ execute **(false);
 DEALLOCATE prepare all;
 EXECUTE **(false); --error, the last prepared statement, no longer exists;
 
+select "statement" from prepared_statements; --empty
+
 DEALLOCATE 100000; --error, it doesn't exist
 DEALLOCATE ALL;
+
+select "statement" from prepared_statements; --empty
