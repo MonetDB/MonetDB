36f527fe6c1039d7bffcc5f850f9fedfa1c46bf6
Martin Kersten <mk@cwi.nl>
2019-12-20 13:52:10 +0100

    Remove the mitosis block experiment code and use the memorylimit only for mal_resource allocation.

--- monetdb5/optimizer/opt_mitosis.c
+++ monetdb5/optimizer/opt_mitosis.c
@@ -124,12 +124,15 @@ OPTmitosisImplementation(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr p)
 	 */
 	activeClients = mb->activeClients = MCactiveClients();
 
+/* This code was used to experiment with block sizes, mis-using the memorylimit  variable 
 	if (cntxt->memorylimit){
-		/* the new mitosis scheme uses a maximum chunck size in MB from the client context */
+		// the new mitosis scheme uses a maximum chunck size in MB from the client context 
 		m = (size_t) ((cntxt->memorylimit * 1024 *1024) / row_size);
 		pieces = (int) (rowcnt / m + (rowcnt - m * pieces > 0));
 	}
 	if (cntxt->memorylimit == 0 || pieces <= 1){
+*/
+	if (pieces <= 1){
 		/* the old allocation scheme */
 		m = GDK_mem_maxsize / argsize;
 		/* if data exceeds memory size,
