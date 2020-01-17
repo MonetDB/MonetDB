c69750ab1a8b97ed16f8297a4b1c408f4a3d631e
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-01-17 12:47:58 +0100

    Removed median, median_avg, quantile and quantile_avg window functions on huge integers

--- sql/scripts/39_analytics_hge.sql
+++ sql/scripts/39_analytics_hge.sql
@@ -43,36 +43,18 @@ GRANT EXECUTE ON WINDOW covar_pop(HUGEINT, HUGEINT) TO PUBLIC;
 create aggregate median(val HUGEINT) returns HUGEINT
 	external name "aggr"."median";
 GRANT EXECUTE ON AGGREGATE median(HUGEINT) TO PUBLIC;
-create window median(val HUGEINT) returns HUGEINT
-	external name "sql"."median";
-GRANT EXECUTE ON WINDOW median(HUGEINT) TO PUBLIC;
 
 create aggregate quantile(val HUGEINT, q DOUBLE) returns HUGEINT
 	external name "aggr"."quantile";
 GRANT EXECUTE ON AGGREGATE quantile(HUGEINT, DOUBLE) TO PUBLIC;
-create window quantile(val HUGEINT, q REAL) returns HUGEINT
-	external name "sql"."quantile";
-GRANT EXECUTE ON WINDOW quantile(HUGEINT, REAL) TO PUBLIC;
-create window quantile(val HUGEINT, q DOUBLE) returns HUGEINT
-	external name "sql"."quantile";
-GRANT EXECUTE ON WINDOW quantile(HUGEINT, DOUBLE) TO PUBLIC;
 
 create aggregate median_avg(val HUGEINT) returns DOUBLE
 	external name "aggr"."median_avg";
 GRANT EXECUTE ON AGGREGATE median_avg(HUGEINT) TO PUBLIC;
-create window median_avg(val HUGEINT) returns HUGEINT
-	external name "sql"."median_avg";
-GRANT EXECUTE ON WINDOW median_avg(HUGEINT) TO PUBLIC;
 
 create aggregate quantile_avg(val HUGEINT, q DOUBLE) returns DOUBLE
 	external name "aggr"."quantile_avg";
 GRANT EXECUTE ON AGGREGATE quantile_avg(HUGEINT, DOUBLE) TO PUBLIC;
-create window quantile_avg(val HUGEINT, q REAL) returns DOUBLE
-	external name "sql"."quantile_avg";
-GRANT EXECUTE ON WINDOW quantile_avg(HUGEINT, REAL) TO PUBLIC;
-create window quantile_avg(val HUGEINT, q DOUBLE) returns DOUBLE
-	external name "sql"."quantile_avg";
-GRANT EXECUTE ON WINDOW quantile_avg(HUGEINT, DOUBLE) TO PUBLIC;
 
 create aggregate corr(e1 HUGEINT, e2 HUGEINT) returns DOUBLE
 	external name "aggr"."corr";
