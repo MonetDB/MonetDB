bef66e260037615625893016f8de52804667eca0
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-01-19 09:08:09 +0100

    Test fixes and defensive lines for mal_client_reset

--- sql/server/rel_schema.c
+++ sql/server/rel_schema.c
@@ -133,7 +133,7 @@ view_rename_columns( mvc *sql, char *name, sql_rel *sq, dlist *column_spec)
 }
 
 static int
-as_subquery(mvc *sql, sql_table *t, sql_rel *sq, dlist *column_spec, const char *msg)
+as_subquery(mvc *sql, sql_table *t, table_types tt, sql_rel *sq, dlist *column_spec, const char *msg)
 {
 	sql_rel *r = sq;
 
@@ -152,7 +152,7 @@ as_subquery(mvc *sql, sql_table *t, sql_rel *sq, dlist *column_spec, const char 
 			sql_exp *e = m->data;
 			sql_subtype *tp = exp_subtype(e);
 
-			if (!isView(t) && cname && cname[0] == '%') {
+			if (tt != tt_view && cname && cname[0] == '%') {
 				sql_error(sql, 01, SQLSTATE(42000) "%s: generated labels not allowed in column names, use an alias instead", msg);
 				return -1;
 			} else if (mvc_bind_column(sql, t, cname)) {
@@ -173,12 +173,13 @@ as_subquery(mvc *sql, sql_table *t, sql_rel *sq, dlist *column_spec, const char 
 			const char *cname = exp_name(e);
 			sql_subtype *tp = exp_subtype(e);
 
-			if (!cname) {
-				cname = "v";
-			} else if (!isView(t) && cname[0] == '%') {
+			if (tt != tt_view && cname && cname[0] == '%') {
 				sql_error(sql, 01, SQLSTATE(42000) "%s: generated labels not allowed in column names, use an alias instead", msg);
 				return -1;
-			} else if (mvc_bind_column(sql, t, cname)) {
+			}
+			if (!cname)
+				cname = "v";
+			if (mvc_bind_column(sql, t, cname)) {
 				sql_error(sql, 01, SQLSTATE(42S21) "%s: duplicate column name %s", msg, cname);
 				return -1;
 			}
@@ -191,13 +192,13 @@ as_subquery(mvc *sql, sql_table *t, sql_rel *sq, dlist *column_spec, const char 
 sql_table *
 mvc_create_table_as_subquery( mvc *sql, sql_rel *sq, sql_schema *s, const char *tname, dlist *column_spec, int temp, int commit_action )
 {
-	int tt =(temp == SQL_REMOTE)?tt_remote:
+	table_types tt =(temp == SQL_REMOTE)?tt_remote:
 		(temp == SQL_STREAM)?tt_stream:
 		(temp == SQL_MERGE_TABLE)?tt_merge_table:
 		(temp == SQL_REPLICA_TABLE)?tt_replica_table:tt_table;
 
 	sql_table *t = mvc_create_table(sql, s, tname, tt, 0, SQL_DECLARED_TABLE, commit_action, -1, 0);
-	if (as_subquery( sql, t, sq, column_spec, "CREATE TABLE") != 0)
+	if (as_subquery(sql, t, tt, sq, column_spec, "CREATE TABLE") != 0)
 		return NULL;
 	return t;
 }
@@ -1223,7 +1224,7 @@ rel_create_view(sql_query *query, sql_schema *ss, dlist *qname, dlist *column_sp
 			q = query_cleaned(q);
 			t = mvc_create_view(sql, s, name, SQL_DECLARED_TABLE, q, 0);
 			GDKfree(q);
-			if (as_subquery(sql, t, sq, column_spec, "CREATE VIEW") != 0) {
+			if (as_subquery(sql, t, tt_view, sq, column_spec, "CREATE VIEW") != 0) {
 				rel_destroy(sq);
 				return NULL;
 			}
