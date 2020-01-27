b2ad1ef0c207361178bb02808799c82aef8a68b7
Aris Koning <aris.koning@monetdbsolutions.com>
2020-01-27 11:01:49 +0100

    Reformat analysis.sql test.

--- sql/test/mosaic/Tests/analysis.sql
+++ sql/test/mosaic/Tests/analysis.sql
@@ -8,15 +8,15 @@ insert into tmp4 select * from generate_series(0, 10000000);
 
 -- tmp4 is currently uncompressed
 
-select technique, factor from mosaic.analysis('sys', 'tmp4', 'i') order by factor desc, technique;
+select technique, factor from mosaic.analysis('sys', 'tmp4', 'i') order by technique, factor desc;
 
 -- should be materialized as the graph of a cutoff function.
 
 insert into tmp4 select 10000000 from tmp4;
 
-select technique, factor from mosaic.analysis('sys', 'tmp4', 'i') order by factor desc, technique;
+select technique, factor from mosaic.analysis('sys', 'tmp4', 'i') order by technique, factor desc;
 
-select technique, factor from mosaic.analysis('sys', 'tmp4', 'i', 'linear, runlength') order by factor desc, technique;
+select technique, factor from mosaic.analysis('sys', 'tmp4', 'i', 'linear, runlength') order by technique, factor desc;
 
 -- Make sure that mosaic.analysis also works correctly on a column with compression
 
@@ -24,9 +24,9 @@ set optimizer='mosaic_pipe';
 
 alter table tmp4 alter column i set storage 'dict';
 
-select technique, factor from mosaic.analysis('sys', 'tmp4', 'i') order by factor desc, technique;
+select technique, factor from mosaic.analysis('sys', 'tmp4', 'i') order by technique, factor desc;
 
-select technique, factor from mosaic.analysis('sys', 'tmp4', 'i', 'linear, runlength') order by factor desc, technique;
+select technique, factor from mosaic.analysis('sys', 'tmp4', 'i', 'linear, runlength') order by technique, factor desc;
 
 drop table tmp4;
 
