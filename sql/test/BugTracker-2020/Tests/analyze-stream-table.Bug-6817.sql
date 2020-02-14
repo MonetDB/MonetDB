7e1013a5a82304b82f00a0097912c4c969fdd8e1
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-02-14 09:21:46 +0100

    Don't use system schemas on tests

--- sql/test/BugTracker-2020/Tests/analyze-stream-table.Bug-6817.sql
+++ sql/test/BugTracker-2020/Tests/analyze-stream-table.Bug-6817.sql
@@ -1,25 +1,30 @@
 delete from statistics;
 select count(*) from statistics;
 
-CREATE STREAM TABLE "sys"."strt" (
+CREATE SCHEMA "ttt";
+
+CREATE TABLE "ttt"."a"("id" INTEGER);
+
+CREATE STREAM TABLE "ttt"."strt" (
         "id" INTEGER       NOT NULL,
         "nm" VARCHAR(123)  NOT NULL,
         CONSTRAINT "strt_id_pkey" PRIMARY KEY ("id")
 );
 
-select * from "sys"."strt";
+select * from "ttt"."strt";
 
-analyze "sys"."strt";
+analyze "ttt"."strt";
 -- Error: Table 'strt' is not persistent   SQLState:  42S02
 select (count(*) > 0) as has_rows from statistics;
 
-analyze sys; --not an error, skip stream table "strt"
+analyze ttt; --not an error, skip stream table "strt"
 select (count(*) > 0) as has_rows from statistics;
 
-drop table "sys"."strt";
+drop table "ttt"."strt";
 -- now run analyze without the stream table
-analyze sys;
+analyze ttt;
 select (count(*) > 0) as has_rows from statistics;
--- true (181)
+-- true
 delete from statistics;
 
+drop schema "ttt" cascade;
