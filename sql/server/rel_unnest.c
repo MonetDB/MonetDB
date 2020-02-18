6159964c438891f3123893c0cd3fde9b2191e0a7
Niels Nes <niels@cwi.nl>
2020-02-18 15:03:28 +0100

    cleanup true=true early, solves later problems in rel_bin rel2bin_semijoin

--- sql/server/rel_unnest.c
+++ sql/server/rel_unnest.c
@@ -2793,6 +2793,7 @@ rel_unnest(mvc *sql, sql_rel *rel)
 	rel = rel_visitor(sql, rel, &rewrite_join2semi);	/* where possible convert anyequal functions into marks */
 	rel = rel_visitor(sql, rel, &rewrite_compare_exp);	/* only allow for e_cmp in selects and  handling */
 	rel = rel_visitor(sql, rel, &rewrite_remove_xp_project);	/* remove crossproducts with project ( project [ atom ] ) [ etc ] */
+	rel = rel_visitor(sql, rel, &rewrite_simplify);		/* as expressions got merged before, lets try to simplify again */
 	rel = _rel_unnest(sql, rel);
 	rel = rel_visitor(sql, rel, &rewrite_fix_count);	/* fix count inside a left join (adds a project (if (cnt IS null) then (0) else (cnt)) */
 	rel = rel_visitor(sql, rel, &rewrite_remove_xp);	/* remove crossproducts with project [ atom ] */
