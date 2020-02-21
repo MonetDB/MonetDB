d1bd95e1fc3c9b2cf614c0628eb88be7494f6317
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-02-21 16:30:36 +0100

    Fixed rename00 test

--- sql/test/rename/Tests/rename00.SQL.py
+++ sql/test/rename/Tests/rename00.SQL.py
@@ -1,9 +1,19 @@
-import sys
-
+import os, socket, sys, tempfile, shutil
 try:
     from MonetDBtesting import process
 except ImportError:
     import process
+
+def freeport():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('', 0))
+    port = sock.getsockname()[1]
+    sock.close()
+    return port
+
+farm_dir = tempfile.mkdtemp()
+os.mkdir(os.path.join(farm_dir, 'db1'))
+myport = freeport()
 
 def server_stop(s):
     out, err = s.communicate()
@@ -11,7 +21,7 @@ def server_stop(s):
     sys.stderr.write(err)
 
 def client(input):
-    c = process.client('sql', stdin=process.PIPE, stdout=process.PIPE, stderr=process.PIPE)
+    c = process.client('sql', port = myport, dbname='db1', stdin = process.PIPE, stdout = process.PIPE, stderr = process.PIPE)
     out, err = c.communicate(input)
     sys.stdout.write(out)
     sys.stderr.write(err)
@@ -30,9 +40,13 @@ select "a" from "newname";\
 drop table "newname";
 '''
 
-s = process.server(args=[], stdin=process.PIPE, stdout=process.PIPE, stderr=process.PIPE)
+s = process.server(mapiport=myport, dbname='db1', dbfarm=os.path.join(farm_dir, 'db1'), stdin = process.PIPE,
+                   stdout = process.PIPE, stderr = process.PIPE)
 client(script1)
 server_stop(s)
-s = process.server(args=[], stdin=process.PIPE, stdout=process.PIPE, stderr=process.PIPE)
+s = process.server(mapiport=myport, dbname='db1', dbfarm=os.path.join(farm_dir, 'db1'), stdin = process.PIPE,
+                   stdout = process.PIPE, stderr = process.PIPE)
 client(script2)
 server_stop(s)
+
+shutil.rmtree(farm_dir)
