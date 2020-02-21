4de346e38143e5dc848b5ab40f08180b40fb465c
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-02-21 14:39:57 +0100

    Added Single server runs for BugTrackers 2011, 2012, 2013 and 2014

---
+++ sql/test/BugTracker-2011/Tests/interrupted-initialization.Bug-2875.SQL.py
@@ -0,0 +1,38 @@
+from __future__ import print_function
+
+import os, socket, sys, tempfile, shutil
+try:
+    from MonetDBtesting import process
+except ImportError:
+    import process
+
+def freeport():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('', 0))
+    port = sock.getsockname()[1]
+    sock.close()
+    return port
+
+farm_dir = tempfile.mkdtemp()
+os.mkdir(os.path.join(farm_dir, 'db1'))
+
+s = process.server(mapiport=freeport(), dbname='db1', dbfarm=os.path.join(farm_dir, 'db1'), stdin = process.PIPE,
+                   stdout = process.PIPE, stderr = process.PIPE)
+out, err = s.communicate()
+sys.stdout.write(out)
+sys.stderr.write(err)
+s = process.server(mapiport=freeport(), dbname='db1', dbfarm=os.path.join(farm_dir, 'db1'), stdin = process.PIPE,
+                   stdout = process.PIPE, stderr = process.PIPE)
+c = process.client(lang = 'sqldump',
+                   stdin = process.PIPE,
+                   stdout = process.PIPE,
+                   stderr = process.PIPE,
+                   server = s)
+out, err = c.communicate()
+sys.stdout.write(out)
+sys.stderr.write(err)
+out, err = s.communicate()
+sys.stdout.write(out)
+sys.stderr.write(err)
+
+shutil.rmtree(farm_dir)
--- sql/test/BugTracker-2013/Tests/corrupt-after-restart.Bug-3282.SQL.py
+++ sql/test/BugTracker-2013/Tests/corrupt-after-restart.Bug-3282.SQL.py
@@ -1,11 +1,21 @@
+import os, socket, sys, tempfile, shutil
 try:
     from MonetDBtesting import process
 except ImportError:
     import process
 
-import sys
+def freeport():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('', 0))
+    port = sock.getsockname()[1]
+    sock.close()
+    return port
 
-s = process.server(stdin = process.PIPE, stdout = process.PIPE, stderr = process.PIPE)
+farm_dir = tempfile.mkdtemp()
+os.mkdir(os.path.join(farm_dir, 'db1'))
+
+s = process.server(mapiport=freeport(), dbname='db1', dbfarm=os.path.join(farm_dir, 'db1'), stdin = process.PIPE,
+                   stdout = process.PIPE, stderr = process.PIPE)
 c = process.client('sql', stdin = process.PIPE, stdout = process.PIPE, stderr = process.PIPE)
 out, err = c.communicate('''start transaction;
 create table table3282 (i int);
@@ -39,7 +49,8 @@ sys.stderr.write(err)
 out, err = s.communicate()
 sys.stdout.write(out)
 sys.stderr.write(err)
-s = process.server(stdin = process.PIPE, stdout = process.PIPE, stderr = process.PIPE)
+s = process.server(mapiport=freeport(), dbname='db1', dbfarm=os.path.join(farm_dir, 'db1'), stdin = process.PIPE,
+                   stdout = process.PIPE, stderr = process.PIPE)
 c = process.client('sql', stdin = process.PIPE, stdout = process.PIPE, stderr = process.PIPE)
 out, err = c.communicate('''select * from table3282 offset 2097140;
 ''')
@@ -48,7 +59,8 @@ sys.stderr.write(err)
 out, err = s.communicate()
 sys.stdout.write(out)
 sys.stderr.write(err)
-s = process.server(stdin = process.PIPE, stdout = process.PIPE, stderr = process.PIPE)
+s = process.server(mapiport=freeport(), dbname='db1', dbfarm=os.path.join(farm_dir, 'db1'), stdin = process.PIPE,
+                   stdout = process.PIPE, stderr = process.PIPE)
 c = process.client('sql', stdin = process.PIPE, stdout = process.PIPE, stderr = process.PIPE)
 out, err = c.communicate('''select * from table3282 offset 2097140;
 drop table table3282;
@@ -58,3 +70,5 @@ sys.stderr.write(err)
 out, err = s.communicate()
 sys.stdout.write(out)
 sys.stderr.write(err)
+
+shutil.rmtree(farm_dir)
