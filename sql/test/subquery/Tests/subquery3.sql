10220eab840ed03c028b3c5aa2abdf0f02dc8291
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-02-26 14:01:29 +0100

    Approved output

--- sql/test/subquery/Tests/subquery3.sql
+++ sql/test/subquery/Tests/subquery3.sql
@@ -427,21 +427,14 @@ SELECT
 	(SELECT outt FROM evilfunction((SELECT t2.col1 FROM another_T t2))) 
 FROM another_T; --error, more than one row returned by a subquery used as an expression
 
-/*SELECT
+SELECT
 	(SELECT outt FROM evilfunction((SELECT MIN(col1)))) 
 FROM another_T;
-	-- 1
-	-- 1
-	-- 1
 	-- 1
 
 SELECT
 	(SELECT outt FROM evilfunction((SELECT MAX(col1) FROM tbl_ProductSales))) 
-FROM another_T; 
-	-- 1111
-	-- 1111
-	-- 1111
-	-- 1111*/
+FROM another_T;  --error, more than one row returned by a subquery used as an expression
 
 SELECT
 	(SELECT outt FROM evilfunction((SELECT MIN(t2.col1) FROM another_T t2))) 
