c2fc9c53168aab040cd4c84ddd136a85cfef386a
Sjoerd Mullender <sjoerd@acm.org>
2020-03-06 12:59:33 +0100

    Some small fixes; new catalog version; upgrade.

--- sql/scripts/17_temporal.sql
+++ sql/scripts/17_temporal.sql
@@ -5,18 +5,18 @@
 -- Copyright 1997 - July 2008 CWI, August 2008 - 2020 MonetDB B.V.
 
 -- assume milliseconds when converted to TIMESTAMP
-create function sys."epoch"(sec BIGINT) returns TIMESTAMP WITH TIME ZONE
-	external name mtime."epoch";
+create function sys.epoch(sec BIGINT) returns TIMESTAMP WITH TIME ZONE
+	external name mtime.epoch;
 
-create function sys."epoch"(sec INT) returns TIMESTAMP WITH TIME ZONE
-	external name mtime."epoch";
+create function sys.epoch(sec INT) returns TIMESTAMP WITH TIME ZONE
+	external name mtime.epoch;
 
-create function sys."epoch"(ts TIMESTAMP WITH TIME ZONE) returns INT
-	external name mtime."epoch";
+create function sys.epoch(ts TIMESTAMP WITH TIME ZONE) returns INT
+	external name mtime.epoch;
 
-grant execute on function sys."epoch" (BIGINT) to public;
-grant execute on function sys."epoch" (INT) to public;
-grant execute on function sys."epoch" (TIMESTAMP WITH TIME ZONE) to public;
+grant execute on function sys.epoch (BIGINT) to public;
+grant execute on function sys.epoch (INT) to public;
+grant execute on function sys.epoch (TIMESTAMP WITH TIME ZONE) to public;
 
 create function sys.date_trunc(txt string, t timestamp)
 returns timestamp
