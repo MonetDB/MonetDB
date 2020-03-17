b29f0a0755f0ea7b7f284b1c01d92d00fc934275
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-03-17 15:07:41 +0100

    Partial fix for bug 6415, ie use second interval for difference between dates. We cannot use month interval because it doesn't fit for the difference

--- sql/test/BugTracker-2016/Tests/date_calculation_and_comparison_wrong_results.Bug-3940.sql
+++ sql/test/BugTracker-2016/Tests/date_calculation_and_comparison_wrong_results.Bug-3940.sql
@@ -3,6 +3,6 @@ INSERT INTO foo VALUES ('2016-01-01', '2016-01-02');
 INSERT INTO foo VALUES ('2016-01-01', '2016-01-01');
 INSERT INTO foo VALUES ('2016-01-01', '2016-01-31');
 
-SELECT (dat2-dat1), (dat2-dat1) < 10 FROM foo;
+SELECT (dat2-dat1), (dat2-dat1) < cast(10*24*3600 as interval second) FROM foo;
 
 DROP TABLE foo;
