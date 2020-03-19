849b9f3c4ef11ba3665493f40ec9602ebfdfede8
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-03-19 16:01:23 +0100

    Related crash

--- sql/test/subquery/Tests/subquery4.sql
+++ sql/test/subquery/Tests/subquery4.sql
@@ -194,6 +194,8 @@ SELECT 1 IN ((SELECT MIN(col2)), (SELECT SUM(col4))) FROM another_t;
 
 DECLARE myvar INT;
 SELECT (SELECT i) INTO myvar FROM integers; --error, one row max
+DECLARE ovar INT;
+SET ovar = (SELECT (SELECT i) FROM integers); --error, one row max
 
 UPDATE another_T SET col1 = MIN(col1); --error, aggregates not allowed in update set clause
 UPDATE another_T SET col2 = 1 WHERE col1 = SUM(col2); --error, aggregates not allowed in update set clause
