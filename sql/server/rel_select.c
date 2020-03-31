5e9e210e66543ccd2410222931fd7aacd936b846
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-03-31 14:45:58 +0200

    Some changes.
    Allow binding of outer query colums at more possible locations of a subquery.
    Corrected identations.
    We have to fix unnesting of VALUES clauses.
    Added queries with with wrong results.

--- sql/server/rel_select.c
+++ sql/server/rel_select.c
@@ -1173,21 +1173,18 @@ rel_column_ref(sql_query *query, sql_rel **rel, symbol *column_r, int f)
 			exp = rel_bind_column(sql, inner, name, f, 0);
 		if (!exp && inner && is_sql_having(f) && inner->op == op_select)
 			inner = inner->l;
-		if (!exp && inner && (is_sql_having(f) || is_sql_aggr(f)) && is_groupby(inner->op)) {
+		if (!exp && inner && (is_sql_having(f) || is_sql_aggr(f)) && is_groupby(inner->op))
 			exp = rel_bind_column(sql, inner->l, name, f, 0);
-		}
 		if (!exp && query && query_has_outer(query)) {
 			int i;
 			sql_rel *outer;
 
 			for (i=query_has_outer(query)-1; i>= 0 && !exp && (outer = query_fetch_outer(query,i)); i--) {
 				exp = rel_bind_column(sql, outer, name, f, 0);
-				if (!exp && (is_sql_having(f) || is_sql_aggr(f)) && is_groupby(outer->op)) {
+				if (!exp && is_groupby(outer->op))
 					exp = rel_bind_column(sql, outer->l, name, f, 0);
-				}
-				if (exp && is_simple_project(outer->op) && !rel_find_exp(outer, exp)) {
+				if (exp && is_simple_project(outer->op) && !rel_find_exp(outer, exp))
 					exp = rel_project_add_exp(sql, outer, exp);
-				}
 				if (exp)
 					break;
 			}
@@ -1196,7 +1193,7 @@ rel_column_ref(sql_query *query, sql_rel **rel, symbol *column_r, int f)
 			if (exp && outer && (is_sql_groupby(f) || is_sql_aggr(f))) {
 				if (query_outer_used_exp( query, i, exp, is_sql_aggr(f) && !is_sql_farg(f))) {
 					sql_exp *lu = query_outer_last_used(query, i);
-					return sql_error(sql, 05, SQLSTATE(42000) "SELECT: subquery uses ungrouped column \"%s.%s\" from outer query", exp_relname(lu), exp_name(lu));
+					return sql_error(sql, ERR_GROUPBY, SQLSTATE(42000) "SELECT: subquery uses ungrouped column \"%s.%s\" from outer query", exp_relname(lu), exp_name(lu));
 				}
 			}
 			if (exp) { 
