01f2f85a1f778e2cc002d9cfe39c93947fe42c65
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-04-02 09:06:26 +0200

    Added testing reads and writes into the global temp table on test

--- sql/test/BugTracker-2020/Tests/global_table_propagation.Bug-6846.py
+++ sql/test/BugTracker-2020/Tests/global_table_propagation.Bug-6846.py
@@ -1,4 +1,4 @@
-import os, pymonetdb
+import sys, os, pymonetdb
 
 port = int(os.environ['MAPIPORT'])
 db = os.environ['TSTDB']
@@ -9,10 +9,24 @@ cursor1 = client1.cursor()
 cursor2 = client2.cursor()
 
 cursor1.execute('CREATE GLOBAL TEMPORARY TABLE close_d (qaid int, value_ float) ON COMMIT PRESERVE ROWS;')
+
 cursor1.execute('INSERT INTO tmp.close_d VALUES (1,1);')
 cursor2.execute('SELECT qaid, value_ FROM tmp.close_d;')
+print(cursor2.fetchall())
 cursor1.execute('SELECT qaid, value_ FROM tmp.close_d;')
+print(cursor1.fetchall())
+
+cursor2.execute('INSERT INTO tmp.close_d VALUES (2,2);')
+cursor2.execute('SELECT qaid, value_ FROM tmp.close_d;')
+print(cursor2.fetchall())
+cursor1.execute('SELECT qaid, value_ FROM tmp.close_d;')
+print(cursor1.fetchall())
+
 cursor1.execute('DROP TABLE tmp.close_d;')
+try:
+    cursor2.execute('SELECT qaid, value_ FROM tmp.close_d;')  # error, tmp.close_d doesn't exist anymore
+except pymonetdb.OperationalError as e:
+    sys.stderr.write(str(e))
 
 cursor1.close()
 cursor2.close()
