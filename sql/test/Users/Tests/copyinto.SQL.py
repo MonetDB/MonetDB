7c90e950c6ec72d6647ea85c990ad2b891e25022
Sjoerd Mullender <sjoerd@acm.org>
2020-04-09 15:40:20 +0200

    Fix tests to only use r or e prefix strings when \s are involved. The tests now work equally well with the MonetDB and SQL way of parsing single quoted strings without e or r prefix.

--- sql/test/Users/Tests/copyinto.SQL.py
+++ sql/test/Users/Tests/copyinto.SQL.py
@@ -10,7 +10,7 @@ DATADIR = os.path.join(SRCDIR,"SF-0.01")
 
 with process.client('sql', stdin = process.PIPE, stdout = process.PIPE, stderr = process.PIPE) as c:
     c.stdin.write("CREATE TABLE REGION ( R_REGIONKEY INTEGER NOT NULL, R_NAME CHAR(25) NOT NULL, R_COMMENT VARCHAR(152));\n")
-    c.stdin.write("COPY 5 RECORDS INTO region from '%s' USING DELIMITERS '|', '|\\n';\n" % os.path.join(DATADIR, 'region.tbl').replace('\\', r'\\'))
+    c.stdin.write("COPY 5 RECORDS INTO region from r'%s' USING DELIMITERS '|', E'|\\n';\n" % os.path.join(DATADIR, 'region.tbl'))
     c.stdin.write("select count(*) from region;\n")
     c.stdin.write("CREATE USER copyuser WITH PASSWORD 'copyuser' name 'copyuser' schema sys;\n")
     c.stdin.write("GRANT INSERT, SELECT on region to copyuser;\n")
@@ -19,7 +19,7 @@ with process.client('sql', stdin = process.PIPE, stdout = process.PIPE, stderr =
     sys.stderr.write(err.replace(DATADIR, '$DATADIR').replace(DATADIR.replace('\\', r'\\'), '$DATADIR').replace(r'DIR\\', 'DIR/').replace('DIR\\', 'DIR/'))
 
 with process.client('sql', user = 'copyuser', passwd = 'copyuser', stdin = process.PIPE, stdout = process.PIPE, stderr = process.PIPE) as c:
-    c.stdin.write("COPY 5 RECORDS INTO region from '%s' USING DELIMITERS '|', '|\\n';\n" % os.path.join(DATADIR, 'region.tbl').replace('\\', r'\\'))
+    c.stdin.write("COPY 5 RECORDS INTO region from r'%s' USING DELIMITERS '|', E'|\\n';\n" % os.path.join(DATADIR, 'region.tbl'))
     c.stdin.write("select count(*) from region;\n")
     out, err = c.communicate()
     sys.stdout.write(out)
@@ -32,7 +32,7 @@ with process.client('sql', stdin = process.PIPE, stdout = process.PIPE, stderr =
     sys.stderr.write(err.replace(DATADIR, '$DATADIR').replace(DATADIR.replace('\\', r'\\'), '$DATADIR').replace(r'DIR\\', 'DIR/').replace('DIR\\', 'DIR/'))
 
 with process.client('sql', user = 'copyuser', passwd = 'copyuser', stdin = process.PIPE, stdout = process.PIPE, stderr = process.PIPE) as c:
-    c.stdin.write("COPY 5 RECORDS INTO region from '%s' USING DELIMITERS '|', '|\\n';\n" % os.path.join(DATADIR, 'region.tbl').replace('\\', r'\\'))
+    c.stdin.write("COPY 5 RECORDS INTO region from r'%s' USING DELIMITERS '|', E'|\\n';\n" % os.path.join(DATADIR, 'region.tbl'))
     c.stdin.write("select count(*) from region;\n")
     out, err = c.communicate()
     sys.stdout.write(out)
