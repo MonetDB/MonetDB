83d360ba9b4b3d45bc651ab02042c10d844469ec
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-04-14 17:30:01 +0200

    New subquery crash :( The any/all rewriter, still doesn't take into account some type of joins

--- sql/test/subquery/Tests/subquery5.sql
+++ sql/test/subquery/Tests/subquery5.sql
@@ -139,6 +139,20 @@ SELECT (VALUES(col1, col2)) FROM another_t; --error, subquery must return only o
 
 SELECT (VALUES(col1), (col2)) FROM another_t; --error, more than one row returned by a subquery used as an expression
 
+SELECT * FROM integers i1 LEFT OUTER JOIN integers i2 ON i2.i = ANY(SELECT SUM(i2.i + i3.i) FROM integers i3) = NOT EXISTS(SELECT MIN(i1.i) OVER ());
+	-- 1 3
+	-- 1 2
+	-- 1 1
+	-- 2 3
+	-- 2 2
+	-- 2 1
+	-- 3 3
+	-- 3 2
+	-- 3 1
+	-- NULL 3
+	-- NULL 2
+	-- NULL 1
+
 DROP FUNCTION evilfunction(INT);
 DROP TABLE tbl_ProductSales;
 DROP TABLE another_T;
