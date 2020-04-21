efddc2cfcf2a714aeaac58acb485796368c39432
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-04-21 11:48:13 +0200

    Approved test

--- monetdb5/extras/mal_optimizer_template/Tests/opt_sql_append.sql
+++ monetdb5/extras/mal_optimizer_template/Tests/opt_sql_append.sql
@@ -8,16 +8,17 @@ explain copy into ttt from '/tmp/xyz';
 explain copy into ttt from E'\\tmp/xyz';
 explain copy into ttt from E'a:\\tmp/xyz';
 
-declare opt_pipe_name string;
-set opt_pipe_name = ( select optimizer );
+start transaction;
+create local temp table "opt_pipe_name" ("opt_pipe_name" string);
+insert into "opt_pipe_name" values ((select optimizer));
 
-declare opt_pipe_def  string;
-set opt_pipe_def  = ( select def from optimizers() where name = opt_pipe_name );
-
-set optimizer = substring(opt_pipe_def,0,length(opt_pipe_def)-length('optimizer.garbageCollector();')) || 'optimizer.sql_append();optimizer.garbageCollector();';
+set optimizer = substring((select def from optimizers() where name = (select opt_pipe_name from "opt_pipe_name")),0,
+                length((select def from optimizers() where name = (select opt_pipe_name from "opt_pipe_name")))-length('optimizer.garbageCollector();')) || 'optimizer.sql_append();optimizer.garbageCollector();';
 select optimizer;
 
 select def from optimizers() where name = optimizer;
+rollback;
+
 explain copy into ttt from '/tmp/xyz';
 explain copy into ttt from E'\\tmp/xyz';
 explain copy into ttt from 'Z:/tmp/xyz';
