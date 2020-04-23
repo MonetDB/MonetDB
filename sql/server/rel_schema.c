0db48ddf10629c8be9a196d35facbb8967e82f9e
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-04-23 09:13:28 +0200

    Removed assertion, actually, limit and sample are both allowed at the same time on a query

--- sql/server/rel_schema.c
+++ sql/server/rel_schema.c
@@ -1179,7 +1179,6 @@ rel_create_view(sql_query *query, sql_schema *ss, dlist *qname, dlist *column_sp
 		if (ast->token == SQL_SELECT) {
 			SelectNode *sn = (SelectNode *) ast;
 
-			assert(!sn->limit || !sn->sample);
 			if (sn->limit || sn->sample)
 				return sql_error(sql, 01, SQLSTATE(42000) "%s VIEW: %s not supported", base, sn->limit ? "LIMIT" : "SAMPLE");
 		}
