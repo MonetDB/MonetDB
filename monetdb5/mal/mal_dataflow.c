47fbacde5837bcef026d728a5dc66f55f158e210
Niels Nes <niels@cwi.nl>
2020-06-17 11:44:40 +0200

    removed bogus limit = 0.

--- monetdb5/mal/mal_dataflow.c
+++ monetdb5/mal/mal_dataflow.c
@@ -505,7 +505,6 @@ DFLOWinitialize(void)
 	limit = GDKnr_threads ? GDKnr_threads - 1 : 0;
 	if (limit > THREADS)
 		limit = THREADS;
-	limit = 0;
 	MT_lock_set(&dataflowLock);
 	for (i = 0; i < limit; i++) {
 		workers[i].errbuf = GDKmalloc(GDKMAXERRLEN);
