0c5864d555e03efb6a04013c499cae42dd21d729
Panagiotis Koutsourakis <kutsurak@monetdbsolutions.com>
2020-06-22 14:24:05 +0300

    Handle empty strings correctly in JSONkeyArray
    This fixes Bug 6858

--- monetdb5/modules/atoms/json.c
+++ monetdb5/modules/atoms/json.c
@@ -1518,6 +1518,13 @@ JSONkeyArray(json *ret, json *js)
 			}
 			if (jt->elm[i].valuelen)
 				strncpy(r, jt->elm[i].value - 1, jt->elm[i].valuelen + 2);
+			else {
+				r = GDKstrdup("\"\"");
+				if(r == NULL) {
+					JSONfree(jt);
+					goto memfail;
+				}
+			}
 			result = JSONglue(result, r, ',');
 			if (result == NULL) {
 				JSONfree(jt);
