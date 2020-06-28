ca9eed685218b16e4de3ce8ec4ea42c173bb53be
Arjen de Rijke <arjen.de.rijke@cwi.nl>
2020-06-28 10:06:37 +0200

    fix almost all bugs in the cmake code to generate rpm packages

--- ctest/cmake/detect-curl.cmake
+++ ctest/cmake/detect-curl.cmake
@@ -53,7 +53,7 @@ elseif(${LINUX_DISTRO} STREQUAL "fedora")
   endif()
   if(${LINUX_DISTRO_VERSION} STREQUAL "32")
     assert_package_detected(
-      detect FALSE
+      detect TRUE
       legacyvariable HAVE_CURL
       variablename CURL_FOUND)
   endif()
