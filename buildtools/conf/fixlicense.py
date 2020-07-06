655e7890971b2565510579dbe23d097ae6d76860
Sjoerd Mullender <sjoerd@acm.org>
2020-07-06 11:00:13 +0200

    Add/fix license text.

--- buildtools/conf/fixlicense.py
+++ buildtools/conf/fixlicense.py
@@ -177,7 +177,11 @@ def addlicense(file, pre = None, post = None, start = None, end = None, verbose 
     except IOError:
         print('Cannot create temp file %s.new' % file, file=sys.stderr)
         return
-    data = f.read()
+    try:
+        data = f.read()
+    except UnicodeError:
+        print('UnicodeError in file %s' % file, file=sys.stderr)
+        return
     if PERL_COPYRIGHT in data:
         notice = PERL_COPYRIGHT
     elif COPYRIGHT_NOTICE in data:
@@ -282,7 +286,11 @@ def dellicense(file, pre = None, post = None, start = None, end = None, verbose 
     except IOError:
         print('Cannot create temp file %s.new' % file, file=sys.stderr)
         return
-    data = f.read()
+    try:
+        data = f.read()
+    except UnicodeError:
+        print('UnicodeError in file %s' % file, file=sys.stderr)
+        return
     if PERL_COPYRIGHT in data:
         notice = PERL_COPYRIGHT
     elif COPYRIGHT_NOTICE in data:
