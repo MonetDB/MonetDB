f6711bb141ff4266c41c43c5c456558d230433b7
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-07-07 10:36:33 +0200

    exp_propagate is buggy somehow, use exp_setname to keep the expression's original names

--- sql/test/mergetables/Tests/sqlsmith-exists2.sql
+++ sql/test/mergetables/Tests/sqlsmith-exists2.sql
@@ -519,4 +519,39 @@ where (case when exists (
  and (subq_0.c3 is null);
   -- empty
 
+WITH ss_items AS
+  (SELECT 1 item_id,
+          1 ss_item_rev
+   FROM another_T,
+        LongTable
+   WHERE another_T.col1 = LongTable.col1),
+     cs_items AS
+  (SELECT 1 item_id,
+          1 cs_item_rev
+   FROM tab0,
+        LongTable
+   WHERE tab0.col1 = LongTable.col1),
+     ws_items AS
+  (SELECT 1 item_id,
+          1 ws_item_rev
+   FROM tbl_ProductSales,
+        LongTable
+   WHERE tbl_ProductSales.col1 = LongTable.col1)
+SELECT ss_items.item_id,
+       ss_item_rev,
+       ss_item_rev/((ss_item_rev+cs_item_rev+ws_item_rev)/3) * 100 ss_dev,
+       cs_item_rev,
+       cs_item_rev/((ss_item_rev+cs_item_rev+ws_item_rev)/3) * 100 cs_dev,
+       ws_item_rev,
+       ws_item_rev/((ss_item_rev+cs_item_rev+ws_item_rev)/3) * 100 ws_dev,
+       (ss_item_rev+cs_item_rev+ws_item_rev)/3 average
+FROM ss_items,
+     cs_items,
+     ws_items
+WHERE ss_items.item_id=cs_items.item_id
+  AND ss_items.item_id=ws_items.item_id
+ORDER BY ss_items.item_id,
+         ss_item_rev
+LIMIT 100;
+
 ROLLBACK;
