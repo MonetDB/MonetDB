3d1a30a0477fec49a8c2862e3b63e821a08dd311
Ying Zhang <y.zhang@cwi.nl>
2020-08-10 17:18:26 +0200

    Make the query output a bit more deterministic

--- sql/test/sysmon/Tests/sys_queue_expand.SQL.py
+++ sql/test/sysmon/Tests/sys_queue_expand.SQL.py
@@ -25,7 +25,7 @@ try:
     #mstdbh = pymonetdb.connect(database = 'demo', autocommit=True)
     mstcur = mstdbh.cursor()
 
-    rowcnt = mstcur.execute('select \'before\', username,status,query from sys.queue()')
+    rowcnt = mstcur.execute('select \'before\', username,status,query from sys.queue() order by status, query')
     print("Before sleep: {no}".format(no=rowcnt))
     [print(row) for row in mstcur.fetchall()]
 
@@ -35,14 +35,14 @@ try:
     [p.start() for p in jobs]
 
     time.sleep(1)
-    rowcnt = mstcur.execute('select \'during\', username,status,query from sys.queue()')
+    rowcnt = mstcur.execute('select \'during\', username,status,query from sys.queue() order by status, query')
     print("\nDuring sleep: {no}".format(no=rowcnt))
     [print(row) for row in mstcur.fetchall()]
 
     # Exit the completed processes
     [p.join() for p in jobs]
 
-    rowcnt = mstcur.execute('select \'after\', username,status,query from sys.queue()')
+    rowcnt = mstcur.execute('select \'after\', username,status,query from sys.queue() order by status, query')
     print("\nAfter sleep: {no}".format(no=rowcnt))
     [print(row) for row in mstcur.fetchall()]
 
