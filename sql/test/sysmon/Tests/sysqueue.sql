db49fbe2acf628a27ae8a75780d7151892c7e688
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-08-24 15:50:26 +0100

    Small hack to make sure sysqueue tests are timezone agnostic

--- sql/test/sysmon/Tests/sysqueue.sql
+++ sql/test/sysmon/Tests/sysqueue.sql
@@ -1,5 +1,6 @@
 -- check if we have a increasing queue table
-select username, status, query from sys.queue();
+-- the case expression is used to make sure the test is timezone agnostic
+select username, status, case when query ilike 'set time%' then 'set time...' else query end from sys.queue();
 select count(*) from sys.queue();
 select count(*) from sys.queue();
 select count(*) from sys.queue();
