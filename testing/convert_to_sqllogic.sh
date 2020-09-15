4f0fe1c6c13de420f30f285dc8cb330b6b0f48df
svetlin <svetlin.stalinov@monetdbsolutions.com>
2020-09-15 17:09:30 +0200

    start convert to sqllogic tests script

---
+++ testing/convert_to_sqllogic.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0.  If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright 1997 - July 2008 CWI, August 2008 - 2020 MonetDB B.V.
+set -e 
+
+usage() {
+echo "Usage $0 --src-dir <source dir> --dst-dir <destination directory>"
+echo "Converts old test sql scripts to sqllogic scripts."
+echo "Options:"
+echo " -s|--src-dir <source dir>          directory with old sql tests."
+echo " -t|--dst-dir <destination dir>     destination dotrectory for *.test sqllogic tests."
+echo " -d|--dry-run                    dry run"
+echo
+}
+
+src=
+dst=
+dry_run=
+
+for arg in "$@"
+do
+    case $arg in
+        -s|--src-dir)
+            src=$(pwd)/$2
+            dst=$src
+            shift
+            shift
+            ;;
+        -t|--dest-dir)
+            dst=$2
+            shift
+            shift
+            ;;
+        -d|--dry-run)
+            dry_run="true"
+            shift
+            ;;
+    esac
+done
+
+if [[ ! -d "${src}" ]];then
+    echo "ERROR: source directory required!";
+    usage;
+    exit 1;
+fi
+
+[[ ! -d "${dst}" ]] || mkdir -p ${dst}
+
+dryrun() {
+    local f=$1;
+    cat $f | mktest.py --database "test";
+}
+
+work() {
+    local f=$1;
+    local dst=$2;
+    cat $f | mktest.py --database "test" > $dst;
+}
+
+for f in $(ls ${src}/*.sql);do
+    echo ">>> converting $f ..."
+    if [[ "${dry_run}" = true ]];then
+        dryrun $f;
+    else
+        bn=$(basename $f .sql)
+        work $f $dst/$bn.test
+    fi
+done;
+
+for f in $(ls ${src}/*.stable.out);do
+    bn=$(basename $f)
+    touch $dst/$bn;
+done;
+for f in $(ls ${src}/*.stable.err);do
+    bn=$(basename $f)
+    touch $dst/$bn;
+done;
+if [[ -e ${src}/All ]];then
+    cp ${src}/All $dst;
+fi
+echo "Done"
