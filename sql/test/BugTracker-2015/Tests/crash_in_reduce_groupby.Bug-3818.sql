d6fa995aec6c6147fe0d291a8d3613ca3a8e108b
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-09-24 09:02:30 +0200

    Use aggregate with common type output on int64 vs int128 platforms. Also added one more duplicate column elimination regression test

--- sql/test/BugTracker-2015/Tests/crash_in_reduce_groupby.Bug-3818.sql
+++ sql/test/BugTracker-2015/Tests/crash_in_reduce_groupby.Bug-3818.sql
@@ -14,7 +14,7 @@ SELECT 0 AS cods, 0 AS elrik, 0 AS ether, 0 AS jaelen, 0 AS sora, cast( SUM(tib0
  FROM t2a
 GROUP BY cods, elrik, ether, jaelen, sora;
 
-PLAN SELECT 0 AS cods, 0 AS elrik, 0 AS ether, 0 AS jaelen, 0 AS sora, cast( SUM(tib0) as bigint)
+PLAN SELECT 0 AS cods, 0 AS elrik, 0 AS ether, 0 AS jaelen, 0 AS sora, MIN(tib0)
  FROM t2a
 GROUP BY cods, elrik, ether, jaelen, sora;
 
