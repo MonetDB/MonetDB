7a10689b7b22ee68d5c9434a621126077877d179
Niels Nes <niels@cwi.nl>
2020-09-29 20:08:41 +0200

    approved output

--- sql/test/BugTracker-2015/Tests/readonly.Bug-3709.sql
+++ sql/test/BugTracker-2015/Tests/readonly.Bug-3709.sql
@@ -2,6 +2,6 @@ create table s10 (x int);
 start transaction; 
 insert into s10 values (8); 
 alter table s10 set read only; 
-trace select * from s10; 
+explain select * from s10; 
 rollback;
 drop table s10; 
