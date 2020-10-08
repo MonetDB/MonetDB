a781a2e57420240facad1a420542505f23dabde0
Joeri van Ruth <joeri.van.ruth@monetdbsolutions.com>
2020-10-08 15:55:07 +0200

    Simplify the endianness code in copybinary.h

--- common/utils/copybinary.h
+++ common/utils/copybinary.h
@@ -22,36 +22,60 @@ typedef struct {
 	copy_binary_date date;
 } copy_binary_timestamp; // natural size: 96, natural alignment: 32
 
-#ifdef _MSC_VER
-    // use intrinsic functions on Windows
-    #define COPY_BINARY_BYTESWAP16(s)	(_byteswap_ushort((uint16_t) (s)))
-    #define COPY_BINARY_BYTESWAP32(i)	(_byteswap_ulong((uint32_t) (i)))
-#else
-    // this code sequence is recognized by gcc and clang
-    #define COPY_BINARY_BYTESWAP16(u) ( \
-              ((uint8_t)(u) << 8) \
-            | ((uint8_t)(u>>8)) \
-        )
-    #define COPY_BINARY_BYTESWAP32(u) ( \
-              ((uint8_t)(u) << 24) \
-            | ((uint8_t)(u>>8) << 16) \
-            | ((uint8_t)(u>>16) << 8) \
-            | ((uint8_t)(u>>24)) \
-        )
-#endif
+
+// These code sequences are recognized by gcc and clang
+#define COPY_BINARY_BYTESWAP16(u) ( \
+		  ((uint8_t)(u) << 8) \
+		| ((uint8_t)(u>>8)) \
+	)
+#define COPY_BINARY_BYTESWAP32(u) ( \
+		  ((uint8_t)(u) << 24) \
+		| ((uint8_t)(u>>8) << 16) \
+		| ((uint8_t)(u>>16) << 8) \
+		| ((uint8_t)(u>>24)) \
+	)
+#define COPY_BINARY_BYTESWAP64(u) ( \
+		  ((uint8_t)(u>>0) << 56) \
+		| ((uint8_t)(u>>8) << 48) \
+		| ((uint8_t)(u>>16) << 40) \
+		| ((uint8_t)(u>>24) << 32) \
+		| ((uint8_t)(u>>32) << 24) \
+		| ((uint8_t)(u>>40) << 16) \
+		| ((uint8_t)(u>>48) << 8) \
+		| ((uint8_t)(u>>56)) \
+	)
+
 
 #ifdef WORDS_BIGENDIAN
-	#define COPY_BINARY_CONVERT_DATE_ENDIAN(d) \
-		do { (d).year = COPY_BINARY_BYTESWAP16((d).year); } while (0)
-	#define COPY_BINARY_CONVERT_TIME_ENDIAN(t) \
-		do { (t).ms = COPY_BINARY_BYTESWAP32((t).ms); } while (0)
+	#define COPY_BINARY_CONVERT16(lhs) \
+		do { (lhs) = COPY_BINARY_BYTESWAP16(lhs); } while (0)
+	#define COPY_BINARY_CONVERT32(lhs) \
+		do { (lhs) = COPY_BINARY_BYTESWAP32(lhs); } while (0)
+	#define COPY_BINARY_CONVERT64(lhs) \
+		do { (lhs) = COPY_BINARY_BYTESWAP64(lhs); } while (0)
+	#define COPY_BINARY_CONVERT128(lhs) \
+		do { (lhs) = COPY_BINARY_BYTESWAP128(lhs); } while (0)
 #else
 	// still refer to the fields to provoke error message when used incorrectly
-	#define COPY_BINARY_CONVERT_DATE_ENDIAN(d) \
-		do { (void)(d).year;  } while (0)
-	#define COPY_BINARY_CONVERT_TIME_ENDIAN(t) \
-		do { (void)(t).ms; } while (0)
+	#define COPY_BINARY_CONVERT16(lhs) \
+		do { (void)(lhs); } while (0)
+	#define COPY_BINARY_CONVERT32(lhs) \
+		do { (void)(lhs); } while (0)
+	#define COPY_BINARY_CONVERT64(lhs) \
+		do { (void)(lhs); } while (0)
+	#define COPY_BINARY_CONVERT128(lhs) \
+		do { (void)(lhs); } while (0)
 #endif
+
+#define COPY_BINARY_CONVERT_DATE_ENDIAN(d) \
+	do { \
+		COPY_BINARY_CONVERT16((d).year); \
+	} while (0)
+
+#define COPY_BINARY_CONVERT_TIME_ENDIAN(t) \
+	do { \
+		COPY_BINARY_CONVERT32((t).ms); \
+	} while (0)
 
 #define COPY_BINARY_CONVERT_TIMESTAMP_ENDIAN(ts) \
     do { \
