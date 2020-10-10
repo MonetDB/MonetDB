b4d4827a328f1e527045d68567ced4f122e2adfe
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-10-10 20:23:15 +0200

    Making SQLancer happy on Windows. Every non integer based storage type needs alocation on the result before returning

--- sql/test/SQLancer/Tests/sqlancer08.sql
+++ sql/test/SQLancer/Tests/sqlancer08.sql
@@ -8,6 +8,28 @@ select cast("isauuid"(t1.tc0) as int) from t0 full outer join t1 on
 not (sql_min(not ((interval '505207731' day) in (interval '1621733891' day)), (nullif(t0.tc0, t1.tc0)) between asymmetric (rtrim(t0.tc0)) and (cast((r'_7') in (r'', t0.tc0) as string(891)))));
 ROLLBACK;
 
+START TRANSACTION;
+CREATE TABLE "sys"."t0" ("tc0" UUID NOT NULL,CONSTRAINT "t0_tc0_pkey" PRIMARY KEY ("tc0"), CONSTRAINT "t0_tc0_unique" UNIQUE ("tc0"));
+COPY 8 RECORDS INTO "sys"."t0" FROM stdin USING DELIMITERS E'\t',E'\n','"';
+c3fc2aee-1e03-50cf-f4c7-e6bbbb3e31a3
+1efaa28b-1e44-0b5b-517b-5790d23acf5f
+32cf1b57-bccb-9e00-80a2-e5af23e5cccc
+5a9fe00d-b21e-6fba-efba-33ceefdebfb5
+68714cba-2af2-3de1-ebd0-eba5d8da68ce
+a40776ba-5e2d-02bd-1b59-0b1ad9b5d311
+b5a5abcd-bb90-56a2-ffd3-f321403b6e9e
+0b2d9fdb-8bfb-5fec-bebb-c658aecb013c
+
+CREATE TABLE "sys"."t1" ("tc1" VARCHAR(486));
+COPY 3 RECORDS INTO "sys"."t1" FROM stdin USING DELIMITERS E'\t',E'\n','"';
+"0.9918446996922964"
+NULL
+"{t鏷>9縣+B"
+
+SELECT CAST(SUM(count) AS BIGINT) FROM (SELECT ALL CAST(t0.tc0 <> ANY(VALUES (UUID 'EaFBB6AC-6Eb9-00d3-7cb0-8EC8b5ad59D8'), (UUID 'bA3ca114-Cb42-7CA8-dCdF-1fB6F2dFF704'), (UUID 'dbcea1AC-60dB-8DdA-ae8C-4FC400321eD6')) AS INT) as count FROM t0, t1) as res;
+	--24
+ROLLBACK;
+
 CREATE TABLE t0(tc0 INTERVAL MONTH DEFAULT (INTERVAL '1997904243' MONTH), tc1 TIME UNIQUE);
 INSERT INTO t0(tc0) VALUES(INTERVAL '444375026' MONTH);
 DELETE FROM t0 WHERE TRUE;
