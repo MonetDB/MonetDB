1960bc12905f01e004ef8be9c856e9ad1fc43d41
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-10-30 10:52:29 +0100

    Approved output

--- sql/test/BugTracker-2020/Tests/window-function-order.Bug-6997.sql
+++ sql/test/BugTracker-2020/Tests/window-function-order.Bug-6997.sql
@@ -4,10 +4,10 @@ create table "ordertest"(str text, orderA int, orderB int, num int);
 insert into "ordertest" values('a',1,1,1), ('a',2,1,10), ('a',3,1,20), ('a',4,1,30);
 
 select *,
-sum("num") over(order by orderA) as orderbyA,
-sum("num") over(order by orderA,orderB) as orderbyAB,
-sum("num") over(order by orderB,orderA) as orderbyBA,
-sum("num") over(order by orderB) as orderbyB
+cast(sum("num") over(order by orderA) as bigint) as orderbyA,
+cast(sum("num") over(order by orderA,orderB) as bigint) as orderbyAB,
+cast(sum("num") over(order by orderB,orderA) as bigint) as orderbyBA,
+cast(sum("num") over(order by orderB) as bigint) as orderbyB
 from "ordertest";
 
 rollback;
