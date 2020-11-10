c0ab9802353930750034ec16d00519c143ac8803
Niels Nes <niels@cwi.nl>
2020-11-10 17:01:34 +0100

    converted test to not output, unless we get some unexpected behaviour

--- sql/test/Users/Tests/dropManyUsers.Bug-3764.SQL.py
+++ sql/test/Users/Tests/dropManyUsers.Bug-3764.SQL.py
@@ -4,36 +4,50 @@
 ###
 
 import os, sys
-try:
-    from MonetDBtesting import process
-except ImportError:
-    import process
+import pymonetdb
 
-def sql_test_client(user, passwd, input):
-    with process.client(lang="sql", user=user, passwd=passwd, communicate=True,
-                        stdin=process.PIPE, stdout=process.PIPE, stderr=process.PIPE,
-                        input=input, port=int(os.getenv("MAPIPORT"))) as c:
-        c.communicate()
+def error(msg):
+    print(msg)
+    sys.exit(-1)
 
-sql_test_client('monetdb', 'monetdb', input="""\
-CREATE SCHEMA newSchema;
-select u.name, u.fullname, s.name from "sys"."users" u left outer join "sys"."schemas" s on u.default_schema = s.id;
+db=os.getenv("TSTDB")
+port=int(os.getenv("MAPIPORT"))
+client = pymonetdb.connect(database=db, port=port, autocommit=True, user='monetdb', password='monetdb')
+cursor = client.cursor()
 
-CREATE USER user1 with password '1' name '1st user' schema newSchema;
-CREATE USER user2 with password '2' name '2nd user' schema newSchema;
-CREATE USER user3 with password '3' name '3rd user' schema newSchema;
-CREATE USER user4 with password '4' name '4th user' schema newSchema;
+cursor.execute("CREATE SCHEMA newSchema");
 
-select u.name, u.fullname, s.name from "sys"."users" u left outer join "sys"."schemas" s on u.default_schema = s.id;
+cursor.execute("""select u.name, u.fullname, s.name from "sys"."users" u left outer join "sys"."schemas" s on u.default_schema = s.id""")
+users=cursor.fetchall()
+if (users[0][0] != 'monetdb'):
+    error('MonetDB user missing')
+if (users[1][0] != '.snapshot'):
+    error('.snapshot user missing')
 
-DROP USER user1;
-DROP USER user2;
-DROP USER user3;
-DROP USER user4;
+cursor.execute("CREATE USER user1 with password '1' name '1st user' schema newSchema");
+cursor.execute("CREATE USER user2 with password '2' name '2nd user' schema newSchema");
+cursor.execute("CREATE USER user3 with password '3' name '3rd user' schema newSchema");
+cursor.execute("CREATE USER user4 with password '4' name '4th user' schema newSchema");
 
-select u.name, u.fullname, s.name from "sys"."users" u left outer join "sys"."schemas" s on u.default_schema = s.id;
-""")
+cursor.execute("""select u.name, u.fullname, s.name from "sys"."users" u left outer join "sys"."schemas" s on u.default_schema = s.id""")
+users=cursor.fetchall()
+if (users[2][0] != 'user1'):
+    error('user1 user missing')
+if (users[3][0] != 'user2'):
+    error('user2 user missing')
+if (users[4][0] != 'user3'):
+    error('user3 user missing')
+if (users[5][0] != 'user4'):
+    error('user4 user missing')
 
+cursor.execute("DROP USER user1")
+cursor.execute("DROP USER user2")
+cursor.execute("DROP USER user3")
+cursor.execute("DROP USER user4")
 
+cursor.execute("""select u.name, u.fullname, s.name from "sys"."users" u left outer join "sys"."schemas" s on u.default_schema = s.id""")
+if len(cursor.fetchall()) != 2:
+    error('users not correctly dropped')
 
-
+cursor.close()
+client.close()
