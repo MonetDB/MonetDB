8e6162f2da970412454eea161079c21818f2b74f
Niels Nes <niels@cwi.nl>
2020-11-11 14:17:47 +0100

    some more tests converted

--- sql/test/Users/Tests/unknown_user.SQL.py
+++ sql/test/Users/Tests/unknown_user.SQL.py
@@ -2,16 +2,24 @@
 # Authenticate unknown USER (not possible).
 ###
 
-import sys
-try:
-    from MonetDBtesting import process
-except ImportError:
-    import process
+import sys,os
+import pymonetdb
 
-with process.client('sql',
-                    user='this_user_does_not_exist',
-                    passwd='this_password_does_not_exist',
-                    stdout=process.PIPE, stderr=process.PIPE) as clt:
-    out, err = clt.communicate()
-    sys.stdout.write(out)
-    sys.stderr.write(err)
+import logging
+
+logger = logging.Logger(__name__)
+logger.setLevel(100); # switch off logging
+
+error = False
+db=os.getenv("TSTDB")
+port=int(os.getenv("MAPIPORT"))
+try:
+    client = pymonetdb.connect(database=db, port=port, autocommit=True,
+        user='this_user_does_not_exist', password='this_password_does_not_exist')
+except pymonetdb.exceptions.DatabaseError:
+    error = True
+    pass
+
+if not error:
+    print("Logged in with invalid credentials")
+    sys.exit(-1)
