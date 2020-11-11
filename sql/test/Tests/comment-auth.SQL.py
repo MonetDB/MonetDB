c6fff7ef2e2f18e643540d9f17a2d419c915c77a
Sjoerd Mullender <sjoerd@acm.org>
2020-11-11 22:55:21 +0100

    Fixed a bunch of tests in sql/test/Tests.

--- sql/test/Tests/comment-auth.SQL.py
+++ sql/test/Tests/comment-auth.SQL.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, sys, re
 try:
     from MonetDBtesting import process
 except ImportError:
@@ -8,19 +8,36 @@ except ImportError:
 with process.client('sql',
                     stdin = open(os.path.join(os.getenv('TSTSRCDIR'),
                                               'comment-auth-superuser.sql')),
-                    stdout = process.PIPE, stderr = process.PIPE,
-                    log = True) as c:
+                    stdout = process.PIPE, stderr = process.PIPE) as c:
     out, err = c.communicate()
-    sys.stdout.write(out)
-    sys.stderr.write(err)
+    if re.search(r'^[^#\n]', out, re.M):
+        sys.stdout.write(out)
+    if re.search(r'^[^#\n]', err, re.M):
+        sys.stderr.write(err)
 
+dump = '''\
+SCHEMA schema_a 'set by super user'
+SCHEMA schema_b 'set by super user'
+SCHEMA schema_a 'set by user_a'
+SCHEMA schema_b 'set by super user'
+'''
+edump = r'''\
+MAPI = (user_a) /var/tmp/mtest-285315/.s.monetdb.\d+
+QUERY = COMMENT ON SCHEMA schema_b IS 'set by user_a';
+ERROR = !COMMENT ON: insufficient privileges for user 'user_a' in schema 'schema_b'
+CODE = 42000
+'''
 # As one of the users, check that we can only comment on our own objects.
 with process.client('sql',
                     user='user_a', passwd='user_a',
-                    stdin = open(os.path.join(os.getenv('TSTSRCDIR'),
+                    stdin=open(os.path.join(os.getenv('TSTSRCDIR'),
                                               'comment-auth-a.sql')),
-                    stdout = process.PIPE, stderr = process.PIPE,
-                    log = True) as c:
+                    echo=False,
+                    stdout=process.PIPE, stderr=process.PIPE) as c:
     out, err = c.communicate()
-    sys.stdout.write(out)
-    sys.stderr.write(err)
+    out = ' '.join(re.split('[ \t]+', out))
+    if out != dump:
+        sys.stdout.write(out)
+    err = ' '.join(re.split('[ \t]+', err))
+    if re.match(edump, err) is not None:
+        sys.stderr.write(err)
