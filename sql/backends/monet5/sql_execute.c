8d745d4369c1407f01586deab33ddca43f517993
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-11-13 10:53:41 +0100

    Added test and fix for bug 7010, ie don't clean query cache on deallocate mode. It is already handled by the mode itself

--- sql/backends/monet5/sql_execute.c
+++ sql/backends/monet5/sql_execute.c
@@ -616,7 +616,7 @@ SQLengineIntern(Client c, backend *be)
 		return MAL_SUCCEED;
 	}
 
-	if (m->emode == m_prepare)
+	if (m->emode == m_deallocate || m->emode == m_prepare)
 		goto cleanup_engine;
 
 	be->language = 'D';
@@ -631,7 +631,7 @@ SQLengineIntern(Client c, backend *be)
 		msg = SQLrun(c,m);
 
 cleanup_engine:
-	if (m->emode != m_prepare && m->type == Q_SCHEMA && m->qc != NULL)
+	if (m->emode != m_deallocate && m->emode != m_prepare && m->type == Q_SCHEMA && m->qc != NULL)
 		qc_clean(m->qc);
 	if (msg) {
 		/* don't print exception decoration, just the message */
