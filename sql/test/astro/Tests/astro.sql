a9a5f7f3d560d8620d6be79750f48ebf3c0250f7
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-11-16 16:43:14 +0100

    Re-converted astro test directory. Use a single test file

--- sql/test/astro/Tests/astro.sql
+++ sql/test/astro/Tests/astro.sql
@@ -24,4 +24,41 @@ CREATE TABLE "cm_flux" (
 	"avg_wfluxsq"      DOUBLE        NOT NULL,
 	"avg_dec_zone_deg" TINYINT       NOT NULL
 );
-COMMIT;
+
+plan UPDATE fluxz
+   SET (filter
+       ,f_datapoints
+       ,avg_flux
+       ,avg_fluxsq
+       ,avg_w
+       ,avg_wflux
+       ,avg_wfluxsq
+       ,avg_dec_zone_deg
+       )
+       =
+       (SELECT filter
+              ,f_datapoints
+              ,avg_flux
+              ,avg_fluxsq
+              ,avg_w
+              ,avg_wflux
+              ,avg_wfluxsq
+              ,avg_dec_zone_deg
+          FROM cm_flux 
+         WHERE cm_flux.runcat = fluxz.runcat 
+           AND cm_flux.active = TRUE 
+           AND cm_flux.filter = 'g' 
+           AND cm_flux.filter = fluxz.filter
+       )
+ WHERE EXISTS (SELECT runcat
+                 FROM cm_flux
+                WHERE cm_flux.runcat = fluxz.runcat 
+                  AND cm_flux.active = TRUE 
+                  AND cm_flux.filter = 'g' 
+                  AND cm_flux.filter = fluxz.filter
+              );
+
+drop table "fluxz";
+drop table "cm_flux";
+
+ROLLBACK;
