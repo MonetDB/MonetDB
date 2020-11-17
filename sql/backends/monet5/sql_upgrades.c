1aaad3d42dd4953312658e9cde8340642995adce
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-11-17 09:09:46 +0100

    Forgot newline

--- sql/backends/monet5/sql_upgrades.c
+++ sql/backends/monet5/sql_upgrades.c
@@ -2531,7 +2531,7 @@ sql_update_oct2020_sp1(Client c, mvc *sql, const char *prev_schema, bool *systab
 		pos += snprintf(buf + pos, bufsize - pos,
 			"create function sys.uuid(d int) returns uuid\n"
 			" external name uuid.\"new\";\n"
-			"GRANT EXECUTE ON FUNCTION sys.uuid(int) TO PUBLIC;");
+			"GRANT EXECUTE ON FUNCTION sys.uuid(int) TO PUBLIC;\n");
 
 		pos += snprintf(buf + pos, bufsize - pos, "set schema \"%s\";\n", prev_schema);
 		assert(pos < bufsize);
