208ad5dcb16a444edd65d9b980c67ac5102ba4bb
Sjoerd Mullender <sjoerd@acm.org>
2020-11-23 19:41:28 +0100

    Convert some of the complex tests.

---
+++ sql/test/emptydb/Tests/dump.SQL.py
@@ -0,0 +1,16 @@
+import os, sys, re
+try:
+    from MonetDBtesting import process
+except ImportError:
+    import process
+
+with process.client('sqldump', stdout=process.PIPE, stderr=process.PIPE) as c:
+    out, err = c.communicate()
+
+for line in out.split('\n'):
+    if line.startswith('--') or not line:
+        continue
+    if line != 'START TRANSACTION;' and line != 'COMMIT;':
+        # unexpected output, dump it all
+        print(out)
+        break
