41eb89319f518d01d8c52d5cae98252137ac3896
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-11-25 16:45:12 +0100

    Silenced null-byte-hang test

--- sql/test/Tests/null-byte-hang.SQL.py
+++ sql/test/Tests/null-byte-hang.SQL.py
@@ -1,14 +1,17 @@
-import os, sys
-try:
-    from MonetDBtesting import process
-except ImportError:
-    import process
+import os, sys, pymonetdb
 
-with process.client('sql',
-                    args = [os.path.join(os.getenv('TSTSRCBASE'),
-                                         os.getenv('TSTDIR'),
-                                         'null-byte-hang.sql')],
-                    stdout = process.PIPE, stderr = process.PIPE) as c:
-    out, err = c.communicate()
-    sys.stdout.write(out)
-    sys.stderr.write(err)
+
+client1 = pymonetdb.connect(port=int(os.getenv('MAPIPORT')), database=os.getenv('TSTDB'))
+cur1 = client1.cursor()
+f = open(os.path.join(os.getenv('TSTSRCBASE'), os.getenv('TSTDIR'), 'null-byte-hang.sql'), 'r')
+q = f.read()
+f.close()
+try:
+    cur1.execute(q)
+    sys.stderr.write('Error expected')
+except Exception as ex:
+    if 'NULL byte in string' not in str(ex):
+        sys.stderr.write('Error: NULL byte in string expected')
+
+cur1.close()
+client1.close()
