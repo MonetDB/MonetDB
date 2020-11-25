62d3559bbf621f614298ce4d171c01684d3ec501
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-11-25 11:47:43 +0100

    Silenced sql/test/miscellaneous Python tests

--- sql/test/miscellaneous/Tests/table_udf_missing_var.py
+++ sql/test/miscellaneous/Tests/table_udf_missing_var.py
@@ -1,24 +1,15 @@
-import sys
-try:
-    from MonetDBtesting import process
-except ImportError:
-    import process
+from MonetDBtesting.sqltest import SQLTestCase
 
-def client(input):
-    with process.client('sql', stdin=process.PIPE, stdout=process.PIPE, stderr=process.PIPE) as c:
-        out, err = c.communicate(input)
-        sys.stdout.write(out)
-        sys.stderr.write(err)
+with SQLTestCase() as tc:
+    tc.connect(username="monetdb", password="monetdb")
+    tc.execute('create function myfunc() returns table (x int) begin declare myvar int; return select myvar; end;').assertSucceeded()
+    tc.execute('create function myfunc2() returns int begin declare myvar int; return myvar; end;').assertSucceeded()
+    tc.execute('select * from myfunc();').assertSucceeded().assertDataResultMatch([(None,)])
+    tc.execute('select myfunc2();').assertSucceeded().assertDataResultMatch([(None,)])
 
-client('''
-create function myfunc() returns table (x int) begin declare myvar int; return select myvar; end;
-create function myfunc2() returns int begin declare myvar int; return myvar; end;
-select * from myfunc();
-select myfunc2();
-''')
-client('''
-select * from myfunc();
-select myfunc2();
-drop function myfunc();
-drop function myfunc2();
-''')
+with SQLTestCase() as tc:
+    tc.connect(username="monetdb", password="monetdb")
+    tc.execute('select * from myfunc();').assertSucceeded().assertDataResultMatch([(None,)])
+    tc.execute('select myfunc2();').assertSucceeded().assertDataResultMatch([(None,)])
+    tc.execute('drop function myfunc();').assertSucceeded()
+    tc.execute('drop function myfunc2();').assertSucceeded()
