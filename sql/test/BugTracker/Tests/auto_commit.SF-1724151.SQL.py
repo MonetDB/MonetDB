9ebb21abe36aacd20b930e39a16a2c62b56446cb
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-11-26 14:03:48 +0100

    Silenced Python tests

--- sql/test/BugTracker/Tests/auto_commit.SF-1724151.SQL.py
+++ sql/test/BugTracker/Tests/auto_commit.SF-1724151.SQL.py
@@ -1,13 +1,32 @@
-import os, sys
-try:
-    from MonetDBtesting import process
-except ImportError:
-    import process
+from MonetDBtesting.sqltest import SQLTestCase
 
-with process.client('sql',
-                    args = [os.path.join(os.getenv('TSTSRCDIR'),
-                                         '%s.txt' % sys.argv[1])],
-                    stdout = process.PIPE, stderr = process.PIPE) as c:
-    out, err = c.communicate()
-    sys.stdout.write(out)
-    sys.stderr.write(err)
+with SQLTestCase() as tc:
+    tc.connect(username="monetdb", password="monetdb")
+    tc.execute("""
+    create table tbl_xy (x int, y int);
+    insert into tbl_xy values (0,0);
+    insert into tbl_xy values (1,1);
+    """).assertSucceeded()
+
+    tc.execute("""
+    insert into tbl_xy values (2,2);
+    insert into tbl_xy values (3,3);
+    insert into tbl_xy values (4,4);
+    """).assertSucceeded()
+
+    tc.execute("""
+    insert into tbl_xy values (5,5);
+    insert into tbl_xy values (6,6);
+    insert into tbl_xy values (7,7);
+    """).assertSucceeded()
+
+    tc.execute("insert into tbl_xy values (-1, -100);").assertSucceeded().assertRowCount(1)
+
+    tc.execute("""
+    insert into tbl_xy values (8,8);
+    insert into tbl_xy values (9,9);
+    insert into tbl_xy values (10,10);
+    """).assertSucceeded()
+
+    tc.execute("select * from tbl_xy;").assertSucceeded().assertRowCount(12).assertDataResultMatch([(0,0),(1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7),(-1,-100),(8,8),(9,9),(10,10)])
+    tc.execute("drop table tbl_xy;").assertSucceeded()
