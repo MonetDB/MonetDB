8152c44715ba60b87a42f10c79f45d2a978e9fd6
Mitchell Weggemans <mitchell.weggemans@monetdbsolutions.com>
2020-11-26 16:05:21 +0100

    convert createUserRollback tests

--- sql/test/Users/Tests/createUserRollback.SQL.py
+++ sql/test/Users/Tests/createUserRollback.SQL.py
@@ -1,25 +1,43 @@
-import sys
-try:
-    from MonetDBtesting import process
-except ImportError:
-    import process
 
-with process.client('sql', stdin=process.PIPE, stdout=process.PIPE, stderr=process.PIPE) as c:
-    out, err = c.communicate('''
-        CREATE TABLE sys.myvar (c BIGINT);
-        INSERT INTO sys.myvar VALUES ((SELECT COUNT(*) FROM sys.users));
+from MonetDBtesting.sqltest import SQLTestCase
 
-        START TRANSACTION;
-        CREATE USER "1" WITH PASSWORD '1' NAME '1' SCHEMA "sys";
-        ROLLBACK;
+with SQLTestCase() as tc:
+    tc.connect(username="monetdb", password="monetdb")
+    tc.execute("CREATE TABLE sys.myvar (c BIGINT);")
+    tc.execute("INSERT INTO sys.myvar VALUES ((SELECT COUNT(*) FROM sys.users));")
+    tc.execute("""
+                START TRANSACTION;
+                CREATE USER "1" WITH PASSWORD '1' NAME '1' SCHEMA "sys";
+                ROLLBACK;
 
-        SELECT CAST(COUNT(*) - (SELECT c FROM sys.myvar) AS BIGINT) FROM sys.users; -- The MAL authorization is not transaction aware, so the count changes :/
-        DROP TABLE sys.myvar;
-    ''')
-    sys.stdout.write(out)
-    sys.stderr.write(err)
+                SELECT CAST(COUNT(*) - (SELECT c FROM sys.myvar) AS BIGINT) FROM sys.users; -- The MAL authorization is not transaction aware, so the count changes :/
+                DROP TABLE sys.myvar;
+            """).assertSucceeded()
+    tc.connect(username="1", password="1")
+    tc.execute("SELECT 1;")
 
-with process.client('sql',user='1',passwd='1', stdin=process.PIPE, stdout=process.PIPE, stderr=process.PIPE) as c: # error should not be possible to login
-    out, err = c.communicate('SELECT 1;')
-    sys.stdout.write(out)
-    sys.stderr.write(err)
+# import sys
+# try:
+#     from MonetDBtesting import process
+# except ImportError:
+#     import process
+
+# with process.client('sql', stdin=process.PIPE, stdout=process.PIPE, stderr=process.PIPE) as c:
+#     out, err = c.communicate('''
+#         CREATE TABLE sys.myvar (c BIGINT);
+#         INSERT INTO sys.myvar VALUES ((SELECT COUNT(*) FROM sys.users));
+
+#         START TRANSACTION;
+#         CREATE USER "1" WITH PASSWORD '1' NAME '1' SCHEMA "sys";
+#         ROLLBACK;
+
+#         SELECT CAST(COUNT(*) - (SELECT c FROM sys.myvar) AS BIGINT) FROM sys.users; -- The MAL authorization is not transaction aware, so the count changes :/
+#         DROP TABLE sys.myvar;
+#     ''')
+#     sys.stdout.write(out)
+#     sys.stderr.write(err)
+
+# with process.client('sql',user='1',passwd='1', stdin=process.PIPE, stdout=process.PIPE, stderr=process.PIPE) as c: # error should not be possible to login
+#     out, err = c.communicate('SELECT 1;')
+#     sys.stdout.write(out)
+#     sys.stderr.write(err)
