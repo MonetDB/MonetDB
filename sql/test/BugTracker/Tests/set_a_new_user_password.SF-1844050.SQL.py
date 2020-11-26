ba391a2097b40b2c150f242d8910becf6d2370f7
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-11-26 14:36:02 +0100

    Silenced more Python tests

--- sql/test/BugTracker/Tests/set_a_new_user_password.SF-1844050.SQL.py
+++ sql/test/BugTracker/Tests/set_a_new_user_password.SF-1844050.SQL.py
@@ -1,4 +1,5 @@
 import os, socket, sys, tempfile
+from MonetDBtesting.sqltest import SQLTestCase
 try:
     from MonetDBtesting import process
 except ImportError:
@@ -13,53 +14,44 @@ def freeport():
 
 class server_start(process.server):
     def __init__(self):
-        sys.stderr.write('#mserver\n')
-        sys.stderr.flush()
         super().__init__(mapiport=myport, dbname='db1',
                          dbfarm=os.path.join(farm_dir, 'db1'),
                          stdin=process.PIPE, stdout=process.PIPE,
                          stderr=process.PIPE)
-
-def client(lang, file, user='monetdb', passwd='monetdb'):
-    sys.stderr.write('#client\n')
-    sys.stderr.flush()
-    clt = process.client(lang, port=myport, dbname='db1',
-                         user=user, passwd=passwd,
-                         stdin=open(file),
-                         stdout=process.PIPE, stderr=process.PIPE)
-    return clt.communicate()
 
 with tempfile.TemporaryDirectory() as farm_dir:
     os.mkdir(os.path.join(farm_dir, 'db1'))
     myport = freeport()
 
     with server_start() as srv:
-        out, err = client('sql',
-                          os.path.join(os.getenv('RELSRCDIR'),
-                                       'set_a_new_user_password.SF-1844050_create_user.sql'))
-        sys.stdout.write(out)
-        sys.stderr.write(err)
+        with SQLTestCase() as tc:
+            tc.connect(username="monetdb", password="monetdb", port=myport, database='db1')
+            tc.execute("""
+            CREATE USER "voc2" WITH PASSWORD 'voc2' NAME 'VOC_EXPLORER' SCHEMA "sys";
+            CREATE SCHEMA "voc2" AUTHORIZATION "voc2";
+            ALTER USER "voc2" SET SCHEMA "voc2";
+            alter user "voc2" with password 'new';
+            """).assertSucceeded()
         out, err = srv.communicate()
-        sys.stdout.write(out)
         sys.stderr.write(err)
 
     with server_start() as srv:
-        out, err = client('sql',
-                          os.path.join(os.getenv('RELSRCDIR'),
-                                       'set_a_new_user_password.SF-1844050_select.sql'),
-                          "voc2", "new")
-        sys.stdout.write(out)
-        sys.stderr.write(err)
+        with SQLTestCase() as tc:
+            tc.connect(username="voc2", password="new", port=myport, database='db1')
+            tc.execute("""
+            select 1;
+            """).assertSucceeded().assertRowCount(1).assertDataResultMatch([(1,)])
         out, err = srv.communicate()
-        sys.stdout.write(out)
         sys.stderr.write(err)
 
     with server_start() as srv:
-        out, err = client('sql',
-                          os.path.join(os.getenv('RELSRCDIR'),
-                                       'set_a_new_user_password.SF-1844050_drop_user.sql'))
-        sys.stdout.write(out)
-        sys.stderr.write(err)
+        with SQLTestCase() as tc:
+            tc.connect(username="monetdb", password="monetdb", port=myport, database='db1')
+            tc.execute("DROP SCHEMA \"voc2\";").assertFailed(err_message='DROP SCHEMA: unable to drop schema \'voc2\' (there are database objects which depend on it)')
+            tc.execute("""
+            ALTER user "voc2" SET SCHEMA "sys";
+            DROP SCHEMA "voc2";
+            DROP USER "voc2";
+            """).assertSucceeded()
         out, err = srv.communicate()
-        sys.stdout.write(out)
         sys.stderr.write(err)
