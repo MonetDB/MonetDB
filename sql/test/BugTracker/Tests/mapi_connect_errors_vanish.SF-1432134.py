ea89ea24df79f8c7a73c8b55502a4b017267889d
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-11-26 13:10:19 +0100

    Converted test to Python, the current sqlite framework doesn't handle with invalid creadentials yet

---
+++ sql/test/BugTracker/Tests/mapi_connect_errors_vanish.SF-1432134.py
@@ -0,0 +1,11 @@
+import sys, os, pymonetdb
+
+db = os.getenv("TSTDB")
+port = int(os.getenv("MAPIPORT"))
+
+try:
+    pymonetdb.connect(database=db, port=port, autocommit=True, username='invalid', password='invalid')
+    sys.stderr.write('Exception expected')
+except Exception as ex:
+    if 'invalid credentials for user \'invalid\'' not in str(ex):
+        sys.stderr.write('Expected error: invalid credentials for user \'invalid\'')
