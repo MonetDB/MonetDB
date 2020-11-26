a01c2a338354b01a041391ecd7dee20d7b423cb4
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-11-26 11:53:42 +0100

    Converted mclient commands and added missing reqtests files

--- sql/test/BugTracker/Tests/multi-column-constraint.SF-1964587.sql
+++ sql/test/BugTracker/Tests/multi-column-constraint.SF-1964587.sql
@@ -1,3 +1,3 @@
 create table test_property(subject integer, p1 integer, p2 integer, unique(subject, p1), unique(subject, p2));
-\D
+select "name", "query", "type", "remark" from describe_table('sys', 'test_property');
 drop table test_property;
