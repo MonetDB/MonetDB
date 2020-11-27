d6ee2187805c099a4380b9e9d88d85bdc6b66010
svetlin <svetlin.stalinov@monetdbsolutions.com>
2020-11-27 17:03:49 +0100

    fix sql/test/Dump

--- sql/test/Dump/Tests/dump.SQL.py
+++ sql/test/Dump/Tests/dump.SQL.py
@@ -1,15 +1,24 @@
 import os, sys
-try:
-    from MonetDBtesting import process
-except ImportError:
-    import process
+from MonetDBtesting.sqltest import SQLTestCase
 
-with process.client('sqldump', stdout=process.PIPE, stderr=process.PIPE) as p:
-    dump, err = p.communicate()
+with SQLTestCase() as tc:
+    d = tc.sqldump()
+    d.assertMatchStableOut(fout='dump.stable.out')
+    # this dump will be used in subsequent test
+    with open(os.path.join(os.environ['TSTTRGDIR'], 'dumpoutput.sql'), 'w') as f:
+        f.write(d.data)
 
-f = open(os.path.join(os.environ['TSTTRGDIR'], 'dumpoutput.sql'), 'w')
-f.write(dump)
-f.close()
-
-sys.stdout.write(dump)
-sys.stderr.write(err)
+#try:
+#    from MonetDBtesting import process
+#except ImportError:
+#    import process
+#
+#with process.client('sqldump', stdout=process.PIPE, stderr=process.PIPE) as p:
+#    dump, err = p.communicate()
+#
+#f = open(os.path.join(os.environ['TSTTRGDIR'], 'dumpoutput.sql'), 'w')
+#f.write(dump)
+#f.close()
+#
+#sys.stdout.write(dump)
+#sys.stderr.write(err)
