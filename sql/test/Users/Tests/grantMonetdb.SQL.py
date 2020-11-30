5f53c2b9a82a43ef57a0b556d4cd7270655042d2
Mitchell Weggemans <mitchell.weggemans@monetdbsolutions.com>
2020-11-30 10:23:01 +0100

    convert tests for grantMonetdb

--- sql/test/Users/Tests/grantMonetdb.SQL.py
+++ sql/test/Users/Tests/grantMonetdb.SQL.py
@@ -3,28 +3,39 @@
 # Verify that the user can assume the monetdb role and CREATE new users, GRANT privileges and roles.
 ###
 
-import os, sys
-try:
-    from MonetDBtesting import process
-except ImportError:
-    import process
+from MonetDBtesting.sqltest import SQLTestCase
 
-def sql_test_client(user, passwd, input):
-    with process.client(lang="sql", user=user, passwd=passwd, communicate=True,
-                        stdin=process.PIPE, stdout=process.PIPE, stderr=process.PIPE,
-                        input=input, port=int(os.getenv("MAPIPORT"))) as c:
-        c.communicate()
+with SQLTestCase() as tc:
+    tc.connect(username="monetdb", password="monetdb")
+    tc.execute("GRANT sysadmin TO alice;")
+    tc.connect(username="alice", password="alice")
+    tc.execute("SET ROLE sysadmin;")
+    tc.execute("CREATE USER may WITH PASSWORD 'may' NAME 'May' SCHEMA library;")
+    tc.execute("GRANT ALL ON orders TO april;")
+    tc.execute("GRANT sysadmin TO april;")
 
-sql_test_client('monetdb', 'monetdb', input="""\
-GRANT sysadmin TO alice;
-""")
+# import os, sys
+# try:
+#     from MonetDBtesting import process
+# except ImportError:
+#     import process
+
+# def sql_test_client(user, passwd, input):
+#     with process.client(lang="sql", user=user, passwd=passwd, communicate=True,
+#                         stdin=process.PIPE, stdout=process.PIPE, stderr=process.PIPE,
+#                         input=input, port=int(os.getenv("MAPIPORT"))) as c:
+#         c.communicate()
+
+# sql_test_client('monetdb', 'monetdb', input="""\
+# GRANT sysadmin TO alice;
+# """)
 
 
-sql_test_client('alice', 'alice', input="""\
-SET ROLE sysadmin;
-CREATE USER may WITH PASSWORD 'may' NAME 'May' SCHEMA library;
-GRANT ALL ON orders TO april;
-GRANT sysadmin TO april;
-""")
+# sql_test_client('alice', 'alice', input="""\
+# SET ROLE sysadmin;
+# CREATE USER may WITH PASSWORD 'may' NAME 'May' SCHEMA library;
+# GRANT ALL ON orders TO april;
+# GRANT sysadmin TO april;
+# """)
 
 
