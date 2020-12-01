3e9786a270be167122dd9d624981a6d9093f0751
Mitchell Weggemans <mitchell.weggemans@monetdbsolutions.com>
2020-12-01 16:47:26 +0100

    convert tests for grantMonetdbToSchemaOwner

--- sql/test/Users/Tests/grantMonetdbToSchemaOwner.Bug-3771.SQL.py
+++ sql/test/Users/Tests/grantMonetdbToSchemaOwner.Bug-3771.SQL.py
@@ -3,44 +3,64 @@
 # Check that by assuming the monetdb role the user has complete privileges (e.g. select, create, drop).
 ###
 
+from MonetDBtesting.sqltest import SQLTestCase
+with SQLTestCase() as tc:
+    tc.connect(username="monetdb", password="monetdb")
+    tc.execute("CREATE USER owner with password 'ThisIsAS3m1S3cur3P4ssw0rd' name 'user gets monetdb rights' schema sys;")
+    tc.execute("CREATE SCHEMA schemaForOwner AUTHORIZATION owner;")
+    tc.execute("CREATE table schemaForOwner.testTable(v1 int, v2 int);")
 
-import os, sys
-try:
-    from MonetDBtesting import process
-except ImportError:
-    import process
+    tc.connect(username="owner", password="ThisIsAS3m1S3cur3P4ssw0rd")
+    tc.execute("set schema schemaForOwner;")
+    tc.execute("""
+         DROP TABLE testTable;
+         CREATE TABLE testTable(v1 INT);
+         ALTER TABLE testTable ADD COLUMN v2 INT;
+    """)
+    tc.execute("""
+        SELECT * FROM testTable;
+        INSERT INTO testTable VALUES (3, 3);
+        UPDATE testTable SET v1 = 2 WHERE v2 = 7;
+        DELETE FROM testTable WHERE v1 = 2;
+    """)
 
-def sql_test_client(user, passwd, input):
-    with process.client(lang="sql", user=user, passwd=passwd, communicate=True,
-                        stdin=process.PIPE, stdout=process.PIPE, stderr=process.PIPE,
-                        input=input, port=int(os.getenv("MAPIPORT"))) as c:
-        c.communicate()
+# import os, sys
+# try:
+#     from MonetDBtesting import process
+# except ImportError:
+#     import process
 
-sql_test_client('monetdb', 'monetdb', input="""\
-CREATE USER owner with password 'ThisIsAS3m1S3cur3P4ssw0rd' name 'user gets monetdb rights' schema sys;
+# def sql_test_client(user, passwd, input):
+#     with process.client(lang="sql", user=user, passwd=passwd, communicate=True,
+#                         stdin=process.PIPE, stdout=process.PIPE, stderr=process.PIPE,
+#                         input=input, port=int(os.getenv("MAPIPORT"))) as c:
+#         c.communicate()
 
-CREATE SCHEMA schemaForOwner AUTHORIZATION owner;
+# sql_test_client('monetdb', 'monetdb', input="""\
+# CREATE USER owner with password 'ThisIsAS3m1S3cur3P4ssw0rd' name 'user gets monetdb rights' schema sys;
 
-CREATE table schemaForOwner.testTable(v1 int, v2 int);
+# CREATE SCHEMA schemaForOwner AUTHORIZATION owner;
 
--- Grant delete rights.
-GRANT sysadmin to owner;
+# CREATE table schemaForOwner.testTable(v1 int, v2 int);
 
-""")
+# -- Grant delete rights.
+# GRANT sysadmin to owner;
 
-sql_test_client('owner', 'ThisIsAS3m1S3cur3P4ssw0rd', input="""\
--- Check delete.
-set schema schemaForOwner;
-set role monetdb;
+# """)
 
-DROP TABLE testTable;
-CREATE TABLE testTable(v1 INT);
-ALTER TABLE testTable ADD COLUMN v2 INT;
+# sql_test_client('owner', 'ThisIsAS3m1S3cur3P4ssw0rd', input="""\
+# -- Check delete.
+# set schema schemaForOwner;
+# set role monetdb;
+
+# DROP TABLE testTable;
+# CREATE TABLE testTable(v1 INT);
+# ALTER TABLE testTable ADD COLUMN v2 INT;
 
 
-SELECT * FROM testTable;
-INSERT INTO testTable VALUES (3, 3);
-UPDATE testTable SET v1 = 2 WHERE v2 = 7;
-DELETE FROM testTable WHERE v1 = 2;
-""")
+# SELECT * FROM testTable;
+# INSERT INTO testTable VALUES (3, 3);
+# UPDATE testTable SET v1 = 2 WHERE v2 = 7;
+# DELETE FROM testTable WHERE v1 = 2;
+# """)
 
