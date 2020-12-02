df37df8a2e3cbd1db9cf31b79beeb504566781c2
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-12-02 11:34:18 +0100

    Approved output with indirect reference from views changes

--- sql/test/Users/test_privs2.sql
+++ sql/test/Users/test_privs2.sql
@@ -6,10 +6,20 @@ SELECT insertversion('test3', 3);
 
 SELECT * FROM version;
 
+update version set i = 2 where i = 10;
+
 SELECT updateversion('test1', 4);
 
 SELECT * FROM version;
 
+delete from version where i = 100;
+
 SELECT deleteversion(4);
+
+SELECT * FROM version;
+
+truncate version;
+
+SELECT truncateversion();
 
 SELECT * FROM version;
