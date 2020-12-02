c9c25f336075d5e4b0b4b6a84b76e7e994edf5c4
Mitchell Weggemans <mitchell.weggemans@monetdbsolutions.com>
2020-12-02 11:53:54 +0100

    convert tests for grantMonetdbToSchemaOwner and grantPrivilegesNonDefaultRole

--- sql/test/Users/Tests/grantMonetdbToSchemaOwner.Bug-3771.SQL.py
+++ sql/test/Users/Tests/grantMonetdbToSchemaOwner.Bug-3771.SQL.py
@@ -6,23 +6,21 @@
 from MonetDBtesting.sqltest import SQLTestCase
 with SQLTestCase() as tc:
     tc.connect(username="monetdb", password="monetdb")
-    tc.execute("CREATE USER owner with password 'ThisIsAS3m1S3cur3P4ssw0rd' name 'user gets monetdb rights' schema sys;")
-    tc.execute("CREATE SCHEMA schemaForOwner AUTHORIZATION owner;")
-    tc.execute("CREATE table schemaForOwner.testTable(v1 int, v2 int);")
+    tc.execute("CREATE USER owner with password 'ThisIsAS3m1S3cur3P4ssw0rd' name 'user gets monetdb rights' schema sys;").assertSucceeded()
+    tc.execute("CREATE SCHEMA schemaForOwner AUTHORIZATION owner;").assertSucceeded()
+    tc.execute("CREATE table schemaForOwner.testTable(v1 int, v2 int);").assertSucceeded()
 
     tc.connect(username="owner", password="ThisIsAS3m1S3cur3P4ssw0rd")
-    tc.execute("set schema schemaForOwner;")
     tc.execute("""
-         DROP TABLE testTable;
-         CREATE TABLE testTable(v1 INT);
-         ALTER TABLE testTable ADD COLUMN v2 INT;
-    """)
-    tc.execute("""
+        set schema schemaForOwner;
+        DROP TABLE testTable;
+        CREATE TABLE testTable(v1 INT);
+        ALTER TABLE testTable ADD COLUMN v2 INT;
         SELECT * FROM testTable;
         INSERT INTO testTable VALUES (3, 3);
         UPDATE testTable SET v1 = 2 WHERE v2 = 7;
         DELETE FROM testTable WHERE v1 = 2;
-    """)
+    """).assertSucceeded()
 
 # import os, sys
 # try:
