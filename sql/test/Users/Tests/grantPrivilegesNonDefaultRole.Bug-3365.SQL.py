b8a735164a26d1a4ff3ffd28fdaa109a9d41fe43
Mitchell Weggemans <mitchell.weggemans@monetdbsolutions.com>
2020-12-02 11:53:54 +0100

    convert tests for grantMonetdbToSchemaOwner and grantPrivilegesNonDefaultRole

--- sql/test/Users/Tests/grantPrivilegesNonDefaultRole.Bug-3365.SQL.py
+++ sql/test/Users/Tests/grantPrivilegesNonDefaultRole.Bug-3365.SQL.py
@@ -3,31 +3,54 @@
 # Check that by assuming the monetdb role the user has complete privileges (e.g. select, create, drop).
 ###
 
+from MonetDBtesting.sqltest import SQLTestCase
 
-import os, sys
-try:
-    from MonetDBtesting import process
-except ImportError:
-    import process
+with SQLTestCase() as tc:
+    tc.connect(username="monetdb", password="monetdb")
+    tc.execute("""
+        create role hr_role;
+        create schema hr authorization hr_role;
+        create user blake with password 'password' name 'Blake' schema "hr";
+        create user clark with password 'password' name 'Clark' schema "hr";
+        grant hr_role to blake;
+        """).assertSucceeded()
 
-def sql_test_client(user, passwd, input):
-    with process.client(lang="sql", user=user, passwd=passwd, communicate=True,
-                        stdin=process.PIPE, stdout=process.PIPE, stderr=process.PIPE,
-                        input=input, port=int(os.getenv("MAPIPORT"))) as c:
-        c.communicate()
+    tc.connect(username="blake", password="password")
+    tc.execute("""
+            set role hr_role;
+            create table employees (id bigint,name varchar(20));
+            grant select on employees to clark from current_role;
+        """).assertSucceeded()
 
-sql_test_client('monetdb', 'monetdb', input="""\
-create role hr_role;
-create schema hr authorization hr_role;
-create user blake with password 'password' name 'Blake' schema "hr";
-create user clark with password 'password' name 'Clark' schema "hr";
-grant hr_role to blake;
-""")
+    tc.execute("""
+        set role hr_role;
+        grant select on employees to clark;
+    """).assertFailed()
 
-sql_test_client('blake', 'password', input="""\
-set role hr_role;
-create table employees (id bigint,name varchar(20));
-grant select on employees to clark;
-grant select on employees to clark from current_role;
-""")
+# import os, sys
+# try:
+#     from MonetDBtesting import process
+# except ImportError:
+#     import process
+
+# def sql_test_client(user, passwd, input):
+#     with process.client(lang="sql", user=user, passwd=passwd, communicate=True,
+#                         stdin=process.PIPE, stdout=process.PIPE, stderr=process.PIPE,
+#                         input=input, port=int(os.getenv("MAPIPORT"))) as c:
+#         c.communicate()
+
+# sql_test_client('monetdb', 'monetdb', input="""\
+# create role hr_role;
+# create schema hr authorization hr_role;
+# create user blake with password 'password' name 'Blake' schema "hr";
+# create user clark with password 'password' name 'Clark' schema "hr";
+# grant hr_role to blake;
+# """)
+
+# sql_test_client('blake', 'password', input="""\
+# set role hr_role;
+# create table employees (id bigint,name varchar(20));
+# grant select on employees to clark;
+# grant select on employees to clark from current_role;
+# """)
 
