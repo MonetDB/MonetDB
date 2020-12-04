f51730a9c288c2fc23bee8d9e22239c660c7390b
Mitchell Weggemans <mitchell.weggemans@monetdbsolutions.com>
2020-12-04 13:47:52 +0100

    convert tests for unknown_user

--- sql/test/Users/Tests/unknown_user.SQL.py
+++ sql/test/Users/Tests/unknown_user.SQL.py
@@ -2,23 +2,32 @@
 # Authenticate unknown USER (not possible).
 ###
 
-import sys,os
-import pymonetdb
+from MonetDBtesting.sqltest import SQLTestCase
 
-import logging
+err_msg = "InvalidCredentialsException:checkCredentials:invalid credentials for user 'this_user_does_not_exist'".strip()
 
-logging.basicConfig(level=logging.FATAL)
+with SQLTestCase() as tc:
+    tc.connect(username="this_user_does_not_exist", password="this_password_does_not_exist")
+    tc.execute("SELECT * FROM sys;").assertFailed(err_message=err_msg)
 
-error = False
-db=os.getenv("TSTDB")
-port=int(os.getenv("MAPIPORT"))
-try:
-    client = pymonetdb.connect(database=db, port=port, autocommit=True,
-        user='this_user_does_not_exist', password='this_password_does_not_exist')
-except pymonetdb.exceptions.DatabaseError:
-    error = True
-    pass
 
-if not error:
-    print("Logged in with invalid credentials")
-    sys.exit(-1)
+# import sys,os
+# import pymonetdb
+
+# import logging
+
+# logging.basicConfig(level=logging.FATAL)
+
+# error = False
+# db=os.getenv("TSTDB")
+# port=int(os.getenv("MAPIPORT"))
+# try:
+#     client = pymonetdb.connect(database=db, port=port, autocommit=True,
+#         user='this_user_does_not_exist', password='this_password_does_not_exist')
+# except pymonetdb.exceptions.DatabaseError:
+#     error = True
+#     pass
+
+# if not error:
+#     print("Logged in with invalid credentials")
+#     sys.exit(-1)
