f1ab98a6e27fce8993aaf036e326589fec6fbf41
Sjoerd Mullender <sjoerd@acm.org>
2020-12-08 17:13:05 +0100

    Silence some tests.

--- clients/R/Tests/deps-install.R
+++ clients/R/Tests/deps-install.R
@@ -8,6 +8,6 @@ cat("#~BeginProfilingOutput~#\n", file=stdout())
 install.packages(packages_required, repos="http://cran.rstudio.com/")
 
 cat("#~EndProfilingOutput~#\n", file=stderr())
-cat("#~EndProfilingOutput~#\n\n", file=stdout())
+cat("#~EndProfilingOutput~#\n", file=stdout())
 
 stopifnot(all(c(packages_required, "DBI") %in% installed.packages()[,"Package"]))
