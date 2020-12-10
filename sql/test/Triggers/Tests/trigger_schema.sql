28ae6a09f25a2b31d8e5ea563573c672476af4f5
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-12-10 14:55:08 +0100

    Disallow schema-qualified trigger name during creation. Use it on the table the trigger will refer to when necessaey, because the trigger and the table will always be on the same schema. This makes our implementation compliant with others

--- sql/test/Triggers/Tests/trigger_schema.sql
+++ sql/test/Triggers/Tests/trigger_schema.sql
@@ -26,3 +26,15 @@ select * from bar.t2;
 select * from foo.t1;
 select * from foo.t2;
 ROLLBACK;
+
+START TRANSACTION;
+create schema foo;
+set schema foo;
+create table t1 (id serial, ref bigint);
+create table t2 (id serial, ref bigint);
+create schema bar;
+set schema bar;
+create trigger foo.extra_insert
+    AFTER INSERT ON t1 referencing new row as new_row
+    FOR EACH statement insert into t2(ref) values (new_row.ref); --error, a trigger will be placed on its table's schema, specify the schema on the table reference, ie ON clause instead
+ROLLBACK;
