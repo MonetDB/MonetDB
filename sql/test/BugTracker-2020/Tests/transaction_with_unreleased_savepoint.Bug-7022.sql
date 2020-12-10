169a4756fe1d06e70425c42faff841176d25d221
Ying Zhang <y.zhang@cwi.nl>
2020-12-10 11:54:07 +0100

    Clean up at the end of the tests and changed sanity check queries to produce determinable output

--- sql/test/BugTracker-2020/Tests/transaction_with_unreleased_savepoint.Bug-7022.sql
+++ sql/test/BugTracker-2020/Tests/transaction_with_unreleased_savepoint.Bug-7022.sql
@@ -5,15 +5,15 @@ savepoint name1;
 insert into savepointtest values(24);
 release savepoint name1;
 commit;
-select * from tables where name = 'savepointtest';
+select name, schema_id, query, type, system, commit_action, access, temporary from tables where name = 'savepointtest';
 select * from savepointtest;
 
 create table savepointtest (id int, primary key(id));
 insert into savepointtest values(42);
-select * from tables where name = 'savepointtest';
+select name, schema_id, query, type, system, commit_action, access, temporary from tables where name = 'savepointtest';
 select * from savepointtest;
 drop table savepointtest;
-select * from tables where name = 'savepointtest';
+select name, schema_id, query, type, system, commit_action, access, temporary from tables where name = 'savepointtest';
 
 -- this shows errors
 start transaction;
@@ -21,13 +21,13 @@ create table savepointtest (id int, primary key(id));
 savepoint name1;
 insert into savepointtest values(24);
 commit;
-select * from tables where name = 'savepointtest';
+select name, schema_id, query, type, system, commit_action, access, temporary from tables where name = 'savepointtest';
 select * from savepointtest;
 
 create table savepointtest (id int, primary key(id));
 insert into savepointtest values(42);
-select * from tables where name = 'savepointtest';
+select name, schema_id, query, type, system, commit_action, access, temporary from tables where name = 'savepointtest';
 -- the 2nd 'savepointtest` is used by SELECT and DROP
 select * from savepointtest;
 drop table savepointtest;
-select * from tables where name = 'savepointtest';
+select name, schema_id, query, type, system, commit_action, access, temporary from tables where name = 'savepointtest';
