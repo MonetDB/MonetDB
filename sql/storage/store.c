5adcb590da6b85220f8208032aecc1672f46fa35
Niels Nes <niels@cwi.nl>
2020-12-12 18:53:28 +0100

    don't return already deleted nodes in cs_find_id more cleanup (cleanup deleted tables in the spare transactions).

--- sql/storage/store.c
+++ sql/storage/store.c
@@ -2357,6 +2357,11 @@ cleanup_table(sql_table *t)
 		for (int i = 0; i<spares; i++) {
 			for (node *m = spare_trans[i]->schemas.set->h; m; m = m->next) {
 				sql_schema * schema = m->data;
+
+				if (schema->tables.dset) {
+					list_destroy(schema->tables.dset);
+					schema->tables.dset = NULL;
+				}
 				node *o = find_sql_table_node(schema, t->base.id);
 				if (o) {
 					list_remove_node(schema->tables.set, o);
@@ -4082,6 +4087,7 @@ rollforward_changeset_updates(sql_trans *tr, int oldest, changeset * fs, changes
 			list_destroy(fs->dset);
 			fs->dset = NULL;
 		}
+		/*
 		if (!apply && ts->dset) {
 			for (n = ts->dset->h; ok == LOG_OK && n; n = n->next) {
 				sql_base *tb = n->data;
@@ -4090,6 +4096,7 @@ rollforward_changeset_updates(sql_trans *tr, int oldest, changeset * fs, changes
 					ok = rollforward_deletes(tr, tb, mode);
 			}
 		}
+		*/
 		if (apply && ts->dset && !cf) {
 			list_destroy(ts->dset);
 			ts->dset = NULL;
