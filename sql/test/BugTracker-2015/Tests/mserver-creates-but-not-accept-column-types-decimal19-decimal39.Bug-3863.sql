abcece002cae63585bfedf01d3c6a2dfbfa9e0b8
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2020-12-13 16:07:47 +0100

    Merged more outputs

--- sql/test/BugTracker-2015/Tests/mserver-creates-but-not-accept-column-types-decimal19-decimal39.Bug-3863.sql
+++ sql/test/BugTracker-2015/Tests/mserver-creates-but-not-accept-column-types-decimal19-decimal39.Bug-3863.sql
@@ -3,7 +3,7 @@ INSERT INTO bar VALUES ('A', 123, 456);
 CREATE TABLE foo AS SELECT cast(SUM(ct + ct2) as bigint) / 100.0 AS eur FROM bar GROUP BY name WITH DATA;
 
 select "name", "query", "type", "remark" from describe_table('sys', 'foo');
-select * from describe_columns('sys', 'foo');
+select "name", "nulls", "cdefault", "number", "remark" from describe_columns('sys', 'foo');
 
 DROP TABLE foo;
 DROP TABLE bar;
