56f9db2d17ef011b289b7d18e15c7e14f36cd8c9
Ying Zhang <y.zhang@cwi.nl>
2020-12-13 14:02:01 +0100

    Cleaned up sysqueue test

--- sql/test/sysmon/Tests/sysqueue.sql
+++ sql/test/sysmon/Tests/sysqueue.sql
@@ -1,8 +1,13 @@
--- check if we have a increasing queue table
--- the case expression is used to make sure the test is timezone agnostic
-select username, status, case when query ilike 'set time%' then 'set time...' else query end from sys.queue();
-select count(*) from sys.queue();
-select count(*) from sys.queue();
-select count(*) from sys.queue();
-select count(*) from sys.queue();
-select count(*) from sys.queue() where query = 'select count(*) from sys.queue();';
+-- check if we have an increasing queue table
+
+-- Clean up sys.queue(): since we're using max_clients=4, four queries should
+-- push out all existing queries, which should have all been finished by now
+select 1;
+select 2;
+select 3;
+select 4;
+-- We expect the last three queries: sys.queue() keeps (MAL_MAXCLIENTS - 1)
+--   queries in its queue so that there is always an empty slot for the next
+--   query
+-- Have to rely on query text to get determinable results
+select username, query from sys.queue() order by query;
