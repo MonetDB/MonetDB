88390dc7980b8e5b5d6eeeb1bc7408342a6ffcd4
Ying Zhang <y.zhang@cwi.nl>
2020-12-13 14:12:06 +0100

    Merged sys_queue_rotate tests into sysqueue.test

--- sql/test/sysmon/Tests/sys_queue_rotate.sql
+++
@@ -1,8 +0,0 @@
--- test the rotation of the queue
--- the case expression is used to make sure the test is timezone agnostic
-select 1, username, status, case when query ilike 'set time%' then 'set time...' else query end from sys.queue();
-select 2, username, status, case when query ilike 'set time%' then 'set time...' else query end from sys.queue();
-select 3, username, status, case when query ilike 'set time%' then 'set time...' else query end from sys.queue();
-select 4, username, status, case when query ilike 'set time%' then 'set time...' else query end from sys.queue();
-select 5, username, status, case when query ilike 'set time%' then 'set time...' else query end from sys.queue();
-select 6, username, status, case when query ilike 'set time%' then 'set time...' else query end from sys.queue();
--- sql/test/sysmon/Tests/sysqueue.sql
+++ sql/test/sysmon/Tests/sysqueue.sql
@@ -1,11 +1,16 @@
--- check if we have an increasing queue table
+-- check if we have an increasing and rotating queue table
 
--- Clean up sys.queue(): since we're using max_clients=4, four queries should
--- push out all existing queries, which should have all been finished by now
+-- Clean up sys.queue(): since we're using max_clients=4, seven queries will
+--   push out all queriesl automatically executed during the database start up
+--   and force sys.queue to rotate since all queries so far are instantly
+--   finished.
 select 1;
 select 2;
 select 3;
 select 4;
+select 5;
+select 6;
+select 7;
 -- We expect the last three queries: sys.queue() keeps (MAL_MAXCLIENTS - 1)
 --   queries in its queue so that there is always an empty slot for the next
 --   query
