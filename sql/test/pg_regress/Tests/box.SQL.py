e6ce55f28fbe052b54b3422a17d08ae2ae6e268c
svetlin <svetlin.stalinov@monetdbsolutions.com>
2020-12-15 12:00:50 +0100

    put back box.test

--- sql/test/pg_regress/Tests/box.SQL.py
+++
@@ -1,9 +0,0 @@
-from MonetDBtesting.sqltest import SQLTestCase
-
-with SQLTestCase() as tc:
-    # optional or default connection
-    tc.connect()
-    with open('box.sql') as f:
-        tc.execute(query=None, client='mclient', stdin=f)\
-            .assertMatchStableOut(fout='box.stable.out')\
-            .assertMatchStableError(ferr='box.stable.err')
