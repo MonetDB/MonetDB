211849ec2ec71b27741918b1fbf01a86360927d9
Sjoerd Mullender <sjoerd@acm.org>
2020-12-17 13:39:16 +0100

    Also test Unix domain socket URI, but not on Windows.

--- clients/Tests/mclient-uri.SQL.py
+++ clients/Tests/mclient-uri.SQL.py
@@ -8,3 +8,12 @@ if out != '1\n':
     print(out)
 if err:
     print(err, file=sys.stderr)
+
+if os.name != 'nt':
+    with process.client('sql', dbname='mapi:monetdb://{}/.s.monetdb.{}?database={}&language=sql&user=monetdb'.format(os.getenv('MAPIHOST', '/tmp'), os.getenv('MAPIPORT', 50000), os.getenv('TSTDB', 'demo')), host='', port='', format='csv', echo=False, stdin=process.PIPE, stdout=process.PIPE, stderr=process.PIPE) as c:
+        out, err = c.communicate('select 1;\n')
+
+    if out != '1\n':
+        print(out)
+    if err:
+        print(err, file=sys.stderr)
