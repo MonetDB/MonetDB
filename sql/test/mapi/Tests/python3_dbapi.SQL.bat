2e360dfad4c09a29b7cac0213e2f1523bb238bdf
Sjoerd Mullender <sjoerd@acm.org>
2020-12-18 10:13:52 +0100

    Silence Windows.

--- sql/test/mapi/Tests/python3_dbapi.SQL.bat
+++ sql/test/mapi/Tests/python3_dbapi.SQL.bat
@@ -1,8 +1,5 @@
 @echo off
 
-prompt # $t $g  
-echo on
-
 rem must be aligned with the installation directory chosen in
 rem clients/examples/python
 set testpath=%TSTSRCBASE%\clients\examples\python
