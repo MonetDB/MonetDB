723bbeb37c1213b8b6998f26877d54041a63e02f
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2021-01-18 14:47:07 +0100

    Don't generate the same column/index bind statement twice

--- sql/test/BugTracker-2016/Tests/decimal_vs_integer.Bug-3941.sql
+++ sql/test/BugTracker-2016/Tests/decimal_vs_integer.Bug-3941.sql
@@ -1,7 +1,8 @@
-
 start transaction;
 create table tmp(i decimal(8));
+set optimizer = 'sequential_pipe';
 explain select count(*) from tmp where i = 20160222;
 explain select count(*) from tmp where i = '20160222';
 explain select count(*) from tmp where i = 201602221;
-
+set optimizer = 'default_pipe';
+rollback;
