dd3ca305081d70191717e41908ffe1438dbd9e85
Martin van Dinther <martin.van.dinther@monetdbsolutions.com>
2021-01-20 21:41:31 +0100

    Add test for query which returns wrong result. It returns all rows instead of no rows.

---
+++ sql/test/bugs/Tests/rtrim_bug.sql
@@ -0,0 +1,22 @@
+CREATE TABLE t1 (m varchar (1) NOT NULL);
+INSERT into t1 values ('0');
+INSERT into t1 values ('2');
+SELECT * FROM sys.t1;
+SELECT length("m") as data_length, "m" as data_value FROM "sys"."t1" WHERE "m" IS NOT NULL AND length("m") > 1;
+-- no rows is expected
+
+CREATE VIEW v1 as select "m" from t1 where m in (select m from sys.t1);
+SELECT * FROM v1;
+SELECT length("m") as data_length, "m" as data_value FROM "sys"."v1" WHERE "m" IS NOT NULL AND length("m") > 1;
+-- no rows is expected
+
+CREATE VIEW v2 as select "m" from t1 where rtrim(m) in (select rtrim(m) from sys.t1);
+SELECT * FROM v2;
+SELECT length("m") as data_length, "m" as data_value FROM "sys"."v2" WHERE "m" IS NOT NULL AND length("m") > 1;
+-- 2 rows returned !! should be 0 rows as with v1 !!
+-- This query produces wrong results!!
+
+DROP VIEW v1;
+DROP VIEW v2;
+DROP TABLE t1;
+
