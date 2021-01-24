7ebf12e1b1947fae728ea865fb0f4316439539c4
Niels Nes <niels@cwi.nl>
2021-01-24 22:38:33 +0100

    fix issue with temp allocator

--- sql/backends/monet5/sql.c
+++ sql/backends/monet5/sql.c
@@ -401,8 +401,10 @@ create_table_or_view(mvc *sql, char* sname, char *tname, sql_table *t, int temp)
 	if (t->idxs.set) {
 		for (n = t->idxs.set->h; n; n = n->next) {
 			sql_idx *i = n->data;
-			if (!mvc_copy_idx(sql, nt, i))
+			if (!mvc_copy_idx(sql, nt, i)) {
+				sql->sa = osa;
 				throw(SQL, "sql.catalog", SQLSTATE(42000) "CREATE TABLE: %s_%s_%s index conflicts", s->base.name, t->base.name, i->base.name);
+			}
 		}
 	}
 	if (t->keys.set) {
@@ -416,15 +418,19 @@ create_table_or_view(mvc *sql, char* sname, char *tname, sql_table *t, int temp)
 				sql->sa = osa;
 				return err;
 			}
-			if (!mvc_copy_key(sql, nt, k))
+			if (!mvc_copy_key(sql, nt, k)) {
+				sql->sa = osa;
 				throw(SQL, "sql.catalog", SQLSTATE(42000) "CREATE TABLE: %s_%s_%s constraint conflicts", s->base.name, t->base.name, k->base.name);
+			}
 		}
 	}
 	if (t->triggers.set) {
 		for (n = t->triggers.set->h; n; n = n->next) {
 			sql_trigger *tr = n->data;
-			if (mvc_copy_trigger(sql, nt, tr))
+			if (mvc_copy_trigger(sql, nt, tr)) {
+				sql->sa = osa;
 				throw(SQL, "sql.catalog", SQLSTATE(42000) "CREATE TABLE: %s_%s_%s trigger conflicts", s->base.name, t->base.name, nt->base.name);
+			}
 		}
 	}
 	/* also create dependencies when not renaming */
