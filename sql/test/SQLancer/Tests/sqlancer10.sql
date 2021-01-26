724b6662b7600a1a9c27656be66d5584a0501433
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2021-01-26 15:38:52 +0100

    SQLancer crash on join with union. Looking into it

--- sql/test/SQLancer/Tests/sqlancer10.sql
+++ sql/test/SQLancer/Tests/sqlancer10.sql
@@ -1,17 +1,18 @@
 START TRANSACTION;
-CREATE TABLE "sys"."t0" ("c0" TIME NOT NULL, "c1" VARCHAR(143),
-	CONSTRAINT "t0_c0_pkey" PRIMARY KEY ("c0"), CONSTRAINT "t0_c0_unique" UNIQUE ("c0"), CONSTRAINT "t0_c1_unique" UNIQUE ("c1"));
-COPY 7 RECORDS INTO "sys"."t0" FROM stdin USING DELIMITERS E'\t',E'\n','"';
-21:19:08	""
-13:02:49	NULL
-01:02:11	NULL
-16:34:25	NULL
-12:11:43	NULL
-10:35:38	NULL
-04:26:50	NULL
+CREATE TABLE "sys"."t0" ("c0" BOOLEAN NOT NULL,"c2" INTEGER,CONSTRAINT "t0_c0_pkey" PRIMARY KEY ("c0"));
+INSERT INTO "sys"."t0" VALUES (true, 0);
 
-CREATE TABLE "sys"."t1" ("c0" CHAR(375) NOT NULL, CONSTRAINT "t1_c0_pkey" PRIMARY KEY ("c0"), CONSTRAINT "t1_c0_fkey" FOREIGN KEY ("c0") REFERENCES "sys"."t0" ("c1"));
-insert into t1 values ('');
-insert into t1(c0) values ((select 'a')), ('b');
-insert into t1(c0) values(r']BW扗}FUp'), (cast((values (greatest(r'Aᨐ', r'_'))) as string(616))), (r'');
+CREATE TABLE "sys"."t2" ("c0" DOUBLE NOT NULL,CONSTRAINT "t2_c0_pkey" PRIMARY KEY ("c0"),CONSTRAINT "t2_c0_unique" UNIQUE ("c0"));
+COPY 6 RECORDS INTO "sys"."t2" FROM stdin USING DELIMITERS E'\t',E'\n','"';
+8
+1
+-139590671
+542699836
+0.852979835289385
+0.9886505493437159
+
+create view v1(vc0, vc1, vc2, vc3) as ((select 10, 7, 'n', 2 where false)
+union (select 2, -0.18, 'a', 2 from t2 as l0t2 join (values (0.23), (-0.24)) as sub0 on false)) with check option;
+
+select 1 from v1, t2, t0 join (select false) as sub0 on true where cast(t0.c0 as clob) between lower(v1.vc0) and v1.vc2;
 ROLLBACK;
---
+++ sql/test/SQLancer/Tests/sqlancer11.sql
@@ -0,0 +1,17 @@
+START TRANSACTION;
+CREATE TABLE "sys"."t0" ("c0" TIME NOT NULL, "c1" VARCHAR(143),
+	CONSTRAINT "t0_c0_pkey" PRIMARY KEY ("c0"), CONSTRAINT "t0_c0_unique" UNIQUE ("c0"), CONSTRAINT "t0_c1_unique" UNIQUE ("c1"));
+COPY 7 RECORDS INTO "sys"."t0" FROM stdin USING DELIMITERS E'\t',E'\n','"';
+21:19:08	""
+13:02:49	NULL
+01:02:11	NULL
+16:34:25	NULL
+12:11:43	NULL
+10:35:38	NULL
+04:26:50	NULL
+
+CREATE TABLE "sys"."t1" ("c0" CHAR(375) NOT NULL, CONSTRAINT "t1_c0_pkey" PRIMARY KEY ("c0"), CONSTRAINT "t1_c0_fkey" FOREIGN KEY ("c0") REFERENCES "sys"."t0" ("c1"));
+insert into t1 values ('');
+insert into t1(c0) values ((select 'a')), ('b');
+insert into t1(c0) values(r']BW扗}FUp'), (cast((values (greatest(r'Aᨐ', r'_'))) as string(616))), (r'');
+ROLLBACK;
