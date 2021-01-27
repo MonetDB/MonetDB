d7e48ebab76320fac017542635a8edd09ef0e2aa
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2021-01-27 13:08:24 +0100

    Small optimization needed for properties branch, if a select or inner join is not going to output tuples, replace the inner relations with a dummy projection

--- sql/server/rel_rewriter.c
+++ sql/server/rel_rewriter.c
@@ -176,8 +176,31 @@ rewrite_simplify(visitor *v, sql_rel *rel)
 	if (!rel)
 		return rel;
 
-	if ((is_select(rel->op) || is_join(rel->op) || is_semi(rel->op)) && !list_empty(rel->exps))
+	if ((is_select(rel->op) || is_join(rel->op) || is_semi(rel->op)) && !list_empty(rel->exps)) {
 		rel->exps = exps_simplify_exp(v, rel->exps);
+		/* At a select or inner join relation if the single expression is false, eliminate the inner relations with a dummy projection */
+		if (list_length(rel->exps) == 1 && exp_is_false(rel->exps->h->data)) {
+			if ((is_select(rel->op) || (is_innerjoin(rel->op) && !rel_is_ref(rel->r))) && rel->card > CARD_ATOM && !rel_is_ref(rel->l)) {
+				list *nexps = sa_list(v->sql->sa), *toconvert = rel_projections(v->sql, rel->l, NULL, 1, 1);
+				if (is_innerjoin(rel->op))
+					toconvert = list_merge(toconvert, rel_projections(v->sql, rel->r, NULL, 1, 1), NULL);
+
+				for (node *n = toconvert->h ; n ; n = n->next) {
+					sql_exp *e = n->data, *a = exp_atom(v->sql->sa, atom_general(v->sql->sa, exp_subtype(e), NULL));
+					exp_prop_alias(v->sql->sa, a, e);
+					list_append(nexps, a);
+				}
+				rel_destroy(rel->l);
+				if (is_innerjoin(rel->op)) {
+					rel_destroy(rel->r);
+					rel->r = NULL;
+					rel->op = op_select;
+				}
+				rel->l = rel_project(v->sql->sa, NULL, nexps);
+				rel->card = CARD_ATOM;
+			}
+		}
+	}
 	return rel;
 }
 
