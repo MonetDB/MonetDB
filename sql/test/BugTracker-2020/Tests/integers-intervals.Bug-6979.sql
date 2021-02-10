0ac4bf1da7347c8a7634a36df8c32c2d69559a85
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2021-02-10 10:15:22 +0100

    Make test deterministic

--- sql/test/BugTracker-2020/Tests/integers-intervals.Bug-6979.sql
+++ sql/test/BugTracker-2020/Tests/integers-intervals.Bug-6979.sql
@@ -1,1 +1,1 @@
-select extract(second from interval '3600' second), extract(hour from interval '3600' second), extract(epoch from interval '3600' second), extract(epoch from now() - now());
+select extract(second from interval '3600' second), extract(hour from interval '3600' second), extract(epoch from interval '3600' second), extract(epoch from timestamp '2010-10-01 10:00:00' - timestamp '2010-10-01 10:00:00');
