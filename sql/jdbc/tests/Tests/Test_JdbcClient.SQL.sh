f926b46d3cc58e3a2396c548e257d518f201feb2
Martin van Dinther <martin.van.dinther@monetdbsolutions.com>
2021-02-17 20:41:40 +0100

    Change full name of JdbcClient program to the new package name (org.monetdb.client), so it will be tested from now on.

--- sql/jdbc/tests/Tests/Test_JdbcClient.SQL.sh
+++ sql/jdbc/tests/Tests/Test_JdbcClient.SQL.sh
@@ -5,11 +5,11 @@ user=monetdb
 password=monetdb
 EOF
 
-Mlog -x "java nl.cwi.monetdb.client.JdbcClient -h $HOST -p $MAPIPORT -d $TSTDB --help"
+Mlog -x "java org.monetdb.client.JdbcClient -h $HOST -p $MAPIPORT -d $TSTDB --help"
 
-Mlog -x "java nl.cwi.monetdb.client.JdbcClient -h $HOST -p $MAPIPORT -d $TSTDB -f $TSTSRCBASE/$TSTDIR/Tests/JdbcClient_create_tables.sql"
-Mlog -x "java nl.cwi.monetdb.client.JdbcClient -h $HOST -p $MAPIPORT -d $TSTDB -f $TSTSRCBASE/$TSTDIR/Tests/JdbcClient_inserts_selects.sql"
-Mlog -x "java nl.cwi.monetdb.client.JdbcClient -h $HOST -p $MAPIPORT -d $TSTDB -D"
-Mlog -x "java nl.cwi.monetdb.client.JdbcClient -h $HOST -p $MAPIPORT -d $TSTDB -f $TSTSRCBASE/$TSTDIR/Tests/JdbcClient_drop_tables.sql"
+Mlog -x "java org.monetdb.client.JdbcClient -h $HOST -p $MAPIPORT -d $TSTDB -f $TSTSRCBASE/$TSTDIR/Tests/JdbcClient_create_tables.sql"
+Mlog -x "java org.monetdb.client.JdbcClient -h $HOST -p $MAPIPORT -d $TSTDB -f $TSTSRCBASE/$TSTDIR/Tests/JdbcClient_inserts_selects.sql"
+Mlog -x "java org.monetdb.client.JdbcClient -h $HOST -p $MAPIPORT -d $TSTDB -D"
+Mlog -x "java org.monetdb.client.JdbcClient -h $HOST -p $MAPIPORT -d $TSTDB -f $TSTSRCBASE/$TSTDIR/Tests/JdbcClient_drop_tables.sql"
 
 rm -f .monetdb
