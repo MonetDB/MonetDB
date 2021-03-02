29a838b7d2f8aef7015d157492fca7231b9de36c
svetlin <svetlin.stalinov@monetdbsolutions.com>
2021-03-02 15:52:47 +0100

    fix test

--- sql/test/sysmon/Tests/timeout_cross.SQL.py
+++ sql/test/sysmon/Tests/timeout_cross.SQL.py
@@ -8,8 +8,3 @@ with SQLTestCase() as tc:
     tc.execute("insert into bar(value) (select * from generate_series(0,10000));").assertSucceeded()
     tc.execute("call sys.setquerytimeout(1); select * from foo, bar;")\
         .assertFailed(err_code="HYT00", err_message="Query aborted due to timeout")
-    # test mergejoin_int with bigger data set
-    tc.execute("insert into foo(value) (select * from generate_series(0,10000000));").assertSucceeded()
-    tc.execute("insert into bar(value) (select * from generate_series(0,10000000));").assertSucceeded()
-    tc.execute("call sys.setquerytimeout(1); select * from foo as f join bar b on f.value=b.value;")\
-        .assertFailed(err_code="HYT00", err_message="Query aborted due to timeout")
