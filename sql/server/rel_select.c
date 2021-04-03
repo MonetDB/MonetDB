cf3dd74fb0c6f979ba6543e60569371c116ed750
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2021-04-03 12:30:14 +0200

    Also swap subquery to rhs on values lists

--- sql/server/rel_select.c
+++ sql/server/rel_select.c
@@ -1742,8 +1742,8 @@ rel_compare(sql_query *query, sql_rel *rel, symbol *sc, symbol *lo, symbol *ro, 
 		compare_op = "=";
 	}
 
-	if (!ro2 && (lo->token == SQL_SELECT || lo->token == SQL_UNION || lo->token == SQL_EXCEPT || lo->token == SQL_INTERSECT) &&
-		(ro->token != SQL_SELECT && ro->token != SQL_UNION && ro->token != SQL_EXCEPT && ro->token != SQL_INTERSECT)) {
+	if (!ro2 && (lo->token == SQL_SELECT || lo->token == SQL_UNION || lo->token == SQL_EXCEPT || lo->token == SQL_INTERSECT || lo->token == SQL_VALUES) &&
+		(ro->token != SQL_SELECT && ro->token != SQL_UNION && ro->token != SQL_EXCEPT && ro->token != SQL_INTERSECT && ro->token != SQL_VALUES)) {
 		symbol *tmp = lo; /* swap subquery to the right hand side */
 
 		lo = ro;
