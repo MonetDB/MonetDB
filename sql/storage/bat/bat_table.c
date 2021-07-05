888724966d19e8c8bcb54f87912417d5aa6fffbb
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2021-07-05 11:17:03 +0200

    More allocation failures and don't forget to unfix update ids

--- sql/storage/bat/bat_table.c
+++ sql/storage/bat/bat_table.c
@@ -36,23 +36,32 @@ full_column(sql_trans *tr, sql_column *c)
 	BAT *b = store->storage_api.bind_col(tr, c, RDONLY);
 	BAT *ui = store->storage_api.bind_col(tr, c, RD_UPD_ID);
 
-	if (BATcount(ui)) {
-		BAT *uv = store->storage_api.bind_col(tr, c, RD_UPD_VAL);
-		if (ui && BATcount(ui)) {
-			BAT *r = COLcopy(b, b->ttype, true, TRANSIENT);
-
-			bat_destroy(b);
-			b = r;
-	    	if (!b || !ui || !uv || BATreplace(b, ui, uv, true) != GDK_SUCCEED) {
-				if (b) BBPunfix(b->batCacheid);
-				if (ui) BBPunfix(ui->batCacheid);
-				if (uv) BBPunfix(uv->batCacheid);
-				return NULL;
-			}
-		}
+	if (!b || !ui) {
+		bat_destroy(b);
 		bat_destroy(ui);
+		return NULL;
+	}
+	if (BATcount(ui)) {
+		BAT *uv = store->storage_api.bind_col(tr, c, RD_UPD_VAL), *r;
+
+		if (!uv) {
+			bat_destroy(b);
+			bat_destroy(ui);
+			return NULL;
+		}
+
+		r = COLcopy(b, b->ttype, true, TRANSIENT);
+		bat_destroy(b);
+		b = r;
+		if (!b || BATreplace(b, ui, uv, true) != GDK_SUCCEED) {
+			bat_destroy(b);
+			bat_destroy(ui);
+			bat_destroy(uv);
+			return NULL;
+		}
 		bat_destroy(uv);
 	}
+	bat_destroy(ui);
 	return b;
 }
 
