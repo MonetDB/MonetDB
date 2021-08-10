dbfb3575178475242f3d310c0ace2c7e41db071f
Sjoerd Mullender <sjoerd@acm.org>
2021-08-10 09:52:09 +0200

    Copy-paste error.

--- clients/mapiclient/mclient.c
+++ clients/mapiclient/mclient.c
@@ -1679,24 +1679,24 @@ setFormatter(const char *s)
 		noquote = true;
 		formatter = CSVformatter;
 		separator = strdup(",");
-	} else if (strncmp(s, "csv-noquote=", 4) == 0) {
+	} else if (strncmp(s, "csv-noquote=", 12) == 0) {
 		noquote = true;
 		formatter = CSVformatter;
-		if (s[4] == '"') {
-			separator = strdup(s + 5);
+		if (s[12] == '"') {
+			separator = strdup(s + 13);
 			if (separator[strlen(separator) - 1] == '"')
 				separator[strlen(separator) - 1] = 0;
 		} else
-			separator = strdup(s + 4);
-	} else if (strncmp(s, "csv-noquote+", 4) == 0) {
+			separator = strdup(s + 12);
+	} else if (strncmp(s, "csv-noquote+", 12) == 0) {
 		noquote = true;
 		formatter = CSVformatter;
-		if (s[4] == '"') {
-			separator = strdup(s + 5);
+		if (s[12] == '"') {
+			separator = strdup(s + 13);
 			if (separator[strlen(separator) - 1] == '"')
 				separator[strlen(separator) - 1] = 0;
 		} else
-			separator = strdup(s + 4);
+			separator = strdup(s + 12);
 		csvheader = true;
 	} else if (strcmp(s, "tab") == 0) {
 		formatter = CSVformatter;
