a5cb204634b83725258a050dbdb9921344e105e9
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2021-08-10 18:12:11 +0100

    Flag changes please

--- sql/server/rel_optimizer.c
+++ sql/server/rel_optimizer.c
@@ -9529,8 +9529,8 @@ rel_basecount(visitor *v, sql_rel *rel)
 	if (is_groupby(rel->op) && rel->l && !rel->r && list_length(rel->exps) == 1 && exp_aggr_is_count(rel->exps->h->data)) {
 		sql_rel *bt = rel->l;
 		sql_exp *e = rel->exps->h->data;
-		if (is_basetable(bt->op) && !e->l) { /* count(*) */
-			/* change into select cnt('schema','table') */;
+		if (is_basetable(bt->op) && list_empty(e->l)) { /* count(*) */
+			/* change into select cnt('schema','table') */
 			sql_table *t = bt->l;
 			/* I need to get the declared table's frame number to make this work correctly for those */
 			if (!isTable(t) || isDeclaredTable(t))
@@ -9543,9 +9543,9 @@ rel_basecount(visitor *v, sql_rel *rel)
 
 			ne = exp_propagate(v->sql->sa, ne, e);
 			exp_setname(v->sql->sa, ne, exp_find_rel_name(e), exp_name(e));
-			return rel_project(v->sql->sa, NULL, append(sa_list(v->sql->sa), ne));
+			rel = rel_project(v->sql->sa, NULL, append(sa_list(v->sql->sa), ne));
+			v->changes++;
 		}
-		return rel;
 	}
 	return rel;
 }
