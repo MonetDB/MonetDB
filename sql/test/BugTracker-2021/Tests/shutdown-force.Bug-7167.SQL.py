40af39dfc6c382dd42bd758b9e613e15a646b0ef
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2021-08-19 16:06:51 +0100

    Added test for bug #7167

---
+++ sql/test/BugTracker-2021/Tests/shutdown-force.Bug-7167.SQL.py
@@ -0,0 +1,26 @@
+import os, socket, tempfile, sys
+
+from MonetDBtesting.sqltest import SQLTestCase
+try:
+    from MonetDBtesting import process
+except ImportError:
+    import process
+
+def freeport():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('', 0))
+    port = sock.getsockname()[1]
+    sock.close()
+    return port
+
+myport = freeport()
+
+with tempfile.TemporaryDirectory() as farm_dir:
+    os.mkdir(os.path.join(farm_dir, 'db1'))
+    with process.server(mapiport=myport, dbname='db1', dbfarm=os.path.join(farm_dir, 'db1'),
+                        stdin=process.PIPE, stdout=process.PIPE, stderr=process.PIPE) as srv:
+        with SQLTestCase() as tc:
+            tc.connect(username="monetdb", password="monetdb", port=myport, database='db1')
+            tc.execute("call sys.shutdown(1, true);")
+        out, err = srv.communicate()
+        sys.stderr.write(err)
