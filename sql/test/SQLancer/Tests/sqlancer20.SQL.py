1a7d41443bff12cf808dc2ad9ff36b8b61a14582
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2021-10-12 14:39:33 +0200

    Issue to be looked on later. Remote relation gets an extra projection while optimized before generating MAL code, which causes issues later

--- sql/test/SQLancer/Tests/sqlancer20.SQL.py
+++ sql/test/SQLancer/Tests/sqlancer20.SQL.py
@@ -10,6 +10,8 @@ with SQLTestCase() as cli:
     cli.connect(username="monetdb", password="monetdb")
     cli.execute("""
     START TRANSACTION;
+    CREATE TABLE "t0" ("c0" INTERVAL SECOND NOT NULL, "c1" JSON);
+    INSERT INTO "t0" VALUES (INTERVAL '9' SECOND, '""');
     CREATE TABLE "t1" ("c0" BIGINT,"c1" INTERVAL MONTH);
     INSERT INTO "t1" VALUES (1, INTERVAL '9' MONTH),(5, INTERVAL '6' MONTH),(5, NULL),(7, NULL),(2, INTERVAL '1' MONTH),(2, INTERVAL '1' MONTH);
     COMMIT;
@@ -17,6 +19,12 @@ with SQLTestCase() as cli:
     START TRANSACTION;
     CREATE REMOTE TABLE "rt1" ("c0" BIGINT,"c1" INTERVAL MONTH) ON 'mapi:monetdb://localhost:%s/%s/sys/t1';
     COMMIT;""" % (port, db)).assertSucceeded()
+
+    cli.execute("""
+        SELECT 1 FROM t0 FULL OUTER JOIN (SELECT rt1.c0 <= CAST(rt1.c0 AS INT) FROM rt1) AS sub0(c0) ON true WHERE sub0.c0
+        UNION ALL
+        SELECT 1 FROM t0 FULL OUTER JOIN (SELECT rt1.c0 <= CAST(rt1.c0 AS INT) FROM rt1) AS sub0(c0) ON true;
+        """).assertSucceeded().assertDataResultMatch([(1,),(1,),(1,),(1,),(1,),(1,),(1,),(1,),(1,),(1,),(1,),(1,)])
 
     # Issues related to digits and scale propagation in the sql layer
     cli.execute("SELECT CAST(2 AS DECIMAL) & CAST(3 AS DOUBLE) FROM t1 where t1.c0 = 1;") \
@@ -27,5 +35,6 @@ with SQLTestCase() as cli:
     cli.execute("""
     START TRANSACTION;
     DROP TABLE rt1;
+    DROP TABLE t0;
     DROP TABLE t1;
     COMMIT;""").assertSucceeded()
