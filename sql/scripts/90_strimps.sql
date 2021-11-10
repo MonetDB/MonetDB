b3f4e4dd641536aab03b4a671ca477f6bd1234e5
Panagiotis Koutsourakis <kutsurak@monetdbsolutions.com>
2021-11-10 18:21:03 +0200

    Put SQL strimp related functions in sys schema

--- sql/scripts/90_strimps.sql
+++ sql/scripts/90_strimps.sql
@@ -1,7 +1,5 @@
-create schema strimps;
+CREATE FILTER FUNCTION sys.strimp_filter(strs STRING, q STRING) EXTERNAL NAME strimps.strimpfilter;
+GRANT EXECUTE ON FILTER FUNCTION sys.strimp_filter TO PUBLIC;
 
-CREATE FILTER FUNCTION strimps.filter(strs STRING, q STRING) EXTERNAL NAME strimps.strimpfilter;
-GRANT EXECUTE ON FILTER FUNCTION strimps.filter TO PUBLIC;
-
-CREATE PROCEDURE sys.createstrimps(sch string, tab string, col string)
+CREATE PROCEDURE sys.strimp_create(sch string, tab string, col string)
        EXTERNAL NAME sql.createstrimps;
