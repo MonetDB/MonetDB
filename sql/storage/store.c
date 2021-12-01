bdebfb9fd0a7f37eabef4ed32cda50c8845bfba6
Aris Koning <aris.koning@monetdbsolutions.com>
2021-12-01 19:55:18 +0100

    Add mangled_name column to functions

--- sql/storage/store.c
+++ sql/storage/store.c
@@ -1847,6 +1847,7 @@ store_load(sqlstore *store, sql_allocator *pa)
 	functions = t = bootstrap_create_table(tr, s, "functions", 2016);
 	bootstrap_create_column(tr, t, "id", 2017, "int", 32);
 	bootstrap_create_column(tr, t, "name", 2018, "varchar", 256);
+	bootstrap_create_column(tr, t, "mangled_name", 2165, "varchar", 256);
 	bootstrap_create_column(tr, t, "func", 2019, "varchar", 8196);
 	bootstrap_create_column(tr, t, "mod", 2020, "varchar", 8196);
 
