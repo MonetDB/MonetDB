fb36b796e7a506f436f31a21a1d8335092aa59a1
Sjoerd Mullender <sjoerd@acm.org>
2021-12-06 17:15:05 +0100

    Memory leak.

--- clients/mapiclient/dump.c
+++ clients/mapiclient/dump.c
@@ -1933,6 +1933,7 @@ dump_table_alters(Mapi mid, const char *schema, const char *tname, stream *toCon
 	}
 	rc = 0;						/* success */
   bailout:
+	free(query);
 	free(s);
 	free(t);
 	mapi_close_handle(hdl);		/* may be NULL */
