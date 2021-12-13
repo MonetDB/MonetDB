aa84969d691207a1a4f7cad1e20750a004f3ffd9
Niels Nes <niels@cwi.nl>
2021-12-13 16:56:25 +0100

    handle sequence numbers differently, ie log them only within the real sql_trans_commit code. ie store_sequnce structs are added to a list (seqchanges), which are logged in the first committing trans.

--- sql/storage/store_sequence.h
+++ sql/storage/store_sequence.h
@@ -14,11 +14,17 @@
 extern void* sequences_init(void);
 extern void sequences_exit(void);
 
+extern int seq_hash(void *seq);
+extern void seq_hash_destroy(sql_hash *h);
 extern int seq_get_value(sql_store store, sql_sequence *seq, lng *val);
 extern int seq_peak_next_value(sql_store store, sql_sequence *seq, lng *val);
 extern int seq_next_value(sql_store store, sql_sequence *seq, lng *val);
 extern int seq_restart(sql_store store, sql_sequence *seq, lng start);
 
+extern void log_store_sequence(sql_store store, void *seq); /* called locked */
 extern int seqbulk_next_value(sql_store store, sql_sequence *seq, lng cnt, lng* dest);
+
+extern void sequences_lock(sql_store store);
+extern void sequences_unlock(sql_store store);
 
 #endif /* STORE_SEQ_H */
