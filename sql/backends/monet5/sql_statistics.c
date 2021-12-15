a0cd92aef0c3f3febae95c519f5cde351478ac00
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2021-12-15 16:28:49 +0000

    Use lock instead of iterator

--- sql/backends/monet5/sql_statistics.c
+++ sql/backends/monet5/sql_statistics.c
@@ -131,10 +131,10 @@ sql_analyze(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci)
 
 					/* Collect new sorted and revsorted properties */
 					/* At the moment the heap must be locked around the BATordered calls to prevent a race condition on it */
-					BATiter bi = bat_iterator(b);
+					MT_lock_set(&b->theaplock);
 					(void) BATordered(b);
 					(void) BATordered_rev(b);
-					bat_iterator_end(&bi);
+					MT_lock_unset(&b->theaplock);
 
 					/* Check for nils existence */
 					(void) BATcount_no_nil(b, NULL);
