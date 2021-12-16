eb7f676a3ef64a46ad93d3de190d4b40ca3cb7d6
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2021-12-16 08:22:11 +0000

    Use min/max positions from iterator

--- sql/storage/bat/bat_storage.c
+++ sql/storage/bat/bat_storage.c
@@ -2342,9 +2342,9 @@ min_max_col(sql_trans *tr, sql_column *c)
 	_DELETE(c->max);
 	if ((d = ATOMIC_PTR_GET(&c->data)) && (b = temp_descriptor(d->cs.bid))) {
 		BATiter bi = bat_iterator(b);
-		if (b->tminpos != BUN_NONE && b->tmaxpos != BUN_NONE) {
-			void *nmin = BUNtail(bi, b->tminpos), *nmax = BUNtail(bi, b->tmaxpos);
-			size_t minlen = ATOMlen(b->ttype, nmin), maxlen = ATOMlen(b->ttype, nmax);
+		if (bi.minpos != BUN_NONE && bi.maxpos != BUN_NONE) {
+			void *nmin = BUNtail(bi, bi.minpos), *nmax = BUNtail(bi, bi.maxpos);
+			size_t minlen = ATOMlen(bi.type, nmin), maxlen = ATOMlen(bi.type, nmax);
 
 			if (!(c->min = GDKmalloc(minlen)) || !(c->max = GDKmalloc(maxlen))) {
 				_DELETE(c->min);
