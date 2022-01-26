dd3c71cf1c3bd8e6dafab7f1afc66fd420856b23
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2022-01-26 11:13:21 +0100

    Use the offset bat count, not the values one

--- sql/backends/monet5/dict.c
+++ sql/backends/monet5/dict.c
@@ -720,7 +720,7 @@ DICTthetaselect(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci)
 			} else
 				assert(0);
 		} else {
-			bn = BATdense(0, 0, op[0] == '!' ? BATcount(lv) : 0); /* for '!' if it didn't find, then all are different */
+			bn = BATdense(0, 0, op[0] == '!' ? BATcount(lo) : 0); /* for '!' if it didn't find, then all are different */
 		}
 	} else { /* select + intersect */
 		if (ATOMextern(lv->ttype))
