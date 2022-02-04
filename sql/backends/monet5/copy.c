0f78c78cc21fa92e4cfe8620f63e0579f6302097
Joeri van Ruth <joeri.van.ruth@monetdbsolutions.com>
2022-02-04 10:57:44 +0100

    Get copy.fixlines and copy.splitlines somewhat working

--- sql/backends/monet5/copy.c
+++ sql/backends/monet5/copy.c
@@ -9,12 +9,13 @@
 #include "monetdb_config.h"
 #include "gdk.h"
 #include "streams.h"
+#include "mel.h"
 #include "mal.h"
 #include "mal_errors.h"
 #include "mal_client.h"
 // #include "mal_instruction.h"
 #include "mal_exception.h"
-// #include "mal_interpreter.h"
+#include "mal_interpreter.h"
 
 
 #define bailout(f, ...) do { \
@@ -56,13 +57,55 @@ end:
 	return msg;
 }
 
+static int
+get_sep_char(str sep, bool backslash_escapes)
+{
+	if (strNil(sep))
+		return 0;
+	int c = *sep;
+
+	if (strlen(sep) != 1)
+		return -1;
+
+	switch (c) {
+		case '\\':
+		case '0':
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '7':
+		case '8':
+		case '9':
+		case 'a':
+		case 'b':
+		case 'c':
+		case 'd':
+		case 'e':
+		case 'f':
+		case 'x':
+		case 'u':
+		case 'U':
+		case 'n':
+		case 'r':
+		case 't':
+			if (backslash_escapes)
+				return -1;
+			break;
+		default:
+			break;
+	}
+	return c;
+}
 
 static str
 COPYfixlines(lng *ret_linecount, lng *ret_bytesmoved, bat *left_block, lng *left_skip_amount, bat *right_block, str *linesep_arg, str *quote_arg, bit *escape)
 {
 	str msg = MAL_SUCCEED;
-	char linesep, quote;
-	bool handle_escapes;
+	int linesep, quote;
+	bool backslash_escapes;
 	BAT *left = NULL, *right = NULL;
 	int start, left_size, right_size;
 	char *left_data, *right_data;
@@ -72,15 +115,18 @@ COPYfixlines(lng *ret_linecount, lng *ret_bytesmoved, bat *left_block, lng *left
 	bool quoted;
 	int borrow;
 
-
-	if (strNil(*linesep_arg) || strlen(*linesep_arg) != 1 || strNil(*quote_arg) || strlen(*quote_arg) != 1)
-		bailout("copy.fixlines", SQLSTATE(42000) "unsupported separator");
-	linesep = **linesep_arg;
-	quote = **quote_arg;
+	backslash_escapes = *escape;
+	linesep = get_sep_char(*linesep_arg, backslash_escapes);
+	if (linesep <= 0) // 0 not ok
+		bailout("copy.fixlines", SQLSTATE(42000) "invalid line separator");
+	quote = get_sep_char(*quote_arg, backslash_escapes);
+	if (quote < 0) // 0 is ok
+		bailout("copy.fixlines", SQLSTATE(42000) "invalid quote character");
+	if (linesep == quote)
+		bailout("copy.fixlines", SQLSTATE(42000) "line separator and quote character cannot be the same");
 
 	if (is_bat_nil(*left_block) || is_bat_nil(*right_block) || is_lng_nil(*left_skip_amount) || is_bit_nil(*escape))
 		bailout("copy.fixlines", "arguments must not be nil");
-	handle_escapes = *escape;
 
 	if ((left = BATdescriptor(*left_block)) == NULL || (right = BATdescriptor(*right_block)) == NULL)
 		bailout("copy.fixlines", SQLSTATE(HY002) RUNTIME_OBJECT_MISSING);
@@ -88,7 +134,6 @@ COPYfixlines(lng *ret_linecount, lng *ret_bytesmoved, bat *left_block, lng *left
 		bailout("copy.fixlines", SQLSTATE(42000) "block size too large");
 	if (BATcount(left) < (BUN)*left_skip_amount || *left_skip_amount > (lng)INT_MAX)
 		bailout("copy.fixlines", SQLSTATE(42000) "skip amount out of bounds");
-
 
 	// Scan 'left' for unquoted newlines. Determine both the count and the position
 	// of the last occurrence.
@@ -105,7 +150,7 @@ COPYfixlines(lng *ret_linecount, lng *ret_bytesmoved, bat *left_block, lng *left
 				escape_pending = false;
 				continue;
 			}
-			if (handle_escapes && left_data[i] == '\\') {
+			if (backslash_escapes && left_data[i] == '\\') {
 				escape_pending = true;
 				continue;
 			}
@@ -141,7 +186,7 @@ COPYfixlines(lng *ret_linecount, lng *ret_bytesmoved, bat *left_block, lng *left
 			escape_pending = false;
 			continue;
 		}
-		if (handle_escapes && left_data[i] == '\\') {
+		if (backslash_escapes && right_data[i] == '\\') {
 			escape_pending = true;
 			continue;
 		}
@@ -176,19 +221,452 @@ end:
 	return msg;
 }
 
+static int
+scan_octal_escape(unsigned char **rr, unsigned char **ww, unsigned char *end, int c)
+{
+	unsigned char *r = *rr + 2;
+	if (r >= end || *r < '0' || *r > '7')
+		goto end;
+	c = 8 * c + *r - '0';
+	r++;
+	if (r >= end || *r < '0' || *r > '7')
+		goto end;
+	c = 8 * c + *r - '0';
+	r++;
+end:
+	if (c > 0xFF)
+		return -1;
+	*rr = r;
+	*(*ww)++ = c;
+	return 0;
+}
+
+static int
+one_hex_digit(unsigned char **rr, unsigned char *end)
+{
+	if (*rr >= end)
+		return -1;
+	unsigned int d = **rr;
+	unsigned int d0 = d - '0';
+	unsigned int da = d - 'a';
+	unsigned int dA = d - 'A';
+	int v0 = ( d0 < 10 ? d0 + 1 : 0);
+	int va = (da < 6 ? da + 11 : 0);
+	int vA = (dA < 6 ? dA + 11 : 0);
+	int v =  v0 | va | vA;
+
+	// If there was a match, v is one too high, otherwise it's 0.
+	*rr += v > 0;
+	return v - 1;
+}
+
+static int
+scan_hex_escape(unsigned char **rr, unsigned char **ww, unsigned char *end)
+{
+	int acc;
+	*rr += 2;
+	int d = one_hex_digit(rr, end);
+	if (d < 0)
+		return d;
+	acc = d;
+	d = one_hex_digit(rr, end);
+	if (d >= 0) {
+		acc = 16 * acc + d;
+	}
+	if (acc > 0xFF)
+		return -1;
+	// rr has already been updated by one_hex_digit
+	*(*ww)++ = acc;
+	return 0;
+}
+
+static int
+utf8cont(unsigned int n, int shift)
+{
+	n = n >> shift;
+	n = n & 0x3F; // 0x3F == 0b0011_1111
+	n = n | 0x80; // 0x80 == 0b1000_0000
+	return n;
+}
+static int
+scan_unicode_escape(unsigned char **rr, unsigned char **ww, unsigned char *end, int digits)
+{
+	unsigned int acc = 0;
+	*rr += 2;
+	if (end - *rr < digits)
+		return -1;
+	for (int i = 0; i < digits; i++) {
+		int d = one_hex_digit(rr, end);
+		if (d < 0)
+			return d;
+		acc = 16 * acc + d;
+	}
+	if (acc == 0)
+		return -1;
+	else if (acc <      0x80) {
+		*(*ww)++ = acc;
+	} else if (acc <  0x0800) {
+		*(*ww)++ = (acc >> 6) | 0xC0; //    0xC0 == 0b1100_0000
+		*(*ww)++ = utf8cont(acc, 0);
+	} else if (acc <  0xD800) {
+		*(*ww)++ = (acc >> 12) | 0xE0; //   0xE0 == 0b1110_0000
+		*(*ww)++ = utf8cont(acc, 6);
+		*(*ww)++ = utf8cont(acc, 0);
+	} else if (acc <  0xE000) {
+		return -1; // reserved for utf16 surrogate halves
+	} else if (acc < 0x10000) {
+		*(*ww)++ = (acc >> 12) | 0xE0; //   0xE0 == 0b1110_0000
+		*(*ww)++ = utf8cont(acc, 6);
+		*(*ww)++ = utf8cont(acc, 0);
+	} else if (acc < 0x110000) {
+		*(*ww)++ = (acc >> 18) | 0xF0; //   0xF0 == 0b1111_0000
+		*(*ww)++ = utf8cont(acc, 12);
+		*(*ww)++ = utf8cont(acc, 6);
+		*(*ww)++ = utf8cont(acc, 0);
+	}
+	return 0;
+}
+
+static int
+scan_backslash_escape(unsigned char **rr, unsigned char **ww, unsigned char *end)
+{
+	unsigned char *r = *rr;
+	if (end - r < 2)
+		return -50;
+	assert(r[0] == '\\');
+	unsigned char c;
+	switch (r[1]) {
+		case '0':
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '7':
+			return scan_octal_escape(rr, ww, end, r[1] - '0');
+		case 'x':
+			return scan_hex_escape(rr, ww, end);
+		case 'u':
+			return scan_unicode_escape(rr, ww, end, 4);
+		case 'U':
+			return scan_unicode_escape(rr, ww, end, 8);
+		case 'a':
+			c = '\a';
+			break;
+		case 'b':
+			c = '\b';
+			break;
+		case 'f':
+			c = '\f';
+			break;
+		case 'n':
+			c = '\n';
+			break;
+		case 'r':
+			c = '\r';
+			break;
+		case 't':
+			c = '\t';
+			break;
+		default:
+			c = r[1];
+	}
+	*rr += 2;
+	*(*ww)++ = c;
+	return 0;
+}
+
+// Scan the text pointed to by 'start', replacing quote pairs with single
+// quote instances. Do not process backslash escapes but keep in mind that
+// when those are enabled, quote characters can be backslash escaped.
+// End at the first nondoubled nonquoted quote character and replace it with
+// a \0 character. Return the number of bytes scanned, or < 0 on error.
+// Never scan past 'end'. Reaching 'end' is considered an error.
+// Writes the number of bytes written, excluding the '\0', to '*nwritten'.
+static int
+scan_quoted(unsigned char *start, unsigned char *end, int quote, bool backslash_escapes, int *nwritten)
+{
+	if (start == end)
+		return -30;
+	unsigned char *last = end - 1;
+	unsigned char *r = start + 1;
+	unsigned char *w = start;
+
+	while (r <= last) {
+		assert(w <= r);
+		if (*r == '\0')
+			return -31;
+		if (*r == quote) {
+			if (r < last && r[1] == quote) {
+				// doubled quote, write only one
+				*w++ = quote;
+				r += 2;
+				continue;
+			} else {
+				// end quote found
+				*w = '\0';
+				*nwritten = w - start;
+				return r - start + 1;
+			}
+		} else if (backslash_escapes && *r == '\\') {
+			int ret = scan_backslash_escape(&r, &w, end);
+			if (ret < 0)
+				return ret;
+			continue;
+		} else {
+			// Some other character
+			*w++ = *r++;
+			continue;
+		}
+		assert(0 /* unreachable */);
+	}
+	return -32;
+}
+
+// Scan the text pointed to by 'start', looking for occurrences of 'col_sep'
+// or 'line_sep'. If found, replace it with a '\0' and return succesfully. .
+// Reaching 'end' is also considered an error.
+// If 'backslash_escapes' is set, backslashes suppress the special .
+// Return the number of bytes scanned, including the separator.
+// On succesful return, write the separator found to '*sep_found'.
+static int
+scan_unquoted(unsigned char *start, unsigned char *end, int col_sep, int line_sep, bool backslash_escapes, unsigned char *sep_found)
+{
+	char *sep;
+
+	if (!backslash_escapes) {
+		// is there a col_sep anywhere?
+		char *pcol = memchr(start, col_sep, end - start);
+		char *pline;
+		if (pcol) {
+			// there is a col_sep. is there a line_sep before that?
+			pline = memchr(start, line_sep, pcol - (char*)start);
+		} else {
+			// there is no col_sep, is there a line_sep anywhere?
+			pline = memchr(start, line_sep, end - start);
+		}
+		// pline is either earlier than pcol or there was no pcol.
+		sep = pline ? pline : pcol;
+		if (sep) {
+			*sep_found = *sep;
+			*sep = 0;
+			return sep - (char*)start + 1;
+		} else {
+			return -40;
+		}
+	}
+
+	// go over it character by character and convert backslash escapes
+	unsigned char *r = start;
+	unsigned char *w = start;
+	while (r < end) {
+		if (*r == col_sep || *r == line_sep) {
+			*sep_found = *r;
+			*w = 0;
+			return r - start + 1;
+		}
+		if (*r == '\\') {
+			int ret = scan_backslash_escape(&r, &w, end);
+			if (ret < 0)
+				return ret;
+		} else {
+			*r++ = *w++;
+		}
+	}
+	// no sep found is an error
+	return -40;
+}
+
+// Scan the text pointed to by 'start' up to the first occurrence of either
+// the column or the line separator, but never beyond 'end'.
+// Reaching 'end' is considered an error.
+// Remove quoting and process backslash escapes. Place a '\0' at the end of
+// the field, overwriting the separator or end quote.
+// Return the number of bytes scanned including the separator, or < 0 on
+// error. Write the separator found to '*sep_found'.
+static int
+scan_field(unsigned char *start, unsigned char *end, int col_sep, int line_sep, int quote, bool backslash_escapes, unsigned char *sep_found)
+{
+	if (start == end)
+		return -10;
+
+	int nread;
+	int nwritten;
+	if (quote && *start == quote) {
+
+		nread = scan_quoted(start, end, quote, backslash_escapes, &nwritten);
+		if (nread < 0)
+			return nread;
+		// scan_quoted errors out if it reaches 'end' so we know 'start[n]' exists
+		if (start[nread] == col_sep || start[nread] == line_sep) {
+			*sep_found = start[nread];
+			// nread should include the separator
+			nread += 1;
+		} else {
+			// end quote must be followed by separator
+			return -11;
+		}
+	} else {
+		nread = scan_unquoted(start, end, col_sep, line_sep, backslash_escapes, sep_found);
+		if (nread < 0)
+			return nread;
+		// with scan_unquoted, nread includes the separator, now replaced with '\0'
+		nwritten = nread - 1;
+	}
+
+	if (backslash_escapes) {
+		(void)nwritten;
+		(void)start;
+	}
+
+	return nread;
+}
+
+// Scan the memory 'start' .. 'end' for fields, unquoting and unescaping.
+// Place the index of each field in the appropriate index array.
+// 'ncols' is the number of columns. 'columns' is a pointer to an array of
+// size 'ncols' of pointers to the index arrays, each of which has room
+// for 'nrows' field indices.
+// Verify that column separators and row separators occur at the appropriate
+// moment. Verify that exactly the right amount of data is offered.
+// Return 0 on succes, < 0 on error.
+// If a field contains the null_repr, replace its index with int_nil.
+static int
+scan_fields(
+	char *start, int len,
+	int col_sep, int line_sep, int quote, bool backslash_escapes, char *null_repr,
+	int ncols, int nrows, int **columns)
+{
+	if (ncols < 0 || nrows < 0)
+		return -1;
+
+	unsigned char *p = (unsigned char*)start;
+	unsigned char *end = p + len;
+	int row = 0;
+	int col = 0;
+	while (p < end && row < nrows) {
+		unsigned char sep = 0;
+		int n = scan_field(p, end, col_sep, line_sep, quote, backslash_escapes, &sep);
+		if (n < 0) {
+			printf("ERROR row=%d col=%d\n", row, col);
+			return n;
+		}
+		bool last_col = col == ncols - 1;
+		int expected_sep = last_col ? line_sep : col_sep;
+		if (sep != expected_sep)
+			return -2;
+		bool is_null = (null_repr && strcasecmp((char*)p, null_repr) == 0);
+		int field = is_null ? int_nil : ((char*)p - start);
+		columns[col][row] = field;
+		p += n;
+		if (last_col) {
+			row += 1;
+			col = 0;
+		} else {
+			col += 1;
+		}
+	}
+
+	if (p < end) {
+		// leftover data
+		return -3;
+	}
+	if (row < nrows || col != 0) {
+		// too few rows
+		return -4;
+	}
+
+	return 0;
+}
 
 static str
 COPYsplitlines(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci)
 {
-	str msg = MAL_SUCCEED;
-
 	(void)cntxt;
 	(void)mb;
-	(void)stk;
-	(void)pci;
+	str msg = MAL_SUCCEED;
+	BAT *block_bat = NULL;
+	BAT **return_bats = NULL;
+	int **return_indices = NULL;
+	int ncols = pci->retc;
+	int line_sep, col_sep, quote;
+	char *data;
+	int len;
 
-	bailout("copy.splitlines", SQLSTATE(42000) "banana");
+	bat block_bat_id = *getArgReference_bat(stk, pci, pci->retc + 0);
+	int skip_amount = *getArgReference_int(stk, pci, pci->retc + 1);
+	lng line_count = *getArgReference_lng(stk, pci, pci->retc + 2);
+	str col_sep_str = *getArgReference_str(stk, pci, pci->retc + 3);
+	str line_sep_str = *getArgReference_str(stk, pci, pci->retc + 4);
+	str quote_str = *getArgReference_str(stk, pci, pci->retc + 5);
+	str null_repr = *getArgReference_str(stk, pci, pci->retc + 6);
+	bool backslash_escapes = *getArgReference_bit(stk, pci, pci->retc + 7);
+
+	line_sep = get_sep_char(line_sep_str, backslash_escapes);
+	if (line_sep <= 0) // 0 not ok
+		bailout("copy.splitlines", SQLSTATE(42000) "invalid line separator");
+	col_sep = get_sep_char(col_sep_str, backslash_escapes);
+	if (col_sep <= 0) // 0 is not ok
+		bailout("copy.splitlines", SQLSTATE(42000) "invalid column separator");
+	quote = get_sep_char(quote_str, backslash_escapes);
+	if (quote < 0) // 0 is ok
+		bailout("copy.splitlines", SQLSTATE(42000) "invalid quote character");
+
+	if (strNil(null_repr))
+		null_repr = NULL;
+
+	if ((block_bat = BATdescriptor(block_bat_id)) == NULL)
+		bailout("copy.splitlines", SQLSTATE(HY002) RUNTIME_OBJECT_MISSING);
+
+	assert(skip_amount >= 0);
+	assert( (line_count == 0) == (skip_amount == (int)BATcount(block_bat)) );
+
+	return_bats = GDKzalloc(ncols * sizeof(*return_bats));
+	return_indices = GDKzalloc(ncols * sizeof(*return_indices));
+	if (!return_bats || !return_indices)
+		bailout("copy.splitlines",  SQLSTATE(HY013) MAL_MALLOC_FAIL);
+
+	for (int i = 0; i < ncols; i++) {
+		BAT *b = COLnew(0, TYPE_int, line_count, TRANSIENT);
+		if (b == NULL)
+			bailout("copy.splitlines", SQLSTATE(HY013) MAL_MALLOC_FAIL);
+		return_bats[i] = b;
+		return_indices[i] = Tloc(b, 0);
+	}
+
+	data = Tloc(block_bat, skip_amount);
+	len = (int)BATcount(block_bat) - skip_amount;
+	int ret;
+	ret = scan_fields(
+		data, len,
+		col_sep, line_sep, quote, backslash_escapes, null_repr,
+		ncols, line_count,
+		return_indices);
+	if (ret < 0)
+		bailout("copy.splitlines", "field splitting failed");
+
 end:
+	if (block_bat)
+		BBPunfix(block_bat->batCacheid);
+	if (return_bats) {
+		for (int i = 0; i < ncols; i++) {
+			if (return_bats[i]) {
+				BAT *b = return_bats[i];
+				bat id = b->batCacheid;
+				if (msg == MAL_SUCCEED) {
+					BATcount(b) = line_count;
+					BBPkeepref(id);
+					*getArgReference_bat(stk, pci, i) = id;
+				}
+				else {
+					BBPunfix(id);
+				}
+			}
+		}
+		GDKfree(return_bats);
+	}
+	free(return_indices);
 	return msg;
 }
 
@@ -196,6 +674,10 @@ static str
 COPYparse_append(int *ret, int *mvc, str *s, str *t, str *c, oid *position, bat *positions, bat *block, bat *fields)
 {
 	str msg = MAL_SUCCEED;
+	BAT *indices_bat = BATdescriptor(*fields);
+
+	if (indices_bat == NULL)
+		bailout("copy.parse_append", SQLSTATE(HY002) RUNTIME_OBJECT_MISSING);
 
 	(void)ret;
 	(void)mvc;
@@ -207,27 +689,29 @@ COPYparse_append(int *ret, int *mvc, str *s, str *t, str *c, oid *position, bat 
 	(void)block;
 	(void)fields;
 
-	bailout("copy.parse_append", SQLSTATE(42000) "banana");
+	if (BATcount(indices_bat) > 0)
+		bailout("copy.parse_append", SQLSTATE(42000) "banana");
 end:
+	if (indices_bat)
+		BBPunfix(indices_bat->batCacheid);
 	return msg;
 }
 
 
-#include "mel.h"
 static mel_func copy_init_funcs[] = {
  command("copy", "read", COPYread, true, "Clear the BAT and read 'block_size' bytes into it from 's'",
 	args(1, 4,
 		arg("",lng),
-		arg("stream", streams), arg("block_size", int), batarg("block", bte)
+		arg("stream", streams), arg("block_size", lng), batarg("block", bte)
  )),
  command("copy", "fixlines", COPYfixlines, true, "Copy bytes from 'right' to 'left' to complete the final line of 'left'. Return left line count and bytes copied",
 	args(2, 7,
 	arg("linecount", lng), arg("bytesmoved", int),
 	batarg("left",bte), arg("left_skip", int), batarg("right", bte), arg("linesep", str), arg("quote", str),
  )),
- pattern("copy", "splitlines", COPYsplitlines, false, "Find the fields of the individual columns", args(1,8,
+ pattern("copy", "splitlines", COPYsplitlines, false, "Find the fields of the individual columns", args(1, 9,
 	batvararg("", int),
-	batarg("block", bte), arg("skip", int), arg("col_sep", str), arg("line_sep", str), arg("quote", str), arg("null_repr", str), arg("escape", bit)
+	batarg("block", bte), arg("skip", int), arg("linecount", lng), arg("col_sep", str), arg("line_sep", str), arg("quote", str), arg("null_repr", str), arg("escape", bit)
  )),
 
  command("copy", "parse_append", COPYparse_append, true, "parse the fields and append them to the column",
