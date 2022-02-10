21a4e32c5aede8ce44a64d67c7a35865f81ef182
Pedro Ferreira <pedro.ferreira@monetdbsolutions.com>
2022-02-10 12:19:35 +0100

    Check MAL recursive calls by tag at query queue, so it doesn't create issues when multiple complex function calls are done by multiple dataflow workers at the same time

--- sql/test/SQLancer/Tests/sqlancer19.SQL.py
+++ sql/test/SQLancer/Tests/sqlancer19.SQL.py
@@ -343,6 +343,17 @@ with SQLTestCase() as cli:
         .assertSucceeded().assertDataResultMatch([(True,)])
     cli.execute("SELECT 3 >= ALL(SELECT vx.vc0 FROM storage((SELECT 'sys', 't3' FROM rt3))) FROM (SELECT 0) vx(vc0);") \
         .assertSucceeded().assertDataResultMatch([(True,)])
+    cli.execute("""
+        SELECT 5 <> ALL((SELECT 2 FROM t3 FULL OUTER JOIN (SELECT 1) AS sub1n0(subc1n0) ON 2 < ANY(SELECT 1))
+        UNION ALL (SELECT 1 FROM t3 RIGHT OUTER JOIN (SELECT 4) AS sub1n0(subc1n0) ON TRUE
+        CROSS JOIN (SELECT FALSE FROM t3) AS sub1n1(subc1n0))) FROM t3;
+    """).assertSucceeded().assertDataResultMatch([(True,),(True,),(True,),(True,),(True,),(True,)])
+    # this query triggers a lot MAL user function calls at the moment. It allows to test concurrency issues with the query queue
+    cli.execute("""
+        SELECT 5 <> ALL((SELECT 2 FROM rt3 FULL OUTER JOIN (SELECT 1) AS sub1n0(subc1n0) ON 2 < ANY(SELECT 1))
+        UNION ALL (SELECT 1 FROM rt3 RIGHT OUTER JOIN (SELECT 4) AS sub1n0(subc1n0) ON TRUE
+        CROSS JOIN (SELECT FALSE FROM rt3) AS sub1n1(subc1n0))) FROM rt3;
+    """).assertSucceeded().assertDataResultMatch([(True,),(True,),(True,),(True,),(True,),(True,)])
     cli.execute("ROLLBACK;")
 
     cli.execute("""
