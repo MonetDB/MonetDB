702c4975e735f5c43d6840531bcd11dfe9cf6fa8
Joeri van Ruth <joeri.van.ruth@monetdbsolutions.com>
2022-02-18 16:09:11 +0100

    More detailed output in monetdb_snapshot.py

--- tools/merovingian/client/Tests/monetdb_snapshot.py
+++ tools/merovingian/client/Tests/monetdb_snapshot.py
@@ -30,8 +30,9 @@ with Runner(False) as run:
 
         header('CHECK SNAPDIR NOT SET')
         output = m.run_monetdbd('get', 'snapshotdir', output=True)
+        run.print(' -->', repr(output))
         assert '<unknown>' in output
-        note("""'<unknown>' in output""")
+        note("""'<unknown>' in output as expected""")
 
         header('TRY TO CREATE')
         try:
