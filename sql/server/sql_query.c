e2784a34bcc57646dddb18bfb4882314c5ea2cd7
Niels Nes <niels@cwi.nl>
2022-02-24 13:15:25 +0100

    improved query context/stack, handles more errors with incorrect combination of grouped/aggregated columns vs non aggregated cols.

--- sql/server/sql_query.c
+++ sql/server/sql_query.c
@@ -10,15 +10,27 @@
 #include "sql_query.h"
 #include "rel_rel.h"
 
+void
+query_processed(sql_query *query )
+{
+	query -> last_rel = NULL;
+	query -> last_exp = NULL;
+	query -> last_state = 0;
+	query -> last_card = 0;
+}
+
 static stacked_query *
-sq_create( sql_allocator *sa, sql_rel *rel, int sql_state)
+sq_create( sql_allocator *sa, sql_rel *rel, sql_exp *exp, sql_exp *prev, int sql_state, int card)
 {
 	stacked_query *q = SA_NEW(sa, stacked_query);
 
 	assert(rel);
 	*q = (stacked_query) {
 		.rel = rel,
+		.last_used = exp,
 		.sql_state = sql_state,
+		.prev = prev,
+		.used_card = card,
 		.grouped = is_groupby(rel->op)|rel->grouped,
 		.groupby = 0, /* not used for groupby of inner */
 	};
@@ -28,7 +40,7 @@ sq_create( sql_allocator *sa, sql_rel *rel, int sql_state)
 sql_query *
 query_create( mvc *sql)
 {
-	sql_query *q = SA_NEW(sql->sa, sql_query);
+	sql_query *q = SA_ZNEW(sql->sa, sql_query);
 
 	q->sql = sql;
 	q->outer = sql_stack_new(sql->sa, 32);
@@ -38,7 +50,18 @@ query_create( mvc *sql)
 void
 query_push_outer(sql_query *q, sql_rel *r, int sql_state)
 {
-	stacked_query *sq = sq_create(q->sql->sa, r, sql_state);
+	if (!q->last_rel)
+		q->last_rel = r;
+	if (r != q->last_rel) {
+		assert((q->last_rel->l == r && (is_groupby(q->last_rel->op) || is_select(q->last_rel->op))) || (r->l == q->last_rel && (is_select(r->op) || is_join(r->op))));
+		r->grouped = is_groupby(q->last_rel->op);
+		q->last_rel = r;
+		q->last_exp = q->prev = NULL;
+		q->last_state = 0;
+		q->last_card = 0;
+	}
+	assert(r== q->last_rel);
+	stacked_query *sq = sq_create(q->sql->sa, q->last_rel, q->last_exp, q->prev, sql_state/*q->last_state*/, q->last_card);
 	assert(sq);
 	sql_stack_push(q->outer, sq);
 }
@@ -48,6 +71,11 @@ query_pop_outer(sql_query *q)
 {
 	stacked_query *sq = sql_stack_pop(q->outer);
 	sql_rel *r = sq->rel;
+
+	q->last_rel = r;
+	q->last_exp = sq->last_used;
+	q->last_state = sq->sql_state;
+	q->last_card = sq->used_card;
 	return r;
 }
 
@@ -75,6 +103,7 @@ query_update_outer(sql_query *q, sql_rel *r, int i)
 	stacked_query *sq = sql_stack_fetch(q->outer, i);
 	sq->rel = r;
 	sq->last_used = NULL;
+	sq->sql_state |= is_groupby(r->op)?sql_aggr:0;
 	sq->used_card = 0;
 	sq->grouped = is_groupby(r->op);
 }
@@ -90,6 +119,11 @@ query_outer_used_exp(sql_query *q, int i, sql_exp *e, int f)
 {
 	stacked_query *sq = sql_stack_fetch(q->outer, i);
 
+	if (sq->last_used == NULL) {
+		sq->last_used = e;
+		sq->used_card = sq->rel->card;
+		return 0;
+	}
 	if (is_sql_aggr(f) && !is_sql_farg(f) && sq->groupby) /* cannot user outer both for inner groupby and aggregation */
 		return -1;
 	if (is_sql_groupby(f))
