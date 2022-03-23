bb88f1f3d9375b9914c9acf7ca24f94f2cc776df
Sjoerd Mullender <sjoerd@acm.org>
2022-03-23 17:22:13 +0100

    Use const.

--- sql/backends/monet5/sql_gencode.c
+++ sql/backends/monet5/sql_gencode.c
@@ -780,7 +780,7 @@ backend_dumpstmt(backend *be, MalBlkPtr mb, sql_rel *r, int top, int add_end, co
 			return -1;
 		}
 		setVarType(mb, getArg(q, 0), TYPE_void);
-		if (!(escaped_q = sql_escape_str(m->ta, (char*) query))) {
+		if (!(escaped_q = sql_escape_str(m->ta, query))) {
 			sql_error(m, 10, SQLSTATE(HY013) MAL_MALLOC_FAIL);
 			return -1;
 		}
