9670d772061611799b4a9813db8663808f991865
Niels Nes <niels@cwi.nl>
2022-03-28 21:47:10 +0200

    looping barriers are allowed in a dataflow/parallel pipeline block

--- monetdb5/mal/mal_function.c
+++ monetdb5/mal/mal_function.c
@@ -701,13 +701,14 @@ str
 chkDeclarations(MalBlkPtr mb){
 	int pc,i, k,l;
 	InstrPtr p, sig;
-	short blks[MAXDEPTH], top= 0, blkId=1;
+	short blks[MAXDEPTH], top= 0, blkId=1, curBlk;
 	int dflow = -1;
 	str msg = MAL_SUCCEED;
 
 	if( mb->errors)
 		return GDKstrdup(mb->errors);
 	blks[top] = blkId;
+	curBlk = blkId;
 
 	/* initialize the scope */
 	for(i=0; i< mb->vtop; i++)
@@ -716,7 +717,7 @@ chkDeclarations(MalBlkPtr mb){
 	/* all signature variables are declared at outer level */
 	sig= getInstrPtr(mb,0);
 	for(k=0; k<sig->argc; k++)
-		setVarScope(mb, getArg(sig, k), blkId);
+		setVarScope(mb, getArg(sig, k), curBlk);
 
 	for(pc=1;pc<mb->stop; pc++){
 		p= getInstrPtr(mb,pc);
@@ -786,9 +787,10 @@ chkDeclarations(MalBlkPtr mb){
 					dflow= blkId;
 				}
 				blks[++top]= blkId;
+				curBlk = blkId;
 			}
 			if( blockExit(p) && top > 0 ){
-				if( dflow == blkId){
+				if( dflow == curBlk){
 					dflow = -1;
 				} else
 				/*
@@ -802,6 +804,7 @@ chkDeclarations(MalBlkPtr mb){
 					clrVarInit(mb,l);
 				}
 			    top--;
+				curBlk=blks[top];
 			}
 		}
 	}
