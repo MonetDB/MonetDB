523c208f841bc2d864a8e5e7d188b6d3055f8bc9
Sjoerd Mullender <sjoerd@acm.org>
2022-04-11 12:35:23 +0200

    Scale was interpreted wrong when calculating average. The code wasn't actually used.

--- gdk/gdk_aggr.c
+++ gdk/gdk_aggr.c
@@ -1976,7 +1976,7 @@ BATgroupavg(BAT **bnp, BAT **cntsp, BAT *b, BAT *g, BAT *e, BAT *s, int tp, bool
 		dbl fac = pow(10.0, (double) scale);
 		for (i = 0; i < ngrp; i++) {
 			if (!is_dbl_nil(dbls[i]))
-				dbls[i] *= fac;
+				dbls[i] /= fac;
 		}
 	}
 	BATsetcount(bn, ngrp);
@@ -3048,7 +3048,7 @@ BATcalcavg(BAT *b, BAT *s, dbl *avg, BUN *vals, int scale)
 	}
 	bat_iterator_end(&bi);
 	if (scale != 0 && !is_dbl_nil(*avg))
-		*avg *= pow(10.0, (double) scale);
+		*avg /= pow(10.0, (double) scale);
 	if (vals)
 		*vals = (BUN) n;
 	return GDK_SUCCEED;
