399050304dbbc0d3afe038fd8835f28cabda3293
Sjoerd Mullender <sjoerd@acm.org>
2022-04-14 14:33:26 +0200

    When we encounter zero values in the WAL, we got to the end, so skip the rest.

--- gdk/gdk_logger.c
+++ gdk/gdk_logger.c
@@ -1141,6 +1141,12 @@ logger_readlog(logger *lg, char *filename, bool *filemissing)
 		char tpe;
 		oid id;
 
+		if (l.flag == 0) {
+			/* end of useful content */
+			assert(l.tid == 0);
+			assert(l.nr == 0);
+			break;
+		}
 		t1 = time(NULL);
 		if (t1 - t0 > 10) {
 			lng fpos;
@@ -1278,8 +1284,6 @@ logger_readlog(logger *lg, char *filename, bool *filemissing)
 				err = LOG_EOF;
 			else
 				err = log_read_clear(lg, tr, name, tpe, id);
-			break;
-		case 0:
 			break;
 		default:
 			err = LOG_ERR;
