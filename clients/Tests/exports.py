41f58d574808d737b83b7a9b3ba00ea05bd2f195
svetlin <svetlin.stalinov@monetdbsolutions.com>
2022-07-06 11:43:54 +0200

    small fix

--- clients/Tests/exports.py
+++ clients/Tests/exports.py
@@ -3,10 +3,6 @@ import difflib
 import MonetDBtesting.listexports
 output = MonetDBtesting.listexports.listexports()
 stable = open('exports.stable.out').readlines()
-try:
-    for line in difflib.unified_diff(stable, output):
-        sys.stderr.write(line)
-except Exception as e:
-        sys.stderr.write(str(e))
-        raise e
+for line in difflib.unified_diff(stable, output):
+    sys.stderr.write(line)
 
