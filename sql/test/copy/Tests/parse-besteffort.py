56cb0258d967d04a24164fca57a96969624bfc9d
Joeri van Ruth <joeri.van.ruth@monetdbsolutions.com>
2022-07-11 15:36:12 +0200

    Adapt the tests to BEST EFFORT now working as it's supposed to

--- sql/test/copy/Tests/parse-besteffort.py
+++ sql/test/copy/Tests/parse-besteffort.py
@@ -10,17 +10,6 @@ dec = Decimal
 suite = setup_suite(level=2)
 run_test = suite.run_test
 
-##############################################################################
-#
-# NOTE NOTE NOTE
-#
-# For the time being our BEST EFFORT implementation doesn't remove the rows,
-# it just replaces the values with NULLs.
-#
-# This means all of these tests will have to change when we change this behaviour.
-#
-##############################################################################
-
 
 # When the parse functions encounter an error we expect them to insert a nil
 # instead and log an error.
@@ -37,26 +26,14 @@ testdata = """\
 """
 tc = TestCase("i INT, d DECIMAL(5,2), t TEXT", testdata, besteffort=True)
 tc = (tc
-    .expect_affected(5)   # will be 2: row 1 and 5
+    .expect_affected(2)
     .expect_value(0, 0, 1)
     .expect_value(0, 1, 1)
     .expect_value(0, 2, 'hello')
     #
-    .expect_value(1, 0, None)   # besteffort for copy.parse_integer
-    .expect_value(1, 1, 2)
-    # .expect_value(1, 2, 'hi%%%%%')
-    #
-    .expect_value(2, 0, 3)
-    .expect_value(2, 1, None)    # besteffort for copy.parse_decimal
-    .expect_value(2, 2, 'good morning')
-    #
-    .expect_value(3, 0, 4)
-    .expect_value(3, 1, 4)
-    .expect_value(3, 2, None)    # besteffort for copy.parse_generic
-    #
-    .expect_value(4, 0, 5)
-    .expect_value(4, 1, 5)
-    .expect_value(4, 2, 'fine, thank you')
+    .expect_value(1, 0, 5)
+    .expect_value(1, 1, 5)
+    .expect_value(1, 2, 'fine, thank you')
     #
     .expect_reject(2, 1, 'unexpected character')
     .expect_reject(3, 2, 'too many decimal digits')
@@ -80,6 +57,7 @@ testdata = """\
 6|and an unterminated line\
 """
 tc = (TestCase("I INT, t TEXT", testdata, besteffort=True, raw=True, blocksize=13)
+    .expect_affected(4)
     .expect_reject(3, 1, "unexpected character")
     .expect_reject(6, None, "unterminated")
 )
@@ -93,12 +71,10 @@ d|e|f
 g|h|i
 """
 tc = (TestCase("s TEXT, t TEXT, u TEXT", testdata, besteffort=True)
+    .expect_affected(2)
     .expect_value(0, 0, 'a')
-    .expect_value(1, 0, None)
-    .expect_value(1, 1, None)
-    .expect_value(1, 2, None)
     # skipped line 2
-    .expect_value(2, 0, 'g')
+    .expect_value(1, 0, 'g')
 )
 run_test(tc.replace(1, r"d\x|e|f").expect_reject(2, 1, "incomplete hex"))
 run_test(tc.replace(1, r"d|e\x|f").expect_reject(2, 2, "incomplete hex"))
@@ -122,12 +98,10 @@ d|e|f
 g|h|i
 """
 tc = (TestCase("s TEXT, t TEXT, u TEXT", testdata, besteffort=True, quote='^')
+    .expect_affected(2)
     .expect_value(0, 0, 'a')
-    .expect_value(1, 0, None)
-    .expect_value(1, 1, None)
-    .expect_value(1, 2, None)
     # skipped line 2
-    .expect_value(2, 0, 'g')
+    .expect_value(1, 0, 'g')
 )
 run_test(tc.replace(1, r"d\x|e|f").expect_reject(2, 1, "incomplete hex"))
 run_test(tc.replace(1, r"d|e\x|f").expect_reject(2, 2, "incomplete hex"))
@@ -151,12 +125,10 @@ d|^e^|f
 g|^^^^|i
 """
 tc = (TestCase("s TEXT, t TEXT, u TEXT", testdata, besteffort=True, quote='^')
+    .expect_affected(2)
     .expect_value(0, 0, 'a')
-    .expect_value(1, 0, None)
-    .expect_value(1, 1, None)
-    .expect_value(1, 2, None)
     # skipped line 2
-    .expect_value(2, 0, 'g')
+    .expect_value(1, 0, 'g')
 )
 run_test(tc.replace(1, r"d\x|^e^|f").expect_reject(2, 1, "incomplete hex"))
 run_test(tc.replace(1, r"d|^e\x^|f").expect_reject(2, 2, "incomplete hex"))
