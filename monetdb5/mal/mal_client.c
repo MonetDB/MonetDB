9ac8c5f483769e26cea366e3c13243091d4035d9
Sjoerd Mullender <sjoerd@acm.org>
2022-07-13 08:57:19 +0200

    "Use" otherwise unused variable.

--- monetdb5/mal/mal_client.c
+++ monetdb5/mal/mal_client.c
@@ -301,6 +301,7 @@ MCinitClient(oid user, bstream *fin, stream *fout)
 
 	if (c) {
 		Client c_old = setClientContext(c);
+		(void) c_old;
 		assert(NULL == c_old);
 		c = MCinitClientRecord(c, user, fin, fout);
 	}
