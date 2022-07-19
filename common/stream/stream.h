3bc27b957687c1199a6c6aac676b69a9904f364b
Joeri van Ruth <joeri.van.ruth@monetdbsolutions.com>
2022-07-19 13:50:02 +0200

    Get rid of set_prompting
    We have the dedicated mapi_request_upload now.

--- common/stream/stream.h
+++ common/stream/stream.h
@@ -219,7 +219,6 @@ stream_export buffer *mnstr_get_buffer(stream *s);
 stream_export stream *block_stream(stream *s); // mapi.c, mal_mapi.c, client.c, merovingian
 stream_export bool isa_block_stream(const stream *s); // mapi.c, mal_client.c, remote.c, sql_scenario.c/sqlReader, sql_scan.c
 stream_export stream *bs_stream(stream *s); // unused
-stream_export void set_prompting(stream *block_stream, const char *prompt, stream *prompt_stream);
 
 
 stream_export stream *mapi_request_upload(const char *filename, bool binary, stream *from, stream *to);
