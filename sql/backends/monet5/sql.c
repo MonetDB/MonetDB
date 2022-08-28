dde733abc593022fc89fc437ba60d89f13f06f66
Niels Nes <niels@cwi.nl>
2022-08-28 14:49:25 +0200

    fixed merge problem

--- sql/backends/monet5/sql.c
+++ sql/backends/monet5/sql.c
@@ -143,7 +143,7 @@ sql_symbol2relation(backend *be, symbol *sym)
 	if (rel)
 		rel = sql_processrelation(be->mvc, rel, profile, 1, value_based_opt, storage_based_opt);
 	if (rel)
-		rel = rel_partition(be->mvc, rel);
+		rel_partition(be->mvc, rel);
 	if (rel && (rel_no_mitosis(be->mvc, rel) || rel_need_distinct_query(rel)))
 		be->no_mitosis = 1;
 	if (rel && (be->mvc->emode != m_plan || (GDKdebug & FORCEMITOMASK) == 0))
