918825294426ba4da3ff4b0f19ae9baf0fe0dbe0
Bernardo Mota <bernardo.mota@monetdbsolutions.com>
2022-10-19 15:15:58 +0100

    Added rtree function ID to optimizers, removed mitosis/mergetable optimizers on rtree functions.

--- monetdb5/optimizer/opt_mitosis.c
+++ monetdb5/optimizer/opt_mitosis.c
@@ -69,6 +69,10 @@ OPTmitosisImplementation(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci
 				goto bailout;
 			}
 
+		/* rtree functions should not be optimized by mitosis (single-threaded execution) */
+		if ( getModuleId(p) == rtreeRef)
+			goto bailout;
+
 		/* do not split up floating point bat that is being summed */
 		if (p->retc == 1 &&
 			(((p->argc == 5 || p->argc == 6) &&
