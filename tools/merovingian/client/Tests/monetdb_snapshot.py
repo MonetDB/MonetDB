c76a122028ad2f9284a1e8f6a2cceb07c77f378a
Sjoerd Mullender <sjoerd@acm.org>
2022-11-11 21:46:16 +0100

    Module pipes is deprecated in Python 3.11, use shlex instead.

--- tools/merovingian/client/Tests/monetdb_snapshot.py
+++ tools/merovingian/client/Tests/monetdb_snapshot.py
@@ -2,7 +2,7 @@
 
 import locale
 import os
-import pipes
+import shlex
 import re
 import subprocess
 import sys
@@ -97,7 +97,7 @@ with Runner(False) as run:
 
         header('CUSTOM FILENAME')
         custom_name = os.path.join(m.snapdir, 'snap.tar')
-        qcustom_name = pipes.quote(custom_name)
+        qcustom_name = shlex.quote(custom_name)
         m.run_monetdb('snapshot', 'create', '-t', qcustom_name, 'foo1')
         assert os.path.exists(custom_name)
         note("""os.path.exists(custom_name)""")
