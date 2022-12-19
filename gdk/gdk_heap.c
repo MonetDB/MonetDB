b697b073ea15c0db4c6b52aa5574906cb2f00a5c
Sjoerd Mullender <sjoerd@acm.org>
2022-12-19 12:26:26 +0100

    Error checking.

--- gdk/gdk_heap.c
+++ gdk/gdk_heap.c
@@ -94,8 +94,7 @@ HEAPgrow(Heap **hp, size_t size, bool mayshare)
 
 	ATOMIC_BASE_TYPE refs = ATOMIC_GET(&(*hp)->refs);
 	if ((refs & HEAPREFS) == 1) {
-		gdk_return rc = HEAPextend((*hp), size, mayshare);
-		return rc;
+		return HEAPextend((*hp), size, mayshare);
 	}
 	new = GDKmalloc(sizeof(Heap));
 	if (new != NULL) {
