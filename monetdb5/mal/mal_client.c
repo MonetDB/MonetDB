36b7d25e96827ffc20bdedc3d56b04c7459ad03f
Sjoerd Mullender <sjoerd@acm.org>
2023-01-13 15:13:50 +0100

    Use mal_contextLock to protect cntxt->idle.

--- monetdb5/mal/mal_client.c
+++ monetdb5/mal/mal_client.c
@@ -550,9 +550,11 @@ MCactiveClients(void)
 	int active = 0;
 	Client cntxt = mal_clients;
 
+	MT_lock_set(&mal_contextLock);
 	for(cntxt = mal_clients;  cntxt<mal_clients+MAL_MAXCLIENTS; cntxt++){
 		active += (cntxt->idle == 0 && cntxt->mode == RUNCLIENT);
 	}
+	MT_lock_unset(&mal_contextLock);
 	return active;
 }
 
@@ -567,14 +569,19 @@ MCmemoryClaim(void)
 
 	Client cntxt = mal_clients;
 
-	for(cntxt = mal_clients;  cntxt<mal_clients+MAL_MAXCLIENTS; cntxt++)
-	if( cntxt->idle == 0 && cntxt->mode == RUNCLIENT){
-		if(cntxt->memorylimit){
-			claim += cntxt->memorylimit;
-			active ++;
-		} else
-			return GDK_mem_maxsize;
+	MT_lock_set(&mal_contextLock);
+	for(cntxt = mal_clients;  cntxt<mal_clients+MAL_MAXCLIENTS; cntxt++) {
+		if( cntxt->idle == 0 && cntxt->mode == RUNCLIENT){
+			if(cntxt->memorylimit){
+				claim += cntxt->memorylimit;
+				active ++;
+			} else {
+				MT_lock_unset(&mal_contextLock);
+				return GDK_mem_maxsize;
+			}
+		}
 	}
+	MT_lock_unset(&mal_contextLock);
 	if(active == 0 ||  claim  * LL_CONSTANT(1048576) >= GDK_mem_maxsize)
 		return GDK_mem_maxsize;
 	return claim * LL_CONSTANT(1048576);
