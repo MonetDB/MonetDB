f1e2ffd256dcbf05c7b5bcf243795c19444e976f
Sjoerd Mullender <sjoerd@acm.org>
2023-01-23 15:31:43 +0100

    Fixed a regression where dependencies weren't always properly checked. This is a rollback of changeset 525a778475ae.

--- sql/storage/store.c
+++ sql/storage/store.c
@@ -3928,7 +3928,6 @@ sql_trans_commit(sql_trans *tr)
 		}
 
 		if (!tr->parent &&
-			ATOMIC_GET(&store->nr_active) == 1 &&
 			(!list_empty(tr->dependencies) || !list_empty(tr->depchanges))) {
 			ok = transaction_check_dependencies_and_removals(tr);
 			if (ok != LOG_OK) {
