2230eb290c46dd3a01e11b414379d070c5fea174
Niels Nes <niels@cwi.nl>
2023-02-10 14:21:40 +0100

    proper order of conditions, ie only for UPDATE logactions cleanup the uid bat

--- gdk/gdk_logger.c
+++ gdk/gdk_logger.c
@@ -879,9 +879,9 @@ la_apply(logger *lg, logaction *c, int tid)
 static void
 la_destroy(logaction *c)
 {
-	if (c->b && (c->type == LOG_UPDATE || c->type == LOG_UPDATE_BULK))
+	if ((c->type == LOG_UPDATE || c->type == LOG_UPDATE_BULK) && c->b)
 		logbat_destroy(c->b);
-	if (c->uid && c->type == LOG_UPDATE)
+	if (c->type == LOG_UPDATE && c->uid)
 		logbat_destroy(c->uid);
 }
 
