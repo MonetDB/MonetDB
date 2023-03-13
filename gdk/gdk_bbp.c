608ddc846e41ba73d309c7fdf803b2b1e188cd6f
Sjoerd Mullender <sjoerd@acm.org>
2023-03-13 19:25:09 +0100

    Don't trim bats during a (sub)commit.

--- gdk/gdk_bbp.c
+++ gdk/gdk_bbp.c
@@ -1185,6 +1185,8 @@ BBPtrim(bool aggressive)
 	if (!aggressive)
 		flag |= BBPHOT;
 	for (bat bid = 1, nbat = (bat) ATOMIC_GET(&BBPsize); bid < nbat; bid++) {
+		/* don't do this during a (sub)commit */
+		MT_lock_set(&GDKtmLock);
 		MT_lock_set(&GDKswapLock(bid));
 		BAT *b = NULL;
 		bool swap = false;
@@ -1210,6 +1212,7 @@ BBPtrim(bool aggressive)
 				GDKerror("unload failed for bat %d", bid);
 			n++;
 		}
+		MT_lock_unset(&GDKtmLock);
 	}
 	TRC_DEBUG(BAT_, "unloaded %d bats%s\n", n, aggressive ? " (also hot)" : "");
 }
