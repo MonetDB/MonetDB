128d9c1d35758375d8e7fa13480f3eb7dcb2e5bf
Sjoerd Mullender <sjoerd@acm.org>
2023-03-28 10:02:03 +0200

    Remove unused function MCforkClient.

--- monetdb5/mal/mal_client.h
+++ monetdb5/mal/mal_client.h
@@ -179,7 +179,6 @@ mal_export ClientRec *mal_clients;
 
 mal_export Client  MCgetClient(int id);
 mal_export Client  MCinitClient(oid user, bstream *fin, stream *fout);
-mal_export Client  MCforkClient(Client father);
 mal_export void	   MCstopClients(Client c);
 mal_export int	   MCactiveClients(void);
 mal_export void    MCcloseClient(Client c);
