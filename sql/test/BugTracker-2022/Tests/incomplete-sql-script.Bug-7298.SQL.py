2b11acc7e1b5033ca4bd5e1ec5be5376f4955f46
Niels Nes <niels@cwi.nl>
2023-04-23 19:50:13 +0200

    approved output

--- sql/test/BugTracker-2022/Tests/incomplete-sql-script.Bug-7298.SQL.py
+++ sql/test/BugTracker-2022/Tests/incomplete-sql-script.Bug-7298.SQL.py
@@ -1,6 +1,7 @@
 from MonetDBtesting.sqltest import SQLTestCase
 import tempfile
 
+
 with SQLTestCase() as tc:
 
     with tempfile.TemporaryFile('w+') as tf:
@@ -9,7 +10,7 @@ with SQLTestCase() as tc:
         tf.flush()
         tf.seek(0)
 
-        tc.execute(None, '-fraw', client='mclient', stdin=tf).assertFailed(err_code="42000", err_message=['!syntax error, unexpected $end in: "select foo"', '!syntax error in: "select foo"'])
+        tc.execute(None, '-fraw', client='mclient', stdin=tf).assertFailed(err_code="42000", err_message=['!syntax error, unexpected end of file in: "select foo"'])
         tf.seek(0)
         tf.truncate(0)
 
@@ -27,7 +28,7 @@ with SQLTestCase() as tc:
         tf.flush()
         tf.seek(0)
 
-        tc.execute(None, '-fraw', client='mclient', stdin=tf).assertFailed(err_code="42000", err_message=['!syntax error, unexpected $end, expecting SCOLON in: "select foo."', '!syntax error in: "select foo."'])
+        tc.execute(None, '-fraw', client='mclient', stdin=tf).assertFailed(err_code="42000", err_message=['!syntax error, unexpected end of file, expecting SCOLON in: "select foo."'])
         tf.seek(0)
         tf.truncate(0)
 
@@ -36,7 +37,7 @@ with SQLTestCase() as tc:
         tf.flush()
         tf.seek(0)
 
-        tc.execute(None, '-fraw', client='mclient', stdin=tf).assertFailed(err_code="42000", err_message=['!syntax error, unexpected $end, expecting SCOLON in: "select foo-"', '!syntax error in: "select foo-"'])
+        tc.execute(None, '-fraw', client='mclient', stdin=tf).assertFailed(err_code="42000", err_message=['!syntax error, unexpected end of file, expecting SCOLON in: "select foo-"'])
         tf.seek(0)
         tf.truncate(0)
 
@@ -45,7 +46,7 @@ with SQLTestCase() as tc:
         tf.flush()
         tf.seek(0)
 
-        tc.execute(None, '-fraw', client='mclient', stdin=tf).assertFailed(err_code="42000", err_message=['!syntax error, unexpected $end in: "select f001234"', '!syntax error in: "select f001234"'])
+        tc.execute(None, '-fraw', client='mclient', stdin=tf).assertFailed(err_code="42000", err_message=['!syntax error, unexpected end of file in: "select f001234"'])
         tf.seek(0)
         tf.truncate(0)
 
