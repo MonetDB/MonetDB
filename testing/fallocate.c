9ed2f92dae2613312989eaee9721915a0084653e
Niels Nes <niels@cwi.nl>
2023-05-23 13:08:35 +0200

    small library to test errors on large file creation using fallocate

---
+++ testing/fallocate.c
@@ -0,0 +1,26 @@
+
+//#define _GNU_SOURCE             /* See feature_test_macros(7) */
+#include <fcntl.h>
+#include <dlfcn.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+
+typedef int (*fallocate_fptr)(int, int, off_t, off_t);
+fallocate_fptr real_fallocate = NULL;
+
+int
+fallocate(int fd, int mode, off_t offset, off_t len)
+{
+	if (!real_fallocate)
+		real_fallocate = (fallocate_fptr)dlsym(RTLD_NEXT, "fallocate");
+	if (!real_fallocate) {
+		fprintf(stderr, "mtest_allocate: could not resolve 'fallocate' in 'libc.so': %s\n", dlerror());
+		exit(1);
+	}
+	if (len >= (512*1024*1024)) {
+		errno = ENOSPC;
+		return -1;
+	}
+	return real_fallocate(fd, mode, offset, len);
+}
