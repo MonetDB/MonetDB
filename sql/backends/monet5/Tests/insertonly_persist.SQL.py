28078554c46acc7e637d7bad14b9179eb443066f
Lucas Pereira <lucas.pereira@monetdbsolutions.com>
2023-09-28 15:32:01 +0200

    Sleep less.

--- sql/backends/monet5/Tests/insertonly_persist.SQL.py
+++ sql/backends/monet5/Tests/insertonly_persist.SQL.py
@@ -22,17 +22,15 @@ with tempfile.TemporaryDirectory() as farm_dir:
             tc.execute("CREATE LOADER up() LANGUAGE PYTHON { _emit.emit({'x': list(range(1,101))}) }").assertSucceeded()
             tc.execute("COPY LOADER INTO foo FROM up()").assertSucceeded()
             tc.execute("SELECT count(*) FROM foo").assertSucceeded().assertDataResultMatch([(100,)])
-            tc.execute("select sleep(2000)")
+            tc.execute("select sleep(5000)")
             tc.execute("SELECT * FROM insertonly_persist('sys')").assertSucceeded()
-            # tc.execute("select sleep(10000)")
         s.communicate()
 
-    with process.server(mapiport='0', dbname='db1',
-                        dbfarm=os.path.join(farm_dir, 'db1'),
-                        stdin=process.PIPE,
-                        stdout=process.PIPE, stderr=process.PIPE) as s:
-        with SQLTestCase() as tc:
-            tc.connect(username="monetdb", password="monetdb", port=s.dbport, database='db1')
-            tc.execute("SELECT COUNT(*) FROM foo").assertSucceeded().assertDataResultMatch([(100,)])
-
-        s.communicate()
+        with process.server(mapiport='0', dbname='db1',
+                            dbfarm=os.path.join(farm_dir, 'db1'),
+                            stdin=process.PIPE,
+                            stdout=process.PIPE, stderr=process.PIPE) as s:
+            with SQLTestCase() as tc:
+                tc.connect(username="monetdb", password="monetdb", port=s.dbport, database='db1')
+                tc.execute("SELECT COUNT(*) FROM foo").assertSucceeded().assertDataResultMatch([(100,)])
+            s.communicate()
