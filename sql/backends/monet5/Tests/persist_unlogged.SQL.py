3cf12f4f407c1bd7527690dadfbc11b4dd393f9f
Lucas Pereira <lucas.pereira@monetdbsolutions.com>
2023-10-17 13:41:53 +0200

    No need to use sleep fun. More test out approves.

--- sql/backends/monet5/Tests/persist_unlogged.SQL.py
+++ sql/backends/monet5/Tests/persist_unlogged.SQL.py
@@ -15,16 +15,15 @@ with tempfile.TemporaryDirectory() as farm_dir:
                         stdout=process.PIPE, stderr=process.PIPE) as s:
         with SQLTestCase() as tc:
             tc.connect(username="monetdb", password="monetdb", port=s.dbport, database='db1')
-            tc.execute("CREATE OR REPLACE FUNCTION sleep(msecs int) RETURNS INT EXTERNAL NAME alarm.sleep")
             tc.execute("CREATE UNLOGGED TABLE foo (x INT)").assertSucceeded()
             tc.execute("ALTER TABLE foo SET INSERT ONLY").assertSucceeded()
             tc.execute("INSERT INTO foo SELECT * FROM generate_series(0,500)")
             tc.execute("SELECT count(*) FROM foo").assertSucceeded().assertDataResultMatch([(500,)])
             tc.execute("SELECT table, rowcount FROM persist_unlogged()").assertSucceeded().assertDataResultMatch([('foo', 0)])
+
+            # Simulate some work in order to trigger WAL flush(note that Mtests runs with --forcemito)
             tc.execute("CREATE TABLE bar (x INT)").assertSucceeded()
             tc.execute("INSERT INTO bar SELECT * FROM generate_series(0,100000)").assertSucceeded()
-
-            tc.execute("SELECT sleep(2000)")
 
             tc.execute("SELECT table, rowcount FROM persist_unlogged()").assertSucceeded().assertDataResultMatch([('foo', 500)])
         s.communicate()
