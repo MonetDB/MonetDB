6ea68fd2dc704c3c5c5aae2a401284b8672f27a5
Niels Nes <niels@cwi.nl>
2023-10-22 15:21:05 +0200

    no strings.h on windows, but seems not needed anyway

--- clients/examples/C/testsfile.c
+++ clients/examples/C/testsfile.c
@@ -9,7 +9,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <strings.h>
 
 // love it when a task is so straightforward I can use global variables!
 static int start_line = -1;
