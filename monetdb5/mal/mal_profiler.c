bbaf328e573e887eb7ce3d0fa15ddb1dd13e8e78
Sjoerd Mullender <sjoerd@acm.org>
2023-11-01 18:00:02 +0100

    Removed workingset array. This is the penultimate step in removing compile-time restrictions on the number of threads.

--- monetdb5/mal/mal_profiler.c
+++ monetdb5/mal/mal_profiler.c
@@ -721,8 +721,6 @@ profilerEvent(MalEvent *me, NonMalEvent *nme)
 str
 openProfilerStream(Client cntxt, int m)
 {
-	int j;
-
 #ifdef HAVE_SYS_RESOURCE_H
 	getrusage(RUSAGE_SELF, &infoUsage);
 	prevUsage = infoUsage;
@@ -759,29 +757,6 @@ openProfilerStream(Client cntxt, int m)
 	maleventstream = cntxt->fdout;
 	profilerUser = cntxt->user;
 
-	// Ignore the JSON rendering mode, use compiled time version
-
-	/* show all in progress instructions for stethoscope startup */
-	/* wait a short time for instructions to finish updating their thread admin
-	 * and then follow the locking scheme */
-
-	MT_sleep_ms(200);
-
-	for (j = 0; j < THREADS; j++) {
-		struct MalEvent me = {
-			.cntxt = workingset[j].cntxt,
-			.mb = workingset[j].mb,
-			.stk = workingset[j].stk,
-			.pci = workingset[j].pci,
-			.clk = workingset[j].clock,
-		};
-		if (me.cntxt && me.mb && me.stk && me.pci) {
-			/* show the event  assuming the quintuple is aligned */
-			MT_lock_unset(&mal_profileLock);
-			profilerEvent(&me, NULL);
-			MT_lock_set(&mal_profileLock);
-		}
-	}
 	MT_lock_unset(&mal_profileLock);
 	return MAL_SUCCEED;
 }
