c378201920afff61a6c21aba456e0af028d6bb5e
stefanos mavros <stemavros@gmail.com>
2023-11-01 23:12:26 +0100

    REMOTE prop dump only prints a list of uris (not tid)

--- sql/server/rel_prop.c
+++ sql/server/rel_prop.c
@@ -144,7 +144,7 @@ propvalue2string(sql_allocator *sa, prop *p)
 			for (node *n = ((list*)p->value.pval)->h; n; n = n->next) {
 				tid_uri *tu = n->data;
 				if (tu->uri)
-					offset += snprintf(buf + offset, BUFSIZ, "[%d]%s\" ", tu->id, sql_escape_ident(sa, tu->uri));
+					offset += snprintf(buf + offset, BUFSIZ, "%s ", sql_escape_ident(sa, tu->uri));
 			}
 			return sa_strdup(sa, buf);
 		}
