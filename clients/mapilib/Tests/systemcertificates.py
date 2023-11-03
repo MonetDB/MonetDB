039c7bc055be909460333c9e5fd8cbe4ff2e3254
Joeri van Ruth <joeri.van.ruth@monetdbsolutions.com>
2023-11-03 15:18:52 +0100

    Roll back intentional test failure

--- clients/mapilib/Tests/systemcertificates.py
+++ clients/mapilib/Tests/systemcertificates.py
@@ -27,12 +27,15 @@ cmd = ['mclient', '-L-', '-d', f"monetdbs://{HOST}:443/demo"]
 proc = subprocess.run(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
 assert proc.returncode == 2, f"mclient is supposed to exit with status 2, not {proc.returncode}"
 
-# We expect the server to send something like 'HTTP/1.1 400 Bad Request'
-# libmapi will interpret the first two bytes as a block header.
+# After the TLS handshake succeeds we expect the server to send something like
+# 'HTTP/1.1 400 Bad Request' because we're sending \x00\x00 instead of an HTTP
+# request. libmapi will interpret the first two bytes 'H' and 'T' as an invalid
+# block header.
 #
-# In ASCII, 'H' + 256 * 'T' is 21576.
-ok = b'21576' not in proc.stderr
-if ok or True:
+# In ASCII,  'H' + 256 * 'T'  ==  72 + 256 * 84  ==  21576.
+tls_works_but_mapi_fails = b'21576' in proc.stderr
+
+if not tls_works_but_mapi_fails:
     msg = str(proc.stderr, 'utf-8')
     print(f"Expected mclient to print an error message containing the number 21576, got:\n--- stderr ---\n{msg}\n---end stderr ---", file=sys.stderr)
     exit(1)
