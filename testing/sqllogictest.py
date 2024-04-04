afdabe542ab75de14bf9c461cc3868d3ccafd3d3
Sjoerd Mullender <sjoerd@acm.org>
2024-04-04 16:14:28 +0200

    Skip blobs when searching for MULTIPOINT.

--- testing/sqllogictest.py
+++ testing/sqllogictest.py
@@ -429,7 +429,7 @@ class SQLLogic:
         for row in data:
             nrow = []
             for col in row:
-                if type(col) in (type(''), type(b'')):
+                if type(col) == type(''):
                     res = geosre.search(col)
                     if res is not None:
                         points = ptsre.sub(r'(\g<0>)', res.group('points'))
