fa754c0be477e57e55abf3978544ebc4a0acef9c
Ying Zhang <ying.zhang@monetdbsolutions.com>
2024-04-15 12:03:17 +0200

    Avoid nested (pipeline) blocks: a semi-join in the subtree is unsafe

--- sql/backends/monet5/bin_partition.c
+++ sql/backends/monet5/bin_partition.c
@@ -51,6 +51,7 @@ rel_is_not_pp_safe(visitor *v, sql_rel *rel)
 {
 	if (is_groupby(rel->op) ||
 	    rel->op == op_table ||
+		rel->op == op_semi ||
 		(is_join(rel->op) && rel->hashjoin))
 		*((int*)v->data) = 1;
 	return rel;
