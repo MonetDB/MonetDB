2a898b2eeb7f7eb22f0e35751859469b2eb6866d
Sjoerd Mullender <sjoerd@acm.org>
2024-05-13 17:42:11 +0200

    Move looking up function pointer out of the inner loop.

--- gdk/gdk_hash.c
+++ gdk/gdk_hash.c
@@ -899,7 +899,8 @@ BAThash_impl(BAT *restrict b, struct canditer *restrict ci, const char *restrict
 		case TYPE_uuid:
 			starthash(uuid);
 			break;
-		default:
+		default: {
+			int (*atomcmp)(const void *, const void *) = ATOMcompare(h->type);
 			TIMEOUT_LOOP(p, timeoffset) {
 				const void *restrict v = BUNtail(bi, o - b->hseqbase);
 				c = hash_any(h, v);
@@ -913,8 +914,7 @@ BAThash_impl(BAT *restrict b, struct canditer *restrict ci, const char *restrict
 					for (hb = hget;
 					     hb != BUN_NONE;
 					     hb = HASHgetlink(h, hb)) {
-						if (ATOMcmp(h->type,
-							    v,
+						if (atomcmp(v,
 							    BUNtail(bi, hb)) == 0)
 							break;
 					}
@@ -927,6 +927,7 @@ BAThash_impl(BAT *restrict b, struct canditer *restrict ci, const char *restrict
 			TIMEOUT_CHECK(timeoffset,
 				      GOTO_LABEL_TIMEOUT_HANDLER(bailout));
 			break;
+		}
 		}
 		TRC_DEBUG_IF(ACCELERATOR) if (p < cnt1)
 			TRC_DEBUG_ENDIF(ACCELERATOR,
@@ -975,7 +976,8 @@ BAThash_impl(BAT *restrict b, struct canditer *restrict ci, const char *restrict
 	case TYPE_uuid:
 		finishhash(uuid);
 		break;
-	default:
+	default: {
+		int (*atomcmp)(const void *, const void *) = ATOMcompare(h->type);
 		TIMEOUT_LOOP(ci->ncand - p, timeoffset) {
 			const void *restrict v = BUNtail(bi, o - b->hseqbase);
 			c = hash_any(h, v);
@@ -985,7 +987,7 @@ BAThash_impl(BAT *restrict b, struct canditer *restrict ci, const char *restrict
 				for (hb = hget;
 				     hb != BUN_NONE;
 				     hb = HASHgetlink(h, hb)) {
-					if (ATOMcmp(h->type, v, BUNtail(bi, hb)) == 0)
+					if (atomcmp(v, BUNtail(bi, hb)) == 0)
 						break;
 				}
 				h->nunique += hb == BUN_NONE;
@@ -998,6 +1000,7 @@ BAThash_impl(BAT *restrict b, struct canditer *restrict ci, const char *restrict
 		TIMEOUT_CHECK(timeoffset,
 			      GOTO_LABEL_TIMEOUT_HANDLER(bailout));
 		break;
+	}
 	}
 	bat_iterator_end(&bi);
 	/* if the number of unique values is equal to the bat count,
