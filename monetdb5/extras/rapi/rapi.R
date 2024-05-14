77c99f5922a1c6c08b5ec61c591e92f14c13f859
Sjoerd Mullender <sjoerd@acm.org>
2024-05-14 10:37:28 +0200

    Add a minus to the module search path.

--- monetdb5/extras/rapi/rapi.R
+++ monetdb5/extras/rapi/rapi.R
@@ -57,7 +57,7 @@ rewireFunc("q", quit, "base")
 rm(rewireFunc)
 
 loopback_query <- function(query) {
-	dyn.load(file.path(MONETDB_LIBDIR, paste0("monetdb5", MONETDB_VERSION), "lib_rapi.so"))
+	dyn.load(file.path(MONETDB_LIBDIR, paste0("monetdb5-", MONETDB_VERSION), "lib_rapi.so"))
 	res <- .Call("RAPIloopback", paste0(query, "\n;"), package="lib_rapi")
 	if (is.character(res)) {
 		stop(res)
