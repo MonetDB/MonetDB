26762ca0fb6169a48916155b26404ab0ebe6293f
Niels Nes <niels@cwi.nl>
2024-06-04 15:56:41 +0200

    make sure to use lng instead of int, solve problems with loading data with > 2147483647 rows

--- sql/storage/bat/bat_storage.c
+++ sql/storage/bat/bat_storage.c
@@ -4723,7 +4723,7 @@ claim_segmentsV2(sql_trans *tr, sql_table *t, storage *s, size_t cnt, BUN *offse
 			in_transaction = true;
 		}
 		if (in_transaction && !NOT_TO_BE_LOGGED(t))
-			tr->logchanges += (int) total;
+			tr->logchanges += (lng) total;
 		if (*offsets) {
 			BAT *pos = *offsets;
 			assert(BATcount(pos) == total);
@@ -4803,7 +4803,7 @@ claim_segments(sql_trans *tr, sql_table *t, storage *s, size_t cnt, BUN *offset,
 			in_transaction = true;
 		}
 		if (in_transaction && !NOT_TO_BE_LOGGED(t))
-			tr->logchanges += (int) cnt;
+			tr->logchanges += (lng) cnt;
 		*offset = slot;
 	}
 	return ok;
