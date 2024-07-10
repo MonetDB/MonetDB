f1b2ae3e7000c0762b71e0e5f065bb7aa9de31d1
Niels Nes <niels@cwi.nl>
2024-07-10 14:02:39 +0200

    some more multiplex inlining, fixes bug #7272

--- monetdb5/optimizer/opt_prelude.c
+++ monetdb5/optimizer/opt_prelude.c
@@ -149,6 +149,7 @@ const char *leftjoinRef;
 const char *likejoinRef;
 const char *likeRef;
 const char *likeselectRef;
+const char *lngRef;
 const char *lockRef;
 const char *lookupRef;
 const char *malRef;
@@ -409,6 +410,7 @@ optimizerInit(void)
 	likejoinRef = putName("likejoin");
 	likeRef = putName("like");
 	likeselectRef = putName("likeselect");
+	lngRef = putName("lng");
 	lockRef = putName("lock");
 	lookupRef = putName("lookup");
 	malRef = putName("mal");
