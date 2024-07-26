dec39f7a2bee9cca3aa855f1ceb05f67b0c6233c
Sjoerd Mullender <sjoerd@acm.org>
2024-07-26 09:44:28 +0200

    Close received file descriptor on exec.

--- monetdb5/modules/mal/mal_mapi.c
+++ monetdb5/modules/mal/mal_mapi.c
@@ -403,7 +403,10 @@ SERVERlistenThread(SOCKET *Sock)
 			msgh.msg_control = ccmsg;
 			msgh.msg_controllen = sizeof(ccmsg);
 
-			rv = recvmsg(msgsock, &msgh, 0);
+#ifndef MSG_CMSG_CLOEXEC
+#define MSG_CMSG_CLOEXEC 0
+#endif
+			rv = recvmsg(msgsock, &msgh, MSG_CMSG_CLOEXEC);
 			if (rv == -1) {
 				closesocket(msgsock);
 				continue;
@@ -431,6 +434,9 @@ SERVERlistenThread(SOCKET *Sock)
 				 */
 				c_d = (int *) CMSG_DATA(cmsg);
 				msgsock = *c_d;
+#if MSG_CMSG_CLOEXEC == 0 && defined(HAVE_FCNTL)
+				(void) fcntl(msgsock, F_SETFD, FD_CLOEXEC);
+#endif
 			}
 				break;
 			default:
