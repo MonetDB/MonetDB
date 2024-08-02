001c4c118cac3627bb5ceb6a0cd02f56e4d6641b
Sjoerd Mullender <sjoerd@acm.org>
2024-08-02 13:13:10 +0200

    Produce correct error message when there is a timeout.

--- monetdb5/mal/mal_interpreter.c
+++ monetdb5/mal/mal_interpreter.c
@@ -593,9 +593,18 @@ runMALsequence(Client cntxt, MalBlkPtr mb, int startpc,
 			if (cntxt->fdin && TIMEOUT_TEST(&cntxt->qryctx)) {
 				if (cntxt->qryctx.endtime != QRY_INTERRUPT && cntxt->qryctx.endtime != QRY_TIMEOUT)
 					cntxt->mode = FINISHCLIENT;
-				stkpc = stoppc;
-				ret = createException(MAL, "mal.interpreter", "%s",
-									  TIMEOUT_MESSAGE(&cntxt->qryctx));
+				switch (cntxt->qryctx.endtime) {
+				case QRY_TIMEOUT:
+					ret = createException(MAL, "mal.interpreter", SQLSTATE(HYT00) RUNTIME_QRY_TIMEOUT);
+					break;
+				case QRY_INTERRUPT:
+					ret = createException(MAL, "mal.interpreter", SQLSTATE(HYT00) RUNTIME_QRY_INTERRUPT);
+					break;
+				default:
+					ret = createException(MAL, "mal.interpreter", SQLSTATE(HYT00) "Client disconnected");
+					cntxt->mode = FINISHCLIENT;
+					break;
+				}
 				break;
 			}
 			lastcheck = runtimeProfile.ticks;
