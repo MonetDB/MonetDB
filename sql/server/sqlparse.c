8638f5a3e93ce9ef188169e986894cc5ab355438
Sjoerd Mullender <sjoerd@acm.org>
2025-01-30 16:57:12 +0100

    We need to #include getopt.h in some form.

--- sql/server/sqlparse.c
+++ sql/server/sqlparse.c
@@ -1,5 +1,23 @@
+/*
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0.  If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright 2024, 2025 MonetDB Foundation;
+ * Copyright August 2008 - 2023 MonetDB B.V.;
+ * Copyright 1997 - July 2008 CWI.
+ */
 
 #include "monetdb_config.h"
+#ifndef HAVE_GETOPT_LONG
+#  include "monet_getopt.h"
+#else
+# ifdef HAVE_GETOPT_H
+#  include "getopt.h"
+# endif
+#endif
 #include "stream.h"
 #include "sql_mvc.h"
 #include "sql_parser.tab.h"
