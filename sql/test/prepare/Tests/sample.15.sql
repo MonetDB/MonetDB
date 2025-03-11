d847fb635eb06e7dbf1fc8ddb79b16af68d6fb5c
Sjoerd Mullender <sjoerd@acm.org>
2025-03-11 16:18:00 +0100

    Convert tests to .test format.

--- sql/test/prepare/Tests/sample.15.sql
+++
@@ -1,6 +0,0 @@
-select * from GENERATE_SERIES(1,41) sample 1.25;            --should give error
-select * from GENERATE_SERIES(1,41) seed 1; --error, seed without sample
-
-prepare select * from GENERATE_SERIES(1,41) sample ?;
-prepare select * from GENERATE_SERIES(1,41) sample 0.2 seed ?;
-prepare select * from GENERATE_SERIES(1,41) sample ? seed ?;
