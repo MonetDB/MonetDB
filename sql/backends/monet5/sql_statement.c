2f83d6e34124822a212ad2e0f0e6260fe3ecb5b8
Niels Nes <niels@cwi.nl>
2025-04-27 14:19:01 +0200

    small fix for simple array of string. Todo find super type of elements.

--- sql/backends/monet5/sql_statement.c
+++ sql/backends/monet5/sql_statement.c
@@ -4462,14 +4462,14 @@ stmt_convert(backend *be, stmt *v, stmt *sel, sql_subtype *f, sql_subtype *t)
 	if (v->nr < 0)
 		goto bailout;
 
+	if (f->type == t->type && t->multiset && v->subtype.multiset)
+		return v;
 	if (f->type->eclass == EC_EXTERNAL && t->type->composite && strcmp(f->type->base.name, "json") == 0)
 		return stmt_from_json(be, v, sel, t);
 	if (t->type->eclass == EC_EXTERNAL && f->type->composite && strcmp(t->type->base.name, "json") == 0)
 		return stmt_to_json(be, v, sel, f, t);
 	if (EC_VARCHAR(f->type->eclass) && (t->type->composite || t->multiset))
 		return stmt_from_varchar(be, v, sel, t);
-	if (f->type == t->type && t->multiset && v->subtype.multiset)
-		return v;
 	if (f->type->eclass != EC_EXTERNAL && t->type->eclass != EC_EXTERNAL &&
 		/* general cases */
 		((t->type->localtype == f->type->localtype && t->type->eclass == f->type->eclass &&
