07fa01e6847c20e569b03b765262915ab2d36743
Niels Nes <niels@cwi.nl>
2025-09-03 10:50:42 +0200

    small cleanup

--- sql/server/rel_optimize_sel.c
+++ sql/server/rel_optimize_sel.c
@@ -3870,27 +3870,24 @@ rel_push_select_down(visitor *v, sql_rel *rel)
 				list_destroy(keyColumns);
 			}
 			/* also push (rewrite) limits on output of row_number/(*)rank like window functions */
-			if (is_simple_project(r->op) /*&& is_simple_project(pl->op)*/) { /* possible window functions */
+			if (is_simple_project(r->op)) { /* possible window functions */
 				for (n = exps->h; n; n = n->next) {
 					sql_exp *e = n->data;
 
-					if (e->type == e_cmp && (e->flag == cmp_lt || e->flag == cmp_lte) && exp_is_atom(e->r)) { /* simple limit */
+					if (e->type == e_cmp && (e->flag == cmp_lt || e->flag == cmp_lte) && exp_is_atom(e->r) && !e->f) { /* simple limit */
 						sql_exp *ranke = rel_find_exp(r, e->l);
 
 						if (ranke && ranke->type == e_func) {
 							sql_subfunc *rankf = ranke->f;
 							if (rankf->func->type == F_ANALYTIC) { /* rank functions cannot have a frame */
-								// For now only for rank/row_number without partition by
 								sql_rel *tn = NULL;
-							   	if (strcmp(rankf->func->base.name, "rank") == 0 && is_simple_project(pl->op) && pl->r /* &&
-										!rank_exp_has_partition_key(ranke)*/) {
+							   	if (strcmp(rankf->func->base.name, "rank") == 0 && is_simple_project(pl->op) && pl->r) {
 									tn = r->l = rel_topn(v->sql->sa, r->l, append(sa_list(v->sql->sa), e->r));
 									tn->grouped = 1;
 									v->changes++;
 									break;
 								}
-							   	if (strcmp(rankf->func->base.name, "row_number") == 0 && list_empty(r->r) && !is_topn(pl->op) /*&&
-										!rank_exp_has_partition_key(ranke)*/) {
+							   	if (strcmp(rankf->func->base.name, "row_number") == 0 && list_empty(r->r) && !is_topn(pl->op)) {
 									tn = r->l = rel_topn(v->sql->sa, r->l, append(sa_list(v->sql->sa), e->r));
 									tn->grouped = 1;
 									v->changes++;
