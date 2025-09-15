8ad464bafee847f3028348c132f5c007e878939d
Sjoerd Mullender <sjoerd@acm.org>
2025-09-15 15:17:21 +0200

    Removed function sys.optimzier_stats() and the collecting of optimizer statistics.

--- monetdb5/optimizer/opt_wrapper.c
+++ monetdb5/optimizer/opt_wrapper.c
@@ -56,52 +56,48 @@
 #include "optimizer_private.h"
 
 // keep the optimizer list sorted
-static struct {
-	const char *nme;
+static const struct {
+	char *nme;
 	str (*fcn)(Client, MalBlkPtr, MalStkPtr, InstrPtr);
-	int calls;
-	lng timing;
 } codes[] = {
-	/* most used compound optiimizers at the top */
-	{"defaultpipe", &OPTdefaultpipeImplementation, 0, 0},
-	{"recursivepipe", &OPTdefaultpipeImplementation, 0, 0},
-	{"nomitosispipe", &OPTdefaultpipeImplementation, 0, 0},
-	{"minimalpipe", &OPTminimalpipeImplementation, 0, 0},
-	{"sequentialpipe", &OPTdefaultpipeImplementation, 0, 0},
+	/* most used compound optimizers at the top */
+	{"defaultpipe", &OPTdefaultpipeImplementation},
+	{"minimalpipe", &OPTminimalpipeImplementation},
+	{"recursivepipe", &OPTdefaultpipeImplementation},
+	{"nomitosispipe", &OPTdefaultpipeImplementation},
+	{"sequentialpipe", &OPTdefaultpipeImplementation},
 
 	/* all regular MAL optimizers */
-	{"aliases", &OPTaliasesImplementation, 0, 0},
-	{"candidates", &OPTcandidatesImplementation, 0, 0},
-	{"coercions", &OPTcoercionImplementation, 0, 0},
-	{"commonTerms", &OPTcommonTermsImplementation, 0, 0},
-	{"constants", &OPTconstantsImplementation, 0, 0},
-	{"costModel", &OPTcostModelImplementation, 0, 0},
-	{"dataflow", &OPTdataflowImplementation, 0, 0},
-	{"deadcode", &OPTdeadcodeImplementation, 0, 0},
-	{"dict", &OPTdictImplementation, 0, 0},
-	{"emptybind", &OPTemptybindImplementation, 0, 0},
-	{"evaluate", &OPTevaluateImplementation, 0, 0},
-	{"for", &OPTforImplementation, 0, 0},
-	{"garbageCollector", &OPTgarbageCollectorImplementation, 0, 0},
-	{"generator", &OPTgeneratorImplementation, 0, 0},
-	{"inline", &OPTinlineImplementation, 0, 0},
-	{"matpack", &OPTmatpackImplementation, 0, 0},
-	{"mergetable", &OPTmergetableImplementation, 0, 0},
-	{"mitosis", &OPTmitosisImplementation, 0, 0},
-	{"multiplex", &OPTmultiplexImplementation, 0, 0},
-	{"postfix", &OPTpostfixImplementation, 0, 0},
-	{"profiler", &OPTprofilerImplementation, 0, 0},
-	{"projectionpath", &OPTprojectionpathImplementation, 0, 0},
-	{"pushselect", &OPTpushselectImplementation, 0, 0},
-	{"querylog", &OPTquerylogImplementation, 0, 0},
-	{"reduce", &OPTreduceImplementation, 0, 0},
-	{"remap", &OPTremapImplementation, 0, 0},
-	{"remoteQueries", &OPTremoteQueriesImplementation, 0, 0},
-	{"reorder", &OPTreorderImplementation, 0, 0},
-	{0, 0, 0, 0}
+	{"aliases", &OPTaliasesImplementation},
+	{"candidates", &OPTcandidatesImplementation},
+	{"coercions", &OPTcoercionImplementation},
+	{"commonTerms", &OPTcommonTermsImplementation},
+	{"constants", &OPTconstantsImplementation},
+	{"costModel", &OPTcostModelImplementation},
+	{"dataflow", &OPTdataflowImplementation},
+	{"deadcode", &OPTdeadcodeImplementation},
+	{"dict", &OPTdictImplementation},
+	{"emptybind", &OPTemptybindImplementation},
+	{"evaluate", &OPTevaluateImplementation},
+	{"for", &OPTforImplementation},
+	{"garbageCollector", &OPTgarbageCollectorImplementation},
+	{"generator", &OPTgeneratorImplementation},
+	{"inline", &OPTinlineImplementation},
+	{"matpack", &OPTmatpackImplementation},
+	{"mergetable", &OPTmergetableImplementation},
+	{"mitosis", &OPTmitosisImplementation},
+	{"multiplex", &OPTmultiplexImplementation},
+	{"postfix", &OPTpostfixImplementation},
+	{"profiler", &OPTprofilerImplementation},
+	{"projectionpath", &OPTprojectionpathImplementation},
+	{"pushselect", &OPTpushselectImplementation},
+	{"querylog", &OPTquerylogImplementation},
+	{"reduce", &OPTreduceImplementation},
+	{"remap", &OPTremapImplementation},
+	{"remoteQueries", &OPTremoteQueriesImplementation},
+	{"reorder", &OPTreorderImplementation},
+	{0, 0}
 };
-
-static MT_Lock codeslock = MT_LOCK_INITIALIZER(codeslock);
 
 str
 OPTwrapper(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr p)
@@ -111,7 +107,6 @@ OPTwrapper(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr p)
 	Symbol s = NULL;
 	int i;
 	str msg = MAL_SUCCEED;
-	lng clk;
 
 	// no optimizer starts with a null byte, initialization sets a zero
 	if (cntxt->mode == FINISHCLIENT)
@@ -156,17 +151,10 @@ OPTwrapper(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr p)
 		p->token = REMsymbol;
 	}
 
-	clk = GDKusec();
 	const char *id = getFunctionId(p);
 	for (i = 0; codes[i].nme != NULL; i++) {
 		if (strcmp(codes[i].nme, id) == 0) {
 			msg = (*codes[i].fcn) (cntxt, mb, stk, p);
-			clk = GDKusec() - clk;
-			MT_lock_set(&codeslock);
-			codes[i].timing += clk;
-			codes[i].calls++;
-			MT_lock_unset(&codeslock);
-			p = pushLng(mb, p, clk);
 			if (msg || mb->errors) {
 				/* we can only return one or the other */
 				if (msg)
@@ -188,47 +176,5 @@ OPTwrapper(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr p)
 		throw(MAL, fcnnme,
 			  SQLSTATE(HY002) "Optimizer implementation '%s' missing", fcnnme);
 
-	return MAL_SUCCEED;
-}
-
-str
-OPTstatistics(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr p)
-{
-	bat *nme = getArgReference_bat(stk, p, 0);
-	bat *cnt = getArgReference_bat(stk, p, 1);
-	bat *time = getArgReference_bat(stk, p, 2);
-	BAT *n, *c, *t;
-	int i;
-
-	(void) cntxt;
-	(void) mb;
-	n = COLnew(0, TYPE_str, 256, TRANSIENT);
-	c = COLnew(0, TYPE_int, 256, TRANSIENT);
-	t = COLnew(0, TYPE_lng, 256, TRANSIENT);
-	if (n == NULL || c == NULL || t == NULL) {
-		BBPreclaim(n);
-		BBPreclaim(c);
-		BBPreclaim(t);
-		throw(MAL, "optimizer.statistics", SQLSTATE(HY013) MAL_MALLOC_FAIL);
-	}
-	MT_lock_set(&codeslock);
-	for (i = 0; codes[i].nme; i++) {
-		if (BUNappend(n, codes[i].nme, false) != GDK_SUCCEED
-			|| BUNappend(c, &codes[i].calls, false) != GDK_SUCCEED
-			|| BUNappend(t, &codes[i].timing, false) != GDK_SUCCEED) {
-			MT_lock_unset(&codeslock);
-			BBPreclaim(n);
-			BBPreclaim(c);
-			BBPreclaim(t);
-			throw(MAL, "optimizer.statistics", SQLSTATE(HY013) MAL_MALLOC_FAIL);
-		}
-	}
-	MT_lock_unset(&codeslock);
-	*nme = n->batCacheid;
-	BBPkeepref(n);
-	*cnt = c->batCacheid;
-	BBPkeepref(c);
-	*time = t->batCacheid;
-	BBPkeepref(t);
 	return MAL_SUCCEED;
 }
