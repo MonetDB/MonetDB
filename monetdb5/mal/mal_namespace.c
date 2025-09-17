f36712d8bef6131768ea52ffe65db2d11662fba2
Niels Nes <niels@cwi.nl>
2025-09-17 11:32:12 +0200

    batsql.diff is order dependent, not batsql.difference (which doesn't exist)

--- monetdb5/mal/mal_namespace.c
+++ monetdb5/mal/mal_namespace.c
@@ -159,6 +159,7 @@ const char dense_rankRef[] = "dense_rank";
 const char dependRef[] = "depend";
 const char deregisterRef[] = "deregister";
 const char dictRef[] = "dict";
+const char diffRef[] = "diff";
 const char diffcandRef[] = "diffcand";
 const char differenceRef[] = "difference";
 const char disconnectRef[] = "disconnect";
@@ -425,6 +426,7 @@ initNamespace(void)
 	fixName(dependRef);
 	fixName(deregisterRef);
 	fixName(dictRef);
+	fixName(diffRef);
 	fixName(diffcandRef);
 	fixName(differenceRef);
 	fixName(disconnectRef);
