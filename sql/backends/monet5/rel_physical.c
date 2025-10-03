2c5f4581e99bf0e50a9f870e99cf5d7f9e292057
Niels Nes <niels@cwi.nl>
2025-10-03 15:36:22 +0200

    only rewrite complex avg

--- sql/backends/monet5/rel_physical.c
+++ sql/backends/monet5/rel_physical.c
@@ -1138,7 +1138,7 @@ static sql_rel *
 rel_avg_rewrite(visitor *v, sql_rel *rel)
 {
 	mvc *sql = v->sql;
-	if (is_groupby(rel->op) /*&& rel->parallel*/) {
+	if (is_groupby(rel->op) && rel->r /*&& rel->parallel*/) {
 		list *pexps, *nexps = new_exp_list(sql->sa), *avgs = new_exp_list(sql->sa);
 		list *aexps = new_exp_list(sql->sa); /* alias list */
 		node *m, *n;
