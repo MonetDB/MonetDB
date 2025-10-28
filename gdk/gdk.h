aa0dc91e9418d9b2fcbed9b4ffe1e2b73f352423
Sjoerd Mullender <sjoerd@acm.org>
2025-10-28 16:01:47 +0100

    Do the Windows magic.

--- gdk/gdk.h
+++ gdk/gdk.h
@@ -1550,7 +1550,7 @@ gdk_export BAT *BATcasefold(BAT *b, BAT *s);
 gdk_export gdk_return GDKasciify(char **restrict buf, size_t *restrict buflen, const char *restrict s);
 gdk_export BAT *BATasciify(BAT *b, BAT *s);
 #ifdef HAVE_OPENSSL
-gdk_return BATaggrdigest(allocator *ma, BAT **bnp, char **shap, const char *digest, BAT *b, BAT *g, BAT *e, BAT *s, bool skip_nils);
+gdk_export gdk_return BATaggrdigest(allocator *ma, BAT **bnp, char **shap, const char *digest, BAT *b, BAT *g, BAT *e, BAT *s, bool skip_nils);
 #endif
 
 gdk_export BAT *BATsample(BAT *b, BUN n);
