8928d1e9c097b3c5e1b0ff8de9cc91d06920d9fc
Sjoerd Mullender <sjoerd@acm.org>
2026-01-08 17:06:59 +0100

    Update copyright year. Henceforth, only two files need to be updated: README.rst and debian/copyright. All other files refer to the latter.

--- clients/mapilib/connect_openssl.c
+++ clients/mapilib/connect_openssl.c
@@ -5,9 +5,7 @@
  * License, v. 2.0.  If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * Copyright 2024, 2025 MonetDB Foundation;
- * Copyright August 2008 - 2023 MonetDB B.V.;
- * Copyright 1997 - July 2008 CWI.
+ * For copyright information, see the file debian/copyright.
  */
 
 // Request compatibility with OpenSSL 1.1.1.
--- sql/scripts/52_describe.sql
+++ sql/scripts/52_describe.sql
@@ -4,9 +4,7 @@
 -- License, v. 2.0.  If a copy of the MPL was not distributed with this
 -- file, You can obtain one at http://mozilla.org/MPL/2.0/.
 --
--- Copyright 2024, 2025 MonetDB Foundation;
--- Copyright August 2008 - 2023 MonetDB B.V.;
--- Copyright 1997 - July 2008 CWI.
+-- For copyright information, see the file debian/copyright.
 
 CREATE FUNCTION sys.sql_datatype(mtype varchar(999), digits integer, tscale integer, nameonly boolean, shortname boolean)
   RETURNS varchar(1024)
