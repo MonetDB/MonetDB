6a9d5ddf7d262fe0675eac01248d252bb13b9ea1
Sjoerd Mullender <sjoerd@acm.org>
2026-01-16 13:59:23 +0100

    Fix copyright blurbs.

--- sql/backends/monet5/sql_pp_statement.h
+++ sql/backends/monet5/sql_pp_statement.h
@@ -5,7 +5,7 @@
  * License, v. 2.0.  If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * Copyright 1997 - July 2008 CWI, August 2008 - 2023 MonetDB B.V.
+ * For copyright information, see the file debian/copyright.
  */
 
 #ifndef _SQL_PP_STATEMENT_H_
