85b9ca70dd76836dcbd31f39f51bca9475b71b15
Sjoerd Mullender <sjoerd@acm.org>
2026-02-05 18:46:51 +0100

    Fix incorrect call to GDKfree.

--- gdk/gdk_bbp.c
+++ gdk/gdk_bbp.c
@@ -1515,6 +1515,8 @@ jsonupgradebat(BAT *b, json_storage_conversion fixJSONStorage)
 		var_t o = ((var_t *) h1.base)[i];
 		const char *s = vh1.base + o;
 		char *ns;
+		allocator *ta = MT_thread_getallocator();
+		allocator_state ta_state = ma_open(ta);
 		if (fixJSONStorage(&ns, &s) != GDK_SUCCEED) {
 			HEAPfree(&h1, false);
 			HEAPfree(&vh1, false);
@@ -1523,10 +1525,11 @@ jsonupgradebat(BAT *b, json_storage_conversion fixJSONStorage)
 			b->tvheap = ovh;
 			TRC_CRITICAL(GDK, "converting value "
 				     "in BAT %d failed\n", b->batCacheid);
+			ma_close(&ta_state);
 			return GDK_FAIL;
 		}
 		var_t no = strPut(b, &o, ns);
-		GDKfree(ns);
+		ma_close(&ta_state);
 		if (no == (var_t) -1) {
 			HEAPfree(&h1, false);
 			HEAPfree(&vh1, false);
