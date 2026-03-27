ea03724b4b6f6fd2c859fb84e58604498a3aa8f0
Sjoerd Mullender <sjoerd@acm.org>
2026-03-27 15:36:23 +0100

    Update link to Mozilla license.

--- misc/python/fixlicense.py
+++ misc/python/fixlicense.py
@@ -4,7 +4,7 @@
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0.  If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
 #
 # For copyright information, see the file debian/copyright.
 
@@ -15,7 +15,7 @@ license = [
     '',
     'This Source Code Form is subject to the terms of the Mozilla Public',
     'License, v. 2.0.  If a copy of the MPL was not distributed with this',
-    'file, You can obtain one at http://mozilla.org/MPL/2.0/.',
+    'file, You can obtain one at https://mozilla.org/MPL/2.0/.',
     '',
     'For copyright information, see the file debian/copyright.',
     ]
