bebb86c1ba3f39f4511ae4f8d6c697bb99cf49a7
Ying Zhang <ying.zhang@monetdbsolutions.com>
2026-03-30 12:02:20 +0200

    It can also happen that an error has occurred, in which case, always stop.

--- monetdb5/modules/mal/pipeline.c
+++ monetdb5/modules/mal/pipeline.c
@@ -156,8 +156,10 @@ sync_counter_done(pp_counter *c, int wid, int nr_workers, int redo)
 			MT_cond_wait(&p->p->cond, &p->p->l);
 		else
 			MT_cond_broadcast(&p->p->cond);
-		assert(c->scnt == nr_workers);
+		assert(c->scnt == nr_workers || p->p->error);
 	}
+	if (p->p->error)
+		res = 1;
 	MT_lock_unset(&p->p->l);
 	assert(c->cur);
 	c->cur[wid] = cur;
