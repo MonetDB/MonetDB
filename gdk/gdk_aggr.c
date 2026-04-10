4727c58f08bdc6590754ead8f9d90b72c8e671bc
Sjoerd Mullender <sjoerd@acm.org>
2026-04-10 17:16:21 +0200

    Fix average calculation if we get overflow half-way through summing. This only happens if averaging huge (integer) values.

--- gdk/gdk_aggr.c
+++ gdk/gdk_aggr.c
@@ -2919,7 +2919,7 @@ BATgroupavg3combine(BAT *avg, BAT *rem, BAT *cnt, BAT *g, BAT *e, bool skip_nils
 									\
 		/* first try to calculate the sum of all values into a */ \
 		/* lng/hge */						\
-		TIMEOUT_LOOP(ci.ncand, qry_ctx) {			\
+		TIMEOUT_LOOP_IDX(idx, ci.ncand, qry_ctx) {		\
 			i = canditer_next(&ci) - b->hseqbase;		\
 			x = ((const TYPE *) src)[i];			\
 			if (is_##TYPE##_nil(x))				\
@@ -2944,6 +2944,8 @@ BATgroupavg3combine(BAT *avg, BAT *rem, BAT *cnt, BAT *g, BAT *e, bool skip_nils
 			/* note that n necessarily is > 0 (else no */	\
 			/* overflow possible) */			\
 			assert(n > 0);					\
+			/* we have to redo the last candidate */	\
+			(void) canditer_prev(&ci);			\
 			if (sum >= 0) {					\
 				a = (TYPE) (sum / n); /* this fits */	\
 				r = (lng) (sum % n);			\
@@ -2956,7 +2958,8 @@ BATgroupavg3combine(BAT *avg, BAT *rem, BAT *cnt, BAT *g, BAT *e, bool skip_nils
 					r = n - r;			\
 				}					\
 			}						\
-			TIMEOUT_LOOP(ci.ncand, qry_ctx) {		\
+			/* idx is how many we've already done successfully */ \
+			TIMEOUT_LOOP(ci.ncand - idx, qry_ctx) {		\
 				/* loop invariant: */			\
 				/* a + r/n == average(x[0],...,x[n]); */ \
 				/* 0 <= r < n */			\
@@ -2999,6 +3002,7 @@ BATcalcavg(BAT *b, BAT *s, dbl *avg, BUN *vals, int scale)
 {
 	lng n = 0, r = 0;
 	BUN i = 0;
+	BUN idx;
 #ifdef HAVE_HGE
 	hge sum = 0;
 #else
