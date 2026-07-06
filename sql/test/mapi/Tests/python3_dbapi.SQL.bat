145338fa77b32cdb4f7e99c1d0981baa49529403
Sjoerd Mullender <sjoerd@acm.org>
2026-07-06 11:26:24 +0200

    Use explicit paths so that pymonetdb can be found.

--- sql/test/mapi/Tests/python3_dbapi.SQL.bat
+++ sql/test/mapi/Tests/python3_dbapi.SQL.bat
@@ -4,4 +4,4 @@ rem must be aligned with the installation directory chosen in
 rem clients/examples/python
 set testpath=%TSTSRCBASE%\clients\examples\python
 
-sqlsample.py %MAPIPORT% %TSTDB% %MAPIHOST%
+"%PYTHON%" "%testpath%\sqlsample.py" %MAPIPORT% %TSTDB% %MAPIHOST%
