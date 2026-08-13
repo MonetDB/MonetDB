2bd87e5f08a40f488ac499e6990a70de3b7c68d1
Sjoerd Mullender <sjoerd@acm.org>
2026-08-13 17:33:51 +0200

    Maintain properties. (grafted from 77104ef75e62dfba2325b57db0a84f20dfe20671)

--- gdk/gdk_batop.c
+++ gdk/gdk_batop.c
@@ -1420,6 +1420,7 @@ BATappend_or_update(BAT *b, BAT *p, const oid *positions, BAT *n,
 							bat_iterator_end(&ni);
 							return rc;
 						}
+						bi = bat_iterator_nolock(b);
 						i += j - 1;
 						continue;
 					}
@@ -1829,6 +1830,7 @@ BATappend_or_update(BAT *b, BAT *p, const oid *positions, BAT *n,
 							bat_iterator_end(&ni);
 							return rc;
 						}
+						bi = bat_iterator_nolock(b);
 						i += j - 1;
 						continue;
 					}
