db2693dcc703adda6c1f17194b4103ccbe17e0ba
Niels Nes <niels@cwi.nl>
2026-08-14 19:55:11 +0200

    implemented next (bits) of join reordering. Also remove any redundant join expressions (left overs from transitivity)

--- sql/include/sql_relation.h
+++ sql/include/sql_relation.h
@@ -38,6 +38,7 @@ typedef struct prop {
 		dbl dval; /* property with estimate */
 		void *pval; /* property value */
 	} value;
+	list *exps;
 	struct prop *p; /* some relations may have many properties, which are kept in a chain list */
 } prop;
 
