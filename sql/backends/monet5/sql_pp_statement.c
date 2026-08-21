20a59232ed4eb539ebfb57bca713a0f67346abd6
Ying Zhang <ying.zhang@monetdbsolutions.com>
2026-08-21 12:48:54 +0200

    we now have oahash.build i.s.o oahash.build_combined_table and oahash.build_table

--- sql/backends/monet5/sql_pp_statement.c
+++ sql/backends/monet5/sql_pp_statement.c
@@ -424,7 +424,7 @@ stmt_oahash_hshmrk_init(backend *be, stmt *stmts_ht, bool moveup)
 stmt *
 stmt_oahash_build_ht(backend *be, stmt *ht, stmt *key, stmt *prnt, bit any)
 {
-	InstrPtr q = newStmt(be->mb, putName("oahash"), prnt?putName("build_combined_table"):putName("build_table"));
+	InstrPtr q = newStmt(be->mb, putName("oahash"), putName("build"));
 	if (q == NULL) return NULL;
 
 	setVarType(be->mb, getArg(q, 0), newBatType(TYPE_oid)); /* slot_id */
