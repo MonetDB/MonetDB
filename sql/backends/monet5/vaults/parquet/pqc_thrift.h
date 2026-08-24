8142acb6ab904dc036a57f9931876d7609490ad7
Sjoerd Mullender <sjoerd@acm.org>
2026-08-24 15:49:21 +0200

    Fixes for big-endian. The get_uint* functions now always read little-endian, so separate conversions that happened all over the place for big-endian are no longer needed.  Also, reading decimals now does do the necessary conversion.

--- sql/backends/monet5/vaults/parquet/pqc_thrift.h
+++ sql/backends/monet5/vaults/parquet/pqc_thrift.h
@@ -39,20 +39,6 @@ typedef enum {
 /* get windows defa */
 #define pqc_export extern
 
-#if 0
-#define get_uint16(c) READ_UNALIGNED(16, c)
-#define get_uint32(c) READ_UNALIGNED(32, c)
-#define get_uint64(c) READ_UNALIGNED(64, c)
-#else
-#define get_uint16(c) get_uint16_slow(c)
-#define get_uint32(c) get_uint32_slow(c)
-#define get_uint64(c) get_uint64_slow(c)
-#endif
-
-pqc_export uint16_t get_uint16_slow(unsigned char *c);
-pqc_export uint32_t get_uint32_slow(unsigned char *c);
-pqc_export uint64_t get_uint64_slow(unsigned char *c);
-
 pqc_export uint32_t get_le_uint32(unsigned char *c);
 
 pqc_export int pqc_get_zint32( char *in, uint32_t *v); /* returns number of read bytes, or error -1 */
@@ -76,58 +62,40 @@ pqc_export int pqc_get_string( char *in, char **s, int *len); /* need way to buf
 #define long_int_SWAP(l)	((int64_t) _byteswap_uint64((unsigned __int64) (l)))
 #else
 #define short_int_SWAP(s)				\
-	((int16_t) (((0x00ff & (uint16_t) (s)) << 8) |	\
-		  ((0xff00 & (uint16_t) (s)) >> 8)))
+	(((0x00ff & (uint16_t) (s)) << 8) |	\
+	 ((0xff00 & (uint16_t) (s)) >> 8))
 
 #define normal_int_SWAP(i)						\
-	((int) (((((unsigned) 0xff <<  0) & (unsigned) (i)) << 24) |	\
-		((((unsigned) 0xff <<  8) & (unsigned) (i)) <<  8) |	\
-		((((unsigned) 0xff << 16) & (unsigned) (i)) >>  8) |	\
-		((((unsigned) 0xff << 24) & (unsigned) (i)) >> 24)))
+	(((((uint32_t) 0xff <<  0) & (uint32_t) (i)) << 24) |	\
+	 ((((uint32_t) 0xff <<  8) & (uint32_t) (i)) <<  8) |	\
+	 ((((uint32_t) 0xff << 16) & (uint32_t) (i)) >>  8) |	\
+	 ((((uint32_t) 0xff << 24) & (uint32_t) (i)) >> 24))
 
 #define long_int_SWAP(l)						\
-	((int64_t) (((((uint64_t) 0xff <<  0) & (uint64_t) (l)) << 56) | \
-		((((uint64_t) 0xff <<  8) & (uint64_t) (l)) << 40) |	\
-		((((uint64_t) 0xff << 16) & (uint64_t) (l)) << 24) |	\
-		((((uint64_t) 0xff << 24) & (uint64_t) (l)) <<  8) |	\
-		((((uint64_t) 0xff << 32) & (uint64_t) (l)) >>  8) |	\
-		((((uint64_t) 0xff << 40) & (uint64_t) (l)) >> 24) |	\
-		((((uint64_t) 0xff << 48) & (uint64_t) (l)) >> 40) |	\
-		((((uint64_t) 0xff << 56) & (uint64_t) (l)) >> 56)))
-#endif
-
-#ifdef HAVE_HGE
-#define huge_int_SWAP(h)					\
-	((hge) (((((uhge) 0xff <<   0) & (uhge) (h)) << 120) |	\
-		((((uhge) 0xff <<   8) & (uhge) (h)) << 104) |	\
-		((((uhge) 0xff <<  16) & (uhge) (h)) <<  88) |	\
-		((((uhge) 0xff <<  24) & (uhge) (h)) <<  72) |	\
-		((((uhge) 0xff <<  32) & (uhge) (h)) <<  56) |	\
-		((((uhge) 0xff <<  40) & (uhge) (h)) <<  40) |	\
-		((((uhge) 0xff <<  48) & (uhge) (h)) <<  24) |	\
-		((((uhge) 0xff <<  56) & (uhge) (h)) <<   8) |	\
-		((((uhge) 0xff <<  64) & (uhge) (h)) >>   8) |	\
-		((((uhge) 0xff <<  72) & (uhge) (h)) >>  24) |	\
-		((((uhge) 0xff <<  80) & (uhge) (h)) >>  40) |	\
-		((((uhge) 0xff <<  88) & (uhge) (h)) >>  56) |	\
-		((((uhge) 0xff <<  96) & (uhge) (h)) >>  72) |	\
-		((((uhge) 0xff << 104) & (uhge) (h)) >>  88) |	\
-		((((uhge) 0xff << 112) & (uhge) (h)) >> 104) |	\
-		((((uhge) 0xff << 120) & (uhge) (h)) >> 120)))
+	(((((uint64_t) 0xff <<  0) & (uint64_t) (l)) << 56) |	\
+	 ((((uint64_t) 0xff <<  8) & (uint64_t) (l)) << 40) |	\
+	 ((((uint64_t) 0xff << 16) & (uint64_t) (l)) << 24) |	\
+	 ((((uint64_t) 0xff << 24) & (uint64_t) (l)) <<  8) |	\
+	 ((((uint64_t) 0xff << 32) & (uint64_t) (l)) >>  8) |	\
+	 ((((uint64_t) 0xff << 40) & (uint64_t) (l)) >> 24) |	\
+	 ((((uint64_t) 0xff << 48) & (uint64_t) (l)) >> 40) |	\
+	 ((((uint64_t) 0xff << 56) & (uint64_t) (l)) >> 56))
 #endif
 
 #ifdef WORDS_BIGENDIAN
 #define pqc_sht(s) short_int_SWAP(s)
 #define pqc_int(s) normal_int_SWAP(s)
 #define pqc_lng(s) long_int_SWAP(s)
-#define pqc_hge(s) huge_int_SWAP(s)
 #define pqc_be_sht(s) (s)
 #else
 #define pqc_sht(s) (s)
 #define pqc_int(s) (s)
 #define pqc_lng(s) (s)
-#define pqc_hge(s) (s)
 #define pqc_be_sht(s) short_int_SWAP(s)
 #endif
+
+#define get_uint16(c) pqc_sht(READ_UNALIGNED(16, c))
+#define get_uint32(c) pqc_int(READ_UNALIGNED(32, c))
+#define get_uint64(c) pqc_lng(READ_UNALIGNED(64, c))
 
 #endif /*_PQC_THRIFT_H_*/
